8fa277204b3859131640df556c2346cafcf57a8f
James Adam <james@lazyatom.com>
2011-09-19 16:21:44 +0100

    Quite literally the initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whitehall::Application.config.secret_token = '6b8dc0c531aed727b5eafebc06a0e3a9b8280c462060fd26cded3d99474d5f58650f232a8b68dba2e75046ebc53c7f6351acabc6b2839a22add4ad6a2e531b68'
