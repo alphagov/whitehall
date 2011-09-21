951146b30e868def1159ad32bffc772a68f1cc32
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2011-09-21 15:30:38 +0100

    Added seed data.
    - It's useful to be able to seed data in development and staging environments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def random_policy_text(number_of_paragraphs=3)
+  @policy_data ||= File.read(File.expand_path("../seed_policy_bodies.txt", __FILE__)).split("\n")
+  @policy_data.shuffle[0...number_of_paragraphs].join("\n\n")
+end
+
+alice = User.create!(name: "Alice Anderson")
+bob = User.create!(name: "Bob Bailey")
+clive = User.create!(name: "Clive Custer")
+
+alice.policies.create! title: "Free cats for pensioners", body: random_policy_text, submitted: false
+bob.policies.create! title: "Decriminalise beards", body: random_policy_text(5), submitted: false
+alice.policies.create! title: "Less gravity on Sundays", body: random_policy_text, submitted: true
+clive.policies.create! title: "Ducks pulling chariots of fire", body: random_policy_text(4), submitted: true
