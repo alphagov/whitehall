984c2a70c3c195dd324a84c1220dbbe0aa12887e
Chris Roos & Jason Cale <pairing+chris.roos+jason.cale@gofreerange.com>
2011-09-22 17:05:33 +0100

    Add basic navigation to policies.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def navigation_link(name, path, html_options = {}, &block)
+    link_to_unless_current(name, path, html_options) do
+       link_to(name, path, :class => 'current')
+    end
+  end
+
 end
