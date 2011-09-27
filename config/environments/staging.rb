4e2bc35de5c8c7fae384b8d4eb2e485317eac662
Chris Roos <chris@seagul.co.uk>
2011-09-27 16:57:49 +0100

    Compile assets on staging.
    - Commit 965fb2902b7c160c9f5e2c7ec1797c29ec89df97 introduced a production like staging environment which, among other things, assumed that we were precompiling our assets.  We're not precompiling them which resulted in us seeing the following error:
    
        ActionView::Template::Error (application.css isn't precompiled):
    
    - It's probably better to have them precompiled on deployment but this fix will get the staging site running again in the short term.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,8 @@
 require File.expand_path('production.rb', File.dirname(__FILE__))
 
 Whitehall::Application.configure do
+  # Compile assets on the fly if they're missing on staging
+  config.assets.compile = true
+
   config.whitehall.host = 'whitehall.staging.alphagov.co.uk:8080'
 end
