6420675fdb81f0b86a913de5d8d776496f575acc
James Mead & Tom Ward <pairing+james.mead+tom.ward@gofreerange.com>
2011-09-28 13:23:31 +0100

    We need to prepare the test database from the development environment

--- jenkins.sh
+++ jenkins.sh
@@ -2,4 +2,4 @@
 source '/usr/local/lib/rvm'
 export GEM_HOME="/home/jenkins/bundles/${JOB_NAME}"
 mkdir -p "${GEM_HOME}"
-bundle install && bundle exec rake db:create db:migrate && RAILS_ENV=test bundle exec rake test cucumber --trace
+bundle install && bundle exec rake db:create db:migrate db:test:prepare && RAILS_ENV=test bundle exec rake test cucumber --trace
