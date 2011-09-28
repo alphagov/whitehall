03a04644171ffc2ea138d04904a30383a953c56d
James Adam <james@lazyatom.com>
2011-09-28 11:29:18 +0100

    General email steps are useful.
    We'll need these when we start clicking on links in the email too.

--- features/step_definitions/fact_checking_steps.rb
+++ features/step_definitions/fact_checking_steps.rb
@@ -1,6 +1,5 @@
 Then /^"([^"]*)" should receive an email requesting fact checking$/ do |email_address|
-  assert_equal 1, ActionMailer::Base.deliveries.length
-  fact_check_email = ActionMailer::Base.deliveries.first
-  assert_equal email_address, fact_check_email.to.first
-  assert_match /fact checking request/i, fact_check_email.subject
+  Then %{"#{email_address}" should receive an email}
+  When %{I open the last email sent to "#{email_address}"}
+  Then %{I should see "Fact checking request" in the email subject}
 end
