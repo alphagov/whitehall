alphagov/whitehall

commit b412f37cb6edccdaf65d8329c8599ddbe9cc5253

Allow a writer to attach a file to a policy.
This functionality is very basic at the moment. Notably the files are stored in the filesystem and in the public directory which means they are accessible by the public *before* the policy is published (although there is some security by dint of the obscurity of the URL). While this will almost certainly need to change, the current commit is enough to satisfy the story.

TODO
@@ -1,0 +1,4 @@
+* do we really need multi-line body for policy steps?
+* think about disabled fields in policy form
+* not sure the split between the 3 policy step files is useful
+* remove comments from gemfile

db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110929093706) do
+ActiveRecord::Schema.define(:version => 20110929105024) do
 
   create_table "documents", :force => true do |t|
     t.datetime "created_at"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20110929093706) do
     t.integer  "document_id"
     t.string   "state",         :default => "draft",  :null => false
     t.string   "document_type", :default => "Policy", :null => false
+    t.string   "attachment"
   end
 
   create_table "fact_check_requests", :force => true do |t|

features/step_definitions/policy_steps.rb
@@ -59,3 +59,25 @@ Then /^they should see the draft policy titled "([^"]*)"$/ do |title|
   assert page.has_css?('.policy .title', :text => edition.title)
   assert page.has_css?('.policy .body', :text => edition.body)
 end
+
+When /^I attach a PDF file to the policy$/ do
+  attach_file "Attachment", pdf_attachment
+end
+
+Given /^a published policy titled "([^"]*)" with a PDF attachment$/ do |title|
+  attachment = File.open(pdf_attachment)
+  create(:published_edition, title: title, attachment: attachment)
+end
+
+When /^I visit the policy titled "([^"]*)"$/ do |title|
+  edition = Edition.find_by_title(title)
+  visit policy_path(edition.policy)
+end
+
+Then /^I should see a link to the PDF attachment$/ do
+  assert page.has_css?(".policy_document .attachment a[href*='attachment.pdf']", :text => /^attachment\.pdf$/)
+end
+
+def pdf_attachment
+  Rails.root.join("features/fixtures/attachment.pdf")
+end

.gitignore
@@ -3,3 +3,4 @@ db/*.sqlite3
 log/*.log
 tmp/
 .sass-cache/
+public/uploads/

Gemfile
@@ -19,6 +19,7 @@ end
 gem 'jquery-rails'
 gem 'haml', '~> 3.1.3'
 gem 'transitions', require: ['transitions', 'active_record/transitions']
+gem 'carrierwave'
 
 # Use unicorn as the web server
 # gem 'unicorn'

app/helpers/application_helper.rb
@@ -36,4 +36,9 @@ module ApplicationHelper
     string.split(/(\r?\n){2}/).collect{|paragraph| "<p>#{paragraph}</p>" }.join.html_safe
   end
 
+  def link_to_attachment(attachment)
+    return unless attachment.present?
+    link_to File.basename(attachment.current_path), attachment.url
+  end
+
 end

app/models/edition.rb
@@ -2,6 +2,8 @@ class Edition < ActiveRecord::Base
   include ::Transitions
   include ActiveRecord::Transitions
 
+  mount_uploader :attachment, AttachmentUploader
+
   state_machine do
     state :draft
     state :published

app/uploaders/attachment_uploader.rb
@@ -1,0 +1,49 @@
+# encoding: utf-8
+
+class AttachmentUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or ImageScience support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+  # include CarrierWave::ImageScience
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+
+end

commit efee77466876124b21e4b2f3637225f9f47397da

Attempt to fix build.
Resolve problems accidentally introduced in b412f37cb6edccdaf65d8329c8599ddbe9cc5253.

TODO
@@ -1,4 +1,0 @@
-* do we really need multi-line body for policy steps?
-* think about disabled fields in policy form
-* not sure the split between the 3 policy step files is useful
-* remove comments from gemfile

db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(:version => 20110929105024) do
     t.integer  "lock_version",  :default => 0
     t.integer  "document_id"
     t.string   "state",         :default => "draft",  :null => false
-    t.string   "document_type", :default => "Policy", :null => false
     t.string   "attachment"
+    t.string   "document_type", :default => "Policy", :null => false
   end
 
   create_table "fact_check_requests", :force => true do |t|

features/step_definitions/policy_steps.rb
@@ -71,7 +71,7 @@ end
 
 When /^I visit the policy titled "([^"]*)"$/ do |title|
   edition = Edition.find_by_title(title)
-  visit policy_path(edition.policy)
+  visit policy_path(edition.document)
 end
 
 Then /^I should see a link to the PDF attachment$/ do