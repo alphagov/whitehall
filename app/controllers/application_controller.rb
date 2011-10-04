7b33addd3b998d695e9ec31a9c8832a842882d43
Jason Cale <jase@gofreerange.com>
2011-10-04 15:58:37 +0100

    Very simple style updates to the admin interface.
      * Ensure normalise is included before any other css files.
      * Remove any unused declarations
      * Link to admin home in the page title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
   
   layout 'website'
 
@@ -15,5 +15,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
