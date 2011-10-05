31fdea2c691ad830b2273f852f4761fd2e934eb1
Chris Roos <chris@seagul.co.uk>
2011-10-05 11:56:34 +0100

    Persist uploads across deploys.
    Capistrano symlinks the Rails.root/public/system directory to the shared directory.  Uploading our attachments to this directory will ensure that they persist across deploys.
    
    *NOTE* You'll either want to delete your local public/uploads directory, or move it to public/system/uploads.

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -4,7 +4,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_white_list
--- test/unit/attachment_uploader_test.rb
+++ test/unit/attachment_uploader_test.rb
@@ -5,4 +5,9 @@ class AttachmentUploaderTest < ActiveSupport::TestCase
     uploader = AttachmentUploader.new
     assert_equal %w(pdf), uploader.extension_white_list
   end
+  test "should store uploads in a directory that persists across deploys" do
+    model = stub("AR Model", id: 1)
+    uploader = AttachmentUploader.new(model, "mounted-as")
+    assert_match /^system/, uploader.store_dir
+  end
 end
