fd05d315f0c7a9517afde064c7b2dd683ae6e251
Tom Ward <tom@popdog.net>
2011-10-05 16:54:41 +0100

    Make it more explicit that we expect to see the policy

--- test/functional/topics_controller_test.rb
+++ test/functional/topics_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
-  test "should only see published policies" do
-    published_edition = build(:published_edition)
-    topic = create(:topic, editions: [published_edition, build(:draft_edition)])
+  test "shows only published policies" do
+    published_policy = create(:published_policy)
+    topic = create(:topic, editions: [published_policy.published_edition, build(:draft_edition)])
 
     get :show, id: topic.to_param
 
-    assert_select_object(published_edition.document)
+    assert_select_object(published_policy)
   end
 end
