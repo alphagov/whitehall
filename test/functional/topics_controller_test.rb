410cb50501cb8329f1b0bca1e24563a19e9ebcb5
Tom Ward <tom@popdog.net>
2011-10-05 17:16:05 +0100

    Show a description alongside each topic

--- test/functional/topics_controller_test.rb
+++ test/functional/topics_controller_test.rb
@@ -1,12 +1,17 @@
-require 'test_helper'
+require "test_helper"
 
 class TopicsControllerTest < ActionController::TestCase
-  test "shows only published policies" do
+  test "shows topic title and description" do
+    topic = create(:topic)
+    get :show, id: topic.to_param
+    assert_select ".topic .name", text: topic.name
+    assert_select ".topic .description", text: topic.description
+  end
+
+  test "shows only published policies associated with topic" do
     published_policy = create(:published_policy)
     topic = create(:topic, editions: [published_policy.published_edition, build(:draft_edition)])
-
     get :show, id: topic.to_param
-
     assert_select_object(published_policy)
   end
 end
