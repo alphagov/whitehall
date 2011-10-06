975cb567cd06e70b0e62d895a4f89c4ff0c60469
Jason Cale <jase@gofreerange.com>
2011-10-06 15:04:44 +0100

    Tentative move towards a fluid grid.
    This is a little bit one step forward two steps back .. but it will give a better base to work from I think.

--- app/assets/javascripts/whitehall.js
+++ app/assets/javascripts/whitehall.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($) {
   $(".flash.notice, .flash.alert").flashNotice();
+
+  // This is useful for toggling CSS helpers
+  // whilst developing..
+  $('body').keydown(function(event) {
+    if (event.keyCode == '66') {
+      event.preventDefault();
+      $(this).toggleClass('dev');
+    }
+  });
 });
