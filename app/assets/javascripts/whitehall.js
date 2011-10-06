9e37ae170537b627bff747d35fbae71d799a21b1
Jason Cale <jase@gofreerange.com>
2011-10-06 17:12:42 +0100

    Added need for cmd key to be pressed alongside 'g' to toggle .dev class on body.

--- app/assets/javascripts/whitehall.js
+++ app/assets/javascripts/whitehall.js
@@ -2,11 +2,19 @@ jQuery(document).ready(function($) {
   $(".flash.notice, .flash.alert").flashNotice();
 
   // This is useful for toggling CSS helpers
-  // whilst developing..
+  // whilst developing.. cmd+G
+  var cmdDown = false;
   $('body').keydown(function(event) {
-    if (event.keyCode == '66') {
+    if (event.keyCode == '91') {
+      cmdDown = true;
+    }
+    if (cmdDown && event.keyCode == '71') {
       event.preventDefault();
       $(this).toggleClass('dev');
+    }
+  }).keyup(function(event) {
+    if (event.keyCode == '91') {
+      cmdDown = false;
     }
   });
 });
