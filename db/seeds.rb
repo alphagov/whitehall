4d83a3d3fb4866611bf25d89b096b543401cf0d0
Chris Roos <chris@seagul.co.uk>
2011-10-07 16:17:19 +0100

    Remove unnecessary comment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 def random_policy_text(number_of_paragraphs=3)
   @policy_data ||= File.read(File.expand_path("../seed_policy_bodies.txt", __FILE__)).split("\n")
   @policy_data.shuffle[0...number_of_paragraphs].join("\n\n")
