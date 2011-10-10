69fa26d5fd2d3c33b7d304b137da1f734223cdfb
Chris Roos & Tom Ward <pairing+chris.roos+tom.ward@gofreerange.com>
2011-10-10 11:32:42 +0100

    Replace handcrafted sql with a defined association

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,17 +1,8 @@
 class Document < ActiveRecord::Base
   has_many :editions
+  has_one :published_edition, class_name: 'Edition', conditions: { state: 'published' }
 
   def self.published
-    where %{
-      EXISTS (
-        SELECT 1 FROM editions AS published_editions
-        WHERE published_editions.document_id = documents.id
-        AND published_editions.state = 'published'
-      )
-    }
-  end
-
-  def published_edition
-    editions.published.first
+    joins(:published_edition)
   end
 end
