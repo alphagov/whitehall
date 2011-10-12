e343be0e4caee3a90f72f69526ffaf153ad9ea69
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2011-10-12 16:15:37 +0100

    Clarify which actions require a logged in user.
    When we modified this test earlier, we were struck that an Admin controller didn't have any authentication around it. Only after looking in more detail was it clear that only the `create` action should require a login; the other actions are authenticated using a token.
    
    We still need to ensure a user is logged in for `create`, but this should hopefully clarify things in the meantime.

--- test/functional/admin/fact_check_requests_controller_test.rb
+++ test/functional/admin/fact_check_requests_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
+
 class Admin::FactCheckRequestsControllerTest < ActionController::TestCase
   setup do
-    login_as "George"
     @document = create(:draft_policy)
+  end
+
+  test 'users with a valid token should be able to access the policy' do
+    fact_check_request = create(:fact_check_request, document: @document)
+    get :edit, document_id: @document.to_param, id: fact_check_request.token
+    assert_response :success
+    assert_template 'admin/fact_check_requests/edit'
+  end
+
+  test 'users with invalid tokens should not be able to access the policy' do
+    get :edit, id: 'invalid-token', document_id: @document.to_param
+
+    assert_response :not_found
   end
 
   test "should render the content using govspeak markup" do
@@ -19,13 +32,6 @@ class Admin::FactCheckRequestsControllerTest < ActionController::TestCase
     assert_select ".body", text: "body-text-as-govspeak"
   end
 
-  test 'users with a valid token should be able to access the policy' do
-    fact_check_request = create(:fact_check_request, document: @document)
-    get :edit, document_id: @document.to_param, id: fact_check_request.token
-    assert_response :success
-    assert_template 'admin/fact_check_requests/edit'
-  end
-
   test 'turn govspeak into nice markup when editing' do
     @document.update_attributes!(body: "body-text")
     fact_check_request = create(:fact_check_request, document: @document)
@@ -39,10 +45,20 @@ class Admin::FactCheckRequestsControllerTest < ActionController::TestCase
     assert_select ".body", text: "body-text-as-govspeak"
   end
 
-  test 'users with invalid tokens should not be able to access the policy' do
-    get :edit, id: 'invalid-token', document_id: @document.to_param
+  test "redirect to the show page when a fact check has been completed" do
+    fact_check_request = create(:fact_check_request, document: @document)
+    put :update, document_id: @document.to_param, id: fact_check_request.to_param,
+        fact_check_request: {email_address: 'fact-checker@example.com', comments: 'looks fine to me'}
+    assert_redirected_to admin_document_fact_check_request_path(@document, fact_check_request)
+  end
+end
 
-    assert_response :not_found
+class Admin::CreatingFactCheckRequestsControllerTest < ActionController::TestCase
+  tests Admin::FactCheckRequestsController
+
+  setup do
+    @document = create(:draft_policy)
+    login_as "George"
   end
 
   test "should send an email when a fact check has been requested" do
@@ -76,12 +92,4 @@ class Admin::FactCheckRequestsControllerTest < ActionController::TestCase
     post :create, document_id: @document.to_param, fact_check_request: {email_address: ''}
     assert_redirected_to edit_admin_document_path(@document)
   end
-
-  test "redirect to the show page when a fact check has been completed" do
-    fact_check_request = create(:fact_check_request, document: @document)
-    put :update, document_id: @document.to_param, id: fact_check_request.to_param,
-        fact_check_request: {email_address: 'fact-checker@example.com', comments: 'looks fine to me'}
-    assert_redirected_to admin_document_fact_check_request_path(@document, fact_check_request)
-  end
-
 end
