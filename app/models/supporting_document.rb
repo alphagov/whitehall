c766f728d55002cbcdeb96578e960885b323b57b
James Mead & Tom Ward <pairing+james.mead+tom.ward@gofreerange.com>
2011-10-13 12:50:30 +0100

    A document should be marked as updated if a supporting document is added or updated
    Doing this prevents publication of a document whose supporting documents have changed.

--- app/models/supporting_document.rb
+++ app/models/supporting_document.rb
@@ -2,4 +2,8 @@ class SupportingDocument < ActiveRecord::Base
   belongs_to :document
 
   validates :title, :body, :document, presence: true
+
+  after_save do
+    document.touch
+  end
 end
--- test/unit/supporting_document_test.rb
+++ test/unit/supporting_document_test.rb
@@ -20,4 +20,23 @@ class SupportingDocumentTest < ActiveSupport::TestCase
     supporting_document = build(:supporting_document, document: nil)
     refute supporting_document.valid?
   end
+
+  test "mark parent document as updated when supporting document is created" do
+    parent_document = create(:draft_policy)
+    lock_version = parent_document.lock_version
+
+    supporting_document = create(:supporting_document, document: parent_document)
+
+    refute_equal lock_version, parent_document.reload.lock_version
+  end
+
+  test "mark parent document as updated when supporting document is updated" do
+    supporting_document = create(:supporting_document)
+    parent_document = supporting_document.document
+    lock_version = parent_document.lock_version
+
+    supporting_document.update_attributes!(title: 'New title')
+
+    refute_equal lock_version, parent_document.reload.lock_version
+  end
 end
