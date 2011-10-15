9e4f6f7969e1e4e2f9c1fa2e0e779fa043ee5f4b
Tom Ward <tom@popdog.net>
2011-10-15 11:11:58 +0100

    Reshuffle!
    This clearly isn't the best way to deal with reshuffles, but until we have some sort of admin for ministers I think it's the best we can do.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111014153312) do
+ActiveRecord::Schema.define(:version => 20111015100013) do
 
   create_table "attachments", :force => true do |t|
     t.string   "name"
