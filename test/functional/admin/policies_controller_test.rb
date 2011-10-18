8bc6cca46c487d1adee2b0864c55ceb2c8d90620
Jason Cale <jase@gofreerange.com>
2011-10-18 16:14:37 +0100

    Supporting document creation should be separate from editing document actions.

--- test/functional/admin/policies_controller_test.rb
+++ test/functional/admin/policies_controller_test.rb
@@ -148,10 +148,10 @@ class Admin::PoliciesControllerTest < ActionController::TestCase
 
     assert_select ".supporting_documents" do
       assert_select_object(first_supporting_document) do
-        assert_select "a[href='#{admin_supporting_document_path(first_supporting_document)}']", text: first_supporting_document.title
+        assert_select "a[href='#{admin_supporting_document_path(first_supporting_document)}'] span.title", text: first_supporting_document.title
       end
       assert_select_object(second_supporting_document) do
-        assert_select "a[href='#{admin_supporting_document_path(second_supporting_document)}']", text: second_supporting_document.title
+        assert_select "a[href='#{admin_supporting_document_path(second_supporting_document)}'] span.title", text: second_supporting_document.title
       end
     end
   end
@@ -161,6 +161,6 @@ class Admin::PoliciesControllerTest < ActionController::TestCase
 
     get :show, id: draft_policy
 
-    assert_select ".supporting_documents", count: 0
+    assert_select ".supporting_documents .supporting_document", count: 0
   end
 end
