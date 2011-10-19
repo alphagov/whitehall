a669b8e4ceb7ff1c53f34d27f3985c40270c5aec
James Mead <james@floehopper.org>
2011-10-19 17:35:50 +0100

    Avoid accidentally leaving features or scenarios tagged @wip.

---
+++ test/unit/wip_features_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class WipFeaturesTest < ActiveSupport::TestCase
+
+  test "do not commit features tagged work-in-progress" do
+    directories = Dir.glob(File.join(Rails.root, 'features', '**', '*.feature'))
+    matching_files = directories.select do |filename|
+      match = false
+      File.open(filename) do |file|
+        match = file.grep(%r{@wip}).any?
+      end
+      match
+    end
+    assert_equal [], matching_files, %{Avoid leaving features or scenarios tagged with @wip}
+  end
+
+end
