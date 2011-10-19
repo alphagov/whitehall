dc6d5f9c567e57d4a768b5f60036ff6e1f39df88
Tom Ward <tom@popdog.net>
2011-10-19 16:35:38 +0100

    Use a namespaced model to allow migrations to work even when MinisterialAppointment is no longer available

--- db/migrate/20111018145329_add_bounding_times_to_ministerial_appointment.rb
+++ db/migrate/20111018145329_add_bounding_times_to_ministerial_appointment.rb
@@ -1,8 +1,12 @@
 class AddBoundingTimesToMinisterialAppointment < ActiveRecord::Migration
+  class MinisterialAppointmentTable < ActiveRecord::Base
+    set_table_name :ministerial_appointments
+  end
+
   def change
     add_column :ministerial_appointments, :started_at, :time
     add_column :ministerial_appointments, :ended_at, :time, default: nil
 
-    MinisterialAppointment.update_all(started_at: 2.years.ago)
+    MinisterialAppointmentTable.update_all(started_at: 2.years.ago)
   end
 end
--- db/migrate/20111019133513_change_ministerial_appointments_bounds_to_datetimes.rb
+++ db/migrate/20111019133513_change_ministerial_appointments_bounds_to_datetimes.rb
@@ -1,8 +1,12 @@
 class ChangeMinisterialAppointmentsBoundsToDatetimes < ActiveRecord::Migration
+  class MinisterialAppointmentTable < ActiveRecord::Base
+    set_table_name :ministerial_appointments
+  end
+
   def change
     change_column :ministerial_appointments, :started_at, :datetime
     change_column :ministerial_appointments, :ended_at, :datetime, default: nil
 
-    MinisterialAppointment.update_all(started_at: 2.years.ago)
+    MinisterialAppointmentTable.update_all(started_at: 2.years.ago)
   end
 end
