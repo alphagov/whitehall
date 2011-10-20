6e7f4905ec5dfe59f5d2bbab1c2943ffa5473193
Tom Ward <tom@popdog.net>
2011-10-20 12:46:18 +0100

    admin.css and website.css both need to be precompiled (as would any css file referred to directly from a layout)
    By default, rails will only precompile application.css, which we are not using.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,6 @@
 require File.expand_path('production.rb', File.dirname(__FILE__))
 
 Whitehall::Application.configure do
-  # Prevent `ActionView::Template::Error (application.css isn't precompiled)` on staging
-  config.assets.compile = true
-
   # Prevent `OpenSSL::SSL::SSLError (hostname was not match with the server certificate)` on staging
   config.action_mailer.smtp_settings = {enable_starttls_auto: false}
 end
