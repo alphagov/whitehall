1fd3517aa94f079ac3b28c4b0022451da9971660
Paul Battley <pbattley@gmail.com>
2011-10-24 16:46:09 +0100

    Remove RVM from build script.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -x
-source '/usr/local/lib/rvm'
-export GEM_HOME="/home/jenkins/bundles/${JOB_NAME}"
-mkdir -p "${GEM_HOME}"
-bundle install && bundle exec rake db:create db:migrate db:test:prepare && RAILS_ENV=test bundle exec rake test cucumber --trace
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake db:create db:migrate db:test:prepare && RAILS_ENV=test bundle exec rake test cucumber --trace
