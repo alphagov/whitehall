bd864b232566d5672f781a98c5f5d316a87d829e
James Mead <james@floehopper.org>
2011-10-24 08:03:06 +0100

    Show who delivered speech and when & where it was delivered on admin page.
    I hope the markup isn't too horrible. My main thought was that this should be part of the document article and not in a separate section, but this may be wrong.
    
    I wonder whether "Written by" is so appropriate for speeches, given that the speech may have been written by someone other than the person entering it into the system. But perhaps this is an issue for all documents...?

--- test/functional/admin/speeches_controller_test.rb
+++ test/functional/admin/speeches_controller_test.rb
@@ -153,4 +153,19 @@ class Admin::SpeechesControllerTest < ActionController::TestCase
 
     assert_select ".body", text: "body-in-html"
   end
+
+  test "should display who delivered the speech, and when & where it was delivered" do
+    home_office = create(:organisation, name: "Home Office")
+    home_secretary = create(:ministerial_role, name: "Secretary of State", organisations: [home_office])
+    theresa_may = create(:person, name: "Theresa May")
+    theresa_may_appointment = create(:role_appointment, role: home_secretary, person: theresa_may)
+    draft_speech = create(:draft_speech, role_appointment: theresa_may_appointment, delivered_on: Date.parse("2011-06-01"), location: "The Guidhall")
+
+    get :show, id: draft_speech
+
+    assert_select ".delivery .ministerial_role", "Theresa May (Secretary of State, Home Office)"
+    assert_select ".delivery .date", "June 1st, 2011"
+    assert_select ".delivery .location", "The Guidhall"
+  end
+
 end
