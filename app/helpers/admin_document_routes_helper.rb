704cfb85715a735445601f0614803a9d3f13ccca
James Mead <james@floehopper.org>
2011-10-25 16:09:05 +0100

    AdminDocumentRoutesHelper#documents_collection_route is no longer used.

--- app/helpers/admin_document_routes_helper.rb
+++ app/helpers/admin_document_routes_helper.rb
@@ -1,17 +1,6 @@
 module AdminDocumentRoutesHelper
   DOCUMENT_TYPES = [Policy, Publication, NewsArticle, Consultation, Speech]
 
-  def self.documents_collection_route(name)
-    DOCUMENT_TYPES.each do |type|
-      method_name = name.to_s.gsub("admin_documents", "admin_#{type.model_name.plural}")
-      class_eval %{
-        def #{method_name}(*args)
-          #{name}(*args)
-        end
-      }
-    end
-  end
-
   def self.document_instance_route(name)
     DOCUMENT_TYPES.each do |type|
       method_name = name.to_s.gsub("admin_document", "admin_#{type.model_name.singular}")
