5b62251bbbc32e02818b623d688b8504569ff95c
Jason Cale <jase@gofreerange.com>
2011-10-26 23:34:11 +0100

    Tidy up (slightly) documentInList plugin.

--- app/assets/javascripts/document_list.js
+++ app/assets/javascripts/document_list.js
@@ -1,14 +1,12 @@
 (function ($) {
-
-  var _documentInList = function() {
+  var _documentInList = function(titleSel, supportingDocSel, toWrap) {
     $(this).each(function() {
       var document = $(this);
-      var title_col = $(document).find('td.title');
+      var title_col = $(document).find(titleSel);
       $(this).find('td.title').each(function () {
         var supporting_documents;
-        if ((supporting_documents = $(this).find('.supporting_documents')).length > 0) {
+        if ((supporting_documents = $(this).find(supportingDocSel)).length > 0) {
           var toggle_link_str = supporting_documents.find('li').length + ' supporting documents';
-
           var grp = $.div('', {'class': 'toggle_group group'});
           var toggle_link = $.a(toggle_link_str, { 'class':'toggle', 'href': "#"+document.attr('id') });
           toggle_link.click(function () {
@@ -16,18 +14,17 @@
             toggle_link.text(supporting_documents.is(':visible') ? 'hide' : toggle_link_str);
             return false;
           });
-          $(this).find('h2').wrap(grp).after(toggle_link);
+          $(this).find(toWrap).wrap(grp).after(toggle_link);
           supporting_documents.hide();
         };
       });
     });
   };
-
   $.fn.extend({
     documentInList: _documentInList
   });
+  $(function() {
+    $(".resource_list table.documents tr").documentInList('td.title', '.supporting_documents', 'h2');
+  });
 })(jQuery);
 
-jQuery(function() {
-  jQuery(".resource_list table.documents tr").documentInList();
-})
