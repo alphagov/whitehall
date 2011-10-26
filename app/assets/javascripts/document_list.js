eae97597779b1b2df541a741c9697d22fdfae0d8
Jason Cale <jase@gofreerange.com>
2011-10-26 18:08:28 +0100

    Anchor supporting document toggle to document id so user doesn't jump about everywhere when clicking on the link, and float the expand link to the right (mobile version should be floated left ... )

--- app/assets/javascripts/document_list.js
+++ app/assets/javascripts/document_list.js
@@ -1,18 +1,20 @@
 (function ($) {
 
-  var _documentList = function() {
+  var _documentInList = function() {
     $(this).each(function() {
-      var title_col = $(this).find('td.title');
+      var document = $(this);
+      var title_col = $(document).find('td.title');
       $(this).find('td.title').each(function () {
         var supporting_documents;
         if ((supporting_documents = $(this).find('.supporting_documents')).length > 0) {
           var toggle_link_str = supporting_documents.find('li').length + ' supporting documents';
 
           var grp = $.div('', {'class': 'toggle_group group'});
-          var toggle_link = $.a(toggle_link_str, { 'class':'toggle', 'href':'#' });
+          var toggle_link = $.a(toggle_link_str, { 'class':'toggle', 'href': "#"+document.attr('id') });
           toggle_link.click(function () {
             supporting_documents.toggle();
             toggle_link.text(supporting_documents.is(':visible') ? 'hide' : toggle_link_str);
+            return false;
           });
           $(this).find('h2').wrap(grp).after(toggle_link);
           supporting_documents.hide();
@@ -22,10 +24,10 @@
   };
 
   $.fn.extend({
-    documentList: _documentList
+    documentInList: _documentInList
   });
 })(jQuery);
 
 jQuery(function() {
-  jQuery(".resource_list table.documents").documentList();
+  jQuery(".resource_list table.documents tr").documentInList();
 })
