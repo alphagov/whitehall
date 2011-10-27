ea84ad23eed867de0fed6b93577cb9c07664b6f0
James Mead <james@floehopper.org>
2011-10-27 15:05:09 +0100

    Extract controller duplication relating to nation inapplicabilities into a module.

--- app/controllers/admin/consultations_controller.rb
+++ app/controllers/admin/consultations_controller.rb
@@ -1,58 +1,10 @@
 class Admin::ConsultationsController < Admin::DocumentsController
 
-  before_filter :build_nation_inapplicabilities, only: [:new, :edit]
-
-  def create
-    nation_inapplicabilities_attributes = params[:document].delete(:nation_inapplicabilities_attributes) || {}
-    @document = document_class.new(params[:document].merge(author: current_user))
-    if @document.valid?
-      @document.nation_inapplicabilities_attributes = nation_inapplicabilities_attributes
-    end
-    if @document.save
-      redirect_to admin_document_path(@document), notice: "The document has been saved"
-    else
-      flash.now[:alert] = "There are some problems with the document"
-      build_nation_inapplicabilities
-      nation_inapplicabilities_attributes.each do |index, attributes|
-        @document.nation_inapplicabilities[index.to_i][:_destroy] = attributes[:_destroy]
-        @document.nation_inapplicabilities[index.to_i][:alternative_url] = attributes[:alternative_url]
-      end
-      render action: "new"
-    end
-  end
-
-  def update
-    nation_inapplicabilities_attributes = params[:document].delete(:nation_inapplicabilities_attributes) || {}
-    @document.attributes = params[:document]
-    if @document.valid?
-      @document.nation_inapplicabilities_attributes = nation_inapplicabilities_attributes
-    end
-    if @document.save
-      redirect_to admin_document_path(@document),
-        notice: "The document has been saved"
-    else
-      flash.now[:alert] = "There are some problems with the document"
-      build_nation_inapplicabilities
-      nation_inapplicabilities_attributes.each do |index, attributes|
-        @document.nation_inapplicabilities[index.to_i][:_destroy] = attributes[:_destroy]
-        @document.nation_inapplicabilities[index.to_i][:alternative_url] = attributes[:alternative_url]
-      end
-      render action: "edit"
-    end
-  rescue ActiveRecord::StaleObjectError
-    flash.now[:alert] = "This document has been saved since you opened it"
-    @conflicting_document = Document.find(params[:id])
-    @document.lock_version = @conflicting_document.lock_version
-    render action: "edit"
-  end
+  include Admin::Documents::NationalApplicability
 
   private
 
   def document_class
     Consultation
-  end
-
-  def build_nation_inapplicabilities
-    @document.build_nation_applicabilities_for_all_nations
   end
 end
---
+++ app/controllers/admin/documents/national_applicability.rb
@@ -0,0 +1,60 @@
+module Admin::Documents::NationalApplicability
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :build_nation_inapplicabilities, only: [:new, :edit]
+  end
+
+  def create
+    nation_inapplicabilities_attributes = params[:document].delete(:nation_inapplicabilities_attributes) || {}
+    @document = document_class.new(params[:document].merge(author: current_user))
+    if @document.valid?
+      @document.nation_inapplicabilities_attributes = nation_inapplicabilities_attributes
+    end
+    if @document.save
+      redirect_to admin_document_path(@document), notice: "The document has been saved"
+    else
+      flash.now[:alert] = "There are some problems with the document"
+      build_nation_inapplicabilities
+      populate_nation_inapplicabilities_from(nation_inapplicabilities_attributes)
+      render action: "new"
+    end
+  end
+
+  def update
+    nation_inapplicabilities_attributes = params[:document].delete(:nation_inapplicabilities_attributes) || {}
+    @document.attributes = params[:document]
+    if @document.valid?
+      @document.nation_inapplicabilities_attributes = nation_inapplicabilities_attributes
+    end
+    if @document.save
+      redirect_to admin_document_path(@document),
+        notice: "The document has been saved"
+    else
+      flash.now[:alert] = "There are some problems with the document"
+      build_nation_inapplicabilities
+      populate_nation_inapplicabilities_from(nation_inapplicabilities_attributes)
+      render action: "edit"
+    end
+  rescue ActiveRecord::StaleObjectError
+    flash.now[:alert] = "This document has been saved since you opened it"
+    @conflicting_document = Document.find(params[:id])
+    @document.lock_version = @conflicting_document.lock_version
+    render action: "edit"
+  end
+
+  private
+
+  def build_nation_inapplicabilities
+    @document.build_nation_applicabilities_for_all_nations
+  end
+
+  def populate_nation_inapplicabilities_from(attributes)
+    attributes.each do |index, hash|
+      inapplicability = @document.nation_inapplicabilities[index.to_i]
+      inapplicability[:_destroy] = hash[:_destroy]
+      inapplicability[:alternative_url] = hash[:alternative_url]
+    end
+  end
+
+end
