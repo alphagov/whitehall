3e59f985db455cd056d6c6e73676b1feb78da744
James Mead <james@floehopper.org>
2011-11-03 22:37:11 +0000

    Basic email notifications for fact check responses.
    * Email only sent if requestor has an email address.
    * Email links to fact check comment anchor on admin document page.

--- test/functional/notifications_test.rb
+++ test/functional/notifications_test.rb
@@ -1,55 +1,116 @@
 require 'test_helper'
 
-class NotificationsTest < ActionMailer::TestCase
+class NotificationsFactCheckRequestTest < ActionMailer::TestCase
   enable_url_helpers
 
   setup do
     @policy = create(:policy, title: "<policy-title>")
     @requestor = create(:fact_check_requestor, name: "<requestor-name>")
     @request = create(:fact_check_request,
-      email_address: 'fact-check@example.com',
+      email_address: 'fact-checker@example.com',
       document: @policy,
       requestor: @requestor
     )
-    @mail = Notifications.fact_check(@request, host: "example.com")
+    @mail = Notifications.fact_check_request(@request, host: "example.com")
   end
 
-  test "fact check should be sent to the specified email address" do
-    assert_equal ['fact-check@example.com'], @mail.to
+  test "email should be sent to the fact checker email address" do
+    assert_equal ['fact-checker@example.com'], @mail.to
   end
 
-  test "fact check should be sent from a generic email address" do
-    assert_equal ["fact-check-request@example.com"], @mail.from
+  test "email should be sent from a generic email address" do
+    assert_equal ["fact-checking@example.com"], @mail.from
   end
 
-  test "fact check subject contains the name of the requestor and document title" do
+  test "email subject should include the name of the requestor and the document title" do
     assert_equal "Fact checking request from <requestor-name>: <policy-title>", @mail.subject
   end
 
-  test "fact check email should contain a policy link containing a token" do
+  test "email body should contain a link to the fact checking comment page" do
     url = edit_admin_fact_check_request_url(@request)
     assert_match Regexp.new(url), @mail.body.to_s
   end
 
-  test "fact check body contains the title of the document to be checked" do
+  test "email body should contain the title of the document to be checked" do
     assert_match %r{<policy-title>}, @mail.body.to_s
   end
 
-  test "fact check body contains the type of the document to be checked" do
+  test "email body should contain the type of the document to be checked" do
     assert_match %r{policy}, @mail.body.to_s
   end
 
-  test "fact check request instructions shouldn't be escaped in the body" do
+  test "email body should contain unescaped instructions" do
     request = create(:fact_check_request, instructions: %{Don't escape "this" text})
-    mail = Notifications.fact_check(request, host: "example.com")
+    mail = Notifications.fact_check_request(request, host: "example.com")
 
     assert_match %r{Don't escape "this" text}, mail.body.to_s
   end
 
-  test "document titles shouldn't be escaped in the body" do
+  test "email body should contain unescaped document title" do
     policy = create(:policy, title: %{Use "double quotes" everywhere})
     request = create(:fact_check_request, document: policy)
-    mail = Notifications.fact_check(request, host: "example.com")
+    mail = Notifications.fact_check_request(request, host: "example.com")
+
+    assert_match %r{Use "double quotes" everywhere}, mail.body.to_s
+  end
+end
+
+class NotificationsFactCheckResponseTest < ActionMailer::TestCase
+  enable_url_helpers
+
+  include ActionController::RecordIdentifier
+  include AdminDocumentRoutesHelper
+
+  setup do
+    @policy = create(:policy, title: "<policy-title>")
+    @requestor = create(:fact_check_requestor,
+      name: "<requestor-name>",
+      email_address: "fact-check-requestor@example.com"
+    )
+    @request = create(:fact_check_request,
+      email_address: 'fact-checker@example.com',
+      document: @policy,
+      requestor: @requestor
+    )
+    @mail = Notifications.fact_check_response(@request, host: "example.com")
+  end
+
+  test "email should be sent to the requestor email address" do
+    assert_equal ['fact-check-requestor@example.com'], @mail.to
+  end
+
+  test "email should be sent from a generic email address" do
+    assert_equal ["fact-checking@example.com"], @mail.from
+  end
+
+  test "email subject should include the name of the requestor and the document title" do
+    assert_equal "Fact check comment added by fact-checker@example.com: <policy-title>", @mail.subject
+  end
+
+  test "email body should contain a link to the comment on the document page" do
+    url = admin_document_url(@request.document, anchor: dom_id(@request))
+    assert_match Regexp.new(url), @mail.body.to_s
+  end
+
+  test "email body should contain the title of the document to be checked" do
+    assert_match %r{<policy-title>}, @mail.body.to_s
+  end
+
+  test "email body should contain the type of the document to be checked" do
+    assert_match %r{policy}, @mail.body.to_s
+  end
+
+  test "email body should contain unescaped instructions" do
+    request = create(:fact_check_request, instructions: %{Don't escape "this" text})
+    mail = Notifications.fact_check_response(request, host: "example.com")
+
+    assert_match %r{Don't escape "this" text}, mail.body.to_s
+  end
+
+  test "email body should contain unescaped document title" do
+    policy = create(:policy, title: %{Use "double quotes" everywhere})
+    request = create(:fact_check_request, document: policy)
+    mail = Notifications.fact_check_request(request, host: "example.com")
 
     assert_match %r{Use "double quotes" everywhere}, mail.body.to_s
   end
