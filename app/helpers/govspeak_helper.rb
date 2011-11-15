be5462fe04ed4c930907a895f680c9b6894f62b2
James Adam <james@lazyatom.com>
2011-11-15 16:32:19 +0000

    Markup deleted supporting document links appropriately.
    This could probably do with refactoring.

--- app/helpers/govspeak_helper.rb
+++ app/helpers/govspeak_helper.rb
@@ -6,12 +6,16 @@ module GovspeakHelper
     doc.search('a').each do |anchor|
       next unless is_internal_admin_link?(anchor['href'])
       document, supporting_document = find_documents_from_uri(anchor['href'])
-      if document.published?
-        public_uri = rewritten_href_for_documents(document, supporting_document)
-        anchor.replace %{<span class="published_link">#{anchor} <sup class="explanation">(<a class="public_link" href="#{public_uri}">public link</a>)</sup></span>}
+      if document
+        if document.published?
+          public_uri = rewritten_href_for_documents(document, supporting_document)
+          anchor.replace %{<span class="published_link">#{anchor} <sup class="explanation">(<a class="public_link" href="#{public_uri}">public link</a>)</sup></span>}
+        else
+          inner_element = document.deleted? ? %{<del>#{anchor.inner_text}</del>} : anchor
+          anchor.replace %{<span class="#{document.state}_link">#{inner_element} <sup class="explanation">(#{document.state})</sup></span>}
+        end
       else
-        inner_element = document.deleted? ? %{<del>#{anchor.inner_text}</del>} : anchor
-        anchor.replace %{<span class="#{document.state}_link">#{inner_element} <sup class="explanation">(#{document.state})</sup></span>}
+        anchor.replace %{<span class="deleted_link"><del>#{anchor.inner_text}</del> <sup class="explanation">(deleted)</sup></span>}
       end
     end
     doc.to_html.html_safe
@@ -23,7 +27,7 @@ module GovspeakHelper
     doc.search('a').each do |anchor|
       next unless is_internal_admin_link?(anchor['href'])
       document, supporting_document = find_documents_from_uri(anchor['href'])
-      if document.published?
+      if document && document.published?
         anchor['href'] = rewritten_href_for_documents(document, supporting_document)
       else
         anchor.replace anchor.inner_text
@@ -44,8 +48,16 @@ module GovspeakHelper
   def find_documents_from_uri(uri)
     id = uri[/\/([^\/]+)$/, 1]
     if uri =~ /\/supporting_documents\//
-      supporting_document = SupportingDocument.find(id)
-      document = supporting_document.document
+      begin
+        supporting_document = SupportingDocument.find(id)
+      rescue ActiveRecord::RecordNotFound
+        supporting_document = nil
+      end
+      if supporting_document
+        document = supporting_document.document
+      else
+        document = nil
+      end
     else
       document = Document.send(:with_exclusive_scope) { Document.find(id) }
       supporting_document = nil
