ed3b29402424e323ab10982af0b2d2e1fb216883
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2011-11-24 11:41:30 +0000

    Use jQuery from the slimmer-included template.
    We need to defer loading of our javascripts so they aren't executed before those from the template; otherwise jQuery won't be available.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,6 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
 //= require jquery_ujs
 //= require_tree ./common
 //= require_tree ./application
