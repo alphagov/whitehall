9ecb689949003c9e97aed3b84e116c2ad942589f
James Adam <james@lazyatom.com>
2011-11-24 17:02:40 +0000

    URIs need a scheme.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,7 +42,7 @@ module ApplicationHelper
 
   def image_for_ministerial_role(ministerial_role)
     url = ministerial_role.current_person_image_url || "/assets/blank-person.jpg"
-    url = "mustachify.me/?src=" + url if params[:plummy]
+    url = "http://mustachify.me/?src=" + url if params[:plummy]
     image_tag url
   end
 
