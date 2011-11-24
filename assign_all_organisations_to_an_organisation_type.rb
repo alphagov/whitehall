4aeeaca2d4e7beb8bc1200fd0326310f87191815
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2011-11-24 17:30:40 +0000

    Remove temporary script.
    We ran the following command on both preview and production:
    
        RAILS_ENV=production script/rails runner assign_all_organisations_to_an_organisation_type.rb

--- assign_all_organisations_to_an_organisation_type.rb
+++
@@ -1,6 +0,0 @@
-organisation_type = OrganisationType.find_by_name!("Ministerial department")
-
-Organisation.find_each do |organisation|
-  organisation.organisation_type = organisation_type
-  organisation.save!
-end
