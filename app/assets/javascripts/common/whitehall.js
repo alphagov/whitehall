267a30184bc6bee72afc52e9c11a86a0a08d6077
Jason Cale <jase@gofreerange.com>
2011-12-02 15:36:14 +0000

    Updated grid helper javascript to have the correct position on resize.

--- app/assets/javascripts/common/whitehall.js
+++ app/assets/javascripts/common/whitehall.js
@@ -6,18 +6,24 @@ jQuery(document).ready(function($) {
   var altKey = '18'
   var altDown = false;
   var gKey = '71';
+  var gId = 'grid-helper';
+  var devClass = 'dev';
+
+  var grid = function () {
+    return $('#' + gId);
+  }
 
   var showGrid = function () {
-    var gId = 'grid-helper';
-    var gh = $('#' + gId);
+    var gh = grid();
     if (gh.length == 0) {
       gh = $.div('', {'class': 'group', 'id': gId});
-      gh.css({height: $('body').height()});
+      gh.css('height', 0);
       var g3 = $.div('', '.g3');
-      g3.css({"margin-top": -$('body').height()});
+      g3.css({"height": $('body').height()});
       g3.append($.div('', '.g1'), $.div('', '.g1'), $.div('', '.g1'));
       gh.append(g3);
       $('#wrapper').append(gh);
+      sizeGrid();
     }
   }
 
@@ -25,14 +31,26 @@ jQuery(document).ready(function($) {
     $('#grid-helper').remove();
   }
 
+  var sizeGrid = function () {
+    var offset = 24; // Unsure why I need this :(
+    grid().find('.g3').css({"margin-top": "-" + (grid().offset().top + offset) + "px"});
+  }
+
+  $(window).resize(function () {
+    if ($('body').hasClass(devClass)) {
+      hideGrid();
+      showGrid();
+    };
+  });
+
   $('body').keydown(function(event) {
     if (event.keyCode == altKey) {
       altDown = true;
     }
     if (altDown && event.keyCode == gKey) {
       event.preventDefault();
-      $(this).toggleClass('dev');
-      if ($(this).hasClass('dev')) {
+      $(this).toggleClass(devClass);
+      if ($(this).hasClass(devClass)) {
         showGrid();
       } else {
         hideGrid();
