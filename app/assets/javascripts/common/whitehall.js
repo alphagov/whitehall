971cc8385b9bef8cf78e4c6bd3d242b3a1db74f5
Jason Cale <jase@gofreerange.com>
2011-12-02 12:31:42 +0000

    Add grid helper styles to overlay a grid/baseline when holding down alt+g.

--- app/assets/javascripts/common/whitehall.js
+++ app/assets/javascripts/common/whitehall.js
@@ -3,18 +3,48 @@ jQuery(document).ready(function($) {
 
   // This is useful for toggling CSS helpers
   // whilst developing.. cmd+G
-  var cmdDown = false;
-  $('body').keydown(function(event) {
-    if (event.keyCode == '91') {
-      cmdDown = true;
+  var showGrid = function () {
+    var gId = 'grid-helper';
+    var gh = $('#' + gId);
+    if (gh.length == 0) {
+      gh = $.div('', {class: 'group', id: gId});
+      gh.css({height: $('body').height()});
+
+      var g3 = $.div('', '.g3');
+
+      g3.css({"margin-top": -$('body').height()});
+
+      g3.append($.div('', '.g1'), $.div('', '.g1'), $.div('', '.g1'));
+
+      gh.append(g3);
+
+      $('#wrapper').append(gh);
     }
-    if (cmdDown && event.keyCode == '71') {
+  }
+
+  var hideGrid = function () {
+    $('#grid-helper').remove();
+  }
+
+  var altKey = '18'
+  var altDown = false;
+  var gKey = '71';
+  $('body').keydown(function(event) {
+    if (event.keyCode == altKey) {
+      altDown = true;
+    }
+    if (altDown && event.keyCode == gKey) {
       event.preventDefault();
       $(this).toggleClass('dev');
+      if ($(this).hasClass('dev')) {
+        showGrid();
+      } else {
+        hideGrid();
+      }
     }
   }).keyup(function(event) {
-    if (event.keyCode == '91') {
-      cmdDown = false;
+    if (event.keyCode == altKey) {
+      altDown = false;
     }
   });
 });
