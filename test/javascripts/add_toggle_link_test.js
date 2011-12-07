64c45967fa1909e8626585063dab2643c7281e8c
Chris Roos <chris@seagul.co.uk>
2011-12-07 11:50:54 +0000

    Avoid polluting the global namespace.
    The qunit pattern for making variables available across setup and test is to to use `this`.

--- test/javascripts/add_toggle_link_test.js
+++ test/javascripts/add_toggle_link_test.js
@@ -1,40 +1,40 @@
 module("Adding a toggle link to an element", {
   setup: function() {
-    container = $('<section id="container"></section>');
+    this.container = $('<section id="container"></section>');
     var heading = $('<h1>Content heading</h1>');
-    content = $('<div class="content">my content</div>');
-    container.append(heading);
-    container.append(content);
-    $('#qunit-fixture').append(container);
-    container.addToggleLink(".content");
+    this.content = $('<div class="content">my content</div>');
+    this.container.append(heading);
+    this.container.append(this.content);
+    $('#qunit-fixture').append(this.container);
+    this.container.addToggleLink(".content");
   }
 });
 
 test("should hide the content", function() {
-  ok(!$(content).is(":visible"));
+  ok(!$(this.content).is(":visible"));
 });
 
 test("should add a 'show' link within the heading of the container", function() {
-  var toggleLink = $('h1 a.toggle', container);
+  var toggleLink = $('h1 a.toggle', this.container);
   equal("show", $(toggleLink).text());
   ok($(toggleLink).is(":visible"));
 });
 
 test("should show the content when the link is clicked", function() {
-  var toggleLink = $('h1 a.toggle', container);
+  var toggleLink = $('h1 a.toggle', this.container);
   toggleLink.click();
-  ok($(content).is(":visible"));
+  ok($(this.content).is(":visible"));
 });
 
 test("should change the link text when the link is clicked", function() {
-  var toggleLink = $('h1 a.toggle', container);
+  var toggleLink = $('h1 a.toggle', this.container);
   toggleLink.click();
   equal("hide", $(toggleLink).text());
 });
 
 test("should hide the content when the link is clicked twice", function() {
-  var toggleLink = $('h1 a.toggle', container);
+  var toggleLink = $('h1 a.toggle', this.container);
   toggleLink.click();
   toggleLink.click();
-  ok(!$(content).is(":visible"));
+  ok(!$(this.content).is(":visible"));
 });
