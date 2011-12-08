a8d8974d919e283f9f1a971ea6c380430b121f78
Jason Cale <jase@gofreerange.com>
2011-12-08 17:04:14 +0000

    Fix functional tests that I'd missed in my last commit (0adc32) — apologies for that.

--- test/functional/policy_areas_controller_test.rb
+++ test/functional/policy_areas_controller_test.rb
@@ -58,7 +58,7 @@ class PolicyAreasControllerTest < ActionController::TestCase
 
     get :show, id: policy_area
 
-    assert_select "#related_policy_areas", count: 0
+    assert_select "#related_policy_areas ul", count: 0
   end
 
   test "show displays recently changed documents relating to policies in the policy area" do
@@ -135,7 +135,7 @@ class PolicyAreasControllerTest < ActionController::TestCase
 
     get :show, id: policy_area
 
-    assert_select "#organisations" do
+    assert_select "nav.meta" do
       assert_select_object first_organisation
       assert_select_object second_organisation
     end
