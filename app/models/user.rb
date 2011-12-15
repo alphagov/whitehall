17c0ef3790bbf80c7dc75c749cb1a320016753bd
James Adam <james@lazyatom.com>
2011-12-15 17:07:18 +0000

    This relationship is unused.
    It's also broken as was stated.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   include GDS::SSO::User
 
   belongs_to :organisation
-  has_many :documents, foreign_key: 'author_id'
 
   validates :name, presence: true
   validates :email, email_format: { allow_blank: true }
