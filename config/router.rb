557d0c3565822775002c44be124f8c54c8dc0baf
James Mead <james@floehopper.org>
2011-12-20 10:29:23 +0000

    Add logging around route registration.
    Occasionally we've seen Router::RemoteErrors occur on deployment. This
    logging should give us a better chance of diagnosing what the problem
    is.

--- config/router.rb
+++ config/router.rb
@@ -1,6 +1,14 @@
 require "router"
 
 router = Router.new("http://router.cluster:8080/router", Rails.logger)
-router.application("whitehall", Plek.current.find("whitehall")) do |app|
-  app.ensure_prefix_route Whitehall.router_prefix
+begin
+  router.application("whitehall", Plek.current.find("whitehall")) do |app|
+    app.ensure_prefix_route Whitehall.router_prefix
+  end
+rescue Router::Conflict => conflict_error
+  Rails.logger.error "Route already exists: #{conflict_error.existing}"
+  raise conflict_error
+rescue Router::Remote => remote_error
+  Rails.logger.error "Remote error response: #{remote_error.response}"
+  raise remote_error
 end
