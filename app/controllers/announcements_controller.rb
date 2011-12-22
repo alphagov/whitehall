40161ab09132f45abff3acb39688161d0bcc90b5
Chris Roos <chris@seagul.co.uk>
2011-12-22 14:26:33 +0000

    Prevent n+1 queries on /announcements.
    *NOTE* This isn't tested. Although I see a slight performance
    improvement on my local machine I want to test the effect when accessed
    through the full stack before adding tests and making the change
    permanent.

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,14 +1,14 @@
 class AnnouncementsController < ApplicationController
   def index
-    @featured_news_articles = NewsArticle.published.featured.by_published_at.limit(3)
+    @featured_news_articles = NewsArticle.published.featured.by_published_at.includes(:document_identity, :document_relations, :policy_areas).limit(3)
     @announcements = find_announcements
   end
 
   protected
 
   def find_announcements
-    speeches = Speech.published.by_published_at
-    news_articles = NewsArticle.published.by_published_at
+    speeches = Speech.published.by_published_at.includes(:document_identity, role_appointment: [:person, :role])
+    news_articles = NewsArticle.published.by_published_at.includes(:document_identity, :document_relations, :policy_areas)
 
     ((news_articles - @featured_news_articles) + speeches).sort_by!{|a| a.published_at }.reverse
   end
