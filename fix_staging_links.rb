fe2b7cfdd898f6cc8ed549140372a0a21ffe8ca2
James Adam <james@lazyatom.com>
2012-01-10 10:20:39 -0600

    Remove this script now.

--- fix_staging_links.rb
+++
@@ -1,7 +0,0 @@
-Document.record_timestamps = false
-documents = Document.where("body like '%whitehall.preview.alphagov.co.uk/admin%'")
-documents.each do |document|
-  fixed_body = document.body.gsub(/whitehall\.preview\.alphagov\.co\.uk\/admin/, "whitehall.preview.alphagov.co.uk#{Whitehall.router_prefix}/admin")
-  document.update_attribute(:body, fixed_body)
-  puts "Updated document #{document.id}"
-end
