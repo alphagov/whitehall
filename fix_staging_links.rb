1faf671c282cef7c6dc6d18296abede5ba0dd774
James Adam <james@lazyatom.com>
2012-01-10 10:05:37 -0600

    Modify fix to include prefix.

--- fix_staging_links.rb
+++ fix_staging_links.rb
@@ -1,7 +1,7 @@
 Document.record_timestamps = false
-documents = Document.where("body like '%whitehall.staging%'")
+documents = Document.where("body like '%whitehall.preview.alphagov.co.uk/admin%'")
 documents.each do |document|
-  fixed_body = document.body.gsub(/whitehall\.staging/, "whitehall.preview")
+  fixed_body = document.body.gsub(/whitehall\.preview\.alphagov\.co\.uk\/admin/, "whitehall.preview.alphagov.co.uk/#{Whitehall.router_prefix}/admin")
   document.update_attribute(:body, fixed_body)
   puts "Updated document #{document.id}"
 end
