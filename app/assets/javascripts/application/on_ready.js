9a7157d11f42c3d5953beeacf111902fecdde650
Jason Cale <jase@gofreerange.com>
2012-01-11 15:00:58 +0000

    Javascript: Removing unnecessary ready() use and also using $ to be consistent.

--- app/assets/javascripts/application/on_ready.js
+++ app/assets/javascripts/application/on_ready.js
@@ -1,7 +1,5 @@
 jQuery(function($) {
   $("#notes_to_editors").addToggleLink(".notes_to_editors");
   $("#featured-news-articles, #featured-consultations").featuredSectionCarousel();
-});
-jQuery(document).ready(function() {
-  jQuery("abbr.time_ago").timeago();
+  $("abbr.time_ago").timeago();
 });
