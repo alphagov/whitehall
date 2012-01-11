3113057af0317b945ede694ad4adbc78abf96d1c
Chris Roos <chris@seagul.co.uk>
2012-01-11 16:22:45 +0000

    Allow us to connect to a local instance of rummager.
    Rummageable is responsible for using Plek to look up the location of rummager.  This breaks down when hosting rummager on anything other than x.dev.gov.uk (e.g. when using pow to host at rummager.dev).

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -1,4 +1,5 @@
 Rummageable.rummager_service_name = "whitehall-search"
 Rummageable.path_prefix = Whitehall.router_prefix
+Rummageable.rummager_host = ENV["RUMMAGER_HOST"] if ENV["RUMMAGER_HOST"]
 
 Whitehall::SearchClient.search_uri = Rummageable.rummager_host + Whitehall.router_prefix + "/search"
