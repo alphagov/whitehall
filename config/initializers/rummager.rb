8ea30cf41c198696016291efa7ea05d644ba7dc7
James Mead <james@floehopper.org>
2012-01-16 08:44:50 +0000

    Update to new version of Rummageable.
    - This allows us to use the Rummageable::Fake implementation in
      development which avoids the need to have Rummager and Solr running on
      your local machine in order to e.g. publish a document.
    - It might also be sensible to use this in the test environment(s), but
      this addresses my immediate need.

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -3,3 +3,7 @@ Rummageable.path_prefix = Whitehall.router_prefix
 Rummageable.rummager_host = ENV["RUMMAGER_HOST"] if ENV["RUMMAGER_HOST"]
 
 Whitehall::SearchClient.search_uri = Rummageable.rummager_host + Whitehall.router_prefix + "/search"
+
+if Rails.env.development?
+  Rummageable.implementation = Rummageable::Fake.new
+end
