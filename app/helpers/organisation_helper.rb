470f1dfb42e0bd3d0b73c5e925e9a76574721d99
Tom Stuart <tom@experthuman.com>
2012-01-19 11:43:57 +0000

    Highlight most relevant organisation navigation link

--- app/helpers/organisation_helper.rb
+++ app/helpers/organisation_helper.rb
@@ -6,4 +6,12 @@ module OrganisationHelper
       organisation.name
     end
   end
+
+  def organisation_navigation_link_to(body, path)
+    link_to body, path, class: ('current' if current_organisation_navigation_path(params) == path)
+  end
+
+  def current_organisation_navigation_path(params)
+    url_for params.slice(:controller, :action, :id).merge(only_path: true)
+  end
 end
--- test/unit/helpers/organisation_helper_test.rb
+++ test/unit/helpers/organisation_helper_test.rb
@@ -10,4 +10,39 @@ class OrganisationHelperTest < ActionView::TestCase
     organisation = build(:organisation, acronym: nil, name: "Building Law and Hygiene")
     assert_equal "Building Law and Hygiene", organisation_display_name(organisation)
   end
+
+  test 'organisation home page should be related to home page in organisation navigation' do
+    organisation = create(:organisation, name: 'Cabinet Office')
+    assert_equal organisation_path(organisation), current_organisation_navigation_path(controller: 'organisations', action: 'show', id: organisation.slug)
+  end
+
+  test 'organisation about page should be related to about page in organisation navigation' do
+    organisation = create(:organisation, name: 'Cabinet Office')
+    assert_equal about_organisation_path(organisation), current_organisation_navigation_path(controller: 'organisations', action: 'about', id: organisation.slug)
+  end
+
+  test 'organisation news page should be related to news page in organisation navigation' do
+    organisation = create(:organisation, name: 'Cabinet Office')
+    assert_equal news_organisation_path(organisation), current_organisation_navigation_path(controller: 'organisations', action: 'news', id: organisation.slug)
+  end
+
+  test 'should add current class to link if current page is related to link' do
+    stubs(:current_organisation_navigation_path).returns('/some/path')
+    html = organisation_navigation_link_to('Link body', '/some/path')
+    anchor = Nokogiri::HTML.fragment(html)/'a'
+    assert_equal 'Link body', anchor.inner_text
+    assert_equal '/some/path', anchor.attr('href').value
+    classes = (anchor.attr('class').try(:value) || '').split
+    assert classes.include?('current')
+  end
+
+  test 'should not add current class to link if current page is not related to link' do
+    stubs(:current_organisation_navigation_path).returns('/some/other/path')
+    html = organisation_navigation_link_to('Link body', '/some/path')
+    anchor = Nokogiri::HTML.fragment(html)/'a'
+    assert_equal 'Link body', anchor.inner_text
+    assert_equal '/some/path', anchor.attr('href').value
+    classes = (anchor.attr('class').try(:value) || '').split
+    refute classes.include?('current')
+  end
 end
