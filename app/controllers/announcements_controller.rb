b57ebe022468d0eee99dc52ac954de4b3f99711b
Tom Stuart <tom@experthuman.com>
2012-01-20 09:31:15 +0000

    Stop checking for featured speeches in AnnouncementsController
    Speeches aren't currently featurable.

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -14,14 +14,14 @@ class AnnouncementsController < PublicFacingController
     news_today = NewsArticle.published.by_published_at.where(
       'published_at > :time && featured = false', time: 1.day.ago).includes(:document_identity, :document_relations, :policy_areas)
     speeches_today = Speech.published.by_published_at.where(
-      'published_at > :time && featured = false', time: 1.day.ago).includes(:document_identity, role_appointment: [:person, :role])
+      'published_at > :time', time: 1.day.ago).includes(:document_identity, role_appointment: [:person, :role])
 
     (news_today + speeches_today).sort_by!{|a| a.published_at }.reverse
   end
 
   def announced_in_last_7_days
     news_this_week = NewsArticle.published.by_published_at.where(featured: false, published_at: 1.week.ago .. 24.hours.ago).includes(:document_identity, :document_relations, :policy_areas)
-    speeches_this_week = Speech.published.by_published_at.where(featured: false, published_at: 1.week.ago .. 24.hours.ago).includes(:document_identity, role_appointment: [:person, :role])
+    speeches_this_week = Speech.published.by_published_at.where(published_at: 1.week.ago .. 24.hours.ago).includes(:document_identity, role_appointment: [:person, :role])
 
     (news_this_week + speeches_this_week).sort_by!{|a| a.published_at }.reverse
   end
