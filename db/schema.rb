16c309ccb17a9904d6a4daa4d3fe0b59c3226ac1
James Adam <james@lazyatom.com>
2012-01-23 11:28:12 +0000

    Reset the slugs for policy areas.
    They had become out of sync, possibly because people were renaming them rather than creating new ones (for example, the policy area with slug transparency actually had the title Olympics).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120122174959) do
+ActiveRecord::Schema.define(:version => 20120123112118) do
 
   create_table "attachments", :force => true do |t|
     t.string   "carrierwave_file"
