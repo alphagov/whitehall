783e141970a9882d7b1f2738d5a3877dd648bfce
James Adam <james@lazyatom.com>
2012-01-24 11:43:48 +0000

    This feels like a more natural home for these methods.

--- app/helpers/admin/documents_helper.rb
+++ app/helpers/admin/documents_helper.rb
@@ -9,33 +9,6 @@ module Admin::DocumentsHelper
     admin_header_link "Documents", admin_documents_path, /^#{Whitehall.router_prefix}\/admin\/(documents|publications|policies|news_articles|consultations|speeches)/
   end
 
-  def admin_policy_areas_header_link
-    admin_header_link "Policy areas", admin_policy_areas_path
-  end
-
-  def admin_organisations_header_link
-    admin_header_link "Organisations", admin_organisations_path
-  end
-
-  def admin_roles_header_link
-    admin_header_link "Roles", admin_roles_path
-  end
-
-  def admin_people_header_link
-    admin_header_link "People", admin_people_path
-  end
-
-  def admin_countries_header_link
-    admin_header_link "Countries", admin_countries_path
-  end
-
-  def admin_header_link(name, path, path_matcher = nil)
-    path_matcher ||= Regexp.new("^#{Regexp.escape(path)}")
-    if user_signed_in?
-      link_to name, path, class: current_link_class(path_matcher)
-    end
-  end
-
   def link_to_filter(link, options)
     link_to link, url_for(params.slice('filter', 'author', 'organisation').merge(options)), class: filter_class(options)
   end
--- app/helpers/admin/url_helper.rb
+++ app/helpers/admin/url_helper.rb
@@ -1,4 +1,31 @@
 module Admin::UrlHelper
+  def admin_policy_areas_header_link
+    admin_header_link "Policy areas", admin_policy_areas_path
+  end
+
+  def admin_organisations_header_link
+    admin_header_link "Organisations", admin_organisations_path
+  end
+
+  def admin_roles_header_link
+    admin_header_link "Roles", admin_roles_path
+  end
+
+  def admin_people_header_link
+    admin_header_link "People", admin_people_path
+  end
+
+  def admin_countries_header_link
+    admin_header_link "Countries", admin_countries_path
+  end
+
+  def admin_header_link(name, path, path_matcher = nil)
+    path_matcher ||= Regexp.new("^#{Regexp.escape(path)}")
+    if user_signed_in?
+      link_to name, path, class: current_link_class(path_matcher)
+    end
+  end
+
   def website_home_url
     if host = Whitehall.public_host_for(request.host)
       "http://#{host}/government"
