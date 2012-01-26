502238730a0fb32c460c2c645c191b711225729f
James Mead <james@floehopper.org>
2012-01-26 15:42:28 +0000

    Multiple file upload javascript unit tests were out-of-date.
    Since 80199e99e791b30ab67231f4709b400427082c98, the field IDs and names
    generated by Rails have changed. The actual javascript implementation
    was unaffected by these changes and continued to work, but the test had
    become unrealistic.

--- test/javascripts/multiple_file_upload_test.js
+++ test/javascripts/multiple_file_upload_test.js
@@ -2,9 +2,10 @@ module("Uploading multiple files", {
   setup: function() {
     this.fieldset = $('<fieldset class="multiple_file_uploads"></fieldset>');
     var file_upload = $('<div class="file_upload"></div>');
-    this.first_input = $('<input id="document_attachments_attributes_0_file" name="document[attachments_attributes][0][file]" type="file" />')
+    this.first_input = $('<input id="document_document_attachments_attributes_0_attachment_attributes_file" name="document[document_attachments_attributes][0][attachment_attributes][file]" type="file" />');
+
     file_upload.append(this.first_input);
-    file_upload.append('<input id="document_attachments_attributes_0_file_cache" name="document[attachments_attributes][0][file_cache]" type="hidden" />');
+    file_upload.append('<input id="document_document_attachments_attributes_0_attachment_attributes_file_cache" name="document[document_attachments_attributes][0][attachment_attributes][file_cache]" type="hidden" />');
 
     this.fieldset.append(file_upload);
     $('#qunit-fixture').append(this.fieldset);
@@ -32,31 +33,31 @@ test("should continue adding new inputs as new files are selected", function() {
   equal(this.fieldset.children(".file_upload").length, 4);
 });
 
-test("should increment the ID and name of the file inputs for each new input added", function() {
+test("should increment the ID and name of the file input for each set of new inputs added", function() {
   this.fieldset.find("input[type=file]:last").change();
   var latest_input = this.fieldset.find("input[type=file]:last")[0];
-  equal(latest_input.id, "document_attachments_attributes_1_file");
-  equal(latest_input.name, "document[attachments_attributes][1][file]");
+  equal(latest_input.id, "document_document_attachments_attributes_1_attachment_attributes_file");
+  equal(latest_input.name, "document[document_attachments_attributes][1][attachment_attributes][file]");
 
   this.fieldset.find("input[type=file]:last").change();
   latest_input = this.fieldset.find("input[type=file]:last")[0];
-  equal(latest_input.id, "document_attachments_attributes_2_file");
-  equal(latest_input.name, "document[attachments_attributes][2][file]");
+  equal(latest_input.id, "document_document_attachments_attributes_2_attachment_attributes_file");
+  equal(latest_input.name, "document[document_attachments_attributes][2][attachment_attributes][file]");
 });
 
-test("should increment the ID and name of the hidden cache inputs for each new input added", function() {
+test("should increment the ID and name of the hidden cache input for each set of new inputs added", function() {
   this.fieldset.find("input[type=file]:last").change();
   var latest_input = this.fieldset.find("input[type=hidden]:last")[0];
-  equal(latest_input.id, "document_attachments_attributes_1_file_cache");
-  equal(latest_input.name, "document[attachments_attributes][1][file_cache]");
+  equal(latest_input.id, "document_document_attachments_attributes_1_attachment_attributes_file_cache");
+  equal(latest_input.name, "document[document_attachments_attributes][1][attachment_attributes][file_cache]");
 
   this.fieldset.find("input[type=file]:last").change();
   latest_input = this.fieldset.find("input[type=hidden]:last")[0];
-  equal(latest_input.id, "document_attachments_attributes_2_file_cache");
-  equal(latest_input.name, "document[attachments_attributes][2][file_cache]");
+  equal(latest_input.id, "document_document_attachments_attributes_2_attachment_attributes_file_cache");
+  equal(latest_input.name, "document[document_attachments_attributes][2][attachment_attributes][file_cache]");
 });
 
-test("should set the value of the hidden cache inputs to blank for each new input added", function() {
+test("should set the value of the hidden cache input to blank for each new input added", function() {
   $("input[type=hidden]:last").val("not-blank");
   this.fieldset.find("input[type=file]:last").change();
   var latest_input = this.fieldset.find("input[type=hidden]:last");
