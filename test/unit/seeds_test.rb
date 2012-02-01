f86431631e3783a249d0123d956b5e169d4bdf18
Tom Stuart <tom@experthuman.com>
2012-02-01 11:05:15 +0000

    Remove database seeds
    These require effort to maintain (e.g. 32e13e115a73c3a28e9b3138022ccf35b86c986b),
    make the test suite slower, and nobody seems to want them any more; we have
    enough real data now.

--- test/unit/seeds_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SeedsTest < ActiveSupport::TestCase
-  test "loading seeds.rb doesn't cause errors" do
-    assert_nothing_raised do
-      load Rails.root + "db" + "seeds.rb"
-    end
-  end
-end
