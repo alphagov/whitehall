f585171c5a387a4afd835da6e6f2a3800f4b02c1
Paul Battley <pbattley@gmail.com>
2012-02-09 15:25:45 +0000

    Don't check CSS in binary files (e.g. Vim swap files).

--- test/unit/css_appropriateness_test.rb
+++ test/unit/css_appropriateness_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CSSAppropriatenessTest < ActiveSupport::TestCase
   test "should not set g1, g2 or g3 classes on non-div elements" do
-    matching_lines = `grep -n -E 'class="[^"]*g[123]' -r #{Rails.root.join("app/views")}`.split("\n").map { |s| s.split(":").map { |s2| s2.strip } }
+    matching_lines = `grep -n -I -E 'class="[^"]*g[123]' -r #{Rails.root.join("app/views")}`.split("\n").map { |s| s.split(":").map { |s2| s2.strip } }
     matching_lines.reject! { |(line, number, match)| match =~ /^\<div/ }
     unless matching_lines.empty?
       message = "Some non-div elements have been given grid classes:\n" + matching_lines.map { |x| x.join(":") }.join("\n")
