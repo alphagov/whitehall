fd1753b62b818f79eb6e71cebb3b743e424bb5c9
James Adam <james@lazyatom.com>
2012-02-10 16:08:32 +0000

    API change in rails...

--- script/migrate_news_article_images.rb
+++ script/migrate_news_article_images.rb
@@ -3,5 +3,5 @@ NewsArticle.where("carrierwave_image IS NOT NULL").each do |article|
   article.image.cache_stored_file!
   path_to_image = article.image.send(:cache_path)
   image = article.images.build(alt_text: article.image_alt_text, caption: article.image_caption, image_data_attributes: {file: File.open(path_to_image)})
-  image.save(false)
+  image.save(validate: false)
 end
