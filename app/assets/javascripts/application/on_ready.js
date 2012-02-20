0b0b5491b9a815a74d7dfdf1ffefc219914a1b71
Frances Berriman <fberriman@gmail.com>
2012-02-20 12:30:19 +0000

    Scope policy change note javascript helper.
    The change notes were being applied to any meta data timestamps, so I have specificially targeted only the updated_at time on policy show pages - Jase.

--- app/assets/javascripts/application/on_ready.js
+++ app/assets/javascripts/application/on_ready.js
@@ -4,7 +4,7 @@ jQuery(function($) {
   $("abbr.time_ago").timeago();
   $('section.featured_items').equalHeightHelper({selectorsToResize: ['h2 a', 'p.summary', 'div.image_summary']});
   $('section.article_group').equalHeightHelper({selectorsToResize: ['article']});
-  $('.change_notes').policyUpdateNotes({link:'.metadata'});
+  $('.change_notes').policyUpdateNotes({link:'.link-to-change-notes'});
   $('#global-nav').navHelper({
     breakpoints: [
       { width: 540, label: 'All sections', exclude: '.home, .current' },
