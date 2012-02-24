5cf820de0977da85fc6447042eb1941acdd70f6a
Paul Battley <pbattley@gmail.com>
2012-02-24 14:25:16 +0000

    Register short URLs with router.

--- config/router.rb
+++ config/router.rb
@@ -4,6 +4,9 @@ router = Router.new("http://router.cluster:8080/router", Rails.logger)
 begin
   router.application("whitehall", Plek.current.find("whitehall")) do |app|
     app.ensure_prefix_route Whitehall.router_prefix
+    VanityRedirector.new(Rails.root.join("app", "data", "vanity-redirects.csv")).each do |r, _|
+      app.ensure_full_route r
+    end
   end
 rescue Router::Conflict => conflict_error
   Rails.logger.error "Route already exists: #{conflict_error.existing}"
