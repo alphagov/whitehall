44dc4d368c5da909f1b32db26f7a178cc26acc63
Paul Battley <pbattley@gmail.com>
2012-03-08 12:11:35 +0000

    Don't cache in the app: varnish will handle it.
    Using Rails's page caching just fills up the disk. This is not helpful.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,9 @@ Whitehall::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
+  # Full error reports are disabled and caching is turned off
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
