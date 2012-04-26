28bb288d3aa818a9600ed16b8abf66556009678c
Jordan Hatch <jordan@jordanh.net>
2012-04-26 17:27:31 +0000

    Fix authentication tests

--- test/integration/authentication_test.rb
+++ test/integration/authentication_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class AuthenticationTest < ActionDispatch::IntegrationTest
+
+  setup do
+    ENV['GDS_SSO_MOCK_INVALID'] = '1'
+  end
+
   test "should use GDS SSO to authenticate" do
     get admin_people_path
     assert_redirected_to "/auth/gds"
