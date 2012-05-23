8a93d7b1063ad3d74db898c2b40dbffc1d935d47
James Mead <james@floehopper.org>
2012-05-23 17:40:28 +0100

    Remove `document_attachment` alias to `edition_attachment` factory.

--- test/unit/edition_attachment_test.rb
+++ test/unit/edition_attachment_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class DocumentAttachmentTest < ActiveSupport::TestCase
   test "destroys attachment when no documents are associated" do
-    document_attachment = create(:document_attachment)
+    document_attachment = create(:edition_attachment)
     attachment = document_attachment.attachment
-    other_document_attachment = create(:document_attachment, attachment: attachment)
+    other_document_attachment = create(:edition_attachment, attachment: attachment)
 
     attachment.expects(:destroy).never
     document_attachment.destroy
   end
 
   test "does not destroy attachment when if more documents are associated" do
-    document_attachment = create(:document_attachment)
+    document_attachment = create(:edition_attachment)
     attachment = document_attachment.attachment
 
     attachment.expects(:destroy)
