568112cb2d5515b29af8d395302e2c2fdfdc0511
Chris Roos <chris@seagul.co.uk>
2012-05-28 11:36:45 +0100

    Rename variables in editorial_remarks_controller_test.
    Rename document to edition where appropriate.

--- test/functional/admin/editorial_remarks_controller_test.rb
+++ test/functional/admin/editorial_remarks_controller_test.rb
@@ -7,33 +7,33 @@ class Admin::EditorialRemarksControllerTest < ActionController::TestCase
 
   should_be_an_admin_controller
 
-  test "should render the document title and body to give context to the person rejecting" do
-    document = create(:submitted_edition, title: "document-title", body: "document-body")
-    get :new, document_id: document
+  test "should render the edition title and body to give context to the person rejecting" do
+    edition = create(:submitted_edition, title: "edition-title", body: "edition-body")
+    get :new, document_id: edition
 
-    assert_select "#{record_css_selector(document)} .title", text: "document-title"
-    assert_select "#{record_css_selector(document)} .body", text: "document-body"
+    assert_select "#{record_css_selector(edition)} .title", text: "edition-title"
+    assert_select "#{record_css_selector(edition)} .body", text: "edition-body"
   end
 
-  test "should redirect to the list of documents that need work" do
-    document = create(:submitted_edition)
-    post :create, document_id: document, editorial_remark: { body: "editorial-remark-body" }
+  test "should redirect to the list of editions that need work" do
+    edition = create(:submitted_edition)
+    post :create, document_id: edition, editorial_remark: { body: "editorial-remark-body" }
     assert_redirected_to admin_documents_path(state: :submitted)
   end
 
   test "should create an editorial remark" do
-    document = create(:submitted_edition)
-    post :create, document_id: document, editorial_remark: { body: "editorial-remark-body" }
+    edition = create(:submitted_edition)
+    post :create, document_id: edition, editorial_remark: { body: "editorial-remark-body" }
 
-    document.reload
-    assert_equal 1, document.editorial_remarks.length
-    assert_equal @logged_in_user, document.editorial_remarks.first.author
-    assert_equal "editorial-remark-body", document.editorial_remarks.first.body
+    edition.reload
+    assert_equal 1, edition.editorial_remarks.length
+    assert_equal @logged_in_user, edition.editorial_remarks.first.author
+    assert_equal "editorial-remark-body", edition.editorial_remarks.first.body
   end
 
   test "should explain why the editorial remark couldn't be saved" do
-    document = create(:submitted_edition)
-    post :create, document_id: document, editorial_remark: { body: "" }
+    edition = create(:submitted_edition)
+    post :create, document_id: edition, editorial_remark: { body: "" }
     assert_template "new"
     assert_select ".form-errors"
   end
