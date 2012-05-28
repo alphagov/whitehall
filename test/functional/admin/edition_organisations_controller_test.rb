195651288649485e0107d9ad1cb6f3b6e99cc99f
Chris Roos <chris@seagul.co.uk>
2012-05-28 11:13:12 +0100

    Rename variables in edition_organisations_controller_test.
    Rename document to edition where appropriate.

--- test/functional/admin/edition_organisations_controller_test.rb
+++ test/functional/admin/edition_organisations_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class Admin::EditionOrganisationsControllerTest < ActionController::TestCase
   should_be_an_admin_controller
 
-  test "should allow featuring of the document organisation" do
-    document_organisation = create(:edition_organisation, featured: false)
+  test "should allow featuring of the edition organisation" do
+    edition_organisation = create(:edition_organisation, featured: false)
     login_as :departmental_editor
-    post :update, id: document_organisation, edition_organisation: {featured: true}
-    assert document_organisation.reload.featured?
+    post :update, id: edition_organisation, edition_organisation: {featured: true}
+    assert edition_organisation.reload.featured?
   end
 
-  test "should allow unfeaturing of the document organisation" do
-    document_organisation = create(:edition_organisation, featured: true)
+  test "should allow unfeaturing of the edition organisation" do
+    edition_organisation = create(:edition_organisation, featured: true)
     login_as :departmental_editor
-    post :update, id: document_organisation, edition_organisation: {featured: false}
-    refute document_organisation.reload.featured?
+    post :update, id: edition_organisation, edition_organisation: {featured: false}
+    refute edition_organisation.reload.featured?
   end
 
   test "should redirect back to the organisation's admin edit page" do
     organisation = create(:organisation)
-    document_organisation = create(:edition_organisation, organisation: organisation)
+    edition_organisation = create(:edition_organisation, organisation: organisation)
     login_as :departmental_editor
-    post :update, id: document_organisation, edition_organisation: {}
+    post :update, id: edition_organisation, edition_organisation: {}
     assert_redirected_to edit_admin_organisation_path(organisation)
   end
 end
