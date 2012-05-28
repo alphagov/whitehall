76dc6bef5e6874662ad72e0901413aead26624fc
Chris Roos <chris@seagul.co.uk>
2012-05-28 11:13:47 +0100

    Rename variables in edition_publishing_controller_test.
    Rename document to edition where appropriate.

--- test/functional/admin/edition_workflow_controller_test.rb
+++ test/functional/admin/edition_workflow_controller_test.rb
@@ -3,48 +3,48 @@ require 'test_helper'
 class Admin::EditionWorkflowControllerTest < ActionController::TestCase
   should_be_an_admin_controller
 
-  test 'publishing should redirect back to published documents' do
-    submitted_document = create(:submitted_policy)
+  test 'publishing should redirect back to published editions' do
+    submitted_edition = create(:submitted_policy)
     login_as :departmental_editor
-    post :publish, id: submitted_document, document: {lock_version: submitted_document.lock_version}
+    post :publish, id: submitted_edition, document: {lock_version: submitted_edition.lock_version}
 
     assert_redirected_to admin_documents_path(state: :published)
-    assert_equal "The document #{submitted_document.title} has been published", flash[:notice]
+    assert_equal "The document #{submitted_edition.title} has been published", flash[:notice]
   end
 
   test 'publishing should remove it from the set of submitted policies' do
-    document_to_publish = create(:submitted_policy)
+    edition_to_publish = create(:submitted_policy)
     login_as :departmental_editor
-    post :publish, id: document_to_publish, document: {lock_version: document_to_publish.lock_version}
+    post :publish, id: edition_to_publish, document: {lock_version: edition_to_publish.lock_version}
 
-    refute Edition.submitted.include?(document_to_publish)
+    refute Edition.submitted.include?(edition_to_publish)
   end
 
-  test 'publishing should not mark the document as force published' do
-    document_to_publish = create(:submitted_policy)
+  test 'publishing should not mark the edition as force published' do
+    edition_to_publish = create(:submitted_policy)
     login_as :departmental_editor
-    post :publish, id: document_to_publish, document: {lock_version: document_to_publish.lock_version}
+    post :publish, id: edition_to_publish, document: {lock_version: edition_to_publish.lock_version}
 
-    refute document_to_publish.reload.force_published?
+    refute edition_to_publish.reload.force_published?
   end
 
-  test 'failing to publish an document should set a flash' do
-    document_to_publish = create(:submitted_policy)
+  test 'failing to publish an edition should set a flash' do
+    edition_to_publish = create(:submitted_policy)
     login_as :policy_writer
-    post :publish, id: document_to_publish, document: {lock_version: document_to_publish.lock_version}
+    post :publish, id: edition_to_publish, document: {lock_version: edition_to_publish.lock_version}
 
     assert_equal "Only departmental editors can publish", flash[:alert]
   end
 
-  test 'failing to publish an document should redirect back to the document' do
-    document_to_publish = create(:submitted_policy)
+  test 'failing to publish an edition should redirect back to the edition' do
+    edition_to_publish = create(:submitted_policy)
     login_as :policy_writer
-    post :publish, id: document_to_publish, document: {lock_version: document_to_publish.lock_version}
+    post :publish, id: edition_to_publish, document: {lock_version: edition_to_publish.lock_version}
 
-    assert_redirected_to admin_policy_path(document_to_publish)
+    assert_redirected_to admin_policy_path(edition_to_publish)
   end
 
-  test 'failing to publish a stale document should redirect back to the document' do
+  test 'failing to publish a stale edition should redirect back to the edition' do
     policy_to_publish = create(:submitted_policy)
     lock_version = policy_to_publish.lock_version
     policy_to_publish.update_attributes!(title: "new title")
@@ -56,42 +56,42 @@ class Admin::EditionWorkflowControllerTest < ActionController::TestCase
   end
 
   test 'force publishing should succeed where normal publishing would fail' do
-    submitted_document = create(:draft_policy)
+    submitted_edition = create(:draft_policy)
     login_as :departmental_editor
-    post :publish, id: submitted_document, document: {lock_version: submitted_document.lock_version}, force: true
+    post :publish, id: submitted_edition, document: {lock_version: submitted_edition.lock_version}, force: true
 
     assert_redirected_to admin_documents_path(state: :published)
-    assert_equal "The document #{submitted_document.title} has been published", flash[:notice]
+    assert_equal "The document #{submitted_edition.title} has been published", flash[:notice]
   end
 
-  test 'force publishing should mark the document as force published' do
-    submitted_document = create(:draft_policy)
+  test 'force publishing should mark the edition as force published' do
+    submitted_edition = create(:draft_policy)
     login_as :departmental_editor
-    post :publish, id: submitted_document, document: {lock_version: submitted_document.lock_version}, force: true
+    post :publish, id: submitted_edition, document: {lock_version: submitted_edition.lock_version}, force: true
 
-    assert submitted_document.reload.force_published?
+    assert submitted_edition.reload.force_published?
   end
 
-  test 'should set change note on document if one is provided' do
-    document = create(:submitted_policy)
+  test 'should set change note on edition if one is provided' do
+    edition = create(:submitted_policy)
     login_as :departmental_editor
-    post :publish, id: document, document: {
+    post :publish, id: edition, document: {
       change_note: "change-note",
-      lock_version: document.lock_version
+      lock_version: edition.lock_version
     }
 
-    assert_equal "change-note", document.reload.change_note
+    assert_equal "change-note", edition.reload.change_note
   end
 
-  test 'should record minor change on a document' do
-    document = create(:submitted_policy)
+  test 'should record minor change on a edition' do
+    edition = create(:submitted_policy)
     login_as :departmental_editor
-    post :publish, id: document, document: {
+    post :publish, id: edition, document: {
       minor_change: true,
-      lock_version: document.lock_version
+      lock_version: edition.lock_version
     }
 
-    assert_equal true, document.reload.minor_change
+    assert_equal true, edition.reload.minor_change
   end
 
 end
