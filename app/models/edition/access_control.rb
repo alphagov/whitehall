f7b4b2c0e2b9338e0711b34d4b64dec52cc38de5
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-05-31 17:19:11 +0100

    Only allow departmental editors to clear the force-published flag

--- app/models/edition/access_control.rb
+++ app/models/edition/access_control.rb
@@ -24,6 +24,8 @@ module Edition::AccessControl
   def reason_to_prevent_force_published_being_cleared_by(user)
     if !force_published?
       "This document has not been force-published"
+    elsif !user.departmental_editor?
+      "Only departmental editors can clear the force-published state"
     elsif user == creator
       "You are not allowed to clear the force-published state of this document, since you created it"
     end
