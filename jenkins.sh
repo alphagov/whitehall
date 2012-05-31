9f5c67e5512eb24a7dfa00888ce5be4797de4269
Chris Roos <chris@seagul.co.uk>
2012-05-31 14:44:05 +0100

    Cleanup temporary files after test run on Jenkins.
    We ran into a problem this morning where the builds were failing on
    Jenkins because we ran out of links (Errno::EMLINK).  To avoid the
    problem in future we're now cleaning out these temporary upload
    directories after running the tests on Jenkins.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 export DISPLAY=:99
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:create db:migrate db:test:prepare && RAILS_ENV=test bundle exec rake --trace
+bundle exec rake db:create db:migrate db:test:prepare && RAILS_ENV=test bundle exec rake test test:cleanup --trace
