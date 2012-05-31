9f77c1837fd305401b3c8b4c9ac4f100e3c32f61
Paul Battley <pbattley@gmail.com>
2012-05-31 14:23:42 +0100

    Show newer fact check responses first

--- test/unit/edition/fact_checkable_test.rb
+++ test/unit/edition/fact_checkable_test.rb
@@ -8,15 +8,23 @@ class Edition::FactCheckableTest < ActiveSupport::TestCase
     refute FactCheckRequest.find_by_id(fact_check_request.id)
   end
 
-  test "should list all completed fact check requests from all editions" do
-    user = create(:user)
-    old_edition = create(:published_policy)
-    old_complete_fcr = create(:fact_check_request, edition: old_edition, comments: "Stuff")
-    old_incomplete_fcr = create(:fact_check_request, edition: old_edition)
-    new_edition = old_edition.create_draft(user)
-    new_complete_fcr = create(:fact_check_request, edition: new_edition, comments: "Stuff")
+  test "should list all completed fact check requests from all editions, newest first" do
+    begin
+      t0 = Time.now
+      user = create(:user)
+      old_edition = create(:published_policy)
+      Timecop.freeze t0
+      old_complete_fcr = create(:fact_check_request, edition: old_edition, comments: "Stuff")
+      Timecop.freeze t0 + 1
+      old_incomplete_fcr = create(:fact_check_request, edition: old_edition)
+      new_edition = old_edition.create_draft(user)
+      Timecop.freeze t0 + 2
+      new_complete_fcr = create(:fact_check_request, edition: new_edition, comments: "Stuff")
 
-    expected = [old_complete_fcr, new_complete_fcr]
-    assert_equal expected, new_edition.all_completed_fact_check_requests
+      expected = [new_complete_fcr, old_complete_fcr]
+      assert_equal expected, new_edition.all_completed_fact_check_requests
+    ensure
+      Timecop.return
+    end
   end
 end
