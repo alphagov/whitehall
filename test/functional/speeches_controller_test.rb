27d52662aad77f54ca84598676fff88c998ae987
James Adam <james@lazyatom.com>
2012-06-12 14:58:03 +0100

    Show the right minister for speeches given by previous appointments to a role.
    Showing the date range here is something I'm not sure about; I'm going to spend some time going through all of the places that a role or appointment is displayed in string form to see if there's a better refactoring.

--- test/functional/speeches_controller_test.rb
+++ test/functional/speeches_controller_test.rb
@@ -19,9 +19,23 @@ class SpeechesControllerTest < ActionController::TestCase
 
     get :show, id: published_speech.document
 
-    assert_select ".details .ministerial_role", "Theresa May (Secretary of State, Home Office)"
+    assert_select ".details .role_appointment", /Theresa May .+Secretary of State, Home Office/
     assert_select ".details .delivered_on", /1 June 2011/
     assert_select ".details .location", /The Guidhall/
+  end
+
+  test "should display who gave the speech even if they are not appointed to the same position anymore" do
+    home_office = create(:organisation, name: "Home Office")
+    home_secretary = create(:ministerial_role, name: "Secretary of State", organisations: [home_office])
+    theresa_may = create(:person, forename: "Theresa", surname: "May", image: fixture_file_upload('minister-of-funk.jpg'))
+    theresa_may_appointment = create(:role_appointment, role: home_secretary, person: theresa_may, started_at: 1.year.ago, ended_at: 1.day.ago)
+    subsequent_appointment = create(:role_appointment, role: home_secretary, started_at: 1.day.ago)
+    speech_type = SpeechType::Transcript
+    published_speech = create(:published_speech, speech_type: speech_type, role_appointment: theresa_may_appointment, delivered_on: 6.months.ago, location: "The Guidhall")
+
+    get :show, id: published_speech.document
+
+    assert_select ".details .role_appointment", /Theresa May .+Secretary of State, Home Office/
   end
 
   test "should display details about a transcript" do
