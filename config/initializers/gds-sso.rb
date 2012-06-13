979d5f2b93a512af062a3969b53dcc20f4638c4f
Kushal Pisavadia <kushi.p@gmail.com>
2012-06-13 06:17:35 -0700

    Use 'signon' as the root URL
    The 'authentication' label was used back in the days of sign-on-tron
    v1. This is now deprecated, so expect to search for 'signon' instead.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -2,5 +2,5 @@ GDS::SSO.config do |config|
   config.user_model   = "User"
   config.oauth_id     = 'abcdefghjasndjkasnd'
   config.oauth_secret = 'secret'
-  config.oauth_root_url = Plek.current.find("authentication")
+  config.oauth_root_url = Plek.current.find("signon")
 end
