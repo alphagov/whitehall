042d727277bdfc132a9fc66cf2ebe00156dc1520
Paul Battley <pbattley@gmail.com>
2012-06-20 15:18:26 +0100

    Fix invisible tab
    Bug description: When viewing a policy, the history and notes tab on the right
    had a count, but you couldn't see the data unless you selected the other tab
    and then came back to this tab.
    
    It's essential that the first pane in a set of tabbed content has the class
    "active", or it will not be shown when the page loads. This missing class was
    the problem here.
    
    It's better if the programmer doesn't have to worry about forgetting things,
    so by delegating generation of the tab wrapper to an object that encapsulates
    the is-first-tab? state, we can ensure that the first tab is always active on
    page load.
    
    As a nice side effect, this also makes tab rendering terser, and eliminates
    some spaghetti templating where which tab was first depended on some
    conditional logic.
    
    I'm not entirely sure that TabPaneState is the best name for this class, but
    that's not a pressing concern.

--- app/helpers/admin/sidebar_helper.rb
+++ app/helpers/admin/sidebar_helper.rb
@@ -1,7 +1,7 @@
 module Admin::SidebarHelper
   def simple_formatting_sidebar
-    sidebar_tabs govspeak_help: "Formatting Help" do
-      content_tag :section, class: "tab-pane active", id: "govspeak_help" do
+    sidebar_tabs govspeak_help: "Formatting Help" do |tabs|
+      tabs.pane id: "govspeak_help" do
         render partial: "admin/editions/govspeak_help"
       end
     end
@@ -43,7 +43,7 @@ module Admin::SidebarHelper
         tab_tags.join.html_safe
       end +
       content_tag(:div, class: "tab-content") do
-        yield
+        yield TabPaneState.new(self)
       end
     end
   end
