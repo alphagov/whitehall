49360d210f498745333b1728bacd2264b359cdde
James Adam <james@lazyatom.com>
2012-06-26 13:13:56 -0500

    Extract common attachment controller behaviour into a module.

--- app/controllers/admin/consultations_controller.rb
+++ app/controllers/admin/consultations_controller.rb
@@ -1,24 +1,12 @@
 class Admin::ConsultationsController < Admin::EditionsController
   include Admin::EditionsController::NationalApplicability
+  include Admin::EditionsController::Attachments
 
-  before_filter :build_edition_attachment, only: [:new, :edit]
   before_filter :build_image, only: [:new, :edit]
 
   private
 
   def edition_class
     Consultation
-  end
-
-  def build_edition_dependencies
-    super
-    build_edition_attachment
-  end
-
-  def build_edition_attachment
-    unless @edition.edition_attachments.any?(&:new_record?)
-      edition_attachment = @edition.edition_attachments.build
-      edition_attachment.build_attachment
-    end
   end
 end
--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -1,24 +1,12 @@
 class Admin::PublicationsController < Admin::EditionsController
   include Admin::EditionsController::NationalApplicability
+  include Admin::EditionsController::Attachments
 
-  before_filter :build_edition_attachment, only: [:new, :edit]
   before_filter :build_image, only: [:new, :edit]
 
   private
 
   def edition_class
     Publication
-  end
-
-  def build_edition_dependencies
-    super
-    build_edition_attachment
-  end
-
-  def build_edition_attachment
-    unless @edition.edition_attachments.any?(&:new_record?)
-      edition_attachment = @edition.edition_attachments.build
-      edition_attachment.build_attachment
-    end
   end
 end
