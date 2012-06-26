5fd10fa0a75a57686b04b5f63ed3f71e615654ef
James Adam <james@lazyatom.com>
2012-06-26 10:41:06 -0500

    Add integration testing for specialist guide pagination.
    Because the javascript operates on various parts of the specialist
    guide page, I felt it was more valuable to test it against the actual
    HTML, rather than against a fixture generated purely for the tests.
    
    This should give us better regression coverage if changes to the guide
    HTML break the javascript.
    
    I've used capybara-webkit as the javascript driver, since that is used
    elsewhere in govuk. You'll need Qt installed to install the gem; see
    https://github.com/thoughtbot/capybara-webkit/wiki/Installing-Qt-and-compiling-capybara-webkit
    for how to do proceed. For me, it was simply `brew install qt`, which
    downloaded a pre-compiled version for Lion.
    
    I've tried to use 'new world' cucumber testing style here, by which I
    mean valuing clarity and explanation in the step names over any reuse
    in their implementation.

---
+++ features/step_definitions/specialist_guide_pagination_steps.rb
@@ -0,0 +1,79 @@
+Given /^a specialist guide with section headings$/ do
+  create(:published_specialist_guide,
+         title: "Specialist guide with pages",
+         summary: "Here's the summary of the guide",
+         body: <<-EOS
+## Page 1
+
+Here's the content for page one
+
+## Page 2
+
+Here's some content for page two
+
+### Page 2, Section 1
+
+A subsection! Well I never.
+
+### Page 2, Section 2
+
+And another! How rare.
+
+## Page 3
+
+You were expecting something a bit more tabloid? Shame on you.
+EOS
+)
+end
+
+When /^I view the guide$/ do
+  visit "/specialist"
+  click_link "Specialist guide with pages"
+  save_and_open_page
+end
+
+Then /^I should see only the first page of the guide$/ do
+  assert page.find("h2#page-1").visible?
+  refute page.find("h2#page-2").visible?
+  refute page.find("h2#page-3").visible?
+end
+
+When /^I navigate to the second page$/ do
+  click_link "Page 2"
+end
+
+Then /^I should see only the second page of the guide$/ do
+  refute page.find("h2#page-1").visible?
+  assert page.find("h2#page-2").visible?
+  refute page.find("h2#page-3").visible?
+end
+
+When /^I view the first page$/ do
+  visit "/specialist"
+  click_link "Specialist guide with pages"
+  click_link "Page 1"
+end
+
+Then /^I should see the guide summary$/ do
+  assert page.find(".summary").visible?
+end
+
+Then /^I should not see the guide summary$/ do
+  refute page.find(".summary").visible?
+end
+
+When /^I view a page with internal headings$/ do
+  visit "/specialist"
+  click_link "Specialist guide with pages"
+  click_link "Page 2"
+end
+
+Then /^I should not see navigation for headings within other pages$/ do
+  refute page.find("a[href='#page-2-section-1']").visible?
+  refute page.find("a[href='#page-2-section-2']").visible?
+end
+
+Then /^I should see navigation for the headings within that page$/ do
+  assert page.find("a[href='#page-2-section-1']").visible?
+  assert page.find("a[href='#page-2-section-2']").visible?
+end
---
+++ features/support/javascript.rb
@@ -0,0 +1,10 @@
+Capybara.javascript_driver = :webkit
+require "slimmer/test"
+
+Before('@javascript') do
+  ENV["USE_SLIMMER"] = "true"
+end
+
+After('@javascript') do
+  ENV.delete("USE_SLIMMER")
+end
