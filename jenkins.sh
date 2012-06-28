ba6168a50a2e39ee95371f5340d40a485baf4ef9
James Mead <james@floehopper.org>
2012-06-28 11:25:07 +0100

    Run Cucumber features as part of the CI build.
    We had accidentally stopped doing this in
    9f5c67e5512eb24a7dfa00888ce5be4797de4269, when the `test:cleanup` task
    was added. I think what happened was that when the rake command was
    changed to run multiple tasks (i.e. because we also wanted to run
    `test:cleanup`), we needed to be explicit about running all the normal
    test tasks. However, instead of using the `default` rake task, we
    chose to use the `test` task. Unfortunately the `cucumber` task is only
    a dependency of the `default` task and not the `test` task.
    
    This happened about a month ago, but happily, because developers tend to
    use the `default` rake task on their local machines and are diligent
    in running the tests locally before committing, all the cucumber
    features still pass. Yay!

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,5 @@
 export DISPLAY=:99
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:create db:migrate db:test:prepare && \
-RAILS_ENV=test CUCUMBER_FORMAT=progress bundle exec rake test test:cleanup --trace && \
+RAILS_ENV=test CUCUMBER_FORMAT=progress bundle exec rake default test:cleanup --trace && \
 RAILS_ENV=production PRECOMPILING_ASSETS=true bundle exec rake assets:precompile --trace
