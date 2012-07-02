ff7f7012dfed4fe3f9aee54786c3680017bf49bc
Chris Roos & James Mead <pairing+chris.roos+james.mead@gofreerange.com>
2012-07-02 12:45:04 +0100

    Temporarily stop using Slimmer in Cucumber @javascript tests.
    We are expecting the build to fail, because jQuery will not be loaded.
    We want to see what's in the console output when this happens, so we can
    get more clues on why some of these tests failed on Friday & Saturday.

--- features/support/javascript.rb
+++ features/support/javascript.rb
@@ -2,7 +2,7 @@ Capybara.javascript_driver = :webkit
 require "slimmer/test"
 
 Before('@javascript') do
-  ENV["USE_SLIMMER"] = "true"
+  # ENV["USE_SLIMMER"] = "true"
 end
 
 After('@javascript') do
