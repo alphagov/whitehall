9d7abc73239d82e2f199c0765afde9fcd47035e5
Edd Sowden <e@e26.co.uk>
2012-07-04 07:44:31 +0100

    Previous/Next Chapter links on specialist guides
    Added javascript to insert previous/next chapter links to the end of
    pages.
    
    Refactored a couple of specialist_guide_pagination_steps to be page
    agnostic to make the steps more DRY.
    
    Added test to make sure the new navigation links work.

--- app/assets/javascripts/application/specialist_guide_pagination.js
+++ app/assets/javascripts/application/specialist_guide_pagination.js
@@ -1,10 +1,12 @@
 $(function() {
-  var container = $(".specialistguide .govspeak");
-  var navigation = $(".specialistguide #document_sections");
+  var container = $(".specialistguide .govspeak"),
+      navigation = $(".specialistguide #document_sections"),
+      pages, headings;
 
   container.splitIntoPages("h2");
   pages = container.find(".page");
   pages.hide();
+  headings = container.find('h2');
 
   var showPage = function() {
     var heading = $(location.hash);
@@ -22,6 +24,24 @@ $(function() {
     heading.parents(".page").show();
     $('html, body').animate({scrollTop:heading.offset().top}, 0);
   }
+
+  pages.each(function(i, el){
+    var currentPage = $(el),
+        prevNextNavigation = [],
+        adjacentPage;
+
+    // if there is a previous page
+    if(i > 0){
+      adjacentPage = $(headings.get(i-1));
+      prevNextNavigation.push('<li><a href="#'+adjacentPage.attr('id')+'">Previous Chapter: <span>'+adjacentPage.text()+'</span></a></li>');
+    }
+    if(i < pages.length-1){
+      adjacentPage = $(headings.get(i+1));
+      prevNextNavigation.push('<li><a href="#'+adjacentPage.attr('id')+'">Next Chapter: <span>'+adjacentPage.text()+'</span></a></li>');
+    }
+
+    currentPage.append('<ul class="previous-next-navigation">' + prevNextNavigation.join('') + '</ul>');
+  });
 
   navigation.find(">li").each(function(el){
     var li = $(this),
