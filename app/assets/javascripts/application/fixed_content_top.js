19a5a751bdd7864be91026e7b581dda6f7809efe
James Mead <james@floehopper.org>
2012-07-10 15:59:07 +0100

    Remove unecessary comments.
    I don't think these comments add much value.

--- app/assets/javascripts/application/fixed_content_top.js
+++ app/assets/javascripts/application/fixed_content_top.js
@@ -3,15 +3,10 @@ $(document).ready(function () {
   if (contentContainer.length > 0) {
     var top = $('.js-fixed-content-top').offset().top - parseFloat($('.js-fixed-content-top').css('marginTop').replace(/auto/, 0));
     $(window).scroll(function (event) {
-      // what the y position of the scroll is
       var y = $(this).scrollTop();
-    
-      // whether that's below
       if (y >= top) {
-        // if so, ad the fixed class
         $('.js-fixed-content-top').addClass('content-fixed');
       } else {
-        // otherwise remove it
         $('.js-fixed-content-top').removeClass('content-fixed');
       }
     });
