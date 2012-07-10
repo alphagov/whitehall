d1e28f99b9943036ecae23df062846f150fabcf6
James Mead <james@floehopper.org>
2012-07-10 15:58:08 +0100

    Use camelCase for javascript variable names.

--- app/assets/javascripts/application/fixed_content_top.js
+++ app/assets/javascripts/application/fixed_content_top.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {  
-  var contents_container = $('.js-fixed-content-top')
-  if (contents_container.length > 0) {
+  var contentContainer = $('.js-fixed-content-top')
+  if (contentContainer.length > 0) {
     var top = $('.js-fixed-content-top').offset().top - parseFloat($('.js-fixed-content-top').css('marginTop').replace(/auto/, 0));
     $(window).scroll(function (event) {
       // what the y position of the scroll is
