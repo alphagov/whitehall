bbec4ee56c7546fb16efd203dbe0e345bc0fe6ed
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-07-11 10:39:47 +0100

    Remove useless commented out tests

--- test/unit/case_study_test.rb
+++ test/unit/case_study_test.rb
@@ -9,24 +9,4 @@ class CaseStudyTest < ActiveSupport::TestCase
     assert article.can_be_related_to_policies?
   end
 
-  # test "#topics includes topics associated with related published policies" do
-  #   related_policy = create(:published_policy, topics: [create(:topic), create(:topic)])
-  #   news_article = create(:news_article, related_policies: [related_policy])
-  #   assert_equal related_policy.topics.sort, news_article.topics.sort
-  # end
-
-  # test "#topics excludes topics associated with related unpublished policies" do
-  #   related_policy = create(:draft_policy, topics: [create(:topic), create(:topic)])
-  #   news_article = create(:case_study, related_policies: [related_policy])
-  #   assert_equal [], news_article.topics
-  # end
-
-  # test "#topics includes each related topic only once, even if associated multiple times" do
-  #   topic = create(:topic)
-  #   first_related_policy = create(:published_policy, topics: [topic])
-  #   second_related_policy = create(:published_policy, topics: [topic])
-  #   news_article = create(:news_article, related_policies: [first_related_policy, second_related_policy])
-  #   assert_equal [topic], news_article.topics
-  # end
-
 end
