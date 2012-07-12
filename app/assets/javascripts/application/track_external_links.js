655cdd90d7c4d7af6bb5908bfac5f8d28dba2bc5
James Mead <james@floehopper.org>
2012-07-12 14:15:12 +0100

    More closely mirror GA recommended code for tracking outbound links.
    Handle any unforeseen javascript exceptions in outbound link tracking
    code - we don't want javascript errors if this code is failing since it
    isn't mission critical. See [1].
    
    [1]
    http://support.google.com/googleanalytics/bin/answer.py?hl=en-GB&answer=55527

--- app/assets/javascripts/application/track_external_links.js
+++ app/assets/javascripts/application/track_external_links.js
@@ -2,10 +2,14 @@
   var _trackExternalLinks = function() {
     $(this).find("a[rel='external']").each(function() {
       $(this).click(function(e) {
-        if (!e.metaKey && !e.ctrlKey) {
-          e.preventDefault();
-          _gat._getTrackerByName()._trackEvent(this.href, "Outbound Links");
-          setTimeout("document.location = '" + this.href + "'", 100);
+        try {
+          if (!e.metaKey && !e.ctrlKey) {
+            e.preventDefault();
+            _gat._getTrackerByName()._trackEvent(this.href, "Outbound Links");
+            setTimeout("document.location = '" + this.href + "'", 100);
+          }
+        } catch(error) {
+          // intentionally left blank
         }
       })
     })
