7b9230180be4e6f6470731f6b0b1c968b92451d7
Tom Ward <tom@popdog.net>
2012-07-16 12:39:14 +0100

    Use X-Slimmer-Search-Path header instead of setting the prefix.
    The prefix functionality will soon be removed from slimmer.

--- app/controllers/public_facing_controller.rb
+++ app/controllers/public_facing_controller.rb
@@ -1,10 +1,15 @@
 class PublicFacingController < ApplicationController
   helper :all
   before_filter :set_cache_control_headers
+  before_filter :set_search_path
 
   private
 
   def set_cache_control_headers
     expires_in 2.minutes, public: true
+  end
+
+  def set_search_path
+    response.headers[Slimmer::SEARCH_PATH_HEADER] = search_path
   end
 end
