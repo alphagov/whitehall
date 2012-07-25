5dd93dd49ed2ee2798508c61bcd0ca17b3ec2afc
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-07-25 17:18:04 +0100

    Stick at top should use real width when floating
    When the element is floated (has .content-fixed applied), it needs to
    have the real pixel width of the element applied. It's not possible to
    work out the width in relative percentage terms for the floated element.

--- app/assets/javascripts/application/stick_at_top_when_scrolling.js
+++ app/assets/javascripts/application/stick_at_top_when_scrolling.js
@@ -1,17 +1,27 @@
 (function ($) {
   var _stickAtTopWhenScrolling = function() {
-    var element = $(this);
-    if (element.length > 0 && $(window).width() > 768) {
-      var elementVerticalPosition = element.offset().top - parseFloat(element.css('marginTop').replace(/auto/, 0));
-      $(window).scroll(function (event) {
-        var windowVerticalPosition = $(this).scrollTop();
-        if (windowVerticalPosition >= elementVerticalPosition) {
-          element.addClass('content-fixed');
-        } else {
-          element.removeClass('content-fixed');
-        }
-      });
+    var element = $(this), elementVerticalPosition;
+    if (element.length == 0) {
+      return;
     }
+
+    elementVerticalPosition = element.offset().top - parseFloat(element.css('marginTop').replace(/auto/, 0));
+        
+    function adaptStickiness() {
+      var windowVerticalPosition = $(window).scrollTop();
+      if ($(window).width() > 768 && windowVerticalPosition >= elementVerticalPosition) {
+        if (!element.hasClass('content-fixed')) {
+          element.css('width', element.width() + "px");
+          element.addClass('content-fixed');
+        }
+      } else {
+        element.removeClass('content-fixed');
+        element.css('width', '');
+      }      
+    }
+
+    $(window).scroll(adaptStickiness);
+    $(window).resize(adaptStickiness);
   }
 
   $.fn.extend({
