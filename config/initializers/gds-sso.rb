2af1128123c13993f9edf4a4548e7bf8ecdae06f
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-07-25 16:18:12 +0100

    Add Signonotron permission updates and suspension.
      * Bump gds-sso to 1.1.1, which includes a controller to accept
        permission updates and session expiry notifications
      * Migrate the schema to support the 'remotely_signed_out' flag.
      * Add HTTP basic auth credentials for development mode, so updates
        from Signonotron can be authenticated.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -4,4 +4,6 @@ GDS::SSO.config do |config|
   config.oauth_secret = 'secret'
   config.oauth_root_url = Plek.current.find("signon")
   config.default_scope = "Whitehall"
+  config.basic_auth_user = 'api'
+  config.basic_auth_password = 'defined_on_rollout_not'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120720112913) do
+ActiveRecord::Schema.define(:version => 20120723095757) do
 
   create_table "attachments", :force => true do |t|
     t.string   "carrierwave_file"
@@ -424,6 +424,7 @@ ActiveRecord::Schema.define(:version => 20120720112913) do
     t.string   "uid"
     t.integer  "version"
     t.text     "permissions"
+    t.boolean  "remotely_signed_out", :default => false
   end
 
   add_index "users", ["organisation_id"], :name => "index_users_on_organisation_id"
