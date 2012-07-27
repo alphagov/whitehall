96d55f0bbb206a52c585c7ef478841729d7c01ed
Chris Roos <chris@seagul.co.uk>
2012-07-27 11:26:57 +0100

    Copy Unique reference numbers from Publications to attachments.
    The Unique reference numbers now exist on both the Publication and the
    Attachment.  The next step will be to remove them from the attachment.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120727093357) do
+ActiveRecord::Schema.define(:version => 20120727100641) do
 
   create_table "attachments", :force => true do |t|
     t.string   "carrierwave_file"
