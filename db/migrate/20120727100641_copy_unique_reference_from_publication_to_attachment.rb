9f8dabdda113d3f78c24211496613d944216da2e
Chris Roos <chris@seagul.co.uk>
2012-07-30 19:08:22 +0100

    Update migration to use SQL rather than AR models.
    This migration currently fails in production because it tries to update
    the Attachment model, which in turn tries to store data in the
    price_in_pence column that doesn't yet exist (it's introduced in a later
    migration).  I did try to redefine the AR models in the migration but I
    just couldn't get it to use my new Attachment model: It continued to try
    to use the class in app/models.
    
    I diff'd the debug output of the current and previous implementation of
    this migration to assure myself that they were doing the same thing.

--- db/migrate/20120727100641_copy_unique_reference_from_publication_to_attachment.rb
+++ db/migrate/20120727100641_copy_unique_reference_from_publication_to_attachment.rb
@@ -1,34 +1,47 @@
 class CopyUniqueReferenceFromPublicationToAttachment < ActiveRecord::Migration
   def up
-    Document.where(document_type: 'Publication').each do |publication|
-      # Get the published edition of this publication
-      published_edition = publication.published_edition
+    connection = ActiveRecord::Base.connection
 
-      # Only proceed if there's a published edition with a unique reference
-      if published_edition && published_edition.unique_reference.present?
-        
-        # If we don't have an attachment to copy the unique reference number to then print the edition ID and 
-        # unique reference number so that we don't lose potentially useful data when we remove the unique reference
-        # number from the editions table.
-        if published_edition.attachments.empty?
-          puts "*NOTE* Edition #{published_edition.id} has a unique reference number (#{published_edition.unique_reference}) but no attachments to copy it to."
-        else
+    connection.select_values("SELECT id FROM documents WHERE document_type = 'Publication'").each do |document_id|
 
-          # If there's more than one attachment then print the edition ID as we'll need to check whether we've copied
-          # the ISBN to the correct attachment
-          if published_edition.attachments.length > 1
-            puts "*NOTE* Edition #{published_edition.id} has multiple attachments.  Manually check that we've copied the unique reference number to the correct one."
-          end
+      # Get the ID and ISBN of the published edition
+      connection.select_rows("SELECT id, unique_reference FROM editions WHERE document_id = #{document_id} AND state = 'published'").each do |(edition_id, edition_unique_reference)|
 
-          first_attachment = published_edition.attachments.order(:created_at).first
+        # Only proceed if there's a published edition with a Unique reference
+        if edition_id && edition_unique_reference.present?
 
-          # Fail fast if we're trying to overwrite an existing unique reference number with something different
-          if first_attachment.unique_reference.present? && first_attachment.unique_reference != published_edition.unique_reference
-            raise "The Unique reference on the attachment is different from the one on the publication.  Aborting."
+          attachments = connection.select_rows("SELECT attachments.id, attachments.unique_reference FROM attachments
+            INNER JOIN edition_attachments ON attachments.id = edition_attachments.attachment_id
+            INNER JOIN editions ON editions.id = edition_attachments.edition_id
+            WHERE editions.id = #{edition_id}
+            ORDER BY attachments.created_at ASC")
+
+          # If we don't have an attachment to copy the Unique reference to then print the edition ID and
+          # Unique reference so that we don't lose potentially useful data when we remove the Unique reference
+          # from the editions table.
+          if attachments.length == 0
+            puts "*NOTE* Edition #{edition_id} has a Unique reference (#{edition_unique_reference}) but no attachments to copy it to."
           else
-            first_attachment.update_attribute(:unique_reference, published_edition.unique_reference)
+
+            # If there's more than one attachment then print the edition ID as we'll need to check whether we've copied
+            # the Unique reference to the correct attachment
+            if attachments.length > 1
+              puts "*NOTE* Edition #{edition_id} has multiple attachments.  Manually check that we've copied the Unique reference to the correct one."
+            end
+
+            attachment_id, attachment_unique_reference = attachments.first
+
+            # Fail fast if we're trying to overwrite an existing Unique reference with something different
+            if attachment_unique_reference.present? && attachment_unique_reference != edition_unique_reference
+              raise "The Unique reference on the attachment is different from the one on the publication.  Aborting."
+            else
+              connection.update("UPDATE attachments SET unique_reference = '#{edition_unique_reference}' WHERE id = #{attachment_id}")
+              puts "*INFO* Set Unique reference to '#{edition_unique_reference}' for attachment #{attachment_id} and edition #{edition_id}"
+            end
+
           end
         end
+
       end
     end
   end
