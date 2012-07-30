8db704d1ba4efbc6ccc16374552a024fb9a29cc5
Chris Roos <chris@seagul.co.uk>
2012-07-30 19:13:42 +0100

    Update migration to use SQL rather than AR models.
    This migration currently fails in production because it tries to update
    the Attachment model, which in turn tries to store data in the
    price_in_pence column that doesn't yet exist (it's introduced in a later
    migration).  I did try to redefine the AR models in the migration but I
    just couldn't get it to use my new Attachment model: It continued to try
    to use the class in app/models.
    
    I diff'd the debug output of the current and previous implementation of
    this migration to assure myself that they were doing the same thing.

--- db/migrate/20120727133855_copy_order_urls_from_publications_to_attachments.rb
+++ db/migrate/20120727133855_copy_order_urls_from_publications_to_attachments.rb
@@ -1,34 +1,47 @@
 class CopyOrderUrlsFromPublicationsToAttachments < ActiveRecord::Migration
   def up
-    Document.where(document_type: 'Publication').each do |publication|
-      # Get the published edition of this publication
-      published_edition = publication.published_edition
+    connection = ActiveRecord::Base.connection
 
-      # Only proceed if there's a published edition with an order url
-      if published_edition && published_edition.order_url.present?
+    connection.select_values("SELECT id FROM documents WHERE document_type = 'Publication'").each do |document_id|
 
-        # If we don't have an attachment to copy the order url to then print the edition ID and
-        # order url so that we don't lose potentially useful data when we remove the order url
-        # from the editions table.
-        if published_edition.attachments.empty?
-          puts "*NOTE* Edition #{published_edition.id} has an order url (#{published_edition.order_url}) but no attachments to copy it to."
-        else
+      # Get the ID and Order URL of the published edition
+      connection.select_rows("SELECT id, order_url FROM editions WHERE document_id = #{document_id} AND state = 'published'").each do |(edition_id, edition_order_url)|
 
-          # If there's more than one attachment then print the edition ID as we'll need to check whether we've copied
-          # the order url to the correct attachment
-          if published_edition.attachments.length > 1
-            puts "*NOTE* Edition #{published_edition.id} has multiple attachments.  Manually check that we've copied the order url to the correct one."
-          end
+        # Only proceed if there's a published edition with an Order URL
+        if edition_id && edition_order_url.present?
 
-          first_attachment = published_edition.attachments.order(:created_at).first
+          attachments = connection.select_rows("SELECT attachments.id, attachments.order_url FROM attachments
+            INNER JOIN edition_attachments ON attachments.id = edition_attachments.attachment_id
+            INNER JOIN editions ON editions.id = edition_attachments.edition_id
+            WHERE editions.id = #{edition_id}
+            ORDER BY attachments.created_at ASC")
 
-          # Fail fast if we're trying to overwrite an existing order url with something different
-          if first_attachment.order_url.present? && first_attachment.order_url != published_edition.order_url
-            raise "The Order URL on the attachment is different from the one on the publication.  Aborting."
+          # If we don't have an attachment to copy the Order URL to then print the edition ID and
+          # Order URL so that we don't lose potentially useful data when we remove the Order URL
+          # from the editions table.
+          if attachments.length == 0
+            puts "*NOTE* Edition #{edition_id} has an Order URL (#{edition_order_url}) but no attachments to copy it to."
           else
-            first_attachment.update_attribute(:order_url, published_edition.order_url)
+
+            # If there's more than one attachment then print the edition ID as we'll need to check whether we've copied
+            # the Order URL to the correct attachment
+            if attachments.length > 1
+              puts "*NOTE* Edition #{edition_id} has multiple attachments.  Manually check that we've copied the Order URL to the correct one."
+            end
+
+            attachment_id, attachment_order_url = attachments.first
+
+            # Fail fast if we're trying to overwrite an existing Order URL with something different
+            if attachment_order_url.present? && attachment_order_url != edition_order_url
+              raise "The Order URL on the attachment is different from the one on the publication.  Aborting."
+            else
+              connection.update("UPDATE attachments SET order_url = '#{edition_order_url}' WHERE id = #{attachment_id}")
+              puts "*INFO* Set Order URL to '#{edition_order_url}' for attachment #{attachment_id} and edition #{edition_id}"
+            end
+
           end
         end
+
       end
     end
   end
