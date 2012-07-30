abb26ee8a980962b9ff1bd1a36adbc9a83716822
James Adam <james@lazyatom.com>
2012-07-30 12:47:54 +0100

    Move private methods to the only place they are used.
    Keeping these in a separate module when they aren't used anywhere
    other than these tests is more complexity than is required.

--- test/support/tests_for_national_applicability.rb
+++ test/support/tests_for_national_applicability.rb
@@ -1,8 +1,5 @@
-require 'support/nation_applicability_assertions'
-
 module TestsForNationalApplicability
   extend ActiveSupport::Testing::Declarative
-  include NationApplicabilityAssertions
 
   test "new displays edition form with nation inapplicability fields" do
     get :new
@@ -163,5 +160,41 @@ module TestsForNationalApplicability
 
   def edition_class
     Edition
+  end
+
+  def assert_nation_inapplicability_fields_exist
+    n = Nation.potentially_inapplicable.count
+    assert_select "input[name*='edition[nation_inapplicabilities_attributes]'][type='checkbox']", count: n
+    assert_select "input[name*='edition[nation_inapplicabilities_attributes]'][type='text']", count: n
+  end
+
+  def nation_inapplicabilities_attributes_for(nations_vs_urls, *existing_applicabilities)
+    result = {}
+    [Nation.scotland, Nation.wales, Nation.northern_ireland].each.with_index do |nation, index|
+      h = result[index.to_s] = {
+        _destroy: (nations_vs_urls.keys.include?(nation) ? "0" : "1"),
+        nation_id: nation
+      }
+      if existing = existing_applicabilities.detect { |ea| ea.nation_id == nation.id }
+        h.merge!(id: existing.id, alternative_url: existing.alternative_url)
+      end
+      if nations_vs_urls[nation]
+        h.merge!(alternative_url: nations_vs_urls[nation])
+      end
+    end
+    {nation_inapplicabilities_attributes: result}
+  end
+
+  def assert_nation_inapplicability_fields_set_as(attributes)
+    name_fragment = "edition[nation_inapplicabilities_attributes][#{attributes[:index]}]"
+    if attributes[:checked]
+      assert_select "input[name='#{name_fragment}[_destroy]'][type='checkbox'][checked='checked']"
+    else
+      refute_select "input[name='#{name_fragment}[_destroy]'][type='checkbox'][checked='checked']"
+      assert_select "input[name='#{name_fragment}[_destroy]'][type='checkbox']"
+    end
+    if attributes[:alternative_url]
+      assert_select "input[name='#{name_fragment}[alternative_url]'][value='#{attributes[:alternative_url]}']"
+    end
   end
 end
