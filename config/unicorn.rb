bd1071a0bf9775ec2fd7138e3544d5cf1fc95141
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-07-31 15:38:51 +0100

    Don't need to redirect stderr/stdout
    It's done by upstart.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,3 @@
 app_root = File.dirname(File.dirname(__FILE__))
-log_path = app_root.gsub %r{/var/apps/}, "/var/log/"
-stderr_path "#{log_path}/stderr.log"
-stdout_path "#{log_path}/stdout.log"
 working_directory app_root
 worker_processes 4
