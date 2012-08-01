1ba1b331edd2e9b97b7428c73ebecfdacfc642c2
Edd Sowden <e@e26.co.uk>
2012-08-01 11:56:59 +0000

    Enable multi-element selectors for hideOtherLinks

--- app/assets/javascripts/application/hide-other-links.js
+++ app/assets/javascripts/application/hide-other-links.js
@@ -7,53 +7,55 @@
 (function($) {
   "use strict";
   $.fn.hideOtherLinks = function() {
-    var $el = $(this),
-        showHide = $('<span class="other-content" />'),
-        shownElements = [],
-        hiddenElements = [],
-        currentlyAppending = shownElements;
+    $(this).each(function(i, elm){
+      var $el = $(elm),
+          showHide = $('<span class="other-content" />'),
+          shownElements = [],
+          hiddenElements = [],
+          currentlyAppending = shownElements;
 
-    $($el.contents()).each(function(i, el) {
-      if (el.nodeValue && el.nodeValue === ".") {
-        return;
+      $($el.contents()).each(function(i, el) {
+        if (el.nodeValue && el.nodeValue === ".") {
+          return;
+        }
+        currentlyAppending.push(el);
+        if ($(el).is('a')) {
+          currentlyAppending = hiddenElements;
+        }
+      });
+
+      if (hiddenElements.length) {
+
+        $el.empty();
+
+        $(shownElements).each(function(i, el) {
+          $el[0].appendChild(el);
+        });
+
+        $(hiddenElements).each(function(i, el) {
+          showHide[0].appendChild(el);
+        });
+
+        $el.append(showHide);
+        $el.append(".");
+
+        showHide.hide();
+
+        var toggle = $('<a href="#" class="show-other-content" title="Show additional links">+ others</a>');
+
+        toggle.on('click', function(e) {
+          e.preventDefault();
+          $(this).remove();
+          showHide.show().focus();
+        });
+
+        showHide.before(toggle);
+
+        $el.attr('aria-live', 'polite');
+
       }
-      currentlyAppending.push(el);
-      if ($(el).is('a')) {
-        currentlyAppending = hiddenElements;
-      }
+
     });
-
-    if (hiddenElements.length) {
-
-      $el.empty();
-
-      $(shownElements).each(function(i, el) {
-        $el[0].appendChild(el);
-      });
-
-      $(hiddenElements).each(function(i, el) {
-        showHide[0].appendChild(el);
-      });
-
-      $el.append(showHide);
-      $el.append(".");
-
-      showHide.hide();
-
-      var toggle = $('<a href="#" class="show-other-content" title="Show additional links">+ others</a>');
-
-      toggle.on('click', function(e) {
-        e.preventDefault();
-        $(this).remove();
-        showHide.show().focus();
-      });
-
-      showHide.before(toggle);
-
-      $el.attr('aria-live', 'polite');
-
-    }
-
-    return $el;
+    return this;
   };
 })(jQuery);
