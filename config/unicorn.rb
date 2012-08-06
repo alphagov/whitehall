9d1e8e1f14138414b39d396183561c5f4d899bd7
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-08-06 15:11:12 +0100

    Load default unicorn conf if it exists.
    This is mainly to get the standard unicorn log paths.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,4 @@
-app_root = File.dirname(File.dirname(__FILE__))
-working_directory app_root
+default_unicorn_config_file = "/etc/govuk/unicorn.rb"
+load(default_unicorn_config_file) if File.exist?(default_unicorn_config_file)
+working_directory File.dirname(File.dirname(__FILE__))
 worker_processes 4
