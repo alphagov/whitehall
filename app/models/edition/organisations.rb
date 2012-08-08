518f23d3c913c57b43e7d1e0e9ede7184ef9537b
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-08-08 15:19:24 +0100

    Don't use a single JOIN to bring in foreign key records
    By using a single JOIN we were only including organisations that matched
    the query, rather than returning all of them. This makes the join
    explicit with an EXISTS clause.
    
    https://www.pivotaltracker.com/story/show/34009669

--- app/models/edition/organisations.rb
+++ app/models/edition/organisations.rb
@@ -21,7 +21,14 @@ module Edition::Organisations
 
   module ClassMethods
     def in_organisation(organisation)
-      joins(:organisations).where('organisations.id' => organisation)
+      organisations = [*organisation]
+      slugs = organisations.map(&:slug)
+      where('exists (
+               select * from edition_organisations eo_orgcheck
+                 join organisations orgcheck on eo_orgcheck.organisation_id=orgcheck.id
+               where
+                 eo_orgcheck.edition_id=editions.id
+               and orgcheck.slug in (?))', slugs)
     end
   end
 
