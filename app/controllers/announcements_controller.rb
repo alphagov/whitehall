9d720546fbf497040ae89a2adb23071cb989f7e1
Bradley Wright <brad@intranation.com>
2012-08-11 12:07:01 +0100

    Add WIP new filter that combines news and speeches
    This is untested (and probably doesn't work) as I'm at home, but I've copied the idiom from `Admin::EditionsController::EditionFilter`, and I think it's a better approach as we can actually paginate this effectively.

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,7 +1,21 @@
 class AnnouncementsController < PublicFacingController
 
   def index
-    @announced = AnnouncementPresenter.new
-    @results = @announced.homepage
+    @announced = AnnouncementsFilter.new
+    @results = @announced.announcements
+  end
+
+private
+
+  class AnnouncementsFilter
+    def announcements
+      @announcements ||= (
+        announcements = Edition
+        announcements = announcements.by_type([NewsArticle, Speech])
+        announcements = announcements.published
+        announcements = announcements.by_first_published_at
+        announcements.reverse
+      )
+    end
   end
 end
