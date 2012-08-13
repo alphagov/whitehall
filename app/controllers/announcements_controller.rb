76e13eabc2b04e9c8f34c13181370e6bb2ba5ab3
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-08-13 09:55:06 +0100

    Style changes and fixes

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,20 +1,23 @@
 class AnnouncementsController < PublicFacingController
 
   def index
-    @announced = AnnouncementsFilter.new
-    @results = @announced.announcements
+    announced = AnnouncementsFilter.new
+    @results = announced.announcements
   end
 
 private
 
   class AnnouncementsFilter
+    def initialize(options={})
+      @options = options
+    end
+
     def announcements
       @announcements ||= (
         announcements = Edition
         announcements = announcements.by_type(["NewsArticle", "Speech"])
         announcements = announcements.published
         announcements = announcements.by_first_published_at
-        announcements.reverse
       )
     end
   end
