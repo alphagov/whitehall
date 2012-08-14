470a8890cc26294d4a5e924cb30a65713e12d7d2
Edd Sowden <e@e26.co.uk>
2012-08-14 14:45:47 +0000

    Use public document paths

--- app/helpers/policy_helper.rb
+++ app/helpers/policy_helper.rb
@@ -6,6 +6,6 @@ module PolicyHelper
   end
 
   def list_of_links_to_policies(policies)
-    policies.map { |policy| link_to policy.title, policy_path(policy), class: "policy", id: "policy_#{policy.id}" }.to_sentence.html_safe
+    policies.map { |policy| link_to policy.title, public_document_path(policy), class: "policy", id: "policy_#{policy.id}" }.to_sentence.html_safe
   end
 end
