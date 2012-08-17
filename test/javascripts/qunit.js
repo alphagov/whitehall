6518fb52e135284466062330897a19275f62f47e
Chris Roos <chris@seagul.co.uk>
2012-08-17 13:33:29 +0100

    Nest the javascript unit tests in their own directory
    I'm not 100% sure about this but it was the easiest change I could make
    so that I could use `require_tree` in qunit.js.  This avoids us having
    to explicitly list each test file, which is prone to error.

--- test/javascripts/qunit.js
+++ test/javascripts/qunit.js
@@ -2,18 +2,4 @@
 // SUITE MANIFEST :: test/javascripts/qunit.js
 // TEST SRC DIR :: test/javascripts
 //
-//= require preview_test
-//= require mark_linked_anchor_test
-//= require sortable_test
-//= require maps_test
-//= require add_toggle_link_test
-//= require multiple_file_upload_test
-//= require organisation_contact_form_test
-//= require equal_height_helper_test
-//= require edition_publishing_test
-//= require toggler_test
-//= require nav_helper_test
-//= require split_into_pages_test
-//= require heading_navigation_test
-//= require hide_topics_link_test
-//= require document_filter_test
+//= require_tree ./unit
