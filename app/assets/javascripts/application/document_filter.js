710a5b487177a820887dee3900a198689be972a8
Edd Sowden <e@e26.co.uk>
2012-08-20 10:01:50 +0000

    Refactor document_filter to be a GOVUK module
    Refactoring to a module makes it far more unit-testable. It also makes
    it far more obvious which parts can be unit tested.
    
    I have added tests and fixed some things that were broken that were
    revealed due to implementing tests.
    
    Also switched to use our `js-` prefix classes so we don't have to rely
    on a form having a specific id.

--- app/assets/javascripts/application/document_filter.js
+++ app/assets/javascripts/application/document_filter.js
@@ -5,150 +5,154 @@
  vars: true */
 /*global jQuery */
 
+if(typeof window.GOVUK === 'undefined'){ window.GOVUK = {}; }
 (function($) {
-    "use strict";
-    function progressSpan(current, total) {
-        var span = $("<span />");
-        span.text(current + " of " + total);
-        return span;
-    }
-    function drawPagination(data, after) {
-      var existingNav = $('#show-more-documents');
-       if (existingNav.length > 0) {
-         existingNav.remove();
-       }
+  "use strict";
+
+  var documentFilter = {
+    options: {
+      infinateScroll: true,
+      $form: null
+    },
+    drawPagination: function (data) {
+      var $existingNav = $('#show-more-documents'),
+          $nav = $('');
+      if ($existingNav.length > 0) {
+        $existingNav.remove();
+      }
       if (data.next_page_url || data.prev_page_url) {
-        var nav = $('<nav id="show-more-documents" role="navigation" />'),
-            ul = $('<ul class="previous-next-navigation" />'),
-            li, a;
-        nav.append(ul);
+        var $ul = $('<ul class="previous-next-navigation" />'),
+            $li, $a;
+
+        $nav = $('<nav id="show-more-documents" role="navigation" />').append($ul);
         if (data.prev_page_url) {
-          li = $('<li class="previous" />');
-          a = $('<a>Previous page</a>');
-          a.attr('href', data.prev_page_url);
-          ul.append(li);
-          li.append(a);
-          a.append(" ");
-          a.append(progressSpan(data.prev_page, data.total_pages));
+          $li = $('<li class="previous" />');
+          $a = $('<a>Previous page</a>').attr('href', data.prev_page_url);
+          $ul.append($li);
+          $li.append($a);
+          $a.append(" ").append(documentFilter.progressSpan(data.prev_page, data.total_pages));
         }
         if (data.next_page_url) {
-          li = $('<li class="next" />');
-          a = $('<a>Next page</a>');
-          a.attr('href', data.next_page_url);
-          ul.append(li);
-          li.append(a);
-          a.append(" ");
-          a.append(progressSpan(data.next_page, data.total_pages));
+          $li = $('<li class="next" />');
+          $a = $('<a>Next page</a>').attr('href', data.next_page_url);
+          $ul.append($li);
+          $li.append($a);
+          $a.append(" ").append(documentFilter.progressSpan(data.next_page, data.total_pages));
         }
-
-        $(after).after(nav);
       }
-    }
-    function importantAttribute(attribute) {
+      return $nav;
+    },
+    progressSpan: function(current, total) {
+      return '<span>' + current + " of " + total + '</span>';
+    },
+    importantAttribute: function(attribute) {
       return ($.inArray(attribute, ["id", "title", "url", "type"]) < 0);
-    }
-    function capitalize(attribute) {
+    },
+    capitalize: function(attribute) {
       return attribute.replace(/_/, " ").replace(/(^|\s)([a-z])/g, function(_,a,b){ return a+b.toUpperCase(); });
-    }
-    function drawTable(data) {
-        var container = $('.filter-results');
-        if (data.results.length > 0) {
-            var tBody, i, l;
-            if (!document.getElementById('document-list')) {
-                var table = $('<table id="document-list" class="document-list" />'),
-                    tHead = $('<thead class="visuallyhidden" />'),
-                    tr = $('<tr />');
-                for(var attribute in data.results[0]) {
-                  if (importantAttribute(attribute)) {
-                    tr.append($('<tr scope="col">' + capitalize(attribute) + '</tr>'))
-                  }
-                }
+    },
+    drawTableRows: function(results) {
+      var $tBody = $('<tbody />'), i,l;
 
-                tHead.append(tr);
-                table.append(tHead);
-                table.append('<tbody />');
-                container.empty().append(table);
-            }
+      for (i=0, l=results.length; i<l; i++) {
+        var row = results[i],
+            $tableRow = $('<tr class="document-row" />'),
+            $th = $('<th scope="row" class="title attribute"/>'),
+            $a = $('<a href="'+ row.url +'" title="View '+ row.title +'">'+ row.title +'</a>');
 
-            tBody = $('<tbody />');
-
-            for (i=0, l=data.results.length; i<l; i++) {
-                var row = data.results[i],
-                    tableRow = $('<tr />'),
-                    th = $('<th />'),
-                    a = $('<a />');
-                tableRow.attr('id', row.type + '_' + row.id).addClass('document-row').addClass((i < 3 ? 'recent' : ''));
-                th.attr('scope', 'row').addClass('title attribute');
-                a.attr('href', row.url).attr('title', "View " + row.title).text(row.title);
-                th.append(a);
-                tableRow.append(th);
-                for(var attribute in row) {
-                  if (importantAttribute(attribute)) {
-                    tableRow.append($('<td class="' + attribute + ' attribute">' + row[attribute] + '</td>'));
-                  }
-                }
-                tBody.append(tableRow);
-            }
-
-            $('.filter-results .document-list tbody').replaceWith(tBody);
-
-            drawPagination(data, tBody.parent());
-
+        $tableRow.attr('id', row.type + '_' + row.id).addClass((i < 3 ? ' recent' : ''));
+        $th.append($a);
+        $tableRow.append($th);
+        for(var attribute in row) {
+          if (documentFilter.importantAttribute(attribute)) {
+            $tableRow.append($('<td class="' + attribute + ' attribute">' + row[attribute] + '</td>'));
+          }
         }
-        else {
-            container.empty();
-            container.append('<div class="no-results"><h2>There are no matching documents.</h2>' +
-                             '<p>Try making your search broader and try again.</p></div>');
+        $tBody.append($tableRow);
+      }
+      return $tBody;
+    },
+    drawTable: function(data) {
+      var $container = $('.filter-results');
+      if (data.results.length > 0) {
+        var $tBody;
+        if ($('#document-list').length < 1) {
+          var $table = $('<table id="document-list" class="document-list" />'),
+              $tHead = $('<thead class="visuallyhidden" />'),
+              $tr = $('<tr />').append('<th scope="col">Title</th>');
+
+          for(var attribute in data.results[0]) {
+            if (documentFilter.importantAttribute(attribute)) {
+              $tr.append($('<th scope="col">' + documentFilter.capitalize(attribute) + '</th>'))
+            }
+          }
+          $tHead.append($tr);
+          $table.append($tHead);
+          $table.append('<tbody />');
+          $container.empty().append($table);
         }
-    }
-    function updateAtomFeed(data) {
+        $tBody = documentFilter.drawTableRows(data.results);
+        $container.find('.document-list tbody').replaceWith($tBody);
+        $container.append(documentFilter.drawPagination(data));
+      } else {
+        $container.empty();
+        $container.append('<div class="no-results"><h2>There are no matching documents.</h2>' +
+                        '<p>Try making your search broader and try again.</p></div>');
+      }
+    },
+    updateAtomFeed: function(data) {
       if (data.atom_feed_url) {
         $(".subscribe a.feed").attr("href", data.atom_feed_url);
       }
-    }
+    },
+    submitFilters: function(e){
+      e.preventDefault();
+      var $form = documentFilter.options.$form,
+          $submitButton = $form.find('input[type=submit]'),
+          url = $form.attr('action'),
+          params = $form.serializeArray();
 
-    var _enableDocumentFilter = function() {
-      if (!History.enabled) {
-        return false;
-      }
-      var $form = $(this);
-      $form.submit(function(e) {
-          e.preventDefault();
-          var $submitButton = $form.find('input[type=submit]'),
-              url = $form.attr('action'),
-              params = $form.serializeArray();
-
-          $submitButton.addClass('disabled');
-          // TODO: make a spinny updating thing
-          $.ajax(url, {
-              cache: false,
-              dataType:'json',
-              data: params,
-              success: function(data) {
-                updateAtomFeed(data);
-                if (data.results) {
-                  drawTable(data);
-                }
-                History.pushState(null, null, url + "?" + $form.serialize());
-                // undo double-click protection
-                $submitButton.removeAttr('disabled').removeClass('disabled');
-              },
-              error: function() {
-                $submitButton.removeAttr('disabled');
-              }
-          });
-
-      });
-      $form.find('select').change(function(e){
-        $form.submit();
+      $submitButton.addClass('disabled');
+      // TODO: make a spinny updating thing
+      $.ajax(url, {
+        cache: false,
+        dataType:'json',
+        data: params,
+        success: function(data) {
+          documentFilter.updateAtomFeed(data);
+          if (data.results) {
+            documentFilter.drawTable(data);
+          }
+          History.pushState(null, null, url + "?" + $form.serialize());
+          // undo double-click protection
+          $submitButton.removeAttr('disabled').removeClass('disabled');
+        },
+        error: function() {
+          $submitButton.removeAttr('disabled');
+        }
       });
     }
+  };
+  window.GOVUK.documentFilter = documentFilter;
 
-    $.fn.extend({
-      enableDocumentFilter: _enableDocumentFilter
+
+
+  var enableDocumentFilter = function(pluginOptions) {
+    if (!History.enabled) {
+      return false;
+    }
+
+    var $form = $(this);
+    documentFilter.options = $.extend(documentFilter.options, pluginOptions);
+    documentFilter.options.$form = $form;
+
+    $form.submit(documentFilter.submitFilters);
+    $form.find('select').change(function(e){
+      $form.submit();
     });
-})(jQuery);
+  }
 
-jQuery(function($) {
-  $("form#document-filter").enableDocumentFilter();
-})
+  $.fn.extend({
+    enableDocumentFilter: enableDocumentFilter
+  });
+})(jQuery);
--- test/javascripts/unit/document_filter_test.js
+++ test/javascripts/unit/document_filter_test.js
@@ -5,18 +5,112 @@ module("Document filter", {
     History.pushState = function(state,title,url){
       return true;
     };
-    
+
     this.filterForm = $('<form id="document-filter" action="/foo/bar"><input type="submit" /></form>');
     $('#qunit-fixture').append(this.filterForm);
 
     this.filterResults = $('<div class="filter-results" />');
     $('#qunit-fixture').append(this.filterResults);
+
+    this.atomLink = $('<div class="subscribe"><a class="feed">feed</a></div>');
+    $('#qunit-fixture').append(this.atomLink);
+
+    this.ajaxData = {
+      "next_page_url": '/next-page-url',
+      "prev_page_url": '/prev-page-url',
+      "atom_feed_url": '/atom-feed',
+      "results": [
+        {
+          "id": 1,
+          "type": "document-type",
+          "title": "document-title",
+          "url": "/document-path",
+          "organisations": "organisation-name-1, organisation-name-2",
+          "topics": "topic-name-1, topic-name-2"
+        },
+        {
+          "id": 2,
+          "type": "document-type-2",
+          "title": "document-title-2",
+          "url": "/document-path-2",
+          "organisations": "organisation-name-2, organisation-name-3",
+          "topics": "topic-name-1, topic-name-2"
+        }
+      ]
+    };
   },
   tearDown: function() {
     History.enabled = this.originalHistoryEnabled;
     History.pushState = this.originalHistoryPushState;
   }
 });
+
+test("should create pagination from ajax data", function() {
+  var data = this.ajaxData;
+
+  var $pagination = GOVUK.documentFilter.drawPagination(data);
+  ok($pagination.find('a[href="/next-page-url"]').length > 0);
+  ok($pagination.find('a[href="/prev-page-url"]').length > 0);
+
+  delete data.next_page_url;
+  var $pagination = GOVUK.documentFilter.drawPagination(data);
+  equals($pagination.find('a[href="/next-page-url"]').length, 0);
+  ok($pagination.find('a[href="/prev-page-url"]').length > 0);
+
+  delete data.prev_page_url;
+  var $pagination = GOVUK.documentFilter.drawPagination(data);
+  equals($pagination.find('a[href="/next-page-url"]').length, 0);
+  equals($pagination.find('a[href="/prev-page-url"]').length, 0);
+});
+
+test("should identify important attributes", function(){
+  var importantAttributes = ['id', 'title', 'url', 'type'],
+      attr;
+
+  for(attr in importantAttributes){
+    ok(GOVUK.documentFilter.importantAttribute(attr));
+  }
+});
+
+test("should create table rows from ajax data", function() {
+  var $tbody = GOVUK.documentFilter.drawTableRows(this.ajaxData.results);
+
+  equals($tbody.find('#document-type_1').length, 1, "document row exists");
+  equals($tbody.find('a[href="/document-path"]').length, 1, "links to document");
+  equals($tbody.find('td:contains("topic-name-1, topic-name-2")').length, 2, "topics are visible");
+
+  equals($tbody.find('#document-type-2_2').length, 1, "document row exists");
+  equals($tbody.find('a[href="/document-path-2"]').length, 1, "links to document");
+  equals($tbody.find('td:contains("organisation-name-2, organisation-name-3")').length, 1, "organisations are visible");
+});
+
+test("should create table from ajax data", function() {
+  GOVUK.documentFilter.drawTable(this.ajaxData);
+
+  equals(this.filterResults.find('thead th:contains("Title")').length, 1);
+  equals(this.filterResults.find('thead th:contains("Topic")').length, 1);
+  equals(this.filterResults.find('thead th:contains("Organisation")').length, 1);
+
+  equals(this.filterResults.find('tbody tr').length, 2);
+
+  equals(this.filterResults.find('#show-more-documents').length, 1);
+});
+
+test("should show message when ajax data is empty", function() {
+  GOVUK.documentFilter.drawTable({ results: [] });
+
+  equals(this.filterResults.find('table').length, 0);
+  equals(this.filterResults.find('.no-results').length, 1);
+});
+
+test("should update the atom feed url", function() {
+  equals(this.atomLink.find('a[href="/atom-feed"]').length, 0);
+
+  GOVUK.documentFilter.updateAtomFeed(this.ajaxData);
+
+  equals(this.atomLink.find('a[href="/atom-feed"]').length, 1);
+});
+
 
 test("should make an ajax request on form submission to obtain filtered results", function() {
   this.filterForm.enableDocumentFilter();
@@ -65,12 +159,12 @@ test("should generate table of results baed on successful ajax response", functi
   this.filterForm.enableDocumentFilter();
 
   var server = this.sandbox.useFakeServer();
-  server.respondWith('{ "results": [ { "id": 1, "type": "document-type", "title": "document-title", "url": "/document-path", "organisations": "organisation-name-1, organisation-name-2", "topics": "topic-name-1, topic-name-2" } ] }')
+  server.respondWith(JSON.stringify(this.ajaxData))
 
   this.filterForm.submit();
   server.respond();
 
-  equals($(this.filterResults).find("table tbody tr").length, 1)
+  equals(this.filterResults.find("table tbody tr").length, 2)
 })
 
 test("should update browser location on successful ajax response", function() {
@@ -78,7 +172,7 @@ test("should update browser location on successful ajax response", function() {
 
   var historyPushState = this.spy(History, "pushState");
   var server = this.sandbox.useFakeServer();
-  server.respondWith('{ "results": [ { "id": 1, "type": "document-type", "title": "document-title", "url": "/document-path", "organisations": "organisation-name-1, organisation-name-2", "topics": "topic-name-1, topic-name-2" } ] }')
+  server.respondWith(JSON.stringify(this.ajaxData))
 
   $(this.filterForm).attr("action", "/specialist")
   $(this.filterForm).append($('<select name="foo"><option value="bar" /></select>'))
@@ -103,11 +197,12 @@ test("should not enable ajax filtering if browser does not support HTML5 History
 
   var ajax = this.spy(jQuery, "ajax");
   var server = this.sandbox.useFakeServer();
-  
+
   this.filterForm.attr('action', 'javascript:void(0)');
   this.filterForm.submit();
   server.respond();
-  
+
   sinon.assert.callCount(ajax, 0);
 })
+
 
