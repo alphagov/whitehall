e5717a257d31cfd0916d517e1d414cc90fe1bdfc
James Adam <james@lazyatom.com>
2012-08-24 16:47:37 +0100

    Don't try to load simplecov before the bundle.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,13 @@
+$:.unshift(File.dirname(__FILE__))
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+
 require 'simplecov'
 require 'simplecov-rcov'
 
 SimpleCov.start 'rails'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 
-$:.unshift(File.dirname(__FILE__))
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha'
 require 'slimmer/test'
