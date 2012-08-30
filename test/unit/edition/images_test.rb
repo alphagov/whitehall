396804738c0b592608f36c69ed59698c5b4e0f48
Chris Roos <chris@seagul.co.uk>
2012-08-30 17:42:27 +0100

    Make the Images test more generic
    This test should be focussed on testing the behaviour in the
    Edition::Images module.  Following on from the approach we use in
    alternative_format_provider_test.rb, I've introduced a new Edition
    subclass that includes nothing more than the Images module.  Prior to
    this, we were actually testing Policies with no guarantee that we
    weren't testing Policy specific behaviour.
    
    I wonder whether it'd be possible/easy to add test class specific
    factory-girl factories to these types of tests, so that we can remove
    some of the duplication introduced by our having to manually build the
    objects required. That can wait for now, though.

--- test/unit/edition/images_test.rb
+++ test/unit/edition/images_test.rb
@@ -1,17 +1,22 @@
 require 'test_helper'
 
 class Edition::ImagesTest < ActiveSupport::TestCase
+  class EditionWithImages < Edition
+    include ::Edition::Images
+  end
+
   include ActionDispatch::TestProcess
 
   test "editions can be created with multiple images" do
-    edition = create(:draft_policy, images_attributes: [
+    edition = EditionWithImages.create!(valid_edition_attributes.merge(
+      images_attributes: [
       {alt_text: "Something about this image",
        caption: "Text to be visible along with the image",
        image_data_attributes: {file: fixture_file_upload('portas-review.jpg')}},
       {alt_text: "alt-text-2",
        caption: "caption-2",
        image_data_attributes: {file: fixture_file_upload('portas-review.jpg')}}
-    ])
+    ]))
 
     assert_equal 2, edition.images.count
     assert_equal "Something about this image", edition.images[0].alt_text
@@ -22,12 +27,17 @@ class Edition::ImagesTest < ActiveSupport::TestCase
 
   test "#create_draft should include copies of image attributes" do
     image = create(:image)
-    published_policy = create(:published_policy, images: [image])
-    draft_policy = published_policy.create_draft(create(:policy_writer))
+    published_edition = EditionWithImages.create!(valid_edition_attributes.merge(
+      state:              'published',
+      published_at:       Time.zone.now,
+      first_published_at: Time.zone.now,
+      images:             [image]
+    ))
+    draft_edition = published_edition.create_draft(build(:user))
 
-    assert draft_policy.valid?
+    assert draft_edition.valid?
 
-    new_image = draft_policy.images.last
+    new_image = draft_edition.images.last
     refute_equal image, new_image
     assert_equal image.alt_text, new_image.alt_text
     assert_equal image.caption, new_image.caption
@@ -35,29 +45,52 @@ class Edition::ImagesTest < ActiveSupport::TestCase
 
   test "#create_draft should not duplicate the actual image data" do
     image = create(:image)
-    published_policy = create(:published_policy, images: [image])
-    draft_policy = published_policy.create_draft(create(:policy_writer))
-    new_image = draft_policy.images.last
+    published_edition = EditionWithImages.create!(valid_edition_attributes.merge(
+      state:              'published',
+      published_at:       Time.zone.now,
+      first_published_at: Time.zone.now,
+      images:             [image]
+    ))
+    draft_edition = published_edition.create_draft(build(:user))
+    new_image = draft_edition.images.last
 
     assert_equal image.image_data_id, new_image.image_data_id
   end
 
   test "captions for images can be changed between versions" do
-    edition = create(:published_policy, images_attributes: [
-      {alt_text: "alt-text",
-       caption: "original-caption",
-       image_data_attributes: {file: fixture_file_upload('portas-review.jpg')}}])
+    published_edition = EditionWithImages.create!(valid_edition_attributes.merge(
+      state:              'published',
+      published_at:       Time.zone.now,
+      first_published_at: Time.zone.now,
+      images_attributes:  [{
+        alt_text: "alt-text",
+        caption: "original-caption",
+        image_data_attributes: {
+          file: fixture_file_upload('portas-review.jpg')
+        }
+      }]
+    ))
 
-    draft = edition.create_draft(create(:policy_writer))
-    draft.images.first.update_attributes(caption: "new-caption")
+    draft_edition = published_edition.create_draft(build(:user))
+    draft_edition.images.first.update_attributes(caption: "new-caption")
 
-    assert_equal "original-caption", edition.images.first.caption
+    assert_equal "original-caption", published_edition.images.first.caption
   end
 
   test "#destroy should also remove the image" do
     image = create(:image)
-    edition = create(:draft_policy, images: [image])
+    edition = EditionWithImages.create!(valid_edition_attributes.merge(images: [image]))
     edition.destroy
     refute Image.find_by_id(image.id)
+  end
+
+  private
+
+  def valid_edition_attributes
+    {
+      title:   'edition-title',
+      body:    'edition-body',
+      creator: build(:user)
+    }
   end
 end
