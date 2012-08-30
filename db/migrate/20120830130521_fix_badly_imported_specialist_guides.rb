1bacd2a7fa36fec3fe8f407af15a01890a0c9c05
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-08-30 13:12:19 +0000

    Add migration to repair badly imported specialist guides
    The guides were imported with a bad regular expression (see da29b5),
    this migration repairs them.

---
+++ db/migrate/20120830130521_fix_badly_imported_specialist_guides.rb
@@ -0,0 +1,18 @@
+class FixBadlyImportedSpecialistGuides < ActiveRecord::Migration
+  def repair_broken_headings(govspeak)
+    govspeak.gsub(/\.##/, ".\n\n##")
+  end
+
+  def update_govspeak(edition)
+    edition.update_column(:body, repair_broken_headings(edition.body))
+  end
+
+  def up
+    SpecialistGuide.where("body LIKE ?", "%.##%").each do |guide|
+      update_govspeak(guide)
+    end
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829110241) do
+ActiveRecord::Schema.define(:version => 20120830130521) do
 
   create_table "attachments", :force => true do |t|
     t.string   "carrierwave_file"
