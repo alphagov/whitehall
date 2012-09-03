3eb10ddc5df5491f5edd7be1b8799640a6074fcd
Edd Sowden <e@e26.co.uk>
2012-09-03 13:29:53 +0000

    Refactor stick at top when scrolling
    The reason for this was to fix a bug where if the content on the page
    changed after page load the stick at top would break. Howerver, I saw
    this as an opertunity to refactor this into the new style that we are
    adopting for javascript functionality.
    
    The reason for using `call(this)` is that it can then be called from a
    non-`window` scop and not break anything.

---
+++ test/javascripts/unit/stick_at_top_when_scrolling_test.js
@@ -0,0 +1,26 @@
+module("Stick at top when scrolling", {
+  setup: function(){
+    this.$nav = $('<div class="stick-at-top-when-scrolling"></div>');
+
+    $('#qunit-fixture').append(this.$nav);
+  }
+});
+
+test('should add fixed class on stick', function(){
+  ok(!this.$nav.hasClass('content-fixed'));
+  GOVUK.stickAtTopWhenScrolling.stick(this.$nav);
+  ok(this.$nav.hasClass('content-fixed'));
+});
+
+test('should remove fixed class on release', function(){
+  this.$nav.addClass('content-fixed');
+  GOVUK.stickAtTopWhenScrolling.release(this.$nav);
+  ok(!this.$nav.hasClass('content-fixed'));
+});
+
+test('should insert shim when sticking content', function(){
+  equal($('.shim').length, 0);
+  GOVUK.stickAtTopWhenScrolling.stick(this.$nav);
+  equal($('.shim').length, 1);
+});
+
