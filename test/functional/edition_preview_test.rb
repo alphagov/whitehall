35854c64ac0e1ac47ad051195359ae044254660b
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-04 14:56:25 +0000

    Remove pointless file as per @lazyatom's comment
    https://github.com/alphagov/whitehall/commit/40065de6edd64ed04f58634a908a0b4407d09d06#commitcomment-1805430

--- test/functional/edition_preview_test.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-require "test_helper"
-
-class EditionPreviewTest < ActionController::TestCase
-  include ActionDispatch::Routing::UrlFor
-  include PublicDocumentRoutesHelper
-  default_url_options[:host] = 'test.host'
-end
