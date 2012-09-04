530e56c9a07024ff9c3a4c38451d49fa5b428571
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-04 12:08:53 +0000

    Make session cookie HTTPS only in !dev/test environments
    One of the outcomes of security auditing was that our session cookie is
    vulnerable to man in the middle/session replay attacks because it's not
    HTTPS only. This change makes the session initializer (sic) enable
    HTTPS-only cookies in non test/dev situations.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Whitehall::Application.config.session_store :cookie_store, key: '_whitehall_session'
+Whitehall::Application.config.session_store :cookie_store, key: '_whitehall_session',
+                                                           secure: !(Rails.env.test? || Rails.env.development?)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
