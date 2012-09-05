aca5fed2b471f46119a6180c7727313b2ef4eb3b
Tom Ward <tom@popdog.net>
2012-09-05 17:11:57 +0100

    Remove unused helper method

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -11,20 +11,6 @@ module AnnouncementsHelper
     end
   end
 
-  def announcement_group(annoucements, options = {})
-    capture do
-      annoucements.in_groups_of(options[:groups_of], false) do |announcement_group|
-        row = content_tag(:div, class: ["row", "row_#{announcement_row_number}", options[:class]].compact.join(" ")) do
-          cells = announcement_group.map do |announcement|
-            render partial: "announcements/home_article", locals: { item: announcement }
-          end
-          cells.join("\n").html_safe
-        end
-        concat row
-      end
-    end
-  end
-
   def announcement_row_number
     @announcement_row_number ||= 0
     @announcement_row_number += 1
