508e710eafb975ce1e8f720d8f235fd00962057f
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-07 07:44:02 +0000

    Update tests to match new validation string
    For the record I'm not entirely happy that a string change can break 40
    tests - shouldn't this be abstracted somewhere?

--- test/support/govspeak_validation_test_helper.rb
+++ test/support/govspeak_validation_test_helper.rb
@@ -4,7 +4,7 @@ module GovspeakValidationTestHelper
       test "should protect against XSS and content attacks via #{attribute}" do
         object = build(factory_name_from_test, attribute => "<script>badThings();</script>")
         refute object.valid?, "should not be valid with unsafe content"
-        assert object.errors[attribute].include?("cannot include invalid Govspeak or JavaScript"), "didn't add validation errors to #{attribute} attribute"
+        assert object.errors[attribute].include?("cannot include invalid formatting or JavaScript"), "didn't add validation errors to #{attribute} attribute"
       end
     end
   end
