9935ca55ee41f2f53ba1f89e451c16d5a37dd28c
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-10 14:20:13 +0000

    Deny WAP requests to public controllers with 406
    Google is crawling us with a WAP-like browser, which causes fatal errors
    due to missing response formats.
    
    Additionally, as our site will not function well on WAP connections due
    to the large size of assets etc. we serve, we should deny the
    connection until a better site is in place.
    
    Note that using a blanket:
    
        respond_to :html
    
    in the controller doesn't work for some reason, hence the filter.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Whitehall::Application.initialize!
+
+# Register WAP request types
+Mime::Type.register 'application/vnd.wap.xhtml+xml', :wap
