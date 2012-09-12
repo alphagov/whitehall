d270d70b9e81c3bdb1273397a4dde0272159aef1
Chris Roos <chris@seagul.co.uk>
2012-09-12 12:12:51 +0100

    Remove Consultation Responses from database
    Consultation responses can only be associated with attachments (through
    edition_attachments) so that's the only join table we need to remove
    records from.  *NOTE* We need to keep the attachments themselves because
    they'll be linked to Responses through the
    consultation_response_attachments join table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120911152726) do
+ActiveRecord::Schema.define(:version => 20120912105635) do
 
   create_table "attachments", :force => true do |t|
     t.string   "carrierwave_file"
