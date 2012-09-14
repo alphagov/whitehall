27db58140f387ca99577b101331b9a355c909aa9
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-09-14 16:59:27 +0100

    Use specialist layout on mainstream category browse page

--- app/controllers/mainstream_categories_controller.rb
+++ app/controllers/mainstream_categories_controller.rb
@@ -1,4 +1,6 @@
 class MainstreamCategoriesController < PublicFacingController
+  layout "specialist"
+
   def show
     @mainstream_category = MainstreamCategory.find_by_slug(params[:id])
     @specialist_guides = @mainstream_category.specialist_guides.published
