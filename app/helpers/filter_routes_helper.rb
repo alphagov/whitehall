2688eb4c0cd28723061634890529aaa27544172d
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-17 14:17:53 +0100

    Make sure "all policies" links to filters view

--- app/helpers/filter_routes_helper.rb
+++ app/helpers/filter_routes_helper.rb
@@ -11,6 +11,12 @@ module FilterRoutesHelper
     end
   end
 
+  def policies_filter_path(obj)
+    if obj.is_a? Organisation
+      policies_path(departments: [obj.slug])
+    end
+  end
+
   def specialist_guides_filter_path(obj)
     if obj.is_a? Organisation
       specialist_guides_path(departments: [obj.slug])
