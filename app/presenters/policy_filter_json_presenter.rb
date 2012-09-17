6214b3c23554bf5bfff244034bfd788305a246ab
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-17 13:51:01 +0100

    Fix to policy filter page to use topics
    Add topics to JSON/index view, and add some extra spacing between the
    results and the filter.
    
    https://www.pivotaltracker.com/story/show/35187489

--- app/presenters/policy_filter_json_presenter.rb
+++ app/presenters/policy_filter_json_presenter.rb
@@ -1,10 +1,7 @@
 class PolicyFilterJsonPresenter < DocumentFilterJsonPresenter
-
   def document_hash(document)
     super.merge(
-      first_published_at: h.render_datetime_microformat(document, :first_published_at) {
-        document.first_published_at.to_s(:long_ordinal)
-      }.html_safe
+      topics: document.topics.map(&:name).join(", ").html_safe
     )
   end
 end
