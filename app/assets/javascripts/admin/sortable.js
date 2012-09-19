3a1054062b338265abca037424868b1ddc253e1c
James Adam <james@lazyatom.com>
2012-09-19 16:25:56 -0500

    Allow multiple sortables on the same page.
    I need this to allow sorting of the non-ministerial roles.

--- app/assets/javascripts/admin/sortable.js
+++ app/assets/javascripts/admin/sortable.js
@@ -1,27 +1,29 @@
 (function ($) {
   var _enableSortable = function() {
-    var fieldset = $(this);
-    var list = $("<ul></ul>");
-    fieldset.find("input.ordering").hide();
-    fieldset.children("div").each(function(i, item) {
-      var li = $('<li class="sort_item"></li>');
-      li.append(item);
-      list.append(li);
+    $(this).each(function() {
+      var fieldset = $(this);
+      var list = $("<ul></ul>");
+      fieldset.find("input.ordering").hide();
+      fieldset.children("div").each(function(i, item) {
+        var li = $('<li class="sort_item"></li>');
+        li.append(item);
+        list.append(li);
+      })
+      fieldset.after(list);
+      list.sortable({
+        distance: 15,
+        opacity: 0.5,
+        update: function(event, ui) {
+          list.children(".sort_item").each(function(index, li) {
+            var input_id = $(li).find("label").attr("for");
+            var input = $("#" + input_id)
+            input.val(index);
+          })
+        },
+        placeholder: "well sortable-drop-target",
+        axis: "y"
+      });
     })
-    fieldset.after(list);
-    list.sortable({
-      distance: 15,
-      opacity: 0.5,
-      update: function(event, ui) {
-        list.children(".sort_item").each(function(index, li) {
-          var input_id = $(li).find("label").attr("for");
-          var input = $("#" + input_id)
-          input.val(index);
-        })
-      },
-      placeholder: "well sortable-drop-target",
-      axis: "y"
-    });
   }
 
   $.fn.extend({
