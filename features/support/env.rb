8f91d77e32bb945bd2d46f504bad26534b54a51b
James Adam <james@lazyatom.com>
2012-09-21 14:09:09 -0500

    Use 1.9 hash syntax.

--- features/support/env.rb
+++ features/support/env.rb
@@ -56,7 +56,7 @@ class ActiveRecord::Base
   @@shared_connection = nil
 
   def self.connection
-    @@shared_connection || ConnectionPool::Wrapper.new(:size => 1) { retrieve_connection }
+    @@shared_connection || ConnectionPool::Wrapper.new(size: 1) { retrieve_connection }
   end
 end
 ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
