da8960cfb3c99a88d246df4bf0335e00b473a7cb
Tom Ward <tom@popdog.net>
2012-09-24 16:58:27 +0100

    Use GdsApi::Rummager in favour of our own search client

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -1,12 +1,14 @@
+require 'gds_api/rummager'
+
 Rummageable.rummager_service_name = "whitehall-search"
 Rummageable.path_prefix = Whitehall.router_prefix
 Rummageable.rummager_host = ENV["RUMMAGER_HOST"] if ENV["RUMMAGER_HOST"]
 
-Whitehall.search_client = Whitehall::SearchClient.new(Rummageable.rummager_host + Whitehall.router_prefix)
+Whitehall.search_client = GdsApi::Rummager.new(Rummageable.rummager_host + Whitehall.router_prefix)
 
 unless Rails.env.production? || ENV["RUMMAGER_HOST"]
   Rummageable.implementation = Rummageable::Fake.new
 end
 
 mainstream_rummager_host = ENV["MAINSTREAM_RUMMAGER_HOST"] || Plek.current.find('search')
-Whitehall.mainstream_search_client = Whitehall::SearchClient.new(mainstream_rummager_host)
+Whitehall.mainstream_search_client = GdsApi::Rummager.new(mainstream_rummager_host)
