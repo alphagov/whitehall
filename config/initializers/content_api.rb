f724ac92d8c5a7eead6128731d6ffc950e4347b3
James Mead <james@floehopper.org>
2012-09-27 11:13:34 +0100

    Use a fake content API in development & test environments.
    Also allow overriding of content API configuration to allow developers
    to point at e.g. preview / production instances of the content API
    service.

--- config/initializers/content_api.rb
+++ config/initializers/content_api.rb
@@ -1,3 +1,16 @@
 require 'gds_api/content_api'
 
-Whitehall.mainstream_content_api = GdsApi::ContentApi.new(Plek.current.environment)
+class GdsApi::ContentApi::Fake
+  def tag(tag)
+    {}
+  end
+end
+
+if endpoint_url = ENV["CONTENT_API_ENDPOINT_URL"]
+  credentials = { user: ENV["CONTENT_API_USERNAME"], password: ENV["CONTENT_API_PASSWORD"] }
+  Whitehall.mainstream_content_api = GdsApi::ContentApi.new(nil, endpoint_url: endpoint_url, basic_auth: credentials)
+elsif Rails.env.production?
+  Whitehall.mainstream_content_api = GdsApi::ContentApi.new(Plek.current.environment)
+else
+  Whitehall.mainstream_content_api = GdsApi::ContentApi::Fake.new
+end
