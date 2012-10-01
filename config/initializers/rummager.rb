6fe6466318486a1979362f5d2cfd148f21ad05c9
Tom Ward <tom@popdog.net>
2012-10-01 10:45:51 +0100

    Use main rummager instance for all search results.
    Now that rummager supports multiple backends and indexes, we don't
    need to have a separate rummager just for whitehall search results.

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -1,16 +1,10 @@
 require 'gds_api/rummager'
 
-Rummageable.rummager_service_name = "whitehall-search"
 Rummageable.rummager_host = ENV["RUMMAGER_HOST"] if ENV["RUMMAGER_HOST"]
 
 Whitehall.government_search_client = GdsApi::Rummager.new(Rummageable.rummager_host + Whitehall.government_search_index_name)
 Whitehall.detailed_guidance_search_client = GdsApi::Rummager.new(Rummageable.rummager_host + Whitehall.detailed_guidance_search_index_name)
-
-# We're still using two Rummager instances until Mainstream is upgraded, at which point we can
-# connect to a single instance that provides access to multiple indexes.
-mainstream_rummager_service_name = 'search'
-mainstream_rummager_host = Plek.current.find(mainstream_rummager_service_name)
-Whitehall.mainstream_search_client = GdsApi::Rummager.new(mainstream_rummager_host)
+Whitehall.mainstream_search_client = GdsApi::Rummager.new(Rummageable.rummager_host + Whitehall.mainstream_search_index_name)
 
 unless Rails.env.production? || ENV["RUMMAGER_HOST"]
   Rummageable.implementation = Rummageable::Fake.new
