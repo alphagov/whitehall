8dbad13321020771ddc72d6f058ad37a56f7c16e
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-02 09:39:06 +0100

    Remove detailed guidance search
    This is now sending traffic to the unified search vertical, so it
    doesn't need to work.
    
    For the moment we'll keep government search, as there's no unified
    equivalent at this point.

--- app/controllers/detailed_guides_controller.rb
+++ app/controllers/detailed_guides_controller.rb
@@ -1,5 +1,6 @@
 class DetailedGuidesController < DocumentsController
   layout "detailed-guidance"
+  skip_before_filter :set_search_path
   before_filter :set_search_index
   before_filter :set_artefact, only: [:show]
 
@@ -16,20 +17,6 @@ class DetailedGuidesController < DocumentsController
     @categories = @document.mainstream_categories
     @topics = @document.topics
     render action: "show"
-  end
-
-  def search
-    @search_term = params[:q]
-    mainstream_results = Whitehall.mainstream_search_client.search(@search_term)
-    @mainstream_results = mainstream_results.take(5)
-    @more_mainstream_results = mainstream_results.length > 5
-    @results = Whitehall.detailed_guidance_search_client.search(@search_term).take(50 - @mainstream_results.length)
-    @total_results = @results.length + @mainstream_results.length
-    respond_with @results
-  end
-
-  def autocomplete
-    render text: Whitehall.detailed_guidance_search_client.autocomplete(params[:q])
   end
 
 private
