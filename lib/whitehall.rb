alphagov/whitehall

commit 0b21b46ecd160d7bb714d35b63067352d2f87859

Use Quarantined File storage in production
Also removed s3 config since nothing uses it now.

lib/whitehall.rb
@@ -70,10 +70,8 @@ module Whitehall
     end
 
     def asset_storage_mechanism
-      if platform == 'preview'
+      if %w{preview production}.include?(platform)
         :quarantined_file
-      elsif !Rails.env.test? && aws_access_key_id && aws_secret_access_key
-        :s3
       else
         :file
       end

test/unit/whitehall_test.rb
@@ -1,11 +1,14 @@
 require 'test_helper'
 
 class WhitehallTest < ActiveSupport::TestCase
-  test 'use S3 storage if AWS access details set' do
-    Whitehall.stubs(:aws_access_key_id).returns('an-id')
-    Whitehall.stubs(:aws_secret_access_key).returns('private-key')
-    Rails.stubs(:env).returns(ActiveSupport::StringInquirer.new('anything-other-than-test'))
-    assert_equal :s3, Whitehall.asset_storage_mechanism
+  test 'use quarantined file store in preview' do
+    Whitehall.stubs(:platform).returns('preview')
+    assert_equal :quarantined_file, Whitehall.asset_storage_mechanism
+  end
+
+  test 'use quarantined file store in production' do
+    Whitehall.stubs(:platform).returns('production')
+    assert_equal :quarantined_file, Whitehall.asset_storage_mechanism
   end
 
   test 'never use S3 storage in test environment' do

config/initializers/carrierwave.rb
@@ -1,15 +1,5 @@
 CarrierWave.configure do |config|
   case Whitehall.asset_storage_mechanism
-  when :s3
-    config.storage :fog
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Whitehall.aws_access_key_id,
-      aws_secret_access_key: Whitehall.aws_secret_access_key,
-      region: 'eu-west-1'
-    }
-    config.fog_directory  = "whitehall-frontend-#{Whitehall.platform}"
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   when :file
     config.storage :file
     config.enable_processing = false if Rails.env.test?

commit a1711c0c2eb36c3ebf947f1b5eff38cb26ee2f8c

Don't use Quarantined file storage in our tests
Commit 0b21b46ecd160d7bb714d35b63067352d2f87859 broke the build on CI,
as that machine has the FACTER_govuk_platform environment variable set
to 'production'.  We're now explicitly setting the storage mechanism to
:file if we're running in the test environment, irrespective of the
value of FACTER_govuk_platform.

lib/whitehall.rb
@@ -70,7 +70,9 @@ module Whitehall
     end
 
     def asset_storage_mechanism
-      if %w{preview production}.include?(platform)
+      if Rails.env.test?
+        :file
+      elsif %w{preview production}.include?(platform)
         :quarantined_file
       else
         :file

test/unit/whitehall_test.rb
@@ -3,17 +3,18 @@ require 'test_helper'
 class WhitehallTest < ActiveSupport::TestCase
   test 'use quarantined file store in preview' do
     Whitehall.stubs(:platform).returns('preview')
+    Rails.stubs(:env).returns(ActiveSupport::StringInquirer.new('not-test-environment'))
     assert_equal :quarantined_file, Whitehall.asset_storage_mechanism
   end
 
   test 'use quarantined file store in production' do
     Whitehall.stubs(:platform).returns('production')
+    Rails.stubs(:env).returns(ActiveSupport::StringInquirer.new('not-test-environment'))
     assert_equal :quarantined_file, Whitehall.asset_storage_mechanism
   end
 
-  test 'never use S3 storage in test environment' do
-    Whitehall.stubs(:aws_access_key_id).returns('an-id')
-    Whitehall.stubs(:aws_secret_access_key).returns('private-key')
+  test 'always uses file storage in test environment' do
+    Whitehall.stubs(:platform).returns('production')
     Rails.stubs(:env).returns(ActiveSupport::StringInquirer.new('test'))
     assert_equal :file, Whitehall.asset_storage_mechanism
   end