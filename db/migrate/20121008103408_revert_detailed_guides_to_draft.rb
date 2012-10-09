5708a571377b5221447fb062e0c8fd196883438a
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-09 17:12:48 +0100

    Convert these existing migrations into data migrations

--- db/migrate/20121008103408_revert_detailed_guides_to_draft.rb
+++ db/migrate/20121008103408_revert_detailed_guides_to_draft.rb
@@ -1,41 +1,8 @@
 class RevertDetailedGuidesToDraft < ActiveRecord::Migration
   def up
-    user = User.find_by_name("Automatic Data Importer")
-    return unless user
-    PaperTrail.enabled = false
-    data.each do |row|
-      guide = Document.at_slug(DetailedGuide, row[0])
-
-      next unless guide
-
-      latest_edition = guide.latest_edition
-
-      next unless latest_edition
-
-      all_editions_including_deleted = Edition.unscoped.where(document_id: guide)
-
-      other_editions = all_editions_including_deleted - [latest_edition]
-
-      other_editions.each do |old_edition|
-        EditionAuthor.where(edition_id: old_edition).update_all(edition_id: latest_edition)
-        old_edition.destroy
-      end
-
-      FactCheckRequest.where(edition_id: other_editions).update_all(edition_id: latest_edition)
-      Version.where(item_id: other_editions).update_all(item_id: latest_edition)
-
-      latest_edition.editorial_remarks.create!(author: user, body: "Reset to draft")
-      latest_edition.update_attributes(state: "draft")
-
-    end
+    # Changed to be a data migration
   end
 
   def down
-  end
-
-  def data
-    CSV.read(
-      File.dirname(__FILE__) + '/20121008103408_revert_detailed_guides_to_draft.csv',
-      headers: false)
   end
 end
