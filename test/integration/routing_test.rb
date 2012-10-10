alphagov/whitehall

commit ab2afb13380d35635bb84f6d6885d519e6652c3a

Redirect / to /government/admin on admin servers

config/routes.rb
@@ -9,6 +9,7 @@ Whitehall::Application.routes.draw do
     super(options[:prefix] + path)
   end
 
+  root to: redirect("/admin"), constraints: AdminRequest
   root to: redirect("/")
 
   match '/browse/*parent_tag/:id', to: 'mainstream_categories#show'

commit 012882690e6bc79713caba6df9cec1c32ea9771b

Fix test failure
caused by ab2afb13380d35635bb84f6d6885d519e6652c3a

config/routes.rb
@@ -9,7 +9,9 @@ Whitehall::Application.routes.draw do
     super(options[:prefix] + path)
   end
 
-  root to: redirect("/admin"), constraints: AdminRequest
+  root to: redirect("/admin/"), constraints: lambda { |request|
+    ::Whitehall.admin_hosts.include?(request.host)
+  }
   root to: redirect("/")
 
   match '/browse/*parent_tag/:id', to: 'mainstream_categories#show'

test/integration/routing_test.rb
@@ -16,9 +16,16 @@ class RoutingTest < ActionDispatch::IntegrationTest
     assert_select "script[src=?]", "#{Whitehall.router_prefix}/assets/application.js"
   end
 
-  test "visiting / redirects to #{Whitehall.router_prefix}" do
+  test "visiting / on frontend redirects to #{Whitehall.router_prefix}" do
+    host! 'whitehall-frontend.production.alphagov.co.uk'
     get "/"
     assert_redirected_to "#{Whitehall.router_prefix}/"
+  end
+
+  test "visiting / on an admin host redirects to #{Whitehall.router_prefix}/admin" do
+    host! 'whitehall-admin.production.alphagov.co.uk'
+    get "/"
+    assert_redirected_to "#{Whitehall.router_prefix}/admin/"
   end
 
   test "visiting unknown route should respond with 404 not found" do