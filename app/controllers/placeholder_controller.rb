a08aa0d00963faca8704a8be8cceaf2e4c674c01
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-10-10 13:53:27 +0100

    Use asset_host to serve virus placeholder
    The placeholder was hard-coded at a relative URL with the redirect,
    which would be on HTTP space within the network as we're reverse
    proxying to http:// internally. This meant users were getting mixed
    content warnings on pages with virus-embargoed uploads.
    
    https://www.pivotaltracker.com/story/show/37022071

--- app/controllers/placeholder_controller.rb
+++ app/controllers/placeholder_controller.rb
@@ -1,4 +1,10 @@
 class PlaceholderController < ApplicationController
+  include ActionView::Helpers::AssetTagHelper
+
   def show
+  end
+
+  def virus_checking_placeholder_image
+     redirect_to view_context.path_to_image('thumbnail-virus-checking.png')
   end
 end
