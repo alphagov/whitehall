170be3e8bbd9b39f066cc310cfd0261a2a16b22c
Chris Roos & James Mead <pairing+chris.roos+james.mead@gofreerange.com>
2012-10-16 13:15:19 +0100

    Tidy up user tests
    * Avoid duplicating the 'Editor' string by using the constant defined in
      User::Permissions.
    
    * Remove setting of email attribute which looks like it was accidentally
      copied from an earlier test body.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -17,23 +17,23 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'should be a departmental editor if has whitehall Editor role' do
-    user = build(:user, email: nil, permissions: {'Whitehall' => ['Editor']})
+    user = build(:user, permissions: {'Whitehall' => [User::Permissions::DEPARTMENTAL_EDITOR]})
     assert user.departmental_editor?
   end
 
   test 'should not be a departmental editor if does not have has whitehall Editor role' do
-    user = build(:user, email: nil, permissions: {'Whitehall' => []})
+    user = build(:user, permissions: {'Whitehall' => []})
     refute user.departmental_editor?
   end
 
   test 'should not normally allow mass assignment of permissions' do
-    user = build(:user, email: nil, permissions: {'Whitehall' => []})
+    user = build(:user, permissions: {'Whitehall' => []})
     user.assign_attributes(permissions: {'Whitehall' => ['Superuser']})
     assert_equal [], user.permissions['Whitehall']
   end
 
   test 'should allow gds-sso to mass assign permissions' do
-    user = build(:user, email: nil, permissions: {'Whitehall' => []})
+    user = build(:user, permissions: {'Whitehall' => []})
     user.assign_attributes({permissions: {'Whitehall' => ['Superuser']}}, as: :oauth)
     assert_equal ['Superuser'], user.permissions['Whitehall']
   end
