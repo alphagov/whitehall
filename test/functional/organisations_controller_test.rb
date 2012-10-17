60ad1ad2b333e29786c8787bb33ffc96135bdeec
Edd Sowden <e@e26.co.uk>
2012-10-17 06:44:24 +0000

    Order announcments by newest first
    Also lower case 'a' in announcements.

--- test/functional/organisations_controller_test.rb
+++ test/functional/organisations_controller_test.rb
@@ -273,18 +273,18 @@ class OrganisationsControllerTest < ActionController::TestCase
 
   test "should display 3 announcements with a link to announcements filter if there are many announcements" do
     organisation = create(:organisation)
-    news_1 = create(:published_news_article, organisations: [organisation])
-    news_2 = create(:published_news_article, organisations: [organisation])
-    news_3 = create(:published_news_article, organisations: [organisation])
-    news_4 = create(:published_news_article, organisations: [organisation])
+    announcement_1 = create(:published_news_article, organisations: [organisation], published_at: 2.days.ago)
+    announcement_2 = create(:published_speech, organisations: [organisation], published_at: 3.days.ago)
+    announcement_3 = create(:published_news_article, organisations: [organisation], published_at: 4.days.ago)
+    announcement_4 = create(:published_news_article, organisations: [organisation], published_at: 1.days.ago)
 
     get :show, id: organisation
 
     assert_select '#announcements' do
-      assert_select_object(news_1)
-      assert_select_object(news_2)
-      assert_select_object(news_3)
-      refute_select_object(news_4)
+      assert_select_object(announcement_1)
+      assert_select_object(announcement_2)
+      assert_select_object(announcement_4)
+      refute_select_object(announcement_3)
       assert_select "a[href='#{announcements_filter_path(organisation)}']"
     end
   end
