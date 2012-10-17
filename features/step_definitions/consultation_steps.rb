f1e47fc022d3c355c9b0292e38ccd12369d3eaf4
Chris Parsons <chris@thinkcodelearn.com>
2012-10-17 14:10:41 +0100

    Initial feature to cover consultation response

--- features/step_definitions/consultation_steps.rb
+++ features/step_definitions/consultation_steps.rb
@@ -6,12 +6,7 @@ When /^I draft a new consultation "([^"]*)"$/ do |title|
   fill_in "Email", with: "participate@gov.uk"
   select_date "Opening Date", with: 1.day.ago.to_s
   select_date "Closing Date", with: 6.days.from_now.to_s
-  @attachment_title = "Attachment Title"
-  @attachment_filename = "attachment.pdf"
-  within ".attachments" do
-    fill_in "Title", with: @attachment_title
-    attach_file "File", Rails.root.join("features/fixtures", @attachment_filename)
-  end
+  add_attachment "Attachment Title", "attachment.pdf", ".attachments"
   check "Wales"
   fill_in "Alternative url", with: "http://www.visitwales.co.uk/"
   check "Scotland"
@@ -22,4 +17,25 @@ end
 Then /^I can see links to the consultations "([^"]*)" and "([^"]*)"$/ do |title_1, title_2|
   assert has_css?(".consultation a", text: title_1)
   assert has_css?(".consultation a", text: title_2)
+end
+
+When /^I add a response to the consultation$/ do
+  visit edit_admin_consultation_path(Consultation.last)
+  click_button "Create new edition"
+  add_attachment("Response Title", "attachment.pdf", "#consultation_response_attachment_fields")
+  select_date "Opening Date", with: 2.days.ago.strftime("%Y-%m-%d")
+  select_date "Closing Date", with: 1.day.ago.strftime("%Y-%m-%d")
+
+  fill_in_change_note_if_required
+  click_button "Save"
+end
+
+When /^I publish the amended consultation$/ do
+  click_button "Force Publish"
+end
+
+Then /^the consultation response should be viewable$/ do
+  click_link(Consultation.last.title)
+  click_link(Consultation.last.title)
+  page.should have_css(".consultation-responded .attachment", :count => 1)
 end
