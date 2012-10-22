8b587cb06f550e11ced036aef7f4a78c80615508
James Adam <james@lazyatom.com>
2012-10-22 13:06:45 +0100

    Split the admin and public govspeak helpers for yet more clarity.
    Now it should be clearer which methods are appropriate to call in
    which situation.

--- app/helpers/govspeak_helper.rb
+++ app/helpers/govspeak_helper.rb
@@ -1,18 +1,6 @@
 require 'addressable/uri'
 
 module GovspeakHelper
-
-  def govspeak_to_admin_html(govspeak, images=[], attachments=[], alternative_format_contact_email = nil)
-    partially_processed_govspeak = govspeak_with_attachments_and_alt_format_information(govspeak, attachments, alternative_format_contact_email)
-    wrapped_in_govspeak_div(bare_govspeak_to_admin_html(partially_processed_govspeak, images))
-  end
-
-  def govspeak_edition_to_admin_html(edition)
-    images = edition.respond_to?(:images) ? edition.images : []
-    partially_processed_govspeak = edition_body_with_attachments_and_alt_format_information(edition)
-    wrapped_in_govspeak_div(bare_govspeak_to_admin_html(partially_processed_govspeak, images))
-  end
-
   def govspeak_to_html(govspeak, images=[])
     wrapped_in_govspeak_div(bare_govspeak_to_html(govspeak, images))
   end
@@ -63,24 +51,6 @@ module GovspeakHelper
 
   def wrapped_in_govspeak_div(html_string)
     content_tag(:div, html_string.html_safe, class: 'govspeak')
-  end
-
-  def bare_govspeak_to_admin_html(govspeak, images = [], attachments = [])
-    govspeak_to_html_with_replaced_admin_links(govspeak, images) do |replacement_html, edition|
-      latest_edition = edition && edition.document.latest_edition
-      if latest_edition.nil?
-        replacement_html = content_tag(:del, replacement_html)
-        explanation = state = "deleted"
-      else
-        state = latest_edition.state
-        explanation = link_to(state, admin_edition_path(latest_edition))
-      end
-
-      content_tag :span, class: "#{state}_link" do
-        annotation = content_tag(:sup, safe_join(['(', explanation, ')']), class: 'explanation')
-        safe_join [replacement_html, annotation], ' '
-      end
-    end
   end
 
   def govspeak_to_html_with_replaced_admin_links(govspeak, images = [], &block)
--- test/unit/helpers/govspeak_helper_test.rb
+++ test/unit/helpers/govspeak_helper_test.rb
@@ -11,29 +11,9 @@ class GovspeakHelperTest < ActionView::TestCase
   end
   attr_reader :request
 
-  test "should wrap admin output with a govspeak class" do
-    html = govspeak_to_admin_html("govspeak-text")
-    assert_select_within_html html, ".govspeak", text: "govspeak-text"
-  end
-
-  test "should mark the admin govspeak output as html safe" do
-    html = govspeak_to_admin_html("govspeak-text")
-    assert html.html_safe?
-  end
-
-  test "should not alter urls to other sites in the admin preview" do
-    html = govspeak_to_admin_html("no [change](http://external.example.com/page.html)")
-    assert_select_within_html html, "a[href=?]", "http://external.example.com/page.html", text: "change"
-  end
-
   test "should not alter urls to other sites" do
     html = govspeak_to_html("no [change](http://external.example.com/page.html)")
     assert_select_within_html html, "a[href=?]", "http://external.example.com/page.html", text: "change"
-  end
-
-  test "should not alter mailto urls in the admin preview" do
-    html = govspeak_to_admin_html("no [change](mailto:dave@example.com)")
-    assert_select_within_html html, "a[href=?]", "mailto:dave@example.com", text: "change"
   end
 
   test "should not alter mailto urls" do
@@ -46,93 +26,10 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, "a[href=?]", "not a valid url", text: "change"
   end
 
-  test "should not alter partial urls in the admin preview" do
-    html = govspeak_to_admin_html("no [change](http://)")
-    assert_select_within_html html, "a[href=?]", "http://", text: "change"
-  end
-
   test "should not alter partial urls" do
     html = govspeak_to_html("no [change](http://)")
     assert_select_within_html html, "a[href=?]", "http://", text: "change"
   end
-
-  test "should rewrite link to draft edition in admin preview" do
-    publication = create(:draft_publication)
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url(publication)})")
-    assert_select_within_html html, "a[href=?]", admin_publication_path(publication), text: "draft"
-  end
-
-  test "should not alter unicode when replacing links" do
-    publication = create(:published_publication)
-    html = govspeak_to_admin_html("the [☃](#{admin_publication_url(publication)})")
-    assert_select_within_html html, "a[href=?]", public_document_url(publication), text: "☃"
-  end
-
-  test "should rewrite link to deleted edition in admin preview" do
-    publication = create(:deleted_publication)
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url(publication)})")
-    assert_select_within_html html, "del", text: "that"
-  end
-
-  test "should rewrite link to missing edition in admin preview" do
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url('missing-id')})")
-    assert_select_within_html html, "del", text: "that"
-  end
-
-  test "should rewrite link to destroyed supporting page in admin preview" do
-    html = govspeak_to_admin_html("this and [that](#{admin_edition_supporting_page_url("doesnt-exist", "missing-id")})")
-    assert_select_within_html html, "del", text: "that"
-  end
-
-  test "should rewrite link to published edition in admin preview" do
-    publication = create(:published_publication)
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url(publication)})")
-    assert_select_within_html html, "a[href=?]", public_document_url(publication), text: "that"
-  end
-
-  test "should rewrite link to published edition with a newer draft in admin preview" do
-    publication = create(:published_publication)
-    new_draft = publication.create_draft(create(:policy_writer))
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url(publication)})")
-    assert_select_within_html html, "a[href=?]", admin_publication_path(new_draft), text: "draft"
-  end
-
-  test "should rewrite link to archived edition with a newer published edition in admin preview" do
-    publication = create(:published_publication)
-    writer = create(:policy_writer)
-    editor = create(:departmental_editor)
-    new_edition = publication.create_draft(writer)
-    new_edition.change_note = "change-note"
-    new_edition.save_as(writer)
-    new_edition.submit!
-    new_edition.publish_as(editor)
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url(publication)})")
-    assert_select_within_html html, "a[href=?]", admin_publication_path(new_edition), text: "published"
-  end
-
-  test "should rewrite link to deleted edition with an older published edition in admin preview" do
-    publication = create(:published_publication)
-    new_draft = publication.create_draft(create(:policy_writer))
-    new_draft.delete!
-    deleted_edition = new_draft
-    html = govspeak_to_admin_html("this and [that](#{admin_publication_url(deleted_edition)})")
-    assert_select_within_html html, "a[href=?]", admin_publication_path(publication), text: "published"
-  end
-
-  test "should allow attached images to be embedded in admin html" do
-    images = [OpenStruct.new(alt_text: "My Alt", url: "/image.jpg")]
-    html = govspeak_to_admin_html("!!1", images)
-    assert_select_within_html html, ".govspeak figure.image.embedded img[src=/image.jpg]"
-  end
-
-  test "prefixes embedded image urls with asset host if present" do
-    Whitehall.stubs(:asset_host).returns("https://some.cdn.com")
-    images = [OpenStruct.new(alt_text: "My Alt", url: "/image.jpg")]
-    html = govspeak_to_admin_html("!!1", images)
-    assert_select_within_html html, ".govspeak figure.image.embedded img[src=https://some.cdn.com/image.jpg]"
-  end
-
-  # public govspeak helper tests
 
   test "should wrap output with a govspeak class" do
     html = govspeak_to_html("govspeak-text")
