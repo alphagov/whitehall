18e6185b4838b4fc5b9ad0284ffa8d0c52853d54
James Adam <james@lazyatom.com>
2012-10-23 12:18:07 +0100

    Re-instate dependent destroy behaviour along with test.

--- app/models/edition_attachment.rb
+++ app/models/edition_attachment.rb
@@ -1,5 +1,5 @@
 class EditionAttachment < ActiveRecord::Base
-  belongs_to :attachment
+  belongs_to :attachment, dependent: :destroy
   belongs_to :edition
 
   accepts_nested_attributes_for :attachment
