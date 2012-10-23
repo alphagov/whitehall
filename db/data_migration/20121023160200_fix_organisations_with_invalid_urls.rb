9a88c581c204104625ebbeebdc39ee5c98810562
James Mead <james@floehopper.org>
2012-10-23 19:06:51 +0100

    A number of organisations have invalid URLs and cannot be saved.
    It looks like the URL validation was added in
    bbfe9d299ef36c3e19760bcc524ddec84662d43f some months after it was
    possible to set the URL through the user interface in
    761bd9e377cfea042e659fa4b04f2c3bf4fad3fe. A number of invalid URLs seem
    to have crept in in the meantime.
    
    This data migration makes them all valid, mostly by setting the URL to
    nil which is allowed, or in one case setting the intended URL which was
    missing the protocol prefix "http://" in the production data.

---
+++ db/data_migration/20121023160200_fix_organisations_with_invalid_urls.rb
@@ -0,0 +1,12 @@
+royal_mint = Organisation.find_by_name!("Royal Mint")
+$stderr.print "Updating 'Royal Mint' url to 'http://www.royalmint.com/'..."
+royal_mint.update_attributes!(url: "http://www.royalmint.com/")
+$stderr.puts "[done]"
+
+Organisation.all.each do |organisation|
+  unless organisation.valid? && organisation.errors[:url].empty?
+    $stderr.print "Updating '#{organisation.name}' url to nil..."
+    organisation.update_attributes!(url: nil)
+    $stderr.puts "[done]"
+  end
+end
