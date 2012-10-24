019dda051fd0b95f77550bd165c888caedddf9e9
James Mead <james@floehopper.org>
2012-10-24 09:50:15 +0100

    Safer not to assume that Royal Mint organisation will always be present.

--- db/data_migration/20121023160200_fix_organisations_with_invalid_urls.rb
+++ db/data_migration/20121023160200_fix_organisations_with_invalid_urls.rb
@@ -1,7 +1,8 @@
-royal_mint = Organisation.find_by_name!("Royal Mint")
-$stderr.print "Updating 'Royal Mint' url to 'http://www.royalmint.com/'..."
-royal_mint.update_attributes!(url: "http://www.royalmint.com/")
-$stderr.puts "[done]"
+if royal_mint = Organisation.find_by_name("Royal Mint")
+  $stderr.print "Updating 'Royal Mint' url to 'http://www.royalmint.com/'..."
+  royal_mint.update_attributes!(url: "http://www.royalmint.com/")
+  $stderr.puts "[done]"
+end
 
 Organisation.all.each do |organisation|
   unless organisation.valid? && organisation.errors[:url].empty?
