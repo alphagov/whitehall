48c0effc53d2055c3b445d2aa1847bc3023a406f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-25 18:09:53 +0100

    Allow selection of editions accessible to a given user

--- app/models/edition/limited_access.rb
+++ app/models/edition/limited_access.rb
@@ -5,6 +5,21 @@ module Edition::LimitedAccess
     before_save ->(record) { record.access_limited = nil unless record.can_limit_access? }
   end
 
+  module ClassMethods
+    def accessible_to(user)
+      clause = 'access_limited is null OR access_limited=false'
+      if user && user.organisation
+        where("(#{clause} OR access_limited=false OR exists (
+               select * from edition_organisations eo_accessibility_check
+               where
+                 eo_accessibility_check.edition_id=editions.id
+               and eo_accessibility_check.organisation_id=?))", user.organisation.id)
+      else
+        where("(#{clause})")
+      end
+    end
+  end
+
   module InstanceMethods
     def access_limited?
       self.can_limit_access? && read_attribute(:access_limited)
