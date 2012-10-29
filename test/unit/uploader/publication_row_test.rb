9dc0d79a8a964851c22dac588eebe14782a1f1cf
James Adam <james@lazyatom.com>
2012-10-29 10:42:21 +0000

    Remove duplicate import 'finder' classes and move 'finder' tests.
    It looks like these were moved into `PublicationRow`, but the old
    classes were left in place. I've removed them, and moved the tests
    for these classes to `publication_row_test.rb`.
    
    I've also stubbed the attachment test since this will now make a
    real HTTP request.

--- test/unit/uploader/publication_row_test.rb
+++ test/unit/uploader/publication_row_test.rb
@@ -68,6 +68,8 @@ class Whitehall::Uploader::PublicationRowTest < ActiveSupport::TestCase
   end
 
   test "finds up to 42 attachments in columns attachment 1 title, attachement 1 url..." do
+    stub_request(:get, "http://example.com/attachment.pdf").to_return(body: "attachment-1-data", status: 200)
+
     row = Whitehall::Uploader::PublicationRow.new({
       "attachment 1 title" => "first title",
       "attachment 1 url" => "http://example.com/attachment.pdf"
@@ -76,5 +78,173 @@ class Whitehall::Uploader::PublicationRowTest < ActiveSupport::TestCase
     attachment = Attachment.new(title: "first title")
     assert_equal [attachment.attributes], row.attachments.collect(&:attributes)
     assert_equal "http://example.com/attachment.pdf", row.attachments.first.attachment_source.url
+  end
+end
+
+class Whitehall::Uploader::PublicationRow::PublicationDateParserTest < ActiveSupport::TestCase
+  def setup
+    @log_buffer = StringIO.new
+    @log = Logger.new(@log_buffer)
+    @line_number = 1
+  end
+
+  test "returns the date" do
+    assert_equal Date.parse('2012-11-01'), Whitehall::Uploader::PublicationRow::PublicationDateParser.parse('11/01/2012', @log, @line_number)
+  end
+
+  test "logs a warning if the date could'nt be parsed" do
+    Whitehall::Uploader::PublicationRow::PublicationDateParser.parse('11/012012', @log, @line_number)
+    assert_match /Unable to parse the date '11\/012012'/, @log_buffer.string
+  end
+end
+
+class Whitehall::Uploader::PublicationRow::PublicationTypeFinderTest < ActiveSupport::TestCase
+  def setup
+    @log_buffer = StringIO.new
+    @log = Logger.new(@log_buffer)
+    @line_number = 1
+  end
+
+  test "returns the publication type found by the slug" do
+    assert_equal PublicationType::CircularLetterOrBulletin, Whitehall::Uploader::PublicationRow::PublicationTypeFinder.find('circulars-letters-and-bulletins', @log, @line_number)
+  end
+
+  test "returns nil if the publication type can't be determined" do
+    assert_nil Whitehall::Uploader::PublicationRow::PublicationTypeFinder.find('made-up-publication-type', @log, @line_number)
+  end
+
+  test "logs a warning if the publication type can't be determined" do
+    Whitehall::Uploader::PublicationRow::PublicationTypeFinder.find('made-up-publication-type-slug', @log, @line_number)
+    assert_match /Unable to find Publication type with slug 'made-up-publication-type-slug'/, @log_buffer.string
+  end
+end
+
+class Whitehall::Uploader::PublicationRow::PoliciesFinderTest < ActiveSupport::TestCase
+  def setup
+    @log_buffer = StringIO.new
+    @log = Logger.new(@log_buffer)
+    @line_number = 1
+  end
+
+  test "returns the published edition of all documents found by the supplied slugs" do
+    policy_1 = create(:published_policy, title: "Policy 1")
+    policy_2 = create(:published_policy, title: "Policy 1")
+    assert_equal [policy_1, policy_2], Whitehall::Uploader::PublicationRow::PoliciesFinder.find(policy_1.slug, policy_2.slug, @log, @line_number)
+  end
+
+  test "ignores blank slugs" do
+    assert_equal [], Whitehall::Uploader::PublicationRow::PoliciesFinder.find('', '', @log, @line_number)
+  end
+
+  test "returns an empty array if a document can't be found for the given slug" do
+    assert_equal [], Whitehall::Uploader::PublicationRow::PoliciesFinder.find('made-up-policy-slug', @log, @line_number)
+  end
+
+  test "logs a warning if a document can't be found for the given slug" do
+    Whitehall::Uploader::PublicationRow::PoliciesFinder.find('made-up-policy-slug', @log, @line_number)
+    assert_match /Unable to find Document with slug 'made-up-policy-slug'/, @log_buffer.string
+  end
+
+  test "returns an empty array if the document for the given slug doesn't have a published edition" do
+    draft_policy = create(:draft_policy)
+    assert_equal [], Whitehall::Uploader::PublicationRow::PoliciesFinder.find(draft_policy.slug, @log, @line_number)
+  end
+
+  test "logs a warning if the document for the given slug doesn't have a published edition" do
+    draft_policy = create(:draft_policy)
+    Whitehall::Uploader::PublicationRow::PoliciesFinder.find(draft_policy.slug, @log, @line_number)
+    assert_match /Unable to find a published edition for the Document with slug '#{draft_policy.slug}'/, @log_buffer.string
+  end
+end
+
+class Whitehall::Uploader::PublicationRow::OrganisationFinderTest < ActiveSupport::TestCase
+  def setup
+    @log_buffer = StringIO.new
+    @log = Logger.new(@log_buffer)
+    @line_number = 1
+  end
+
+  test "returns a single element array containing the organisation identified by name" do
+    organisation = create(:organisation)
+    assert_equal [organisation], Whitehall::Uploader::PublicationRow::OrganisationFinder.find(organisation.name, @log, @line_number)
+  end
+
+  test "returns an empty array if the name is blank" do
+    assert_equal [], Whitehall::Uploader::PublicationRow::OrganisationFinder.find('', @log, @line_number)
+  end
+
+  test "doesn't log a warning if name is blank" do
+    Whitehall::Uploader::PublicationRow::OrganisationFinder.find('', @log, @line_number)
+    assert_equal '', @log_buffer.string
+  end
+
+  test "returns an empty array if the organisation can't be found" do
+    assert_equal [], Whitehall::Uploader::PublicationRow::OrganisationFinder.find('made-up-organisation-name', @log, @line_number)
+  end
+
+  test "logs a warning if the organisation can't be found" do
+    Whitehall::Uploader::PublicationRow::OrganisationFinder.find('made-up-organisation-name', @log, @line_number)
+    assert_match /Unable to find Organisation named 'made-up-organisation-name'/, @log_buffer.string
+  end
+end
+
+class Whitehall::Uploader::PublicationRow::DocumentSeriesFinderTest < ActiveSupport::TestCase
+  def setup
+    @log_buffer = StringIO.new
+    @log = Logger.new(@log_buffer)
+    @line_number = 1
+  end
+
+  test "returns the document series identified by slug" do
+    document_series = create(:document_series)
+    assert_equal document_series, Whitehall::Uploader::PublicationRow::DocumentSeriesFinder.find(document_series.slug, @log, @line_number)
+  end
+
+  test "returns nil if the slug is blank" do
+    assert_equal nil, Whitehall::Uploader::PublicationRow::DocumentSeriesFinder.find('', @log, @line_number)
+  end
+
+  test "does not add an error if the slug is blank" do
+    Whitehall::Uploader::PublicationRow::DocumentSeriesFinder.find('', @log, @line_number)
+    assert_equal '', @log_buffer.string
+  end
+
+  test "returns nil if the document series can't be found" do
+    assert_equal nil, Whitehall::Uploader::PublicationRow::DocumentSeriesFinder.find('made-up-document-series-slug', @log, @line_number)
+  end
+
+  test "logs a warning if the document series can't be found" do
+    Whitehall::Uploader::PublicationRow::DocumentSeriesFinder.find('made-up-document-series-slug', @log, @line_number)
+    assert_match /Unable to find Document series with slug 'made-up-document-series-slug'/, @log_buffer.string
+  end
+end
+
+class Whitehall::Uploader::PublicationRow::MinisterialRoleFinderTest < ActiveSupport::TestCase
+  def setup
+    @log_buffer = StringIO.new
+    @log = Logger.new(@log_buffer)
+    @line_number = 1
+  end
+
+  test "returns the ministerial roles occupied by the ministers identified by slug at the date specified" do
+    minister = create(:person)
+    role = create(:ministerial_role)
+    create(:role_appointment, role: role, person: minister, started_at: 6.months.ago)
+    assert_equal [role], Whitehall::Uploader::PublicationRow::MinisterialRoleFinder.find(1.month.ago, minister.slug, @log, @line_number)
+  end
+
+  test "ignores blank slugs" do
+    assert_equal [], Whitehall::Uploader::PublicationRow::MinisterialRoleFinder.find(1.day.ago, '', @log, @line_number)
+  end
+
+  test "logs a warning if a person can't be found for the given slug" do
+    Whitehall::Uploader::PublicationRow::MinisterialRoleFinder.find(1.day.ago, 'made-up-person-slug', @log, @line_number)
+    assert_match /Unable to find Person with slug 'made-up-person-slug'/, @log_buffer.string
+  end
+
+  test "logs a warning if the person we find didn't have a role on the date specified" do
+    person = create(:person)
+    Whitehall::Uploader::PublicationRow::MinisterialRoleFinder.find(Date.today, person.slug, @log, @line_number)
+    assert_match /Unable to find a Role for '#{person.slug}' at '#{Date.today}'/, @log_buffer.string
   end
 end
