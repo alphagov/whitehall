9a389c7c8994bda571e108da3abeb1c1412bb068
David Heath <david.heath@digital.cabinet-office.gov.uk>
2012-10-29 11:02:35 +0000

    Limit access to editorial remarks

--- app/controllers/admin/editorial_remarks_controller.rb
+++ app/controllers/admin/editorial_remarks_controller.rb
@@ -1,16 +1,22 @@
 class Admin::EditorialRemarksController < Admin::BaseController
+  before_filter :find_edition
+  before_filter :limit_edition_access!
+
   def new
-    @edition = Edition.find(params[:edition_id])
     @editorial_remark = @edition.editorial_remarks.build
   end
 
   def create
-    @edition = Edition.find(params[:edition_id])
     @editorial_remark = @edition.editorial_remarks.build(params[:editorial_remark].merge(author: current_user))
     if @editorial_remark.save
       redirect_to admin_edition_path(@edition)
     else
       render "new"
     end
+  end
+
+private
+  def find_edition
+    @edition = Edition.find(params[:edition_id])
   end
 end
