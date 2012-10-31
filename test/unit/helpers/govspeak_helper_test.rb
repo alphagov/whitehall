61bfff5cc0663f97958e6e10b8f1399d1b85c964
James Mead <james@floehopper.org>
2012-10-31 14:04:26 +0000

    Separate govspeak helper link re-writing tests into their own file.

--- test/unit/helpers/govspeak_helper_test.rb
+++ test/unit/helpers/govspeak_helper_test.rb
@@ -2,9 +2,7 @@
 require 'test_helper'
 
 class GovspeakHelperTest < ActionView::TestCase
-  include Admin::EditionRoutesHelper
   include PublicDocumentRoutesHelper
-  include Rails.application.routes.url_helpers
 
   setup do
     @request  = ActionController::TestRequest.new
@@ -47,20 +45,6 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, "a", text: "‘funny’"
   end
 
-  test "should not link to draft editions with no published edition" do
-    publication = create(:draft_publication)
-    url = admin_publication_url(publication)
-    html = govspeak_to_html("this and [that](#{url})")
-    refute_select_within_html html, "a"
-  end
-
-  test "should not link to deleted editions with no published edition" do
-    publication = create(:deleted_publication)
-    url = admin_publication_url(publication)
-    html = govspeak_to_html("this and [that](#{url})")
-    refute_select_within_html html, "a"
-  end
-
   test "should not mark admin links as 'external'" do
     request.host = public_preview_host
     speech = create(:published_speech)
@@ -84,102 +68,6 @@ class GovspeakHelperTest < ActionView::TestCase
     govspeak = "this and [that](#{url}) yeah?"
     html = govspeak_to_html(govspeak)
     refute_select_within_html html, "a[rel='external']", text: "that"
-  end
-
-  test "should rewrite absolute link to an admin page for a published speech as link to its public page" do
-    speech = create(:published_speech)
-    assert_rewrites_link(from: admin_edition_url(speech), to: public_document_url(speech))
-  end
-
-  test "should rewrite relative link to an admin page for a published speech as link to its public page" do
-    speech = create(:published_speech)
-    assert_rewrites_link(from: admin_edition_path(speech), to: public_document_url(speech))
-  end
-
-  test "should rewrite absolute link to an admin page for a published supporting page as link to its public page" do
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    assert_rewrites_link(from: admin_supporting_page_url(supporting_page), to: public_supporting_page_url(policy, supporting_page))
-  end
-
-  test "should rewrite absolute link to an old-style admin page for a published supporting page as link to its public page" do
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    assert_rewrites_link(from: old_style_admin_supporting_page_url(supporting_page), to: public_supporting_page_url(policy, supporting_page))
-  end
-
-  test 'should rewrite admin link to an archived edition as a link to its published edition' do
-    archived_edition, published_edition = create_archived_policy_with_published_edition
-    assert_rewrites_link(from: admin_edition_url(archived_edition), to: public_document_url(published_edition))
-  end
-
-  test 'should rewrite admin link to a draft edition as a link to its published edition' do
-    published_edition, new_draft = create_draft_policy_with_published_edition
-    assert_rewrites_link(from: admin_edition_url(new_draft), to: public_document_url(published_edition))
-  end
-
-  test "should rewrite absolute link to an admin page for a speech as a link to its public page on the internal preview host" do
-    request.host = ActionController::Base.default_url_options[:host] = internal_preview_host
-    speech = create(:published_speech)
-    assert_rewrites_link(from: admin_speech_url(speech), to: public_document_url(speech))
-  end
-
-  test "should rewrite absolute link to an admin page for a speech as a link to its public page on the public preview host" do
-    request.host = public_preview_host
-    ActionController::Base.default_url_options[:host] = internal_preview_host
-    speech = create(:published_speech)
-    assert_rewrites_link(from: admin_speech_url(speech), to: public_document_url(speech))
-  end
-
-  test "should rewrite absolute link to an admin page for a supporting page as a link to its public page on the internal preview host" do
-    request.host = ActionController::Base.default_url_options[:host] = internal_preview_host
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    assert_rewrites_link(from: admin_supporting_page_url(supporting_page), to: public_supporting_page_url(policy, supporting_page, host: public_preview_host))
-  end
-
-  test "should rewrite absolute link to an admin page for a supporting page as a link to its public page on the public preview host" do
-    request.host = public_preview_host
-    ActionController::Base.default_url_options[:host] = internal_preview_host
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    assert_rewrites_link(from: admin_supporting_page_url(supporting_page), to: public_supporting_page_url(policy, supporting_page, host: public_preview_host))
-  end
-
-  test "should rewrite absolute link to an admin page for a speech as a link to its public page on the internal production host" do
-    request.host = ActionController::Base.default_url_options[:host] = internal_production_host
-    speech = create(:published_speech)
-    assert_rewrites_link(from: admin_speech_url(speech), to: public_document_url(speech))
-  end
-
-  test "should rewrite absolute link to an admin page for a speech as a link to its public page on the public production host" do
-    request.host = public_production_host
-    ActionController::Base.default_url_options[:host] = internal_production_host
-    speech = create(:published_speech)
-    assert_rewrites_link(from: admin_speech_url(speech), to: public_document_url(speech))
-  end
-
-  test "should rewrite absolute link to an admin page for a supporting page as a link to its public page on the internal production host" do
-    request.host = ActionController::Base.default_url_options[:host] = internal_production_host
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    assert_rewrites_link(from: admin_supporting_page_url(supporting_page), to: public_supporting_page_url(policy, supporting_page, host: public_production_host))
-  end
-
-  test "should rewrite absolute link to an admin page for a supporting page as a link to its public page on the public production host" do
-    request.host = public_production_host
-    ActionController::Base.default_url_options[:host] = internal_production_host
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    assert_rewrites_link(from: admin_supporting_page_url(supporting_page), to: public_supporting_page_url(policy, supporting_page, host: public_production_host))
-  end
-
-  test "should not link to supporting pages whose editions are not published" do
-    policy = create(:draft_policy)
-    supporting_page = create(:supporting_page, edition: policy)
-    url = admin_supporting_page_url(supporting_page)
-    html = govspeak_to_html("this and [that](#{url}) yeah?")
-    refute_select_within_html html, "a"
   end
 
   test "should allow attached images to be embedded in public html" do
@@ -310,35 +198,5 @@ class GovspeakHelperTest < ActionView::TestCase
 
   def public_production_host
     "www.gov.uk"
-  end
-
-  def assert_rewrites_link(options = {})
-    html = govspeak_to_html("this and [that](#{options[:from]}) yeah?")
-    assert_select_within_html html, "a[href=?]", options[:to], text: "that"
-  end
-
-  def old_style_admin_supporting_page_url(supporting_page)
-    admin_supporting_page_url(supporting_page).gsub(/editions/, "documents")
-  end
-
-  def create_archived_policy_with_published_edition
-    edition = create(:published_policy)
-    writer = create(:policy_writer)
-    editor = create(:departmental_editor)
-    new_draft = edition.create_draft(writer)
-    new_draft.change_note = 'change-note'
-    new_draft.save_as(writer)
-    new_draft.submit!
-    new_draft.publish_as(editor)
-    [edition, new_draft]
-  end
-
-  def create_draft_policy_with_published_edition
-    edition = create(:published_policy)
-    writer = create(:policy_writer)
-    new_draft = edition.create_draft(writer)
-    new_draft.change_note = 'change-note'
-    new_draft.save_as(writer)
-    [edition, new_draft]
   end
 end
