b3fff8ed866afceea5e3cd44d3d043bd903c1501
James Adam <james@lazyatom.com>
2012-10-31 13:12:05 +0000

    Create resized images for use in other parts of the site.
    Due to the helpful methods that carrierwave tries to create for
    accessing versions, we can't call the versions '960' (since '960'
    isn't a valid method name). Technically we could patch carrierwave
    to fix this, but it would be ultimately so awkward to actually
    use the created methods (image.file.send(:960)) that it didn't feel
    worth it.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,26 @@
 class ImageUploader < WhitehallUploader
+  include CarrierWave::MiniMagick
+
   def extension_white_list
     %w(jpg jpeg gif png)
+  end
+
+  version :s960 do
+    process resize_to_fill: [960, 640]
+  end
+  version :s712, from_version: :s960 do
+    process resize_to_fill: [712, 480]
+  end
+  version :s630, from_version: :s712 do
+    process resize_to_fill: [630, 420]
+  end
+  version :s465, from_version: :s630 do
+    process resize_to_fill: [465, 310]
+  end
+  version :s300, from_version: :s465 do
+    process resize_to_fill: [300, 195]
+  end
+  version :s216, from_version: :s300 do
+    process resize_to_fill: [216, 140]
   end
 end
