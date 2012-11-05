7a430f428828ea08c39ce5d8ddfec1028e6be4b1
David Heath + Tom Ward <tom+dh+tw@popdog.net>
2012-11-05 12:35:54 +0000

    Ignore nil content when converting links

--- test/unit/uploader/parsers/relative_to_absolute_links_test.rb
+++ test/unit/uploader/parsers/relative_to_absolute_links_test.rb
@@ -13,5 +13,9 @@ module Whitehall::Uploader::Parsers
       assert_equal "[link](http://example.com/foo/bar)", RelativeToAbsoluteLinks.parse("[link](/foo/bar)", "http://example.com")
       assert_equal "[link](http://example.com/foo/bar?baz=qux)", RelativeToAbsoluteLinks.parse("[link](/foo/bar?baz=qux)", "http://example.com")
     end
+
+    test 'ingores nil input' do
+      assert_nil RelativeToAbsoluteLinks.parse(nil, "http://example.com")
+    end
   end
 end
