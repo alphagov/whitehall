63755b253bdacac7f5b6706c1456bb08e3126bc3
David Heath + Tom Ward <tom+dh+tw@popdog.net>
2012-11-05 12:11:49 +0000

    Use same location for attachment cache on all environments.
    The folder /mnt/apps/whitehall-admin/attachment-cache isn't writable
    on staging or production, so the location needed to change.  Instead
    of configuring this within the app, we are instead symlinking a set
    location (<app-root>/attachment-cache) on deployment.

--- config/initializers/attachment_cache.rb
+++ config/initializers/attachment_cache.rb
@@ -1,5 +1,1 @@
-Whitehall::Uploader::AttachmentCache.default_root_directory = if Rails.env.production?
-  "/mnt/apps/whitehall-admin/attachment-cache"
-else
-  Rails.root.join("tmp", "cache", "attachment-cache")
-end
+Whitehall::Uploader::AttachmentCache.default_root_directory = Rails.root.join("attachment-cache")
