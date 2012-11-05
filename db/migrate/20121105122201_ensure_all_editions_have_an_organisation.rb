33847f40391fe377d01fcd5fbcc05ac93175ab04
Tom Ward <tom@popdog.net>
2012-11-05 13:12:05 +0000

    CI has no data, so no default organisation

--- db/migrate/20121105122201_ensure_all_editions_have_an_organisation.rb
+++ db/migrate/20121105122201_ensure_all_editions_have_an_organisation.rb
@@ -2,13 +2,14 @@ class EnsureAllEditionsHaveAnOrganisation < ActiveRecord::Migration
   class Organisation < ActiveRecord::Base; end
 
   def up
-    default_organisation = Organisation.order(:name).first
-    execute "
-    insert into edition_organisations (edition_id, organisation_id, updated_at)
-      select editions.id, #{default_organisation.id}, now() from editions where not exists (
-        select * from edition_organisations eocheck where eocheck.edition_id = editions.id
-        );
-    "
+    if default_organisation = Organisation.order(:name).first
+      execute "
+      insert into edition_organisations (edition_id, organisation_id, updated_at)
+        select editions.id, #{default_organisation.id}, now() from editions where not exists (
+          select * from edition_organisations eocheck where eocheck.edition_id = editions.id
+          );
+      "
+    end
   end
 
   def down
