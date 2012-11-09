8c07621c34e61767bc7e4c4f1d1eb05b17954e2a
James Mead <james@floehopper.org>
2012-11-09 13:01:46 +0000

    Use absolute paths to fixture image so test will run within an editor.

--- test/unit/edition_organisation_image_data_test.rb
+++ test/unit/edition_organisation_image_data_test.rb
@@ -7,12 +7,12 @@ class EditionOrganisationImageDataTest < ActiveSupport::TestCase
   end
 
   test 'should be invalid if image is not 960x640px' do
-    image_data = build(:edition_organisation_image_data, file: File.open('test/fixtures/horrible-image.64x96.jpg'))
+    image_data = build(:edition_organisation_image_data, file: File.open(Rails.root.join('test/fixtures/horrible-image.64x96.jpg')))
     refute image_data.valid?
   end
 
   test 'should be valid if legacy image is not 960x640px' do
-    image_data = build(:edition_organisation_image_data, file: File.open('test/fixtures/horrible-image.64x96.jpg'))
+    image_data = build(:edition_organisation_image_data, file: File.open(Rails.root.join('test/fixtures/horrible-image.64x96.jpg')))
     image_data.save(validate: false)
     assert image_data.reload.valid?
   end
