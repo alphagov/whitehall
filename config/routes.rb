e9a489889c795bb192a98405a0ea7569e86ddd7e
Chris Parsons <chris@thinkcodelearn.com>
2012-11-15 06:17:00 +0000

    Replace IG homepage with real one

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,7 @@ Whitehall::Application.routes.draw do
   end
 
   scope Whitehall.router_prefix, shallow_path: Whitehall.router_prefix do
-    root to: "home#sunset"
-    match '/home' => 'home#home'
+    root to: "home#home"
     match "/how-government-works" => "home#how-government-works", as: 'how_government_works'
     match '/feed' => 'home#feed', constraints: { format: :atom }, as: :atom_feed
     match '/tour' => redirect("/tour", prefix: "")
