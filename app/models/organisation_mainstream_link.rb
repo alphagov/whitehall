e0fdfad6ca5c3cee07ec93d48ee45580555117e4
James Adam <james@lazyatom.com>
2012-11-23 14:48:59 +0000

    Show mainstream links in the admin org display.

--- app/models/organisation_mainstream_link.rb
+++ app/models/organisation_mainstream_link.rb
@@ -1,4 +1,19 @@
 class OrganisationMainstreamLink < ActiveRecord::Base
   belongs_to :organisation
   validates :slug, :title, presence: true
+
+  def public_url(host)
+    slug_with_leading_slash
+    if slug =~ /^http/
+      slug
+    else
+      "http://" + Whitehall.public_host_for(host) + slug_with_leading_slash
+    end
+  end
+
+  private
+
+  def slug_with_leading_slash
+    slug =~ /^\// ? slug : "/#{slug}"
+  end
 end
--- test/unit/organisation_mainstream_link_test.rb
+++ test/unit/organisation_mainstream_link_test.rb
@@ -10,4 +10,21 @@ class OrganisationMainstreamLinkTest < ActiveSupport::TestCase
     link = build(:organisation_mainstream_link, title: nil)
     refute link.valid?
   end
+
+  test "#public_url should prepend the public host if a slug is given" do
+    link = build(:organisation_mainstream_link, slug: "/mainstream/gov")
+    Whitehall.stubs(:public_host_for).with("admin-host").returns("public-host")
+    assert_equal "http://public-host/mainstream/gov", link.public_url("admin-host")
+  end
+
+  test "#public_url should return the slug if it starts with a protocol" do
+    link = build(:organisation_mainstream_link, slug: "https://some-other-host/mainstream/gov")
+    assert_equal "https://some-other-host/mainstream/gov", link.public_url("admin-host")
+  end
+
+  test "#public_url should add a slash where a leading one is missing from the slug" do
+    link = build(:organisation_mainstream_link, slug: "mainstream/gov")
+    Whitehall.stubs(:public_host_for).with("admin-host").returns("public-host")
+    assert_equal "http://public-host/mainstream/gov", link.public_url("admin-host")
+  end
 end
