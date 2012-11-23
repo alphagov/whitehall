3bf20b68bf2c25770dc93aae36bd5af7986da922
Edd Sowden <e@e26.co.uk>
2012-11-23 10:23:07 +0000

    news_articles#index is no longer used so removing

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,10 +1,6 @@
 class NewsArticlesController < DocumentsController
   FORMAT_NAME = "news"
 
-  def index
-    @news_articles = NewsArticle.published.by_first_published_at
-  end
-
   def show
     @related_policies = @document.published_related_policies
     @document = NewsArticlePresenter.decorate(@document)
