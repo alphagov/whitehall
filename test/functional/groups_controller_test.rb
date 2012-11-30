4a094571b515f7e5c1488d924ef5fd7ed2e4d36e
James Mead <james@floehopper.org>
2012-11-30 15:10:22 +0000

    Display govspeak-ified description on group page.

--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -3,6 +3,22 @@ require "test_helper"
 class GroupsControllerTest < ActionController::TestCase
   should_be_a_public_facing_controller
 
+  test "should display name and govspeak-ified description of group" do
+    organisation = create(:organisation)
+    group = create(:group,
+      name: "Defence Council",
+      description: "description-in-govspeak",
+      organisation: organisation
+    )
+
+    govspeak_transformation_fixture "description-in-govspeak" => "description-in-html" do
+      get :show, organisation_id: organisation, id: group
+    end
+
+    assert_select "h1.main", "Defence Council"
+    assert_select "p.description", "description-in-html"
+  end
+
   test "should display a list of group members" do
     person_one, person_two = create(:person), create(:person)
     organisation = create(:organisation)
