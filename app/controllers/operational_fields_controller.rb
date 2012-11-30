0d75f252ca8c8fee7a0a629b9517c89899a205fb
Chris Parsons <chris@thinkcodelearn.com>
2012-11-30 16:13:55 +0000

    Fix hash syntax

--- app/controllers/operational_fields_controller.rb
+++ app/controllers/operational_fields_controller.rb
@@ -5,7 +5,7 @@ class OperationalFieldsController < PublicFacingController
 
   def show
     @operational_field = OperationalField.find(params[:id])
-    @organisation = Organisation.where(:handles_fatalities => true).first
+    @organisation = Organisation.where(handles_fatalities: true).first
     @fatality_notices = @operational_field.published_fatality_notices.map do |fatality_notice|
       FatalityNoticePresenter.new(fatality_notice)
     end
