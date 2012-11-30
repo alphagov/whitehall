8258e4c160c38a15cddf8c9838277d760d41fc26
Chris Parsons <chris@thinkcodelearn.com>
2012-11-30 13:46:22 +0000

    Switch hash syntax

--- db/migrate/20121130095754_add_handles_fatalities_to_organisation.rb
+++ db/migrate/20121130095754_add_handles_fatalities_to_organisation.rb
@@ -1,6 +1,6 @@
 class AddHandlesFatalitiesToOrganisation < ActiveRecord::Migration
   def change
-    add_column :organisations, :handles_fatalities, :boolean, :default => false
+    add_column :organisations, :handles_fatalities, :boolean, default: false
     execute("UPDATE organisations SET handles_fatalities = true WHERE slug = 'ministry-of-defence'")
   end
 end
