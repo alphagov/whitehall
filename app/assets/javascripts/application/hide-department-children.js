ff1045bf88a6f9b14ec1c08a9fd5d7ab7e268365
Edd Sowden <e@e26.co.uk>
2012-12-06 09:29:37 +0000

    Hide department children on page load
    We were waiting till after dom ready to hide the department children and
    this was causing page scroll to be in the wrong place. This will now
    hide the children as soon as the page loads.

--- app/assets/javascripts/application/hide-department-children.js
+++ app/assets/javascripts/application/hide-department-children.js
@@ -11,17 +11,20 @@
 
       $departments.each(function(i, el){
         var $department = $(el),
+            $childOrganisations = $department.find('.child-organisations'),
             $viewAll = $('<a href="#" class="view-all">view all</a>');
 
         if($department.find(windowHash).length === 0){
           // only hide if its not the department we should be showing
           $department.toggleClass('js-hiding-children');
         }
+        $childOrganisations.removeClass('js-hidden');
+
         $viewAll.click(function(e){
           e.preventDefault();
           $department.toggleClass('js-hiding-children');
         });
-        $viewAll.insertBefore($department.find('.child-organisations'))
+        $viewAll.insertBefore($childOrganisations)
       });
 
       $(document).on('govuk.hideDepartmentChildren.hideAll', hideDepartmentChildren.hideAllChildren);
