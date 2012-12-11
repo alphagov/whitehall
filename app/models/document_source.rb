58a99c0f5be89d152fd34e0a8f1c5397fdb6f9b9
Matthew Ford <matt@bitzesty.com>
2012-12-11 17:10:14 +0000

    only allow correctly formatted urls

--- app/models/document_source.rb
+++ app/models/document_source.rb
@@ -2,6 +2,5 @@ class DocumentSource < ActiveRecord::Base
   belongs_to :document
   belongs_to :import
 
-  validates :url, presence: true, uniqueness: true
-
+  validates :url, presence: true, uniqueness: true, format: URI::regexp(%w(http https))
 end
