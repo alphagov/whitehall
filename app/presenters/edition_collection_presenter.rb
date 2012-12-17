37408e60c2e528a3d5708583f7a3bd25df3d21f8
Matthew Ford <matt@bitzesty.com>
2012-12-17 11:13:04 +0000

    Adding InternationalPriorityPresenter to fix atom feed
    This is do that we display the date correctly
    
    https://www.pivotaltracker.com/story/show/41075833

--- app/presenters/edition_collection_presenter.rb
+++ app/presenters/edition_collection_presenter.rb
@@ -39,6 +39,8 @@ class EditionCollectionPresenter
       NewsArticlePresenter
     when DetailedGuide
       DetailedGuidePresenter
+    when InternationalPriority
+      InternationalPriorityPresenter
     end
   end
 end
