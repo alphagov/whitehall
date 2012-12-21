bd8d6d78e5bc125e63ba92f15c3dd91254731b82
Matthew Ford <matt@bitzesty.com>
2012-12-21 13:08:02 +0000

    fix test for classification_featuring

--- test/unit/topical_event_test.rb
+++ test/unit/topical_event_test.rb
@@ -10,8 +10,10 @@ class TopicalEventTestTest < ActiveSupport::TestCase
   test "a featured news article is featured" do
     topical_event = create(:topical_event)
     news_article = create(:published_news_article)
-    topical_event.feature!(edition_id: news_article.id, alt_text: "A thing")
-    assert featuring = topical_event.featuring_of(news_article)
+    image = create(:classification_featuring_image_data)
+    topical_event.feature(edition_id: news_article.id, alt_text: "A thing", image: image)
+    featuring = topical_event.featuring_of(news_article)
+    assert featuring
     assert_equal 1, featuring.ordering
     assert topical_event.featured?(news_article)
   end
