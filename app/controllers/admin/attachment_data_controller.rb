2d3f4b8c2a4cd2485d4494a5b4e1bbe6232e71bd
Matthew Ford <matt@bitzesty.com>
2013-01-04 11:17:58 +0000

    Move error message into form and update tests

--- app/controllers/admin/attachment_data_controller.rb
+++ app/controllers/admin/attachment_data_controller.rb
@@ -9,13 +9,13 @@ class Admin::AttachmentDataController < Admin::BaseController
     params[:attachment_data] ||= {}
     new_file_name = params[:attachment_data][:file].try(:original_filename)
     unless new_file_name == @attachment_data.carrierwave_file
-      flash[:alert] = "You can only update a file if the new file has the same name as the old one, if this is not the case please add a new attachment instead"
+      @attachment_data.errors.add(:base, "You can only update a file if the new file has the same name as the old one, if this is not the case please add a new attachment instead.")
       render :edit
     else
       @attachment_data.file = params[:attachment_data][:file]
       @attachment_data.update_file_attributes #dont remove me please
       @attachment_data.save
-      flash[:notice] = "Attachment data updated, you can close this tab"
+      flash[:notice] = "Attachment data updated, you can close this tab."
       render :edit
     end
   end
--- test/functional/admin/attachment_data_controller_test.rb
+++ test/functional/admin/attachment_data_controller_test.rb
@@ -24,8 +24,8 @@ class Admin::AttachmentDataControllerTest < ActionController::TestCase
 
     put :update, id: attachment_data.id, attachment_data: {file: fixture_file_upload("/attachment_data_update/greenpaper.final.pdf", "application/pdf")}
 
-    assert_equal 'You can only update a file if the new file has the same name as the old one, if this is not the case please add a new attachment instead', flash[:alert]
-    not_updated_attachment_data = AttachmentData.find(attachment_data.id)
+    not_updated_attachment_data = assigns(:attachment_data)
+    assert_equal 'You can only update a file if the new file has the same name as the old one, if this is not the case please add a new attachment instead.', not_updated_attachment_data.errors[:base].first
     assert_not_equal not_updated_attachment_data.carrierwave_file, "greenpaper.final.pdf"
     assert_equal 3470, not_updated_attachment_data.file_size
   end
@@ -38,8 +38,8 @@ class Admin::AttachmentDataControllerTest < ActionController::TestCase
 
     assert_equal 12167, fixture_file_upload("/attachment_data_update/greenpaper.pdf", "application/pdf").size
 
-    assert_equal 'Attachment data updated, you can close this tab', flash[:notice]
-    updated_attachment_data = AttachmentData.find(attachment_data.id)
+    assert_equal 'Attachment data updated, you can close this tab.', flash[:notice]
+    updated_attachment_data = assigns(:attachment_data)
     assert_equal updated_attachment_data.carrierwave_file, "greenpaper.pdf"
     assert_equal 12167, updated_attachment_data.file_size
   end
