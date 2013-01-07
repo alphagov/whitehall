6295e458c8bd67b103befbc045542fdcb7744161
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-01-07 17:03:16 +0000

    Use govuk_setenv correctly for Jenkins
    Note: this depends on the `whitehall` directory existing on its own in
    the env.d directory, as it will break in its current state.
    
    This work has been done in alphagov/puppet#217 .

--- jenkins-parallel.sh
+++ jenkins-parallel.sh
@@ -1,15 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 export DISPLAY=:99
-export GOVUK_APP_DOMAIN=test.gov.uk
-export GOVUK_ASSET_ROOT=http://static.test.gov.uk
-
 env
 
 time bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-time bundle exec rake db:create db:migrate db:test:prepare --trace && \
-    RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:clean --trace && \
-    RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest parallel:prepare parallel:test parallel:features test:javascript test:cleanup --trace && \
-    RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:precompile --trace
+
+govuk_setenv whitehall time bundle exec rake db:create db:migrate db:test:prepare --trace
+govuk_setenv whitehall env RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:clean --trace
+govuk_setenv whitehall env RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest parallel:prepare parallel:test parallel:features test:javascript test:cleanup --trace
+govuk_setenv whitehall env RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:precompile --trace
 
 EXIT_STATUS=$?
 echo "EXIT STATUS: $EXIT_STATUS"
