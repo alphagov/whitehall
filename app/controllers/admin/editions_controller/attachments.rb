3cded5a1bdf1f0b7031e1cb59fb46398d7dd6eeb
Murray Steele <muz@h-lame.com>
2013-01-11 18:02:45 +0000

    Extract attachment_action handling from EditionsController::Attachments
    Things that aren't editions can have attachments and their controllers will use the attachments_fields partial but not the EditionsController::Attachments module.  Therefore the code for handling the attachment_action param needs to be available outside of that module.

--- app/controllers/admin/editions_controller/attachments.rb
+++ app/controllers/admin/editions_controller/attachments.rb
@@ -18,22 +18,7 @@ module Admin::EditionsController::Attachments
   def cope_with_attachment_action_params
     return unless params[:edition] && params[:edition][:edition_attachments_attributes]
     params[:edition][:edition_attachments_attributes].each do |_, edition_attachment_params|
-      if edition_attachment_params[:attachment_attributes] && edition_attachment_params[:id]
-        case edition_attachment_params[:attachment_attributes].delete(:attachment_action).to_s.downcase
-        when 'keep'
-          edition_attachment_params.delete(:_destroy)
-          edition_attachment_params[:attachment_attributes].delete(:attachment_data_attributes)
-        when 'remove'
-          edition_attachment_params['_destroy'] = '1'
-          edition_attachment_params[:attachment_attributes].delete(:attachment_data_attributes)
-        when 'replace'
-          edition_attachment_params.delete(:_destroy)
-          edition_attachment_params[:attachment_attributes][:attachment_data_attributes].delete(:id)
-        else
-          edition_attachment_params.delete(:_destroy)
-          edition_attachment_params[:attachment_attributes].delete(:attachment_data_attributes)
-        end
-      end
+      Admin::AttachmentActionParamHandler.handle!(edition_attachment_params)
     end
   end
 end
