3c48dd679608dba2377f2999652ae73c562f157c
Edd Sowden <e@e26.co.uk>
2013-01-16 12:15:27 +0000

    Make document series pages sort documents by age

--- app/controllers/document_series_controller.rb
+++ app/controllers/document_series_controller.rb
@@ -8,8 +8,8 @@ class DocumentSeriesController < PublicFacingController
 
   def show
     @document_series = @organisation.document_series.find(params[:id])
-    @published_publications = PublicationesquePresenter.decorate(@document_series.published_publications)
-    @published_statistical_data_sets = StatisticalDataSetPresenter.decorate(@document_series.published_statistical_data_sets)
+    @published_publications = PublicationesquePresenter.decorate(@document_series.published_publications.in_reverse_chronological_order)
+    @published_statistical_data_sets = StatisticalDataSetPresenter.decorate(@document_series.published_statistical_data_sets.in_reverse_chronological_order)
     set_slimmer_organisations_header([@document_series.organisation])
     expire_on_next_scheduled_publication(@document_series.scheduled_publications + @document_series.scheduled_statistical_data_sets)
   end
