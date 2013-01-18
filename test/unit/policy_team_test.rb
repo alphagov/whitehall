4439cb5aae4f2d389408b3a45a995797ff4ed49b
Matthew Ford <matt@bitzesty.com>
2013-01-18 16:07:20 +0000

    Fix tests on policy team

--- test/unit/policy_team_test.rb
+++ test/unit/policy_team_test.rb
@@ -6,15 +6,15 @@ class PolicyTeamTest < ActiveSupport::TestCase
     refute policy_team.valid?
   end
 
-  test "should be invalid without an email" do
+  test "should be valid without an email" do
     policy_team = build(:policy_team, email: "")
-    refute policy_team.valid?
+    assert policy_team.valid?
   end
 
-  test "should be invalid without a unique email" do
+  test "should be valid without a unique email" do
     existing_policy_team = create(:policy_team)
     new_policy_team = build(:policy_team, email: existing_policy_team.email)
-    refute new_policy_team.valid?
+    assert new_policy_team.valid?
   end
 
   test "should be invalid without a valid email" do
