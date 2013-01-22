08cf63a4196e4be4d1943428ccb9d467b1dbdb38
Murray Steele <muz@h-lame.com>
2013-01-22 14:01:44 +0000

    Extract data import steps to a helper to allow reuse

--- features/support/import_helpers.rb
+++ features/support/import_helpers.rb
@@ -10,6 +10,31 @@ module ImportHelpers
     yield tf.path
     tf.unlink
   end
+
+  def import_data_as_document_type_for_organisation(data, document_type, organisation)
+    make_sure_data_importer_user_exists
+    Import.use_separate_connection
+
+    with_import_csv_file(data) do |path|
+      visit new_admin_import_path
+      select document_type, from: 'Type'
+      attach_file 'CSV File', path
+      select organisation.name, from: 'Default organisation'
+      click_button 'Save'
+      click_button 'Run'
+
+      run_last_import
+
+      visit current_path
+    end
+    Import.find(current_path.match(/admin\/imports\/(\d+)\Z/)[1])
+  end
+
+  def make_sure_data_importer_user_exists
+    unless User.find_by_name('Automatic Data Importer')
+      create(:user, name: 'Automatic Data Importer')
+    end
+  end
 end
 
 World(ImportHelpers)
