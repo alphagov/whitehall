d1baef005555947795bbe69129e0d988f5113799
Chris Parsons <chris@thinkcodelearn.com>
2013-01-24 11:47:53 +0000

    Futher changes to admin -> public url script
    1. Re-added the database discovery steps to ensure it works in
    production
    2. Added *all* editions to the list now
    3. Added organisation edit/show admin links

--- script/dump_all_admin_to_public_documents_and_non_documents.rb
+++ script/dump_all_admin_to_public_documents_and_non_documents.rb
@@ -2,9 +2,9 @@ require 'csv'
 require 'ostruct'
 include Rails.application.routes.url_helpers, PublicDocumentRoutesHelper, Admin::EditionRoutesHelper
 
-#mysql_slave_config = ActiveRecord::Base.configurations['production'].merge('host' => 'slave.mysql')
-#ActiveRecord::Base.establish_connection(mysql_slave_config)
-#
+mysql_slave_config = ActiveRecord::Base.configurations['production'].merge('host' => 'slave.mysql')
+ActiveRecord::Base.establish_connection(mysql_slave_config)
+
 ENV['FACTER_govuk_platform'] ||= "production"
 
 admin_host = "whitehall-admin.#{ENV['FACTER_govuk_platform']}.alphagov.co.uk"
@@ -17,17 +17,35 @@ module PublicDocumentRoutesHelper
   end
 end
 
+def row(public_url, admin_url)
+  [
+    '',
+    public_url,
+    '',
+    '',
+    '',
+    admin_url,
+    ''
+  ]
+end
+
+
 CSV.open(Rails.root.join('public/government/all_document_attachment_and_non_document_mappings.csv'), 'wb') do |csv_out|
 
   csv_out << ['Old Url','New Url','Status','Whole Tag','Slug','Admin Url','State']
-  Document.all.each do |document|
-    edition = document.published_edition || document.latest_edition
-    if edition
+  Document.find_each do |document|
+    document.editions.each do |edition|
       status = (edition.state == 'published' ? '301' : '')
       whole_tag = (edition.state == 'published' ? 'Closed' : 'Open')
-      csv_out << [document.document_sources.any? ? document.document_sources.first.url : '', public_document_url(edition, protocol: 'https'), status, whole_tag, document.slug, admin_edition_url(edition, host: admin_host, protocol: 'https'), edition.state]
-    else
-      csv_out << [document.document_sources.any? ? document.document_sources.first.url : '' , '', '', 'Open']
+      csv_out << [
+        document.document_sources.any? ? document.document_sources.first.url : '',
+        public_document_url(document.published_edition || document.latest_edition, protocol: 'https'),
+        status,
+        whole_tag,
+        document.slug,
+        admin_edition_url(edition, host: admin_host, protocol: 'https'),
+        edition.state
+      ]
     end
   end
 
@@ -42,80 +60,64 @@ CSV.open(Rails.root.join('public/government/all_document_attachment_and_non_docu
 
   SupportingPage.find_each do |page|
     next unless page.edition.present?
-    csv_out << [
-      '',
-      admin_edition_supporting_page_url(page, edition_id: page.edition_id, host: admin_host, protocol: 'https'),
+    csv_out << row(
       policy_supporting_page_url(page.edition.document, page, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
+      admin_edition_supporting_page_url(page, edition_id: page.edition_id, host: admin_host, protocol: 'https')
+    )
   end
 
   Person.find_each do |person|
-    csv_out << [
-      '',
-      admin_person_url(person, host: admin_host, protocol: 'https'),
+    csv_out << row(
       person_url(person, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
-    csv_out << [
-      '',
+      admin_person_url(person, host: admin_host, protocol: 'https')
+    )
+    csv_out << row(
+      person_url(person, host: host_name, protocol: 'https'),
       edit_admin_person_url(person, host: admin_host, protocol: 'https'),
-      person_url(person, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
+    )
   end
 
   PolicyAdvisoryGroup.find_each do |group|
-    csv_out << [
-      '',
-      admin_policy_advisory_group_url(group, host: admin_host, protocol: 'https'),
+    csv_out << row(
       policy_advisory_group_url(group, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
-    csv_out << [
-      '',
-      edit_admin_policy_advisory_group_url(group, host: admin_host, protocol: 'https'),
+      admin_policy_advisory_group_url(group, host: admin_host, protocol: 'https')
+    )
+    csv_out << row(
       policy_advisory_group_url(group, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
+      edit_admin_policy_advisory_group_url(group, host: admin_host, protocol: 'https')
+    )
   end
 
   PolicyTeam.find_each do |team|
-    csv_out << [
-      '',
+    csv_out << row(
+      policy_team_url(team, host: host_name, protocol: 'https'),
       admin_policy_team_url(team, host: admin_host, protocol: 'https'),
+    )
+    csv_out << row(
       policy_team_url(team, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
-    csv_out << [
-      '',
       edit_admin_policy_team_url(team, host: admin_host, protocol: 'https'),
-      policy_team_url(team, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
+    )
   end
 
   Role.find_each do |role|
-    csv_out << [
-      '',
+    csv_out << row(
+      ministerial_role_url(role, host: host_name, protocol: 'https'),
       admin_role_url(role, host: admin_host, protocol: 'https'),
+    )
+    csv_out << row(
       ministerial_role_url(role, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
-    csv_out << [
-      '',
       edit_admin_role_url(role, host: admin_host, protocol: 'https'),
-      ministerial_role_url(role, host: host_name, protocol: 'https'),
-      '',
-      'Open'
-    ]
+    )
+  end
+
+  Organisation.find_each do |organisation|
+    csv_out << row(
+      organisation_url(organisation, host: host_name, protocol: 'https'),
+      admin_organisation_url(organisation, host: admin_host, protocol: 'https'),
+    )
+    csv_out << row(
+      organisation_url(organisation, host: host_name, protocol: 'https'),
+      edit_admin_organisation_url(organisation, host: admin_host, protocol: 'https'),
+    )
   end
 end
