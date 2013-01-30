c5e8e8f7e11ac319bd682b7968ff01fe7f18f5c4
Murray Steele <muz@h-lame.com>
2013-01-30 15:28:58 +0000

    Allow for world_locations as filters in routes
    Nothing has been done to make the DocumentFilter action these params though.  There are big changes incoming, so it feels like it's not worthwhile (yet).

--- test/unit/helpers/filter_routes_helper_test.rb
+++ test/unit/helpers/filter_routes_helper_test.rb
@@ -12,11 +12,18 @@ class FilterRoutesHelperTest < ActionView::TestCase
       assert_equal send("#{filter}_path", topics: [topic.slug]), send("#{filter}_filter_path", topic)
     end
 
-    test "uses the organisation and topic to generate the route to #{filter} filter" do
+    test "uses the world location to generate the route to #{filter} filter" do
+      world_location = create(:world_location)
+      assert_equal send("#{filter}_path", world_locations: [world_location.slug]), send("#{filter}_filter_path", world_location)
+    end
+
+    test "uses the organisation and topic and world_location to generate the route to #{filter} filter" do
       organisation = create(:organisation)
       topic = create(:topic)
-      assert_equal send("#{filter}_path", departments: [organisation.slug], topics: [topic.slug]), send("#{filter}_filter_path", organisation, topic)
+      world_location = create(:world_location)
+      assert_equal send("#{filter}_path", departments: [organisation.slug], topics: [topic.slug], world_locations: [world_location.slug]), send("#{filter}_filter_path", organisation, topic, world_location)
     end
+
 
     test "uses optional hash to route to #{filter} filter" do
       assert_equal send("#{filter}_path", publication_type: 'transparency-data'), send("#{filter}_filter_path", publication_type: 'transparency-data')
