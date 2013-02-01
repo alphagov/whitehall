bc9410128bc9d2df726791daabf003555e22febe
Murray Steele <muz@h-lame.com>
2013-02-01 13:10:18 +0000

    Add data migration to create extra countries required by FCO and DFID

---
+++ db/data_migration/20130201124727_add_more_countries.rb
@@ -0,0 +1,12 @@
+require 'csv'
+
+lines = created = 0
+CSV.read(__FILE__.gsub(/\.rb/, '.csv'), headers: false, encoding: "UTF-8").each do |row|
+  unless row[0].blank?
+    lines += 1
+    wl = WorldLocation.create(name: row[0], world_location_type: WorldLocationType::Country)
+    created += 1 if wl.persisted?
+  end
+end
+
+puts "Total rows: #{lines}, created countries: #{created}"
