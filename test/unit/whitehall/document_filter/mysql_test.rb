772c963899acaa043234c0f8a88d259587cdf47a
Murray Steele <muz@h-lame.com>
2013-02-04 08:55:49 +0000

    Refactor mysql document filter test

--- test/unit/whitehall/document_filter/mysql_test.rb
+++ test/unit/whitehall/document_filter/mysql_test.rb
@@ -18,9 +18,7 @@ module Whitehall::DocumentFilter
 
     test "alphabetical direction returns the given set of documents ordered alphabetically" do
       document_scope.expects(:alphabetical)
-      filter = Whitehall::DocumentFilter::Mysql.new(direction: "alphabetical")
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, direction: "alphabetical")
     end
 
     test "topics param filters the documents by topic using slugs" do
@@ -29,9 +27,7 @@ module Whitehall::DocumentFilter
       filtered_scope = stub_document_scope('filtered_scope')
       document_scope.expects(:published_in_topic).with([topic]).returns(filtered_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new(topics: [topic.slug])
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, topics: [topic.slug])
 
       assert_equal filtered_scope, filter.documents
     end
@@ -42,9 +38,7 @@ module Whitehall::DocumentFilter
       filtered_scope = stub_document_scope('filtered_scope')
       document_scope.stubs(:published_in_topic).with([topic]).returns(filtered_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new(topics: [topic.slug])
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, topics: [topic.slug])
 
       assert_equal [topic], filter.selected_topics
     end
@@ -52,9 +46,7 @@ module Whitehall::DocumentFilter
     test "topics param does not filter if topics are not present" do
       document_scope.expects(:published_in_topic).never
 
-      filter = Whitehall::DocumentFilter::Mysql.new( topics: "")
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, topics: "")
 
       assert_equal document_scope, filter.documents
     end
@@ -62,9 +54,7 @@ module Whitehall::DocumentFilter
     test "topics param does not filter if topic is 'all'" do
       document_scope.expects(:published_in_topic).never
 
-      filter = Whitehall::DocumentFilter::Mysql.new( topics: ['all'])
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, topics: ['all'])
 
       assert_equal document_scope, filter.documents
     end
@@ -75,9 +65,7 @@ module Whitehall::DocumentFilter
       filtered_scope = stub_document_scope('filtered_scope')
       document_scope.stubs(:in_organisation).with([organisation]).returns(filtered_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new( departments: [organisation.slug])
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, departments: [organisation.slug])
 
       assert_equal filtered_scope, filter.documents
     end
@@ -85,34 +73,26 @@ module Whitehall::DocumentFilter
     test "departments param sets #selected_organisations" do
       organisation = stub_organisation('defra')
 
-      filter = Whitehall::DocumentFilter::Mysql.new( departments: [organisation.slug])
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, departments: [organisation.slug])
 
       assert_equal [organisation], filter.selected_organisations
     end
 
     test "does not filter if departments are not present" do
       document_scope.expects(:in_organisation).never
-      filter = Whitehall::DocumentFilter::Mysql.new(departments: "")
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, departments: "")
     end
 
     test "does not filter if departments is 'all'" do
       document_scope.expects(:in_organisation).never
-      filter = Whitehall::DocumentFilter::Mysql.new( departments: ['all'])
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, departments: ['all'])
     end
 
     test "keywords param filters by content containing each keyword" do
       filtered_scope = stub_document_scope('filtered scope')
       document_scope.expects(:with_title_or_summary_containing).with("alpha", "beta").returns(filtered_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new( keywords: "alpha beta")
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, keywords: "alpha beta")
 
       assert_equal filtered_scope, filter.documents
     end
@@ -124,60 +104,47 @@ module Whitehall::DocumentFilter
 
     test "keywords param does not filter if no keywords were given" do
       document_scope.expects(:with_title_or_summary_containing).never
-      filter = Whitehall::DocumentFilter::Mysql.new(document_scope, keywords: '')
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, keywords: '')
     end
 
     test "strips leading and trailing spaces from keywords" do
       filtered_scope = stub_document_scope('filtered scope')
       document_scope.expects(:with_title_or_summary_containing).with("alpha", "beta").returns(filtered_scope)
-
-      filter = Whitehall::DocumentFilter::Mysql.new( keywords: " alpha   beta ")
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, keywords: " alpha   beta ")
 
       assert_equal filtered_scope, filter.documents
     end
 
     test "date and direction param allows filtering before a date" do
       document_scope.expects(:published_before).with(Date.parse("2012-01-01 12:23:45")).returns(document_scope)
-      filter = Whitehall::DocumentFilter::Mysql.new( date: "2012-01-01 12:23:45", direction: "before")
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, date: "2012-01-01 12:23:45", direction: "before")
     end
 
     test "direction before a date returns documents in reverse chronological order" do
       document_scope.expects(:in_reverse_chronological_order).returns(document_scope)
-      filter = Whitehall::DocumentFilter::Mysql.new( date: "2012-01-01 12:23:45", direction: "before")
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, date: "2012-01-01 12:23:45", direction: "before")
     end
 
     test "direction param sets direction attribute" do
-      assert_equal "before", Whitehall::DocumentFilter::Mysql.new( direction: "before").direction
+      assert_equal "before", Whitehall::DocumentFilter::Mysql.new(direction: "before").direction
     end
 
     test "date param sets date attribute" do
-      assert_equal Date.parse("2012-01-01 12:23:45"), Whitehall::DocumentFilter::Mysql.new( date: "2012-01-01 12:23:45").date
+      assert_equal Date.parse("2012-01-01 12:23:45"), Whitehall::DocumentFilter::Mysql.new(date: "2012-01-01 12:23:45").date
     end
 
     test "invalid date param sets date attribute to nil" do
-      assert_equal nil, Whitehall::DocumentFilter::Mysql.new( date: "invalid-date").date
+      assert_equal nil, Whitehall::DocumentFilter::Mysql.new(date: "invalid-date").date
     end
 
     test "can filter after a date" do
       document_scope.expects(:published_after).with(Date.parse("2012-01-01 12:23:45")).returns(document_scope)
-      filter = Whitehall::DocumentFilter::Mysql.new(date: "2012-01-01 12:23:45", direction: "after")
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, date: "2012-01-01 12:23:45", direction: "after")
     end
 
     test "filtering after a date returns documents in chronological order" do
       document_scope.expects(:in_chronological_order).returns(document_scope)
-      filter = Whitehall::DocumentFilter::Mysql.new(date: "2012-01-01 12:23:45", direction: "after")
-      filter.documents = document_scope
-      filter.apply_filters
+      create_filter(document_scope, date: "2012-01-01 12:23:45", direction: "after")
     end
 
     test "publication_type param filters by publication type" do
@@ -186,9 +153,7 @@ module Whitehall::DocumentFilter
       filtered_scope = stub_document_scope('filtered_scope')
       document_scope.expects(:where).with(publication_type_id: [123]).returns(filtered_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new( publication_filter_option: publication_filter_option.slug)
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, publication_filter_option: publication_filter_option.slug)
       assert_equal filtered_scope, filter.documents
     end
 
@@ -199,27 +164,20 @@ module Whitehall::DocumentFilter
       expected_query = "(`editions`.`publication_type_id` IN (123, 234) OR `editions`.`type` IN ('EditionType'))"
       document_scope.expects(:where).with(responds_with(:to_sql, expected_query)).returns(filtered_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new( publication_filter_option: publication_filter_option.slug)
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, publication_filter_option: publication_filter_option.slug)
       assert_equal filtered_scope, filter.documents
     end
 
     test "publication_filter_option param sets #selected_publication_filter_option" do
       publication_filter_option = stub_publication_filter_option("testing filter option - statistics")
-
-      filter = Whitehall::DocumentFilter::Mysql.new( publication_filter_option: publication_filter_option.slug)
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, publication_filter_option: publication_filter_option.slug)
 
       assert_equal publication_filter_option, filter.selected_publication_filter_option
     end
 
     test "publication_type param also sets #selected_publication_filter_option to keep old links working" do
       publication_filter_option = stub_publication_filter_option("testing filter option - statistics")
-      filter = Whitehall::DocumentFilter::Mysql.new( publication_type: publication_filter_option.slug)
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, publication_type: publication_filter_option.slug)
 
       assert_equal publication_filter_option, filter.selected_publication_filter_option
     end
@@ -244,32 +202,22 @@ module Whitehall::DocumentFilter
       transcript = create(:published_speech, speech_type: SpeechType::Transcript)
       statement = create(:published_speech, speech_type: SpeechType::WrittenStatement)
 
-      filter = Whitehall::DocumentFilter::Mysql.new(announcement_type_option: "news-stories")
-      filter.documents = Announcement.published
-      filter.apply_filters
+      filter = create_filter(Announcement.published, announcement_type_option: "news-stories")
       assert_equal [news_article.id], filter.documents.map(&:id)
       
-      filter = Whitehall::DocumentFilter::Mysql.new(announcement_type_option: "fatality-notices")
-      filter.documents = Announcement.published
-      filter.apply_filters
+      filter = create_filter(Announcement.published, announcement_type_option: "fatality-notices")
       assert_equal [fatality_notice.id], filter.documents.map(&:id)
 
-      filter = Whitehall::DocumentFilter::Mysql.new(announcement_type_option: "speeches")
-      filter.documents = Announcement.published
-      filter.apply_filters
+      filter = create_filter(Announcement.published, announcement_type_option: "speeches")
       assert_equal [transcript.id], filter.documents.map(&:id)
       
-      filter = Whitehall::DocumentFilter::Mysql.new(announcement_type_option: "statements")
-      filter.documents = Announcement.published
-      filter.apply_filters
+      filter = create_filter(Announcement.published, announcement_type_option: "statements")
       assert_equal [statement.id], filter.documents.map(&:id)
     end
 
     test "publication_filter_option overwrites older publication_type param" do
       publication_filter_option = stub_publication_filter_option("testing filter option - statistics")
-      filter = Whitehall::DocumentFilter::Mysql.new(publication_type: 'foobar', publication_filter_option: publication_filter_option.slug)
-      filter.documents = document_scope
-      filter.apply_filters
+      filter = create_filter(document_scope, publication_type: 'foobar', publication_filter_option: publication_filter_option.slug)
 
       assert_equal publication_filter_option, filter.selected_publication_filter_option
     end
@@ -278,9 +226,7 @@ module Whitehall::DocumentFilter
       document_scope.expects(:page).with(3).returns(document_scope)
       document_scope.expects(:per).with(20).returns(document_scope)
       with_number_of_documents_per_page(20) do
-        filter = Whitehall::DocumentFilter::Mysql.new( page: 3)
-        filter.documents = document_scope
-        filter.apply_filters
+        create_filter(document_scope, page: 3)
       end
     end
 
@@ -292,13 +238,10 @@ module Whitehall::DocumentFilter
       document_scope.expects(:published_in_topic).with([topic]).returns(document_scope)
       document_scope.expects(:page).with(2).returns(document_scope)
 
-      filter = Whitehall::DocumentFilter::Mysql.new(
+      filter = create_filter(document_scope,
         departments: [organisation.slug],
         topics: [topic.slug],
         page: 2)
-      filter.documents = document_scope
-      filter.apply_filters
-
 
       assert_equal [organisation], filter.selected_organisations
       assert_equal [topic], filter.selected_topics
@@ -311,12 +254,10 @@ module Whitehall::DocumentFilter
       news_article = create(:published_news_article, related_policies: [policy], organisations: [organisation])
 
       document_scope = Announcement.published.includes(:document, :organisations)
-      filter = Whitehall::DocumentFilter::Mysql.new(
+      filter = create_filter(document_scope, 
         departments: [organisation.slug],
         topics: [topic.slug],
         page: 1)
-      filter.documents = document_scope
-      filter.apply_filters
 
       assert_equal news_article.document_id, filter.documents.first.document.id
     end
@@ -326,10 +267,7 @@ module Whitehall::DocumentFilter
       topic = create(:topic, policies: [policy])
       3.times { create(:published_publication, related_policies: [policy]) }
       queries = count_queries { 
-        filter = Whitehall::DocumentFilter::Mysql.new(topics: [topic.slug])
-        filter.documents = Publication.published
-        filter.apply_filters
-        filter.documents
+        create_filter(Publication.published, topics: [topic.slug]).documents
       }
       assert 3 > queries
     end
@@ -338,10 +276,7 @@ module Whitehall::DocumentFilter
       organisation = create(:organisation)
       3.times { create(:published_publication, organisations: [organisation]) }
       queries = count_queries {
-        filter = Whitehall::DocumentFilter::Mysql.new(departments: [organisation.slug])
-        filter.documents = Publication.published
-        filter.apply_filters
-        filter.documents
+        create_filter(Publication.published, departments: [organisation.slug]).documents
       }
       assert 3 > queries
     end
@@ -349,10 +284,7 @@ module Whitehall::DocumentFilter
     test 'does not use n+1 selects when filtering by keywords' do
       3.times { |i| create(:published_publication, title: "keyword-#{i}") }
       queries = count_queries { 
-        filter = Whitehall::DocumentFilter::Mysql.new(keywords: "keyword")
-        filter.documents = Publication.published
-        filter.apply_filters
-        filter.documents
+        create_filter(Publication.published, keywords: "keyword").documents
       }
       assert 3 > queries
     end
@@ -360,10 +292,7 @@ module Whitehall::DocumentFilter
     test 'does not use n+1 selects when filtering by date' do
       3.times { |i| create(:published_publication, publication_date: i.months.ago) }
       queries = count_queries {
-        filter = Whitehall::DocumentFilter::Mysql.new(date: "2012-01-01 12:23:45", direction: "before")
-        filter.documents = Publication.published
-        filter.apply_filters
-        filter.documents
+        create_filter(Publication.published, date: "2012-01-01 12:23:45", direction: "before").documents
       }
       assert 3 > queries
     end
@@ -376,9 +305,7 @@ module Whitehall::DocumentFilter
       create(:published_speech)
       create(:published_news_article)
       unfiltered_announcements = Announcement.published
-      filter = Whitehall::DocumentFilter::Mysql.new(topics: [topic.slug])
-      filter.documents = unfiltered_announcements
-      filter.apply_filters
+      filter = create_filter(unfiltered_announcements, topics: [topic.slug])
 
       assert_equal 2, filter.documents.count
     end
@@ -397,6 +324,13 @@ module Whitehall::DocumentFilter
     # test_delegates_to_documents(:first_page?)
 
   private
+
+    def create_filter(document_set, args)
+      filter =Whitehall::DocumentFilter::Mysql.new(args)
+      filter.documents = document_set
+      filter.apply_filters
+      filter
+    end
 
     def document_scope
       @document_scope ||= stub_document_scope('unfiltered document scope')
