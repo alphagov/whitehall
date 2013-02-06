1124e2e4f516d05d3060c8bf2328f72a8b2d6b43
Tom Ward <tom@popdog.net>
2013-02-06 17:47:48 +0000

    Use a translations table for world locations
    This does everything required to move the name and description of
    every WorldLocation into a translations table, into which alternative
    translations can be added.  The work to actually add alternative
    translations or allow them to be displayed will be added in future
    commits.
    
    In general this was straightforward; the main issue was how to sort
    locations on values no longer in the main table.  As there the number
    of locations is limited, I have resorted to sorting them in ruby but
    for other model types, a more efficient solution will need to be
    found.

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Whitehall
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+
     config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
