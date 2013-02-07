cd284e6714a21674b4fb0added643244ec9a9d92
Tom Ward <tom@popdog.net>
2013-02-07 09:00:42 +0000

    Limit the available locales to English, French and Spanish (for now)
    I plan on using the available locales to build a select box when
    creating a new translation.

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Whitehall
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
     config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :fr, :es]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
