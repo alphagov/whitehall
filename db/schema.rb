aa09a5f101de57d6d1c017afe4baec844848f30f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-02-07 11:58:35 +0000

    Remove country from contacts with blank addresses
    This is a cleanup after a previous migration which added country field
    and set all countries to UK by default. If the contact doesn't have a
    postal address we don't want Country to be set.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207090657) do
+ActiveRecord::Schema.define(:version => 20130207115522) do
 
   create_table "attachment_data", :force => true do |t|
     t.string   "carrierwave_file"
