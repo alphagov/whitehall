63129cd8a1b1fde31ededcfc9e2d86e4fd435ebe
Chris Roos & James Mead <pairing+chris.roos+james.mead@gofreerange.com>
2013-02-07 15:12:57 +0000

    Replace PaperTrail's Version with our own
    We've taken only those parts of PaperTrail's Version that we use in our code,
    notably `Version#previous`.

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,5 +1,23 @@
 class Version < ActiveRecord::Base
-  attr_accessible :state
+  belongs_to :item, polymorphic: true
+  validates_presence_of :event
+  attr_accessible :item_type, :item_id, :event, :whodunnit, :state
+
+  def self.with_item_keys(item_type, item_id)
+    scoped(conditions: {item_type: item_type, item_id: item_id})
+  end
+
+  scope :preceding, lambda { |version|
+    where(["id < ?", version.id]).order("id DESC")
+  }
+
+  def sibling_versions
+    self.class.with_item_keys(item_type, item_id)
+  end
+
+  def previous
+    sibling_versions.preceding(self).first
+  end
 
   def user
     if whodunnit
