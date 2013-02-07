67e75020c8e2e60c3f2e08475c427f9e5a36f18c
Edd Sowden <e@e26.co.uk>
2013-02-07 15:48:40 +0000

    Add new default images and make thumbnails
    We only ever use these images at 300px wide so have created some 300px
    wide versions till we create a proper admin interface to manage these.

--- app/presenters/news_article_presenter.rb
+++ app/presenters/news_article_presenter.rb
@@ -6,8 +6,8 @@ class NewsArticlePresenter < Draper::Base
 
   def lead_image_path
     image = super
-    if image =~ /placeholder/ && news_article.organisations.any? && find_asset("organisation_default_news/#{news_article.organisations.first.slug}.jpg")
-      "organisation_default_news/#{news_article.organisations.first.slug}.jpg"
+    if image =~ /placeholder/ && news_article.organisations.any? && find_asset("organisation_default_news/s300_#{news_article.organisations.first.slug}.jpg")
+      "organisation_default_news/s300_#{news_article.organisations.first.slug}.jpg"
     else
       image
     end
