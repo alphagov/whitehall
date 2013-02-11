ae058c03fc1ba1c0ae6d27b22337b6a1a90b706e
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-02-11 16:03:42 +0000

    Better counting of file sizes
    This now only counts each attachment_data row once.

--- script/imported_filesize_reporter.rb
+++ script/imported_filesize_reporter.rb
@@ -4,13 +4,14 @@
 include ActionView::Helpers::NumberHelper
 
 query = ActiveRecord::Base.connection.execute(%Q{
-SELECT e.type, SUM(ad.file_size) FROM attachment_data ad
-       LEFT JOIN attachments a ON a.attachment_data_id = ad.id
-       LEFT JOIN edition_attachments ea ON ea.attachment_id = a.id
-       LEFT JOIN editions e ON e.id = ea.edition_id
+SELECT e.type, COUNT(e.id), SUM(ad.file_size) FROM attachment_data ad
+  JOIN (SELECT attachment_data_id, id FROM attachments GROUP BY attachment_data_id) a ON a.attachment_data_id = ad.id
+  LEFT JOIN edition_attachments ea ON ea.attachment_id = a.id
+  JOIN (SELECT id, type FROM editions WHERE editions.type IS NOT NULL) e ON e.id = ea.edition_id
 GROUP BY e.type;
-                                              })
+})
 query.each do |row|
   next if row.first.nil?
-  puts "#{row.first} -> #{number_to_human_size(row[1].to_f)}"
+  document_type, count, file_size = row
+  puts "#{document_type.underscore.humanize} -> #{number_to_human_size(file_size.to_f)} (average #{number_to_human_size(file_size.to_f / count.to_f)} from #{count.to_i} documents)"
 end
