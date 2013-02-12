e99cc7a3fc0b49b57c86087204c5e9ce0d3ca3fd
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-02-12 12:58:21 +0000

    Better estimation of file size growth
    I've turned off using joins for now as it wasn't giving me the control I
    wanted. We now tally all latest editions' attachments if they're present.

--- script/imported_filesize_reporter.rb
+++ script/imported_filesize_reporter.rb
@@ -3,15 +3,19 @@
 
 include ActionView::Helpers::NumberHelper
 
-query = ActiveRecord::Base.connection.execute(%Q{
-SELECT e.type, COUNT(e.id), SUM(ad.file_size) FROM attachment_data ad
-  JOIN (SELECT attachment_data_id, id FROM attachments GROUP BY attachment_data_id) a ON a.attachment_data_id = ad.id
-  LEFT JOIN edition_attachments ea ON ea.attachment_id = a.id
-  JOIN (SELECT id, type FROM editions WHERE editions.type IS NOT NULL) e ON e.id = ea.edition_id
-GROUP BY e.type;
-})
-query.each do |row|
-  next if row.first.nil?
-  document_type, count, file_size = row
-  puts "#{document_type.underscore.humanize} -> #{number_to_human_size(file_size.to_f)} (average #{number_to_human_size(file_size.to_f / count.to_f)} from #{count.to_i} documents)"
+file_sizes = Hash.new {|hash,key| hash[key] = []}
+
+Document.find_each do |document|
+  latest_edition = document.latest_edition
+  next unless latest_edition
+  next unless latest_edition.respond_to? :attachments
+  next unless latest_edition.attachments.any?
+  total_for_edition = latest_edition.attachments.map { |attachment|
+    attachment.file_size
+  }.inject(:+)
+  (file_sizes[latest_edition.class.name] ||= []) << total_for_edition
+end
+
+file_sizes.each do |document_type, sizes|
+  puts "#{document_type.underscore.humanize}: #{number_to_human_size(sizes.inject(:+))} (avg: #{number_to_human_size(sizes.inject(:+) / sizes.size)} from #{sizes.size} documents)"
 end
