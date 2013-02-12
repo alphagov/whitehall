445d404be7c6ec220bef28cc80e1cd2e79275dca
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-12 14:54:53 +0000

    Revert weird embedded appointments in people form

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -4,13 +4,13 @@ class Admin::PeopleController < Admin::BaseController
   end
 
   def new
-    @person = new_person
+    @person = Person.new
   end
 
   def create
     @person = Person.new(params[:person])
     if @person.save
-      redirect_to post_create_path_for(@person), notice: %{"#{@person.name}" created.}
+      redirect_to admin_people_url, notice: %{"#{@person.name}" created.}
     else
       render action: "new"
     end
@@ -36,19 +36,5 @@ class Admin::PeopleController < Admin::BaseController
     else
       redirect_to admin_people_path, alert: "Cannot destroy a person with appointments"
     end
-  end
-
-  private
-
-  def new_person
-    if params[:worldwide_office_id] && worldwide_office = WorldwideOffice.find(params[:worldwide_office_id])
-      Person.new(worldwide_office_appointment: WorldwideOfficeAppointment.new(worldwide_office: worldwide_office))
-    else
-      Person.new
-    end
-  end
-
-  def post_create_path_for(person)
-    person.worldwide_office ? people_admin_worldwide_office_url(person.worldwide_office) : admin_people_url
   end
 end
