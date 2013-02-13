5050c38ea7f5ef6cf6eb74f2a1d645716c9ca0ff
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-13 11:02:09 +0000

    Failing feature

--- features/step_definitions/unpublishing_published_documents_steps.rb
+++ features/step_definitions/unpublishing_published_documents_steps.rb
@@ -1,11 +1,13 @@
-When /^I unpublish the document "([^"]*)"$/ do |title|
-  edition = Edition.find_by_title!(title)
+When /^I unpublish the document because it was published in error$/ do
+  edition = Edition.last
   visit admin_edition_path(edition)
-  click_button "Unpublish"
+  click_button 'Unpublish'
+  select 'Published in error', from: 'Reason for unpublishing'
 end
 
-Then /^the document "([^"]*)" should not be visible to the public$/ do |title|
-  edition = Edition.find_by_title!(title)
+Then /^the document should not be visible to the public, with the reason why given$/ do
+  edition = Edition.last
   visit public_document_path(edition)
-  assert_equal 404, page.status_code
+  assert_equal 410, page.status_code
+  assert page.has_content?('The information on this page has been removed because it was published in error')
 end
--- features/unpublishing-published-documents.feature
+++ features/unpublishing-published-documents.feature
@@ -3,8 +3,8 @@ Feature: Unpublishing published documents
   I want to be able to revert published documents back to the draft state
   So that I can remove documents from the site that were published in error
 
-  Scenario: Unpublishing a published document
+  Scenario: Unpublishing a document published in error
     Given I am a GDS editor
     And a published document "Published by accident" exists
-    When I unpublish the document "Published by accident"
-    Then the document "Published by accident" should not be visible to the public
+    When I unpublish the document because it was published in error
+    Then the document should not be visible to the public, with the reason why given
