126ad12dd5dfbd8be7d44ff57f3d34897978e0ab
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-14 11:15:14 +0000

    Remove contentious unpublish reason

--- app/models/unpublishing_reason.rb
+++ app/models/unpublishing_reason.rb
@@ -4,7 +4,6 @@ class UnpublishingReason
   attr_accessor :id, :name
 
   PublishedInError  = create(id: 1, name: 'Published in error')
-  NoLongerCurrent   = create(id: 2, name: 'No longer current')
-  Duplicate         = create(id: 3, name: 'Duplicate of another page')
-  Superseded        = create(id: 4, name: 'Superseded by another page')
+  Duplicate         = create(id: 2, name: 'Duplicate of another page')
+  Superseded        = create(id: 3, name: 'Superseded by another page')
 end
