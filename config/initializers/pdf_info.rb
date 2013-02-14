198e60f2f6cdf8cc27a4740b679792d601f5c4e2
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-02-14 12:01:15 +0000

    Don't crash if pdfinfo binary not found
    It's only installed on the backend machines.

--- config/initializers/pdf_info.rb
+++ config/initializers/pdf_info.rb
@@ -1,4 +1,8 @@
 require 'pdf_info'
 possible_pdfinfo_binaries = ["/usr/bin/pdfinfo", "/usr/local/bin/pdfinfo"]
 pdfinfo_binary = possible_pdfinfo_binaries.find { |path| File.exist?(path) }
-PDFINFO_SERVICE = PdfInfo.new(pdfinfo_binary)
+if pdfinfo_binary
+  PDFINFO_SERVICE = PdfInfo.new(pdfinfo_binary)
+else
+  Rails.logger.warn "WARNING: pdfinfo binary not found. PDF Page counting will not work."
+end
