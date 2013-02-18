720e4193f041bf700f1b7487de74192b2bd5a402
Chris Roos <chris@seagul.co.uk>
2013-02-18 12:36:20 +0000

    Add govspeak markdown help to edition translation form
    I think it probably makes sense to include the markdown help here, but we can
    easily remove it if it's found to be a problem, particularly when it comes to
    right to left languages.

--- test/functional/admin/edition_translations_controller_test.rb
+++ test/functional/admin/edition_translations_controller_test.rb
@@ -52,6 +52,14 @@ class Admin::EditionTranslationsControllerTest < ActionController::TestCase
     assert_select '.english-body', text: edition.body
   end
 
+  view_test "edit shows the govspeak helper" do
+    edition = create(:edition)
+
+    get :edit, edition_id: edition, id: 'fr'
+
+    assert_select '#govspeak_help'
+  end
+
   test "update creates a translation for an edition that's yet to be published, and redirect back to the edition admin page" do
     edition = create(:draft_edition)
 
