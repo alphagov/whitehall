8999ffa6cd78cf4d696fa8a7553ee626f79b49c1
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-18 15:00:10 +0000

    Add more examples for size

--- lib/h_card.rb
+++ lib/h_card.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class HCard
   attr_reader :properties, :country_code
 
@@ -7,17 +9,32 @@ class HCard
   end
 
   def render
-"<div class=\"vcard\">\n  <div class=\"adr\">\n" <<
-"    #{hcard_property_tag('fn')}<br />\n" <<
-"    #{hcard_property_tag('street-address')}<br />\n" <<
-"    #{hcard_property_tag('postal-code')}<br />\n" <<
-"    #{hcard_property_tag('locality')}<br />\n" <<
-"    #{hcard_property_tag('region')}<br />\n" <<
-"    #{hcard_property_tag('country-name')}\n" <<
-"  </div>\n</div>"
+    "<div class=\"vcard\">\n<div class=\"adr\">\n#{address_tags}\n</div>\n</div>"
+  end
+
+  def address_tags
+    format_string = address_format_string
+    property_keys.each do |key|
+      format_string.gsub!(/\{\{#{key}\}\}/, hcard_property_tag(key))
+    end
+    format_string.gsub(/\n/, "<br />\n")
+  end
+
+  def property_keys
+    ['fn','street-address', 'postal-code', 'locality', 'region', 'country-name']
   end
 
   def hcard_property_tag(name)
-    "<span class=\"#{name}\">#{properties[name]}</span>"
+    properties[name] ? "<span class=\"#{name}\">#{properties[name]}</span>" : ""
+  end
+
+  def address_format_string
+    if country_code == 'GB'
+      "{{fn}}\n{{street-address}}\n{{locality}}\n{{region}}\n{{postal-code}}\n{{country-name}}"
+    elsif country_code == 'ES'
+      "{{fn}}\n{{street-address}}\n{{postal-code}} {{locality}} {{region}}\n{{country-name}}"
+    else
+      "〒{{postal-code}}\n{{region}}{{locality}}{{street-address}}\n{{fn}}\n{{country-name}}"
+    end
   end
 end
--- test/unit/h_card_test.rb
+++ test/unit/h_card_test.rb
@@ -1,33 +1,62 @@
+# encoding: utf-8
 require 'test_helper'
 
 class HCardTest < ActiveSupport::TestCase
 
-  test "renders addreses in UK format" do
-    h_card = HCard.new(hcard_fields, 'GB')
+  test "renders address in UK format" do
+    assert_equal gb_hcard, HCard.new(hcard_fields, 'GB').render
+  end
 
-    assert_equal gb_hcard, h_card.render
+  test "renders address in Spanish format" do
+    assert_equal es_hcard, HCard.new(hcard_fields, 'ES').render
+  end
+
+  test "renders address in Japanese format" do
+    assert_equal jp_hcard, HCard.new(hcard_fields, 'JP').render
   end
 
   def hcard_fields
-    { 'fn' => 'Department for Business, Innovation and Skills',
-      'street-address' => '1 Victoria Street',
-      'postal-code' => 'SW1H 0ET',
-      'locality' => 'London',
-      'region' => 'Greater London',
-      'country-name' => 'United Kingdom'
+    { 'fn' => 'Recipient',
+      'street-address' => 'Street',
+      'postal-code' => 'Postcode',
+      'locality' => 'Locality',
+      'region' => 'Region',
+      'country-name' => 'Country'
     }
   end
 
   def gb_hcard
 '<div class="vcard">
-  <div class="adr">
-    <span class="fn">Department for Business, Innovation and Skills</span><br />
-    <span class="street-address">1 Victoria Street</span><br />
-    <span class="postal-code">SW1H 0ET</span><br />
-    <span class="locality">London</span><br />
-    <span class="region">Greater London</span><br />
-    <span class="country-name">United Kingdom</span>
-  </div>
+<div class="adr">
+<span class="fn">Recipient</span><br />
+<span class="street-address">Street</span><br />
+<span class="locality">Locality</span><br />
+<span class="region">Region</span><br />
+<span class="postal-code">Postcode</span><br />
+<span class="country-name">Country</span>
+</div>
+</div>'
+  end
+
+  def es_hcard
+'<div class="vcard">
+<div class="adr">
+<span class="fn">Recipient</span><br />
+<span class="street-address">Street</span><br />
+<span class="postal-code">Postcode</span> <span class="locality">Locality</span> <span class="region">Region</span><br />
+<span class="country-name">Country</span>
+</div>
+</div>'
+  end
+
+  def jp_hcard
+'<div class="vcard">
+<div class="adr">
+〒<span class="postal-code">Postcode</span><br />
+<span class="region">Region</span><span class="locality">Locality</span><span class="street-address">Street</span><br />
+<span class="fn">Recipient</span><br />
+<span class="country-name">Country</span>
+</div>
 </div>'
   end
 end
