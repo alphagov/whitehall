973a01a947db7ca51a2eb37a2aecd3efcd9ecc28
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-02-19 17:32:27 +0000

    Improve layout of whips on ministers page

--- lib/whitehall/whip_organisation.rb
+++ lib/whitehall/whip_organisation.rb
@@ -14,8 +14,8 @@ module Whitehall
       all.find { |wo| wo.slug == slug }
     end
 
-    WhipsHouseOfCommons = create(id: 1, label: "Whips - House of Commons")
-    WhipsHouseofLords = create(id: 2, label: "Whips - House of Lords")
+    WhipsHouseOfCommons = create(id: 1, label: "House of Commons")
+    WhipsHouseofLords = create(id: 2, label: "House of Lords")
     JuniorLordsoftheTreasury = create(id: 3, label: "Junior Lords of the Treasury")
     AssistantWhips = create(id: 4, label: "Assistant Whips")
     BaronessAndLordsInWaiting = create(id: 5, label: "Baroness and Lords in Waiting")
