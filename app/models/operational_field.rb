ff9f8b98280737a27cf5ecd86d0c074bd19a470b
Matthew Ford <matt@bitzesty.com>
2013-02-21 16:42:17 +0000

    Index operational fields

--- app/models/operational_field.rb
+++ app/models/operational_field.rb
@@ -1,10 +1,21 @@
 class OperationalField < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+  include Searchable
+
   validates :name, presence: true, uniqueness: true
 
   has_many :fatality_notices
 
+  searchable title: :name,
+             link: :search_link,
+             content: :description
+
   extend FriendlyId
   friendly_id
+
+  def search_link
+    operational_field_path(slug)
+  end
 
   def published_fatality_notices
     fatality_notices.published
