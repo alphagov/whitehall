1cfacdfe256edb4ce938681b8886e62e8ad6a6d1
Chris Parsons <chris@thinkcodelearn.com>
2013-02-21 13:50:06 +0000

    Added corporate information pages to mappings

--- lib/whitehall/exporters/document_mappings.rb
+++ lib/whitehall/exporters/document_mappings.rb
@@ -119,5 +119,17 @@ class Whitehall::Exporters::DocumentMappings < Struct.new(:platform)
         edit_admin_organisation_url(organisation, host: admin_host, protocol: 'https'),
       )
     end
+
+    CorporateInformationPage.find_each do |page|
+      organisation = page.organisation
+      target << row(
+        organisation_corporate_information_page_url(page, organisation_id: organisation, host: host_name, protocol: 'https'),
+        admin_organisation_corporate_information_page_url(page, organisation_id: organisation, host: admin_host, protocol: 'https')
+      )
+      target << row(
+        organisation_corporate_information_page_url(page, organisation_id: organisation, host: host_name, protocol: 'https'),
+        edit_admin_organisation_corporate_information_page_url(page, organisation_id: organisation, host: admin_host, protocol: 'https')
+      )
+    end
   end
 end
