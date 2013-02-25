4f97d667ab7156c4bf45b034348ffcf7784eb719
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-25 09:30:35 +0000

    Don't rely on ivar in helper

--- app/helpers/admin/worldwide_organisations_helper.rb
+++ app/helpers/admin/worldwide_organisations_helper.rb
@@ -1,9 +1,9 @@
 module Admin::WorldwideOrganisationsHelper
-  def worldwide_organisation_tab_navigation(&block)
+  def worldwide_organisation_tab_navigation(worldwide_organisation, &block)
     tabs = {
-      "Details" => admin_worldwide_organisation_path(@worldwide_organisation),
-      "Offices" => offices_admin_worldwide_organisation_path(@worldwide_organisation),
-      "Social Media Accounts" => social_media_accounts_admin_worldwide_organisation_path(@worldwide_organisation)
+      "Details" => admin_worldwide_organisation_path(worldwide_organisation),
+      "Offices" => offices_admin_worldwide_organisation_path(worldwide_organisation),
+      "Social Media Accounts" => social_media_accounts_admin_worldwide_organisation_path(worldwide_organisation)
     }
 
     tab_navigation(tabs, &block)
