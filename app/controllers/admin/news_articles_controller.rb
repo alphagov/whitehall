b436e401dc44a5f152ccad6e23db5c029ba4b3a4
Chris Roos <chris@seagul.co.uk>
2013-02-25 16:53:59 +0000

    Simplify the news article and priority controllers
    I don't particularly think it's any worse to access
    `@edition.missing_translations` in the template rather than assigning it to
    `@missing_translations` in the controller. The advantage is that I *think*
    this might make it possible to test the translation functionality in the
    edition controller tests, rather than having to test it in each of the
    controllers that correspond to translatable models.

--- app/controllers/admin/news_articles_controller.rb
+++ app/controllers/admin/news_articles_controller.rb
@@ -1,10 +1,6 @@
 class Admin::NewsArticlesController < Admin::EditionsController
   before_filter :build_image, only: [:new, :edit]
 
-  def show
-    @missing_translations = @edition.missing_translations
-  end
-
   private
 
   def edition_class
