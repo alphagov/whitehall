aa3838392d96f719cea7feee0baa8c705db3dc04
Chris Roos <chris@seagul.co.uk>
2013-02-25 15:30:13 +0000

    Add functionality for translating News Articles
    Making it possible to translate News Articles is trivial (ensuring that
    `NewsArticle#translatable?` returns true and that we populate the
    `@missing_translations` array in the controller). Unfortunately, testing this
    in the same way that we're testing the translation functionality for Worldwide
    priorities has lead me to add 11 controller tests, which will definitely slow
    the test suite.
    
    My current plan is to add this duplication in the controllers and tests and to
    see what I can do about it later.

--- app/controllers/admin/news_articles_controller.rb
+++ app/controllers/admin/news_articles_controller.rb
@@ -1,6 +1,11 @@
 class Admin::NewsArticlesController < Admin::EditionsController
   before_filter :build_image, only: [:new, :edit]
 
+  def show
+    translated_locales = (@edition.translated_locales - [:en]).map {|l| Locale.new(l)}
+    @missing_translations = Locale.non_english - translated_locales
+  end
+
   private
 
   def edition_class
