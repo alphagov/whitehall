25b9788f42a86c092a791cc34ca6937ede33d05a
James Adam <james@lazyatom.com>
2013-02-25 16:19:59 +0000

    Always show the default locale first in available translation list.
    https://www.pivotaltracker.com/story/show/44789949

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -1,4 +1,9 @@
 module TranslationHelper
+  def sorted_locales(locale_codes)
+    codes = locale_codes.sort_by { |c| c.to_s }
+    codes.unshift(I18n.default_locale) if codes.delete(I18n.default_locale)
+  end
+
   def t_world_location(world_location)
     t("world_location.type.#{world_location.display_type_key}", count: 1)
   end
--- test/unit/helpers/translation_helper_test.rb
+++ test/unit/helpers/translation_helper_test.rb
@@ -9,4 +9,12 @@ class TranslationHelperTest < ActionView::TestCase
     I18n.backend.store_translations :en, {document: {type: {stub: {one: 'Stub'}}}}
     assert_equal "Stub", t_display_type(@document)
   end
+
+  test "sorted_locales returns default locale first" do
+    assert_equal I18n.default_locale, sorted_locales([:fr, :es, I18n.default_locale, :de]).first
+  end
+
+  test "sorted_locales returns other locals in alphabetically locale code order" do
+    assert_equal [:de, :es, :fr], sorted_locales([:fr, :es, I18n.default_locale, :de])[1..-1]
+  end
 end
