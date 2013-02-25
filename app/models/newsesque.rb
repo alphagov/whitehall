e214a2481dc5d6d09415b4719f71390d219c99e3
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-02-25 12:56:48 +0000

    Make sure Newsesque can be STI'd

--- app/models/newsesque.rb
+++ app/models/newsesque.rb
@@ -2,6 +2,10 @@ class Newsesque < Announcement
   include Edition::FactCheckable
   include Edition::FirstImagePulledOut
 
+  def self.sti_names
+    ([self] + descendants).map { |model| model.sti_name }
+  end
+
   def apply_any_extra_validations_when_converting_from_imported_to_draft
     class << self
       validates :first_published_at, presence: true
