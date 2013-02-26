511ce118467dfe9aec43b6b1b71bcdfb5f9a107d
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-02-26 11:52:18 +0000

    Migrate two topical events to correct slugs
    There's a corresponding pull request in Puppet to do the honours as far
    as redirecting goes.
    
    https://www.pivotaltracker.com/story/show/44803437
    https://www.pivotaltracker.com/story/show/44911919

---
+++ db/data_migration/20130226114348_migrate_topical_event_urls.rb
@@ -0,0 +1,2 @@
+TopicalEvent.find_by_slug("somalia-conference").update_column(:slug, "somalia-conference-2013")
+TopicalEvent.find_by_slug("friends-of-yemen").update_column(:slug, "friends-of-yemen-2013")
