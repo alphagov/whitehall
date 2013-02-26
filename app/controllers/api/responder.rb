a78952f5e34a2cd7fc2f3cc81532649af11c2b6c
Murray Steele <muz@h-lame.com>
2013-02-26 13:06:30 +0000

    Change Api::Responder to support links
    If links are present in the options they will be dropped into the _response_info json blob, and set as the Link HTTP header.
    
    We use LinkHeader to render, but have a slightly saner API for sending the links.
    
    It wants:
     [[url_1, [[attr_1, url_1_attr_1_value], [attr_2, url_1_attr_2_value]]],
      [url_2, [[attr_1, url_2_attr_1_value], [attr_2, url_2_attr_2_value]]]]
    
    We use:
     [[url_1, {attr_1 => url_1_attr_1_value, attr_2, url_1_attr_2_value}],
      [url_2, {attr_1 => url_2_attr_1_value, attr_2, url_2_attr_2_value}]]
    
    And handle the conversion for you.  URLs can't be a hash (you could specify the same URL for multiple links), but the attrs can.

--- app/controllers/api/responder.rb
+++ app/controllers/api/responder.rb
@@ -3,12 +3,43 @@ class Api::Responder < ActionController::Responder
     display resource.as_json.merge(response_info)
   end
 
+  def display(*args)
+    set_links_header
+    super
+  end
+
   private
 
   def response_info
+    response_info = {status: status_for_response_info}
+    response_info[:links] = links_for_response_info if link_header.links.any?
+
+    {_response_info: response_info}
+  end
+
+  def status_for_response_info
     status = @options[:status] || :ok
     code = Rack::Utils.status_code(status)
     text = Rack::Utils::HTTP_STATUS_CODES[code]
-    {_response_info: {status: text.downcase}}
+    text.downcase
+  end
+
+  def link_header
+    @link_hedaer ||= extract_link_header_from_options
+  end
+  def extract_link_header_from_options
+    links = @options[:links] || []
+    links_for_link_header = links.map {|(url, attrs)| [url, attrs.to_a]}
+    LinkHeader.new(links_for_link_header)
+  end
+
+  def set_links_header
+    controller.headers["Link"] = link_header.to_s if link_header.links.any?
+  end
+
+  def links_for_response_info
+    link_header.links.map { |link|
+      { href: link.href }.merge(link.attrs.symbolize_keys)
+    }
   end
 end
