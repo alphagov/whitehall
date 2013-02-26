d6ee1bcfe2e21b8899ebe7a21ce3a54b10212c69
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-26 14:42:53 +0000

    Speed tagging exposes associated document series

--- features/step_definitions/speed_tagging_steps.rb
+++ features/step_definitions/speed_tagging_steps.rb
@@ -37,3 +37,7 @@ end
 When /^I can only tag the (?:publication|news article) with "([^"]*)" once$/ do |label|
   assert page.has_css?("label.checkbox", text: /#{label}/, count: 1)
 end
+
+Then /^I should be able to select the document series "([^"]*)"$/ do |name|
+  select name, from: 'Document series'
+end
