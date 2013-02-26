4ea6dd5ad7dbdcd40d1edb79082ddc842cd0cfd8
Murray Steele <muz@h-lame.com>
2013-02-26 13:22:18 +0000

    Migration to social media accounts data for worldwide organisations
    For: https://www.pivotaltracker.com/story/show/45145601

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221151634) do
+ActiveRecord::Schema.define(:version => 20130226132051) do
 
   create_table "attachment_data", :force => true do |t|
     t.string   "carrierwave_file"
