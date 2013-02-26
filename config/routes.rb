d8b6a5c976918b2c1289652139e0069d6b9d33ec
Tekin Suleyman <tekin@tekin.co.uk>
2013-02-26 09:28:24 +0000

    Keep urls hackable
    There is little point having an additional "about" page for worldwide
    organisations - the show page is all the about anyone will ever need.
    To keep the urls hackable, we redirect /about to the show page.

--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,7 @@ Whitehall::Application.routes.draw do
     resources :worldwide_organisations, path: 'world/organisations', only: [:show], localised: true do
       resources :corporate_information_pages, only: [:show], path: 'about'
     end
+    match 'world/organisations/:organisation_id/about' => redirect('/world/organisations/%{organisation_id}')
 
     constraints(AdminRequest) do
       namespace :admin do
