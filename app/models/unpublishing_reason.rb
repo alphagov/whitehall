450826a75309ba7f829d46661b667fae9f2b3b52
Edd Sowden <e@e26.co.uk>
2013-02-27 09:04:31 +0000

    Add missing "it"s

--- app/models/unpublishing_reason.rb
+++ app/models/unpublishing_reason.rb
@@ -4,6 +4,6 @@ class UnpublishingReason
   attr_accessor :id, :name, :as_sentence
 
   PublishedInError  = create(id: 1, name: 'Published in error', as_sentence: 'it was published in error')
-  Duplicate         = create(id: 2, name: 'Duplicate of another page', as_sentence: 'is a duplicate of another page')
-  Superseded        = create(id: 3, name: 'Superseded by another page', as_sentence: 'has been superseded by another page')
+  Duplicate         = create(id: 2, name: 'Duplicate of another page', as_sentence: 'it is a duplicate of another page')
+  Superseded        = create(id: 3, name: 'Superseded by another page', as_sentence: 'it has been superseded by another page')
 end
