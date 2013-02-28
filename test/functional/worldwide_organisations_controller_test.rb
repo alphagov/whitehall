89c395e7a9634ab84bb8218aee62092ff91520aa
Murray Steele <muz@h-lame.com>
2013-02-28 15:19:02 +0000

    Redirect human urls the api endpoints if .json is requested
    Just for world locations (index, show) and worldwide_organisations (show).

--- test/functional/worldwide_organisations_controller_test.rb
+++ test/functional/worldwide_organisations_controller_test.rb
@@ -11,4 +11,10 @@ class WorldwideOrganisationsControllerTest < ActionController::TestCase
     get :show, id: organisation.id
     assert_equal organisation, assigns(:worldwide_organisation)
   end
+
+  test "show redirects to the api worldwide organisation endpoint when json is requested" do
+    organisation = create(:worldwide_organisation)
+    get :show, id: organisation.id, format: :json
+    assert_redirected_to api_worldwide_organisation_path(organisation, format: :json)
+  end
 end
