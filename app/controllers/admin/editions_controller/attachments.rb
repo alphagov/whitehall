757b80984bc16ff756b0a6fa93901ecd21008dfb
Tekin Suleyman <tekin@tekin.co.uk>
2013-03-01 14:11:40 +0000

    Wire up the controller to set the override.
    We set the flag for skipping file type verification based on the
    user's privileges.

--- app/controllers/admin/editions_controller/attachments.rb
+++ app/controllers/admin/editions_controller/attachments.rb
@@ -6,6 +6,7 @@ module Admin::EditionsController::Attachments
     before_filter :cope_with_attachment_action_params, only: [:update]
     before_filter :build_bulk_uploader, only: [:new, :edit]
     before_filter :extract_attachments_from_zip_file, only: [:update]
+    prepend_before_filter :skip_file_content_examination_for_privileged_users, only: [:create, :update]
   end
 
   def build_edition
@@ -41,6 +42,20 @@ module Admin::EditionsController::Attachments
       zip_file_to_attachments.manipulate_params!
     else
       params[:edition]['bulk_upload_zip_file_invalid'] = true
+    end
+  end
+
+  def skip_file_content_examination_for_privileged_users
+    return unless params[:edition] && params[:edition][:edition_attachments_attributes]
+
+    params[:edition][:edition_attachments_attributes].each do |_, edition_attachment_join_params|
+      if edition_attachment_join_params && edition_attachment_join_params[:attachment_attributes] && edition_attachment_join_params[:attachment_attributes][:attachment_data_attributes]
+        if current_user.can_upload_executable_attachments?
+          edition_attachment_join_params[:attachment_attributes][:attachment_data_attributes][:skip_file_content_examination] = true
+        else
+          edition_attachment_join_params[:attachment_attributes][:attachment_data_attributes][:skip_file_content_examination] = false
+        end
+      end
     end
   end
 end
