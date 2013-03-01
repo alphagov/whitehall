915cbba8316cebf166f2395b630a71c250a60f51
Chris Roos <chris@seagul.co.uk>
2013-03-01 11:23:22 +0000

    Make it easier to add further localised routing tests
    I wanted to add the `localised: true` option to the top level `people` route
    but wasn't sure where to test it. I removed all the `localised: true` options
    and these were pretty much the only tests that failed, so I figured that I'd
    make it easier to add tests for further localised routing changes.

--- test/integration/localised_routing_test.rb
+++ test/integration/localised_routing_test.rb
@@ -1,40 +1,32 @@
 require 'test_helper'
 
 class RoutingLocaleTest < ActionDispatch::IntegrationTest
-  setup do
-    @show_world_location = {controller: 'world_locations', action: 'show', id: 'spain', locale: 'en'}
-    @show_world_priority = {controller: 'worldwide_priorities', action: 'show', id: 'a-world-priority', locale: 'en'}
-  end
+  [
+    {
+      name:            'world locations',
+      expected_url:    '/government/world/spain',
+      routing_options: {controller: 'world_locations', action: 'show', id: 'spain', locale: 'en'}
+    },
+    {
+      name:            'world priorities',
+      expected_url:    '/government/priority/a-world-priority',
+      routing_options: {controller: 'worldwide_priorities', action: 'show', id: 'a-world-priority', locale: 'en'}
+    }
+  ].each do |test|
+    test "matches #{test[:name]} without locale or format" do
+      assert_routing test[:expected_url], test[:routing_options]
+    end
 
-  test 'matches world locations without locale or format' do
-    assert_routing '/government/world/spain', @show_world_location
-  end
+    test "matches #{test[:name]} with a format" do
+      assert_routing "#{test[:expected_url]}.json", test[:routing_options].merge(format: 'json')
+    end
 
-  test 'matches world locations with a format' do
-    assert_routing '/government/world/spain.json', @show_world_location.merge(format: 'json')
-  end
+    test "matches #{test[:name]} with a given locale" do
+      assert_routing "#{test[:expected_url]}.es", test[:routing_options].merge(locale: 'es')
+    end
 
-  test 'matches world locations with a given locale' do
-    assert_routing '/government/world/spain.es', @show_world_location.merge(locale: 'es')
-  end
-
-  test 'matches world locations with both a locale and format' do
-    assert_routing '/government/world/spain.es.json', @show_world_location.merge(locale: 'es', format: 'json')
-  end
-
-  test 'matches world priorities without locale or format' do
-    assert_routing '/government/priority/a-world-priority', @show_world_priority
-  end
-
-  test 'matches world priorities with a format' do
-    assert_routing '/government/priority/a-world-priority.json', @show_world_priority.merge(format: 'json')
-  end
-
-  test 'matches world priorities with a given locale' do
-    assert_routing '/government/priority/a-world-priority.es', @show_world_priority.merge(locale: 'es')
-  end
-
-  test 'matches world priorities with both a locale and format' do
-    assert_routing '/government/priority/a-world-priority.es.json', @show_world_priority.merge(locale: 'es', format: 'json')
+    test "matches #{test[:name]} with both a locale and format" do
+      assert_routing "#{test[:expected_url]}.es.json", test[:routing_options].merge(locale: 'es', format: 'json')
+    end
   end
 end
