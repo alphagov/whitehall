2a6652449c6434e54b99fbd600c34f112902e542
Chris Roos <chris@seagul.co.uk>
2013-03-01 12:49:51 +0000

    DRY up person_translations_controller
    We can use the methods provided by TranslatableModel to DRY up this
    controller.
    
    *NOTE* I've had to explicitly load the person in a before_filter. We were
    previously populating the `@person` instance variable with our call to
    `#person` in the `#index` action.

--- app/controllers/admin/person_translations_controller.rb
+++ app/controllers/admin/person_translations_controller.rb
@@ -1,14 +1,13 @@
 class Admin::PersonTranslationsController < Admin::BaseController
+  before_filter :load_person
   before_filter :load_translated_and_english_people, except: [:index]
   helper_method :translation_locale
 
   def index
-    @translated_locales = (person.translated_locales - [:en]).map {|l| Locale.new(l)}
-    @missing_locales = Locale.non_english - @translated_locales
   end
 
   def create
-    redirect_to edit_admin_person_translation_path(person, id: translation_locale)
+    redirect_to edit_admin_person_translation_path(@person, id: translation_locale)
   end
 
   def edit
@@ -32,19 +31,19 @@ class Admin::PersonTranslationsController < Admin::BaseController
   private
 
   def notice_message(action)
-    %{#{translation_locale.english_language_name} translation for "#{person.name}" #{action}.}
+    %{#{translation_locale.english_language_name} translation for "#{@person.name}" #{action}.}
   end
 
   def load_translated_and_english_people
-    @translated_person = LocalisedModel.new(person, translation_locale.code)
-    @english_person = LocalisedModel.new(person, :en)
+    @translated_person = LocalisedModel.new(@person, translation_locale.code)
+    @english_person = LocalisedModel.new(@person, :en)
   end
 
   def translation_locale
     @translation_locale ||= Locale.new(params[:translation_locale] || params[:id])
   end
 
-  def person
+  def load_person
     @person ||= Person.find(params[:person_id])
   end
 end
