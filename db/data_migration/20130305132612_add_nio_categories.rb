39cb079ec0672ef6308594897c2dd9590bdf5ccd
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-03-05 13:35:06 +0000

    Add NIO detailed guidance categories
    https://www.pivotaltracker.com/story/show/45587649

---
+++ db/data_migration/20130305132612_add_nio_categories.rb
@@ -0,0 +1,22 @@
+categories = [
+  {
+    title: "Gun and weapon controls in Northern Ireland",
+    description: "Administration of firearm licensing and authorisation for prohibited weapons in Northern Ireland.",
+    parent_tag: "justice/rights",
+    parent_title: "Your rights and the law"
+  },
+  {
+    title: "Visiting publicly owned buildings",
+    description: "Government buildings that are part of Britain's heritage: opening dates and times.",
+    parent_tag: "citizenship/government",
+    parent_title: "Living in the UK, government and democracy"
+  },
+]
+
+categories.each do |category_data|
+  category_data[:slug] = category_data[:title].parameterize
+  category = MainstreamCategory.create(category_data)
+  if category
+    puts "Mainstream category '#{category.title}' created"
+  end
+end
