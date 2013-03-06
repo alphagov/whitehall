3864e189241e934ce7b9d027d62b6a49f7eb9c53
Matthew Ford <matt@bitzesty.com>
2013-03-06 17:30:38 +0000

    Test should_allow_association_with_worldwide_priorities
    Conflicts:
    	test/functional/admin/case_studies_controller_test.rb

--- test/functional/admin/case_studies_controller_test.rb
+++ test/functional/admin/case_studies_controller_test.rb
@@ -19,4 +19,5 @@ class Admin::CaseStudiesControllerTest < ActionController::TestCase
   should_allow_setting_first_published_at_during_speed_tagging :case_study
   should_allow_association_with_worldwide_organisations :case_study
   should_allow_association_between_world_locations_and :case_study
+  should_allow_association_with_worldwide_priorities :case_study
 end
--- test/support/admin_edition_controller_test_helpers.rb
+++ test/support/admin_edition_controller_test_helpers.rb
@@ -1542,5 +1542,39 @@ module AdminEditionControllerTestHelpers
         assert_equal [], edition.reload.worldwide_organisations
       end
     end
+
+    def should_allow_association_with_worldwide_priorities(edition_type)
+      edition_class = class_for(edition_type)
+
+      view_test "new should display worldwide priorities field" do
+        get :new
+
+        assert_select "form#edition_new" do
+          assert_select "select[name*='edition[worldwide_priority_ids]']"
+        end
+      end
+
+      test "create should associate worldwide priorities with the edition" do
+        first_worldwide_priority = create(:worldwide_priority)
+        second_worldwide_priority = create(:worldwide_priority)
+        attributes = controller_attributes_for(edition_type)
+
+        post :create, edition: attributes.merge(
+          worldwide_priority_ids: [first_worldwide_priority.id, second_worldwide_priority.id]
+        )
+
+        assert edition = edition_class.last
+        assert_equal [first_worldwide_priority, second_worldwide_priority], edition.worldwide_priorities
+      end
+
+      test "update should remove all worldwide priorities if none specified at all" do
+        worldwide_priority = create(:worldwide_priority)
+        edition = create("draft_#{edition_type}", worldwide_priorities: [worldwide_priority])
+        put :update, id: edition, edition: controller_attributes_for_instance(edition).except(:worldwide_priority_ids)
+
+        assert_equal [], edition.reload.worldwide_priorities
+      end
+    end
+
   end
 end
