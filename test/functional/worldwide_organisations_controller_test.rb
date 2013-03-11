e2879e25595c83318fc183f052d7d14a8e61391a
Edd Sowden <e@e26.co.uk>
2013-03-11 09:45:49 +0000

    Don't escape links to world locations
    If there was more than one location the auto-escaping was clobbering
    this and turning them into html entites

--- test/functional/worldwide_organisations_controller_test.rb
+++ test/functional/worldwide_organisations_controller_test.rb
@@ -12,6 +12,17 @@ class WorldwideOrganisationsControllerTest < ActionController::TestCase
     assert_equal organisation, assigns(:worldwide_organisation)
   end
 
+  view_test "shows links to associated world locations" do
+    location_1 = create(:world_location)
+    location_2 = create(:world_location)
+    organisation = create(:worldwide_organisation, world_locations: [location_1, location_2])
+
+    get :show, id: organisation.id
+
+    assert_select "a[href='#{world_location_path(location_1)}']"
+    assert_select "a[href='#{world_location_path(location_2)}']"
+  end
+
   test "show redirects to the api worldwide organisation endpoint when json is requested" do
     organisation = create(:worldwide_organisation)
     get :show, id: organisation.id, format: :json
