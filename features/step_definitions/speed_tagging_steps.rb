9fef49a0d223288577ceca6b49d3e68fa763c5ef
Matthew Ford <matt@bitzesty.com>
2013-03-13 18:01:11 +0000

    Add title, summary and body to speed tagging
    https://www.pivotaltracker.com/story/show/42298931

--- features/step_definitions/speed_tagging_steps.rb
+++ features/step_definitions/speed_tagging_steps.rb
@@ -9,6 +9,11 @@ end
 Then /^I should have to select the publication sub\-type$/ do
   assert page.has_css?("select[id*=edition_publication_type_id]")
 end
+Then /^I should be able to edit the text content$/ do
+  assert page.has_css?("edition[title]")
+  assert page.has_css?("edition[summary]")
+  assert page.has_css?("edition[body]")
+end
 
 Then /^I should have to select the news article sub\-type$/ do
   assert page.has_css?("select[id*=edition_news_article_type_id]")
