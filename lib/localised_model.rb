af7052872c424768c0180f2d9421f07293a1bef4
Tekin Suleyman <tekin@tekin.co.uk>
2013-03-15 09:47:24 +0000

    LocalisedModel generates English errors
    Because of the way LocalisedModel scopes all method calls to a locale,
    validation errors end up generated in the locale, rather than in
    English. This overrides the error generation code such that error
    messages will always be in English.

--- lib/localised_model.rb
+++ lib/localised_model.rb
@@ -4,11 +4,21 @@ class LocalisedModel < BasicObject
   def initialize(model, locale)
     @model = model
     @fixed_locale = locale
+    @model.instance_variable_set(:@errors, ::ErrorsInEnglish.new(@model))
   end
 
   def method_missing(method, *args, &block)
     ::I18n.with_locale @fixed_locale do
       @model.__send__ method, *args, &block
+    end
+  end
+end
+
+class ErrorsInEnglish < ::ActiveModel::Errors
+
+  def generate_message(*args)
+    ::I18n.with_locale(:en) do
+      super
     end
   end
 end
