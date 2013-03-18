a66c344a24cdc47223eca7e05c92b56e1ddcd748
Murray Steele <muz@h-lame.com>
2013-03-18 15:42:24 +0000

    World writers and editors can't do anything with Fatality Notices

--- lib/whitehall/authority/rules/fatality_notice_rules.rb
+++ lib/whitehall/authority/rules/fatality_notice_rules.rb
@@ -2,7 +2,13 @@ module Whitehall::Authority::Rules
   class FatalityNoticeRules < Whitehall::Authority::Rules::EditionRules
     protected
     def actor_can_handle_fatalities?
-      actor.gds_editor? || (actor.organisation && actor.organisation.handles_fatalities?)
+      if actor.gds_editor?
+        true
+      elsif actor.world_editor? || actor.world_writer?
+        false
+      else
+        (actor.organisation && actor.organisation.handles_fatalities?)
+      end
     end
     def can_with_a_class?(action)
       actor_can_handle_fatalities? && super
