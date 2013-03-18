d0bd9626484e538213d6a97d97308746d0a4e26c
Murray Steele <muz@h-lame.com>
2013-03-18 11:40:23 +0000

    Add enforcer to admin controllers
    Provide enforce_permission! that renders a 403 when the current_user doesn't have permission to perform the requested action on the supplied subject.  Also can? that doe the same check but returns true or false and is exposed as a helper method for use in views.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -18,7 +18,24 @@ class Admin::BaseController < ApplicationController
     authorise_user!(User::Permissions::IMPORT)
   end
 
+  def can?(action, subject)
+    enforcer_for(subject).can?(action)
+  end
+  helper_method :can?
+
+  def enforce_permission!(action, subject)
+    unless can?(action, subject)
+      puts "You can't #{action} that #{subject.inspect}"
+      forbidden!
+    end
+  end
+
   private
+
+  def enforcer_for(subject)
+    actor = current_user || User.new
+    enforcer = Whitehall::Authority::Enforcer.new(actor, subject)
+  end
 
   def forbidden!
     render "admin/editions/forbidden", status: 403
