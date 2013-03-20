1f6c19c4bdeb6fc98c982fb0200aa3006f84a082
Matthew Ford <matt@bitzesty.com>
2013-03-20 18:30:28 +0000

    update locals

--- test/unit/board_member_role_test.rb
+++ test/unit/board_member_role_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class BoardMemberRoleTest < ActiveSupport::TestCase
-  test "can never be a cabinet member" do
+  test "should not be a cabinet member" do
     board_member_role = build(:board_member_role)
     refute board_member_role.cabinet_member?
   end
 
-  test "can never be a chief of the defence staff" do
+  test "should not be a chief of the defence staff" do
     board_member_role = build(:board_member_role)
     refute board_member_role.chief_of_the_defence_staff?
   end
--- test/unit/special_representative_role_test.rb
+++ test/unit/special_representative_role_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class SpecialRepresentativeRoleTest < ActiveSupport::TestCase
-  test "can never be a permanent secretary" do
+  test "should not be a permanent secretary" do
     role = build(:special_representative_role)
     refute role.permanent_secretary?
   end
 
-  test "can never be a cabinet member" do
+  test "should not be a cabinet member" do
     role = build(:special_representative_role)
     refute role.cabinet_member?
   end
 
-  test "can never be a chief of the defence staff" do
+  test "should not be a chief of the defence staff" do
     role = build(:special_representative_role)
     refute role.chief_of_the_defence_staff?
   end
