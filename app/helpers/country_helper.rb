9870ab42a85067f4d4562c125cc85e9cb54f8c1d
Murray Steele <muz@h-lame.com>
2013-03-21 11:10:13 +0000

    Rename world_location_news_feature? to world_feature?
    We'll be using it to control revealing all the world stuff so it makes sense that it's a more generic name.

--- app/helpers/country_helper.rb
+++ app/helpers/country_helper.rb
@@ -9,7 +9,7 @@ module CountryHelper
 
   def list_of_links_to_world_locations(world_locations)
     world_locations.map { |world_location|
-      if Whitehall.world_location_news_feature?
+      if Whitehall.world_feature?
         link_to(world_location.name, world_location, class: dom_class(world_location), id: dom_id(world_location))
       else
         content_tag_for(:span, world_location) { world_location.name }
@@ -19,7 +19,7 @@ module CountryHelper
 
   def list_of_links_to_worldwide_organisations(worldwide_organisations)
     worldwide_organisations.map { |worldwide_organisation|
-      if Whitehall.world_location_news_feature?
+      if Whitehall.world_feature?
         link_to(worldwide_organisation.name, worldwide_organisation, class: dom_class(worldwide_organisation), id: dom_id(worldwide_organisation))
       else
         content_tag_for(:span, worldwide_organisation) { worldwide_organisation.name }
