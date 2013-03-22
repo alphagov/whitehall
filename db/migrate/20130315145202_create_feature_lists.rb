11bf68d6f86e9d3b055f98ba257e41b51a8ba13c
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-03-22 14:57:44 +0000

    Avoid duplicate featurings when migrating
    Duplicates have occured when publishing a new edition of a document:
    
    https://www.pivotaltracker.com/story/show/46707577
    
    This avoids replicating the duplicates

--- db/migrate/20130315145202_create_feature_lists.rb
+++ db/migrate/20130315145202_create_feature_lists.rb
@@ -52,6 +52,7 @@ class CreateFeatureLists < ActiveRecord::Migration
     WorldLocation.find_each do |wl|
       wl.feature_list = ::FeatureList.create!
       wl.featured_edition_world_locations.each do |ewl|
+        next if wl.feature_list.features.where(document_id: ewl.edition.document_id).any?
         new_feature = wl.feature_list.features.create!(
           document_id: ewl.edition.document_id,
           carrierwave_image: ewl.image.carrierwave_image,
