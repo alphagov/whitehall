8abec355d0e9bf2aa5079f3d16ca8e0c8784a938
Murray Steele <muz@h-lame.com>
2013-03-26 12:09:33 +0000

    Limit mainstream links for orgs & world locs to 5
    Rather than enforcing this in validations, we only show the first 5 (starting with the oldest) on the frontend and show a warning on the admin to let folk know this will happen.  This means we can relax or tighten the rule later without destroying any data.

--- app/models/mainstream_link.rb
+++ app/models/mainstream_link.rb
@@ -1,4 +1,14 @@
 class MainstreamLink < ActiveRecord::Base
   validates :url, :title, presence: true
   validates :url, format: URI::regexp(%w(http https))
+
+  DEFAULT_INITIAL_SET_SIZE = 5
+
+  def self.in_creation_order
+    order(:created_at)
+  end
+
+  def self.only_the_initial_set(set_size = MainstreamLink::DEFAULT_INITIAL_SET_SIZE)
+    in_creation_order.limit(set_size)
+  end
 end
--- test/unit/mainstream_link_test.rb
+++ test/unit/mainstream_link_test.rb
@@ -15,4 +15,15 @@ class MainstreamLinkTest < ActiveSupport::TestCase
     link = build(:mainstream_link, url: "not a link")
     refute link.valid?
   end
+
+  test 'only_the_initial_set retreives the first 5 in creation order' do
+    link_1 = create(:mainstream_link, created_at: 2.days.ago)
+    link_2 = create(:mainstream_link, created_at: 12.days.ago)
+    link_3 = create(:mainstream_link, created_at: 1.hour.ago)
+    link_4 = create(:mainstream_link, created_at: 2.hours.ago)
+    link_5 = create(:mainstream_link, created_at: 20.minutes.ago)
+    link_6 = create(:mainstream_link, created_at: 2.years.ago)
+
+    assert_equal [link_6, link_2, link_1, link_4, link_3], MainstreamLink.only_the_initial_set
+  end
 end
