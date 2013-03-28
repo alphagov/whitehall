3ca96b3d1349ed417f56941870ed9f08a23b459e
Matthew Ford <matt@bitzesty.com>
2013-03-28 19:26:53 +0000

    remove redundant gov_delivery_url

--- lib/whitehall.rb
+++ lib/whitehall.rb
@@ -127,10 +127,6 @@ module Whitehall
       Rails.root.join('clean-uploads').realpath
     end
 
-    def gov_delivery_url
-      "https://whitehall-admin.preview.alphagov.co.uk" #TODO Replace with plek for govdelivery
-    end
-
     def government_search_index_name
       "government"
     end
