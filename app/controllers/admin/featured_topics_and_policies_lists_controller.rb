6c6b85e9fb0aad37971c05075aef2e339f1d43ca
Murray Steele <muz@h-lame.com>
2013-04-02 17:04:19 +0100

    Present a sortable interface for selecting featured items
    The user gets a choice of topic or policy and choosing will hide the dropdown for the choice they didn't make.  They can also drag the boxes around to reorder.  The form will always present one empty featured item so you can add new ones.
    
    Because of a bug in chosen (https://github.com/harvesthq/chosen/issues/92) we have to only hide the dropdowns once they are all ready or the one hidden by default will end up being rendered incredibly narrow.

--- app/controllers/admin/featured_topics_and_policies_lists_controller.rb
+++ app/controllers/admin/featured_topics_and_policies_lists_controller.rb
@@ -3,22 +3,49 @@ class Admin::FeaturedTopicsAndPoliciesListsController < Admin::BaseController
   before_filter :fetch_featured_topics_and_policies_list
 
   def show
+    fetch_topics_and_policies
+    @featured_topics_and_policies_list.featured_items.build(item_type: 'Topic')
   end
 
   def update
-    if @featured_topics_and_policies_list.update_attributes(params[:featured_topics_and_policies_list])
+    params_with_item_ids = prepare_item_id_params(params[:featured_topics_and_policies_list])
+    if @featured_topics_and_policies_list.update_attributes(params_with_item_ids)
       redirect_to admin_organisation_featured_topics_and_policies_list_path(@organisation), notice: "Featured topics and policies for #{@organisation.name} updated"
     else
+      fetch_topics_and_policies
       render :show
     end
   end
 
   private
+  def fetch_topics_and_policies
+    @topics = Topic.all
+    @policies = Policy.latest_edition.with_translations.includes(:document)
+  end
+
   def load_organisation
     @organisation = Organisation.find(params[:organisation_id])
   end
 
   def fetch_featured_topics_and_policies_list
     @featured_topics_and_policies_list = @organisation.featured_topics_and_policies_list || @organisation.build_featured_topics_and_policies_list
+  end
+
+  def prepare_item_id_params(feature_list_params)
+    (feature_list_params['featured_items_attributes'] || {}).each do |k, v|
+      case v['item_type']
+      when 'Topic'
+        v.delete('document_id')
+        v['item_id'] = v.delete('topic_id')
+      when 'Document'
+        v.delete('topic_id')
+        v['item_id'] = v.delete('document_id')
+      else
+        v.delete('topic_id')
+        v.delete('document_id')
+        v.delete('item_id')
+      end
+    end
+    feature_list_params
   end
 end
--- test/functional/admin/featured_topics_and_policies_lists_controller_test.rb
+++ test/functional/admin/featured_topics_and_policies_lists_controller_test.rb
@@ -25,6 +25,17 @@ class Admin::FeaturedTopicsAndPoliciesListsControllerTest < ActionController::Te
     refute list.persisted?
   end
 
+  test 'GET show will add an unsaved featured_item of the Topic type to the featured topics and policies list' do
+    org = create(:organisation)
+
+    get :show, organisation_id: org
+    list = assigns(:featured_topics_and_policies_list)
+    items = list.featured_items
+    assert_equal 1, items.size
+    refute items.first.persisted?
+    assert_equal 'Topic', items.first.item_type
+  end
+
   test "PUT update will save the supplied changes to the featured topics and policies list for the supplied org" do
     org = create(:organisation)
     featured_topics_and_policies_list = create(:featured_topics_and_policies_list, organisation: org)
@@ -44,6 +55,40 @@ class Admin::FeaturedTopicsAndPoliciesListsControllerTest < ActionController::Te
     assert_equal org, list.organisation
     assert list.persisted?
     assert_equal 'Wooo', list.summary
+  end
+
+  test "PUT update will save featured items, using item_type to choose between topic_id and document_id params" do
+    org = create(:organisation)
+    t = create(:topic)
+    p = create(:policy, :with_document)
+    featured_topics_and_policies_list = create(:featured_topics_and_policies_list, organisation: org)
+
+    put :update, organisation_id: org, featured_topics_and_policies_list: {
+      featured_items_attributes: {
+        :"0" => {
+          item_type: 'Topic',
+          topic_id: t.id.to_s,
+          document_id: p.document.id.to_s,
+          ordering: '1'
+        },
+        :"1" => {
+          item_type: 'Document',
+          topic_id: t.id.to_s,
+          document_id: p.document.id.to_s,
+          ordering: '2'
+        }
+      }
+    }
+
+    list = assigns(:featured_topics_and_policies_list)
+    items = list.featured_items
+    featured_topic = items.detect { |i| i.ordering == 1 }
+    assert_equal t, featured_topic.item
+    assert featured_topic.persisted?
+
+    featured_policy = items.detect { |i| i.ordering == 2 }
+    assert_equal p.document, featured_policy.item
+    assert featured_policy.persisted?
   end
 
   test "PUT update that fails will render the show template" do
