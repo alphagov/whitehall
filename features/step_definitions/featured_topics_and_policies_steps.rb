af2b998a006571d6c1a3d2619a3b1cf42e708809
Murray Steele <muz@h-lame.com>
2013-04-02 17:51:22 +0100

    Steps for featuring topics and policies on an exec office

--- features/step_definitions/featured_topics_and_policies_steps.rb
+++ features/step_definitions/featured_topics_and_policies_steps.rb
@@ -8,7 +8,36 @@ When /^I write some copy to describe the featured topics and policies for the ex
 end
 
 When /^I feature some topics and policies for the executive office in a specific order$/ do
-  pending
+  topic_1 = create(:topic)
+  policy_1 = create(:published_policy)
+  policy_2 = create(:published_policy)
+
+  visit admin_organisation_path(@the_featuring_org)
+  click_on 'Featured topics and policies'
+
+  within('.featured-topics-and-policies-items .well:nth-child(1)') do
+    choose 'Topic'
+    # no better way to identify the select than by direct name
+    select topic_1.name, from: 'featured_topics_and_policies_list[featured_items_attributes][0][topic_id]'
+    fill_in 'Ordering', with: '1'
+  end
+  click_on 'Save'
+
+  within('.featured-topics-and-policies-items .well:nth-child(2)') do
+    choose 'Policy'
+    select policy_2.title, from: 'featured_topics_and_policies_list[featured_items_attributes][1][document_id]'
+    fill_in 'Ordering', with: '2'
+  end
+  click_on 'Save'
+
+  within('.featured-topics-and-policies-items .well:nth-child(3)') do
+    choose 'Policy'
+    select policy_1.title, from: 'featured_topics_and_policies_list[featured_items_attributes][2][document_id]'
+    fill_in 'Ordering', with: '3'
+  end
+  click_on 'Save'
+  
+  @the_featured_items = [topic_1, policy_2, policy_1]
 end
 
 Then /^I see my copy on the executive office page$/ do
@@ -20,7 +49,13 @@ Then /^I see my copy on the executive office page$/ do
 end
 
 Then /^the featured topics and policies are in my specified order$/ do
-  pending
+  visit_organisation @the_featuring_org.name
+
+  within '#featured-topics-and-policies' do
+    @the_featured_items.each.with_index do |item, idx|
+      assert page.has_css?("li:nth-child(#{idx + 1})", text: item.respond_to?(:name) ? item.name : item.title)
+    end
+  end
 end
 
 Then /^I am invited to click through to see all the policies the executive office is involved with$/ do
