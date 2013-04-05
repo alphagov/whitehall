54c9aeccce84b656f215ead0351f32b340171e97
Matthew Ford <matt@bitzesty.com>
2013-04-05 13:04:13 +0100

    Ordering is handled in the admin interface and db

--- test/unit/ministerial_role_test.rb
+++ test/unit/ministerial_role_test.rb
@@ -166,24 +166,4 @@ class MinisterialRoleTest < ActiveSupport::TestCase
                   'format' => 'minister',
                   'description' => ''}, results[3])
   end
-
-  test 'prime minister should have seniority 1' do
-    pm = create(:ministerial_role, name: 'Prime Minister')
-    assert_equal 1, pm.seniority
-  end
-
-  test 'deputy prime minister should have seniority 2' do
-    dpm = create(:ministerial_role, name: 'Deputy Prime Minister')
-    assert_equal 2, dpm.seniority
-  end
-
-  test 'first secretaries should have seniority 3' do
-    fs = create(:ministerial_role, name: 'First Secretary of State for Caprid Husbandry')
-    assert_equal 3, fs.seniority
-  end
-
-  test 'other ministers should have seniority 4' do
-    role = create(:ministerial_role)
-    assert_equal 4, role.seniority
-  end
 end
