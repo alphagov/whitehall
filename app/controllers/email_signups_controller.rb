7fbc609a332b8cd7b58316e7ad970020cce3f2bc
Murray Steele <muz@h-lame.com>
2013-04-05 17:53:56 +0100

    Pull down translations, don't fetch world locations
    World locations aren't used and the translations speed up the page.

--- app/controllers/email_signups_controller.rb
+++ app/controllers/email_signups_controller.rb
@@ -5,9 +5,8 @@ class EmailSignupsController < PublicFacingController
     @classifications = Classification.order(:name).where("(type = 'Topic' and published_policies_count <> 0) or (type = 'TopicalEvent')").alphabetical
     ministerial_department_type = OrganisationType.find_by_name('Ministerial department')
     sub_organisation_type = OrganisationType.find_by_name('Sub-organisation')
-    @live_ministerial_departments = Organisation.where("organisation_type_id = ? AND govuk_status ='live'", ministerial_department_type)
-    @live_other_departments = Organisation.where("organisation_type_id NOT IN (?,?) AND govuk_status='live'", ministerial_department_type, sub_organisation_type)
-    @world_locations = WorldLocation.all_by_type
+    @live_ministerial_departments = Organisation.with_translations.where("organisation_type_id = ? AND govuk_status ='live'", ministerial_department_type)
+    @live_other_departments = Organisation.with_translations.where("organisation_type_id NOT IN (?,?) AND govuk_status='live'", ministerial_department_type, sub_organisation_type)
     @email_signup = EmailSignup.new
   end
 
