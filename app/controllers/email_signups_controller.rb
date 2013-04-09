223def507e3f4677f05769e281e7b0bef3efa768
Murray Steele <muz@h-lame.com>
2013-04-09 17:17:53 +0100

    Wrap it all up in the controller
    If the email signup is valid then we take the first (and only the first) alert and redirect the user to the new signup url for that alert using the EmailSignup::GovUkDeliveryRedirectUrlExtractor.

--- app/controllers/email_signups_controller.rb
+++ app/controllers/email_signups_controller.rb
@@ -2,13 +2,62 @@ class EmailSignupsController < PublicFacingController
   layout 'frontend'
 
   def show
+    fetch_topics
+    fetch_organisations
+    fetch_document_types
+    @email_signup = EmailSignup.new
+    @email_signup.alerts = [@email_signup.build_alert]
+  end
+
+  def create
+    @email_signup = EmailSignup.new
+    @email_signup.alerts = extract_alerts_params
+    if @email_signup.valid?
+      redirector = EmailSignup::GovUkDeliveryRedirectUrlExtractor.new(@email_signup.alerts.first)
+      redirect_to redirector.redirect_url
+    else
+      fetch_topics
+      fetch_organisations
+      fetch_document_types
+      render :show
+    end
+  end
+
+  protected
+  def fetch_topics
     @classifications = EmailSignup.valid_topics
+  end
+  def fetch_organisations
     orgs_by_type = EmailSignup.valid_organisations_by_type
     @live_ministerial_departments = orgs_by_type[:ministerial]
     @live_other_departments = orgs_by_type[:other]
+  end
+  def fetch_document_types
     @document_types = EmailSignup.valid_document_types_by_type
-    @email_signup = EmailSignup.new
-    @email_signup.alerts = [@email_signup.build_alert]
+  end
+
+  def extract_alerts_params
+    alerts_params = (params[:email_signup] || {})[:alerts]
+    case alerts_params
+    when Array
+      alerts_params
+    when Hash
+      convert_alerts_params_from_hash_to_array(alerts_params)
+    else
+      []
+    end
+  end
+
+  # rails will turn param called a[0][b] into {a: { "0": { b: ...} } }
+  # we want [{b: ...}], so if all the keys are numeric then we'll use
+  # the values, otherwise, assume it's a hash for a single element and
+  # put it in an array on it's own
+  def convert_alerts_params_from_hash_to_array(alerts_params)
+    if alerts_params.keys.all? { |k| k =~ /\A\d+\Z/ }
+      alerts_params.values
+    else
+      [alerts_params]
+    end
   end
 
 end
