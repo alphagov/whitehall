2dc01d53cb04a1cc1d25cab7fb591eebc6a35f31
Edd Sowden <e@e26.co.uk>
2013-04-10 10:57:42 +0000

    Add tests for elements on organistion pages
    Tried to split them as best I could I think these are all the tests
    which should be shared between both executive offices and normal
    organisations.

--- test/functional/organisations_controller_test.rb
+++ test/functional/organisations_controller_test.rb
@@ -4,8 +4,11 @@ class OrganisationsControllerTest < ActionController::TestCase
   include ActionDispatch::Routing::UrlFor
   include PublicDocumentRoutesHelper
   include FilterRoutesHelper
+  include OrganisationControllerTestHelpers
 
   should_be_a_public_facing_controller
+  should_display_organisation_page_elements_for(:organisation)
+  should_display_organisation_page_elements_for(:executive_office)
 
   view_test "should display a list of organisations" do
     ministerial_org_type = create(:ministerial_organisation_type)
@@ -192,51 +195,20 @@ class OrganisationsControllerTest < ActionController::TestCase
     end
   end
 
-  test "shows primary featured editions in ordering defined by association" do
-    organisation = create(:organisation)
-    news_article = create(:published_news_article)
-    policy = create(:published_policy)
-    create(:featured_edition_organisation, edition: news_article, organisation: organisation, ordering: 1)
-    create(:featured_edition_organisation, edition: policy, organisation: organisation, ordering: 0)
-
-    get :show, id: organisation
-
-    assert_equal [policy, news_article], assigns(:featured_editions).collect(&:model)
-  end
-
-  view_test "shows a maximum of 6 featured editions" do
-    organisation = create(:organisation)
-    editions = []
-    7.times do |i|
-      edition = create(:published_news_article, first_published_at: i.days.ago)
-      editions << create(:featured_edition_organisation, edition: edition, organisation: organisation)
-    end
-
-    get :show, id: organisation
-
-    assert_select_object editions[0].edition do
-      assert_select "img[src$='#{editions[0].image.file.url(:s630)}'][alt=?]", editions[0].alt_text
-    end
-    editions[1...6].each do |edition|
-      assert_select_object edition.edition do
-        assert_select "img[src$='#{edition.image.file.url(:s300)}'][alt=?]", edition.alt_text
-      end
-    end
-    refute_select_object editions.last.edition
-  end
-
-  view_test "should not display an empty featured editions section" do
-    organisation = create(:organisation)
-    get :show, id: organisation
-    refute_select "#featured-documents article"
-  end
-
   test "showing a live organisation renders the show template" do
     organisation = create(:organisation, govuk_status: 'live')
 
     get :show, id: organisation
 
     assert_template 'show'
+  end
+
+  test "showing a live executive office organisation renders the show external-office template" do
+    organisation = create(:executive_office, govuk_status: 'live')
+
+    get :show, id: organisation
+
+    assert_template 'show-executive-office'
   end
 
   test "showing a joining organisation renders the external template" do
@@ -513,43 +485,6 @@ class OrganisationsControllerTest < ActionController::TestCase
     end
   end
 
-  view_test "presents the contact details of the organisation using hcard" do
-    ministerial_department = create(:organisation_type, name: "Ministerial Department")
-    organisation = create(:organisation, organisation_type: ministerial_department,
-      name: "Ministry of Pomp"
-    )
-    organisation.contacts.create!(
-      title: "Main",
-      recipient: "Ministry of Pomp",
-      email: "pomp@gov.uk",
-      contact_form_url: "http://pomp.gov.uk/contact",
-      street_address: "1 Smashing Place, London", postal_code: "LO1 8DN",
-      contact_numbers_attributes: [
-        { label: "Helpline", number: "02079460000" },
-        { label: "Fax", number: "02079460001" }
-      ],
-      country: create(:world_location, iso2: 'GB')
-    )
-    get :show, id: organisation
-
-    assert_select ".vcard" do
-      assert_select ".fn", "Ministry of Pomp"
-      assert_select ".adr" do
-        assert_select ".street-address", "1 Smashing Place, London"
-        assert_select ".postal-code", "LO1 8DN"
-      end
-      assert_select ".tel", /02079460000/ do
-        assert_select ".type", "Helpline"
-      end
-      assert_select ".email", /pomp@gov\.uk/ do
-        assert_select ".type", "Email"
-      end
-      assert_select ".contact_form_url" do
-        assert_select "a[href=http://pomp.gov.uk/contact]"
-      end
-    end
-  end
-
   view_test "should display sub-organisations" do
     organisation = create(:organisation)
     sub_organisation = create(:sub_organisation, parent_organisations: [organisation])
@@ -559,22 +494,6 @@ class OrganisationsControllerTest < ActionController::TestCase
     assert_select ".sub_organisations" do
       assert_select_object(sub_organisation)
     end
-  end
-
-  view_test 'show has atom feed autodiscovery link' do
-    organisation = create(:organisation)
-
-    get :show, id: organisation
-
-    assert_select_autodiscovery_link organisation_url(organisation, format: "atom")
-  end
-
-  view_test 'show includes a link to the atom feed' do
-    organisation = create(:organisation)
-
-    get :show, id: organisation
-
-    assert_select "a.feed[href=?]", organisation_url(organisation, format: :atom)
   end
 
   view_test "show generates an atom feed with entries for latest activity" do
@@ -840,50 +759,6 @@ class OrganisationsControllerTest < ActionController::TestCase
     end
   end
 
-  view_test "shows 3 most recently published editions associated with organisation" do
-    # different edition types sort on different attributes
-    editions = [create(:published_policy, first_published_at: 1.days.ago),
-               create(:published_publication, publication_date: 2.days.ago),
-               create(:published_consultation, first_published_at: 3.days.ago),
-               create(:published_speech, delivered_on: 4.days.ago)]
-
-    organisation = create(:organisation, editions: editions)
-    get :show, id: organisation
-
-    editions[0,3].each do |edition|
-      assert_select_prefix_object edition, :recent
-    end
-    refute_select_prefix_object editions[3], :recent
-  end
-
-  view_test "should not show most recently published editions when there are none" do
-    organisation = create(:organisation, editions: [])
-    get :show, id: organisation
-
-    refute_select "h1", text: "Recently updated"
-  end
-
-  view_test "should show list of links to social media accounts" do
-    twitter = create(:social_media_service, name: "Twitter")
-    flickr = create(:social_media_service, name: "Flickr")
-    twitter_account = create(:social_media_account, social_media_service: twitter, url: "https://twitter.com/#!/bisgovuk")
-    flickr_account = create(:social_media_account, social_media_service: flickr, url: "http://www.flickr.com/photos/bisgovuk")
-    organisation = create(:organisation, social_media_accounts: [twitter_account, flickr_account])
-
-    get :show, id: organisation
-
-    assert_select_object twitter_account
-    assert_select_object flickr_account
-  end
-
-  view_test "should not show list of links to social media accounts if there are none" do
-    organisation = create(:organisation, social_media_accounts: [])
-
-    get :show, id: organisation
-
-    refute_select ".social-media-accounts"
-  end
-
   view_test "should show mainstream category links if there are some" do
     organisation = create(:organisation)
     link = create(:organisation_mainstream_link, organisation: organisation)
@@ -903,14 +778,6 @@ class OrganisationsControllerTest < ActionController::TestCase
     get :show, id: sub_organisation
 
     refute_select "a[href='#{link.mainstream_link.url}']", text: link.mainstream_link.title
-  end
-
-  view_test 'show hass a link to govdelivery if one exists' do
-    organisation = create(:organisation, govdelivery_url: 'http://my-govdelivery-url.com')
-
-    get :show, id: organisation
-
-    assert_select ".govdelivery[href='http://my-govdelivery-url.com']"
   end
 
   view_test 'show lists only the 5 oldest mainstream links' do
