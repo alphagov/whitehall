1ba097f20881316c44a1733ba4ee285b13da014b
Matthew Ford <matt@bitzesty.com>
2013-04-10 11:58:51 +0100

    Add test for WorldLocationNewsArticlePresenterTest

---
+++ test/unit/presenters/world_location_news_article_presenter_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class WorldLocationNewsArticlePresenterTest < ActionView::TestCase
+
+  setup do
+    Draper::ViewContext.current = @controller.view_context
+  end
+
+  test "lead_image_path returns the world wide organisation default image" do
+    image = create(:default_news_organisation_image_data)
+    worldwide_organisation = create(:worldwide_organisation, default_news_image: image)
+    world_location_news_article = create(:world_location_news_article, worldwide_organisations: [worldwide_organisation])
+    presenter = WorldLocationNewsArticlePresenter.decorate(world_location_news_article)
+    assert_match worldwide_organisation.default_news_image.file.url(:s300), presenter.lead_image_path
+  end
+end
