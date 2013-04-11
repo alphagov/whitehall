fb90b8ff9690d364ff3417db82c1c177b2647d9e
Tekin Suleyman <tekin@tekin.co.uk>
2013-04-11 13:23:53 +0100

    Remove unused model

--- app/models/promotional_section_layout.rb
+++
@@ -1,9 +0,0 @@
-class PromotionalSectionLayout
-  include ActiveRecordLikeInterface
-
-  attr_accessor :id, :name, :columns, :title
-
-  TwoColumn           = create(id: 1, name: 'Two column', columns: 2, title: false)
-  ThreeColumn         = create(id: 2, name: 'Three column', columns: 3, title: false)
-  ThreeColumnGrouped  = create(id: 3, name: 'Three column grouped', columns: 3, title: true)
-end
