926df18dd3c98e3e96847668b89f5a39fdc2e4da
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-04-11 12:57:33 +0100

    Make this more clear with parentheses

--- config/initializers/public_host.rb
+++ config/initializers/public_host.rb
@@ -1,1 +1,1 @@
-Whitehall.public_host = ENV['FACTER_govuk_platform'] == 'production' ? 'www.gov.uk' : 'www.dev.gov.uk'
+Whitehall.public_host = (ENV['FACTER_govuk_platform'] == 'production' ? 'www.gov.uk' : 'www.dev.gov.uk')
