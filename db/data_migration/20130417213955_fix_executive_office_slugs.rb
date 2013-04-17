b4c77a364cc5325e13721790cbe0d7719b9ba06e
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-04-17 21:44:45 +0100

    Move the slugs for No10 and DPMO
    Separate Puppet pull request to come with redirection. This needs to be
    released first.
    
    https://www.pivotaltracker.com/story/show/47364021

---
+++ db/data_migration/20130417213955_fix_executive_office_slugs.rb
@@ -0,0 +1,2 @@
+Organisation.find_by_slug('the-prime-ministers-office-number-10').update_attribute(:slug, 'prime-ministers-office-10-downing-street')
+Organisation.find_by_slug('the-deputy-prime-ministers-office').update_attribute(:slug, 'deputy-prime-ministers-office')
