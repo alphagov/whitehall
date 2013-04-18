98e9828eb2b36ebd1b72cf5993aecf05d1066be7
Murray Steele <muz@h-lame.com>
2013-04-18 12:06:34 +0100

    Placeholder feature to sketch out what we want

---
+++ features/step_definitions/take_part_pages_steps.rb
@@ -0,0 +1,27 @@
+Given /^there are some take part pages for the get involved section$/ do
+  pending
+end
+
+When /^I create a new take part page called "([^"]*)"$/ do |arg1|
+  pending
+end
+
+When /^I reorder the take part pages to highlight my new page$/ do
+  pending
+end
+
+Then /^I see the take part pages in my specified order on the frontend get involved section$/ do
+  pending
+end
+
+Then /^I can click through to read all about my new page$/ do
+  pending
+end
+
+When /^I remove one of the take part pages because it's not something we want to promote$/ do
+  pending
+end
+
+Then /^the removed take part page is no longer displayed on the frontend get involved section$/ do
+  pending
+end
