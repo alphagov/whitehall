863b76c3b26b5ad9e63b5f54131313e7a7aea21d
Murray Steele <muz@h-lame.com>
2013-04-23 11:44:50 +0100

    Make reindexing all related editions for a policy a delayed process
    It was still too slow in a single request/response cycle to create all the jobs for a policy with large numbers of related content, so we make that part of the process a job on it's own.

--- test/unit/policy_search_index_observer_test.rb
+++ test/unit/policy_search_index_observer_test.rb
@@ -1,21 +1,44 @@
 require 'test_helper'
 
 class PolicySearchIndexObserverTest < ActiveSupport::TestCase
-  test 'should re-index all editions that are related to this policy when the policy is published' do
+  test 'after publishing a policy, it requests to reindex all related editions for that policy later' do
     policy = create(:submitted_policy)
-    publication = create(:published_publication, related_editions: [policy])
-    policy.stubs(:published_related_editions).returns([publication])
-    publication.expects(:update_in_search_index)
+
+    PolicySearchIndexObserver::ReindexRelatedEditions.expects(:later).with(policy)
 
     policy.publish_as(create(:departmental_editor))
   end
 
-  test 'should re-index all editions that are related when the policy is unpublished' do
+  test 'after unpublishing a policy, it requests to reindex all related editions for that policy later' do
     policy = create(:published_policy)
-    publication = create(:published_publication, related_editions: [policy])
-    policy.stubs(:published_related_editions).returns([publication])
-    publication.expects(:update_in_search_index)
-    policy.reload
+
+    PolicySearchIndexObserver::ReindexRelatedEditions.expects(:later).with(policy)
+
     policy.unpublish_as(create(:gds_editor))
+  end
+
+  test 'ReindexRelatedEditions.later enqueues a job for the supplied policy' do
+    policy = create(:published_policy)
+
+    job = PolicySearchIndexObserver::ReindexRelatedEditions.new(policy.id)
+    PolicySearchIndexObserver::ReindexRelatedEditions.expects(:new).with(policy.id).returns(job)
+    Delayed::Job.expects(:enqueue).with(job)
+
+    PolicySearchIndexObserver::ReindexRelatedEditions.later(policy)
+  end
+
+  test 'ReindexRelatedEditions#perform reindexes all published editions that are related to the policy' do
+    policy = create(:submitted_policy)
+    related_editions = mock('related editions')
+    related_editions.expects(:reindex_all)
+    policy.stubs(:published_related_editions).returns(related_editions)
+    Policy.stubs(:find).with(policy.id).returns(policy)
+
+    PolicySearchIndexObserver::ReindexRelatedEditions.new(policy.id).perform
+  end
+
+  test 'ReindexRelatedEditions#perform fails if the supplied policy cannot be found' do
+    job = PolicySearchIndexObserver::ReindexRelatedEditions.new(1000)
+    assert_raises(ActiveRecord::RecordNotFound) { job.perform }
   end
 end
