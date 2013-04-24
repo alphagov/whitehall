658ee0fac0931c94c872b0fa83f1c9a025746a5f
Matthew Ford <matt@bitzesty.com>
2013-04-24 14:24:32 +0100

    Clear the role_appointment_id in the controller and fix label

--- app/controllers/admin/speeches_controller.rb
+++ app/controllers/admin/speeches_controller.rb
@@ -1,9 +1,17 @@
 class Admin::SpeechesController < Admin::EditionsController
   before_filter :build_image, only: [:new, :edit]
+  before_filter :clear_role_appointment_param_on_override, only: [:update, :create]
 
   private
 
   def edition_class
     Speech
+  end
+
+  def clear_role_appointment_param_on_override
+    if (params[:person_override_active] == "1" || params[:edition][:person_override].present?)
+      params[:edition][:role_appointment_id] = nil
+      true
+    end
   end
 end
