ec70ffd78194e340fdeb53126d7a593038f9a001
Tekin Suleyman <tekin@tekin.co.uk>
2013-04-24 17:13:50 +0100

    Show all dates in office for historic accounts
    We want to list all the periods that someone served when they have
    had more than one term in a particular role

--- app/helpers/historic_appointments_helper.rb
+++ app/helpers/historic_appointments_helper.rb
@@ -3,4 +3,8 @@ module HistoricAppointmentsHelper
     return if text.blank?
     content_tag(:h3, title) + content_tag(:p, text)
   end
+
+  def previous_dates_in_office(role, person)
+    RoleAppointmentPresenter.decorate(role.previous_appointments.for_person(person)).collect(&:date_range).join(', ')
+  end
 end
