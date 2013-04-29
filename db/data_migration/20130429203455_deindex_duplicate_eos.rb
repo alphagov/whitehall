4ee46bf09682bc97297a11177287f3f66555eebc
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-04-29 20:52:41 +0100

    Remove bad URLs for Number10 and DPMO from search
    https://www.pivotaltracker.com/story/show/48925175

---
+++ db/data_migration/20130429203455_deindex_duplicate_eos.rb
@@ -0,0 +1,4 @@
+Rummageable.delete('/government/organisations/the-prime-ministers-office-number-10', Whitehall.government_search_index_path)
+Rummageable.delete('/government/organisations/the-deputy-prime-ministers-office', Whitehall.government_search_index_path)
+
+puts "Removed bad records from Rummager"
