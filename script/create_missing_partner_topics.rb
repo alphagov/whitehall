b6c88b534563f60f0b94f741b1c5fbde5ad59460
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-05-03 16:27:59 +0100

    Script to generate URLs from topic CSV
    https://www.pivotaltracker.com/story/show/49192237

---
+++ script/create_missing_partner_topics.rb
@@ -0,0 +1,23 @@
+require 'csv'
+
+CSV.read(ARGV[0], headers: true, header_converters: :symbol).each_with_index do |row, i|
+  url = row[0]
+  parts = url.sub('https://www.gov.uk/government/', '').split('/')
+  if parts.first == 'policies'
+    alert = EmailSignup::Alert.new(policy: parts[1])
+    puts EmailSignup::FeedUrlExtractor.new(alert).feed_url
+  elsif parts.first.include? 'feed'
+    parsed = Rack::Utils.parse_nested_query(parts.first.split('?')[1])
+    alert = nil
+    if parsed['topics']
+      alert = EmailSignup::Alert.new(topic: parsed['topics'].first, document_type: 'all')
+    elsif parsed['departments']
+      alert = EmailSignup::Alert.new(organisation: parsed['departments'].first, document_type: 'all')
+    end
+    if alert
+      puts EmailSignup::FeedUrlExtractor.new(alert).feed_url
+    else
+      puts parsed
+    end
+  end
+end
