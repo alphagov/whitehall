ce316c1711628e322a1744046ca9e0e0fbceb1f8
Murray Steele <muz@h-lame.com>
2013-05-20 17:47:16 +0100

    Remove draper from featured edition presenter
    Only used in Topical Events controller, is this a candidate for removal?

--- app/controllers/topical_events_controller.rb
+++ app/controllers/topical_events_controller.rb
@@ -12,7 +12,7 @@ class TopicalEventsController < ClassificationsController
     @detailed_guides = @classification.detailed_guides.published.limit(5)
     @related_classifications = @classification.related_classifications
     @recently_changed_documents = @classification.recently_changed_documents
-    @featured_editions = FeaturedEditionPresenter.decorate(@classification.classification_featurings.limit(6))
+    @featured_editions = decorate_collection(@classification.classification_featurings.limit(6), FeaturedEditionPresenter)
     set_slimmer_organisations_header(@classification.organisations)
 
     respond_to do |format|
