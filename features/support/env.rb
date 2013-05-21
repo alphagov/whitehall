30fed0f2c1f613f74fdf2318608eb010071a6929
Tekin Suleyman <tekin@tekin.co.uk>
2013-05-21 13:21:02 +0100

    Use transactional strategy for database cleaner
    The parallel test runner performs better with the transaction strategy

--- features/support/env.rb
+++ features/support/env.rb
@@ -35,7 +35,7 @@ Cucumber::Rails::World.use_transactional_fixtures = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.strategy = :transaction
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
