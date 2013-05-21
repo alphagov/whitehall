e1939606467bddad654f07f143ec064d71263bbc
Tekin Suleyman <tekin@tekin.co.uk>
2013-05-21 13:54:25 +0100

    Ensure the parallel databases exist in Jenkins

--- jenkins-parallel.sh
+++ jenkins-parallel.sh
@@ -8,7 +8,7 @@ time bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
 time bundle exec rake db:create db:migrate db:test:prepare --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:clean --trace
-RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest parallel:prepare parallel:test parallel:features test:javascript test:cleanup --trace
+RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest parallel:create parallel:prepare parallel:test parallel:features test:javascript test:cleanup --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:precompile --trace
 
 EXIT_STATUS=$?
