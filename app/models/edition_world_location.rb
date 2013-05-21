b52cbb4057d513d45a4a84b01b56902ab21fe4a6
Matthew Ford <matt@bitzesty.com>
2013-05-21 12:06:59 +0100

    deprecated_columns doesn't work

--- app/models/edition_world_location.rb
+++ app/models/edition_world_location.rb
@@ -6,7 +6,7 @@ class EditionWorldLocation < ActiveRecord::Base
 
   validates :edition, :world_location, presence: true
 
-  deprecated_columns :featured, :ordering, :edition_world_location_image_data_id, :alt_text
+  # deprecated_columns :featured, :ordering, :edition_world_location_image_data_id, :alt_text
 
   scope :with_translations, -> *locales { joins(edition: :translations).merge(Edition.with_locales(*locales)).merge(Edition.with_required_attributes) }
 end
