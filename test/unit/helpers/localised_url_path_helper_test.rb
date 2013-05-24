ad7947c16a533ca0ee07dd59ba850b8a4ab2a47e
Murray Steele <muz@h-lame.com>
2013-05-24 14:34:12 +0100

    Fix localised_path_helper to fallback to I18n.locale if not provided
    This stops it asking if the object is available in locale: 'NULL' and slowing pages down unnecessarily in the normal case.

--- test/unit/helpers/localised_url_path_helper_test.rb
+++ test/unit/helpers/localised_url_path_helper_test.rb
@@ -34,6 +34,23 @@ class LocalisedUrlPathHelperTest < ActiveSupport::TestCase
     controller.worldwide_organisation_corporate_information_page_path(parent, object)
   end
 
+  test 'if locale is not provided to path helper, and it is not set in params, should fall back to I18n.locale' do
+    controller = FakeController.new()
+    object = stub('news article')
+
+    object.expects(:available_in_locale?).with(:fr).returns(true)
+    I18n.with_locale :fr do
+      controller.expects(:news_article_path_was_called).with(object, {locale: :fr})
+      controller.news_article_path(object)
+    end
+
+    object.expects(:available_in_locale?).never
+    I18n.with_locale :en do
+      controller.expects(:news_article_path_was_called).with(object, {})
+      controller.news_article_path(object)
+    end
+  end
+
   module FakeRouting
     def news_article_path(object, options={})
       news_article_path_was_called(object, options)
