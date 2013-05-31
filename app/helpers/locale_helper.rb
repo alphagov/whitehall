afdb9e1274c08c9ea23f5046cc503e8f9ae006ba
Tekin Suleyman <tekin@tekin.co.uk>
2013-05-31 10:45:15 +0100

    Adjust select for non-English editions
    We shouldn't include English in select options. This has necessitated
    a tweak to the controller to remove the locale parameter if the
    submitted value is blank.

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -6,4 +6,9 @@ module LocaleHelper
   def options_for_locales(locales)
     locales.map { |l| ["#{l.native_language_name} (#{l.english_language_name})", l.code.to_s] }
   end
+
+  def options_for_foreign_language_locale(edition)
+    options = [['Choose foreign language...', nil]] + options_for_locales(Locale.non_english)
+    options_for_select(options, edition.non_english_edition? ? edition.locale : nil)
+  end
 end
