eea289de46171b6c331b5f824b07541e9cf65390
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-05-31 15:39:28 +0100

    Preload Whitehall in production
    The time before the app begins serving requests is currently quite large
    due to the way Rails lazily loads its object graph. During this time it
    times out frequently while it warms up.
    
    This commit tells Unicorn to preload the entire application, and
    includes the standard database connection futzing that seems to be
    common to these solutions.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,3 +4,23 @@ end
 load_file_if_exists(self, "/etc/govuk/unicorn.rb")
 working_directory File.dirname(File.dirname(__FILE__))
 worker_processes 4
+
+if env == 'production'
+  # We want to preload the entire app in production mode
+  preload_app true
+
+  before_fork do |server, worker|
+    # the following is highly recomended for Rails + "preload_app true"
+    # as there's no need for the master process to hold a connection
+    if defined?(ActiveRecord::Base)
+      ActiveRecord::Base.connection.disconnect!
+    end
+  end
+
+  after_fork do |server, worker|
+    # the following is *required* for Rails + "preload_app true",
+    if defined?(ActiveRecord::Base)
+      ActiveRecord::Base.establish_connection
+    end
+  end
+end
