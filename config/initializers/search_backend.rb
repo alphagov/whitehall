d9d6c87d6933f1578e836eea48400e9be9958c8a
Tekin Suleyman <tekin@tekin.co.uk>
2013-05-31 16:11:44 +0100

    Use rummager if ENV['RUMMAGER_HOST'] is set.
    Makes it easier to test rummager in development.

--- config/initializers/search_backend.rb
+++ config/initializers/search_backend.rb
@@ -1,1 +1,5 @@
-Whitehall.search_backend = Whitehall::DocumentFilter::Mysql
+if Rails.env.production? || ENV["RUMMAGER_HOST"]
+  Whitehall.search_backend = Whitehall::DocumentFilter::Rummager
+else
+  Whitehall.search_backend = Whitehall::DocumentFilter::Mysql
+end
