5ead9afffd026e1b079cd5763c682cb4482e9c8e
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-06-03 13:47:42 +0100

    Revert preforking behaviour
    This doesn't work well in production under current stack. Needs more
    work before deployment.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,24 +4,3 @@ end
 load_file_if_exists(self, "/etc/govuk/unicorn.rb")
 working_directory File.dirname(File.dirname(__FILE__))
 worker_processes 4
-
-# We want to preload the entire app in production mode
-preload_app true
-
-before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  # Preload i18n records
-  I18n.t('activerecord')
-end
-
-after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true",
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
-end
