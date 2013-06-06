fae9991f600551d637be87b36a79b0e291933f7b
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-06-06 16:02:44 +0100

    Remove gc_precompile
    We didn't actually use it in Capistrano anyway.

--- jenkins.sh
+++ jenkins.sh
@@ -14,7 +14,7 @@ time bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 time bundle exec rake db:create db:migrate db:test:prepare --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:clean --trace
 RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest default test:cleanup --trace
-RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:gc_precompile --trace
+RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:precompile --trace
 
 EXIT_STATUS=$?
 echo "EXIT STATUS: $EXIT_STATUS"
