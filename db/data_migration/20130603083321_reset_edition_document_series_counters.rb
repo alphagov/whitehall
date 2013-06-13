0045d2707dfd6087909b00d9c0f171abc6e861b6
Tekin Suleyman <tekin@tekin.co.uk>
2013-06-13 16:57:21 +0100

    Make the data migration faster
    Instead of several minutes, this executes in a few seconds.

--- db/data_migration/20130603083321_reset_edition_document_series_counters.rb
+++ db/data_migration/20130603083321_reset_edition_document_series_counters.rb
@@ -1,12 +1,6 @@
-# There is a bug in Rails 3.x that prevents us using the reset_counters method here.
-# It's fixed in Rails 4 though: https://github.com/rails/rails/pull/7822
-edition_ids = Set.new
-EditionDocumentSeries.all.each { |eds| edition_ids << eds.edition_id }
-puts "Reseting the document_series_count on #{edition_ids.size} Editions. This may take some time..."
-edition_ids.each do |edition_id|
-  edition = Edition.find_by_id(edition_id)
-  if edition && edition.respond_to?(:document_series)
-    count = edition.document_series.count
-    Edition.update(edition_id, document_series_count: count)
-  end
+counts = EditionDocumentSeries.group(:edition_id).count
+puts "Reseting the document_series_count on #{counts.size} Editions."
+
+counts.each do |edition_id, count|
+  ActiveRecord::Base.connection.execute("UPDATE editions SET document_series_count=#{count} WHERE id=#{edition_id}")
 end
