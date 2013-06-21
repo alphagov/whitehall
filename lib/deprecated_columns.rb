54ef265e3c3547a9f69cf72c2d2c4d86983f76af
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-06-21 15:26:15 +0100

    Deprecated columns helper should exclude attributes
    ..as well as columns
    
    The attributes are used during #process_associations_before_save to
    calculate what values to copy to the new edition. I think that the
    implementation of #attributes must have changed between Rails versions,
    so that it's no longer derived from .columns.
    
    This change ensures that #attributes and #attribute_names exclude
    deprecated columns.
    
    This fixes the following crash when creating a new draft:
    
      ActiveRecord::UnknownAttributeError occurred in editions#revise
    
    https://www.pivotaltracker.com/story/show/52135215

--- lib/deprecated_columns.rb
+++ lib/deprecated_columns.rb
@@ -1,11 +1,24 @@
 module DeprecatedColumns
+
   # This is here to enable us to gracefully remove columns
   # in a future commit, *after* the code marking the column
   # as deprecated has been deployed
-  def deprecated_columns(*deprecated_column_list)
-    @deprecated_column_list = deprecated_column_list
+  def deprecated_columns(*names)
+    cattr_accessor :deprecated_column_list do
+      # default value
+      names.map(&:to_s)
+    end
+
     def columns
-      super().reject { |column| @deprecated_column_list.include?(column.name.to_sym) }
+      super().reject { |column| deprecated_column_list.include?(column.name) }
+    end
+
+    include InstanceMethods
+  end
+
+  module InstanceMethods
+    def attribute_names
+      super().reject {|name| deprecated_column_list.include?(name)}
     end
   end
 end
