2f5fd5a13552e5c4511780f156c2393e01e3d637
Edd Sowden <e@e26.co.uk>
2013-06-24 09:41:58 +0000

    Add tracking to change note viewing
    We don't know if anyone actually looks at change notes. It would be good
    to get some visibility to how many people want them or look for them.

--- app/assets/javascripts/application/on_ready.js
+++ app/assets/javascripts/application/on_ready.js
@@ -9,7 +9,14 @@ jQuery(function($) {
   GOVUK.stickAtTopWhenScrolling.init();
   GOVUK.backToContent.init();
 
-  $('.js-toggle-change-notes').toggler();
+  $('.js-toggle-change-notes').toggler({actLikeLightbox: true});
+  $('.js-toggle-change-notes').click(function(){
+    if(!window.changeNoteTrackingEventSent){
+      window._gaq && _gaq.push(['_trackEvent', 'edd_inside_gov', 'change_note', 'shown', 0, true]);
+      window.changeNoteTrackingEventSent = true;
+    }
+  });
+
   $('.js-toggle-accessibility-warning').toggler({header: ".toggler", content: ".help-block"})
   $('.js-toggle-org-list').toggler({actLikeLightbox: true})
 
