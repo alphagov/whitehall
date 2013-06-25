9c82417fe4fc879422074314477137ab877676ae
Tekin Suleyman <tekin@tekin.co.uk>
2013-06-25 14:27:27 +0100

    Add addtional signposting of monkey-patch

--- lib/whitehall/carrier_wave/sanitized_file.rb
+++ lib/whitehall/carrier_wave/sanitized_file.rb
@@ -1,6 +1,17 @@
-# TODO: Explain this monkey-patch
 module CarrierWave
   class SanitizedFile
+    # CarrierWave has been designed with the assumption that your files will
+    # be uploaded to and served from the same directory. We've added a virus
+    # checking step, and so have two folders involved ("incoming" and "clean").
+    #
+    # By default, CarrierWave will generate the URL of an asset by taking its
+    # path relative to the folder it was uploaded to (e.g. /public) and append
+    # it to `asset_host`.
+    #
+    # We've changed this behaviour by defining this `url` method, which returns
+    # a consistent value regardless of whether or not an uploaded file has been
+    # checked for viruses, as the view code (that calls it) needs to return the
+    # same URL in both cases.
     def url
       CarrierWaveFilePath.new(path).to_public_path
     end
