4ae5c2c766b191ef680084ca68ca230ea6f71a0d
Matthew Ford <matt@bitzesty.com>
2013-06-25 12:25:59 +0100

    Fix unit tests
    with removed html5 required field

--- test/unit/whitehall/form_builder_test.rb
+++ test/unit/whitehall/form_builder_test.rb
@@ -21,7 +21,7 @@ class FormBuilderTest < ActionView::TestCase
 
   test 'Whitehall::FormBuilder#upload returns a label and file upload input field, and a hidden cache field by default' do
     expected_html = '<label class="required" for="promotional_feature_item_image">Image<span>*</span></label>' +
-                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" type="file" />' +
                     hidden_image_cache_field
 
     assert_dom_equal expected_html, @builder.upload(:image)
@@ -29,7 +29,7 @@ class FormBuilderTest < ActionView::TestCase
 
   test 'Whitehall::FormBuilder#upload includes a removal checkbox if the allow_removal option is true' do
     expected_html = '<label class="required" for="promotional_feature_item_image">Image<span>*</span></label>' +
-                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" type="file" />' +
                     hidden_image_cache_field +
                     removal_check_box
 
@@ -38,7 +38,7 @@ class FormBuilderTest < ActionView::TestCase
 
   test 'Whitehall::FormBuilder#upload includes a removal checkbox with custom label text if the allow_removal option is true and the allow_removal_label_text is specified' do
     expected_html = '<label class="required" for="promotional_feature_item_image">Image<span>*</span></label>' +
-                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" type="file" />' +
                     hidden_image_cache_field +
                     removal_check_box('Tick this box to remove image')
 
@@ -47,7 +47,7 @@ class FormBuilderTest < ActionView::TestCase
 
   test 'Whitehall::FormBuilder#upload allows the label text to be overridden' do
     expected_html = '<label class="required" for="promotional_feature_item_image">Image upload<span>*</span></label>' +
-                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" type="file" />' +
                     hidden_image_cache_field
 
     assert_dom_equal expected_html, @builder.upload(:image, label_text: "Image upload")
@@ -56,7 +56,7 @@ class FormBuilderTest < ActionView::TestCase
   test 'Whitehall::FormBuilder#upload includes upload cache fields if object has a cached file' do
     @object.image = image_fixture_file
     expected_html = '<label class="required" for="promotional_feature_item_image">Image upload<span>*</span></label>' +
-                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                    '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" type="file" />' +
                     "<span class='already_uploaded'>#{File.basename(image_fixture_file)} already uploaded</span>" +
                     hidden_image_cache_field("value = '#{@object.image_cache}'")
 
@@ -68,7 +68,7 @@ class FormBuilderTest < ActionView::TestCase
     expected_html = '<div class="control-group">' +
                       '<label class="control-label required" for="promotional_feature_item_image">Image<span>*</span></label>' +
                       '<div class="controls">' +
-                        '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                        '<input id="promotional_feature_item_image" name="promotional_feature_item[image]"  type="file" />' +
                         hidden_image_cache_field +
                       '</div>' +
                     '</div>'
@@ -81,7 +81,7 @@ class FormBuilderTest < ActionView::TestCase
     expected_html = '<div class="control-group">' +
                       '<label for="promotional_feature_item_image" class="control-label required">Image<span>*</span></label>' +
                       '<div class="controls">' +
-                        '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required"  type="file" />' +
+                        '<input id="promotional_feature_item_image" name="promotional_feature_item[image]"   type="file" />' +
                         "<span class='already_uploaded'>#{File.basename(image_fixture_file)} already uploaded</span>" +
                         "<input id='promotional_feature_item_image_cache' name='promotional_feature_item[image_cache]' type='hidden' value='#{@object.image_cache}' />" +
                       '</div>' +
@@ -94,7 +94,7 @@ class FormBuilderTest < ActionView::TestCase
     expected_html = '<div class="control-group">' +
                       '<label for="promotional_feature_item_image" class="control-label required">Image<span>*</span></label>' +
                       '<div class="controls">' +
-                        '<input id="promotional_feature_item_image" name="promotional_feature_item[image]" required="required" type="file" />' +
+                        '<input id="promotional_feature_item_image" name="promotional_feature_item[image]"  type="file" />' +
                         "<span class='already_uploaded'>#{File.basename(image_fixture_file)} already uploaded</span>" +
                         "<input id='promotional_feature_item_image_cache' name='promotional_feature_item[image_cache]' type='hidden' value='#{@object.image_cache}' />" +
                         removal_check_box +
