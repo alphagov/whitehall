ef9c6c6725ed7cda017621ee40a3c521d1725d17
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-06-28 15:22:24 +0000

    Make topical event about pages searchable

--- app/models/about_page.rb
+++ app/models/about_page.rb
@@ -1,4 +1,6 @@
 class AboutPage < ActiveRecord::Base
+  include Searchable
+
   attr_accessible :body, :name, :summary, :read_more_link_text
 
   belongs_to :subject, polymorphic: true
@@ -7,4 +9,17 @@ class AboutPage < ActiveRecord::Base
   validates :read_more_link_text, presence: true
   validates :summary, presence: true
   validates :body, presence: true
+
+  searchable title: :name,
+             link: :search_link,
+             content: :indexable_content,
+             description: :summary
+
+  def search_link
+    Whitehall.url_maker.topical_event_about_pages_path(subject.slug)
+  end
+
+  def indexable_content
+    Govspeak::Document.new(body).to_text
+  end
 end
