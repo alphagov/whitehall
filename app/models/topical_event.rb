77380df33b43731809041ff9d1c89095eef5cf5a
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-07-03 07:43:30 +0000

    Re-implement date comparison with Rails date helpers

--- app/models/topical_event.rb
+++ app/models/topical_event.rb
@@ -105,35 +105,7 @@ class TopicalEvent < Classification
     end
   end
 
-  #adapted from action_view/helpers/date_helper.rb
   def more_than_a_year(from_time, to_time = 0)
-    from_time = from_time.to_time if from_time.respond_to?(:to_time)
-    to_time = to_time.to_time if to_time.respond_to?(:to_time)
-    distance = (to_time.to_f - from_time.to_f).abs
-    distance_in_minutes = (distance / 60.0).round
-    distance_in_seconds = distance.round
-
-    fyear = from_time.year
-    fyear += 1 if from_time.month >= 3
-    tyear = to_time.year
-    tyear -= 1 if to_time.month < 3
-    leap_years = (fyear > tyear) ? 0 : (fyear..tyear).count{|x| Date.leap?(x)}
-    minute_offset_for_leap_year = leap_years * 1440
-    # Discount the leap year days when calculating year distance.
-    # e.g. if there are 20 leap year days between 2 dates having the same day
-    # and month then the based on 365 days calculation
-    # the distance in years will come out to over 80 years when in written
-    # english it would read better as about 80 years.
-    minutes_with_offset         = distance_in_minutes - minute_offset_for_leap_year
-    remainder                   = (minutes_with_offset % 525600)
-    distance_in_years           = (minutes_with_offset / 525600)
-
-    if distance_in_years > 1
-      true
-    elsif distance_in_years == 1 && remainder > 1440 #Allow one day overrun
-      true
-    else
-      false
-    end
+    to_time > from_time + 1.year + 1.day  # allow 1 day's leeway
   end
 end
