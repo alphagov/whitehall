de02eae54a8bb174a8ec626735caf6dda0929493
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-07-19 09:48:18 +0100

    Make data migration safer
    As per feedback from @tekin

--- db/data_migration/20130718142536_fix_force_published_flag.rb
+++ db/data_migration/20130718142536_fix_force_published_flag.rb
@@ -4,13 +4,14 @@ gds_user = User.find_by_name!("GDS Inside Government Team")
 Edition::AuditTrail.whodunnit = gds_user
 
 CSV.foreach('db/data_migration/20130711131323_reindex_organisations_for_acronym_changes.csv') do |row|
-  edition = Document.find(row.first).published_edition
-  if edition && edition.force_published?
-    if edition.authors.include? gds_user
-      # Call update_column because we don't want to cause another
-      # visible update
-      edition.update_column(:force_published, nil)
-      puts "#{edition.title} un-force-published"
+  if document = Document.find_by_id(row.first)
+    if edition = document.published_edition
+      if (edition.force_published?) && edition.authors.include?(gds_user)
+        # Call update_column because we don't want to cause another
+        # visible update
+        edition.update_column(:force_published, nil)
+        puts "#{edition.title} un-force-published"
+      end
     end
   end
 end
