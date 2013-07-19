77cd63e26b16deb6f0a7d581de16329365c839c5
Tekin Suleyman <tekin@tekin.co.uk>
2013-07-19 15:46:11 +0100

    Fix tabs
    For some reason, my editor decided to revert back to hard tabs :/
    I fixed a few more I found whilst I was at it.

--- app/controllers/admin/bulk_uploads_controller.rb
+++ app/controllers/admin/bulk_uploads_controller.rb
@@ -1,39 +1,39 @@
 class Admin::BulkUploadsController < Admin::BaseController
-	before_filter :find_edition
-	before_filter :limit_edition_access!
-	before_filter :enforce_permissions!
-	before_filter :prevent_modification_of_unmodifiable_edition
+  before_filter :find_edition
+  before_filter :limit_edition_access!
+  before_filter :enforce_permissions!
+  before_filter :prevent_modification_of_unmodifiable_edition
 
-	def new
-		@bulk_upload_zip_file = BulkUpload::ZipFile.new
-	end
+  def new
+    @bulk_upload_zip_file = BulkUpload::ZipFile.new
+  end
 
-	def upload_zip
-		@bulk_upload_zip_file = BulkUpload::ZipFile.new(params[:bulk_upload_zip_file][:zip_file])
-		if @bulk_upload_zip_file.valid?
-			@bulk_upload = BulkUpload.from_files(@bulk_upload_zip_file.extracted_file_paths)
-			render :set_titles
-		else
-			render :new
-		end
-	end
+  def upload_zip
+    @bulk_upload_zip_file = BulkUpload::ZipFile.new(params[:bulk_upload_zip_file][:zip_file])
+    if @bulk_upload_zip_file.valid?
+      @bulk_upload = BulkUpload.from_files(@bulk_upload_zip_file.extracted_file_paths)
+      render :set_titles
+    else
+      render :new
+    end
+  end
 
-	def create
-		@bulk_upload = BulkUpload.new(params[:bulk_upload])
-		if @bulk_upload.save_attachments_to_edition(@edition)
-			redirect_to admin_edition_attachments_path(@edition)
-		else
-			render :set_titles
-		end
-	end
+  def create
+    @bulk_upload = BulkUpload.new(params[:bulk_upload])
+    if @bulk_upload.save_attachments_to_edition(@edition)
+      redirect_to admin_edition_attachments_path(@edition)
+    else
+      render :set_titles
+    end
+  end
 
-	private
+  private
 
-	def find_edition
-		@edition = Edition.find(params[:edition_id])
-	end
+  def find_edition
+    @edition = Edition.find(params[:edition_id])
+  end
 
-	def enforce_permissions!
-		enforce_permission!(:update, @edition)
-	end
+  def enforce_permissions!
+    enforce_permission!(:update, @edition)
+  end
 end
--- test/factories/attachments.rb
+++ test/factories/attachments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
-  	sequence(:title) { |index| "attachment-title-#{index}" }
+    sequence(:title) { |index| "attachment-title-#{index}" }
 
     ignore do
       file { File.open(File.join(Rails.root, 'test', 'fixtures', 'greenpaper.pdf')) }
