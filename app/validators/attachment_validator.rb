d3d0cf866df94769e9ecdcf6ab6f685d5c8e4c17
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-07-22 14:43:04 +0000

    Check valid combinations of metadata on (un)numbered papers

--- app/validators/attachment_validator.rb
+++ app/validators/attachment_validator.rb
@@ -1,28 +1,68 @@
 class AttachmentValidator < ActiveModel::Validator
 
   def validate(attachment)
+    check_unnumbered_command_papers_dont_have_numbers(attachment)
+    check_unnumbered_hoc_papers_dont_have_numbers(attachment)
+    check_unnumbered_command_paper_has_no_hoc_paper_metadata(attachment)
+    check_unnumbered_hoc_paper_has_no_command_paper_metadata(attachment)
     check_if_hoc_paper_number_required(attachment)
     check_if_parliamentary_session_required(attachment)
     check_format_of_hoc_paper_number(attachment)
   end
 
   private
+  def check_unnumbered_command_papers_dont_have_numbers(attachment)
+    if attachment.unnumbered_command_paper? && attachment.command_paper_number.present?
+      attachment.errors[:command_paper_number] << 'cannot be set on an unnumbered paper'
+    end
+  end
+
+  def check_unnumbered_hoc_papers_dont_have_numbers(attachment)
+    if attachment.unnumbered_hoc_paper?
+      if attachment.hoc_paper_number.present?
+        attachment.errors[:hoc_paper_number] << 'cannot be set on an unnumbered paper'
+      elsif attachment.parliamentary_session.present?
+        attachment.errors[:parliamentary_session] << 'cannot be set on an unnumbered paper'
+      end
+    end
+  end
+
+  def check_unnumbered_command_paper_has_no_hoc_paper_metadata(attachment)
+    if attachment.unnumbered_command_paper?
+      if attachment.unnumbered_hoc_paper?
+        attachment.errors[:unnumbered_hoc_paper] << 'cannot be set on an unnumbered Command Paper'
+      end
+      if attachment.hoc_paper_number.present?
+        attachment.errors[:hoc_paper_number] << 'cannot be set on a Command Paper'
+      end
+      if attachment.hoc_paper_number.present?
+        attachment.errors[:parliamentary_session] << 'cannot be set on a Command Paper'
+      end
+    end
+  end
+
+  def check_unnumbered_hoc_paper_has_no_command_paper_metadata(attachment)
+    if attachment.unnumbered_hoc_paper? && attachment.command_paper_number.present?
+      attachment.errors[:command_paper_number] << 'cannot be set on a House of Commons paper'
+    end
+  end
+
   def check_if_hoc_paper_number_required(attachment)
     if attachment.parliamentary_session.present? && attachment.hoc_paper_number.blank?
-      attachment.errors[:hoc_paper_number] = 'is required when parliamentary session is set'
+      attachment.errors[:hoc_paper_number] << 'is required when parliamentary session is set'
     end
   end
 
   def check_if_parliamentary_session_required(attachment)
     if attachment.hoc_paper_number.present? && attachment.parliamentary_session.blank?
-      attachment.errors[:parliamentary_session] = 'is required when House of Commons number is set'
+      attachment.errors[:parliamentary_session] << 'is required when House of Commons number is set'
     end
   end
 
   def check_format_of_hoc_paper_number(attachment)
     number = attachment.hoc_paper_number
     if number.present? && (number !~ /^\d{4}/)
-      attachment.errors[:hoc_paper_number] = 'must start with a number'
+      attachment.errors[:hoc_paper_number] << 'must start with a number'
     end
   end
 end
--- test/unit/attachment_validator_test.rb
+++ test/unit/attachment_validator_test.rb
@@ -10,8 +10,14 @@ class AttachmentValidatorTest < ActiveSupport::TestCase
         "expected error messages to contain #{expectation}"
   end
 
+  test 'command papers cannot have a number whilst marked as unnumbered' do
+    attachment = build(:attachment, command_paper_number: '1234', unnumbered_command_paper: true)
+    @validator.validate(attachment)
+    assert_error_message /^cannot be set on an unnumbered paper/, attachment.errors[:command_paper_number]
+  end
+
   test 'must provide house of commons paper number if parliamentary session set' do
-    attachment = build(:attachment, parliamentary_session: '2013/14')
+    attachment = build(:attachment, parliamentary_session: '2013-14')
     @validator.validate(attachment)
     assert_error_message /^is required when/, attachment.errors[:hoc_paper_number]
   end
@@ -32,5 +38,32 @@ class AttachmentValidatorTest < ActiveSupport::TestCase
     attachment = build(:attachment, hoc_paper_number: '1234-i')
     @validator.validate(attachment)
     assert attachment.errors[:hoc_paper_number].empty?, 'expected no error'
+  end
+
+  test 'house of commons papers cannot have a number while unnumbered' do
+    attachment = build(:attachment, hoc_paper_number: '1234', unnumbered_hoc_paper: true)
+    @validator.validate(attachment)
+    assert_error_message /^cannot be set on an unnumbered paper/, attachment.errors[:hoc_paper_number]
+  end
+
+  test 'house of commons papers cannot have a parliamentary session while unnumbered' do
+    attachment = build(:attachment, parliamentary_session: '2010-11', unnumbered_hoc_paper: true)
+    @validator.validate(attachment)
+    assert_error_message /^cannot be set on an unnumbered paper/, attachment.errors[:parliamentary_session]
+  end
+
+  test 'unnumbered papers cannot be both "command" and "house of commons" at the same time' do
+    attachment = build(:attachment, unnumbered_command_paper: true, unnumbered_hoc_paper: true)
+    @validator.validate(attachment)
+    assert_error_message /^cannot be set on an unnumbered Command Paper/, attachment.errors[:unnumbered_hoc_paper]
+
+    attachment = build(:attachment, unnumbered_command_paper: true, hoc_paper_number: '1234', parliamentary_session: '2010-11')
+    @validator.validate(attachment)
+    assert_error_message /^cannot be set on a Command Paper/, attachment.errors[:hoc_paper_number]
+    assert_error_message /^cannot be set on a Command Paper/, attachment.errors[:parliamentary_session]
+
+    attachment = build(:attachment, unnumbered_hoc_paper: true, command_paper_number: '1234')
+    @validator.validate(attachment)
+    assert_error_message /^cannot be set on a House of Commons paper/, attachment.errors[:command_paper_number]
   end
 end
