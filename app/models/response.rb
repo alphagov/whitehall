8dd20ddf75526b11d2c5690647589f1abe39ca43
Tekin Suleyman <tekin@tekin.co.uk>
2013-07-26 12:23:47 +0100

    Response can use Attachable
    We can use Attachable to DRY things up. Note that the attachment
    associations are defined explicitly, rather than calling the
    attachable class method. We have to do this because the join table
    used for response attachments doesn't follow the same naming
    convention as Attachable

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,7 @@
 class Response < ActiveRecord::Base
-  belongs_to :consultation, foreign_key: :edition_id
+  include Attachable
 
+  belongs_to :consultation, foreign_key: :edition_id
   has_many :consultation_response_attachments, dependent: :destroy
   has_many :attachments, through: :consultation_response_attachments, order: [:ordering, :id], before_add: :set_order
 
@@ -15,26 +16,7 @@ class Response < ActiveRecord::Base
     consultation.alternative_format_contact_email
   end
 
-  def allows_attachment_references?
-    false
-  end
-
   def can_order_attachments?
-    !allows_inline_attachments?
-  end
-
-  def allows_inline_attachments?
-    false
-  end
-
-  private
-
-  def set_order(new_attachment)
-    new_attachment.ordering = next_ordering unless new_attachment.ordering.present?
-  end
-
-  def next_ordering
-    max = attachments.maximum(:ordering)
-    max ? max + 1 : 0
+    true
   end
 end
