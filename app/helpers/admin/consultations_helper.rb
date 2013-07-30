d1c96f41233d1f9a2dbc0945af3282c64f331886
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-07-30 17:58:58 +0100

    Change wording of hints on consultation forms
    After discussion with Alice, we think this wording is more succinct. The
    key concern is to alert people to the fact that they can upload
    attachments on the next page, so a shorter message gives more chance
    that they'll read this info.

--- app/helpers/admin/consultations_helper.rb
+++ app/helpers/admin/consultations_helper.rb
@@ -1,13 +1,9 @@
 module Admin::ConsultationsHelper
   def consulation_response_help_text(response)
     if response.is_a?(ConsultationOutcome)
-      "Here you can publish the final outcome of the consultation. 
-       Either provide the full details of the outcome below, or provide
-       a summary here and upload the full outcome as an attachment once you have saved a summary."
+      "You can add attachments after saving this page."
     else
-      "Here you can publish the feedback received from the public on this consultation.
-       Once you have saved a summary, you will be able to upload attachments containing their feedback.
-       Note that publishing public feedback is optional"
+      "Optional - publish the feedback received from the public. You can add attachments after saving this page."
     end
   end
 end
