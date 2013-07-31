6957549376016cef3a1b979c01c2cc9e0e144d3d
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-07-31 13:00:46 +0100

    Fix JS

--- app/assets/javascripts/admin/organisation_edit.js
+++ app/assets/javascripts/admin/organisation_edit.js
@@ -1,42 +1,46 @@
+/*jslint
+ white: true,
+ vars: true,
+ indent: 2
+*/
 (function () {
+  "use strict";
+  var root = this,
+      $ = root.jQuery,
+      GOVUK = root.GOVUK || {};
 
-    var root = this,
-        $ = root.jQuery;
-
-    var GOVUK = root.GOVUK || {};
-
-    GOVUK.hideClosedAtDates = function() {
-        $closedAtGroup = $('#organisation_closed_at_group'),
+  GOVUK.hideClosedAtDates = function() {
+    var $closedAtGroup = $('#organisation_closed_at_group'),
         $closedAtLabel = $closedAtGroup.prev('label[for=organisation_closed_at]'),
         $govUkStatus = $('#organisation_govuk_status');
 
-        function toggleShown(status) {
-            if (status === 'closed') {
-                $closedAtLabel.show();
-                $closedAtGroup.show();
-                $closedAtGroup.find('select').each(function(el) {
-                    // some older browsers are weird, so explicitly use the expando
-                    el.disabled = false;
-                    $(el).removeAttr('disabled');
-                });
-            }
-            else {
-                $closedAtLabel.hide();
-                $closedAtGroup.hide();
-                $closedAtGroup.find('select').each(function(el) {
-                    // some older browsers are weird, so explicitly use the expando
-                    el.disabled = true;
-                    $(el).attr('disabled', true);
-                });
-            }
-        }
-
-        if ($closedAtGroup && $closedAtLabel) {
-            toggleShown($govUkStatus.val());
-            $govUkStatus.on('change', function(e) {
-                toggleShown($(this).val());
-            });
-        }
+    function toggleShown(status) {
+      if (status === 'closed') {
+        $closedAtLabel.show();
+        $closedAtGroup.show();
+        $closedAtGroup.find('select').each(function(el) {
+          // some older browsers are weird, so explicitly use the expando
+          el.disabled = false;
+          $(el).removeAttr('disabled');
+        });
+      }
+      else {
+        $closedAtLabel.hide();
+        $closedAtGroup.hide();
+        $closedAtGroup.find('select').each(function(el) {
+          // some older browsers are weird, so explicitly use the expando
+          el.disabled = true;
+          $(el).attr('disabled', true);
+        });
+      }
     }
+
+    if ($closedAtGroup && $closedAtLabel) {
+      toggleShown($govUkStatus.val());
+      $govUkStatus.on('change', function(e) {
+        toggleShown($(this).val());
+      });
+    }
+  };
 
 }).call(this);
