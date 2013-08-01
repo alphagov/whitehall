cd90da651174e1e16bbfb44bc04a3d00e7819c5f
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-08-01 11:09:44 +0000

    DRY up Edition::TopicsTest

--- test/unit/edition/topics_test.rb
+++ test/unit/edition/topics_test.rb
@@ -1,22 +1,24 @@
 require "test_helper"
 
 class Edition::TopicsTest < ActiveSupport::TestCase
+  setup do
+    @topic = create(:topic)
+  end
+
   test "#destroy should also remove the classification membership relationship" do
-    topic = create(:topic)
-    edition = create(:draft_policy, topics: [topic])
+    edition = create(:draft_policy, topics: [@topic])
     relation = edition.classification_memberships.first
     edition.destroy
     refute ClassificationMembership.find_by_id(relation.id)
   end
 
   test "new edition of document that is a member of a topic should remain a member of that topic" do
-    topic = create(:topic)
-    edition = create(:published_policy, topics: [topic])
+    edition = create(:published_policy, topics: [@topic])
 
     new_edition = edition.create_draft(create(:policy_writer))
     new_edition.change_note = 'change-note'
     new_edition.publish_as(create(:departmental_editor), force: true)
 
-    assert_equal topic, new_edition.topics.first
+    assert_equal @topic, new_edition.topics.first
   end
 end
