17cb6325d7fc03de457960f64ba00aa3d39caf21
Tekin Suleyman <tekin@tekin.co.uk>
2013-08-01 09:30:38 +0100

    Integration test for importing official docs

---
+++ test/integration/official_docs_import_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class OfficialDocsImportTest < ActiveSupport::TestCase
+  test "importer recognises attachments with hoc/command numbers and parliamentary sessions" do
+    creator = User.create!(name: "Automatic Data Importer")
+    od_document_series = create(:document_series, name: "official-documents")
+    hoc_document_series = create(:document_series, name: "house-of-commons-papers")
+    organisation = create(:organisation_with_alternative_format_contact_email, name: "the-national-archives")
+
+    stub_request(:get, "http://www.official-documents.gov.uk/document/hc0708/hc10/1043/1043.pdf").to_return(body: "attachment-1-content")
+
+    filename = Rails.root.join("test/fixtures/official_docs_import_sample.csv")
+    file = stub("uploaded file", read: File.read(filename), original_filename: filename)
+    import = Import.create_from_file(creator, file, "publication", organisation.id)
+    assert import.valid?, import.errors.full_messages.join(", ")
+    import.perform
+
+    assert_equal [], import.import_errors
+
+    assert publication = import.editions.first
+    assert_equal creator, publication.creator
+    assert_equal [organisation], publication.organisations
+    assert_equal [od_document_series, hoc_document_series], publication.document_series
+
+    assert_equal 1, publication.attachments.size
+    attachment = publication.attachments.first
+    simulate_virus_scan(attachment.attachment_data.file)
+    assert_equal "attachment-1-content", File.read(attachment.file.path)
+    assert_equal "1043 2007-08", attachment.hoc_paper_number
+    assert_equal "2007-08", attachment.parliamentary_session
+    refute attachment.unnumbered_command_paper?
+    refute attachment.unnumbered_hoc_paper?
+  end
+end
