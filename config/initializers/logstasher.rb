0bbc1055b9ae8b23c9fe672c7d0a799090e74bdf
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-02 11:54:22 +0100

    Custom @fields.request format to match Nginx logs

---
+++ config/initializers/logstasher.rb
@@ -0,0 +1,5 @@
+if LogStasher.enabled
+  LogStasher.add_custom_fields do |fields|
+    fields[:request] = "#{request.request_method} #{request.path} #{request.headers['SERVER_PROTOCOL']}"
+  end
+end
