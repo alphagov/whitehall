7ed9167448d74afa1d9f4e4f8314d1861432a94c
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-02 13:56:44 +0100

    Protect against LogStasher not being defined

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -1,4 +1,4 @@
-if LogStasher.enabled
+if Object.const_defined?('LogStasher') && LogStasher.enabled
   LogStasher.add_custom_fields do |fields|
     fields[:request] = "#{request.request_method} #{request.path} #{request.headers['SERVER_PROTOCOL']}"
   end
