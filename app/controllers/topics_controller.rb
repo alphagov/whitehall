cc7aec0d0f303fcbf5214f2d6a835087d0ed8516
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-08-03 19:21:33 +0000

    Tidy up TopicsController

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,8 +2,8 @@ class TopicsController < ClassificationsController
   def show
     @classification = Topic.find(params[:id])
     @policies = @classification.published_policies
-    @publications = decorate_collection(Publication.published_in_topic([@classification]).in_reverse_chronological_order.limit(3), PublicationesquePresenter)
-    @announcements = decorate_collection(Announcement.published_in_topic([@classification]).in_reverse_chronological_order.limit(3), AnnouncementPresenter)
+    @publications = presenters_for_latest(:publicationesque)
+    @announcements = presenters_for_latest(:announcement)
     @detailed_guides = @classification.detailed_guides.published.limit(5)
     @related_classifications = @classification.related_classifications
     @recently_changed_documents = @classification.recently_changed_documents
@@ -11,9 +11,11 @@ class TopicsController < ClassificationsController
     set_slimmer_page_owner_header(@classification.lead_organisations.first)
     set_meta_description(@classification.description)
 
-    expire_on_next_scheduled_publication(@classification.scheduled_editions +
+    expire_on_next_scheduled_publication(
+      @classification.scheduled_editions +
       Publication.scheduled_in_topic([@classification]) +
-      Announcement.scheduled_in_topic([@classification]))
+      Announcement.scheduled_in_topic([@classification])
+    )
 
     respond_to do |format|
       format.html do
@@ -23,5 +25,17 @@ class TopicsController < ClassificationsController
         @recently_changed_documents = @recently_changed_documents[0...10]
       end
     end
+  end
+
+  private
+  def presenters_for_latest(type)
+    presenter_cls = Object.const_get(type.to_s.classify + 'Presenter')
+    decorate_collection(latest_editions(type), presenter_cls)
+  end
+
+  def latest_editions(type)
+    type_cls = Object.const_get(type.to_s.classify)
+    scope = type_cls.published_in_topic([@classification])
+    scope.in_reverse_chronological_order.limit(3)
   end
 end
