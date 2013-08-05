cab4ab70952d17bb2afef889f8c4643a48030726
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-08-05 16:32:31 +0000

    Improve functional tests on FinancialReportsController

--- test/functional/admin/financial_reports_controller_test.rb
+++ test/functional/admin/financial_reports_controller_test.rb
@@ -8,22 +8,18 @@ class Admin::FinancialReportsControllerTest < ActionController::TestCase
   
   should_be_an_admin_controller
 
-  test "GET on :index is found" do
-    organisation = FactoryGirl.create(:organisation)
-    get :index, organisation_id: organisation
-
-    assert_response :success
+  view_test "GET :index lists the organisation's reports" do
+    report = create(:financial_report, funding: 20000)
+    get :index, organisation_id: report.organisation
+    assert_select 'td', text: report.year
+    assert_select 'td', /20,000/
   end
   
   test "POST to :create with valid data creates a new financial_report" do
-    organisation = FactoryGirl.create(:organisation)
-    post :create,
-    financial_report: {
-      year: 2013,
-      spending: 2400,
-      funding: 2380
-    },
-    organisation_id: organisation.id
+    organisation = create(:organisation)
+    post :create, organisation_id: organisation, financial_report: {
+      year: 2013, spending: 2400, funding: 2380
+    }
 
     assert_redirected_to admin_organisation_financial_reports_url(organisation)
     assert_equal "Created Financial Report", flash[:notice]
@@ -31,45 +27,35 @@ class Admin::FinancialReportsControllerTest < ActionController::TestCase
   end
   
   test "PUT to :update with valid data updates the financial report" do
-    financial_report = FactoryGirl.create(:financial_report)
+    financial_report = create(:financial_report)
     organisation = financial_report.organisation
-    put :update,
-    financial_report: {
-      year: financial_report.year,
-      spending: 4096,
-      funding: 0,
-    },
-    id: financial_report.id,
-    organisation_id: financial_report.organisation_id
-
+    put :update, organisation_id: organisation, id: financial_report, financial_report: {
+      year: financial_report.year, spending: 4096, funding: 0
+    }
     
     assert_redirected_to admin_organisation_financial_reports_url(organisation)
     assert_equal "Updated Financial Report", flash[:notice]
     assert_equal 1, organisation.financial_reports.count
   end
 
-  test "PUT to :update with invalid data gives error 400" do
-    financial_report = FactoryGirl.create(:financial_report)
+  view_test "PUT to :update with invalid data re-renders the form with code 400" do
+    financial_report = create(:financial_report)
     organisation = financial_report.organisation
-    put :update,
-    financial_report: {
-      year: 'not-a-year',
-      spending: 0,
-      funding: 0
-    },
-    id: financial_report.id,
-    organisation_id: financial_report.organisation_id
-  
+    put :update, organisation_id: organisation, id: financial_report, financial_report: {
+      year: 'not-a-year', spending: 0, funding: 0
+    }
+    assert_select 'div.alert'
     assert_response :bad_request
   end
 
   test "DELETE on a financial report removes it" do
-    financial_report = FactoryGirl.create(:financial_report, year: 2013, spending: 0, funding: 0)
+    financial_report = create(:financial_report, year: 2013, spending: 0, funding: 0)
     organisation = financial_report.organisation
     
-    delete :destroy, organisation_id: financial_report.organisation_id, id: financial_report.id
+    assert_difference 'organisation.financial_reports.count', -1 do
+      delete :destroy, organisation_id: organisation, id: financial_report
+    end
 
     assert_redirected_to admin_organisation_financial_reports_url(organisation)
-    assert_equal 0, organisation.financial_reports.count
   end
 end
