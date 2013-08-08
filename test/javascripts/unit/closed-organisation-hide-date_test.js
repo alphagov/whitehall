4a568906863d5f85b86103e01d6af3862369767e
Ben Firshman <ben@firshman.co.uk>
2013-08-08 11:24:53 +0100

    Fix broken closed-organisation hide-date test

--- test/javascripts/unit/closed-organisation-hide-date_test.js
+++ test/javascripts/unit/closed-organisation-hide-date_test.js
@@ -1,6 +1,6 @@
 module("create-new", {
   setup: function() {
-    $('#qunit-fixture').append('<select id="organisation_govuk_status" name="organisation_govuk_status"><option value="closed">Closed</option><option value="open">Open</option></select><div id="organisation_closed_at_group"><label for="organisation_closed_at">Closed at?</label><input id="test_child"></div>');
+    $('#qunit-fixture').append('<select id="organisation_govuk_status" name="organisation_govuk_status"><option value="closed">Closed</option><option value="open" selected="selected">Open</option></select><div id="organisation_closed_at_group"><label for="organisation_closed_at">Closed at?</label><input id="test_child"></div>');
   }
 });
 
@@ -8,5 +8,5 @@ test("Should hide child elements", function() {
   var $select = $('.organisation_govuk_status'),
       $group = $('#organisation_closed_at_group');
   GOVUK.hideClosedAtDates();
-  ok($group.find('*').is(':hidden');
+  ok($group.find('*').is(':hidden'));
 });
