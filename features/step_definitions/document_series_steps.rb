0f8259658b1d6f4d74b0e2d8f50afe9e5b2f152e
Tekin Suleyman <tekin@tekin.co.uk>
2013-08-12 12:26:04 +0100

    Rework document series management scenarios
    Regressions for the key features and those not covered by existing
    functional tests

--- features/step_definitions/document_series_steps.rb
+++ features/step_definitions/document_series_steps.rb
@@ -1,76 +1,42 @@
-Given /^I create a series called "([^"]*)" in the "([^"]*)" organisation$/ do |name, organisation_name|
-  organisation = Organisation.find_by_name!(organisation_name)
+Given(/^a document series "([^"]*)" exists$/) do |name|
+  @document_series = create(:document_series, name: name)
+end
 
-  visit admin_organisations_path
-  within record_css_selector(organisation) do
-    click_link organisation_name
+Given(/^a published publication called "(.*?)" in the series "(.*?)"$/) do |publication_name, series_name|
+  @publication = create(:published_publication, title: publication_name)
+  @document_series = create(:document_series, name: series_name, documents: [@publication.document])
+end
+
+Then(/^I should be able to search for "(.*?)" and add the document to the series$/) do |search_text|
+  visit admin_organisation_document_series_path(@document_series.organisation, @document_series)
+  click_on 'Series documents'
+  fill_in 'title', with: search_text
+  click_button "Search"
+  edition = Edition.last
+
+  within record_css_selector(edition, 'search') do
+    click_on "Add to this series"
   end
-  within ".organisation-details" do
-    click_link "Document series"
+
+  within ('#series-documents') do
+    assert page.has_content? edition.title
   end
-  click_link "New series"
-  fill_in "Name", with: name
-  fill_in "Summary", with: "This is a summary of #{name}"
-  click_button "Save"
+
+  assert @document_series.documents.include?(edition.document), "Document has not been added to the series"
 end
 
-Given /^a document series "([^"]*)" exists$/ do |name|
-  create(:document_series, name: name)
-end
-
-Given /^series from several other organisations exist$/ do
-  create(:document_series)
-  create(:document_series)
-end
-
-When /^I create a document called "([^"]*)" in the "([^"]*)" series$/ do |title, series|
-  begin_drafting_publication(title)
-  select series, from: "Document series"
-  click_button "Save"
-end
-
-When /^I view the "([^"]*)" series$/ do |series_name|
-  series = DocumentSeries.find_by_name(series_name)
-  # It would be better to navigate to this, but at the moment we're not sure
-  # where the seriess will sit
-  visit organisation_document_series_path(series.organisation, series)
-end
-
-Then /^I should see a summary of the series clearly displayed$/ do
-  assert page.has_css?(".summary p", DocumentSeries.last.summary)
-end
-
-Then /^I should see links to all the documents in the "([^"]*)" series$/ do |series_name|
-  series = DocumentSeries.find_by_name(series_name)
-  series.editions.each do |edition|
-    assert page.has_css?("a[href='#{public_document_path(edition)}']", text: edition.title)
+Then(/^I should be able to remove the publication from the series$/) do
+  visit admin_organisation_document_series_path(@document_series.organisation, @document_series)
+  click_on 'Series documents'
+  within record_css_selector(@publication) do
+    click_on 'Remove'
   end
+
+  page.has_content?('removed from series')
+  refute @document_series.reload.documents.include?(@publication.document), "Document has not been removed from the series"
 end
 
-Then /^I should see links back to the "([^"]*)" series$/ do |series_name|
-  series = DocumentSeries.find_by_name(series_name)
-  organisation = series.organisation
-  assert page.has_css?("a[href='#{organisation_document_series_path(organisation, series)}']")
-end
-
-Then /^I should see the series from "([^"]*)" first in the series list$/ do |organisation_name|
-  assert page.has_css?("select optgroup:nth-child(1)[label='#{organisation_name}']")
-end
-
-Given /^a series "([^"]*)" for the organisation "([^"]*)"$/ do |series_name, organisation_name|
-  organisation = create(:organisation, name: organisation_name)
-  series = create(:document_series, name: series_name, organisation: organisation)
-end
-
-Given /^a published publication "([^"]*)" in the series "([^"]*)"$/ do |publication_name, series_name|
-  series = DocumentSeries.find_by_name(series_name)
-  publication = create(:published_publication, title: publication_name, document_series: [series], organisations: [series.organisation])
-end
-
-Then /^I should see the publication "([^"]*)" belongs to the "([^"]*)" series$/ do |publication_name, series_name|
-  publication = Publication.find_by_title(publication_name)
-  series = DocumentSeries.find_by_name(series_name)
-  within record_css_selector(publication) do
-    assert page.has_css? "a[href='#{organisation_document_series_path(series.organisation, series)}']", text: series.name
-  end
+Then(/^I should see links back to the series$/) do
+  organisation = @document_series.organisation
+  assert page.has_css?("a[href='#{organisation_document_series_path(organisation, @document_series)}']")
 end
