065428363593664c20c9356ec428c413b773bb1e
Tekin Suleyman <tekin@tekin.co.uk>
2013-08-23 14:18:21 +0100

    New flag for unnumbered HTML versions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130820151644) do
+ActiveRecord::Schema.define(:version => 20130823095707) do
 
   create_table "about_pages", :force => true do |t|
     t.integer  "topical_event_id"
@@ -700,10 +700,11 @@ ActiveRecord::Schema.define(:version => 20130820151644) do
   create_table "html_versions", :force => true do |t|
     t.integer  "edition_id"
     t.string   "title"
-    t.text     "body",       :limit => 2147483647
+    t.text     "body",              :limit => 2147483647
     t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "manually_numbered",                       :default => false
   end
 
   add_index "html_versions", ["slug"], :name => "index_html_versions_on_slug"
