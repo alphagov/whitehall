866d57c1d8839acafe4309dc37bcc391ee55503f
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-08-26 14:18:48 +0000

    Shorten well understood instance var name

--- app/controllers/admin/document_series_memberships_controller.rb
+++ app/controllers/admin/document_series_memberships_controller.rb
@@ -2,13 +2,17 @@ class Admin::DocumentSeriesMembershipsController  < Admin::BaseController
   before_filter :load_document_series
   before_filter :load_document, only: [:create, :destroy]
 
+  def index
+    @groups = @series.groups
+  end
+
   def create
-    @document_series.documents << @document
+    @series.documents << @document
     flash.now[:notice] = %Q("#{document_title}" added to series)
   end
 
   def destroy
-    @document_series.documents.delete(@document)
+    @series.documents.delete(@document)
     flash.now[:notice] = %Q("#{document_title}" removed from series)
   end
 
@@ -19,7 +23,7 @@ class Admin::DocumentSeriesMembershipsController  < Admin::BaseController
 
   private
   def load_document_series
-    @document_series = DocumentSeries.find(params[:document_series_id])
+    @series = DocumentSeries.find(params[:document_series_id])
   end
 
   def load_document
