69c36204546fce689a9d2633a42ab74178bcb4bb
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-08-26 18:37:26 +0000

    Edit heading/body of doc series groups

--- app/controllers/admin/document_series_groups_controller.rb
+++ app/controllers/admin/document_series_groups_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DocumentSeriesGroupsController < Admin::BaseController
   before_filter :load_document_series
+  before_filter :load_document_series_group, only: [:edit, :update]
 
   def new
     @group = @series.groups.build
@@ -15,8 +16,20 @@ class Admin::DocumentSeriesGroupsController < Admin::BaseController
     end
   end
 
+  def update
+    @group.update_attributes!(params[:document_series_group])
+    redirect_to admin_document_series_documents_path(@series),
+                notice: "'#{@group.heading}' saved"
+  rescue ActiveRecord::RecordInvalid
+    render :edit
+  end
+
   private
   def load_document_series
     @series = DocumentSeries.find(params[:document_series_id])
+  end
+
+  def load_document_series_group
+    @group = @series.groups.find(params[:id])
   end
 end
--- test/functional/admin/document_series_groups_controller_test.rb
+++ test/functional/admin/document_series_groups_controller_test.rb
@@ -1,13 +1,9 @@
 require 'test_helper'
 
 class Admin::DocumentSeriesGroupsControllerTest < ActionController::TestCase
-  def post_create(params = {})
-    params.reverse_merge!(heading: 'Heading', body: '')
-    post :create, document_series_id: @series, document_series_group: params
-  end
-
   setup do
     @series = create(:document_series, :with_group)
+    @group = @series.groups.first
     login_as create(:policy_writer)
   end
 
@@ -16,6 +12,11 @@ class Admin::DocumentSeriesGroupsControllerTest < ActionController::TestCase
   view_test 'GET #new renders successfully' do
     get :new, document_series_id: @series
     assert_response :ok
+  end
+
+  def post_create(params = {})
+    params.reverse_merge!(heading: 'Heading', body: '')
+    post :create, document_series_id: @series, document_series_group: params
   end
 
   test 'POST #create creates a new group from valid data and redirects' do
@@ -30,6 +31,30 @@ class Admin::DocumentSeriesGroupsControllerTest < ActionController::TestCase
 
   view_test 'POST #create prompts for missing data if new group invalid' do
     post_create(heading: '')
+    assert_response :success
+    assert_select '.errors li', text: /Heading/
+  end
+
+  view_test 'GET #edit renders successfully' do
+    get :edit, document_series_id: @series, id: @group
+    assert_response :ok
+  end
+
+  def put_update(params)
+    put :update, document_series_id: @series, id: @group,
+                 document_series_group: params
+  end
+
+  test 'PUT #update modifies the group and redirects' do
+    put_update(heading: 'New heading', body: 'New body')
+    @group.reload
+    assert_equal 'New heading', @group.heading
+    assert_equal 'New body', @group.body
+    assert_redirected_to admin_document_series_documents_path(@series)
+  end
+
+  view_test 'PUT #update prompts for missing data if group invalid' do
+    put_update(heading: '')
     assert_response :success
     assert_select '.errors li', text: /Heading/
   end
