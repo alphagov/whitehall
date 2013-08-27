0174d4a57b06f99bcd3ee986905e75c3ce67af60
Tekin Suleyman <tekin@tekin.co.uk>
2013-08-27 11:27:33 +0100

    Use js-hidden class to stop flashing of buttons

--- app/assets/javascripts/admin/edition_select_filter.js
+++ app/assets/javascripts/admin/edition_select_filter.js
@@ -1,9 +1,7 @@
 (function($) {
   $(function() {
-    $(".js-edition-select-filter input[type=submit]").hide();
     $(".js-edition-select-filter select").change(function() {
       $(this).parent().submit();
     })
   })
 })(jQuery);
-
