1d0d2bf4fd3717daa45ce66300f9d71a03cb1d46
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-08-29 18:18:00 +0000

    Use Rails helper method to generate grouped select
    Perhaps this was built manually as the `with_translations_for` method was
    involved? It doesn't appear to have mattered, as including translations
    in the AREL code seems to have done the job nicely; the HTML produced is
    identical, other than for the order of the optgroup tags (which come out
    in alphabetical order with this new approach).

--- app/helpers/admin/document_series_helper.rb
+++ app/helpers/admin/document_series_helper.rb
@@ -1,20 +1,9 @@
 module Admin::DocumentSeriesHelper
   def document_series_select_options(edition, user, document_series_ids)
-    user_organisation = user.organisation
-    grouped_series = DocumentSeries.with_translations_for(:organisation).all.group_by(&:organisation)
-    primary_series = grouped_series.delete(user_organisation)
-
-    series_options = [%{<option value=""></option>}]
-    if primary_series
-      series_options << (%{<optgroup label="#{user_organisation.name}">} +
-      options_from_collection_for_select(primary_series, 'id', 'name', document_series_ids) +
-      %{</optgroup>})
-    end
-    series_options << grouped_series.map do |organisation, series|
-      %{<optgroup label="#{organisation.name}">} +
-      options_from_collection_for_select(series, 'id', 'name', document_series_ids) +
-      %{</optgroup>}
-    end
-    series_options.join.html_safe
+    orgs = Organisation.scoped.includes(:document_series, :translations).to_a
+    ours = orgs.detect { |org| org == user.organisation }
+    collection = orgs.reject { |org| org == user.organisation }.unshift(ours)
+    option_groups_from_collection_for_select(
+        collection, :document_series, :name, :id, :name)
   end
 end
