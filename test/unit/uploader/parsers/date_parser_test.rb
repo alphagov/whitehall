61f585edd621a42bce758bd2ebbfcfd78958da8c
Tekin Suleyman <tekin@tekin.co.uk>
2013-08-29 17:03:49 +0100

    Make tests more robust

--- test/unit/uploader/parsers/date_parser_test.rb
+++ test/unit/uploader/parsers/date_parser_test.rb
@@ -9,15 +9,15 @@ class Whitehall::Uploader::Parsers::DateParserTest < ActiveSupport::TestCase
   end
 
   test "can parse dates in dd-MMM-yy format" do
-    assert_equal Date.parse('2012-05-23'), Whitehall::Uploader::Parsers::DateParser.parse('23-May-12', @log, @line_number)
+    assert_equal Date.new(2012, 05, 23), Whitehall::Uploader::Parsers::DateParser.parse('23-May-12', @log, @line_number)
   end
 
   test "can parse dates in dd-MMM-yyyy format" do
-    assert_equal Date.parse('2013-07-10'), Whitehall::Uploader::Parsers::DateParser.parse('10-Jul-2013', @log, @line_number)
+    assert_equal Date.new(2013, 07, 10), Whitehall::Uploader::Parsers::DateParser.parse('10-Jul-2013', @log, @line_number)
   end
 
   test "can parse dates in yyyy-mm-dd format" do
-    assert_equal Date.parse('2001-10-31'), Whitehall::Uploader::Parsers::DateParser.parse('2001-10-31', @log, @line_number)
+    assert_equal Date.new(2001, 10, 31), Whitehall::Uploader::Parsers::DateParser.parse('2001-10-31', @log, @line_number)
   end
 
   test "returns nil if passed string is empty" do
