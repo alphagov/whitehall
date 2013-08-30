16fbacdd5ab4158f1237eb99f25301b875586289
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-30 13:45:48 +0100

    Use export:redirector_mappings in the cron job
    This replaces the old and now removed script.
    
    Additionally clean up the now unused job_type.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,8 +5,6 @@ env :PATH, '/usr/local/bin:/usr/bin:/bin'
 
 # We need Rake to use our own environment
 job_type :rake, "cd :path && govuk_setenv whitehall bundle exec rake :task --silent :output"
-# We need Rails to use our own environment
-job_type :runner, "cd :path && govuk_setenv whitehall script/rails runner -e :environment ':task' :output"
 
 # Run every SCHEDULED_PUBLISHING_PRECISION_IN_MINUTES minutes, 2 minutes before the due moment
 # this allows the the ruby process (and rails) time to boot up. The scheduler then sleeps until
@@ -18,5 +16,5 @@ end
 ## 14:45 is during our regular release slot, this may have to change
 ## post-April. 2am is our regular time for this.
 every :day, at: ['2am', '11:45am'], roles: [:admin] do
-  runner 'script/dump_all_admin_to_public_documents_and_non_documents.rb'
+  rake "export:redirector_mappings"
 end
