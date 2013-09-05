1751eac22d072c0e3930475f0b19228bc9f04566
Tekin Suleyman <tekin@tekin.co.uk>
2013-09-05 11:46:05 +0100

    DRY up date rendering

--- app/helpers/admin/document_searches_helper.rb
+++ app/helpers/admin/document_searches_helper.rb
@@ -1,9 +1,7 @@
 module Admin::DocumentSearchesHelper
   def public_time_for_edition(edition)
     if edition.public_timestamp.present?
-      render_datetime_microformat(edition, :public_timestamp) do
-        edition.public_timestamp.to_date.to_s(:long_ordinal)
-      end
+      absolute_date(edition.public_timestamp)
     else
       "(#{edition.state.humanize})"
     end
