4f57814946d53ae50803c6055be5cf31a6af9d9f
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-09-06 13:55:44 +0000

    Reload code in app/uploaders in development

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,13 @@ module Whitehall
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters #{config.root}/app/validators #{config.root}/app/workers)
+    config.autoload_paths += %W(
+      #{config.root}/app/presenters
+      #{config.root}/app/uploaders
+      #{config.root}/app/validators
+      #{config.root}/app/workers
+      #{config.root}/lib
+    )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
