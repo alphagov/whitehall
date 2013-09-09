c2950b307d1633c9386b4ad99e708400808fbcab
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-09-09 17:51:05 +0100

    Use db:schema:load on jenkins
    Rather than running migrations we should load the schema. It avoids the
    need to keep all of the historical migrations (in a runnable condition)

--- jenkins.sh
+++ jenkins.sh
@@ -12,7 +12,7 @@ mkdir -p ./attachment-cache
 
 time bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
-time bundle exec rake db:create db:migrate db:test:prepare --trace
+time bundle exec rake db:create db:schema:load db:test:prepare --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:clean --trace
 RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest default test:cleanup --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:precompile --trace
