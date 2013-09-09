0815e38e1bf76983fe824fd17dcd9a6ee113ced5
Graham Ashton <graham.ashton@digital.cabinet-office.gov.uk>
2013-09-09 18:28:46 +0000

    Migrate HtmlVersion to HtmlAttachment

---
+++ db/migrate/20130909180231_add_html_columns_to_attachments.rb
@@ -0,0 +1,39 @@
+class AddHtmlColumnsToAttachments < ActiveRecord::Migration
+  def up
+    add_column :attachments, :slug, :string
+    add_column :attachments, :body, :text, limit: 4.gigabytes - 1
+    add_column :attachments, :manually_numbered_headings, :boolean
+
+    execute %Q{
+      INSERT INTO attachments
+        (`type`, attachable_id, attachable_type, title, body, slug, manually_numbered_headings, ordering, created_at, updated_at)
+      SELECT 'HtmlAttachment', edition_id, 'Edition', title, body, slug, manually_numbered, 0, created_at, updated_at
+      FROM html_versions
+      WHERE edition_id IS NOT NULL
+      AND title IS NOT NULL
+      AND title != ''
+      AND body IS NOT NULL
+      AND body != '';
+    }
+
+    execute %Q{
+      UPDATE attachments
+      JOIN html_versions
+      ON attachable_id = edition_id
+      SET ordering = ordering + 1
+      WHERE `type` = 'FileAttachment'
+      AND attachable_type = 'Edition'
+      AND edition_id IS NOT NULL
+      AND html_versions.title IS NOT NULL
+      AND html_versions.title != ''
+      AND html_versions.body IS NOT NULL
+      AND html_versions.body != '';
+    }
+  end
+
+  def down
+    remove_column :attachments, :slug
+    remove_column :attachments, :body
+    remove_column :attachments, :manually_numbered_headings
+  end
+end
