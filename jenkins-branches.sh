1d14d80d5371b113ef0b1f691c04ea9c9f47f19a
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-09-10 15:35:38 +0100

    Update branches script to run parallel build

--- jenkins-branches.sh
+++ jenkins-branches.sh
@@ -2,7 +2,6 @@
 export DISPLAY=:99
 export GOVUK_APP_DOMAIN=test.gov.uk
 export GOVUK_ASSET_ROOT=http://static.test.gov.uk
-export TEST_ENV_NUMBER=1
 env
 
 # Generate directories for upload tests
@@ -13,9 +12,10 @@ mkdir -p ./attachment-cache
 
 time bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
-RAILS_ENV=test time bundle exec rake db:reset db:schema:load --trace
+time bundle exec rake db:drop db:create db:schema:load --trace
+time bundle exec rake db:test:prepare --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:clean --trace
-RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest default test:cleanup --trace
+RAILS_ENV=test CUCUMBER_FORMAT=progress time bundle exec rake ci:setup:minitest parallel:create parallel:prepare test_queue shared_mustache:compile parallel:features test:javascript test:cleanup --trace
 RAILS_ENV=production SKIP_OBSERVERS_FOR_ASSET_TASKS=true time bundle exec rake assets:precompile --trace
 
 EXIT_STATUS=$?
