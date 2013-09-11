86971c3d1bf51a280eb305b5e38b71e1c98bba55
Will Tomlins <will.tomlins@unboxedconsulting.com>
2013-09-11 11:32:38 +0100

    Migration to add new devolved administration organisations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130910101513) do
+ActiveRecord::Schema.define(:version => 20130911094439) do
 
   create_table "about_pages", :force => true do |t|
     t.integer  "topical_event_id"
