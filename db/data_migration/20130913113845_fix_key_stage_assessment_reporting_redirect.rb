94da9e9127c6fbe615cb5c41915d567fce98a1b8
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-09-13 12:44:38 +0100

    Add data migration to fix bad unpublishing

---
+++ db/data_migration/20130913113845_fix_key_stage_assessment_reporting_redirect.rb
@@ -0,0 +1,3 @@
+Unpublishing.from_slug('assessment-and-reporting-arrangements-key-stage-2-2014', Publication).
+  update_attributes({alternative_url: 'https://education.gov.uk/schools/teachingandlearning/assessment/keystage2/g00227867/ks2-assessment-reporting',
+                     redirect: true})
