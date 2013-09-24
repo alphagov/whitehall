c0c3ac22b58acab6d53050c960fac99479158386
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-09-24 15:08:23 +0100

    [DG-UN] Add autocomplete for user needs fields.

--- app/models/user_need.rb
+++ app/models/user_need.rb
@@ -1,6 +1,10 @@
 class UserNeed < ActiveRecord::Base
   belongs_to :organisation
 
+  def self.existing_content(field_type)
+    UserNeed.order(field_type).pluck(field_type).uniq
+  end
+
   def to_s
     "As a #{user} I need to #{need} in order to #{goal}"
   end
