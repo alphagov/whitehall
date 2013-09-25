2da7809e7daf6495f1ad6aa6e6bee5539f80be5c
Tekin Suleyman <tekin@tekin.co.uk>
2013-09-25 10:18:36 +0100

    Simplify tab_navigation_for helper

--- app/helpers/admin/tabbed_nav_helper.rb
+++ app/helpers/admin/tabbed_nav_helper.rb
@@ -1,19 +1,7 @@
 module Admin::TabbedNavHelper
-  def tab_navigation_for(content_object, *extra_classes, &block)
-    case content_object
-    when Organisation
-      tab_navigation(organisation_tabs(content_object), *extra_classes, &block)
-    when WorldwideOrganisation
-      tab_navigation(worldwide_organisation_tabs(content_object), *extra_classes, &block)
-    when WorldLocation
-      tab_navigation(world_location_tabs(content_object), *extra_classes, &block)
-    when Person
-      tab_navigation(person_tabs(content_object), *extra_classes, &block)
-    when TopicalEvent
-      tab_navigation(topical_event_tabs(content_object), *extra_classes, &block)
-    when Topic
-      tab_navigation(topic_tabs(content_object), *extra_classes, &block)
-    end
+  def tab_navigation_for(model, *extra_classes, &block)
+    tabs = send("#{model.class.model_name.underscore}_tabs", model)
+    tab_navigation(tabs, *extra_classes, &block)
   end
 
   def document_series_tab_navigation(organisation, document_series, &block)
