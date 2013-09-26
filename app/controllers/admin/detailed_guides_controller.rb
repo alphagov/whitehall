e6e9b1ef85c881a4f0edd559f506547d946dfae4
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-09-26 14:37:24 +0100

    Don't blat pre-entered user needs.

--- app/controllers/admin/detailed_guides_controller.rb
+++ app/controllers/admin/detailed_guides_controller.rb
@@ -8,4 +8,10 @@ class Admin::DetailedGuidesController < Admin::EditionsController
   def edition_class
     DetailedGuide
   end
+
+  def new_user_need
+     @new_user_need ||= @edition.user_needs.to_a.detect(&:new_record?) || UserNeed.new
+  end
+  helper_method :new_user_need
+
 end
