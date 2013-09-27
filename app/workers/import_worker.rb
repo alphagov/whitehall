595e6eebdbb6003c148dae1c7115aab8b989faf1
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-09-27 10:52:16 +0100

    Tidy up ImportWorker.

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -3,21 +3,12 @@ class ImportWorker
   sidekiq_options queue: :imports
 
   def perform(id, options={})
-    import(id)
-    @import.perform options
+    Import.use_separate_connection
+    @import = Import.find(id)
+    @import.perform(options)
   end
 
   def error(job, error)
     @import.progress_logger.error(error.to_s + error.backtrace.join("\n"), nil)
   end
-
-  private
-
-  def import(id)
-    @import ||= begin
-                  Import.use_separate_connection
-                  Import.find(id)
-                end
-  end
-
 end
