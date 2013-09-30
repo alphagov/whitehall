d6b6c1a00e09ad9c8ad8c82d39faf53d6e2ba29f
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-09-30 22:19:45 +0100

    Moved set_cache_max_age out of before_filter

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,6 @@ class TopicsController < ClassificationsController
   include CacheControlHelper
 
   skip_before_filter :set_cache_control_headers, only: [:show], if: :is_html?
-  before_filter :set_cache_max_age, only: [:show], if: :is_html?
 
   def show
     @classification = Topic.find(params[:id])
@@ -15,11 +14,14 @@ class TopicsController < ClassificationsController
         @detailed_guides = @classification.detailed_guides.published.includes(:translations, :document).limit(5)
         @related_classifications = @classification.related_classifications
         @featured_editions = decorate_collection(@classification.classification_featurings.includes(:image, edition: [:document, :translations]).limit(5), FeaturedEditionPresenter)
+
         set_slimmer_organisations_header(@classification.organisations.includes(:translations))
         set_slimmer_page_owner_header(@classification.lead_organisations.includes(:translations).first)
         set_meta_description(@classification.description)
 
+        set_cache_max_age
         expire_on_next_scheduled_publication(@classification.scheduled_editions)
+
         @recently_changed_documents = @classification.latest(3)
       end
       format.atom do
