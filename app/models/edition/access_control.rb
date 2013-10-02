e087e19cb1eefa39da87360ffbe2da99ac5c6e67
Tekin Suleyman <tekin@tekin.co.uk>
2013-10-02 11:30:24 +0100

    Move related methods together
    It doesn't make sense for the reject/approve-related methods to be
    apart from the rest of the publishing-related methods

--- app/models/edition/access_control.rb
+++ app/models/edition/access_control.rb
@@ -6,24 +6,4 @@ module Edition::AccessControl
   def can_have_some_invalid_data?
     imported? || deleted? || archived?
   end
-
-  def rejectable_by?(user)
-    submitted? && enforcer(user).can?(:reject)
-  end
-
-  def approvable_retrospectively_by?(user)
-    !reason_to_prevent_retrospective_approval_by(user)
-  end
-
-  def reason_to_prevent_retrospective_approval_by(user)
-    if !force_published?
-      "This document has not been force-published"
-    elsif scheduled_by && user == scheduled_by
-      "You are not allowed to retrospectively approve this document, since you force-scheduled it"
-    elsif user == published_by
-      "You are not allowed to retrospectively approve this document, since you force-published it"
-    elsif !enforcer(user).can?(:approve)
-      "Only departmental editors can retrospectively approve a force-published document"
-    end
-  end
 end
--- app/models/edition/publishing.rb
+++ app/models/edition/publishing.rb
@@ -106,6 +106,26 @@ module Edition::Publishing
     errors
   end
 
+  def rejectable_by?(user)
+    submitted? && enforcer(user).can?(:reject)
+  end
+
+  def approvable_retrospectively_by?(user)
+    !reason_to_prevent_retrospective_approval_by(user)
+  end
+
+  def reason_to_prevent_retrospective_approval_by(user)
+    if !force_published?
+      "This document has not been force-published"
+    elsif scheduled_by && user == scheduled_by
+      "You are not allowed to retrospectively approve this document, since you force-scheduled it"
+    elsif user == published_by
+      "You are not allowed to retrospectively approve this document, since you force-published it"
+    elsif !enforcer(user).can?(:approve)
+      "Only departmental editors can retrospectively approve a force-published document"
+    end
+  end
+
   def publish_as(user, options = {})
     if publishable_by?(user, options)
       self.major_change_published_at = Time.zone.now unless self.minor_change?
