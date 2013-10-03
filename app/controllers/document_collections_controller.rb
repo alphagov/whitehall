900f3451ca4f9dc015d92e7a3e544723f8d4b3e4
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-10-03 17:46:38 +0100

    Set meta description on doc collection show

--- app/controllers/document_collections_controller.rb
+++ app/controllers/document_collections_controller.rb
@@ -2,6 +2,7 @@ class DocumentCollectionsController < DocumentsController
   def show
     expire_on_next_scheduled_publication(@document.editions)
     @document_collection = @document
+    set_meta_description(@document_collection.summary)
     @groups = @document_collection.groups.visible.map do |group|
       editions = EditionCollectionPresenter.new(group.published_editions, view_context)
       [group, editions]
