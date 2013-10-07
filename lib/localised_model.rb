8353b4aaad3cec619d4a1b3a8dc8ff601f2a9e57
Dominic Baggott <dominic.baggott@gmail.com>
2013-10-07 11:58:37 +0100

    Make LocalisedModel localise assocations

--- lib/localised_model.rb
+++ lib/localised_model.rb
@@ -1,15 +1,48 @@
 class LocalisedModel < BasicObject
   attr_reader :fixed_locale
 
-  def initialize(model, locale)
+  def initialize(model, locale, associations = [])
     @model = model
     @fixed_locale = locale
+    @associations_to_localise = Array(associations)
     @model.instance_variable_set(:@errors, ::ErrorsInEnglish.new(@model))
   end
 
   def method_missing(method, *args, &block)
     ::I18n.with_locale @fixed_locale do
-      @model.__send__ method, *args, &block
+      response = @model.__send__(method, *args, &block)
+
+      # Automatically localise any ActiveRecord associations
+      localise_association(method, response)
+    end
+  end
+
+  # Rails calls this a lot in form builder code. By default #to_model will
+  # revert our localised model back to the standard model, so we override it
+  # to get the behaviour we want.
+  def to_model
+    self
+  end
+
+private
+  def localise_association(method, response)
+    return response unless @model.class.respond_to?(:reflect_on_association)
+
+    association = @model.class.reflect_on_association(method)
+    return response if association.nil?
+
+    return response unless @associations_to_localise.include?(method)
+
+    association_class = association.collection? ? association.class_name.constantize : response.class
+    return response unless association_class.respond_to?(:translates?) && association_class.translates?
+
+    if association.collection?
+      # Converting to an array early isn't ideal, but Rails expects this to be
+      # array-like throughout the form builder code and will just reload the
+      # collection from scratch if it doesn't behave like one.
+      response.map { |r| ::LocalisedModel.new(r, @fixed_locale) }
+    else
+      ::LocalisedModel.new(response, @fixed_locale)
     end
   end
 end
