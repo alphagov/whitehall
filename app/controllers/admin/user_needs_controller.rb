03a774bab2f6c0e75de68127dd6346acf56c1eea
Dominic Baggott <dominic.baggott@gmail.com>
2013-10-09 14:54:48 +0100

    Add validation to UserNeed
    Test that the user needs controller responds appropriately
    if given invalid data

--- app/controllers/admin/user_needs_controller.rb
+++ app/controllers/admin/user_needs_controller.rb
@@ -1,7 +1,12 @@
 class Admin::UserNeedsController < Admin::BaseController
   def create
-    user_need = UserNeed.create(params[:user_need])
-    render json: { id: user_need.id, text: user_need.to_s }
+    user_need = UserNeed.new(params[:user_need])
+
+    if user_need.save
+      render json: { id: user_need.id, text: user_need.to_s }
+    else
+      render json: { errors: user_need.errors }, status: 422
+    end
   end
 end
 
--- test/functional/admin/user_needs_controller_test.rb
+++ test/functional/admin/user_needs_controller_test.rb
@@ -17,5 +17,14 @@ class Admin::UserNeedsControllerTest < ActionController::TestCase
     assert_equal user_need.id, json_response["id"]
     assert_equal user_need.to_s, json_response["text"]
   end
+
+  test "POST on :create with invalid data returns errors" do
+    data = { user: "test user", need: "this to work", goal: "" }
+    post :create, format: :json, user_need: data
+
+    assert_response 422
+
+    assert json_response["errors"].any?
+  end
 end
 
