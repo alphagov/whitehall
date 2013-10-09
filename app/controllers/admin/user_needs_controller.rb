bc9bfadc1d8d8c75aaf628dd5fb12bd441477431
Dominic Baggott <dominic.baggott@gmail.com>
2013-10-09 14:52:43 +0100

    Tidy up user needs controller and test

--- app/controllers/admin/user_needs_controller.rb
+++ app/controllers/admin/user_needs_controller.rb
@@ -1,11 +1,7 @@
 class Admin::UserNeedsController < Admin::BaseController
   def create
     user_need = UserNeed.create(params[:user_need])
-    obj = {
-      id: user_need.id,
-      text: user_need.to_s
-    }
-    render json: obj.to_json
+    render json: { id: user_need.id, text: user_need.to_s }
   end
 end
 
--- test/functional/admin/user_needs_controller_test.rb
+++ test/functional/admin/user_needs_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Admin::UserNeedsControllerTest < ActionController::TestCase
   setup do
@@ -8,14 +8,14 @@ class Admin::UserNeedsControllerTest < ActionController::TestCase
   should_be_an_admin_controller
 
   test "POST on :create creates a new user need" do
-    data = {user: 'test user', need: 'this to work', goal: 'pass my tests'}
+    data = { user: "test user", need: "this to work", goal: "pass my tests" }
     post :create, format: :json, user_need: data
 
     assert_response :success
     assert user_need = UserNeed.last
 
-    assert_equal json_response['id'], user_need.id
-    assert_equal json_response['text'], user_need.to_s
+    assert_equal user_need.id, json_response["id"]
+    assert_equal user_need.to_s, json_response["text"]
   end
 end
 
