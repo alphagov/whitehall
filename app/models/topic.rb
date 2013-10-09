bf2f25c1c6b481f118433d48f7676b64e4ccfd26
Will Tomlins <will.tomlins@unboxedconsulting.com>
2013-10-09 14:33:16 +0100

    Dried up top task inclusion in models

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,3 @@
 class Topic < Classification
-  has_many :top_tasks, as: :linkable, dependent: :destroy, order: :created_at
-
-  accepts_nested_attributes_for :top_tasks, reject_if: -> attributes { attributes['url'].blank? }, allow_destroy: true
+  include HasTopTasks
 end
