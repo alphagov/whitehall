75681b205b122f9cfd16d990195ddc5ff56b2ec5
Dominic Baggott <dominic.baggott@gmail.com>
2013-10-10 11:33:14 +0100

    Overload DetailedGuide#display_type

--- app/models/detailed_guide.rb
+++ app/models/detailed_guide.rb
@@ -68,6 +68,10 @@ class DetailedGuide < Edition
     additional_related_mainstream_content_url.present?
   end
 
+  def display_type
+    "Detailed guide"
+  end
+
   def display_type_key
     "detailed_guidance"
   end
