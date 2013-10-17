5e2f1d3264b0b04ed6b1273cebfe3bb08fd77a22
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-10-17 10:26:11 +0100

    Quick fix to restore admin delete confirmations.
    Tech debt:
    This is enough to fix the reported bug, but the
    double click protection needs rewriting, and
    Rails UJS needs removing and replacing with
    something clearer.
    
    This is associated with a move to update jQuery on
    admin (currently v1.7.1 from 2011).

--- app/assets/javascripts/common/double_click_protection.js
+++ app/assets/javascripts/common/double_click_protection.js
@@ -6,10 +6,14 @@
   if(typeof root.GOVUK === 'undefined') { root.GOVUK = {}; }
 
   var doubleClickProtection = function () {
-    $('form input[type=submit]').on('click', function () {
+    $('form input[type=submit]').live('click', function () {
       var submit = $(this),
           name = submit.attr('name'),
           value = submit.val();
+
+      if (submit.data('confirm')) {
+        return;
+      }
 
       submit.before('<input type="hidden" name="' + name + '" value="' + value + '">');
       submit.attr('disabled', 'disabled');
