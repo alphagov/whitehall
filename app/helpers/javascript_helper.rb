826aabf79e7d4b7bb835820bad08b12382918f03
Will Tomlins <will.tomlins@unboxedconsulting.com>
2013-10-21 12:35:03 +0100

    JavascriptHelper.initialise_script raws output
    params passed to initialise script were being escaped. This
    fixes that.

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -1,5 +1,5 @@
 module JavascriptHelper
   def initialise_script(script_name, params = {})
-    content_for :javascript_initialisers, "GOVUK.init(#{script_name}, #{params.to_json})";
+    content_for :javascript_initialisers, raw("GOVUK.init(#{script_name}, #{params.to_json})");
   end
 end
