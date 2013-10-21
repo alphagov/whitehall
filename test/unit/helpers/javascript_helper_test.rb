0f2e2d944807a9bd8bcfcfb99cb785cda497c2e5
Will Tomlins <will.tomlins@unboxedconsulting.com>
2013-10-21 14:17:37 +0100

    Fixed javascript_helper_test.rb...
    and added a test to ensure helper returns an html_safe string.

--- test/unit/helpers/javascript_helper_test.rb
+++ test/unit/helpers/javascript_helper_test.rb
@@ -6,15 +6,22 @@ class JavascriptHelperTest < ActionView::TestCase
   test "initialise_script should render a script initialisation to :javascript_initialisers" do
     self.stubs(:content_for).with() {|yield_block, script|
       assert_equal :javascript_initialisers, yield_block
-      assert script.include?("Whitehall.init(Whitehall.SomeObject"), "expected #{script} to include 'Whitehall.init(Whitehall.SomeObject'"
+      assert script.include?("GOVUK.init(GOVUK.SomeObject"), "expected #{script} to include 'GOVUK.init(GOVUK.SomeObject'"
     }
-    initialise_script "Whitehall.SomeObject"
+    initialise_script "GOVUK.SomeObject"
   end
 
   test "initialise_script should pass params down to script initialisation as json" do
     self.stubs(:content_for).with() {|yield_block, script|
       assert script.include?('{"foo":"bar"}'), "expected #{script} to include '{\"foo\":\"bar\"}'"
     }
-    initialise_script "Whitehall.SomeObject", foo: "bar"
+    initialise_script "GOVUK.SomeObject", foo: "bar"
+  end
+
+  test "initialise_script should return a string marked as html_safe" do
+    self.stubs(:content_for).with() {|yield_block, script|
+      assert script.html_safe?
+    }
+    initialise_script "GOVUK.SomeObject"
   end
 end
