a212e843ffbb94bf5f30b678ec4fa4130722e87d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-10-22 15:29:07 +0100

    Add manually numbered headings field back into HTML attachments

--- features/step_definitions/attachment_steps.rb
+++ features/step_definitions/attachment_steps.rb
@@ -50,6 +50,7 @@ When(/^I upload an html attachment with the title "(.*?)" and the body "(.*?)"$/
   click_on 'Add new HTML attachment'
   fill_in 'Title', with: title
   fill_in 'Body', with: body
+  check 'Manually numbered headings'
   click_on 'Save'
 end
 
