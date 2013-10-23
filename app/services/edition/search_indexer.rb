c902ba0d4c886475491608ebfae4b7853a23e62b
Tekin Suleyman <tekin@tekin.co.uk>
2013-10-23 14:04:14 +0100

    Sprinkle on some OO
    Functionally the same, but arguably makes the code more readable.

--- app/services/edition/search_indexer.rb
+++ app/services/edition/search_indexer.rb
@@ -1,16 +1,27 @@
 class Edition::SearchIndexer
+  attr_accessor :edition
 
   def self.edition_published(edition, options={})
     if edition.can_index_in_search?
-      Searchable::Index.later(edition)
-
-      if edition.allows_supporting_pages?
-        edition.supporting_pages.each { |supporting_page| Searchable::Index.later(supporting_page) }
-      end
-
-      if edition.is_a?(Policy)
-        PolicySearchIndexObserver::ReindexRelatedEditions.later(edition)
-      end
+      new(edition).index!
     end
+  end
+
+  def initialize(edition)
+    @edition = edition
+  end
+
+  def index!
+    Searchable::Index.later(edition)
+    index_supporting_pages if edition.allows_supporting_pages?
+    index_related_editions if edition.is_a?(Policy)
+  end
+
+  def index_supporting_pages
+    edition.supporting_pages.each { |supporting_page| Searchable::Index.later(supporting_page) }
+  end
+
+  def index_related_editions
+    PolicySearchIndexObserver::ReindexRelatedEditions.later(edition)
   end
 end
