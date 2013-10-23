9770f789fcc56641d7cc570be0e86c39fd5f15ef
Tekin Suleyman <tekin@tekin.co.uk>
2013-10-23 10:46:49 +0100

    Move related edition indexing to service object
    The ReindexRelatedEditions class is still in the observer for now and
    will need to be moved elsewhere once we have the unpublish service
    object.

--- app/services/edition/search_indexer.rb
+++ app/services/edition/search_indexer.rb
@@ -7,6 +7,10 @@ class Edition::SearchIndexer
       if edition.allows_supporting_pages?
         edition.supporting_pages.each { |supporting_page| Searchable::Index.later(supporting_page) }
       end
+
+      if edition.is_a?(Policy)
+        PolicySearchIndexObserver::ReindexRelatedEditions.later(edition)
+      end
     end
   end
 end
