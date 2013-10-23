6710c2b212883b25f47865e05419988e93ecdfce
Tekin Suleyman <tekin@tekin.co.uk>
2013-10-23 14:23:50 +0100

    Pull force-publish remark to service subscriber
    This greatly simplifies EditionForcePublisher as it now mirrors the
    basic behaviour of EditionPublisher - the only differences are in the
    validations and the subscribers.

--- app/services/edition_force_publisher.rb
+++ app/services/edition_force_publisher.rb
@@ -1,26 +1,19 @@
 class EditionForcePublisher < EditionPublisher
 
-  def perform!
-    if can_perform?
-      prepare_edition
-      edition.force_publish!
-      edition.archive_previous_editions!
-      edition.editorial_remarks.create(body: "Force published: #{force_publish_reason}", author: user)
-      subscribers.each { |subscriber| subscriber.edition_published(edition, options) }
-      true
-    end
-  end
-
   def failure_reason
     @failure_reason ||= if !edition.valid?
       "This edition is invalid: #{edition.errors.full_messages.to_sentence}"
     elsif edition.scheduled_publication.present? && Time.zone.now < edition.scheduled_publication
       "This edition is scheduled for publication on #{edition.scheduled_publication.to_s} so cannot be force published"
-    elsif force_publish_reason.blank?
+    elsif options[:reason].blank?
       'You cannot force publish an edition without a reason'
     elsif !edition.can_force_publish?
       "An edition that is #{edition.current_state} cannot be force published"
     end
+  end
+
+  def default_subscribers
+    super + [Edition::ForcePublishLogger]
   end
 
 private
@@ -30,11 +23,7 @@ private
     super
   end
 
-  def force_publish_reason
-    options[:reason]
-  end
-
-  def user
-    options[:user]
+  def fire_transition!
+    edition.force_publish!
   end
 end
