4a4f079f255afd7e2235ffb3f555cb5dce86d8b3
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-10-25 16:59:20 +0100

    Revert "Merge pull request #996 from alphagov/prevent-duplicate-attachment-orderings"
    This reverts commit 3f567213fd77c7ae5ff643f608995bfdf8a3036f, reversing
    changes made to a6e30e99269a66bb6affad3637bae37d79524c7b.

--- db/migrate/20131025143125_prevent_duplicate_attachment_orderings.rb
+++
@@ -1,6 +0,0 @@
-class PreventDuplicateAttachmentOrderings < ActiveRecord::Migration
-  def change
-    change_column :attachments, :ordering, :integer, :null => false
-    add_index :attachments, [:attachable_type, :attachable_id, :ordering], unique: :true, name: "no_duplicate_attachment_orderings"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131025143125) do
+ActiveRecord::Schema.define(:version => 20131021134040) do
 
   create_table "about_pages", :force => true do |t|
     t.integer  "topical_event_id"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(:version => 20131025143125) do
     t.string   "order_url"
     t.integer  "price_in_pence"
     t.integer  "attachment_data_id"
-    t.integer  "ordering",                 :null => false
+    t.integer  "ordering"
     t.string   "hoc_paper_number"
     t.string   "parliamentary_session"
     t.boolean  "unnumbered_command_paper"
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(:version => 20131025143125) do
   end
 
   add_index "attachments", ["attachable_id", "attachable_type"], :name => "index_attachments_on_attachable_id_and_attachable_type"
-  add_index "attachments", ["attachable_type", "attachable_id", "ordering"], :name => "no_duplicate_attachment_orderings", :unique => true
   add_index "attachments", ["attachment_data_id"], :name => "index_attachments_on_attachment_data_id"
   add_index "attachments", ["ordering"], :name => "index_attachments_on_ordering"
 
