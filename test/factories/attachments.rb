52c5e546f52c93dcf4aea42722dfa7a7908e26de
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-10-25 16:58:18 +0100

    Revert "Fix test failures due to not null constraint on attachments ordering"
    This reverts commit 9ff012b1495c1b50e284dba3c6fbd300e9e9dc6d.

--- test/factories/attachments.rb
+++ test/factories/attachments.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
     sequence(:title) { |index| "attachment-title-#{index}" }
-    sequence(:ordering)
 
     ignore do
       file { File.open(File.join(Rails.root, 'test', 'fixtures', 'greenpaper.pdf')) }
