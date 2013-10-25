9ff012b1495c1b50e284dba3c6fbd300e9e9dc6d
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-10-25 16:22:34 +0100

    Fix test failures due to not null constraint on attachments ordering

--- test/factories/attachments.rb
+++ test/factories/attachments.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :attachment do
     sequence(:title) { |index| "attachment-title-#{index}" }
+    sequence(:ordering)
 
     ignore do
       file { File.open(File.join(Rails.root, 'test', 'fixtures', 'greenpaper.pdf')) }
