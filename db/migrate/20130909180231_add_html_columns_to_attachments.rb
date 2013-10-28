9b1069ef4e26c455e39025ff13de231bd83c6ab3
Dominic Baggott <dominic.baggott@gmail.com>
2013-10-28 11:30:00 +0000

    Don't try to renumber existing attachments
    Put new attachments in at -1 to avoid having to modify existing
    attachment records in a specific order.

--- db/migrate/20130909180231_add_html_columns_to_attachments.rb
+++ db/migrate/20130909180231_add_html_columns_to_attachments.rb
@@ -9,27 +9,9 @@ class AddHtmlColumnsToAttachments < ActiveRecord::Migration
       execute "UPDATE attachments SET type = 'FileAttachment' WHERE type IS NULL"
 
       execute %Q{
-        UPDATE
-          attachments
-        JOIN
-          html_versions ON attachable_id = edition_id
-        SET
-          ordering = ordering + 1
-        WHERE
-          `type` = 'FileAttachment'
-        AND attachable_type = 'Edition'
-        AND edition_id IS NOT NULL
-        AND html_versions.title IS NOT NULL
-        AND html_versions.title != ''
-        AND html_versions.body IS NOT NULL
-        AND html_versions.body != ''
-        ORDER BY ordering desc;
-      }
-
-      execute %Q{
         INSERT INTO attachments
           (`type`, attachable_id, attachable_type, title, body, slug, manually_numbered_headings, ordering, created_at, updated_at)
-        SELECT 'HtmlAttachment', edition_id, 'Edition', title, body, slug, manually_numbered, 0, created_at, updated_at
+        SELECT 'HtmlAttachment', edition_id, 'Edition', title, body, slug, manually_numbered, -1, created_at, updated_at
         FROM html_versions
         WHERE edition_id IS NOT NULL
         AND title IS NOT NULL
