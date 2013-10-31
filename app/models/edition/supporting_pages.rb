034b9ba697eca66e6b9192a557d0d7366c8889ae
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2013-10-31 15:58:56 +0000

    Rewrite the supporting pages mixin.
    Currently used only by Policy.

--- app/models/edition/supporting_pages.rb
+++ app/models/edition/supporting_pages.rb
@@ -1,22 +1,8 @@
 module Edition::SupportingPages
   extend ActiveSupport::Concern
 
-  class Trait < Edition::Traits::Trait
-    def process_associations_after_save(edition)
-      @edition.supporting_pages.each do |sd|
-        new_supporting_page = edition.supporting_pages.create(sd.attributes.except("id", "edition_id"))
-        new_supporting_page.update_column(:slug, sd.slug)
-        sd.attachments.each do |attachment|
-          new_supporting_page.attachments << attachment.class.new(attachment.attributes)
-        end
-      end
-    end
-  end
-
-  included do
-    has_many :supporting_pages, foreign_key: :edition_id, dependent: :delete_all
-
-    add_trait Trait
+  def supporting_pages
+    related_editions.where(type: 'SupportingPage')
   end
 
   def allows_supporting_pages?
