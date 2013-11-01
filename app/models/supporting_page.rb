54732da369b173510c6f59fd415efebafb61c124
Dominic Baggott <dominic.baggott@gmail.com>
2013-11-01 08:37:09 +0000

    Validate that supporting page has > 0 policies

--- app/models/supporting_page.rb
+++ app/models/supporting_page.rb
@@ -2,4 +2,5 @@ class SupportingPage < Edition
   include Edition::RelatedPolicies
   include ::Attachable
 
+  validates :related_policies, length: { minimum: 1, message: "must include at least one policy" }
 end
