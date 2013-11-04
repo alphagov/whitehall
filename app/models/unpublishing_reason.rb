d135d37fd05321b11697bc1b9c343708cb022812
Tekin Suleyman <tekin@tekin.co.uk>
2013-11-04 16:21:25 +0000

    Add new unpublishing reasons and mark legacy ones
    We have to keep the existing reasons around for the unpublishings
    already in the database. This is the simplest way to roll out this
    feature. If we need to migrate these old reasons to the new ones, we
    can do that as a separate step

--- app/models/unpublishing_reason.rb
+++ app/models/unpublishing_reason.rb
@@ -4,6 +4,9 @@ class UnpublishingReason
   attr_accessor :id, :name, :as_sentence
 
   PublishedInError  = create(id: 1, name: 'Published in error', as_sentence: 'it was published in error')
+  Consolidated      = create(id: 4, name: 'Consolidated into another GOV.UK page', as_sentence: 'it has been consolidated into another GOV.UK page')
+  Archived          = create(id: 5, name: 'No longer current government policy/activity', as_sentence: 'it is no longer current government policy/activity')
+  # Legacy reasons; no longer available in admin.
   Duplicate         = create(id: 2, name: 'Duplicate of another page', as_sentence: 'it is a duplicate of another page')
   Superseded        = create(id: 3, name: 'Superseded by another page', as_sentence: 'it has been superseded by another page')
 end
