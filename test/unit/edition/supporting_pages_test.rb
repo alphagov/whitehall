63a9cc96ce647333df157ae81a8553d3467aa576
Dominic Baggott <dominic.baggott@gmail.com>
2013-11-04 12:08:44 +0000

    Remove obsolete Edition::SupportingPages tests
    Supporting pages aren't destroyed or copied when modifying the edition
    they support.

--- test/unit/edition/supporting_pages_test.rb
+++ test/unit/edition/supporting_pages_test.rb
@@ -1,35 +1,10 @@
 require "test_helper"
 
 class Edition::SupportingPagesTest < ActiveSupport::TestCase
-  test "#destroy should also remove the supporting pages" do
-    edition = create(:draft_policy)
-    supporting_page = create(:supporting_page, edition: edition)
-    edition.destroy
-    refute SupportingPage.find_by_id(supporting_page.id)
-  end
+  test "#supporting_pages should return a supporting page related to this edition" do
+    policy = create(:policy)
+    supporting_page = create(:supporting_page, related_policies: [policy])
 
-  test "#destroy should also remove the supporting pages for published editions" do
-    edition = create(:published_policy)
-    supporting_page = create(:supporting_page, edition: edition)
-    edition.destroy
-    refute SupportingPage.find_by_id(supporting_page.id)
-  end
-
-  test "supporting pages and their attachments are versioned when a new draft is created" do
-    policy = create(:published_policy)
-    attachment = create(:attachment)
-    supporting_page = create(:supporting_page, edition: policy, attachments: [attachment])
-
-    new_draft = policy.create_draft(create(:policy_writer))
-
-    assert new_supporting_page = new_draft.supporting_pages.first
-    assert_not_equal supporting_page, new_supporting_page
-    assert_equal supporting_page.title, new_supporting_page.title
-    assert_equal supporting_page.body, new_supporting_page.body
-
-    assert new_attachment = new_supporting_page.attachments.first
-    assert_not_equal attachment, new_attachment
-    assert_equal attachment.title, new_attachment.title
-    assert_equal attachment.attachment_data_id, new_attachment.attachment_data_id
+    assert_equal [supporting_page], policy.supporting_pages
   end
 end
