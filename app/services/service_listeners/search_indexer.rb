40e41c8ee072b29ff597182b51637ee637c6150f
Dominic Baggott <dominic.baggott@gmail.com>
2013-11-07 14:21:02 +0000

    Remove supporting page code from search indexer
    Now that supporting pages are proper editions they'll be indexed
    independently when published without taking special steps when a
    policy is reindexed or removed.

--- app/services/service_listeners/search_indexer.rb
+++ app/services/service_listeners/search_indexer.rb
@@ -1,33 +1,18 @@
 module ServiceListeners
   SearchIndexer = Struct.new(:edition) do
-
     def index!
       if edition.can_index_in_search?
         Searchable::Index.later(edition)
-        refresh_supporting_page_indexing if edition.allows_supporting_pages?
         index_related_editions if edition.is_a?(Policy)
       end
     end
 
     def remove!
       Searchable::Delete.later(edition)
-      clear_supporting_page_indexing if edition.allows_supporting_pages?
       index_related_editions if edition.is_a?(Policy)
     end
 
   private
-
-    def refresh_supporting_page_indexing
-      if previous_edition = edition.previous_edition
-        previous_edition.supporting_pages.each { |supporting_page| Searchable::Delete.later(supporting_page) }
-      end
-      edition.supporting_pages.each { |supporting_page| Searchable::Index.later(supporting_page) }
-    end
-
-    def clear_supporting_page_indexing
-      edition.supporting_pages.each { |supporting_page| Searchable::Delete.later(supporting_page) }
-    end
-
     def index_related_editions
       ReindexRelatedEditions.later(edition)
     end
--- test/unit/services/listeners/search_indexer_test.rb
+++ test/unit/services/listeners/search_indexer_test.rb
@@ -16,29 +16,6 @@ class ServiceListeners::SearchIndexerTest < ActiveSupport::TestCase
     ServiceListeners::SearchIndexer.new(non_english_edition).index!
   end
 
-  test '#index! with a policy indexes supporting pages' do
-    policy = create(:published_policy)
-    supporting_page = create(:supporting_page, related_policies: [policy])
-
-    expect_indexing(policy)
-    expect_indexing(supporting_page)
-
-    ServiceListeners::SearchIndexer.new(policy).index!
-  end
-
-  test "#index! with a republished policy removes the previous edition's supporting pages and indexes the latest ones" do
-    old_version = create(:published_policy, supporting_pages: [create(:supporting_page), create(:supporting_page)])
-    new_version = old_version.create_draft(create(:policy_writer)).reload
-    new_version.minor_change = true
-    force_publish(new_version)
-
-    expect_indexing(new_version)
-    expect_removal_from_index(old_version.supporting_pages.to_a)
-    expect_indexing(new_version.supporting_pages.to_a)
-
-    ServiceListeners::SearchIndexer.new(new_version).index!
-  end
-
   test '#index! with a policy re-indexes related editions' do
     policy = create(:published_policy)
 
@@ -54,11 +31,10 @@ class ServiceListeners::SearchIndexerTest < ActiveSupport::TestCase
     ServiceListeners::SearchIndexer.new(edition).remove!
   end
 
-  test '#remove! with a policy also re-indexes related editions and removes supporting pages' do
+  test '#remove! with a policy also re-indexes related editions' do
     policy = create(:published_policy)
 
     expect_removal_from_index(policy)
-    expect_removal_from_index(policy.supporting_pages.to_a)
     ReindexRelatedEditions.expects(:later).with(policy)
     ServiceListeners::SearchIndexer.new(policy).remove!
   end
