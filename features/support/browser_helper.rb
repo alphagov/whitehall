b99f91ae768ab2882e77c2bf6b78660860eaca79
Tekin Suleyman <tekin@tekin.co.uk>
2013-11-11 10:49:39 +0000

    Attempt to get the build working again
    Capybara's #find method should wait until the element is present on
    the page.

--- features/support/browser_helper.rb
+++ features/support/browser_helper.rb
@@ -1,9 +1,9 @@
 module BrowserHelper
 
-  #Poltergeist can't switch to a new tab unless the tab's name is known.
-  #This is a problem when following links with a target of _blank, this helper is a workaround.
+  # We've had trouble getting poltergeist/phantomjs to follow links to new windows (e.g. target="_blank").
+  # This helper bybasses things by visiting the URL instead of clicking the link.
   def visit_link_href(link_text)
-    assert page.has_css?('a', text: link_text), "Couldn't find a link \"#{link_text}\"."
+    assert page.find('a', text: link_text), "Couldn't find a link \"#{link_text}\"."
     visit(find('a', text: link_text)[:href])
   end
 end
