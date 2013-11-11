6c682f4c6bab0be1da8b832f721f000cede8707a
Will Tomlins <will.tomlins@unboxedconsulting.com>
2013-11-11 14:28:15 +0000

    Use explicit org names in test - fixes build

--- test/unit/helpers/document_filter_helper_test.rb
+++ test/unit/helpers/document_filter_helper_test.rb
@@ -62,17 +62,17 @@ class DocumentFilterHelperTest < ActionView::TestCase
 
   test "#organisation_filter_options return organisations as select options grouped into \
     'Ministerial departments', 'Other departments & public bodies' and 'Closed organisations'" do
-    ministerial_dept = create(:ministerial_department, :with_published_edition)
-    other_dept = create(:executive_office, :with_published_edition)
-    closed_ministerial_dept = create(:ministerial_department, :with_published_edition, :closed)
-    closed_other_dept = create(:executive_office, :with_published_edition, :closed)
+    ministerial_dept = create(:ministerial_department, :with_published_edition, name: "Ministerial department")
+    other_dept = create(:executive_office, :with_published_edition, name: "Other department")
+    closed_ministerial_dept = create(:ministerial_department, :with_published_edition, :closed, name: "1-Closed Ministerial department")
+    closed_other_dept = create(:executive_office, :with_published_edition, :closed, name: "2-Closed Other department")
 
     option_set = Nokogiri::HTML::DocumentFragment.parse(organisation_filter_options)
 
     assert_equal [
-      ["Ministerial departments", [ministerial_dept.name]],
-      ["Other departments & public bodies", [other_dept.name]],
-      ["Closed organisations", [closed_ministerial_dept.name, closed_other_dept.name]],
+      ["Ministerial departments", ["Ministerial department"]],
+      ["Other departments & public bodies", ["Other department"]],
+      ["Closed organisations", ["1-Closed Ministerial department", "2-Closed Other department"]],
     ], option_set.css('optgroup').map { |optgroup|
       [
         optgroup["label"],
