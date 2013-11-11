30bb25e8ebece8a4316ee90dc237edee2a16b41e
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-11-11 15:25:50 +0000

    Attempt to fix flakey build
    explicit count to #has_css? hopefully will force it to
    wait.

--- features/support/browser_helper.rb
+++ features/support/browser_helper.rb
@@ -3,8 +3,8 @@ module BrowserHelper
   # We've had trouble getting poltergeist/phantomjs to follow links to new windows (e.g. target="_blank").
   # This helper bybasses things by visiting the URL instead of clicking the link.
   def visit_link_href(link_text)
-    assert page.find('a', text: link_text), "Couldn't find a link \"#{link_text}\"."
-    visit(find('a', text: link_text)[:href])
+    assert page.has_css?('a', text: link_text, count: 1)
+    visit(find(:link, link_text)[:href])
   end
 end
 
