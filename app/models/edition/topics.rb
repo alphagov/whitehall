e7f9c490a101fd3c1b394fef8bfe0d91a056c5e9
Dominic Baggott <dominic.baggott@gmail.com>
2013-11-15 09:32:29 +0000

    Add topic suggestion field to edition form
    Also validate that there's an associated topic or a suggestion.

--- app/models/edition/topics.rb
+++ app/models/edition/topics.rb
@@ -5,6 +5,14 @@ module Edition::Topics
   included do
     has_many :topics, through: :classification_memberships, source: :topic
     has_one :topic_suggestion, foreign_key: :edition_id
+
+    accepts_nested_attributes_for :topic_suggestion, allow_destroy: true, reject_if: ->(attrs) {
+      attrs[:id].blank? && attrs[:name].blank?
+    }
+
+    validate :validate_presence_of_topic
+
+    before_validation :mark_topic_suggestion_for_destruction
   end
 
   def can_be_associated_with_topics?
@@ -26,6 +34,19 @@ module Edition::Topics
 
     def scheduled_in_topic(topic)
       scheduled.in_topic(topic)
+    end
+  end
+
+private
+  def validate_presence_of_topic
+    if topics.empty? && (topic_suggestion.blank? || topic_suggestion.marked_for_destruction?)
+      errors.add(:topics, "at least one required")
+    end
+  end
+
+  def mark_topic_suggestion_for_destruction
+    if topic_suggestion.present? && topic_suggestion.name.blank?
+      topic_suggestion.mark_for_destruction
     end
   end
 end
