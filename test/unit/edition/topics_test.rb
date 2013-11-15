382df231a0d9e7ec29cafe214f2c9e504721887d
Dominic Baggott <dominic.baggott@gmail.com>
2013-11-15 12:02:14 +0000

    Test edition topics validation

--- test/unit/edition/topics_test.rb
+++ test/unit/edition/topics_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class Edition::TopicsTest < ActiveSupport::TestCase
+  class EditionWithTopics < Edition
+    include Edition::Topics
+  end
+
   setup do
     @topic = create(:topic)
   end
@@ -14,11 +18,33 @@ class Edition::TopicsTest < ActiveSupport::TestCase
 
   test "new edition of document that is a member of a topic should remain a member of that topic" do
     edition = create(:published_policy, topics: [@topic])
-
     new_edition = edition.create_draft(create(:policy_writer))
-    new_edition.change_note = 'change-note'
-    force_publish(new_edition)
 
-    assert_equal @topic, new_edition.topics.first
+    assert_equal [@topic], new_edition.topics
+  end
+
+  test "edition should be invalid with neither topic nor topic suggestion" do
+    edition = EditionWithTopics.new(attributes_for_edition)
+
+    refute edition.valid?, "Edition should not be valid"
+  end
+
+  test "edition should be valid with an associated topic" do
+    topic = build(:topic)
+    edition = EditionWithTopics.new(attributes_for_edition.merge(topics: [topic]))
+
+    assert edition.valid?, "Edition should be valid"
+  end
+
+  test "edition should be valid with an associated topic suggestion" do
+    suggestion = TopicSuggestion.new(name: "a new topic")
+    edition = EditionWithTopics.new(attributes_for_edition.merge(topic_suggestion: suggestion))
+
+    assert edition.valid?, "Edition should be valid"
+  end
+
+private
+  def attributes_for_edition
+    attributes_for(:edition).merge(creator: build(:creator))
   end
 end
