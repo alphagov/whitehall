8b123b6434e2b9fa968292821d6e5dcbb040cc27
Ben Firshman <ben@firshman.co.uk>
2013-11-22 13:01:33 +0000

    Add analytics headers to operational fields

--- app/controllers/operational_fields_controller.rb
+++ app/controllers/operational_fields_controller.rb
@@ -6,6 +6,8 @@ class OperationalFieldsController < PublicFacingController
   def show
     @operational_field = OperationalField.find(params[:id])
     @organisation = Organisation.where(handles_fatalities: true).first
+    set_slimmer_organisations_header([@organisation])
+    set_slimmer_page_owner_header(@organisation)
     @fatality_notices = @operational_field.published_fatality_notices.in_reverse_chronological_order.map do |fatality_notice|
       FatalityNoticePresenter.new(fatality_notice)
     end
