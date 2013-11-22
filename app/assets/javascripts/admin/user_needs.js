da37aabf43b184efcdde54369ada91f9a431eb86
Ben Firshman <ben@firshman.co.uk>
2013-11-22 16:07:23 +0000

    Make user needs on detailed guides always show
    ... and remove toggle that lets you hide it.
    
    We want to always show user needs so they are fresh in the minds of
    editors when they are editing an edition.

--- app/assets/javascripts/admin/user_needs.js
+++ app/assets/javascripts/admin/user_needs.js
@@ -1,17 +1,6 @@
 (function ($) {
 
-  var $fieldset = $('#user-needs');
-
-  // Shrink fieldset if user needs have been provided
-  if ($('#edition_user_need_ids').val() !== null) {
-    var $heading = $fieldset.find('h2');
-    $heading
-      .text($heading.text() + ' (click to show/hide)')
-      .addClass('enhanced')
-      .click(function() {
-        $(this).nextAll().slideToggle();
-      }).click();
-  }
+  var $fieldset = $('.user-needs');
 
   // Autocomplete fields to reduce duplicates
   $fieldset.find('.user-need-input').each(function(index, input) {
