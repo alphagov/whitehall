a90ce3d94804d595e401e494deb676055906a4b1
Dominic Baggott <dominic.baggott@gmail.com>
2013-11-29 13:05:54 +0000

    Refactor Edition#in_organisation to use org ID
    Using the slug just meant an unnecessary join to the organisations
    table.

--- app/models/edition/findable_by_organisation.rb
+++ app/models/edition/findable_by_organisation.rb
@@ -1,13 +1,11 @@
 # This mixin should go away when we switch to a search backend for admin documents
 module Edition::FindableByOrganisation
   def in_organisation(organisation)
-    organisations = [*organisation]
-    slugs = organisations.map(&:slug)
-    where('exists (
-             select * from edition_organisations eo_orgcheck
-               join organisations orgcheck on eo_orgcheck.organisation_id=orgcheck.id
-             where
-               eo_orgcheck.edition_id=editions.id
-             and orgcheck.slug in (?))', slugs)
+    organisation_ids = Array(organisation).map(&:id)
+
+    where('exists (SELECT * FROM edition_organisations eo_orgcheck
+                   WHERE eo_orgcheck.edition_id = editions.id
+                   AND eo_orgcheck.organisation_id IN (:ids))',
+          ids: organisation_ids)
   end
 end
