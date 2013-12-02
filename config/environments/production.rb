148da93bc4a0a4f2d986119db530390d5c53a93f
Will Tomlins <will@tomlins.org.uk>
2013-12-02 14:46:55 +0000

    Upgrade logstasher and correct typo
    Logstasher had a typo on a config flag in version 0.2.5 (suppress_app_log was spelt supress_app_log).
    Version 0.3 seems to have had the typo fixed, but also accepts the incorrectly spelt version.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Whitehall::Application.configure do
   # Enable JSON-style logging
   config.logstasher.enabled = true
   config.logstasher.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.json.log")
-  config.logstasher.supress_app_log = true
+  config.logstasher.suppress_app_log = true
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
