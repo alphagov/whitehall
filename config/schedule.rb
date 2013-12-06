4c931835c5535ea3865706b7b04f3d88538da48f
Dominic Baggott <dominic.baggott@gmail.com>
2013-12-06 16:08:19 +0000

    Remove unused require from config/schedule.rb
    The SCHEDULED_PUBLISHING_PRECISION_IN_MINUTES constant is no longer
    being used in the whenever definitions and the initializer now uses code
    not loaded by whenever.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,12 @@
-$: << '.'
-require File.dirname(__FILE__) + "/initializers/scheduled_publishing"
 # default cron env is "/usr/bin:/bin" which is not sufficient as govuk_env is in /usr/local/bin
 env :PATH, '/usr/local/bin:/usr/bin:/bin'
 
 # We need Rake to use our own environment
 job_type :rake, "cd :path && govuk_setenv whitehall bundle exec rake :task --silent :output"
 
-# Run every SCHEDULED_PUBLISHING_PRECISION_IN_MINUTES minutes, 2 minutes before the due moment
-# this allows the the ruby process (and rails) time to boot up. The scheduler then sleeps until
-# the edition is due
+# Run every 15 minutes, 2 minutes before the due moment this allows the the
+# ruby process (and rails) time to boot up. The scheduler then sleeps until the
+# edition is due
 every '13,28,43,58 * * * *', roles: [:backend] do
   rake "publishing:due:publish"
 end
