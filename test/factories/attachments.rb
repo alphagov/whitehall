b2e29e30ac0d9c0ac79566f207f69d02c3daf078
Will Tomlins <will@tomlins.org.uk>
2013-12-09 13:52:28 +0000

    Attachment factory doesn't set sequence
    It no longer needs to, the model does this anyway.
    
    Also dryed up the attachment factory a little.

--- test/factories/attachments.rb
+++ test/factories/attachments.rb
@@ -1,29 +1,28 @@
 FactoryGirl.define do
   sequence(:attachment_ordering)
 
-  factory :file_attachment do
+  trait :abstract_attachment do
+    attachable { build :policy_advisory_group }
+  end
+
+  factory :file_attachment, traits: [:abstract_attachment] do
+    sequence(:title) { |index| "file-attachment-title-#{index}" }
     ignore do
       file { File.open(Rails.root.join('test', 'fixtures', 'greenpaper.pdf')) }
     end
-    attachable { build :policy_advisory_group }
-    sequence(:title) { |index| "file-attachment-title-#{index}" }
-    ordering { generate(:attachment_ordering) }
     after(:build) do |attachment, evaluator|
       attachment.attachment_data ||= build(:attachment_data, file: evaluator.file)
     end
   end
 
   factory :csv_attachment, parent: :file_attachment do
-    attachable { build :policy_advisory_group }
     ignore do
       file { File.open(Rails.root.join('test', 'fixtures', 'sample.csv')) }
     end
   end
 
-  factory :html_attachment do
-    attachable { build :policy_advisory_group }
+  factory :html_attachment, traits: [:abstract_attachment] do
     sequence(:title) { |index| "html-attachment-title-#{index}" }
-    ordering { generate(:attachment_ordering) }
     body 'Attachment body'
   end
 end
