501ff68a86a722efde309151e620f345b5804f35
Tekin Suleyman <tekin@tekin.co.uk>
2013-12-13 13:58:52 +0000

    Fix Attachment.files scope to deal with STI fail
    STI does not work very well with nested attributes because the type
    column is protected by ActiveRecord (in 3.2 at least). And because
    attachments (at least currently) on policy advisory groups and
    corporate information pages are managed using nested attributes, we
    can't prevent base Attachments from existing in the database. Once we
    have got rid of the nested attributes, we can make the base Attachment
    class abstract and prevent instances being created in future.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -46,7 +46,7 @@ class Attachment < ActiveRecord::Base
     joins(:attachment_data).where('attachment_data.carrierwave_file = ?', basename)
   }
 
-  scope :files, where('type = ?', 'FileAttachment')
+  scope :files, where("type != ?", 'HtmlAttachment')
 
   scope :for_current_locale, -> { where(locale: [nil, I18n.locale]) }
 
