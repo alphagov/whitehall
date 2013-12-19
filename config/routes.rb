2397936e843fa70f80064593e6314b439ad2713a
Tekin Suleyman <tekin@tekin.co.uk>
2013-12-19 11:20:27 +0000

    Update routes for Rails 4 compatibility
    - The `match` method is no longer supported.
    - Rails 4 does not like there to be more than one root route specified
      unless they are explicitly named.

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Whitehall::Application.routes.draw do
     external_redirect '/organisations/ministry-of-defence-police-and-guarding-agency',
       "http://webarchive.nationalarchives.gov.uk/20121212174735/http://www.mod.uk/DefenceInternet/AboutDefence/WhatWeDo/SecurityandIntelligence/MDPGA/"
 
-    root to: redirect("/", { prefix: '' }), via: :get
+    root to: redirect("/", { prefix: '' }), via: :get, as: :main_root
     get "/how-government-works" => "home#how_government_works", as: 'how_government_works'
     scope '/get-involved' do
       root to: 'home#get_involved', as: :get_involved, via: :get
@@ -99,7 +99,7 @@ Whitehall::Application.routes.draw do
         get :upcoming
       end
     end
-    match "/consultations/:consultation_id/:id" => 'html_attachments#show', as: 'consultation_html_attachment'
+    get "/consultations/:consultation_id/:id" => 'html_attachments#show', as: 'consultation_html_attachment'
 
     resources :topics, path: "topics", only: [:index, :show]
     resources :topical_events, path: "topical-events", only: [:index, :show] do
@@ -110,8 +110,8 @@ Whitehall::Application.routes.draw do
     get '/collections' => redirect("/publications")
     resources :organisations, only: [:index], localised: false
     resources :organisations, only: [:show], localised: true do
-      match '/series/:slug' => redirect("/collections/%{slug}")
-      match '/series' => redirect("/publications")
+      get '/series/:slug' => redirect("/collections/%{slug}")
+      get '/series' => redirect("/publications")
 
       member do
         get :about, localised: true
