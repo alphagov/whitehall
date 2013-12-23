be93076d3a3c77af5a66da2303169a5911562ca6
Tekin Suleyman <tekin@tekin.co.uk>
2013-12-23 10:55:55 +0000

    Move parameter filtering to initializer
    This is what Rails 4 does by default

--- config/application.rb
+++ config/application.rb
@@ -61,9 +61,6 @@ module Whitehall
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
     # Enable the asset pipeline
     config.assets.initialize_on_precompile = true
 
