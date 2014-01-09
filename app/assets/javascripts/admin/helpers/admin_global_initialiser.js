1e148d8ff46a23c4b9ea0b274c8bcab3489344b2
Will Tomlins <will@tomlins.org.uk>
2014-01-09 11:21:32 +0000

    Move on_ready stuff into adminGlobalInitialiser

---
+++ app/assets/javascripts/admin/helpers/admin_global_initialiser.js
@@ -0,0 +1,23 @@
+(function() {
+  "use strict";
+  window.GOVUK = window.GOVUK || {};
+
+  window.GOVUK.adminGlobalInitialiser = {
+    init: function init() {
+      GOVUK.init(GOVUK.ieHandler);
+      GOVUK.init(GOVUK.formsHelper);
+      GOVUK.init(GOVUK.navBarHelper);
+      GOVUK.init(GOVUK.tabs);
+
+      jQuery(document).ready(function($) {
+        if(typeof GOVUK === 'undefined') { window.GOVUK = {}; }
+
+        GOVUK.doubleClickProtection();
+        GOVUK.duplicateFields.init();
+        GOVUK.formChangeProtection.init($('#edit_edition'), 'You have unsaved changes that will be lost if you leave this page.');
+        GOVUK.hideClosedAtDates();
+        GOVUK.toggleCustomLogoField();
+      });
+    }
+  };
+}());
