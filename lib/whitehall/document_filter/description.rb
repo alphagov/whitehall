b0641fd5834feb9b0424da17986ed8cf6a737d6b
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-01-09 14:26:10 +0000

    Refactor Description#text

--- lib/whitehall/document_filter/description.rb
+++ lib/whitehall/document_filter/description.rb
@@ -14,25 +14,34 @@ module Whitehall
       end
 
       def text
-        if @feed_url =~ %r{(policies|ministers|people)/([a-z-]+)}
-          type = $1
-          slug = $2
-
-          klass = classify_type(type)
-
-          if klass == Policy
-            if policy = Document.where(slug: slug, document_type: 'Policy').first
-              policy.published_edition.try(:title)
-            end
-          else
-            klass.find_by_slug(slug).try(:name)
-          end
+        if @feed_url =~ %r{(policies|ministers|people)/([\w-]+)}
+          label_from_slug(type: $1, slug: $2)
         else
           labels_from_params.join(', ')
         end
       end
 
     protected
+
+      def labels_from_params
+        @params.flat_map do |key, values|
+          Array(values).map do |value|
+            @options_manager.sentence_fragment_for(key, value)
+          end
+        end.compact
+      end
+
+      def label_from_slug(type: nil, slug: nil)
+        klass = classify_type(type)
+
+        if klass == Policy
+          if policy = Document.where(slug: slug, document_type: 'Policy').first
+            policy.published_edition.try(:title)
+          end
+        else
+          klass.find_by_slug(slug).try(:name)
+        end
+      end
 
       def classify_type(type)
         case type
@@ -42,15 +51,9 @@ module Whitehall
           Role
         when 'people'
           Person
+        else
+          raise ArgumentError.new("Can't process a feed for unknown type '#{type}'")
         end
-      end
-
-      def labels_from_params
-        @params.flat_map do |key, values|
-          Array(values).map do |value|
-            @options_manager.sentence_fragment_for(key, value)
-          end
-        end.compact
       end
     end
   end
