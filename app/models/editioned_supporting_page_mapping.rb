64012d478387416ee9705fb1db070b4973b6ee62
Dominic Baggott <dominic.baggott@gmail.com>
2014-01-10 11:07:24 +0000

    Remove unnecessary attr_accessible calls
    A hash of user input is never used to create or update these models so
    attribute protected is unnecessary.

--- app/models/editioned_supporting_page_mapping.rb
+++ app/models/editioned_supporting_page_mapping.rb
@@ -1,5 +1,3 @@
 class EditionedSupportingPageMapping < ActiveRecord::Base
-  attr_accessible :new_supporting_page_id, :old_supporting_page_id
-
   belongs_to :new_supporting_page, class_name: 'SupportingPage'
 end
