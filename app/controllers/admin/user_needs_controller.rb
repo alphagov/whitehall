5f4388c2eb86f9dac60db06651101fb2dc3118e5
Dominic Baggott <dominic.baggott@gmail.com>
2014-01-15 14:30:14 +0000

    Add strong params for user needs

--- app/controllers/admin/user_needs_controller.rb
+++ app/controllers/admin/user_needs_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UserNeedsController < Admin::BaseController
   def create
-    user_need = UserNeed.new(params[:user_need])
+    user_need = UserNeed.new(user_need_params)
 
     if user_need.save
       render json: { id: user_need.id, text: user_need.to_s }
@@ -8,5 +8,9 @@ class Admin::UserNeedsController < Admin::BaseController
       render json: { errors: user_need.errors }, status: 422
     end
   end
-end
 
+private
+  def user_need_params
+    params.require(:user_need).permit(:user, :need, :goal)
+  end
+end
