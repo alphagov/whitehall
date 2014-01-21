de766a2248f365e86ef5ed97cd130b7606a534f6
Dominic Baggott <dominic.baggott@gmail.com>
2014-01-21 14:00:21 +0000

    Include organisation state in #search_index

--- app/models/searchable.rb
+++ app/models/searchable.rb
@@ -9,7 +9,7 @@ module Searchable
     :people, :publication_type, :speech_type,
     :topics, :news_article_type, :display_type,
     :slug, :search_format_types, :world_locations,
-    :attachments, :operational_field
+    :attachments, :operational_field, :organisation_state
   ]
 
   included do
