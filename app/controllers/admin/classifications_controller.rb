54ace4a78f71b7c64df73fac5edd4dbf4d4d481c
Tekin Suleyman <tekin@tekin.co.uk>
2014-02-03 11:01:01 +0000

    Permit topical event logo param

--- app/controllers/admin/classifications_controller.rb
+++ app/controllers/admin/classifications_controller.rb
@@ -62,7 +62,7 @@ class Admin::ClassificationsController < Admin::BaseController
 
   def object_params
     params.require(model_name).permit(
-      :name, :description, :logo_alt_text, :logo_cache, :remove_logo,
+      :name, :description, :logo, :logo_alt_text, :logo_cache, :remove_logo,
       :start_date, :end_date,
       related_classification_ids: [],
       classification_memberships_attributes: [:id, :ordering],
