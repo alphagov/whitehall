3ac3865a48cb1a58f2c9fab2aa812dd6459eb44e
Dominic Baggott <dominic.baggott@gmail.com>
2014-02-05 13:39:23 +0000

    Rename CESG device guidance slug
    https://www.pivotaltracker.com/story/show/59541766

---
+++ db/data_migration/20140205133122_rename_cesg_device_guidance_slug.rb
@@ -0,0 +1,10 @@
+OLD_SLUG = 'end-user-devices-security-guidance--2'
+NEW_SLUG = 'end-user-devices-security-guidance'
+
+document = Document.find_by_slug(OLD_SLUG)
+document.update_attribute(:slug, NEW_SLUG)
+
+router = GdsApi::Router.new(Plek.current.find('router-api'))
+router.add_redirect_router("/government/collections/#{OLD_SLUG}",
+                           'exact',
+                           "/government/collections/#{NEW_SLUG}")
