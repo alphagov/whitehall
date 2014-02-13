b6b589340a56449819a39a6d0ac1dc5f2164c475
Tekin Suleyman <tekin@tekin.co.uk>
2014-02-13 13:59:17 +0000

    Make controller work with updated EmailSignup
    Flesh out the tests and make the controller code more idiomatic.

--- app/controllers/email_signups_controller.rb
+++ app/controllers/email_signups_controller.rb
@@ -2,14 +2,12 @@ class EmailSignupsController < PublicFacingController
 
   def new
     @email_signup = EmailSignup.new(email_signup_params)
-  rescue ArgumentError
-    render text: 'Not found', status: :not_found
   end
 
   def create
-    @email_signup = EmailSignup.create(email_signup_params)
+    @email_signup = EmailSignup.new(email_signup_params)
 
-    if @email_signup.valid?
+    if @email_signup.save
       redirect_to @email_signup.govdelivery_url
     else
       render action: 'new'
--- test/functional/email_signups_controller_test.rb
+++ test/functional/email_signups_controller_test.rb
@@ -1,14 +1,38 @@
 require 'test_helper'
 
 class EmailSignupsControllerTest < ActionController::TestCase
+  include FeedHelper
 
-  view_test 'GET :new will display error messages if the email signup is not valid' do
-    get :new, email_signup: { feed: ''}
-    assert_match /Feed can&#x27;t be blank/, response.body
+  view_test 'GET :new with a valid field displays the subscription form' do
+    topic = create(:topic)
+    get :new, email_signup: { feed: atom_feed_url_for(topic) }
+
+    assert_response :success
+    assert_select "input[name='email_signup[feed]'][value='#{atom_feed_url_for(topic)}']"
   end
 
-  test 'POST :create will re-render the "new" template if the constructed email signup is not valid' do
-    post :create, email_signup: { feed: ''}
-    assert_template 'new'
+  view_test 'GET :new with an invalid feed shows an error message' do
+    get :new, email_signup: { feed: 'http://nonse-feed.atom' }
+
+    assert_response :success
+    refute_select "input[name='email_signup[feed]']"
+    assert_select "p", text: /we could not find a valid email alerts feed/
+  end
+
+  test 'POST :create with a valid email signup redirects to the govdelivery URL' do
+    Whitehall.govuk_delivery_client.stubs(:signup_url).returns('http://govdelivery_signup_url')
+    topic = create(:topic)
+    post :create, email_signup: { feed: atom_feed_url_for(topic) }
+
+    assert_response :redirect
+    assert_redirected_to 'http://govdelivery_signup_url'
+  end
+
+  view_test 'POST :create with an invalid email signup shows an error message' do
+    Whitehall.govuk_delivery_client.expects(:topic).never
+    post :create, email_signup: { feed: 'http://gov.uk/invalid/feed.atom'}
+
+    assert_response :success
+    assert_select "p", text: /we could not find a valid email alerts feed/
   end
 end
