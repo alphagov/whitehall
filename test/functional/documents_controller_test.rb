5ed1e1d020c078a058f21907efd86ffdb8afae5a
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-02-19 15:57:58 +0000

    Add Worldwide Org to slimmer header for GA

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -122,7 +122,7 @@ class DocumentsControllerTest < ActionController::TestCase
     get :show, id: edition.document
 
     assert_response :success
-    expected_header_value = "<#{edition.world_locations.map(&:analytics_identifier).join('><')}>"
+    expected_header_value = "<#{edition.world_locations.first.analytics_identifier}>"
     assert_equal expected_header_value, response.headers["X-Slimmer-World-Locations"]
   end
 
@@ -135,5 +135,28 @@ class DocumentsControllerTest < ActionController::TestCase
 
     assert_response :success
     assert_nil response.headers["X-Slimmer-World-Locations"]
+  end
+
+  test "adds worldwide organisation to slimmer header if the document can be associated with a worldwide organisation" do
+    DocumentsController.any_instance.stubs(document_class: WorldLocationNewsArticle)
+    edition = create(:world_location_news_article)
+    force_publish(edition)
+
+    get :show, id: edition.document
+
+    assert_response :success
+    expected_header_value = "<#{edition.worldwide_organisations.first.analytics_identifier}>"
+    assert_equal expected_header_value, response.headers["X-Slimmer-Organisations"]
+  end
+
+  test "does not add worldwide organisation to slimmer header if the document can not be associated with a worldwide organisation" do
+    DocumentsController.any_instance.stubs(document_class: StatisticalDataSet)
+    statistical_data_set = create(:statistical_data_set)
+    force_publish(statistical_data_set)
+
+    get :show, id: statistical_data_set.document
+
+    assert_response :success
+    assert_no_match /^<WO\d+>$/, response.headers["X-Slimmer-Organisations"]
   end
 end
