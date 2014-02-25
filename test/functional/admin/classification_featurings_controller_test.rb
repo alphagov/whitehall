e21db1c1b3078496e96e1f389e4cb9fa7dc1d363
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-02-25 15:35:25 +0000

    Prevents time-outs if too many published documents
    Added pagination as required to fix:
    https://www.pivotaltracker.com/story/show/66349416
    and a few more tests to cover the controller index.

--- test/functional/admin/classification_featurings_controller_test.rb
+++ test/functional/admin/classification_featurings_controller_test.rb
@@ -8,6 +8,23 @@ class Admin::ClassificationFeaturingsControllerTest < ActionController::TestCase
     login_as :policy_writer
   end
 
+  test "GET :index assigns tagged_editions with a paginated collection of published editions related to the topic ordered by most recently created editions first" do
+    @news_article_foo = create(:published_news_article, :with_topics, topics: [@topic])
+    Timecop.travel(10.minutes) do
+      @news_article_bar = create(:published_news_article, :with_topics, topics: [@topic])
+    end
+    draft_article = create(:news_article, :with_topics, topics: [@topic])
+    unrelated_article = create(:news_article, :with_topics)
+
+    get :index, topic_id: @topic, page: 1
+
+    tagged_editions = assigns(:tagged_editions)
+    assert_equal [@news_article_bar, @news_article_foo], tagged_editions
+    assert_equal 1, tagged_editions.current_page
+    assert_equal 1, tagged_editions.num_pages
+    assert_equal 25, tagged_editions.limit_value
+  end
+
   test "PUT :order saves the new order of featurings" do
     feature1 = create(:classification_featuring, classification: @topic)
     feature2 = create(:classification_featuring, classification: @topic)
