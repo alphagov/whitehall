f3ad80110e6942e6d78637cc78e6b0569552f1f3
Tekin Suleyman <tekin@tekin.co.uk>
2014-03-06 17:31:44 +0000

    Use consistent naming for release announcements in controller code

--- app/controllers/admin/statistical_release_announcements_controller.rb
+++ app/controllers/admin/statistical_release_announcements_controller.rb
@@ -2,11 +2,11 @@ class Admin::StatisticalReleaseAnnouncementsController < Admin::BaseController
   before_filter :find_release_announcement, only: [:edit, :update, :destroy]
 
   def new
-    @release_announcement = build_announcement(organisation_id: current_user.organisation.try(:id))
+    @release_announcement = build_release_announcement(organisation_id: current_user.organisation.try(:id))
   end
 
   def create
-    @release_announcement = build_announcement(statistical_release_announcement_params)
+    @release_announcement = build_release_announcement(release_announcement_params)
 
     if @release_announcement.save
       redirect_to admin_root_url, notice: "Announcement saved successfully"
@@ -19,7 +19,7 @@ class Admin::StatisticalReleaseAnnouncementsController < Admin::BaseController
   end
 
   def update
-    if @release_announcement.update_attributes(statistical_release_announcement_params)
+    if @release_announcement.update_attributes(release_announcement_params)
       redirect_to admin_root_url, notice: "Announcement updated successfully"
     else
       render :edit
@@ -40,11 +40,11 @@ class Admin::StatisticalReleaseAnnouncementsController < Admin::BaseController
     @release_announcement = StatisticalReleaseAnnouncement.find(params[:id])
   end
 
-  def build_announcement(attributes={})
+  def build_release_announcement(attributes={})
     current_user.statistical_release_announcements.new(attributes)
   end
 
-  def statistical_release_announcement_params
+  def release_announcement_params
     params.require(:statistical_release_announcement).permit(
       :title, :summary, :expected_release_date, :display_release_date_override,
       :organisation_id, :topic_id, :publication_type_id
