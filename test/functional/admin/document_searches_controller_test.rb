59b6a239124e0f572c8b6f0a889ea1f8f22255c8
Tekin Suleyman <tekin@tekin.co.uk>
2014-03-11 17:08:35 +0000

    Allow document search to be filtered by type and subtype

--- test/functional/admin/document_searches_controller_test.rb
+++ test/functional/admin/document_searches_controller_test.rb
@@ -25,4 +25,22 @@ class Admin::DocumentSearchesControllerTest < ActionController::TestCase
     assert_equal 'publication', publication_json['type']
     assert_equal publication.display_type, publication_json['display_type']
   end
+
+  view_test 'GET #show can filter by edition type and subtype' do
+    guidance = create(:publication, title: 'search term', publication_type: PublicationType::Guidance)
+    form = create(:publication, title: 'search term', publication_type: PublicationType::Form)
+
+    get :show, title: 'search term', type: 'publication', subtypes: [PublicationType::Guidance.id], format: :json
+
+    assert_response :success
+    assert_equal true, json_response['results_any?']
+    assert_equal 1, json_response['results'].size
+
+    publication_json = json_response['results'].first
+    assert_equal guidance.id, publication_json['id']
+    assert_equal guidance.document_id, publication_json['document_id']
+    assert_equal guidance.title, publication_json['title']
+    assert_equal 'publication', publication_json['type']
+    assert_equal guidance.display_type, publication_json['display_type']
+  end
 end
