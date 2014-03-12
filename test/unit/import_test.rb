cce265fbedd67f26716a6954f64028999e5c49b4
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-03-12 12:39:49 +0000

    Setting misbehaving citizens right
    took me a while to realize that the connection
    my test transaction was running within was not
    the connection Import uses. when running with
    other tests, my test picked-up one of these
    previously opened connections which we've
    removed in this commit.
    
    because ActiveRecord transactions are per-connection,
    Import was totally oblivious of the data setup in my
    test causing unexpected failures.

--- test/unit/import_test.rb
+++ test/unit/import_test.rb
@@ -233,13 +233,17 @@ class ImportTest < ActiveSupport::TestCase
   end
 
   test 'logs failure if unable to parse a date' do
-    i = perform_import(csv_data: consultation_csv_sample("opening_date" => "31/10/2012"))
-    assert i.import_errors.detect {|e| e[:message] =~ /Unable to parse the date/}
+    perform_import_cleanup do
+      i = perform_import(csv_data: consultation_csv_sample("opening_date" => "31/10/2012"))
+      assert i.import_errors.detect {|e| e[:message] =~ /Unable to parse the date/}
+    end
   end
 
   test 'logs failure if unable to find an organisation' do
-    i = perform_import(csv_data: consultation_csv_sample("organisation" => "does-not-exist"))
-    assert i.import_errors.detect {|e| e[:message] =~ /Unable to find Organisation/}
+    perform_import_cleanup do
+      i = perform_import(csv_data: consultation_csv_sample("organisation" => "does-not-exist"))
+      assert i.import_errors.detect {|e| e[:message] =~ /Unable to find Organisation/}
+    end
   end
 
   test 'logs failures within attachments if save unsuccessful' do
@@ -461,9 +465,9 @@ class ImportTest < ActiveSupport::TestCase
     Import.use_separate_connection
     yield
   ensure
-    Import.destroy_all
-    ImportError.destroy_all
-    ImportLog.destroy_all
+    Import.destroy_all; Import.remove_connection
+    ImportError.destroy_all; ImportError.remove_connection
+    ImportLog.destroy_all; ImportLog.remove_connection
   end
 
   def translated_news_article_csv
