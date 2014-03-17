83a035495373bff309e6d763e0dc67736923f5e9
Tekin Suleyman <tekin@tekin.co.uk>
2014-03-17 14:27:10 +0000

    New class for cleaning document filter parameters
    We have been getting random requests where filter parameters are being
    specified as incorrect types. For example:
    
      {
        "action" => "index",
        "controller" => "publications",
        "departments" => "[\"department-of-energy-climate-change\"]",
        "locale" => "en",
        "page" => {
          "&#36;acunetix" => "1"
        },
        "publication_type" => "transparency-data"
      }
    
    This new class returns a clean set of parameters that can be used by
    the various search backends to filter documents.
    
    It incorporates the correcting of array parameters that become
    malformed when passed through the Facebook referrer from here:
    https://github.com/alphagov/whitehall/blob/858d4d789cbdd0e9bd45e0cc47f70c0e54f7d331/app/controllers/application_controller.rb#L75

---
+++ test/unit/whitehall/document_filter/cleaned_params_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+module Whitehall::DocumentFilter
+  class CleanedParamsTest < ActiveSupport::TestCase
+    setup do
+      @old_config_value = ActionController::Parameters.action_on_unpermitted_parameters
+      ActionController::Parameters.action_on_unpermitted_parameters = false
+    end
+
+    teardown do
+      ActionController::Parameters.action_on_unpermitted_parameters = @old_config_value
+    end
+
+    CleanedParams::PERMITTED_ARRAY_PARAMETER_KEYS.each do |param_key|
+      test "converts #{param_key} parameter converted to a hash by Facebook referrer back to an array" do
+        raw_params     = build_unclean_params(param_key => { '0' => "#{param_key}-1", '1' => "#{param_key}-2" })
+        cleaned_params = CleanedParams.new(raw_params)
+
+        assert_equal ["#{param_key}-1", "#{param_key}-2"], cleaned_params[param_key]
+      end
+    end
+
+    test "unrecognised scaler parameters are scrubbed" do
+      Rails.application.config.action_controller.action_on_unpermitted_parameters = false
+      raw_params       = build_unclean_params('page' => '3', 'keywords' => 'statistics', 'hax' => 'javascript:alert("boo!")')
+      cleaned_params   = CleanedParams.new(raw_params)
+
+      assert_equal({ 'page' => '3', 'keywords' => 'statistics' }, cleaned_params)
+    end
+
+    test "permitted scaler parameters passed in as an array are scrubbed" do
+      raw_params       = build_unclean_params('page' => [], 'keywords' => 'statistics')
+      cleaned_params   = CleanedParams.new(raw_params)
+
+      assert_equal({ 'keywords' => 'statistics'}, cleaned_params)
+    end
+
+    test "permitted scaler parameters passed in as a hash are scrubbed" do
+      raw_params       = build_unclean_params('page' => {"$acunetix"=>"1"}, 'keywords' => 'statistics')
+      cleaned_params   = CleanedParams.new(raw_params)
+
+      assert_equal({ 'keywords' => 'statistics'}, cleaned_params)
+    end
+
+  private
+
+    def build_unclean_params(param_hash)
+      params = ActionController::Parameters.new(param_hash)
+    end
+  end
+end
