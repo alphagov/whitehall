1ae39f3df1c88d8c524d5db2d24cd2583cf352e6
Tekin Suleyman <tekin@tekin.co.uk>
2014-03-18 10:42:49 +0000

    Notify on unpermitted params in document filters
    This allows us to keep an eye on requests with unpermitted params
    without 500-ing at the user. Once we are satisfied that we are not
    incorrectly rejecting legitimate parameters in document filter
    requests, we can revert this commit to reduce exception noise due to
    script kidies sending strange requests.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,8 +6,9 @@ class DocumentsController < PublicFacingController
   before_filter :redirect_to_canonical_url
   before_filter :find_document, only: [:show]
   before_filter :set_slimmer_headers_for_document, only: [:show]
+  before_filter :notify_if_unpermitted_filter_params, only: :index
 
-  private
+private
 
   def build_document_filter
     search_backend.new(cleaned_document_filter_params)
@@ -15,6 +16,13 @@ class DocumentsController < PublicFacingController
 
   def cleaned_document_filter_params
     Whitehall::DocumentFilter::CleanedParams.new(params.except(:format, :commit, :_))
+  end
+
+  def notify_if_unpermitted_filter_params
+    if cleaned_document_filter_params.unpermitted_keys.any?
+      ex = ActionController::UnpermittedParameters.new(cleaned_document_filter_params.unpermitted_keys)
+      notify_airbrake(ex)
+    end
   end
 
   def preview?
