04f767f3de19802354535578f486330b79fe48cf
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-03-18 11:32:42 +0000

    Split inline attachments out into partials.
    Add explicit success message.

--- app/assets/javascripts/admin/rename_attachments.js
+++ app/assets/javascripts/admin/rename_attachments.js
@@ -8,13 +8,16 @@
     $form.on('ajax:beforeSend', function(event, xhr, settings) {
       $form.find('td').removeClass('error success');
       $form.find('span.help-block').remove();
+      $form.find('span.notice').remove();
     }).on('ajax:error', function(event, xhr, status) {
       $.each(xhr.responseJSON.errors, function(key, error) {
         $form.find("td#js-attachment-" + key)
           .addClass('error')
           .append('<span class="help-block">' + error + '</span>');
       });
-    }).on('ajax:complete', function(xhr, status) {
+    }).on('ajax:success', function(event, data, status, xhr) {
+      $form.find('.inline-submit').append('<span class="notice">Attachments saved</span>');
+    }).on('ajax:complete', function(event, xhr, status) {
       $form.find('input[type=hidden][name=commit]').remove();
       $form.find('input[name=commit]').removeAttr('disabled');
       $form.find('td.control-group').not('.error').addClass('success');
