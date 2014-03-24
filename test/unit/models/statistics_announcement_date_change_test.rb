62ca9001d71bb3fda835230bcb9b259a191b8df5
Tekin Suleyman <tekin@tekin.co.uk>
2014-03-24 16:59:06 +0000

    Cannot have a confirmed non-exact precision date

--- test/unit/models/statistics_announcement_date_change_test.rb
+++ test/unit/models/statistics_announcement_date_change_test.rb
@@ -16,12 +16,14 @@ class StatisticsAnnouncementDateChangeTest < ActiveSupport::TestCase
 
   test "change note required if changing date beyond a one-month precision" do
     current_date  = build(:statistics_announcement_date,
-                      precision: StatisticsAnnouncementDate::PRECISION[:one_month])
+                      precision: StatisticsAnnouncementDate::PRECISION[:one_month],
+                      confirmed: false)
 
     new_date      = build(:statistics_announcement_date_change,
                       current_release_date: current_date,
                       precision: StatisticsAnnouncementDate::PRECISION[:one_month],
-                      release_date: current_date.release_date + 3.weeks)
+                      release_date: current_date.release_date + 3.weeks,
+                      confirmed: false)
 
     assert new_date.valid?
     new_date.release_date = current_date.release_date + 1.month
@@ -32,12 +34,14 @@ class StatisticsAnnouncementDateChangeTest < ActiveSupport::TestCase
 
   test "change note required if changing date beyond a two-month precision" do
     current_date  = build(:statistics_announcement_date,
-                      precision: StatisticsAnnouncementDate::PRECISION[:two_month])
+                      precision: StatisticsAnnouncementDate::PRECISION[:two_month],
+                      confirmed: false)
 
     new_date      = build(:statistics_announcement_date_change,
                       current_release_date: current_date,
                       precision: StatisticsAnnouncementDate::PRECISION[:two_month],
-                      release_date: current_date.release_date + 6.weeks)
+                      release_date: current_date.release_date + 6.weeks,
+                      confirmed: false)
 
     assert new_date.valid?
     new_date.release_date = current_date.release_date + 2.months
@@ -48,12 +52,14 @@ class StatisticsAnnouncementDateChangeTest < ActiveSupport::TestCase
 
   test "change note required if reducing precision" do
     current_date  = build(:statistics_announcement_date,
-                      precision: StatisticsAnnouncementDate::PRECISION[:one_month])
+                      precision: StatisticsAnnouncementDate::PRECISION[:one_month],
+                      confirmed: false)
 
     new_date      = build(:statistics_announcement_date_change,
                       current_release_date: current_date,
                       precision: StatisticsAnnouncementDate::PRECISION[:two_month],
-                      release_date: current_date.release_date)
+                      release_date: current_date.release_date,
+                      confirmed: false)
 
     refute new_date.valid?
     assert_match /required/, new_date.errors[:change_note].first
@@ -61,7 +67,8 @@ class StatisticsAnnouncementDateChangeTest < ActiveSupport::TestCase
 
   test "valid if improving precision of date" do
     current_date  = build(:statistics_announcement_date,
-                      precision: StatisticsAnnouncementDate::PRECISION[:one_month])
+                      precision: StatisticsAnnouncementDate::PRECISION[:one_month],
+                      confirmed: false)
 
     new_date      = build(:statistics_announcement_date_change,
                       current_release_date: current_date,
@@ -73,7 +80,8 @@ class StatisticsAnnouncementDateChangeTest < ActiveSupport::TestCase
 
   test "valid if improving precision and changing date within previous precision" do
     current_date  = build(:statistics_announcement_date,
-                      precision: StatisticsAnnouncementDate::PRECISION[:one_month])
+                      precision: StatisticsAnnouncementDate::PRECISION[:one_month],
+                      confirmed: false)
 
     new_date      = build(:statistics_announcement_date_change,
                       current_release_date: current_date,
@@ -85,7 +93,8 @@ class StatisticsAnnouncementDateChangeTest < ActiveSupport::TestCase
 
   test "change note required if improving precision, but changing date beyond previous precision" do
     current_date  = build(:statistics_announcement_date,
-                      precision: StatisticsAnnouncementDate::PRECISION[:one_month])
+                      precision: StatisticsAnnouncementDate::PRECISION[:one_month],
+                      confirmed: false)
 
     new_date      = build(:statistics_announcement_date_change,
                       current_release_date: current_date,
