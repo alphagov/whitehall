9ec80b48f8b5fb7e9fbee740f67b442711faedf2
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-03-24 17:36:37 +0000

    Making it play with the markdown preview JS
    the edition body preview text was being covered-up
    by the highlighting wrapper div. fixed that by making
    the preview text sit outside the highlighter wrapper.

--- app/assets/javascripts/admin/modules/words_to_avoid_highlighter.js
+++ app/assets/javascripts/admin/modules/words_to_avoid_highlighter.js
@@ -5,50 +5,57 @@
   function WordsToAvoidHighlighter(options) {
     var $el = $(options.el),
         $wordsToAvoidAlert = $(options.wordsToAvoidAlert),
-        $wordsToAvoidCounter = $(options.wordsToAvoidCounter);
+        $wordsToAvoidCounter = $(options.wordsToAvoidCounter),
+        $textareaHighlightSelector = "span.highlight",
+        $wordsToAvoid = [
+          "agenda",
+          "advancing",
+          "collaborate",
+          "combating",
+          "commit",
+          "countering",
+          "deliver",
+          "deploy",
+          "dialogue",
+          "disincentivise",
+          "drive",
+          "drive out",
+          "empower",
+          "facilitate",
+          "focusing",
+          "foster",
+          "going forward",
+          "impact",
+          "initiate",
+          "in order to",
+          "key",
+          "land",
+          "leverage",
+          "liaise",
+          "one-stop shop",
+          "overarching",
+          "pledge",
+          "progress",
+          "promote",
+          "ring fencing",
+          "robust",
+          "slimming down",
+          "streamline",
+          "strengthening",
+          "tackling",
+          "transforming",
+          "utilise"
+        ];
 
-    $el.highlightTextarea({
-      color: '#FFB040',
-      caseSensitive: false,
-      words: [
-        "agenda",
-        "advancing",
-        "collaborate",
-        "combating",
-        "commit",
-        "countering",
-        "deliver",
-        "deploy",
-        "dialogue",
-        "disincentivise",
-        "drive",
-        "drive out",
-        "empower",
-        "facilitate",
-        "focusing",
-        "foster",
-        "going forward",
-        "impact",
-        "initiate",
-        "in order to",
-        "key",
-        "land",
-        "leverage",
-        "liaise",
-        "one-stop shop",
-        "overarching",
-        "pledge",
-        "progress",
-        "promote",
-        "ring fencing",
-        "robust",
-        "slimming down",
-        "streamline",
-        "strengthening",
-        "tackling",
-        "transforming",
-        "utilise"
-      ]
+    // without instantiating an instance of the widget
+    // per textarea, the `disable` and `enable` calls
+    // to the widget don't work as expected.
+    $.map($el, function(textarea) {
+      $(textarea).highlightTextarea({
+        color: '#FFB040',
+        caseSensitive: false,
+        words: $wordsToAvoid
+      });
     });
 
     var updateHighlightedWordsCount = function() {
@@ -63,6 +70,24 @@
     }
     $el.debounce("keyup", updateHighlightedWordsCount, 500);
     updateHighlightedWordsCount();
+
+    var disable = function() {
+      $.map($el, function(textarea) {
+        $(textarea).highlightTextarea('disable');
+      });
+      $($textareaHighlightSelector).hide();
+      $wordsToAvoidAlert.hide();
+    }
+    $(document).bind("govuk.wordsToAvoidHighlighter.disable", disable);
+
+    var enable = function() {
+      $.map($el, function(textarea) {
+        $(textarea).highlightTextarea('highlight');
+      });
+      $($textareaHighlightSelector).show();
+      $wordsToAvoidAlert.show();
+    }
+    $(document).bind("govuk.wordsToAvoidHighlighter.enable", enable);
   }
 
   GOVUK.WordsToAvoidHighlighter = WordsToAvoidHighlighter;
