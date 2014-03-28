88ca77885f6b0b86e24be2cb90081d2477c8d9a0
Tekin Suleyman <tekin@tekin.co.uk>
2014-03-28 11:21:19 +0000

    Allow announcement linked documents to be changed
    This allows a linked document to be changed after the fact so that
    mistakes can be corrected.

--- app/assets/javascripts/admin/views/statistical_release_announcements/statistics_announcement_linker.js
+++ app/assets/javascripts/admin/views/statistical_release_announcements/statistics_announcement_linker.js
@@ -7,19 +7,28 @@
       var $linker = $('.js-document-announcement-linker'),
           $documentFinder = $('#document-finder'),
           $documentFinderResult = $documentFinder.find('#edition_id'),
-          $annoucementForm = $('form.edit_statistics_announcement');
+          $annoucementForm = $('form.edit_statistics_announcement'),
+          $publicationInput = $annoucementForm.find('input#statistics_announcement_publication_id');
 
-      // add link to reveal document finder
-      var $revealLink = $('<a href=# class="js-announcement-linker-link">Link to an existing draft document</a>');
-      $linker.append(' or ').append($revealLink);
-
+      // Link to reveal document finder
+      var $revealLink = $('<a href=# class="js-announcement-linker-link"></a>');
       $revealLink.on('click', function () {
         $linker.hide();
         $documentFinder.show();
         $documentFinder.find('input#title').focus();
       });
 
-      // add link to cancel and reset back to the default locale
+      // insert the revealLink at the appropriate point with the appropriate
+      // text depending on whether it's linking or changing an existing link
+      if($publicationInput.val() == '') {
+        $revealLink.text('Link to an existing draft document');
+        $linker.append(' or ').append($revealLink);
+      } else {
+        $revealLink.text('Change linked document');
+        $linker.append('<br />').append($revealLink);
+      }
+
+      // add link to cancel document linking
       var $resetLink = $('<a href=# class="js-cancel-announcement-linker-link">cancel</a>');
       $documentFinder.append(' or ').append($resetLink);
       $resetLink.on('click', function () {
@@ -30,7 +39,7 @@
 
       // listener to assign publication when a result is clicked on in the documentFinder
       $documentFinderResult.change(function() {
-        $annoucementForm.find('input#statistics_announcement_publication_id').val($(this).val());
+        $publicationInput.val($(this).val());
         $annoucementForm.submit();
       });
     }
