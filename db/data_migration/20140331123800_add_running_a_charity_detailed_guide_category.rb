a6d6207dba052736f0c67398bacb98d4a09863f8
Stuart Gale <bishboria@gmail.com>
2014-03-31 11:49:17 +0000

    Add 'Running a charity' detailed guide category

---
+++ db/data_migration/20140331123800_add_running_a_charity_detailed_guide_category.rb
@@ -0,0 +1,7 @@
+MainstreamCategory.create!(
+  slug: "running-charity",
+  title: "Running a charity",
+  description: "Guidance and regulations for managing a charity and trustees' responsibilities",
+  parent_title: "Charities, volunteering and honours",
+  parent_tag: "citizenship/charities-honours"
+)
