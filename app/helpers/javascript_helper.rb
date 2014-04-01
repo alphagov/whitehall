a8765a8789a6a37d44021c66ed8d07ad76790bdf
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-04-01 12:38:28 +0000

    Helper to execute scripts only in modern browsers

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -1,5 +1,14 @@
 module JavascriptHelper
   def initialise_script(constructor_or_singleton_name, params = {})
-    content_for :javascript_initialisers, raw("GOVUK.init(#{constructor_or_singleton_name}, #{params.to_json});\n");
+    content_for :javascript_initialisers, initialisation_script(constructor_or_singleton_name, params)
+  end
+
+  # real browsers and IE9+ are collectively refered to as modern browsers
+  def initialise_script_on_modern_browsers(constructor_or_singleton_name, params = {})
+    content_for :javascript_initialisers_for_modern_browsers, initialisation_script(constructor_or_singleton_name, params)
+  end
+
+  def initialisation_script(constructor_or_singleton_name, params)
+    raw("GOVUK.init(#{constructor_or_singleton_name}, #{params.to_json});\n");
   end
 end
