c3d0208aed28c8a203988f59c201c20a7985c9aa
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-04-04 12:31:00 +0000

    Fix public CIP controller functional test.
    Ensure CIPs are published, and set worldwide org correctly.

--- test/functional/corporate_information_pages_controller_test.rb
+++ test/functional/corporate_information_pages_controller_test.rb
@@ -4,14 +4,14 @@ class CorporateInformationPagesControllerTest < ActionController::TestCase
   should_be_a_public_facing_controller
 
   view_test "show renders the summary as plain text" do
-    @corporate_information_page = create(:corporate_information_page, summary: "Just plain text")
+    @corporate_information_page = create(:corporate_information_page, :published, summary: "Just plain text")
     get :show, organisation_id: @corporate_information_page.organisation, id: @corporate_information_page.slug
 
     assert_select ".description", text: "Just plain text"
   end
 
   view_test "show renders the body as govspeak" do
-    @corporate_information_page = create(:corporate_information_page, body: "## Title\n\npara1\n\n")
+    @corporate_information_page = create(:corporate_information_page, :published, body: "## Title\n\npara1\n\n")
     get :show, organisation_id: @corporate_information_page.organisation, id: @corporate_information_page.slug
 
     assert_select ".body" do
@@ -23,9 +23,9 @@ class CorporateInformationPagesControllerTest < ActionController::TestCase
   view_test "should link to world location organisation belongs to" do
     world_location = create(:world_location)
     worldwide_organisation = create(:worldwide_organisation, world_locations: [world_location])
-    corporate_information_page = create(:corporate_information_page, organisation: worldwide_organisation)
+    corporate_information_page = create(:corporate_information_page, :published, worldwide_organisation: worldwide_organisation, organisation: nil)
 
-    get :show, worldwide_organisation_id: worldwide_organisation, id: corporate_information_page.slug
+    get :show, organisation: nil, worldwide_organisation_id: worldwide_organisation, id: corporate_information_page.slug
 
     assert_select "a[href=#{worldwide_organisation_path(worldwide_organisation)}]"
     assert_select "a[href=#{world_location_path(world_location)}]"
