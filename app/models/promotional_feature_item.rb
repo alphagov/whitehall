54c71c69324e5771e0f33544f54b2bda539e69e6
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-04-11 11:43:40 +0100

    Relax requirements for promotional feature items.
    - Remove requirement for an image
    - Remove cap on links within an item
    
    This is primarily driven by need for a link-heavy
    item without an image for the Civil Service page.
    
    We can add restrictions back in if it begins to be
    abused.

--- app/models/promotional_feature_item.rb
+++ app/models/promotional_feature_item.rb
@@ -1,15 +1,11 @@
 class PromotionalFeatureItem < ActiveRecord::Base
-  LINK_LIMIT = 6
-
   belongs_to :promotional_feature, inverse_of: :promotional_feature_items
   has_one :organisation, through: :promotional_feature
   has_many :links, class_name: 'PromotionalFeatureLink', dependent: :destroy, inverse_of: :promotional_feature_item
 
   validates :summary, presence: true, length: { maximum: 500 }
-  validates :image, :image_alt_text, presence: true, on: :create
   validates_with ImageValidator, method: :image, size: [960, 640], if: :image_changed?
   validates :title_url, uri: true, allow_blank: true
-  validates :links, length: { maximum: LINK_LIMIT, message: "are limited to a maximum of #{LINK_LIMIT}" }
 
   accepts_nested_attributes_for :links, allow_destroy: true, reject_if: -> attributes { attributes['url'].blank? }
 
