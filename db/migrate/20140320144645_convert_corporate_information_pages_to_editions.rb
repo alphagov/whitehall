9aa181fa6d8e3a2272f051d3c368fc5132b7515a
Daniel Roseman <daniel@roseman.org.uk>
2014-04-28 11:45:59 +0100

    Fixes to CIP migration.
    * Make query to get existing attachments for old CIP work by adding a
    name class method.
    * Remove redundant hard-coded name.
    * Use org.build_corporate_information page methods.
    * Fix use of wrong variable in attachment creation code, create relevant
    attachment type from existing class.
    * Add down method.

--- db/migrate/20140320144645_convert_corporate_information_pages_to_editions.rb
+++ db/migrate/20140320144645_convert_corporate_information_pages_to_editions.rb
@@ -2,7 +2,7 @@ class ConvertCorporateInformationPagesToEditions < ActiveRecord::Migration
   class ::OldCorporateInformationPage < ActiveRecord::Base
     translates :summary, :body, foreign_key: :corporate_information_page_id
     belongs_to :organisation, polymorphic: true
-    has_many :attachments, as: :attachable, order: 'attachments.ordering, attachments.id', source_type: "CorporateInformationPage"
+    has_many :attachments, as: :attachable, order: 'attachments.ordering, attachments.id'
     delegate :slug, :display_type_key, to: :type
     def type
       CorporateInformationPageType.find_by_id(type_id)
@@ -10,18 +10,17 @@ class ConvertCorporateInformationPagesToEditions < ActiveRecord::Migration
     def title
       type.title(organisation)
     end
+    # Need a name classmethod so that has_many query (eg attachments) uses
+    # correct type to find existing items.
+    def self.name
+      'CorporateInformationPage'
+    end
 
   end
   OldCorporateInformationPage.table_name = 'corporate_information_pages'
   OldCorporateInformationPage::Translation.table_name = 'corporate_information_page_translations'
   Whitehall.skip_safe_html_validation = true
 
-  # default title text for validation only.
-  CorporateInformationPage.class_eval do
-    def title
-      "title"
-    end
-  end
   def up
 
     gds_ig_team_user = User.find_by_name!('GDS Inside Government Team')
@@ -32,29 +31,18 @@ class ConvertCorporateInformationPagesToEditions < ActiveRecord::Migration
         doc = Document.create!(document_type: 'CorporateInformationPage',
                                created_at: old_cip.created_at,
                                updated_at: old_cip.updated_at)
-        new_cip = CorporateInformationPage.create!(
+        new_cip = org.build_corporate_information_page(
           created_at: old_cip.created_at,
           updated_at: old_cip.updated_at,
           lock_version: old_cip.lock_version,
           document_id: doc.id,
           creator: gds_ig_team_user,
-          title: old_cip.title,
           summary: old_cip.summary,
           body: old_cip.body,
           corporate_information_page_type_id: old_cip.type_id,
           major_change_published_at: old_cip.updated_at,
           state: 'published')
-        if org.is_a? Organisation
-          EditionOrganisation.create!(
-            edition: new_cip,
-            organisation: org
-          )
-        else
-          EditionWorldwideOrganisation.create!(
-            edition: new_cip,
-            worldwide_organisation: org
-          )
-        end
+        new_cip.save!
         old_cip.translations.each do |old_trans|
           unless old_trans.locale == :en
             new_cip.translations.create!(
@@ -67,25 +55,30 @@ class ConvertCorporateInformationPagesToEditions < ActiveRecord::Migration
         end
         old_cip.attachments.each do |old_att|
           # Create new Attachments, but keep existing attachment_data instances.
-          new_cip.attachments.create!(
-            title: old_cip.title,
-            accessible: old_cip.accessible,
-            isbn: old_cip.isbn,
-            unique_reference: old_cip.unique_reference,
-            command_paper_number: old_cip.command_paper_number,
-            order_url: old_cip.url,
-            price_in_pence: old_cip.price_in_pence,
-            attachment_data_id: old_cip.attachment_data_id,
-            ordering: old_cip.ordering,
-            hoc_paper_number: old_cip.hoc_paper_number,
-            parliamentary_session: old_cip.hoc_parliamentary_session,
-            unnumbered_command_paper: old_cip.unnumbered_command_paper,
-            unnumbered_hoc_paper: old_cip.unnumbered_hoc_paper,
-            type: old_cip.type,
-            slug: old_cip.slug,
-            body: old_cip.body,
-            manually_numbered_headings: old_cip.manually_numbered_headings,
-            locale: old_cip.locale
+          # Can't set attachment_type directly, so use the class of the
+          # existing instance to create the new one.
+          old_att.class.create!(
+            created_at: old_att.created_at,
+            updated_at: old_att.updated_at,
+            title: old_att.title,
+            accessible: old_att.accessible,
+            isbn: old_att.isbn,
+            unique_reference: old_att.unique_reference,
+            command_paper_number: old_att.command_paper_number,
+            order_url: old_att.order_url,
+            price_in_pence: old_att.price_in_pence,
+            attachment_data_id: old_att.attachment_data_id,
+            ordering: old_att.ordering,
+            hoc_paper_number: old_att.hoc_paper_number,
+            parliamentary_session: old_att.parliamentary_session,
+            unnumbered_command_paper: old_att.unnumbered_command_paper,
+            unnumbered_hoc_paper: old_att.unnumbered_hoc_paper,
+            slug: old_att.slug,
+            body: old_att.body,
+            manually_numbered_headings: old_att.manually_numbered_headings,
+            locale: old_att.locale,
+            attachable_type: "Edition",
+            attachable_id: new_cip.id,
           )
         end
         # The unique identifier in the search index is the page URL, which has
@@ -96,5 +89,6 @@ class ConvertCorporateInformationPagesToEditions < ActiveRecord::Migration
   end
 
   def down
+    Document.where(document_type: "CorporateInformationPage").find_each {|doc| doc.destroy }
   end
 end
