309a4c329e6c90a07200b1cb057d5c807b864994
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-04-30 10:54:10 +0000

    Prevent Whitehall::Wait from oversleeping.
    This was sleeping for too long because `time_to_sleep` wasn't being
    updated on each loop iteration, and `time_slept` was being compared with
    `<` not `<=`.  The result was that most of the time it would sleep twice
    as long as it was expected to, resulting in a c. 2 minute delay in
    scheduled publishing.

--- test/unit/whitehall/wait_test.rb
+++ test/unit/whitehall/wait_test.rb
@@ -2,30 +2,40 @@ require 'test_helper'
 
 class WaitTest < ActiveSupport::TestCase
 
+  setup do
+    # reset global Timecop.freeze call in test_helper
+    Timecop.return
+
+    @base_time = Time.zone.now
+  end
+
   test "sleeps for the number of seconds until the timestamp before executing the block" do
-    order_of_work = sequence('order of work')
-    Whitehall::Wait.expects(:sleep).returns(2).in_sequence(order_of_work)
-    self.expects(:do_some_work).in_sequence(order_of_work)
+    time_state = states('time_state').starts_as('initial')
+    Time.zone.stubs(:now).when(time_state.is('initial')).returns(@base_time)
+    Time.zone.stubs(:now).when(time_state.is('slept_once')).returns(@base_time + 2.seconds)
 
-    Timecop.freeze(Time.zone.now) do
-      Whitehall::Wait.until(2.seconds.from_now) do
-        do_some_work
-      end
+    Whitehall::Wait.expects(:sleep).with(2.0).when(time_state.is('initial')).then(time_state.is('slept_once'))
+    self.expects(:do_some_work).when(time_state.is('slept_once'))
+
+    Whitehall::Wait.until(2.seconds.from_now) do
+      do_some_work
     end
-
   end
 
   test "re-sleeps if the actual time slept is less than that asked for" do
-    order_of_work = sequence('order of work')
-    Whitehall::Wait.expects(:sleep).returns(45).in_sequence(order_of_work)
-    Whitehall::Wait.expects(:sleep).returns(3).in_sequence(order_of_work)
-    Whitehall::Wait.expects(:sleep).returns(2).in_sequence(order_of_work)
-    self.expects(:do_some_work).in_sequence(order_of_work)
+    time_state = states('time_state').starts_as('initial')
+    Time.zone.stubs(:now).when(time_state.is('initial')).returns(@base_time)
+    Time.zone.stubs(:now).when(time_state.is('slept_once')).returns(@base_time + 45.seconds)
+    Time.zone.stubs(:now).when(time_state.is('slept_twice')).returns(@base_time + 48.seconds)
+    Time.zone.stubs(:now).when(time_state.is('slept_thrice')).returns(@base_time + 50.seconds)
 
-    Timecop.freeze(Time.zone.now) do
-      Whitehall::Wait.until(50.seconds.from_now) do
-        do_some_work
-      end
+    Whitehall::Wait.expects(:sleep).with(50.0).when(time_state.is('initial')).then(time_state.is('slept_once'))
+    Whitehall::Wait.expects(:sleep).with(5.0).when(time_state.is('slept_once')).then(time_state.is('slept_twice'))
+    Whitehall::Wait.expects(:sleep).with(2.0).when(time_state.is('slept_twice')).then(time_state.is('slept_thrice'))
+    self.expects(:do_some_work).when(time_state.is('slept_thrice'))
+
+    Whitehall::Wait.until(50.seconds.from_now) do
+      do_some_work
     end
   end
 
