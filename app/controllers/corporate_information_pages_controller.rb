3e9ac8678b0beed9f216e39c10c4c20dc3f1969a
Tekin Suleyman <tekin@tekin.co.uk>
2014-05-01 12:30:40 +0100

    No need for explicit return
    Plus it goes against our styleguide

--- app/controllers/corporate_information_pages_controller.rb
+++ app/controllers/corporate_information_pages_controller.rb
@@ -13,7 +13,7 @@ class CorporateInformationPagesController < DocumentsController
 
   def find_document_or_edition_for_public
     published_edition = @organisation.corporate_information_pages.published.for_slug(params[:id])
-    return published_edition if published_edition.present? && published_edition.available_in_locale?(I18n.locale)
+    published_edition if published_edition.present? && published_edition.available_in_locale?(I18n.locale)
   end
 
   def set_slimmer_headers_for_document
