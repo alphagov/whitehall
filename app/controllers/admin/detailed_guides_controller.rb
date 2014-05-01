e6580991903414edbffb75682617fb3e987ab54c
Tekin Suleyman <tekin@tekin.co.uk>
2014-05-01 11:15:57 +0100

    Handle legacy params temporarily
    This is so that any editors that have a detailed guide open for editing
    whilst the deploy is in progress can still successfully submit their
    changes.
    
    This can be rolled back once the code has been successfully deployed.

--- app/controllers/admin/detailed_guides_controller.rb
+++ app/controllers/admin/detailed_guides_controller.rb
@@ -1,10 +1,17 @@
 class Admin::DetailedGuidesController < Admin::EditionsController
   include Admin::EditionsController::NationalApplicability
 
+  prepend_before_filter :massage_legacy_related_detailed_guide_ids, only: [:create, :update]
   before_filter :build_image, only: [:new, :edit]
 
 private
   def edition_class
     DetailedGuide
+  end
+
+  def massage_legacy_related_detailed_guide_ids
+    if params.fetch(:edition, {})[:outbound_related_detailed_guide_ids]
+      params[:edition][:related_document_ids] = params[:edition].delete(:outbound_related_detailed_guide_ids)
+    end
   end
 end
