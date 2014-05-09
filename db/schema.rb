15482860e7c2d45e67548bd5ec8912144e61f134
Tekin Suleyman <tekin@tekin.co.uk>
2014-05-09 09:43:54 +0100

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -253,22 +253,6 @@ ActiveRecord::Schema.define(:version => 20140429162738) do
     t.datetime "updated_at"
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",                       :default => 0
-    t.integer  "attempts",                       :default => 0
-    t.text     "handler",    :limit => 16777215
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "document_collection_group_memberships", :force => true do |t|
     t.integer  "document_id"
     t.integer  "document_collection_group_id"
