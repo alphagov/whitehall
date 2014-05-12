f166e001f05f43204c215112c31ac3f161cc9e01
Tekin Suleyman <tekin@tekin.co.uk>
2014-05-12 15:02:02 +0100

    Delay search indexing to work around transaction
    Scheduled publishing is currently performed every 15 minutes across all
    backend machines (keeps all machines equal and introduces redundancy
    into the process) using a rake task. Each due edition is published by
    EditionPublishingWorker, which performs the action with a transaction
    lock so that only one backend machine will actually succeed if more than
    one attempts to publish the same edition. The issue is that whilst the
    transation is in progress, the edition will remain in a "scheduled"
    state in the database. And because search indexing is performed
    asynchronously by sidekiq, it is sometimes the case that the search
    indexing job will be picked up and attempted before the transaction can
    complete. This results in the edition not being indexed, as
    SearchIndexAddWorker will not index a non-"published" edition.
    
    This change introduces a 10 second delay to the search indexing job to
    give the transaction enough time to complete.
    
    Note that this is a temporary measure to get scheduled editions indexing
    reliably. A story is to be spun up for a fuller solution that doesn't
    rely on a transaction lock when publishing scheduled editions.

--- test/unit/whitehall/search_index_test.rb
+++ test/unit/whitehall/search_index_test.rb
@@ -5,18 +5,27 @@ module Whitehall
     class ::SearchableClass
     end
 
-    test 'SearchIndex.add queues a search index job for the instance' do
+    test 'SearchIndex.add queues a search index add job for 10 seconds time to allow any transactions to complete' do
       searchable_thing = stub(class: SearchableClass, id: 'id')
 
-      SearchIndexAddWorker.expects(:perform_async).with('SearchableClass', 'id')
-      SearchIndex.add(searchable_thing)
+      Sidekiq::Testing.fake! do
+        SearchIndex.add(searchable_thing)
+        job = SearchIndexAddWorker.jobs.last
+
+        assert_equal ['SearchableClass', 'id'], job['args']
+        assert_equal 10.seconds.from_now.to_i, job['at']
+      end
     end
 
-    test 'SearchIndex.delete queues a search index removal job for the instance' do
+    test 'SearchIndex.delete queues a search index removal job for the instance based on its slug and rummager index' do
       searchable_thing = stub(search_index: { 'link' => 'full_slug' }, rummager_index: :index_name)
 
-      SearchIndexDeleteWorker.expects(:perform_async).with('full_slug', :index_name)
-      SearchIndex.delete(searchable_thing)
+      Sidekiq::Testing.fake! do
+        SearchIndex.delete(searchable_thing)
+        job = SearchIndexDeleteWorker.jobs.last
+
+        assert_equal ['full_slug', 'index_name'], job['args']
+      end
     end
   end
 end
