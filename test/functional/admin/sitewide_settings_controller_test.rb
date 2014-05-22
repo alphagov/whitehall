af119a104ab0300775dc4f9d64a140d633b1f59f
Camille Baldock <camille@camillebaldock.co.uk>
2014-05-22 11:07:18 +0100

    Additional test for sitewide settings update method

--- test/functional/admin/sitewide_settings_controller_test.rb
+++ test/functional/admin/sitewide_settings_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class Admin::SitewideSettingsControllerTest < ActionController::TestCase
+  setup do
+    login_as :gds_editor
+  end
+
   should_be_an_admin_controller
 
   [:index, :edit, :update].each do |action_method|
@@ -9,6 +13,15 @@ class Admin::SitewideSettingsControllerTest < ActionController::TestCase
       get action_method
       assert_response 403
     end
+  end
+
+  test "PUT on :update updates the sitewide setting" do
+    sitewide_setting = create(:sitewide_setting)
+    put :update, id: sitewide_setting, :sitewide_setting => {:on => true, :govspeak => "govspeak text"}
+
+    assert_equal 'govspeak text', sitewide_setting.reload.govspeak
+    assert_equal true, sitewide_setting.reload.on
+    assert_redirected_to admin_sitewide_settings_path
   end
 
 end
