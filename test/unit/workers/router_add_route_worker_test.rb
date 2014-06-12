9e4c1de6ce02bdc2bafc582ccd4f01ded39551e6
Tekin Suleyman <tekin@tekin.co.uk>
2014-06-12 10:55:17 +0100

    Remove more redundant code
    Direct route registration is no longer performed - route registration
    happens during panopticon registration now.

--- test/unit/workers/router_add_route_worker_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require 'gds_api/router'
-
-class RouterAddRouteWorkerTest < ActiveSupport::TestCase
-  test "adds route to router" do
-    GdsApi::Router.any_instance.expects(:add_route).with("/slug-here", "exact", "whitehall-frontend").once
-    RouterAddRouteWorker.new.perform("/slug-here")
-  end
-end
