c0714ba55735651589eef4d9819cba2f110b26a4
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-07-08 14:40:55 +0000

    Slug change for UKTI Belgium.

---
+++ db/data_migration/20140708140023_ukti_belgium_slug_change.rb
@@ -0,0 +1,12 @@
+require 'gds_api/router'
+
+router = GdsApi::Router.new(Plek.current.find('router-api'))
+
+world_org = WorldwideOrganisation.find_by_slug('uk-trade-investment')
+world_org.slug = 'uk-trade-investment-belgium'
+world_org.save!
+
+# Redirect old URL to UKTI organisation page.
+router.add_redirect_route("/government/world/organisations/uk-trade-investment",
+                          'exact',
+                          "/government/world/organisations/uk-trade-investment-belgium")
