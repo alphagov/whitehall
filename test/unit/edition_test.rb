77f0ee3ee82ed32121f52019341f8e8ca2156112
Tekin Suleyman <tekin@tekin.co.uk>
2014-07-09 12:31:37 +0100

    Inline scheduled publishing scopes
    No need to junk-draw these into another module.

--- test/unit/edition_test.rb
+++ test/unit/edition_test.rb
@@ -104,6 +104,39 @@ class EditionTest < ActiveSupport::TestCase
     assert_equal 'My new version', version_3.most_recent_change_note
   end
 
+    test "can find editions due for publication" do
+    due_in_one_day = create(:edition, :scheduled, scheduled_publication: 1.day.from_now)
+    due_in_two_days = create(:edition, :scheduled, scheduled_publication: 2.days.from_now)
+    already_published = create(:edition, :published, scheduled_publication: 1.day.from_now)
+    Timecop.freeze 1.day.from_now do
+      assert_equal [due_in_one_day], Edition.due_for_publication
+    end
+    Timecop.freeze 2.days.from_now do
+      assert_equal [due_in_one_day, due_in_two_days], Edition.due_for_publication
+    end
+  end
+
+  test "can find editions due for publication within a certain time span" do
+    due_in_one_day = create(:edition, :scheduled, scheduled_publication: 1.day.from_now)
+    due_in_two_days = create(:edition, :scheduled, scheduled_publication: 2.days.from_now)
+    assert_equal [due_in_one_day], Edition.due_for_publication(1.day)
+    assert_equal [due_in_one_day, due_in_two_days], Edition.due_for_publication(2.days)
+  end
+
+  test ".scheduled_for_publication_as returns edition if edition is scheduled" do
+    edition = create(:scheduled_publication, scheduled_publication: 1.day.from_now)
+    assert_equal edition, Publication.scheduled_for_publication_as(edition.document.to_param)
+  end
+
+  test ".scheduled_for_publication_as returns nil if edition is not scheduled" do
+    edition = create(:draft_publication, scheduled_publication: 1.day.from_now)
+    assert_nil Edition.scheduled_for_publication_as(edition.document.to_param)
+  end
+
+  test ".scheduled_for_publication_as returns nil if document is unknown" do
+    assert_nil Edition.scheduled_for_publication_as('unknown')
+  end
+
   test "should return a list of editions in a topic" do
     topic_1 = create(:topic)
     topic_2 = create(:topic)
