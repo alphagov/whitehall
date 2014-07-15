8ca68cdfc1979520cffc90ba6c54931739087523
Tekin Suleyman <tekin@tekin.co.uk>
2014-07-15 11:53:38 +0100

    Configure sidekiq error handling
    Sidekiq 3 removed automatic error handling so we explicitly set it up
    here.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@ redis_config = YAML.load_file(Rails.root.join("config", "redis.yml")).symbolize_
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
+  config.error_handlers << Proc.new {|ex, context_hash| Airbrake.notify(ex, context_hash) }
 end
 
 Sidekiq.configure_client do |config|
