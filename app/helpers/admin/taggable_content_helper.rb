00296462655c3a110223ffb28340f1108e58a1ad
Tekin Suleyman <tekin@tekin.co.uk>
2014-09-09 16:52:34 +0100

    Caching for topical event selects

--- app/helpers/admin/taggable_content_helper.rb
+++ app/helpers/admin/taggable_content_helper.rb
@@ -11,10 +11,27 @@ module Admin::TaggableContentHelper
     end
   end
 
+  # Returns an Array that represents the current set of taggable topical
+  # events. Each element of the array consists of two values: the first is the
+  # topical event name; the second the topic ID.
+  def taggable_topical_events_container
+    Rails.cache.fetch(taggable_topical_events_cache_digest) do
+      TopicalEvent.order(:name).map { |te| [te.name, te.id] }
+    end
+  end
+
   # Returns an MD5 digest representing the current set of taggable topics. This
   # will change if any of the Topics should change or if a new topic is added.
   def taggable_topics_cache_digest
-    update_timestamps = Topic.order(:created_at).pluck(:updated_at).map(&:to_i).join
+    update_timestamps = Topic.order(:id).pluck(:updated_at).map(&:to_i).join
     Digest::MD5.hexdigest "taggable-topics-#{update_timestamps}"
+  end
+
+  # Returns an MD5 digest representing the current set of taggable topical
+  # events. This will change if any of the Topics should change or if a new
+  # topic is added.
+  def taggable_topical_events_cache_digest
+    update_timestamps = TopicalEvent.order(:id).pluck(:updated_at).map(&:to_i).join
+    Digest::MD5.hexdigest "taggable-topical-events-#{update_timestamps}"
   end
 end
--- test/unit/helpers/admin/taggable_content_helper_test.rb
+++ test/unit/helpers/admin/taggable_content_helper_test.rb
@@ -13,4 +13,16 @@ class Admin::TaggableContentHelperTest < ActionView::TestCase
       ['Topic C', topic_c.id],
     ], taggable_topics_container
   end
+
+  test '#taggable_topical_events_container returns an array of name/ID pairs for all TopicalEvents' do
+    event_a = create(:topical_event, name: 'Event A')
+    event_c = create(:topical_event, name: 'Event C')
+    event_b = create(:topical_event, name: 'Event B')
+
+    assert_equal [
+      ['Event A', event_a.id],
+      ['Event B', event_b.id],
+      ['Event C', event_c.id],
+    ], taggable_topical_events_container
+  end
 end
