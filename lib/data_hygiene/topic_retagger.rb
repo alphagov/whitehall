db3e731e1f8a1f9d6388280d9af4fc621be658fd
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-09-09 10:19:56 +0100

    Script to move content from one topic to another
    This is part of https://www.pivotaltracker.com/story/show/76471962
    
    All editions tagged with the source topic are retagged replacing the
    source topic with the destination topic.
    
    I decided to apply this to all editions including old ones since this
    script is expected to be used when doing bulk re-assignment of topics;
    we'll be likely to delete the source topic using the existing
    specialist_sector_cleanup task after running this, so any old editions
    where we didn't re-assign the topic would just lose their topic.

---
+++ lib/data_hygiene/topic_retagger.rb
@@ -0,0 +1,93 @@
+class TopicRetagger
+  attr_reader :source_topic_id, :destination_topic_id
+
+  def initialize(source_topic_id, destination_topic_id)
+    @source_topic_id = source_topic_id
+    @destination_topic_id = destination_topic_id
+  end
+
+  def retag
+    # Grab the list of published editions before we make any changes to the
+    # tags.  We'll need to re-register these after the re-tagging is done.
+    taggings, published_editions = get_taggings_and_editions(source_topic_id)
+
+    log "Updating #{taggings.count} taggings of editions (#{published_editions.count} published) to change #{source_topic_id} to #{destination_topic_id}"
+
+    update_taggings(taggings)
+    register_editions(published_editions)
+  end
+
+private
+
+  def update_taggings(taggings)
+    taggings.reject { |tagging| tagging.edition.nil? }.each do |tagging|
+      if tagging.edition.specialist_sector_tags.include? destination_topic_id
+        remove_tagging(tagging)
+      else
+        change_tagging(tagging)
+      end
+    end
+  end
+
+  def get_taggings_and_editions(topic_id)
+    taggings = SpecialistSector.where(tag: topic_id)
+
+    published_editions = taggings.map { |tagging|
+      tagging.edition.try(:document).try(:published_edition)
+    }.compact.uniq
+
+    [taggings, published_editions]
+  end
+
+  def register_editions(editions)
+    editions.each do |edition|
+      log "registering '#{edition.slug}'"
+      registerable_edition = RegisterableEdition.new(edition)
+      registerer           = Whitehall.panopticon_registerer_for(registerable_edition)
+      registerer.register(registerable_edition)
+    end
+  end
+
+  def remove_tagging(tagging)
+    edition = tagging.edition
+    log "removing tagging on '#{edition.slug}' edition #{edition.id}"
+    tagging.destroy
+
+    add_editorial_remark(edition,
+      "Bulk retagging from topic '#{source_topic_id}' to '#{destination_topic_id}' resulted in duplicate tag - removed it"
+    )
+  end
+
+  def change_tagging(tagging)
+    edition = tagging.edition
+    log "tagging '#{edition.slug}' edition #{edition.id}"
+    tagging.tag = destination_topic_id
+    tagging.save!
+
+    add_editorial_remark(edition,
+      "Bulk retagging from topic '#{source_topic_id}' to '#{destination_topic_id}' changed tag"
+    )
+  end
+
+  def add_editorial_remark(edition, message)
+    if edition.nil?
+      log " - no edition (probably deleted)"
+    elsif Edition::FROZEN_STATES.include?(edition.state)
+      log " - edition is frozen; skipping editorial remarks"
+    else
+      log " - adding editorial remark"
+      edition.editorial_remarks.create!(
+        author: gds_user,
+        body: message
+      )
+    end
+  end
+
+  def gds_user
+    @gds_user ||= User.find_by_email("govuk-whitehall@digital.cabinet-office.gov.uk")
+  end
+
+  def log(message)
+    puts message
+  end
+end
