bf522fc97511cf4843f262541f564583fd14d8db
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-10-20 08:15:09 +0000

    Simplify validates statement.

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -9,8 +9,7 @@ class Consultation < Publicationesque
 
   validates :opening_at, presence: true, unless: ->(consultation) { consultation.can_have_some_invalid_data? }
   validates :closing_at, presence: true, unless: ->(consultation) { consultation.can_have_some_invalid_data? }
-  validates :external_url, presence: { if: :external? }
-  validates :external_url, uri: true, allow_blank: true
+  validates :external_url, uri: true, allow_blank: true, presence: { if: :external? }
   validate :validate_closes_after_opens
 
   has_one :outcome, class_name: 'ConsultationOutcome', foreign_key: :edition_id, dependent: :destroy
