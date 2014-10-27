3847e30f63513365a7d3b1dbef786355e163b52c
Tekin Suleyman <tekin@tekin.co.uk>
2014-10-27 15:14:13 +0000

    Don't show warning if matches filter
    If a user has clicked the link in the warning, then we don't want to show them
    the warning again.

--- app/controllers/admin/statistics_announcements_controller.rb
+++ app/controllers/admin/statistics_announcements_controller.rb
@@ -1,7 +1,7 @@
 class Admin::StatisticsAnnouncementsController < Admin::BaseController
   before_filter :find_statistics_announcement, only: [:show, :edit, :update, :cancel, :publish_cancellation, :cancel_reason]
   before_filter :redirect_to_show_if_cancelled, only: [:cancel, :publish_cancellation]
-  helper_method :unlinked_announcements_count
+  helper_method :unlinked_announcements_count, :show_unlinked_announcements_warning?
 
   def index
     @filter = Admin::StatisticsAnnouncementFilter.new(filter_params)
@@ -82,6 +82,14 @@ private
       dates: 'future',
       user_id: current_user.id
     }
+  end
+
+  def show_unlinked_announcements_warning?
+    !filtering_imminent_unlinked_announcements? && unlinked_announcements_count > 0
+  end
+
+  def filtering_imminent_unlinked_announcements?
+    @filter.options[:dates] == 'four-weeks' && @filter.options[:unlinked_only] == '1'
   end
 
   def unlinked_announcements_count
