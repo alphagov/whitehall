8a13f18db428d0ff711b088c6159205396060940
Tekin Suleyman <tekin@tekin.co.uk>
2014-10-28 16:48:26 +0000

    Cleanup old constant references
    Now that the code that renamed and reworked these workers has been deployed and
    the queue has had adequate time to run down, we can get rid of these temporary
    references to the old workers.

--- app/workers/publishing_api_register_organisation_worker.rb
+++
@@ -1,4 +0,0 @@
-# FIXME: This is a temporary constant here to allow any existing queued jobs to
-# be processed by sidekiq. Once the queue has had a reasonable time to be worked
-# through, this file can be deleted.
-PublishingApiRegisterOrganisationWorker = PublishingApiOrganisationWorker
