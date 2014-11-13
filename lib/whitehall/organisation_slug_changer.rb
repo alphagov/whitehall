5c5ac122bda181d9272ff0c979532086bb0201e5
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-11-13 17:45:36 +0000

    Add organisation slug changer command

---
+++ lib/whitehall/organisation_slug_changer.rb
@@ -0,0 +1,41 @@
+require 'gds_api/router'
+
+class Whitehall::OrganisationSlugChanger
+  attr_reader :organisation, :old_slug, :new_slug, :logger, :router
+
+  def initialize(organisation, new_slug, logger: nil, router: nil)
+    @organisation = organisation
+    @old_slug = organisation.slug
+    @new_slug = new_slug
+    @logger = logger || Logger.new(nil)
+    @router = router || default_router
+  end
+
+  def call
+    logger.info "Changing org slug from #{old_slug} to #{new_slug}"
+
+    # Remove document at old slug from search
+    organisation.remove_from_search_index
+
+    Organisation.transaction do
+      organisation.slug = new_slug
+      organisation.save! # saving also indexes at new slug
+
+      User.where(:organisation_slug => old_slug).update_all(:organisation_slug => new_slug)
+    end
+
+    logger.info "Creating redirect for old org URL in router"
+    router.add_redirect_route("/government/organisations/#{old_slug}",
+                              "exact",
+                              "/government/organisations/#{new_slug}")
+
+    logger.info "Re-registering #{new_slug} published editions in search"
+    organisation.editions.published.each do |edition|
+      edition.update_in_search_index
+    end
+  end
+
+  def default_router
+    GdsApi::Router.new(Plek.current.find('router-api'))
+  end
+end
---
+++ test/unit/whitehall/organisation_slug_changer_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+module Whitehall
+  class OrganisationSlugChangerTest < ActiveSupport::TestCase
+    setup do
+      @router = stub("router", add_redirect_route: nil)
+      @organisation = create(:organisation)
+      @organisation.stubs(:remove_from_search_index)
+      @new_slug = 'new-slug'
+      @slug_changer = OrganisationSlugChanger.new(@organisation, @new_slug, router: @router)
+    end
+
+    test 'it removes the org from search index' do
+      @organisation.expects(:remove_from_search_index)
+
+      @slug_changer.call
+    end
+
+    test 'changes the org slug' do
+      @slug_changer.call
+
+      assert_equal @new_slug, @organisation.reload.slug
+    end
+
+    test 'changes org slugs of any users' do
+      user = create(:user, organisation: @organisation)
+
+      @slug_changer.call
+
+      assert_equal @new_slug, user.reload.organisation_slug
+    end
+
+    test 'adds to search index' do
+      @organisation.expects(:update_in_search_index)
+
+      @slug_changer.call
+    end
+
+    test 'adds redirect route' do
+      @router.expects(:add_redirect_route).with(
+        "/government/organisations/#{@organisation.slug}",
+        "exact",
+        "/government/organisations/#{@new_slug}"
+      )
+      @slug_changer.call
+    end
+
+    test 're-registers any published editions associated with the organisation' do
+      Whitehall::SearchIndex.stubs(:add)
+
+      edition = create(:published_publication, organisations: [@organisation])
+      Whitehall::SearchIndex.expects(:add).with(edition)
+
+      @slug_changer.call
+    end
+  end
+end
