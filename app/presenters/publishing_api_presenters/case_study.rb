ab301106bb2373d26ccbc2b29432cad28e9c30f8
Tekin Suleyman <tekin@tekin.co.uk>
2014-11-13 12:14:17 +0000

    Publishing API: Include an image for case studies
    If the case study or it's org has an image available, we include it in the JSON
    sent to the Publishing API.
    
    Note that this code will not fall back to the "placeholder" image if neither the
    case study or the organisation doesn't have an image. This is intentional as the
    responsibility for deciding what to display if no image is available should lie
    with the public-facing application that renders the content.

--- app/presenters/publishing_api_presenters/case_study.rb
+++ app/presenters/publishing_api_presenters/case_study.rb
@@ -7,14 +7,33 @@ module PublishingApiPresenters
   private
 
     def details
-      super.merge({
-        body: body,
-        first_published_at: edition.first_public_at,
-      })
+      super.merge({body: body, first_published_at: edition.first_public_at}).tap do |json|
+        json[:image] = image_details if image_available?
+      end
+    end
+
+    def image_details
+      {
+        url: Whitehall.asset_root + presented_case_study.lead_image_path,
+        alt_text: presented_case_study.lead_image_alt_text,
+        caption: presented_case_study.lead_image_caption,
+      }
     end
 
     def body
       Whitehall::EditionGovspeakRenderer.new(edition).body
+    end
+
+    def image_available?
+      edition.images.any? || lead_organisation_default_image_available?
+    end
+
+    def lead_organisation_default_image_available?
+      edition.lead_organisations.first.default_news_image.present?
+    end
+
+    def presented_case_study
+      CaseStudyPresenter.new(edition)
     end
   end
 end
--- test/unit/presenters/publishing_api_presenters/case_study_test.rb
+++ test/unit/presenters/publishing_api_presenters/case_study_test.rb
@@ -47,7 +47,37 @@ class PublishingApiPresenters::CaseStudyTest < ActiveSupport::TestCase
     assert_equivalent_html expected_hash[:details].delete(:body),
       presented_hash[:details].delete(:body)
 
-    assert_equal expected_hash[:details], presented_hash[:details]
+    assert_equal_hash expected_hash[:details], presented_hash[:details]
+  end
+
+  test "includes details of the case study image if present" do
+    image = build(:image, alt_text: 'Image alt text', caption: 'A caption')
+    case_study = create(:published_case_study, images: [image])
+
+    expected_hash = {
+      url: (Whitehall.asset_root + image.url(:s300)),
+      alt_text: image.alt_text,
+      caption: image.caption
+    }
+    presented_hash = present(case_study)
+
+    assert_equal_hash expected_hash, presented_hash[:details][:image]
+  end
+
+  test "falls back to the organisation's default news image when there is no image" do
+    organisation_image = DefaultNewsOrganisationImageData.new(file: image_fixture_file)
+    organisation = create(:organisation, default_news_image: organisation_image)
+
+    case_study = create(:published_case_study, lead_organisations: [organisation])
+
+    expected_hash = {
+      url: (Whitehall.asset_root + organisation_image.file.url(:s300)),
+      alt_text: 'placeholder',
+      caption: nil
+    }
+    presented_hash = present(case_study)
+
+    assert_equal_hash expected_hash, presented_hash[:details][:image]
   end
 
   def assert_equal_hash(expected, actual)
