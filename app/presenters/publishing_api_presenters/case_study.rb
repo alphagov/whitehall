4c826e20c86af5ebb8aedd725a59913ebd0abd4b
Tekin Suleyman <tekin@tekin.co.uk>
2014-11-13 10:55:55 +0000

    Refactor PublishingApiPresenter::CaseStudy
    Remove the duplication between the presenters with inheritance

--- app/presenters/publishing_api_presenters/case_study.rb
+++ app/presenters/publishing_api_presenters/case_study.rb
@@ -1,50 +1,20 @@
 module PublishingApiPresenters
-  class CaseStudy < Struct.new(:edition)
-    def base_path
-      Whitehall.url_maker.public_document_path(edition)
-    end
-
+  class CaseStudy < PublishingApiPresenters::Edition
     def as_json
-      {
-        title: edition.title,
-        base_path: base_path,
-        description: edition.summary,
-        format: "case_study",
-        need_ids: edition.need_ids,
-        public_updated_at: edition.public_timestamp,
-        update_type: update_type,
-        publishing_app: "whitehall",
-        rendering_app: "whitehall-frontend",
-        routes: [ { path: base_path, type: "exact" } ],
-        redirects: [],
-        details: details
-      }
+      super.merge(format: "case_study")
     end
 
   private
 
     def details
-      {
+      super.merge({
         body: body,
         first_published_at: edition.first_public_at,
-        change_note: edition.most_recent_change_note,
-        tags: {
-          browse_pages: [],
-          topics: specialist_sectors,
-        }
-      }
+      })
     end
 
     def body
       Whitehall::EditionGovspeakRenderer.new(edition).body
-    end
-
-    def update_type
-      edition.minor_change? ? 'minor' : 'major'
-    end
-
-    def specialist_sectors
-      [edition.primary_specialist_sector_tag].compact + edition.secondary_specialist_sector_tags
     end
   end
 end
