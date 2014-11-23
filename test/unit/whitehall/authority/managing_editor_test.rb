d974dd2ad06a284a5b1f6a5a4bff44da7148ff48
marksheldonGDS <mark.sheldon@digital.cabinet-office.gov.uk>
2014-11-23 13:33:31 +0000

    Managing Editor permissions for featured links
    Allows Managing Editors to add and edit featured links on and
    organisation homepage, when they are a member of that organisation or a
    parent organisation.

--- test/unit/whitehall/authority/managing_editor_test.rb
+++ test/unit/whitehall/authority/managing_editor_test.rb
@@ -156,13 +156,15 @@ class ManagingEditorTest < ActiveSupport::TestCase
     refute enforcer_for(managing_editor, :sitewide_settings_section).can?(:administer)
   end
 
-  test 'can manage featured links for their organisation' do
+  test 'can manage featured links for their organisation and child organisation' do
     user = managing_editor
 
     editors_org = user.organisation
+    child_org = create(:organisation, parent_organisations: [editors_org])
     other_org = build(:organisation)
 
     assert enforcer_for(user, editors_org).can?(:manage_featured_links)
+    assert enforcer_for(user, child_org).can?(:manage_featured_links)
     refute enforcer_for(user, other_org).can?(:manage_featured_links)
   end
 end
