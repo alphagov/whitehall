75c54f2ec25069ba87d1bb44a9822d853a60c542
Tekin Suleyman <tekin@tekin.co.uk>
2014-11-24 10:24:40 +0000

    Ensure class namespacing is honored

--- app/presenters/publishing_api_presenters/case_study.rb
+++ app/presenters/publishing_api_presenters/case_study.rb
@@ -1,50 +1,48 @@
-module PublishingApiPresenters
-  class CaseStudy < PublishingApiPresenters::Edition
-    def as_json
-      super.merge(format: "case_study", links: links)
-    end
+class PublishingApiPresenters::CaseStudy < PublishingApiPresenters::Edition
+  def as_json
+    super.merge(format: "case_study", links: links)
+  end
 
-  private
+private
 
-    def links
-      {
-        lead_organisations: edition.lead_organisations.map(&:content_id),
-        supporting_organisations: edition.supporting_organisations.map(&:content_id)
-      }
-    end
+  def links
+    {
+      lead_organisations: edition.lead_organisations.map(&:content_id),
+      supporting_organisations: edition.supporting_organisations.map(&:content_id)
+    }
+  end
 
-    def details
-      super.merge({
-        body: body,
-        first_public_at: edition.first_public_at,
-        change_history: edition.change_history.as_json,
-      }).tap do |json|
-        json[:image] = image_details if image_available?
-      end
+  def details
+    super.merge({
+      body: body,
+      first_public_at: edition.first_public_at,
+      change_history: edition.change_history.as_json,
+    }).tap do |json|
+      json[:image] = image_details if image_available?
     end
+  end
 
-    def image_details
-      {
-        url: Whitehall.asset_root + presented_case_study.lead_image_path,
-        alt_text: presented_case_study.lead_image_alt_text,
-        caption: presented_case_study.lead_image_caption,
-      }
-    end
+  def image_details
+    {
+      url: Whitehall.asset_root + presented_case_study.lead_image_path,
+      alt_text: presented_case_study.lead_image_alt_text,
+      caption: presented_case_study.lead_image_caption,
+    }
+  end
 
-    def body
-      Whitehall::EditionGovspeakRenderer.new(edition).body
-    end
+  def body
+    Whitehall::EditionGovspeakRenderer.new(edition).body
+  end
 
-    def image_available?
-      edition.images.any? || lead_organisation_default_image_available?
-    end
+  def image_available?
+    edition.images.any? || lead_organisation_default_image_available?
+  end
 
-    def lead_organisation_default_image_available?
-      edition.lead_organisations.first.default_news_image.present?
-    end
+  def lead_organisation_default_image_available?
+    edition.lead_organisations.first.default_news_image.present?
+  end
 
-    def presented_case_study
-      CaseStudyPresenter.new(edition)
-    end
+  def presented_case_study
+    CaseStudyPresenter.new(edition)
   end
 end
--- app/presenters/publishing_api_presenters/edition.rb
+++ app/presenters/publishing_api_presenters/edition.rb
@@ -1,62 +1,59 @@
-module PublishingApiPresenters
+# For now, this is used to register dummy editions in the content store as
+# "placeholder" content items. Only the specialist topics information is
+# exposed. This is to enable the email alerts service to generate alerts
+# when content is tagged to these topics.
+class PublishingApiPresenters::Edition < Struct.new(:edition)
+  def base_path
+    Whitehall.url_maker.public_document_path(edition)
+  end
 
-  # For now, this is used to register dummy editions in the content store as
-  # "placeholder" content items. Only the specialist topics information is
-  # exposed. This is to enable the email alerts service to generate alerts
-  # when content is tagged to these topics.
-  class Edition < Struct.new(:edition)
-    def base_path
-      Whitehall.url_maker.public_document_path(edition)
-    end
+  def as_json
+    {
+      title: edition.title,
+      base_path: base_path,
+      description: edition.summary,
+      format: "placeholder",
+      need_ids: edition.need_ids,
+      public_updated_at: edition.public_timestamp,
+      update_type: update_type,
+      publishing_app: "whitehall",
+      rendering_app: "whitehall-frontend",
+      routes: [ { path: base_path, type: "exact" } ],
+      redirects: [],
+      details: details
+    }
+  end
 
-    def as_json
-      {
-        title: edition.title,
-        base_path: base_path,
-        description: edition.summary,
-        format: "placeholder",
-        need_ids: edition.need_ids,
-        public_updated_at: edition.public_timestamp,
-        update_type: update_type,
-        publishing_app: "whitehall",
-        rendering_app: "whitehall-frontend",
-        routes: [ { path: base_path, type: "exact" } ],
-        redirects: [],
-        details: details
+private
+
+  # An incomplete and temporary details hash that only currently includes
+  # specialist topics. This is to enable the email alerts service to generate
+  # email alerts based on taggings to topics.
+  #
+  # Note that we are referencing tags using the temporary `tags` hash here in
+  # `details` instead of the top-level `links` hash because topics are not yet
+  # being registered in the content store. Once they are available in the
+  # content store, the `links` hash should be used instead.
+  #
+  # Note also that the `browse_pages` key is here as a marker for a future
+  # feature where email alerts will be generated based on taggings to browse
+  # pages. Again, this should be moved to the `links` hash at the earliest
+  # possible time.
+  def details
+    {
+      change_note: edition.most_recent_change_note,
+      tags: {
+        browse_pages: [],
+        topics: specialist_sectors,
       }
-    end
+    }
+  end
 
-  private
+  def update_type
+    edition.minor_change? ? 'minor' : 'major'
+  end
 
-    # An incomplete and temporary details hash that only currently includes
-    # specialist topics. This is to enable the email alerts service to generate
-    # email alerts based on taggings to topics.
-    #
-    # Note that we are referencing tags using the temporary `tags` hash here in
-    # `details` instead of the top-level `links` hash because topics are not yet
-    # being registered in the content store. Once they are available in the
-    # content store, the `links` hash should be used instead.
-    #
-    # Note also that the `browse_pages` key is here as a marker for a future
-    # feature where email alerts will be generated based on taggings to browse
-    # pages. Again, this should be moved to the `links` hash at the earliest
-    # possible time.
-    def details
-      {
-        change_note: edition.most_recent_change_note,
-        tags: {
-          browse_pages: [],
-          topics: specialist_sectors,
-        }
-      }
-    end
-
-    def update_type
-      edition.minor_change? ? 'minor' : 'major'
-    end
-
-    def specialist_sectors
-      [edition.primary_specialist_sector_tag].compact + edition.secondary_specialist_sector_tags
-    end
+  def specialist_sectors
+    [edition.primary_specialist_sector_tag].compact + edition.secondary_specialist_sector_tags
   end
 end
