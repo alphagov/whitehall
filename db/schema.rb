a172fd0273051581fafdbd673bec471c753a015e
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-11-25 15:16:35 +0000

    Add content_id field to documents.
    Migration to create the field. The content_ids will be populated
    separately in a data migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141120074831) do
+ActiveRecord::Schema.define(:version => 20141125151010) do
 
   create_table "about_pages", :force => true do |t|
     t.integer  "topical_event_id"
@@ -292,6 +292,7 @@ ActiveRecord::Schema.define(:version => 20141120074831) do
     t.datetime "updated_at"
     t.string   "slug"
     t.string   "document_type"
+    t.string   "content_id"
   end
 
   add_index "documents", ["document_type"], :name => "index_documents_on_document_type"
@@ -1203,11 +1204,11 @@ ActiveRecord::Schema.define(:version => 20141120074831) do
     t.text     "permissions"
     t.boolean  "remotely_signed_out", :default => false
     t.string   "organisation_slug"
-    t.boolean  "disabled",           :default => false
+    t.boolean  "disabled",            :default => false
   end
 
-  add_index "users", ["organisation_slug"], :name => "index_users_on_organisation_slug"
   add_index "users", ["disabled"], :name => "index_users_on_disabled"
+  add_index "users", ["organisation_slug"], :name => "index_users_on_organisation_slug"
 
   create_table "versions", :force => true do |t|
     t.string   "item_type",  :null => false
