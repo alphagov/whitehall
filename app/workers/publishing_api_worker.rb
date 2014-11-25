d257fb25929490eac8f6cb5976d044c2b5e30bdf
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-11-25 17:43:59 +0000

    Options passed to the API worker should pass to the presenter
    We want to be able to 'republish' documents, so we need to be able to
    pass an `update_type` option to the worker.
    
    In an ideal world the test case for this could have been neater. But the
    helpers provided by gds-api-adapters do not allow stubbing requests
    without doing an exact match on the body json, so we need to
    render the entire presenter output.

--- app/workers/publishing_api_worker.rb
+++ app/workers/publishing_api_worker.rb
@@ -1,9 +1,9 @@
 class PublishingApiWorker
   include Sidekiq::Worker
 
-  def perform(model_name, id)
+  def perform(model_name, id, options = {})
     model = class_for(model_name).find(id)
-    presenter = PublishingApiPresenters.presenter_for(model)
+    presenter = PublishingApiPresenters.presenter_for(model, options)
 
     Whitehall.publishing_api_client.put_content_item(
       presenter.base_path,
