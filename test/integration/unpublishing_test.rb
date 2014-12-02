5ae1d651493cf747ec8863dc7acd0676b7809c7a
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-12-02 16:58:26 +0000

    Send unpublishing on unpublish action.

--- test/integration/unpublishing_test.rb
+++ test/integration/unpublishing_test.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 require "gds_api/panopticon"
 require "gds_api/test_helpers/panopticon"
+require "gds_api/test_helpers/publishing_api"
 
 class UnpublishingTest < ActiveSupport::TestCase
   include GdsApi::TestHelpers::Panopticon
+  include GdsApi::TestHelpers::PublishingApi
 
   setup do
     @published_edition = create(:published_edition)
@@ -31,6 +33,13 @@ class UnpublishingTest < ActiveSupport::TestCase
 
     assert_requested @request
     assert_equal "archived", @registerable.state
+  end
+
+  test "When unpublishing an edition, it gets republished to the Publishing API with an unpublish format" do
+    path = Whitehall.url_maker.public_document_path(@published_edition)
+    stub_panopticon_registration(@published_edition)
+    unpublish(@published_edition, unpublishing_params)
+    assert_publishing_api_put_item(path, format: 'unpublishing')
   end
 
 private
