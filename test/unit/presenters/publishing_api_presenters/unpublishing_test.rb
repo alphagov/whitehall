4c1d24d5eac92786eebd6d0a2f22cd3fc9dcd6f8
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2014-12-04 15:27:16 +0000

    Add schema for unpublishing, validate in test.

--- test/unit/presenters/publishing_api_presenters/unpublishing_test.rb
+++ test/unit/presenters/publishing_api_presenters/unpublishing_test.rb
@@ -1,21 +1,31 @@
 require 'test_helper'
 
 class PublishingApiPresenters::UnpublishingTest < ActiveSupport::TestCase
+  setup do
+    @case_study = create(:draft_case_study,
+                        title: 'Case study title',
+                        first_published_at: Time.zone.now,
+                        summary: 'The summary')
+    @unpublishing = create(:unpublishing, edition: @case_study,
+                          explanation: 'it is rubbish',
+                          alternative_url: 'https://www.test.alphagov.co.uk/foobar')
+  end
+
   def present(edition, options = {})
     PublishingApiPresenters::Unpublishing.new(edition, options).as_json
   end
 
-  test 'presents an unpublished edition' do
-    case_study = create(:draft_case_study,
-                        title: 'Case study title',
-                        summary: 'The summary')
-    unpublishing = create(:unpublishing, edition: case_study,
-                          explanation: 'it is rubbish',
-                          alternative_url: 'https://www.test.alphagov.co.uk/foobar')
+  test "presenter generates valid JSON according to the schema" do
+    presented_json = present(@case_study).to_json
 
-    public_path = Whitehall.url_maker.public_document_path(case_study)
+    validator = GovukContentSchema::Validator.new('unpublishing', presented_json)
+    assert validator.valid?, "JSON not valid against unpublishing schema: #{validator.errors.to_s}"
+  end
+
+  test 'presents an unpublished edition' do
+    public_path = Whitehall.url_maker.public_document_path(@case_study)
     expected_hash = {
-      content_id: case_study.document.content_id,
+      content_id: @case_study.document.content_id,
       title: 'Case study title',
       description: 'The summary',
       base_path: public_path,
@@ -24,7 +34,7 @@ class PublishingApiPresenters::UnpublishingTest < ActiveSupport::TestCase
       need_ids: [],
       publishing_app: 'whitehall',
       rendering_app: 'whitehall-frontend',
-      public_updated_at: case_study.public_timestamp,
+      public_updated_at: @case_study.public_timestamp,
       update_type: 'major',
       routes: [
         { path: public_path, type: 'exact' }
@@ -32,11 +42,11 @@ class PublishingApiPresenters::UnpublishingTest < ActiveSupport::TestCase
       redirects: [],
       details: {
         explanation: 'it is rubbish',
-        unpublished_at: unpublishing.created_at,
+        unpublished_at: @unpublishing.created_at,
         alternative_url: 'https://www.test.alphagov.co.uk/foobar'
       }
     }
 
-    assert_equal expected_hash, present(case_study)
+    assert_equal expected_hash, present(@case_study)
   end
 end
