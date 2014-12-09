23d8b602a97561d86b2ce83c2076646d5e721006
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-09 11:52:04 +0000

    Tidy up DataHygiene::PublishingApiRepublisher
    Whitehall::PublishingApi.republish no longer returns anything significant and
    instead raises an exception if something that isn't publishable is passed in.

--- lib/data_hygiene/publishing_api_republisher.rb
+++ lib/data_hygiene/publishing_api_republisher.rb
@@ -7,18 +7,17 @@ module DataHygiene
   #   publisher = DataHygiene::PublishingApiRepublisher.new(scope)
   #   publisher.perform
   #
-  # Note: when the scope passed in is for Editions, this will skip over any
-  # that are not in a publically visible state (i.e. not published or archived).
-  # Once the Publishing API has support for handling draft content, this should
-  # be updated so that draft content can be pushed to the draft stack.
+  # Note: Whitehall::PublishingApi will raise an exception if an attempt is made
+  # to publish an instance that is not currently supported (e.g. draft editions
+  # cannot be pubilshed yet). The scope passed in should not include any
+  # instances that are not publishable.
   class PublishingApiRepublisher
-    attr_reader :logger, :scope, :republished, :skipped
+    attr_reader :logger, :scope, :republished
 
     def initialize(scope, logger=Logger.new(STDOUT))
       @scope = scope
       @logger = logger
       @republished = 0
-      @skipped = 0
     end
 
     def perform
@@ -27,19 +26,14 @@ module DataHygiene
       scope.find_each { |instance| republish(instance) }
 
       logger.info("Republished #{republished} instances")
-      logger.info("Skipped #{skipped} instances (which were not published)") if skipped > 0
     end
 
   private
 
     def republish(instance)
-      if Whitehall::PublishingApi.republish(instance)
-        logger << '.'
-        @republished += 1
-      else
-        logger << '*'
-        @skipped += 1
-      end
+      Whitehall::PublishingApi.republish(instance)
+      logger << '.'
+      @republished +=1
     end
   end
 end
