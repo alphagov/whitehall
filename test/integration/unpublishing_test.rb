e0dd459fa3f84a8d142315164fabe5e62a11f16d
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-11 12:10:28 +0000

    Add additional test coverage around translations
    Specifically to ensure that unpublishings for each translation are pushed to the
    Publishing API.

--- test/integration/unpublishing_test.rb
+++ test/integration/unpublishing_test.rb
@@ -8,7 +8,7 @@ class UnpublishingTest < ActiveSupport::TestCase
   include GdsApi::TestHelpers::PublishingApi
 
   setup do
-    @published_edition = create(:published_edition)
+    @published_edition = create(:published_case_study)
     @registerable = RegisterableEdition.new(@published_edition)
     @request = stub_artefact_registration(@registerable.slug)
   end
@@ -35,11 +35,27 @@ class UnpublishingTest < ActiveSupport::TestCase
     assert_equal "archived", @registerable.state
   end
 
-  test "When unpublishing an edition, it gets published to the Publishing API with an unpublish format" do
+  test 'When an edition is unpublished, an "unpublishing" is published to the Publishing API' do
     path = Whitehall.url_maker.public_document_path(@published_edition)
     stub_panopticon_registration(@published_edition)
     unpublish(@published_edition, unpublishing_params)
     assert_publishing_api_put_item(path, format: 'unpublishing')
+  end
+
+  test 'When a translated edition is unpublished, an "unpublishing" is publsihed to the Publishing API for each of its translations' do
+    en_path = Whitehall.url_maker.public_document_path(@published_edition)
+    fr_path = Whitehall.url_maker.public_document_path(@published_edition, locale: 'fr')
+
+    I18n.with_locale 'fr' do
+      @published_edition.title = "French title"
+      @published_edition.body = "French body"
+      @published_edition.save!(validate: false)
+    end
+
+    unpublish(@published_edition, unpublishing_params)
+
+    assert_publishing_api_put_item(en_path, format: 'unpublishing')
+    assert_publishing_api_put_item(fr_path, format: 'unpublishing')
   end
 
 private
