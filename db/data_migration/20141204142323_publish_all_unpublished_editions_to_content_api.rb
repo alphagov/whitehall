17f77c5b2f516fdb01eeee20f4a8d1884b4b648a
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-12 13:56:32 +0000

    Update data migration to pass in Unpublishings
    Now that we publish the unpublishing rather than the edition associated with the
    unpublishing, the data migration needs updating.

--- db/data_migration/20141204142323_publish_all_unpublished_editions_to_content_api.rb
+++ db/data_migration/20141204142323_publish_all_unpublished_editions_to_content_api.rb
@@ -1,2 +1,15 @@
-unpublished_ids = Edition.unscoped.joins(:unpublishing).includes(:document).where(state: ['draft', 'deleted']).reject {|e| e.document.published_edition.present? }.map(&:id)
-DataHygiene::PublishingApiRepublisher.new(Edition.unscoped.where(id: unpublished_ids)).perform
+# All non-published/superseded editions that have an unpublishing.
+non_published_editions_with_unpublishings = Edition.unscoped.
+                                              joins(:unpublishing).
+                                              includes(:document).
+                                              where(state: ['draft', 'deleted'])
+
+# reject any editions that have subsequently been re-published
+unpublished_editions = non_published_editions_with_unpublishings.reject do |edition|
+  edition.document.published_edition.present?
+end
+
+# DataHygiene::PublishingApiRepublisher expects a scope.
+unpublishing_scope = Unpublishing.where(edition_id: unpublished_editions.map(&:id))
+
+DataHygiene::PublishingApiRepublisher.new(unpublishing_scope).perform
