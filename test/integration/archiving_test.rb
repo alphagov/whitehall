6d2129c5962b670af97a17d92dfce6bf2c8e83c5
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-12 16:19:25 +0000

    Revert "Archive/unpublish trigger publish not republish."
    This reverts commit f9e05594c63b7b5c4da6455ce4d3c87e88cfb996.
    
    For now, we'll keep an archiving as a "republish" to avoid generating email
    alerts.

--- test/integration/archiving_test.rb
+++ test/integration/archiving_test.rb
@@ -4,7 +4,7 @@ require "gds_api/test_helpers/publishing_api"
 class ArchivingTest < ActiveSupport::TestCase
   include GdsApi::TestHelpers::PublishingApi
 
-  test "When an edition is archived, it gets published to the Publishing API with an archive notice" do
+  test "When an edition is archived, it gets republished to the Publishing API with an archive notice" do
     edition   = create(:published_case_study)
     presenter = PublishingApiPresenters.presenter_for(edition)
     edition.build_unpublishing(explanation: 'Old information',
@@ -13,9 +13,9 @@ class ArchivingTest < ActiveSupport::TestCase
     stub_panopticon_registration(edition)
     perform_archiving(edition)
 
-    publish_url = Plek.current.find('publishing-api') + "/content" + presenter.base_path
+    republish_url = Plek.current.find('publishing-api') + "/content" + presenter.base_path
 
-    assert_requested(:put, publish_url) { |req| archived_payload?(req.body) }
+    assert_requested(:put, republish_url) { |req| archived_payload?(req.body) }
   end
 
 private
@@ -27,7 +27,7 @@ private
 
   def archived_payload?(json)
     payload = JSON.parse(json)
-    payload['update_type'] == 'major' &&
+    payload['update_type'] == 'republish' &&
       payload['details']['archive_notice'].is_a?(Hash)
   end
 end
