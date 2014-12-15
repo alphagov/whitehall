6314e7728630aeb59c07a97ae301eaef8f830203
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-15 12:10:56 +0000

    Fix broken data migration
    Because the orphan CIP pages may belong to the same document, the document might
    not exist as it may have been destroyed already in a previous iteration.

--- db/data_migration/20141209072457_destroy_orphaned_corporate_information_pages.rb
+++ db/data_migration/20141209072457_destroy_orphaned_corporate_information_pages.rb
@@ -4,6 +4,6 @@ orphaned_cip_ids = CorporateInformationPage.pluck(:id) -
                     CorporateInformationPage.joins(:organisation).pluck(:id) -
                     CorporateInformationPage.joins(:worldwide_organisation).pluck(:id)
 
-CorporateInformationPage.where(id: orphaned_cip_ids).map { |cip| cip.document.destroy } # also deletes editions
+CorporateInformationPage.where(id: orphaned_cip_ids).map { |cip| cip.document.try(:destroy) } # also deletes editions
 
 puts "Deleted #{orphaned_cip_ids.count} orphaned corporate information page editions having ids: #{orphaned_cip_ids.to_sentence}"
