26c024d515ca65395852a7e975cdb962155960a2
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-16 16:51:49 +0000

    Tweak test to ensure full coverage
    We want to ensure that the publish intent is still sent, just that the "coming
    soon" holiding page isn't.

--- test/integration/scheduling_test.rb
+++ test/integration/scheduling_test.rb
@@ -24,16 +24,20 @@ class SchedulingTest < ActiveSupport::TestCase
     assert_publishing_api_put_item(path, format: 'coming_soon')
   end
 
-  test "When scheduling an edition that has been published before, no content format is sent to content-store" do
+  test "When scheduling an edition that has been published before, no coming_soon format is sent to content-store" do
     published_edition = create(:published_edition)
     new_draft = published_edition.create_draft(published_edition.creator)
     new_draft.change_note = 'changed'
     new_draft.scheduled_publication = 1.day.from_now
     new_draft.save!
+    new_draft.submit!
+
     path = Whitehall.url_maker.public_document_path(new_draft)
 
     acting_as(create(:user)) { schedule(new_draft) }
+
     assert_not_requested(:put, %r{#{PUBLISHING_API_ENDPOINT}/content.*})
+    assert_publishing_api_put_intent(path, publish_time: new_draft.scheduled_publication.as_json)
   end
 
 private
