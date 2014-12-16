0652a6f1c6ba9e2ca730952c970d850060a2b28e
Tekin Suleyman <tekin@tekin.co.uk>
2014-12-16 17:01:28 +0000

    Rename method for consistency and clarity

--- app/workers/publishing_api_schedule_worker.rb
+++ app/workers/publishing_api_schedule_worker.rb
@@ -11,12 +11,12 @@ class PublishingApiScheduleWorker
     # Coming soon needs to be sent first, as currently content-store deletes
     # an intent when a content item is created for that path.
     publish_coming_soon unless @model.document.published?
-    publish_intent
+    publish_publish_intent
   end
 
 private
 
-  def publish_intent
+  def publish_publish_intent
     presenter = PublishingApiPresenters.publish_intent_for(model)
     I18n.with_locale(locale) do
       Whitehall.publishing_api_client.put_intent(
