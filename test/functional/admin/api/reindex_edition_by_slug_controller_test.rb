7e03007ba2702c1135fb1f9c3063f14a67945280
Pedro Moreira <pedro.moreira@unboxedconsulting.com>
2014-12-18 16:09:32 +0000

    Add reindex edition by slug endpoint
    Story: https://www.pivotaltracker.com/story/show/84836766
    
    This adds an endpoint for triggering a reindex of editions whose
    document matches the provided slug.
    
    Since slugs are only unique across document types, we select documents
    across all types known to the Document class that have the slug to
    reindex.

---
+++ test/functional/admin/api/reindex_edition_by_slug_controller_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class Admin::Api::ReindexEditionBySlugControllerTest < ActionController::TestCase
+
+  def setup
+    login_as :user
+  end
+
+  test "#create reindexes the all published editions for the given slug" do
+    slug_to_reindex = "log-in-register-to-online-services"
+    slug_not_to_reindex = "embassy-closure-for-holiday"
+    document = create :document, slug: slug_to_reindex
+    control_document = create :document, slug: slug_not_to_reindex
+    create(:publication, :published, document: control_document)
+
+    list_of_editions_to_reindex = [
+      create(:publication, :published, document: document),
+      create(:detailed_guide, :published, document: document)
+    ]
+
+    list_of_editions_to_reindex.each do |edition_to_reindex|
+      Whitehall::SearchIndex.expects(:add).with(edition_to_reindex).once
+    end
+
+    post :create,
+    { slug: slug_to_reindex },
+    { "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json" }
+  end
+end
