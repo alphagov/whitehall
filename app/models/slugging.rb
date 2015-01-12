edf679d38d2b036651e7797ad6aca7932a033c94
Tekin Suleyman <tekin@tekin.co.uk>
2015-01-12 13:35:04 +0000

    Generate sequential slugs with friendly_id
    This re-instates the friendly_id v4 behaviour of generating sequential slugs
    when there are conflicts. Note that the `should_generate_new_friendly_id?`
    method no longer needs overriding as the default behaviour in friendly_id v5 is
    to only generate the friendld_id on new records.

--- app/models/slugging.rb
+++
@@ -1,9 +0,0 @@
-module Slugging
-  def should_generate_new_friendly_id?
-    new_record?
-  end
-
-  def normalize_friendly_id(input)
-    super input.to_s.to_slug.truncate(150).normalize.to_s
-  end
-end
