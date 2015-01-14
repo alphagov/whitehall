369091b996def15bf81830c0db580e5971a4d6de
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-01-14 10:46:17 +0000

    Expose content id for Orgs in API
    As we want to query the content ID for Organisations we need to expose
    them from the API in Whitehall. This commit adds it to the
    Api::OrganisationsPresenter.

--- app/presenters/api/organisation_presenter.rb
+++ app/presenters/api/organisation_presenter.rb
@@ -14,6 +14,7 @@ class Api::OrganisationPresenter < Api::BasePresenter
         organisation_logo_type_class_name: model.organisation_logo_type.try(:class_name),
         closed_at: model.closed_at,
         govuk_status: model.govuk_status,
+        content_id: model.content_id,
       },
       parent_organisations: parent_organisations,
       child_organisations: child_organisations,
