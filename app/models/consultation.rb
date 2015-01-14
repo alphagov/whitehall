342e90b3170ba1bb6f6e93c1fda42511e4d6ec28
Tekin Suleyman <tekin@tekin.co.uk>
2015-01-14 19:30:44 +0000

    Update conditional presence validation syntax
    The previous syntax no longer works. The new syntax requires that the valid URI
    validation check be done separately.

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -9,7 +9,8 @@ class Consultation < Publicationesque
 
   validates :opening_at, presence: true, unless: ->(consultation) { consultation.can_have_some_invalid_data? }
   validates :closing_at, presence: true, unless: ->(consultation) { consultation.can_have_some_invalid_data? }
-  validates :external_url, uri: true, allow_blank: true, presence: { if: :external? }
+  validates :external_url, presence: true, if: :external?
+  validates :external_url, uri: true, allow_blank: true
   validate :validate_closes_after_opens
 
   has_one :outcome, class_name: 'ConsultationOutcome', foreign_key: :edition_id, dependent: :destroy
