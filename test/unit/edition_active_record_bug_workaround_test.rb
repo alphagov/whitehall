3b8ff94dda605fec62527e1d91eb90cb0161ff2a
Tekin Suleyman <tekin@tekin.co.uk>
2015-01-15 09:48:07 +0000

    Remove redundant ActiveRcord bug workaround code
    The workaround for the ActiveRecord bug is no longer required. In fact, it
    appears to have the opposite effect now! Reworded the regression test to
    hopefully make it clearer what it is covering.

--- test/unit/edition_active_record_bug_workaround_test.rb
+++ test/unit/edition_active_record_bug_workaround_test.rb
@@ -3,15 +3,12 @@ require "test_helper"
 class EditionActiveRecordBugWorkaroundTest < ActiveSupport::TestCase
   include ActionDispatch::TestProcess
 
-  test ".related_to avoids wierd bug in active record" do
+  test "related_editions scope is not affected by calling Edition.related_to with other scopes" do
     policy = create(:published_policy)
     publications = [
       create(:published_publication, related_editions: [policy]),
       create(:draft_publication, related_editions: [policy])
     ]
-
-    # This has a side-effect of causing active-record to cache the
-    # policy.related_to relation including the 'published' scope
     Edition.published.related_to(policy).load
 
     assert_equal publications, policy.related_editions
