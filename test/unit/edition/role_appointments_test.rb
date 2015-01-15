d6cc6ba598fa330263aacca48a9a7501a6ecb610
Tekin Suleyman <tekin@tekin.co.uk>
2015-01-15 11:18:02 +0000

    Add additional test coverage for edition role appointments
    This adds some missing coverage around exposing the role appointments in search
    data when present.

--- test/unit/edition/role_appointments_test.rb
+++ test/unit/edition/role_appointments_test.rb
@@ -9,4 +9,11 @@ class Edition::RoleAppointmentsTest < ActiveSupport::TestCase
     published = create(:published_news_article, role_appointments: appointments)
     assert_equal appointments, published.create_draft(create(:user)).role_appointments
   end
+
+  test "editions with role appointments include them in their search info" do
+    appointment = create(:role_appointment)
+    news_article = create(:news_article, role_appointments: [appointment])
+
+    assert_equal [appointment.slug], news_article.search_index['people']
+  end
 end
