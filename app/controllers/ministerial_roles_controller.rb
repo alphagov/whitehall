5288e3e5139f42782bbc762ba638c593c5ae143d
Tekin Suleyman <tekin@tekin.co.uk>
2015-01-20 15:38:17 +0000

    Use friendly scope on MinisterialRole find

--- app/controllers/ministerial_roles_controller.rb
+++ app/controllers/ministerial_roles_controller.rb
@@ -17,7 +17,7 @@ class MinisterialRolesController < PublicFacingController
   end
 
   def show
-    @ministerial_role = RolePresenter.new(MinisterialRole.find(params[:id]), view_context)
+    @ministerial_role = RolePresenter.new(MinisterialRole.friendly.find(params[:id]), view_context)
     @policies = decorate_collection(@ministerial_role.policies.published.in_reverse_chronological_order, PolicyPresenter)
     set_slimmer_organisations_header(@ministerial_role.organisations)
     set_slimmer_page_owner_header(@ministerial_role.organisations.first)
