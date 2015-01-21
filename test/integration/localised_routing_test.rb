1994af32bfc290cd2c5f492348a26b3f9710463c
Tekin Suleyman <tekin@tekin.co.uk>
2015-01-21 10:58:48 +0000

    Improve test coverage around localised routing
    This makes the expected behaviour around localised routes a bit more explicit.
    
    We've done this to give us more confidence whilst we refactor the localised
    routing patches to deal with an issue around default url options and
    specifically  the way routing behaves when :format is set to a default on
    non-localised routes.

--- test/integration/localised_routing_test.rb
+++ test/integration/localised_routing_test.rb
@@ -1,47 +1,81 @@
 require 'test_helper'
 
 class RoutingLocaleTest < ActionDispatch::IntegrationTest
-  [
-    {
-      name:            'world locations',
-      expected_url:    '/government/world/spain',
-      routing_options: {controller: 'world_locations', action: 'show', id: 'spain', locale: 'en'}
-    },
-    {
-      name:            'world location news articles',
-      expected_url:    '/government/world-location-news/a-thing-happened-abroad',
-      routing_options: {controller: 'world_location_news_articles', action: 'show', id: 'a-thing-happened-abroad', locale: 'en'}
-    },
-    {
-      name:            'world priorities',
-      expected_url:    '/government/priority/a-world-priority',
-      routing_options: {controller: 'worldwide_priorities', action: 'show', id: 'a-world-priority', locale: 'en'}
-    },
-    {
-      name:            'publications',
-      expected_url:    '/government/publications/look-at-this-thing-we-did',
-      routing_options: {controller: 'publications', action: 'show', id: 'look-at-this-thing-we-did', locale: 'en'}
-    },
-    {
-      name:            'people',
-      expected_url:    '/government/people/lord-ahmad-of-wimbledon',
-      routing_options: {controller: 'people', action: 'show', id: 'lord-ahmad-of-wimbledon', locale: 'en'}
-    }
-  ].each do |test|
-    test "matches #{test[:name]} without locale or format" do
-      assert_routing test[:expected_url], test[:routing_options]
-    end
+  test "#index in default locale" do
+    assert_equal '/government/ministers', ministerial_roles_path
+  end
 
-    test "matches #{test[:name]} with a format" do
-      assert_routing "#{test[:expected_url]}.json", test[:routing_options].merge(format: 'json')
-    end
+  test "#index with english locale doesn't include locale in the path" do
+    assert_equal '/government/ministers', ministerial_roles_path(locale: 'en')
+  end
 
-    test "matches #{test[:name]} with a given locale" do
-      assert_routing "#{test[:expected_url]}.es", test[:routing_options].merge(locale: 'es')
-    end
+  test "#index with a non-english locale includes the locale in the path" do
+    assert_equal '/government/ministers.fr', ministerial_roles_path(locale: 'fr')
+  end
 
-    test "matches #{test[:name]} with both a locale and format" do
-      assert_routing "#{test[:expected_url]}.es.json", test[:routing_options].merge(locale: 'es', format: 'json')
-    end
+  test "#index with a format includes it in the path" do
+    assert_equal '/government/ministers.atom', ministerial_roles_path(format: 'atom')
+  end
+
+  test "#index with a english locale and a format includes the format in the path" do
+    assert_equal '/government/ministers.atom', ministerial_roles_path(locale: 'en', format: 'atom')
+  end
+
+  test "#index with a non-english locale and a format includes the locale and format in the path" do
+    assert_equal '/government/ministers.dk.atom', ministerial_roles_path(locale: 'dk', format: 'atom')
+  end
+
+  test "#show with no locale" do
+    ministerial_role = create(:ministerial_role)
+    assert_equal "/government/ministers/#{ministerial_role.slug}",
+      ministerial_role_path(ministerial_role)
+  end
+
+  test "#show with english locale doesn't includes the locale in the path" do
+    ministerial_role = create(:ministerial_role)
+    assert_equal "/government/ministers/#{ministerial_role.slug}",
+      ministerial_role_path(ministerial_role, locale: 'en')
+  end
+
+  test "#show with non-english locale includes the locale in the path" do
+    ministerial_role = create(:ministerial_role)
+    assert_equal "/government/ministers/#{ministerial_role.slug}.dr",
+      ministerial_role_path(ministerial_role, locale: 'dr')
+  end
+
+  test "#show with a format includes it in the path" do
+    ministerial_role = create(:ministerial_role)
+    assert_equal "/government/ministers/#{ministerial_role.slug}.json",
+      ministerial_role_path(ministerial_role, format: 'json')
+  end
+
+  test "#show with a english locale and a format includes the format in the path" do
+    ministerial_role = create(:ministerial_role)
+    assert_equal "/government/ministers/#{ministerial_role.slug}.json",
+      ministerial_role_path(ministerial_role, locale: 'en', format: 'json')
+  end
+
+  test "#show with a non-english locale and a format includes the locale and format in the path" do
+    ministerial_role = create(:ministerial_role)
+    assert_equal "/government/ministers/#{ministerial_role.slug}.cy.json",
+      ministerial_role_path(ministerial_role, locale: 'cy', format: 'json')
+  end
+
+  test "#index for a non-localised resource" do
+    assert_equal "/government/topics", topics_path
+  end
+
+  test "#index for a non-localised resource with a format" do
+    assert_equal "/government/topics.json", topics_path(format: 'json')
+  end
+
+  test "#show for a non-localised resource" do
+    topic = create(:topic)
+    assert_equal "/government/topics/#{topic.slug}", topic_path(topic)
+  end
+
+  test "#show for a non-localised resource with a format" do
+    topic = create(:topic)
+    assert_equal "/government/topics/#{topic.slug}.json", topic_path(topic, format: 'json')
   end
 end
