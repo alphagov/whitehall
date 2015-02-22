c334621f369d07332c020f95735a0e9e18562227
Tekin Suleyman <tekin@tekin.co.uk>
2015-02-23 07:41:38 +1100

    Remove deprecated configuration
    Implicit join references are now always disabled.

--- config/application.rb
+++ config/application.rb
@@ -39,8 +39,6 @@ module Whitehall
       :corporate_information_page_search_index_observer
     ]
 
-    config.active_record.disable_implicit_join_references = true
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'London'
