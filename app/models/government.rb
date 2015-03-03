ec448b60e22e8fd830e13ba7138aa5ebc26ca3fa
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2015-03-03 15:28:07 +0000

    Remove unused association on Government
    This association is not used anywhere in the app. Getting rid of it
    as a pre step to refactoring the derivation of government on editions.

--- app/models/government.rb
+++ app/models/government.rb
@@ -3,8 +3,6 @@ class Government < ActiveRecord::Base
   validates :slug, presence: true, uniqueness: true
   validates :start_date, presence: true
 
-  has_many :documents
-
   before_validation on: :create do |government|
     government.slug = government.name.to_s.parameterize
   end
