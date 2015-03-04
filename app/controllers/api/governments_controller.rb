f83b5150290805bfe0d50e61a7997c5c2ef092fa
Edd Sowden <e@e26.co.uk>
2015-03-04 16:29:55 +0000

    Add basic government API
    Copying the approach from the organisation API. Creates both a paginated
    index and a show method for each government. Currently the only details
    on each government are start and end dates.

---
+++ app/controllers/api/governments_controller.rb
@@ -0,0 +1,24 @@
+class Api::GovernmentsController < PublicFacingController
+  skip_before_filter :restrict_request_formats
+  respond_to :json
+
+  self.responder = Api::Responder
+
+  def index
+    respond_with Api::GovernmentPresenter.paginate(Government.order(start_date: :desc), view_context)
+  end
+
+  def show
+    @government = Government.find_by(slug: params[:id])
+    if @government
+      respond_with Api::GovernmentPresenter.new(@government, view_context)
+    else
+      respond_with_not_found
+    end
+  end
+
+private
+  def respond_with_not_found
+    respond_with Hash.new, status: :not_found
+  end
+end
---
+++ test/functional/api/governments_controller_test.rb
@@ -0,0 +1,61 @@
+require 'test_helper'
+
+class Api::GovernmentsControllerTest < ActionController::TestCase
+  disable_database_queries
+  should_be_a_public_facing_controller
+
+  view_test "index paginates governments" do
+    presenter = Api::PagePresenter.new(Kaminari.paginate_array([]).page(1).per(1), controller.view_context)
+    presenter.stubs(:as_json).returns(paged: :representation)
+
+    Government.stubs(:order).returns([])
+    Api::GovernmentPresenter.stubs(:paginate).with(Government.order(start_date: :desc), anything).returns(presenter)
+
+    get :index, format: 'json'
+
+    assert_equal 'representation', json_response['paged']
+  end
+
+  view_test "index includes _response_info in response" do
+    presenter = Api::PagePresenter.new(Kaminari.paginate_array([]).page(1).per(1), controller.view_context)
+    presenter.stubs(:as_json).returns(paged: :representation)
+
+    Government.stubs(:order).returns([])
+    Api::GovernmentPresenter.stubs(:paginate).with(Government.order(start_date: :desc), anything).returns(presenter)
+
+    get :index, format: 'json'
+
+    assert_equal 'ok', json_response['_response_info']['status']
+  end
+
+  view_test "show responds with JSON representation of found government" do
+    government = stub_record(:government, slug: 'old-gov')
+    government.stubs(:to_param).returns('old-gov')
+    Government.stubs(:find_by).with(slug: government.slug).returns(government)
+    presenter = Api::GovernmentPresenter.new(government, controller.view_context)
+    presenter.stubs(:as_json).returns(foo: :bar)
+    Api::GovernmentPresenter.stubs(:new).with(government, anything).returns(presenter)
+
+    get :show, id: government.slug, format: 'json'
+    assert_equal 'bar', json_response['foo']
+  end
+
+  view_test "show includes _response_info in response" do
+    government = stub_record(:government, slug: 'old-gov')
+    government.stubs(:to_param).returns('old-gov')
+    Government.stubs(:find_by).with(slug: government.slug).returns(government)
+    presenter = Api::GovernmentPresenter.new(government, controller.view_context)
+    presenter.stubs(:as_json).returns(foo: :bar)
+    Api::GovernmentPresenter.stubs(:new).with(government, anything).returns(presenter)
+
+    get :show, id: government.slug, format: 'json'
+    assert_equal 'ok', json_response['_response_info']['status']
+  end
+
+  view_test "show responds with 404 if government is not found" do
+    Government.stubs(:find_by).with(slug: 'unknown').returns nil
+    get :show, id: 'unknown', format: 'json'
+    assert_response :not_found
+    assert_equal 'not found', json_response['_response_info']['status']
+  end
+end
