af23e84d97a36faa28a5b349a638c2d7de0176f3
Dominic Baggott <dominic.baggott@gmail.com>
2015-03-06 12:24:13 +0000

    Add StatisticsAnnouncementUnpublisher
    2nd line is sometimes asked to unpublish unwanted statistics
    announcements. Because there is no workflow in Whitehall for these, the
    best way we have to unpublish them is to delete the records.
    
    This class also registers a GONE route via the router_api, so that we
    correctly serve a 410 for any deleted statistics announcement.

---
+++ test/unit/data_hygiene/statistics_announcement_unpublisher_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+require 'gds_api/test_helpers/router'
+
+module DataHygiene
+  class StatisticsAnnouncementUnpublisherTest < ActiveSupport::TestCase
+    include GdsApi::TestHelpers::Router
+
+    setup do
+      @fake_logger = NullLogger.instance
+      @announcement = create(:statistics_announcement)
+    end
+
+    test "reports an error when given a non-existent slug" do
+      unpublisher = StatisticsAnnouncementUnpublisher.new(
+        announcement_slug: "not-a-real-slug",
+        logger: @fake_logger,
+      )
+
+      @fake_logger.expects(:error).once.with(regexp_matches(/not-a-real-slug/))
+
+      unpublisher.call
+    end
+
+    test "destroys the announcement and registers a 'Gone' route when given a valid slug" do
+      unpublisher = StatisticsAnnouncementUnpublisher.new(
+        announcement_slug: @announcement.slug,
+        logger: @fake_logger,
+      )
+
+      register_request, commit_request = stub_gone_route_registration(@announcement.public_path, :exact)
+
+      unpublisher.call
+
+      refute StatisticsAnnouncement.exists?(@announcement)
+
+      assert_requested(register_request)
+      assert_requested(commit_request)
+    end
+  end
+end
