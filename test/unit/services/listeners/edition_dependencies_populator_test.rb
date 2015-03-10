c99bde8925a51bbb06da13cfbb9c8f7238eeb096
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-03-10 08:27:08 +0000

    Dependency populator extracts edition dependencies
    https://trello.com/c/oYy2udbc
    
    ... in addition to contact dependencies that it handles.

--- test/unit/services/listeners/edition_dependencies_populator_test.rb
+++ test/unit/services/listeners/edition_dependencies_populator_test.rb
@@ -4,15 +4,15 @@ class ServiceListeners::EditionDependenciesPopulatorTest < ActiveSupport::TestCa
 
   [:publisher, :force_publisher].each do |service_name|
     test "Whitehall.edition_services.#{service_name} populates edition's dependencies" do
-      contacts = create_list(:contact, 2)
+      contact, speech = create(:contact), create(:speech)
       news_article = create(:submitted_news_article, body: "For more information, get in touch at:
-        [Contact:#{contacts[0].id}] or [Contact:#{contacts[1].id}]", major_change_published_at: Time.zone.now)
+        [Contact:#{contact.id}] or read our [official statement](/government/admin/speeches/#{speech.id})", major_change_published_at: Time.zone.now)
 
       stub_panopticon_registration(news_article)
 
       assert Whitehall.edition_services.send(service_name, news_article).perform!
-      assert_same_elements contacts, news_article.depended_upon_contacts
+      assert_equal [contact], news_article.depended_upon_contacts
+      assert_equal [speech], news_article.depended_upon_editions
     end
   end
-
 end
