1eceed3e2254901b896b9ca35b5aa05a3dae3049
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-03-12 07:57:23 +0000

    Rename association to remove ambiguity
    https://trello.com/c/oYy2udbc
    
    dependency_records means two things: records of edition's dependency,
    or records of other edition's dependency on this edition. this name
    makes it clear what we intend to refer to.

--- lib/dependable.rb
+++ lib/dependable.rb
@@ -2,18 +2,18 @@ module Dependable
   extend ActiveSupport::Concern
 
   included do
-    has_many :dependency_records, class_name: 'EditionDependency', as: :dependable, dependent: :destroy
-    has_many :dependent_editions, through: :dependency_records, source: :edition
+    has_many :records_of_dependent_editions, class_name: 'EditionDependency', as: :dependable, dependent: :destroy
+    has_many :dependent_editions, through: :records_of_dependent_editions, source: :edition
   end
 
   def republish_dependent_editions
     dependent_editions.each { |e| Whitehall::PublishingApi.republish(e) }
   end
 
-  def destroy_dependency_records
+  def destroy_records_of_dependent_editions
     # once a draft edition gets published, dependent editions are no longer
     # affected by future changes to that edition, so no longer remain dependent.
-    dependency_records.destroy_all
+    records_of_dependent_editions.destroy_all
   end
 
 end
