5564a91a9d803d36787cd8204f0b190fa3e6cae7
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-03-18 10:01:04 +0000

    Re-organise edition_services.rb
    this groups related listeners together, for quickly understanding
    what's happening here.

--- config/initializers/edition_services.rb
+++ config/initializers/edition_services.rb
@@ -1,16 +1,24 @@
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |event, edition, options| ServiceListeners::AuthorNotifier.new(edition, options[:user]).notify! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish|unpublish|archive)$/) { |event, edition, options| ServiceListeners::EditorialRemarker.new(edition, options[:user], options[:remark]).save_remark! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |event, edition, options| Whitehall::GovUkDelivery::Notifier.new(edition).edition_published! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish|unpublish|archive)$/) { |_, edition, _| ServiceListeners::PanopticonRegistrar.new(edition).register! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |_, edition, _| ServiceListeners::AnnouncementClearer.new(edition).clear! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |event, edition, options| ServiceListeners::SearchIndexer.new(edition).index! }
-Whitehall.edition_services.subscribe("unpublish") { |event, edition, options| ServiceListeners::SearchIndexer.new(edition).remove! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |_, edition, _| Whitehall::PublishingApi.publish(edition) }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |_, edition, _| EditionDependenciesPopulator.new(edition).populate! }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |_, edition, _| edition.republish_dependent_editions }
-Whitehall.edition_services.subscribe(/^(force_publish|publish)$/) { |_, edition, _| edition.destroy_records_of_dependent_editions }
-Whitehall.edition_services.subscribe(/^(archive)$/) { |_, edition, _| Whitehall::PublishingApi.republish(edition) }
-Whitehall.edition_services.subscribe(/^(unpublish)$/) { |_, edition, _| Whitehall::PublishingApi.publish(edition.unpublishing) }
-Whitehall.edition_services.subscribe(/^(unpublish)$/) { |_, edition, _| edition.edition_dependencies.destroy_all }
-Whitehall.edition_services.subscribe(/^(force_schedule|schedule)$/) { |_, edition, _| Whitehall::PublishingApi.schedule(edition) }
-Whitehall.edition_services.subscribe("unschedule") { |_, edition, _| Whitehall::PublishingApi.unschedule(edition) }
+Whitehall.edition_services.tap do |es|
+  es.subscribe(/^(force_publish|publish)$/)                   { |event, edition, options| ServiceListeners::AuthorNotifier.new(edition, options[:user]).notify! }
+  es.subscribe(/^(force_publish|publish|unpublish|archive)$/) { |event, edition, options| ServiceListeners::EditorialRemarker.new(edition, options[:user], options[:remark]).save_remark! }
+  es.subscribe(/^(force_publish|publish)$/)                   { |event, edition, options| Whitehall::GovUkDelivery::Notifier.new(edition).edition_published! }
+  es.subscribe(/^(force_publish|publish|unpublish|archive)$/) { |_, edition, _| ServiceListeners::PanopticonRegistrar.new(edition).register! }
+  es.subscribe(/^(force_publish|publish)$/)                   { |_, edition, _| ServiceListeners::AnnouncementClearer.new(edition).clear! }
+
+  # search
+  es.subscribe(/^(force_publish|publish)$/) { |event, edition, options| ServiceListeners::SearchIndexer.new(edition).index! }
+  es.subscribe("unpublish")                 { |event, edition, options| ServiceListeners::SearchIndexer.new(edition).remove! }
+
+  # publishing API
+  es.subscribe(/^(force_publish|publish)$/)   { |_, edition, _| Whitehall::PublishingApi.publish(edition) }
+  es.subscribe("archive")                     { |_, edition, _| Whitehall::PublishingApi.republish(edition) }
+  es.subscribe("unpublish")                   { |_, edition, _| Whitehall::PublishingApi.publish(edition.unpublishing) }
+  es.subscribe(/^(force_schedule|schedule)$/) { |_, edition, _| Whitehall::PublishingApi.schedule(edition) }
+  es.subscribe("unschedule")                  { |_, edition, _| Whitehall::PublishingApi.unschedule(edition) }
+
+  # handling edition's dependency on other content
+  es.subscribe(/^(force_publish|publish)$/) { |_, edition, _| EditionDependenciesPopulator.new(edition).populate! }
+  es.subscribe(/^(force_publish|publish)$/) { |_, edition, _| edition.republish_dependent_editions }
+  es.subscribe(/^(force_publish|publish)$/) { |_, edition, _| edition.destroy_records_of_dependent_editions }
+  es.subscribe("unpublish")                 { |_, edition, _| edition.edition_dependencies.destroy_all }
+end
