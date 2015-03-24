ac99f4fb4e9164980bf371a1283c529be73a1da0
Edd Sowden <e@e26.co.uk>
2015-03-24 12:54:07 +0000

    Revert "Merge pull request #2010 from alphagov/remove-department-access-to-political"
    This reverts commit 01d76ed06724bd2c88790b9d8e3672ca0586ee51, reversing
    changes made to 7b8ffba3ba66803fc05452987e2d105ed5d6764b.

--- lib/whitehall/authority/rules/edition_rules.rb
+++ lib/whitehall/authority/rules/edition_rules.rb
@@ -149,7 +149,7 @@ module Whitehall::Authority::Rules
     def managing_editor_can?(action)
       case action
       when :mark_political
-        false
+        true
       else
         departmental_editor_can?(action)
       end
