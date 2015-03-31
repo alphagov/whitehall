521c40e1fc74aa041865e45f1a26478b58311c1c
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-03-31 18:13:43 +0100

    Add some comments and logger parameter

--- lib/data_hygiene/specialist_sector_cleanup.rb
+++ lib/data_hygiene/specialist_sector_cleanup.rb
@@ -1,6 +1,38 @@
+=begin
+Specialist sector cleanup
+
+A `SpecialistSector` is a join model which associates an edition with a
+specialist sector tag. The specialist sectors are not stored in whitehall,
+they are produced by collections publisher and stored in the content-store
+(they used to be in panopticon).
+
+If a SpecialistSector is removed, these associations need to be deleted,
+otherwise whitehall will display links to no-existent pages from the edition
+show pages.
+
+After removing these associations we also need to inform dependent systems
+about the change, in particular:
+
+* rummager (which records the association in search document)
+* content-store (which records the association from the content item to the
+  SpecialistSector in the details hash, so it's not able to benefit from the
+  dynamic resolution of associations provided by content store)
+* content-api/panopticon (stores )
+
+This is done by using the `EditionReregisterer`
+
+Does the following:
+
+* removes the Deletes any SpecialistSector records.
+
+=end
+
 class SpecialistSectorCleanup
-  def initialize(slug)
-    @slug = slug
+  attr_reader :logger
+
+  def initialize(specialist_sector_slug, logger: Logger.new(nil))
+    @specialist_sector_slug = specialist_sector_slug
+    @logger = logger
   end
 
   def any_taggings?
@@ -12,15 +44,15 @@ class SpecialistSectorCleanup
       edition = tagging.edition
 
       if edition
-        puts "Removing tagging to edition ##{edition.id}"
+        logger.info "Removing tagging to edition ##{edition.id}"
       else
-        puts "Removing tagging where edition was nil"
+        logger.info "Removing tagging where edition was nil"
       end
 
       tagging.destroy
 
       if edition.nil?
-        puts "no edition (probably deleted)"
+        logger.info "no edition (probably deleted)"
       else
         add_remark(edition)
 
@@ -35,19 +67,19 @@ private
 
   def add_remark(edition)
     if Edition::FROZEN_STATES.include?(edition.state)
-      puts "edition is frozen; not adding editorial remark"
+      logger.info "edition is frozen; not adding editorial remark"
     else
-      puts "adding an editorial remark"
+      logger.info "adding an editorial remark"
 
       edition.editorial_remarks.create!(
         author: gds_user,
-        body: "Automatically untagged from old sector '#{@slug}'"
+        body: "Automatically untagged from old sector '#{@specialist_sector_slug}'"
       )
     end
   end
 
   def register_edition(edition)
-    puts "registering '#{edition.slug}' (id #{edition.id})"
+    logger.info "registering '#{edition.slug}' (id #{edition.id})"
     edition.reload
     Whitehall.edition_services.publisher(edition).perform!
   end
@@ -57,6 +89,6 @@ private
   end
 
   def taggings
-    SpecialistSector.where(tag: @slug)
+    SpecialistSector.where(tag: @specialist_sector_slug)
   end
 end
