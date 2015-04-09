3997f40f35dfc6334a910ae28be6bfa5352dc272
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-04-09 14:23:05 +0100

    Add logging

--- lib/whitehall/publishing_api/draft_environment_populator.rb
+++ lib/whitehall/publishing_api/draft_environment_populator.rb
@@ -1,10 +1,11 @@
 module Whitehall
   class PublishingApi
     class DraftEnvironmentPopulator
-      attr_reader :draft_items
+      attr_reader :draft_items, :logger
 
-      def initialize(draft_items: default_draft_items)
-        @draft_items = draft_items
+      def initialize(draft_items: nil, logger: Logger.new(nil))
+        @logger = logger
+        @draft_items = draft_items || default_draft_items
       end
 
       def call
@@ -13,10 +14,32 @@ module Whitehall
         end
       end
 
-      def self.default_draft_items
+    private
+      class ProgressLogger
+        attr_reader :name, :count, :logger
+
+        def initialize(count, logger)
+          @count = count
+          @logger = logger
+          @i = 0
+        end
+
+        def inc
+          @i += 1
+          if @i % 1000 == 0
+            logger.info "done #{i}..."
+          end
+        end
+      end
+
+      def default_draft_items
         Enumerator.new do |yielder|
+          progress_logger = ProgressLogger.new(Edition.latest_edition.count, logger)
+          logger.info "Exporting #{progress_logger.count} Editions"
+
           Edition.latest_edition.find_each do |edition|
             yielder << edition
+            progress_logger.inc
           end
 
           [
@@ -26,8 +49,12 @@ module Whitehall
             WorldLocation,
             WorldwideOrganisation
           ].each do |klass|
+            progress_logger = ProgressLogger.new(klass.count, logger)
+            logger.info "Exporting #{progress_logger.count} #{klass.name}s"
+
             klass.find_each do |item|
               yielder << item
+              progress_logger.inc
             end
           end
         end
