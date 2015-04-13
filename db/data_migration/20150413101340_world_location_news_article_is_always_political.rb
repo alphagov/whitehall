d912dcf31dcf6fb2c70a6e501c846380da46a936
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-13 11:16:33 +0100

    Make WorldLocationNewsArticle always political
    WorldLocationNewsArticles are political but are never published by a
    Political Org. This commit updates the PoliticalContentIdentifier to do
    this and adds a data migration to update all existing
    WorldLocationNewsArticles.

---
+++ db/data_migration/20150413101340_world_location_news_article_is_always_political.rb
@@ -0,0 +1,12 @@
+index = 0
+
+PUBLISHED_AND_PUBLISHABLE_STATES = %w(published draft archived submitted rejected scheduled)
+edition_scope = Edition.where(state: PUBLISHED_AND_PUBLISHABLE_STATES, type: WorldLocationNewsArticle)
+edition_count = edition_scope.count
+
+edition_scope.find_each do |edition|
+  edition.update_column(:political, true)
+  index += 1
+
+  puts "Processed #{index} of #{edition_count} editions (#{(index.to_f/edition_count.to_f)*100}%)" if index % 1000 == 0
+end
