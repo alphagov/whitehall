2a87dc140a26a32fe5406c4359e4ad200dfa31e0
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-04-16 11:47:39 +0100

    Dump a CSV of policy papers generated from policies.
    Dumps to app-local tmp folder.

--- db/data_migration/20150407095340_convert_policies_to_html_publications.rb
+++ db/data_migration/20150407095340_convert_policies_to_html_publications.rb
@@ -67,78 +67,83 @@ end
 gds_user = User.find_by!(name: "GDS Inside Government Team")
 url_maker = Whitehall.url_maker
 
-Policy.publicly_visible.with_translations.each do |policy|
-  puts %{Creating policy paper from policy ##{policy.id}}
+CSV.open(Rails.root+"tmp/#{Time.zone.now.strftime('%F-%H-%M-%S')}-policy_paper_creation_output.csv", "wb") do |csv|
+  csv << ["policy_id", "policy_paper_id", "policy_state"]
 
-  title = "2010 to 2015 Conservative and Liberal Democrat coalition policy: #{policy.title}"
-  short_title = "2010 to 2015 coalition policy: #{policy.title}"
+  Policy.publicly_visible.with_translations.each do |policy|
+    puts %{Creating policy paper from policy ##{policy.id}}
 
-  supporting_pages = policy.supporting_pages.published.with_translations
+    title = "2010 to 2015 Conservative and Liberal Democrat coalition policy: #{policy.title}"
+    short_title = "2010 to 2015 coalition policy: #{policy.title}"
 
-  alternative_format_provider = policy.lead_organisations.first
+    supporting_pages = policy.supporting_pages.published.with_translations
 
-  html_attachment = HtmlAttachment.new(
-    title: title,
-    ordering: 0,
-    govspeak_content: GovspeakContent.new(
-      body: PolicyMigrationHelpers.build_html_attachment_body(policy, url_maker, alternative_format_provider),
-      manually_numbered_headings: false,
-    ),
-  )
+    alternative_format_provider = policy.lead_organisations.first
 
-  policy_paper_body = "This policy paper shows the policy of the 2010 to 2015 Conservative and Liberal Democrat coalition government.
+    html_attachment = HtmlAttachment.new(
+      title: title,
+      ordering: 0,
+      govspeak_content: GovspeakContent.new(
+        body: PolicyMigrationHelpers.build_html_attachment_body(policy, url_maker, alternative_format_provider),
+        manually_numbered_headings: false,
+      ),
+    )
 
-Find out about the [current government’s policies](#{url_maker.policies_url})."
+    policy_paper_body = "This policy paper shows the policy of the 2010 to 2015 Conservative and Liberal Democrat coalition government.
 
-  policy_paper = Publication.new(
-    title: title,
-    summary: policy.summary,
-    body: policy_paper_body,
-    publication_type_id: PublicationType::PolicyPaper.id,
-    first_published_at: DateTime.new(2015, 3, 27, 6, 0, 0), # 6am, 27 March 2015
-    political: true,
-    creator: gds_user,
-    alternative_format_provider: alternative_format_provider,
-    document: Document.new(
-      sluggable_string: short_title,
-      content_id: SecureRandom.uuid
-    ),
-  )
+  Find out about the [current government’s policies](#{url_maker.policies_url})."
 
-  PolicyMigrationHelpers.copy_associations!(policy, policy_paper)
+    policy_paper = Publication.new(
+      title: title,
+      summary: policy.summary,
+      body: policy_paper_body,
+      publication_type_id: PublicationType::PolicyPaper.id,
+      first_published_at: DateTime.new(2015, 3, 27, 6, 0, 0), # 6am, 27 March 2015
+      political: true,
+      creator: gds_user,
+      alternative_format_provider: alternative_format_provider,
+      document: Document.new(
+        sluggable_string: short_title,
+        content_id: SecureRandom.uuid
+      ),
+    )
 
-  if policy_paper.save
+    PolicyMigrationHelpers.copy_associations!(policy, policy_paper)
 
-    policy_paper.attachments << html_attachment
-    supporting_pages.map(&:attachments).flatten.uniq.each_with_index do |attachment, index|
-      puts %{-- Adding attachment "#{attachment.html? ? 'HTML' : attachment.filename}"}
+    if policy_paper.save
 
-      existing_attachment = attachment.deep_clone
+      policy_paper.attachments << html_attachment
+      supporting_pages.map(&:attachments).flatten.uniq.each_with_index do |attachment, index|
+        puts %{-- Adding attachment "#{attachment.html? ? 'HTML' : attachment.filename}"}
 
-      # Reset attachment ordering, accounting for the HTML attachment
-      # These have never been manually set for supporting pages,
-      # so we don't need to worry about preserving order.
-      existing_attachment.ordering = index + 1
+        existing_attachment = attachment.deep_clone
 
-      policy_paper.attachments << existing_attachment
-    end
+        # Reset attachment ordering, accounting for the HTML attachment
+        # These have never been manually set for supporting pages,
+        # so we don't need to worry about preserving order.
+        existing_attachment.ordering = index + 1
 
-    puts %{Created policy paper ##{policy_paper.id} "#{policy_paper.title}" from policy ##{policy.id}}
-  else
-    policy_paper.errors.full_messages.each do |error|
-      puts %{-- #{error}}
+        policy_paper.attachments << existing_attachment
+      end
 
-      puts %{---- #{policy_paper.body}} if error =~ /invalid formatting/
+      csv << [policy.id, policy_paper.id, policy.state]
+      puts %{Created policy paper ##{policy_paper.id} "#{policy_paper.title}" from policy ##{policy.id}}
+    else
+      policy_paper.errors.full_messages.each do |error|
+        puts %{-- #{error}}
 
-      if error =~ /Attachments/
-        policy_paper.attachments.each do |attachment|
-          puts %{---- #{attachment.errors.full_messages.join(',')}}
+        puts %{---- #{policy_paper.body}} if error =~ /invalid formatting/
+
+        if error =~ /Attachments/
+          policy_paper.attachments.each do |attachment|
+            puts %{---- #{attachment.errors.full_messages.join(',')}}
+          end
         end
       end
+
+      puts %{Failed to create policy paper from policy ##{policy.id} "#{policy_paper.title}"}
     end
 
-    puts %{Failed to create policy paper from policy ##{policy.id} "#{policy_paper.title}"}
+    puts
   end
-
-  puts
 end
