5800f00c5dd9a76f639f54ec9f2445697af4b283
Paul Hayes <fofr@users.noreply.github.com>
2015-04-21 14:12:15 +0100

    Remove transition visualisation feature flag
    Remove flag, but leave accessor in place until alphagov-deployment has
    been updated.

--- config/initializers/organisations_transition_visualisation_feature_flag.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.test? || Rails.env.cucumber?
-  Whitehall::organisations_transition_visualisation_feature_enabled = true
-else
-  Whitehall::organisations_transition_visualisation_feature_enabled = false
-end
