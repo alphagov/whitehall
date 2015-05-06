9a04ca65e3c8720d15e5cb290c9a112c52801d7d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-05-06 15:18:51 +0100

    Publish via the EditionForcePublisher.
    This makes more of the normal processes work whilst
    still not doing things like sending emails.

--- lib/tasks/election/policy_paper_publisher.rb
+++ lib/tasks/election/policy_paper_publisher.rb
@@ -10,16 +10,25 @@ module Election
           if policy_paper.published?
             Rails.logger.warn("WARNING: Policy paper ##{policy_paper_id} already published")
           else
-            Rails.logger.info("Force publishing policy paper ##{policy_paper_id} as a minor change")
-            policy_paper.minor_change = true
-            policy_paper.major_change_published_at = policy_paper.first_published_at
-            policy_paper.force_publish!
+            Rails.logger.info("Force publishing policy paper ##{policy_paper_id} as a major change without email alerts")
+            policy_paper.minor_change = false
+
+            Edition::AuditTrail.acting_as(gds_user) do
+              EditionForcePublisher.new(policy_paper).perform!
+            end
+
             policy_paper.update_column(:public_timestamp, 1.day.ago)
           end
         else
           Rails.logger.warn("WARNING: Couldn't find policy paper ##{policy_paper_id}")
         end
       end
+    end
+
+  private
+
+    def gds_user
+      @gds_user ||= User.find_by(email: "govuk-whitehall@digital.cabinet-office.gov.uk")
     end
   end
 end
--- test/unit/tasks/election/policy_paper_publisher_test.rb
+++ test/unit/tasks/election/policy_paper_publisher_test.rb
@@ -6,7 +6,11 @@ class PolicyPaperPublisherTest < ActiveSupport::TestCase
     @policy_paper = create(:draft_policy_paper,
       first_published_at: 1.year.ago
     )
+
+    @gds_user = FactoryGirl.create(:user, email: "govuk-whitehall@digital.cabinet-office.gov.uk")
+
     Election::PolicyPaperPublisher.new([@policy_paper.id]).run!
+
     @policy_paper.reload
   end
 
@@ -14,16 +18,12 @@ class PolicyPaperPublisherTest < ActiveSupport::TestCase
     assert @policy_paper.published?
   end
 
-  test "it publishes as a minor change" do
-    assert @policy_paper.minor_change?
+  test "it doesn't send emails" do
+    Whitehall::GovUkDelivery::Worker.expects(:notify!).never
   end
 
   test "it retains the first_published_at from the draft" do
     assert_equal 1.year.ago, @policy_paper.first_published_at
-  end
-
-  test "it sets the major change timestamp to match the first published timestamp" do
-    assert_equal @policy_paper.first_published_at, @policy_paper.major_change_published_at
   end
 
   test "doesn't explode if given a published publication" do
@@ -33,5 +33,9 @@ class PolicyPaperPublisherTest < ActiveSupport::TestCase
 
   test "it sets the public_timestamp to 24 hours ago" do
     assert_equal Time.zone.now - 1.day, @policy_paper.public_timestamp
+  end
+
+  test "it publishes as the GDS user" do
+    assert_equal @gds_user, @policy_paper.versions.where(state: "published").last.user
   end
 end
