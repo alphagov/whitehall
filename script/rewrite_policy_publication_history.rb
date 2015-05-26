510669a14e0258ff180f1da9a2fab59f466ddc0e
Tekin Suleyman <tekin@tekin.co.uk>
2015-05-26 10:21:12 +0100

    Fail early in history rewrite
    If there is an open edition on any of the publications, then we want to fail
    early as we won't be able to rewrite the history on these documents.

--- script/rewrite_policy_publication_history.rb
+++ script/rewrite_policy_publication_history.rb
@@ -7,6 +7,19 @@ require 'csv'
 mapping_csv_path = Rails.root+"lib/tasks/election/2015-04-20-13-42-38-policy_paper_creation_output.csv"
 logger = Logger.new(STDOUT)
 
+# Make sure no publications have an open edition as we can't handle those
+CSV.parse(File.open(mapping_csv_path).read, headers: true).each do |row|
+  publication = Publication.find_by_id(row["policy_paper_id"])
+  next unless publication
+
+  document = publication.document
+  if document.published_edition != document.latest_edition
+    @cannot_proceed = true
+    logger.warn "Warning: Publication #{document.published_edition.id} has a #{document.latest_edition.state} edition"
+  end
+end
+raise 'Cannot proceed with open editions on publications' if @cannot_proceed
+
 ActiveRecord::Base.transaction do
   CSV.parse(File.open(mapping_csv_path).read, headers: true).each do |row|
     publication = Publication.find(row["policy_paper_id"])
