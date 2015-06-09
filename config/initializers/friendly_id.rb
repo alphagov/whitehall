1200eec7bed761109d7c852c76ac67d1ce1dce5f
Tekin Suleyman <tekin@tekin.co.uk>
2015-06-09 14:36:13 +0100

    Remove friendly_id monkey patches
    The friendly_id sequential slugging add-on is now part of the gem itself with
    the latest beta release. We still need to do custom string normalisation to
    maintain the existing behaviour of the app.

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -1,6 +1,12 @@
 FriendlyId.defaults do |config|
-  config.base = :name
-  config.use :slugged, :finders, FriendlyId::SequentialSlugs
+  module CustomNormalisation
+    def normalize_friendly_id(input)
+      # Use the babosa gem for better string parameterisation
+      super input.to_s.to_slug.truncate(150).normalize.to_s
+    end
+  end
 
+  config.base = :name
+  config.use :sequentially_slugged, :finders, CustomNormalisation
   config.sequence_separator = '--'
 end
