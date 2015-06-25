b5cf04d1fdc4c70221e3484f726d252da1e8083d
Paul Bowsher <paul.bowsher@gmail.com>
2015-06-25 09:34:34 +0100

    Use the correct slug for the British Office Taipei
    Story:
    https://www.pivotaltracker.com/n/projects/1261204/stories/96081066
    
    This adds a new Data Hygiene class for renaming slugs of both
    organisations and worldwide organisations, based on the Ops Manual
    guide and the work done in
    https://github.com/alphagov/whitehall/pull/1596.
    
    After investigation, I’ve determined that the work required to reslug
    Worldwide Organisations only touches Whitehall. From the Ops Manual
    page for reslugging normal Organisations, steps 4 thru 9 and step 12 do
    not apply, we only need to rename things in Whitehall, publish to
    Publishing API / Search and add redirects.

---
+++ test/unit/data_hygiene/organisation_reslugger_test.rb
@@ -0,0 +1,107 @@
+require 'test_helper'
+require "gds_api/test_helpers/publishing_api"
+
+module OrganisationResluggerTest
+  module SharedTests
+    include GdsApi::TestHelpers::PublishingApi
+    extend ActiveSupport::Testing::Declarative
+
+    def self.included(klass)
+      klass.use_transactional_fixtures = false
+    end
+
+    def setup
+      DatabaseCleaner.clean_with :truncation
+      @organisation = create_organisation
+      @reslugger = DataHygiene::OrganisationReslugger.new(@organisation, 'corrected-slug')
+    end
+
+    def teardown
+      WebMock.reset!
+      DatabaseCleaner.clean_with :truncation
+    end
+
+    test "re-slugs the organisation" do
+      @reslugger.run!
+      assert_equal 'corrected-slug', @organisation.slug
+    end
+
+    test "publishes to Publishing API with the new slug and redirects the old" do
+      content_item = PublishingApiPresenters.presenter_for(@organisation).as_json
+      old_base_path = @organisation.search_link
+      new_base_path = "#{base_path}/corrected-slug"
+      content_item[:routes][0][:path] = new_base_path
+
+      redirects = [
+        { path: old_base_path, type: "exact", destination: new_base_path },
+      ]
+
+      if @organisation.is_a? Organisation
+        redirects << { path: (old_base_path + ".atom"),
+                       type: "exact",
+                       destination: (new_base_path + ".atom") }
+      end
+
+      redirect_item = Whitehall::PublishingApi::Redirect.new(old_base_path, redirects).as_json
+
+      expected_publish_request = stub_publishing_api_put_item(new_base_path, content_item)
+      expected_redirect = stub_publishing_api_put_item(old_base_path, redirect_item)
+
+      @reslugger.run!
+
+      assert_requested expected_redirect
+      assert_requested expected_publish_request
+    end
+
+    test "deletes the old slug from the search index" do
+      Whitehall::SearchIndex.expects(:delete).with { |org| org.slug == 'old-slug' }
+      @reslugger.run!
+    end
+
+    test "adds the new slug from the search index" do
+      Whitehall::SearchIndex.expects(:add).with { |org| org.slug == 'corrected-slug' }
+      @reslugger.run!
+    end
+  end
+
+  class OrganisationTest < ActiveSupport::TestCase
+    include SharedTests
+
+    def create_organisation
+      create(:organisation, name: "Old slug")
+    end
+
+    def base_path
+      "/government/organisations"
+    end
+
+    test "updates users belonging to the organisation" do
+      user = create(:user, organisation_slug: @organisation.slug)
+
+      @reslugger.run!
+
+      user.reload
+      assert_equal user.organisation_slug, "corrected-slug"
+    end
+
+    test "re-registers editions belonging to the organisation" do
+      edition = create(:published_corporate_information_page, :published, organisation: @organisation)
+
+      Whitehall::SearchIndex.stubs(:add)
+      Whitehall::SearchIndex.expects(:add).with edition
+      @reslugger.run!
+    end
+  end
+
+  class WorldwideOrganisationTest < ActiveSupport::TestCase
+    include SharedTests
+
+    def create_organisation
+      create(:worldwide_organisation, name: "Old slug")
+    end
+
+    def base_path
+      "/government/world/organisations"
+    end
+  end
+end
