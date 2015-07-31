e13717de8dfb3a0312b9f10263fc61bc673b9ec9
Paul Bowsher <paul.bowsher@gmail.com>
2015-07-31 15:12:45 +0100

    Add routes for deleted guides if ever published
    Uses the `first_published_at` field to determine if document ever had a
    published edition.

--- db/data_migration/20150730134022_republish_routes_for_all_detailed_guides.rb
+++ db/data_migration/20150730134022_republish_routes_for_all_detailed_guides.rb
@@ -16,26 +16,26 @@ end
 router_api = GdsApi::Router.new(Plek.find('router-api'))
 url_arbiter = GdsApi::UrlArbiter.new(Plek.find('url-arbiter'))
 
-scope = Document.distinct.
-  joins(:editions).
-  where(document_type: "DetailedGuide",
-    editions: {state: Edition::POST_PUBLICATION_STATES})
+scope = Edition.unscoped.
+  where(type: "DetailedGuide").
+  where("first_published_at IS NOT NULL").
+  includes(:document).
+  joins(:document).
+  group(:document_id)
 
-count = scope.count
+count = scope.count.keys.size
 
-scope.each_with_index do |guide, i|
-  if guide.ever_published_editions.any?
-    slug = guide.slug.sub(%r{^deleted-}, '')
-    publishing_app = url_arbiter.publishing_app_for_path("/#{slug}")
-    if publishing_app && publishing_app != "whitehall"
-      puts "WARNING: Couldn't add route for /#{slug}, owned by #{publishing_app}"
-      next
-    elsif publishing_app.nil?
-      url_arbiter.set_publishing_app_for_path("/#{slug}", "whitehall")
-    end
-    puts "Adding route #{i+1}/#{count} for /#{slug}"
-    router_api.add_route("/#{slug}", "exact", "whitehall-frontend")
+scope.each_with_index do |edition, i|
+  slug = edition.document.slug.sub(%r{^deleted-}, '')
+  publishing_app = url_arbiter.publishing_app_for_path("/#{slug}")
+  if publishing_app && publishing_app != "whitehall"
+    puts "WARNING: Couldn't add route for /#{slug}, owned by #{publishing_app}"
+    next
+  elsif publishing_app.nil?
+    url_arbiter.set_publishing_app_for_path("/#{slug}", "whitehall")
   end
+  puts "Adding route #{i+1}/#{count} for /#{slug}"
+  router_api.add_route("/#{slug}", "exact", "whitehall-frontend")
 end
 
 router_api.commit_routes
