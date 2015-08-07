8e10631d33e18763477ed3974e17da3f941c2761
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-08-07 10:10:07 +0000

    Stop registering detailed guide root paths
    This is part of our plan to move detailed guides under guidance/

--- app/models/registerable_edition.rb
+++ app/models/registerable_edition.rb
@@ -89,8 +89,6 @@ private
   end
 
   def detailed_guide_paths
-    old_slug = slug.sub(%r{^guidance/}, '')
-    ["/#{old_slug}"] + edition.non_english_translations.map { |t| "/#{edition.slug}.#{t.locale}" } +
-      ["/#{slug}"] + edition.non_english_translations.map { |t| "/guidance/#{edition.slug}.#{t.locale}" }
+    ["/#{slug}"] + edition.non_english_translations.map { |t| "/#{slug}.#{t.locale}" }
   end
 end
