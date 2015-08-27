d9af9d7ea07ac9521d1dc93e929ecc1255fd2849
Jack Scotti <jack.scotti@digital.cabinet-office.gov.uk>
2015-08-27 09:34:10 +0100

    Add meta description tag to Detailed Guides
    Part of improving search result snippets shown on external search
    engines. The lack of a meta-description tag makes some search result
    snippets unhelpful, confusing, or misleading. Ticket:
    https://trello.com/c/S6VUwRWQ/273-follow-up-add-meta-description-tag-to-custom-content-types

---
+++ test/integration/detailed_guide_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class DetailedGuideIntegrationTest < ActionDispatch::IntegrationTest
+  test "meta data tag is present" do
+    detailed_guide = create(:published_detailed_guide, summary: "This is a published detailed guide summary")
+
+    get "/#{detailed_guide.slug}"
+
+    assert response.body.include? "<meta name=\"description\" content=\"This is a published detailed guide summary\" />"
+  end
+end
