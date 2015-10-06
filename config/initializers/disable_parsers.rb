bc27f91c3fe4c6b0a7f86409dcc61dfb11ec2d59
Jake Benilov <benilov@gmail.com>
2015-10-06 12:15:41 +0100

    Whitehall: Don't remove default parsers
    This app is on Rails 4, which doesn't configure the XML parser by default.
    
    The JSON parser is deemed safe, and having it configured is the Rails 4 default.
    There's no obvious reason to deviate from the default for this app.

--- config/initializers/disable_parsers.rb
+++
@@ -1,4 +0,0 @@
-# Turn off XML and JSON parsing:
-# https://groups.google.com/forum/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
-ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
-ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::JSON)
