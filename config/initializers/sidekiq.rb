3f16359c2be1dc026ec25b7f8eecebd64f52598e
Stuart Gale <bishboria@gmail.com>
2015-11-05 15:20:12 +0000

    Using sidekiq-statsd gem to plot sidekiq activity

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,14 @@ redis_config = YAML.load_file(Rails.root.join("config", "redis.yml")).symbolize_
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
+
   config.error_handlers << lambda do |exception, context|
      Airbrake.notify(exception, parameters: context)
-   end
+  end
+
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Statsd::ServerMiddleware, env: 'govuk.app.whitehall', prefix: 'workers'
+  end
 end
 
 Sidekiq.configure_client do |config|
