f8e8c8d06fe7b2429b4dacab68ae47ec1fd0cd18
Jake Benilov <benilov@gmail.com>
2015-11-18 15:26:56 +0000

    Remove securerandom require from PublishingApiGoneWorker
    It's not used in the class.

--- app/workers/publishing_api_gone_worker.rb
+++ app/workers/publishing_api_gone_worker.rb
@@ -1,5 +1,3 @@
-require "securerandom"
-
 class PublishingApiGoneWorker < PublishingApiWorker
   def perform(base_path, edition_content_id)
     gone_item = PublishingApiPresenters::Gone.new(base_path, edition_content_id)
