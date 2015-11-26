ed64df40f2dff2bb90c5ea766a45b861fd216671
Paul Bowsher <paul.bowsher@gmail.com>
2015-11-26 16:04:33 +0000

    Fix dependent deletion of Corporate Information Pages
    When an object with Corporate Information Pages is deleted, we first
    attempt to delete the CIPs. This was broken as it deleted each document
    once for each edition of that document. The second iteration of this
    loop would call `destroy` on `nil` as the document no longer exists.
    
    Uniquifying the list of documents first before destroying them fixes
    this issue.

--- app/models/has_corporate_information_pages.rb
+++ app/models/has_corporate_information_pages.rb
@@ -4,7 +4,7 @@ module HasCorporateInformationPages
   included do
     has_many :corporate_information_pages, through: "edition_#{table_name}".to_sym, source: :edition, class_name: "CorporateInformationPage"
     before_destroy do |record|
-      record.corporate_information_pages.map { |cip| cip.document.destroy }
+      record.corporate_information_pages.map(&:document).uniq.each(&:destroy)
     end
   end
 
