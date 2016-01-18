9905416611d7255eaccd470f5ccb9a3ff4267580
Paul Bowsher <paul.bowsher@gmail.com>
2016-01-18 17:25:10 +0000

    Update Pub API test helpers

--- test/support/publishing_api_test_helpers.rb
+++ test/support/publishing_api_test_helpers.rb
@@ -7,7 +7,9 @@ module PublishingApiTestHelpers
   def stub_publishing_api_registration_for(editions)
     Array(editions).each do |edition|
       presenter = PublishingApiPresenters::Edition.new(edition)
-      stub_publishing_api_put_content_links_and_publish(presenter.as_json)
+      stub_publishing_api_put_content(presenter.content_id, presenter.content)
+      stub_publishing_api_put_links(presenter.content_id, links: presenter.links)
+      stub_publishing_api_publish(presenter.content_id, locale: presenter.content[:locale], update_type: 'major')
     end
   end
 
@@ -15,8 +17,10 @@ module PublishingApiTestHelpers
     editions.each do |edition|
       Whitehall.publishing_api_v2_client.expects(:put_content)
         .with(edition.content_id,
-          has_entries(content_id: edition.content_id, update_type: 'major',
-            publishing_app: 'whitehall', rendering_app: 'whitehall-frontend'))
+          has_entries(publishing_app: 'whitehall', rendering_app: 'whitehall-frontend'))
+      Whitehall.publishing_api_v2_client.stubs(:put_links)
+        .with(edition.content_id,
+          has_entries(links: {}))
       Whitehall.publishing_api_v2_client.expects(:publish)
         .with(edition.content_id, 'major', locale: "en")
     end
@@ -26,8 +30,10 @@ module PublishingApiTestHelpers
     editions.each do |edition|
       Whitehall.publishing_api_v2_client.expects(:put_content)
         .with(edition.content_id,
-          has_entries(content_id: edition.content_id, update_type: 'republish',
-            publishing_app: 'whitehall', rendering_app: 'whitehall-frontend'))
+          has_entries(publishing_app: 'whitehall', rendering_app: 'whitehall-frontend'))
+      Whitehall.publishing_api_v2_client.stubs(:put_links)
+        .with(edition.content_id,
+          has_entries(links: {}))
       Whitehall.publishing_api_v2_client.expects(:publish)
         .with(edition.content_id, 'republish', locale: "en")
     end
