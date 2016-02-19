2671b6418bcf04c13c170d09d7f3bcbe6f833eba
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2016-02-19 14:30:37 +0000

    Send a proper content item instead of a placeholder
    We won't switch the rendering_app over until much later, when the frontend work
    has been done.

--- app/presenters/publishing_api_presenters/working_group.rb
+++ app/presenters/publishing_api_presenters/working_group.rb
@@ -1,7 +1,31 @@
-class PublishingApiPresenters::WorkingGroup < PublishingApiPresenters::Placeholder
-  private
+class PublishingApiPresenters::WorkingGroup < PublishingApiPresenters::Item
+private
+
+  def title
+    item.name
+  end
 
   def document_format
-    "placeholder_working_group"
+    "working_group"
+  end
+
+  def description
+    item.summary # This is deliberately the 'wrong' way around
+  end
+
+  def details
+    {
+      email: item.email,
+      body: body,
+    }
+  end
+
+  def body
+    # It looks 'wrong' using the description as the body, but it isn't
+    Whitehall::GovspeakRenderer.new.govspeak_to_html(item.description)
+  end
+
+  def public_updated_at
+    item.updated_at
   end
 end
