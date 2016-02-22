ba6cd5b75f3a4cf7e38e0e8e7920c9ade7e3f2da
Tijmen Brommet <tijmen@gmail.com>
2016-02-22 10:22:10 +0000

    Put Rummager clients in one file
    This combines the initialisation of the Rummager clients into one file,
    so it's easier to figure out what is happening. Also removes some
    indirection with the `Whitehall::SearchIndex.rummager_host`.

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -1,7 +1,21 @@
 require 'gds_api/rummager'
 
 Whitehall.government_search_client = GdsApi::Rummager.new(
-    Whitehall::SearchIndex.rummager_host + Whitehall.government_search_index_path)
+  Plek.find('search') + Whitehall.government_search_index_path
+)
 
 Whitehall.unified_search_client = GdsApi::Rummager.new(
-    Whitehall::SearchIndex.rummager_host)
+  Plek.find('search')
+)
+
+def statistics_announcement_search_client
+  if Rails.env.test?
+    DevelopmentModeStubs::FakeRummagerApiForStatisticsAnnouncements
+  else
+    GdsApi::Rummager.new(
+      Plek.find('search') + Whitehall.government_search_index_path
+    )
+  end
+end
+
+Whitehall.statistics_announcement_search_client = statistics_announcement_search_client
