ff0bc1742b540497a342e7f46b77c092037df9af
Paul Bowsher <paul.bowsher@gmail.com>
2016-03-09 15:05:57 +0000

    Don't retry 422 errors for discard draft
    The publishing api will return a 422 error if there’s no draft for a
    content item, or a 404 if the content item doesn’t exist at all.
    Neither error should be retried.
    
    Bumps the api adapters version to provide an explicit exception for the
    422 error code.

--- app/workers/publishing_api_discard_draft_worker.rb
+++ app/workers/publishing_api_discard_draft_worker.rb
@@ -1,9 +1,10 @@
 class PublishingApiDiscardDraftWorker < PublishingApiWorker
   def perform(content_id, locale)
     Whitehall.publishing_api_v2_client.discard_draft(content_id, locale: locale)
-  rescue GdsApi::HTTPNotFound
+  rescue GdsApi::HTTPNotFound, GdsApi::HTTPUnprocessableEntity
     # nothing to do here as the draft has already been deleted
     # this shouldn't really happen but can still occur due to inconsistencies
     # between this app's data and what's in the Content Store/Publishing API
+    nil
   end
 end
