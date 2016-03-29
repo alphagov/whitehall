dce2c18d263ee7d56efabd67d065a1efb221a84d
Graham Pengelly <graham@three-tier.com>
2016-03-29 15:44:16 +0100

    `PublishingApiPusher` also handles `HtmlAttachment`
    `Edition` events now update/create/redirect any associated
    `HtmlAttachment` content with the Publishing API.
    
    By hooking up the transmission of attachments to the `Edition` events we
    are hoping to avoid the attachment having to know too much about the
    `Edition` state.

--- app/services/service_listeners/publishing_api_pusher.rb
+++ app/services/service_listeners/publishing_api_pusher.rb
@@ -9,28 +9,105 @@ module ServiceListeners
     def push(event:, options: {})
       case event
       when "force_publish", "publish"
-        api.publish_async(edition)
+        perform_publishing_api_action_on_pushable_items(
+          action: :publish_async
+        )
       when "update_draft"
-        api.save_draft_async(edition)
+        perform_publishing_api_action_on_pushable_items(
+          action: :save_draft_async
+        )
       when "update_draft_translation"
-        api.save_draft_translation_async(edition, options.fetch(:locale))
-      when "withdraw"
-        api.republish_document_async(edition.document)
+        pushable_items.each do |record|
+          api.save_draft_translation_async(record, options.fetch(:locale))
+        end
       when "unpublish"
         api.publish_async(edition.unpublishing)
+        edition_html_attachments.each do |attachment|
+          redirect_if_required(attachment)
+        end
+      when "withdraw"
+        api.republish_document_async(edition.document)
+        edition_html_attachments.each do |attachment|
+          api.republish_async(attachment)
+        end
       when "force_schedule", "schedule"
         api.schedule_async(edition)
+        #ignore attachments
       when "unschedule"
         api.unschedule_async(edition)
+        #ignore attachments
       when "delete"
-        api.discard_draft_async(edition)
+        perform_publishing_api_action_on_pushable_items(
+          action: :discard_draft_async
+        )
       end
     end
 
   private
 
+    def perform_publishing_api_action_on_pushable_items(action:)
+      pushable_items.each do |record|
+        api.send(action, record)
+      end
+    end
+
+    def pushable_items
+      pushable_items = [@edition]
+      pushable_items + edition_html_attachments
+    end
+
+    def edition_html_attachments
+      @edition.respond_to?(:html_attachments) ? @edition.html_attachments : []
+    end
+
     def api
       Whitehall::PublishingApi
+    end
+
+    def redirect_if_required(attachment)
+      if attachment.is_a?(HtmlAttachment)
+        if requires_redirect_to_alternative?(attachment)
+          redirect_to_unpublishing_alternative(attachment)
+        else
+          redirect_to_parent(attachment)
+        end
+      end
+    end
+
+    def requires_redirect_to_alternative?(attachment)
+      unpublishing = attachment.attachable.unpublishing
+      unpublishing.unpublishing_reason_id == UnpublishingReason::Consolidated.id ||
+        unpublishing.redirect
+    end
+
+    def redirect_to_parent(attachment)
+      publish_redirect(
+        attachment.url,
+        Whitehall.url_maker.public_document_path(edition)
+      )
+    end
+
+    def redirect_to_unpublishing_alternative(attachment)
+      edition = attachment.attachable
+      publish_redirect(
+        attachment.url,
+        Addressable::URI.parse(
+          edition.unpublishing.alternative_url
+        ).path
+      )
+    end
+
+    def publish_redirect(path, destination)
+      api.publish_redirect_async(
+        path,
+        [
+          {
+            path: path,
+            destination: destination,
+            type: "exact",
+          }
+        ]
+      )
     end
   end
 end
--- test/unit/services/listeners/publishing_api_pusher_test.rb
+++ test/unit/services/listeners/publishing_api_pusher_test.rb
@@ -8,9 +8,29 @@ module ServiceListeners
       PublishingApiPusher.new(edition).push(event: "update_draft")
     end
 
+    test "saves attachments draft" do
+      edition = build(
+        :draft_publication,
+        html_attachments: [attachment = build(:html_attachment)]
+      )
+      Whitehall::PublishingApi.expects(:save_draft_async).with(edition)
+      Whitehall::PublishingApi.expects(:save_draft_async).with(attachment)
+      PublishingApiPusher.new(edition).push(event: "update_draft")
+    end
+
     test "publish publishes" do
       edition = build(:publication)
       Whitehall::PublishingApi.expects(:publish_async).with(edition)
+      PublishingApiPusher.new(edition).push(event: "publish")
+    end
+
+    test "publish publishes attachments" do
+      edition = build(
+        :publication,
+        html_attachments: [attachment = build(:html_attachment)]
+      )
+      Whitehall::PublishingApi.expects(:publish_async).with(edition)
+      Whitehall::PublishingApi.expects(:publish_async).with(attachment)
       PublishingApiPusher.new(edition).push(event: "publish")
     end
 
@@ -20,9 +40,29 @@ module ServiceListeners
       PublishingApiPusher.new(edition).push(event: "force_publish")
     end
 
+    test "force_publish publishes attachments" do
+      edition = build(
+        :publication,
+        html_attachments: [attachment = build(:html_attachment)]
+      )
+      Whitehall::PublishingApi.expects(:publish_async).with(edition)
+      Whitehall::PublishingApi.expects(:publish_async).with(attachment)
+      PublishingApiPusher.new(edition).push(event: "force_publish")
+    end
+
     test "update_draft_translation saves draft translation" do
       edition = build(:publication)
       Whitehall::PublishingApi.expects(:save_draft_translation_async).with(edition, 'en')
+      PublishingApiPusher.new(edition).push(event: "update_draft_translation", options: { locale: "en" })
+    end
+
+    test "update_draft_translation updates the attachments" do
+      edition = build(
+        :publication,
+        html_attachments: [attachment = build(:html_attachment)]
+      )
+      Whitehall::PublishingApi.expects(:save_draft_translation_async).with(edition, 'en')
+      Whitehall::PublishingApi.expects(:save_draft_translation_async).with(attachment, 'en')
       PublishingApiPusher.new(edition).push(event: "update_draft_translation", options: { locale: "en" })
     end
 
@@ -43,24 +83,85 @@ module ServiceListeners
     end
 
     test "unpublish publishes the unpublishing" do
-      edition = build(:unpublished_publication)
+      edition = create(:unpublished_publication)
       Whitehall::PublishingApi.expects(:publish_async).with(edition.unpublishing)
       PublishingApiPusher.new(edition).push(event: "unpublish")
     end
 
-    test "force_schedule schedules" do
+    test "unpublish redirects the attachments to the alternative_url if in
+      error with redirect" do
+      edition = create(
+        :unpublished_publication_in_error_redirect,
+      )
+      attachment = edition.attachments.first
+      Whitehall::PublishingApi.expects(:publish_redirect_async).with(
+        attachment.url,
+        [
+          {
+            path: attachment.url,
+            destination: Addressable::URI.parse(
+              edition.unpublishing.alternative_url
+            ).path,
+            type: 'exact',
+          }
+        ]
+      )
+      PublishingApiPusher.new(edition).push(event: "unpublish")
+    end
+
+    test "unpublish redirects the attachments to the parent if in error
+      and !redirect" do
+      edition = create(
+        :unpublished_publication_in_error_no_redirect
+      )
+      attachment = edition.attachments.first
+      Whitehall::PublishingApi.expects(:publish_redirect_async).with(
+        attachment.url,
+        [
+          {
+            path: attachment.url,
+            destination: Whitehall.url_maker.public_document_path(edition),
+            type: 'exact',
+          }
+        ]
+      )
+      PublishingApiPusher.new(edition).push(event: "unpublish")
+    end
+
+    test "unpublish redirects the attachments to the alternative_url if
+      unpublished consolidated" do
+      edition = create(
+        :unpublished_publication_consolidated,
+      )
+      attachment = edition.attachments.first
+      Whitehall::PublishingApi.expects(:publish_redirect_async).with(
+        attachment.url,
+        [
+          {
+            path: attachment.url,
+            destination: Addressable::URI.parse(
+              edition.unpublishing.alternative_url
+            ).path,
+            type: 'exact',
+          }
+        ]
+      )
+      PublishingApiPusher.new(edition).push(event: "unpublish")
+    end
+
+    test "force_schedule schedules the edition" do
       edition = build(:publication)
       Whitehall::PublishingApi.expects(:schedule_async).with(edition)
       PublishingApiPusher.new(edition).push(event: "force_schedule")
     end
 
-    test "schedule schedules" do
+    test "schedule schedules the edition" do
       edition = build(:publication)
       Whitehall::PublishingApi.expects(:schedule_async).with(edition)
       PublishingApiPusher.new(edition).push(event: "schedule")
     end
 
-    test "unschedule unschedules" do
+    test "unschedule unschedules the edition" do
       edition = build(:publication)
       Whitehall::PublishingApi.expects(:unschedule_async).with(edition)
       PublishingApiPusher.new(edition).push(event: "unschedule")
@@ -70,6 +171,21 @@ module ServiceListeners
       edition = build(:publication)
       Whitehall::PublishingApi.expects(:discard_draft_async).with(edition)
       PublishingApiPusher.new(edition).push(event: "delete")
+    end
+
+    test "delete discards draft attachments" do
+      edition = build(
+        :publication,
+        html_attachments: [attachment = build(:html_attachment)]
+      )
+      Whitehall::PublishingApi.expects(:discard_draft_async).with(edition)
+      Whitehall::PublishingApi.expects(:discard_draft_async).with(attachment)
+      PublishingApiPusher.new(edition).push(event: "delete")
+    end
+
+    test "does not raise an error if the edition has no html_attachments association" do
+      edition = create(:published_document_collection)
+      PublishingApiPusher.new(edition).push(event: "publish")
     end
   end
 end
