1c107640c626fca6bf878e74ae2bcd4710e6c0c5
Graham Pengelly <graham@three-tier.com>
2016-03-31 09:55:08 +0100

    Improve test coverage
    Unit test all of the paths through
    `ServiceListeners::PublishingApiPusher#push`

--- test/unit/services/listeners/publishing_api_pusher_test.rb
+++ test/unit/services/listeners/publishing_api_pusher_test.rb
@@ -19,5 +19,47 @@ module ServiceListeners
       Whitehall::PublishingApi.expects(:publish_async).with(edition)
       PublishingApiPusher.new(edition).push(event: "force_publish")
     end
+
+    test "update_draft_translation saves draft translation" do
+      edition = build(:publication)
+      Whitehall::PublishingApi.expects(:save_draft_translation_async).with(edition, 'en')
+      PublishingApiPusher.new(edition).push(event: "update_draft_translation", options: { locale: "en" })
+    end
+
+    test "withdraw republishes" do
+      edition = build(:publication)
+      Whitehall::PublishingApi.expects(:republish_document_async).with(edition.document)
+      PublishingApiPusher.new(edition).push(event: "withdraw")
+    end
+
+    test "unpublish publishes the unpublishing" do
+      edition = build(:unpublished_publication)
+      Whitehall::PublishingApi.expects(:publish_async).with(edition.unpublishing)
+      PublishingApiPusher.new(edition).push(event: "unpublish")
+    end
+
+    test "force_schedule schedules" do
+      edition = build(:publication)
+      Whitehall::PublishingApi.expects(:schedule_async).with(edition)
+      PublishingApiPusher.new(edition).push(event: "force_schedule")
+    end
+
+    test "schedule schedules" do
+      edition = build(:publication)
+      Whitehall::PublishingApi.expects(:schedule_async).with(edition)
+      PublishingApiPusher.new(edition).push(event: "schedule")
+    end
+
+    test "unschedule unschedules" do
+      edition = build(:publication)
+      Whitehall::PublishingApi.expects(:unschedule_async).with(edition)
+      PublishingApiPusher.new(edition).push(event: "unschedule")
+    end
+
+    test "delete discards draft" do
+      edition = build(:publication)
+      Whitehall::PublishingApi.expects(:discard_draft_async).with(edition)
+      PublishingApiPusher.new(edition).push(event: "delete")
+    end
   end
 end
