6db073d751184dc3c2091006c97430474a7fe01d
Graham Pengelly <graham@three-tier.com>
2016-04-04 15:34:35 +0100

    Stub V1 `publish-intent` API endpoint
    Required for dependent features to pass since adding `publish-intent`
    support to `StatisticsAnnouncement`

--- features/support/publishing_api.rb
+++ features/support/publishing_api.rb
@@ -1,17 +1,16 @@
 require 'gds_api/publishing_api_v2'
 require_relative '../../test/support/policy_tagging_helpers'
 
-def stub_publishing_api_v1_requests
-  v1_url = Plek.current.find('publishing-api')
-  stub_request(:any, %r{\A#{ v1_url }})
-end
-
 Before do
+  publishing_api_v1_endpoint = File.join(
+    Plek.find('publishing-api'),
+    "publish-intent"
+  )
+  stub_request(:any, %r{\A#{publishing_api_v1_endpoint}})
   GdsApi::PublishingApiV2.any_instance.stubs(:publish)
   GdsApi::PublishingApiV2.any_instance.stubs(:put_content)
   GdsApi::PublishingApiV2.any_instance.stubs(:patch_links)
   stub_publishing_api_policies
-  stub_publishing_api_v1_requests
 end
 
 World(PolicyTaggingHelpers)
