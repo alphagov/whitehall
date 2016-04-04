cde2060fc329638f4309a345a2dfda87165d5563
Jack Scotti <jacopojackscotti@gmail.com>
2016-04-04 13:01:49 +0000

    Publish placeholders for static pages
    Currently the static pages are not present in the new publishing platform and
    do not have content ids. This ensures they are present with placeholders.
    
    We will migrate the links once, so that any tags from content tagger do not get
    overriden in the future when we deploy.
    
    The rake task that publishes to rummager and publishing api is currently manually
    run - we'll rerun it for this deploy.

--- lib/publish_static_pages.rb
+++ lib/publish_static_pages.rb
@@ -1,29 +1,74 @@
 class PublishStaticPages
   def publish
     index = Whitehall::SearchIndex.for(:government)
-    pages.each { |page| index.add(page) }
+    pages.each do |page|
+      index.add(present_for_rummager(page))
+
+      payload = present_for_publishing_api(page)
+      publishing_api.put_content(payload[:content_id], payload[:content])
+      publishing_api.publish(payload[:content_id], "minor", locale: "en")
+    end
   end
 
-private
+  def patch_links(content_id, links)
+    publishing_api.patch_links(content_id, links)
+  end
 
   def pages
     [
       {
-        _id: "/government/how-government-works",
-        link: "/government/how-government-works",
-        format: "edition",
+        content_id: "f56cfe74-8e5c-432d-bfcf-fd2521c5919c",
         title: "How government works",
         description: "About the UK system of government. Understand who runs government, and how government is run.",
         indexable_content: "government, parliament, coalition, civil service, civil servants, policies, policy, minister, ministers, MP, rt hon, right honourable, department, ndpb, agency, executive agency, agencies, organisation, public bodies, public body, FOI, freedom of information, transparency, democracy, westminster, whitehall, house of commons, house of lords",
+        base_path: "/government/how-government-works",
       },
       {
-        _id: "/government/get-involved",
-        link: "/government/get-involved",
-        format: "edition",
+        content_id: "dbe329f1-359c-43f7-8944-580d4742aa91",
         title: "Get involved",
         description: "Find out how you can engage with government directly, and take part locally, nationally or internationally.",
         indexable_content: "changing government policies, consultations, government departments",
+        base_path: "/government/get-involved",
       }
     ]
+  end
+
+  def present_for_rummager(page)
+    {
+      _id: page[:base_path],
+      link: page[:base_path],
+      format: "edition", # Used for the rummager document type
+      title: page[:title],
+      description: page[:description],
+      indexable_content: page[:indexable_content],
+    }
+  end
+
+  def present_for_publishing_api(page)
+    {
+      content_id: page[:content_id],
+      content: {
+        title: page[:title],
+        description: page[:description],
+        format: "placeholder", # This content will never be rendered by content store
+        locale: "en",
+        base_path: page[:base_path],
+        publishing_app: "whitehall",
+        rendering_app: "whitehall",
+        routes: [
+          {
+            path: page[:base_path],
+            type: "exact",
+          },
+        ],
+        public_updated_at: Time.zone.now.iso8601,
+      }
+    }
+  end
+
+private
+
+  def publishing_api
+    @publishing_api ||= Whitehall.publishing_api_v2_client
   end
 end
--- test/unit/publish_static_pages_test.rb
+++ test/unit/publish_static_pages_test.rb
@@ -1,9 +1,60 @@
 require "test_helper"
+require "govuk-content-schema-test-helpers"
 
 class PublishStaticPagesTest < ActiveSupport::TestCase
-  test 'sends static pages to rummager' do
+  test 'sends static pages to rummager and publishing api' do
     Whitehall::FakeRummageableIndex.any_instance.expects(:add).twice.with(kind_of(Hash))
+    publisher = PublishStaticPages.new
+    expect_publishing(publisher.pages)
 
     PublishStaticPages.new.publish
+  end
+
+  test 'static pages presented to the publishing api are valid placeholders' do
+    publisher = PublishStaticPages.new
+    presented = publisher.present_for_publishing_api(publisher.pages.first)
+    expect_valid_placeholder(presented[:content])
+  end
+
+  test 'base paths do not change' do
+    base_paths = PublishStaticPages.new.pages.map { |page| page[:base_path] }
+    assert_equal(
+      base_paths,
+      ["/government/how-government-works", "/government/get-involved"],
+      "Base paths for static content should not be changed without first setting up a redirect"
+    )
+  end
+
+  def expect_patch_links(pages)
+    pages.each do |page|
+      Whitehall.publishing_api_v2_client.expects(:patch_links)
+        .with(page[:content_id], { links: page[:links] })
+    end
+  end
+
+  def expect_publishing(pages)
+    pages.each do |page|
+      Whitehall.publishing_api_v2_client.expects(:put_content)
+        .with(
+          page[:content_id],
+          has_entries(
+            format: "placeholder",
+            base_path: page[:base_path],
+            title: page[:title]
+          )
+        )
+
+      Whitehall.publishing_api_v2_client.expects(:publish)
+        .with(page[:content_id], 'minor', locale: "en")
+    end
+  end
+
+  def expect_valid_placeholder(presented_page)
+    validator = GovukContentSchemaTestHelpers::Validator.new(
+      "placeholder",
+      "schema",
+      presented_page
+    )
+    assert validator.valid?, validator.errors.join("\n")
   end
 end
