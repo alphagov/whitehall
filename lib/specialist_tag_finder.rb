a9aa2b17cff698af77193ce495adf291e3992966
Mo Baig <mo@mobaig.com>
2016-04-13 15:17:15 +0100

    Remove unused SpecialistTagFinder methods

--- lib/specialist_tag_finder.rb
+++ lib/specialist_tag_finder.rb
@@ -28,29 +28,4 @@ class SpecialistTagFinder
     parent_content_item = Whitehall.content_store.content_item(parent_path)
     Array(parent_content_item.links["parent"]).first
   end
-
-  def primary_sector_tag
-    primary_subsector_tag.parent if primary_subsector_tag
-  end
-
-  def primary_subsector_tag
-    if (primary_tag_slug = @edition.primary_specialist_sector_tag)
-      specialist_sector_tags.find {|t| t.slug == primary_tag_slug }
-    end
-  end
-
-  def sectors_and_subsectors
-    specialist_sector_tags.map { |t| [t, t.parent] }.flatten.compact.uniq
-  end
-
-private
-
-  def artefact
-    @artefact ||= Whitehall.content_api.artefact(RegisterableEdition.new(@edition).slug)
-  end
-
-  def specialist_sector_tags
-    return [] if artefact.nil?
-    artefact.tags.select {|t| t.details['type'] == 'specialist_sector' }
-  end
 end
--- test/unit/specialist_tag_finder_test.rb
+++ test/unit/specialist_tag_finder_test.rb
@@ -4,26 +4,6 @@ require "gds_api/test_helpers/content_store"
 class SpecialistTagFinderTest < ActiveSupport::TestCase
   include GdsApi::TestHelpers::ContentStore
 
-  setup do
-    @edition = stub
-    @tag_finder = SpecialistTagFinder.new(@edition)
-
-    @parent_tag = stub(details: {'type' => 'specialist_sector'}, slug: 'super')
-    @primary_tag = stub(details: {'type' => 'specialist_sector'},
-                        slug: 'super/primary', parent: @parent_tag)
-    @secondary_tag = stub(details: {'type' => 'specialist_sector'},
-                          slug: 'super/secondary', parent: @parent_tag)
-    @irrelevant_tag = stub(details: {'type' => 'something_else'})
-
-    @artefact = stub(tags: [
-      @primary_tag,
-      @secondary_tag,
-      @irrelevant_tag
-    ])
-
-    @tag_finder.stubs(artefact: @artefact)
-  end
-
   test "#topics returns all linked topics" do
     edition = create(:edition_with_document)
     edition_base_path = PublishingApiPresenters::Edition.new(edition).base_path
@@ -57,35 +37,5 @@ class SpecialistTagFinderTest < ActiveSupport::TestCase
 
 
     assert_equal "/grandpa", SpecialistTagFinder.new(edition).grandparent_topic.base_path
-  end
-
-  test "#primary_sector_tag returns the primary subsector tag's parent" do
-    subsector_tag = mock(parent: 'parent_tag')
-    @tag_finder.stubs(primary_subsector_tag: subsector_tag)
-
-    assert_equal 'parent_tag', @tag_finder.primary_sector_tag
-  end
-
-  test "#primary_sector_tag returns nil if no primary subsector tag" do
-    @tag_finder.stubs(primary_subsector_tag: nil)
-
-    assert_equal nil, @tag_finder.primary_sector_tag
-  end
-
-  test "#primary_subsector_tag returns the specialist sector tag whose slug matches the primary" do
-    @edition.stubs(primary_specialist_sector_tag: 'super/primary')
-
-    assert_equal @primary_tag, @tag_finder.primary_subsector_tag
-  end
-
-  test "#primary_subsector_tag returns nil if no primary specialist sector tag" do
-    @edition.stubs(primary_specialist_sector_tag: nil)
-
-    assert_equal nil, @tag_finder.primary_subsector_tag
-  end
-
-  test "#sectors_and_subsectors returns all relevant tags and parent tags" do
-    assert_equal [@parent_tag, @primary_tag, @secondary_tag].to_set,
-                 @tag_finder.sectors_and_subsectors.to_set
   end
 end
