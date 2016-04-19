1d8adddb84b1a9bdcefc0455af3f2d27f3148ffa
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2016-04-19 09:28:02 +0000

    Remove inconsistent data
    While migrating links data, we noticed a CorporateInformationPage that belongs
    to a document that no longer exists.
    
    We're not addressing the root cause in this PR (no foreign keys for our associations)
    but this migration corrects this situation for all editions.

---
+++ db/data_migration/20160419091436_remove_inconsistent_editions.rb
@@ -0,0 +1,5 @@
+# Remove editions that do not have an associated documents.
+# This is not enforced in the database via a foreign key.
+# Edition.where("not exists (select 1 from documents where documents.id = document_id)")
+
+CorporateInformationPage.delete(336520)
