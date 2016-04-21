149c6a3207adc04f56cb59c37a5c34c45ff6c951
Mo Baig <mo@mobaig.com>
2016-04-21 16:06:01 +0100

    SpecialistTagFinder refactor - extract common content item logic.

--- lib/specialist_tag_finder.rb
+++ lib/specialist_tag_finder.rb
@@ -15,15 +15,10 @@ class SpecialistTagFinder
   end
 
   def topics
-    @topics ||=
-      begin
-        presented_edition = PublishingApiPresenters::Edition.new(@edition)
-        edition_path = presented_edition.base_path
-        content_item = Whitehall.content_store.content_item(edition_path)
-
-        return [] unless content_item
-        Array(content_item.links["topics"])
-      end
+    @topics ||= begin
+      return [] unless edition_content_item
+      Array(edition_content_item.links["topics"])
+    end
   end
 
   def top_level_topic
@@ -34,21 +29,27 @@ class SpecialistTagFinder
     # in the frontend when rendering an Edition's breadcrumb.
 
     @top_level_topic ||= begin
+      return unless edition_content_item
+      parents = Array(edition_content_item.links["parent"])
+      return unless parents.any?
+
+      # FIXME: We now need to fetch the parent topic from the content store to
+      # retrieve its parent. We should replace this implementation with the
+      # publishing API's links expansion / dependency resolution, which is
+      # currently WIP.
+      parent_path = parents.first["base_path"]
+      parent_content_item = Whitehall.content_store.content_item(parent_path)
+      Array(parent_content_item.links["parent"]).first
+    end
+  end
+
+private
+
+  def edition_content_item
+    @edition_content_item ||= begin
       presented_edition = PublishingApiPresenters::Edition.new(@edition)
       edition_path = presented_edition.base_path
-      edition_content_item = Whitehall.content_store.content_item(edition_path)
-
-        return unless edition_content_item
-        parents = Array(edition_content_item.links["parent"])
-        return unless parents.any?
-
-        # FIXME: We now need to fetch the parent topic from the content store to
-        # retrieve its parent. We should replace this implementation with the
-        # publishing API's links expansion / dependency resolution, which is
-        # currently WIP.
-        parent_path = parents.first["base_path"]
-        parent_content_item = Whitehall.content_store.content_item(parent_path)
-        Array(parent_content_item.links["parent"]).first
-      end
+      Whitehall.content_store.content_item(edition_path)
+    end
   end
 end
