aa2f7f9aced7030f6a06168d6d6579e852196b1f
Mo Baig <mo@mobaig.com>
2016-04-21 15:28:30 +0100

    Memoize methods in SpecialistTagFinder
    So that #topics and #grandparent_topic don't make repeat network calls
    when called multiple times on same instance.

--- lib/specialist_tag_finder.rb
+++ lib/specialist_tag_finder.rb
@@ -5,29 +5,35 @@ class SpecialistTagFinder
   end
 
   def topics
-    presented_edition = PublishingApiPresenters::Edition.new(@edition)
-    edition_path = presented_edition.base_path
-    content_item = Whitehall.content_store.content_item(edition_path)
+    @topics ||=
+      begin
+        presented_edition = PublishingApiPresenters::Edition.new(@edition)
+        edition_path = presented_edition.base_path
+        content_item = Whitehall.content_store.content_item(edition_path)
 
-    return [] unless content_item
-    Array(content_item.links["topics"])
+        return [] unless content_item
+        Array(content_item.links["topics"])
+      end
   end
 
   def grandparent_topic
-    presented_edition = PublishingApiPresenters::Edition.new(@edition)
-    edition_path = presented_edition.base_path
-    edition_content_item = Whitehall.content_store.content_item(edition_path)
+    @grandparent_topic ||=
+      begin
+        presented_edition = PublishingApiPresenters::Edition.new(@edition)
+        edition_path = presented_edition.base_path
+        edition_content_item = Whitehall.content_store.content_item(edition_path)
 
-    return unless edition_content_item
-    parents = Array(edition_content_item.links["parent"])
-    return unless parents.any?
+        return unless edition_content_item
+        parents = Array(edition_content_item.links["parent"])
+        return unless parents.any?
 
-    # FIXME: We now need to fetch the parent topic from the content store to
-    # retrieve its parent. We should replace this implementation with the
-    # publishing API's links expansion / dependency resolution, which is
-    # currently WIP.
-    parent_path = parents.first["base_path"]
-    parent_content_item = Whitehall.content_store.content_item(parent_path)
-    Array(parent_content_item.links["parent"]).first
+        # FIXME: We now need to fetch the parent topic from the content store to
+        # retrieve its parent. We should replace this implementation with the
+        # publishing API's links expansion / dependency resolution, which is
+        # currently WIP.
+        parent_path = parents.first["base_path"]
+        parent_content_item = Whitehall.content_store.content_item(parent_path)
+        Array(parent_content_item.links["parent"]).first
+      end
   end
 end
