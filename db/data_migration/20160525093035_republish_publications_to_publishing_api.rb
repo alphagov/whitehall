c2989e4f3ba2506a7c7e92211efd1b74f96992dd
Paul Bowsher <paul.bowsher@gmail.com>
2016-06-07 11:22:23 +0100

    Send republish jobs to correct queue

--- db/data_migration/20160525093035_republish_publications_to_publishing_api.rb
+++ db/data_migration/20160525093035_republish_publications_to_publishing_api.rb
@@ -1,3 +1,3 @@
 Publication.includes(:document).find_each do |pub|
-  Whitehall::PublishingApi.republish_document_async(pub.document)
+  Whitehall::PublishingApi.republish_document_async(pub.document, bulk: true)
 end
