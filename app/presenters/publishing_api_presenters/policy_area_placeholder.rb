52a746a3e5f180bae335e7acdf121589a35ad4f6
Mo Baig <mo@mobaig.com>
2016-07-04 15:52:00 +0100

    Correct links payload for two additional publishing API presenters
    Remove topic links from PolicyArea and StatisticsAnnouncement presenters.
    Neither of these can be tagged with specialist sectors, so we shouldn't
    be attempting to evaluate the specialist sector content IDs.

--- app/presenters/publishing_api_presenters/policy_area_placeholder.rb
+++ app/presenters/publishing_api_presenters/policy_area_placeholder.rb
@@ -1,10 +1,7 @@
 # Note that "Policy Area" is the new name for "Topic".
 class PublishingApiPresenters::PolicyAreaPlaceholder < PublishingApiPresenters::Placeholder
   def links
-    extract_links([
-      :topics,
-      :organisations,
-    ])
+    extract_links([:organisations])
   end
 
   private
