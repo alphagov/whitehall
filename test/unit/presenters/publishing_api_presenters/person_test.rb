e97ca36f5c365372e0f5f40d00bf91de0d9caf06
Mo Baig <mo@mobaig.com>
2016-07-04 15:49:12 +0100

    Add explicit publishing API presenters for 4 document types
    This commit adds presenters for the Person, MinisterialRole,
    WorldLocation, and WordlwideOrganisation items.
    
    Why?
    
    These were previously presented by the publishing API
    PlaceholderPresenter. This presenter was attempting to extract topic and
    organisation link types from the item, regardless of whether the item
    could be tagged to topics and organisations. By providing specific
    presenters for each item, we gain the following:
    
    - We can specify the links payload explicitly rather than relying on the
      implementation in PlaceholderPresenter.
    - It's much clearer how items of this type are presented to the
      publishing API.
    
    How?
    
    - Add new presenter classes for each of the 4 items.
    - Move the pre-existing test cases from PlaceholderTest into individual
      test files.
    - Update the PublishingApiPresenters case statement to select the
      appropriate presenter at runtime.

---
+++ test/unit/presenters/publishing_api_presenters/person_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class PublishingApiPresenters::PersonTest < ActiveSupport::TestCase
+  def present(model_instance, options = {})
+    PublishingApiPresenters::Person.new(model_instance, options)
+  end
+
+  test 'presents a Person ready for adding to the publishing API' do
+    person = create(:person, forename: "Winston")
+    public_path = Whitehall.url_maker.person_path(person)
+
+    expected_hash = {
+      base_path: public_path,
+      title: "Winston",
+      description: nil,
+      schema_name: "placeholder",
+      document_type: "person",
+      locale: 'en',
+      publishing_app: 'whitehall',
+      rendering_app: 'whitehall-frontend',
+      public_updated_at: person.updated_at,
+      routes: [{ path: public_path, type: "exact" }],
+      redirects: [],
+      need_ids: [],
+      details: {},
+    }
+    expected_links = {}
+
+    presented_item = present(person)
+
+    assert_equal expected_hash, presented_item.content
+    assert_equal expected_links, presented_item.links
+    assert_equal "major", presented_item.update_type
+    assert_equal person.content_id, presented_item.content_id
+
+    assert_valid_against_schema(presented_item.content, 'placeholder')
+  end
+end
