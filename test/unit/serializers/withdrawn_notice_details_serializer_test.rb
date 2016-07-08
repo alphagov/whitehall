08d639a5b76de084fdd8dc9c21d487a27d1c0d00
Carlos Vilhena <carlosvilhena@gmail.com>
2016-07-08 15:23:51 +0100

    Adding Withdrawn Notice serializer
    A number of publishing API presenters conditionally include withdrawn notice
    in the payload. This serializer represents that logic, including the
    conditional. This way, presenters can merge the contents of the serializer
    without worrying about the conditional logic.

---
+++ test/unit/serializers/withdrawn_notice_details_serializer_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class WithdrawnNoticeDetailsSerializerTest < ActiveSupport::TestCase
+  def stubbed_item
+    stub(
+      withdrawn_at: 'a date',
+      unpublishing: stub(explanation: explanation),
+      updated_at: 'a date'
+    )
+  end
+
+  def explanation
+    "An explanation"
+  end
+
+  def serializer
+    WithdrawnNoticeDetailsSerializer.new(stubbed_item)
+  end
+
+  test "it includes a withdrawn_at attribute" do
+    assert_equal serializer.withdrawn_at, stubbed_item.withdrawn_at
+  end
+
+  test 'it includes an explanation attribute' do
+    assert_equal(
+      serializer.explanation,
+      "<div class=\"govspeak\"><p>#{explanation}</p>\n</div>"
+    )
+  end
+end
