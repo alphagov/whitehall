4e5e6ab3e415c780f64800df269b68b7939e44c5
Carlos Vilhena <carlosvilhena@gmail.com>
2016-07-08 15:25:23 +0100

    Adding an Image Details serializer
    This has the responsibility of building attributes that represent an image
    for the case study's details hash.

---
+++ test/unit/serializers/image_details_serializer_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class ImageDetailsSerializerTest < ActiveSupport::TestCase
+  def stubbed_item
+    stub(
+      lead_image_path: '/lead_image.png',
+      lead_image_alt_text: 'An image',
+      lead_image_caption: 'An image caption'
+    )
+  end
+
+  def serializer
+    ImageDetailsSerializer.new(stubbed_item)
+  end
+
+  test "it includes the URL for the image" do
+    host = "http://www.somehost.com"
+    Whitehall.stub(:public_asset_host, host) do
+      assert_equal serializer.url, "#{host}/lead_image.png"
+    end
+  end
+
+  test "it includes the alt test of the image" do
+    assert_equal serializer.alt_text, stubbed_item.lead_image_alt_text
+  end
+
+  test "it includes the caption of the image" do
+    assert_equal serializer.caption, stubbed_item.lead_image_caption
+  end
+end
