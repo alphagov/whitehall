004803acc9299039e1644903611d06dfeb6c7703
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-07-19 14:19:10 +0000

    Fix typo

--- test/factories/classifications.rb
+++ test/factories/classifications.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :classification do
     sequence(:name) { |index| "classification-#{index}" }
-    description 'Classifcation description'
+    description 'Classification description'
   end
 end
