a393c69242eeac18cd544d70f1c0d9ae19893dab
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-07-20 10:14:18 +0100

    Add an optional allow_draft parameter to the PublishingApiWithdrawalWorker

--- app/workers/publishing_api_withdrawal_worker.rb
+++ app/workers/publishing_api_withdrawal_worker.rb
@@ -1,10 +1,11 @@
 class PublishingApiWithdrawalWorker < PublishingApiWorker
-  def perform(content_id, explanation, locale)
+  def perform(content_id, explanation, locale, allow_draft = false)
     Whitehall.publishing_api_v2_client.unpublish(
       content_id,
       type: "withdrawal",
       locale: locale,
-      explanation: explanation
+      explanation: explanation,
+      allow_draft: allow_draft,
     )
   end
 end
