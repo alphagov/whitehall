63e97efe93d2de2ba39b03947e54cd9d9fdf4be4
Graham Pengelly <graham@three-tier.com>
2016-07-27 13:24:42 +0100

    Switch `DetailedGuide` rendering to `government-frontend`
    This commit switches over rendering from Whitehall to
    government-frontend as the penultimate step to migrating this format to
    to Publishing API -> Content Store -> Government Frontend

--- app/models/detailed_guide.rb
+++ app/models/detailed_guide.rb
@@ -33,6 +33,10 @@ class DetailedGuide < Edition
 
   validates_with HeadingHierarchyValidator
 
+  def rendering_app
+    Whitehall::RenderingApp::GOVERNMENT_FRONTEND
+  end
+
   def rummager_index
     :detailed_guides
   end
