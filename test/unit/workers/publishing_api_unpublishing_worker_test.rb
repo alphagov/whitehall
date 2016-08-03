f671e3714c9c989deb42f69c92d32d6535ec0dda
Graham Pengelly <graham@three-tier.com>
2016-08-03 18:05:54 +0100

    Add `PublishingApiUnpublishingWorker`
    This worker wraps the various `/unpublish` endpoint workers to allow the
    various behaviours of unpublishing to be defined in one place.

---
+++ test/unit/workers/publishing_api_unpublishing_worker_test.rb
@@ -0,0 +1,92 @@
+require "test_helper"
+require "gds_api/test_helpers/publishing_api_v2"
+
+class PublishingApiUnpublishingWorkerTest < ActiveSupport::TestCase
+  include GdsApi::TestHelpers::PublishingApiV2
+
+  #UnpublishingReason: 1 - in error
+  test "runs PublishingApiGoneWorker with path and explanation when redirect is false" do
+    unpublished_edition = create(
+      :unpublished_edition,
+      :published_in_error_no_redirect
+    )
+
+    PublishingApiGoneWorker.expects(:new).returns(gone_worker = mock)
+    unpublishing = unpublished_edition.unpublishing
+
+    gone_worker.expects(:perform).with(
+      unpublished_edition.document.content_id,
+      unpublishing.alternative_path,
+      unpublishing.explanation,
+      :en
+    )
+
+    PublishingApiUnpublishingWorker.new.perform(unpublished_edition.unpublishing.id)
+  end
+
+  #UnpublishingReason: 1 - in error, redirect: true
+  test "runs PublishingApiGoneWorker with path when redirect is true" do
+    unpublished_edition = create(
+      :unpublished_edition,
+      :published_in_error_redirect
+    )
+
+    PublishingApiRedirectWorker.expects(:new).returns(redirect_worker = mock)
+    unpublishing = unpublished_edition.unpublishing
+
+    redirect_worker.expects(:perform).with(
+      unpublished_edition.document.content_id,
+      unpublishing.alternative_path,
+      :en
+    )
+
+    PublishingApiUnpublishingWorker.new.perform(unpublished_edition.unpublishing.id)
+  end
+
+  #UnpublishingReason: 4 - consolidated
+  test "runs PublishingApiRedirectWorker with alternative path" do
+    unpublished_edition = create(
+      :unpublished_edition,
+      :consolidated_redirect
+    )
+
+    PublishingApiRedirectWorker.expects(:new).returns(redirect_worker = mock)
+    unpublishing = unpublished_edition.unpublishing
+
+    redirect_worker.expects(:perform).with(
+      unpublished_edition.document.content_id,
+      unpublishing.alternative_path,
+      :en
+    )
+
+    PublishingApiUnpublishingWorker.new.perform(unpublished_edition.unpublishing.id)
+  end
+
+  #UnpublishingReason: 5 - withdrawn
+  test "runs PublishingApiWithdrawalWorker with alternative path" do
+    unpublished_edition = create(
+      :withdrawn_edition
+    )
+
+    PublishingApiWithdrawalWorker.expects(:new).returns(withdrawal_worker = mock)
+    unpublishing = unpublished_edition.unpublishing
+
+    withdrawal_worker.expects(:perform).with(
+      unpublished_edition.document.content_id,
+      unpublishing.explanation,
+      :en
+    )
+
+    PublishingApiUnpublishingWorker.new.perform(unpublished_edition.unpublishing.id)
+  end
+
+  test "resends a draft to the publishing api" do
+    unpublished_edition = create(
+      :unpublished_edition,
+      :consolidated_redirect
+    )
+
+    Whitehall::PublishingApi.expects(:save_draft_async).with(unpublished_edition)
+    PublishingApiUnpublishingWorker.new.perform(unpublished_edition.unpublishing.id)
+  end
+end
