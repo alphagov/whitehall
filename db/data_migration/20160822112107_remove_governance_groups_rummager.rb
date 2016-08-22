edff8c3bb7dd4f5b2003f6594bd8c35cf367407b
Matteo Grassotti <matteo.grassotti@digital.cabinet-office.gov.uk>
2016-08-22 13:30:58 +0200

    Remove governance groups from searches
    Create a new data migration to remove governance groups from rummager,
    so they don’t appear anymore in searches.
    
    Add 404s handling.

---
+++ db/data_migration/20160822112107_remove_governance_groups_rummager.rb
@@ -0,0 +1,19 @@
+BASE_PATH_ROOT = "/government/organisations/"
+
+SLUGS = [
+  "national-security/groups/intelligence-and-security-committee",
+  "national-security/groups/joint-intelligence-committee",
+  "national-security/groups/joint-intelligence-organisation",
+  "national-security/groups/national-security-council",
+  "civil-service/groups/civil-service-board",
+  "cabinet-office/groups/cabinet-office-board"
+]
+
+SLUGS.each do |slug|
+  base_path = BASE_PATH_ROOT + slug
+  begin
+    Whitehall.government_search_client.delete_content! base_path
+  rescue GdsApi::HTTPNotFound => e 
+    puts "\n" + "="*25 + "\nURL not found error:\n#{e}"
+  end
+end
