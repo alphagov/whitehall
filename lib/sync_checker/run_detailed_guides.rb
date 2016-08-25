e5bc9b5525003280af5ec7d5dd0b661cbaa8e373
Paul Bowsher <paul.bowsher@gmail.com>
2016-08-25 21:27:32 +0100

    Check provided ids
    Pass ids on STDIN to only check those documents.
    
    ```
    cat sync_check.csv | awk -F, '{print $3}' | grep -E '^[0-9]' | bundle exec rails r lib/sync_checker/run_detailed_guides.rb
    ```

--- lib/sync_checker/run_detailed_guides.rb
+++ lib/sync_checker/run_detailed_guides.rb
@@ -1,5 +1,11 @@
-# documents = Document.where(id: 293884)
-documents = Document.where(id: DetailedGuide.all.pluck(:document_id).uniq.last(1000))
+if !STDIN.tty?
+  ids = STDIN.readlines.map(&:to_i)
+  STDIN.close
+else
+  ids = DetailedGuide.all.pluck(:document_id).uniq
+end
+
+documents = Document.where(id: ids)
 dg_checks = documents.map { |doc| SyncChecker::DetailedGuideCheck.new(doc) }
 checker = SyncChecker::SyncCheck.new(dg_checks, csv_file_path: ARGV[0])
 checker.run
