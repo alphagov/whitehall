5756cf79978947a36abcb519560c6aeb88f606ec
Paul Bowsher <paul.bowsher@gmail.com>
2016-08-26 01:05:43 +0100

    Add ability to re-run failures

--- lib/sync_checker/result_set.rb
+++ lib/sync_checker/result_set.rb
@@ -9,16 +9,19 @@ module SyncChecker
       @results = []
 
       if csv_file_path.present? && !Rails.env.test?
-        file = File.open(File.expand_path(csv_file_path), "w")
-        @csv = CSV.new(file)
+        @csv_file = File.open(File.expand_path(csv_file_path), "w")
+        @csv = CSV.new(@csv_file)
         @csv << Failure.members
       else
         @csv = NullCSV
       end
+
+      @failure_file = File.open(Rails.root.join("tmp/.sync_check_failures"), "w")
     end
 
     def <<(result)
       if result
+        @failure_file.puts result.document_id
         results << result
         csv << result.to_row
         progress_bar.log result.to_s
