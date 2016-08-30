f910fc33357401dd13de4efca2cbe621c831f04b
Graham Pengelly <graham@three-tier.com>
2016-08-30 16:05:28 +0100

    `DetailsCheck` checks other fields if supplied

--- lib/sync_checker/checks/details_check.rb
+++ lib/sync_checker/checks/details_check.rb
@@ -9,16 +9,31 @@ module SyncChecker
         if response.response_code == 200
           @content_item = JSON.parse(response.body)
           if run_check?
-            failures << "details body doesn't match" unless body_is_equivalent?
+            expected_details.each do |k, v|
+              failures << check_key(k, v)
+            end
           end
         end
-        failures
+        failures.compact
       end
 
     private
 
       def run_check?
         %w(redirect gone).exclude?(content_item["schema_name"])
+      end
+
+      def check_key(key, value)
+        return check_body if key.to_s == "body"
+        response_value = content_item["details"][key.to_s]
+        return "expected details to contain '#{key}' == '#{value}'" if response_value.nil?
+        if response_value != value
+          "expected details '#{key}' to equal '#{value}' but got '#{response_value}'"
+        end
+      end
+
+      def check_body
+        "details body doesn't match" unless body_is_equivalent?
       end
 
       def body_is_equivalent?
