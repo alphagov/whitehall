e01f0c5e03872c336029b98454792fe093150bc7
Graham Pengelly <graham@three-tier.com>
2016-09-06 13:46:29 +0100

    Set unpublished `DetailedGuide` state to `draft`
    These three detailed guides are in rejected/submitted and thus can't be
    correctly migrated as they have an unpublishing.

---
+++ db/data_migration/20160906123915_fix_detailed_guide_state.rb
@@ -0,0 +1,8 @@
+#These are detailed guides that have been unpublished and then quickly
+#submitted and/or rejected. This means they don't get republished correctly
+#during migration.
+
+DetailedGuide.where(id: [629523, 516694, 437572]).each do | detailed_guide |
+ detailed_guide.state = "draft"
+ detailed_guide.save!
+end
