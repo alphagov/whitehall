f3d764831e46eecdbb3f32fe5abe8018b69ab760
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2016-09-12 09:13:09 +0100

    Refactor output file creation

--- db/data_migration/20160908141608_remove_chapters_from_related_mainstream_urls.rb
+++ db/data_migration/20160908141608_remove_chapters_from_related_mainstream_urls.rb
@@ -5,38 +5,20 @@ def validate_url_content_id(url)
   url = url.chomp
   base_path = url[18..-1] if url.length > 17
   content_id = Whitehall.publishing_api_v2_client.lookup_content_id(base_path: base_path) if base_path
-  p base_path
-  p content_id
   content_id
 end
 
-
-def write_detailed_guide_to_file(id, url)
-  File.open("improve_mainstream_content.detailed_guide_items_found", "a+") do |f|
-    f.write "#{id}, #{url}\n"
-  end
-end
-
-def write_content_items_not_found_to_file(id, url)
-  File.open("improve_mainstream_content.content_items_not_found", "a+") do |f|
-    f.write "#{id}, #{url}\n"
-  end
-end
-
-def write_updated_items_to_file(id, old_url, updated_url)
-  File.open("improve_mainstream_content.related_content_found_and_updated", "a+") do |f|
-    f.write "#{id}, #{old_url}, #{updated_url}\n"
-  end
-end
-
-def write_items_that_dont_need_updating(id, old_url, content_id)
-  File.open("improve_mainstream_content.original_url_already_perfect", "a+") do |f|
-    f.write "#{id}, #{content_id}, #{old_url}\n"
+def write_to_file(file_name, params)
+  File.open(file_name, "a+") do |f|
+    params.each do |param|
+      f.write "#{param}, "
+    end
+    f.write "\n"
   end
 end
 
 def detailed_guide?(id, url)
-  write_detailed_guide_to_file(id, url) if url.start_with?("https://www.gov.uk/guidance/")
+  write_to_file("related_mainstream_content.detailed_guide_items_found", [id, url]) if url.start_with?("https://www.gov.uk/guidance/")
   url.start_with?("https://www.gov.uk/guidance/")
 end
 
@@ -44,7 +26,7 @@ def update_mainstream_url(id, clean_url)
   detailed_guide = DetailedGuide.find(id)
   incorrect_url = detailed_guide.related_mainstream_content_url
   #detailed_guide.update_attribute(:related_mainstream_content_url, clean_url)
-  write_updated_items_to_file(id, incorrect_url, clean_url)
+  write_to_file("related_mainstream_content.related_content_found_and_updated", [id, incorrect_url, clean_url])
 end
 
 def clean_the_url(url)
@@ -55,10 +37,10 @@ def clean_the_url(url)
   clean_url
 end
 
-files = %w(improve_mainstream_content.original_url_already_perfect
-          improve_mainstream_content.content_items_not_found
-          improve_mainstream_content.detailed_guide_items_found
-          original_url_already_perfect.related_content_found_and_updated)
+files = %w(related_mainstream_content.original_url_already_perfect
+          related_mainstream_content.content_items_not_found
+          related_mainstream_content.detailed_guide_items_found
+          related_mainstream_content.related_content_found_and_updated)
 files.each do |f|
   File.delete(f) if File.exists?(f)
 end
@@ -81,9 +63,9 @@ related_mainstream_content_urls.each do |detailed_guide|
     content_id = validate_url_content_id(chopped_url) if chopped_url
     update_mainstream_url(detailed_guide[:id], chopped_url) if !content_id.nil?
   else
-    write_items_that_dont_need_updating(detailed_guide[:id], content_id, detailed_guide[:related_mainstream_content_url])
+    write_to_file("related_mainstream_content.original_url_already_perfect", [detailed_guide[:id], content_id, detailed_guide[:related_mainstream_content_url]])
   end
-  write_content_items_not_found_to_file(detailed_guide[:id], detailed_guide[:related_mainstream_content_url]) if content_id.nil?
+  write_to_file("related_mainstream_content.content_items_not_found", [detailed_guide[:id], detailed_guide[:related_mainstream_content_url]]) if content_id.nil?
 end
 
 
@@ -94,6 +76,16 @@ end
 #                                     "additional_related_mainstream_content_url IS NOT NULL
 #                                     AND additional_related_mainstream_content_url != ''"
 #                                   ])
+# n = 0
+# additional_related_mainstream_content_urls.each do |detailed_guide|
+#   id = detailed_guide.id
+#   url = detailed_guide.additional_related_mainstream_content_url
+#
+#   next if detailed_guide?(id, url)
+# end
+
+
+
 #
 # additional_related_mainstream_content_urls.each do |detailed_guide|
 #   p detailed_guide[:id]
