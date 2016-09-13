1e885e69f9e6ad14671e7b202519c6dd26f1dbd8
Mo Baig <mo@mobaig.com>
2016-09-13 11:56:03 +0100

    Use expanded links to resolve the grandparent in SpecialistTagFinder
    This was identified a while ago as something to fix once the Publishing
    API's link expansion functionality was complete. We can now access an
    edition's grandparent topic without making an additional network
    request.

--- lib/specialist_tag_finder.rb
+++ lib/specialist_tag_finder.rb
@@ -26,19 +26,13 @@ class SpecialistTagFinder
     # tagged with a parent - this is always one of the 2nd level topics.  The
     # top level topic (i.e. - the parent of the edition's parent) is required
     # in the frontend when rendering an Edition's breadcrumb.
-
     @top_level_topic ||= begin
       return unless edition_content_item
-      parents = Array(edition_content_item.links["parent"])
-      return unless parents.any?
+      parent = Array(edition_content_item.links["parent"])
+      return unless parent.any?
 
-      # FIXME: We now need to fetch the parent topic from the content store to
-      # retrieve its parent. We should replace this implementation with the
-      # publishing API's links expansion / dependency resolution, which is
-      # currently WIP.
-      parent_path = parents.first["base_path"]
-      parent_content_item = Whitehall.content_store.content_item(parent_path)
-      Array(parent_content_item.links["parent"]).first
+      parent_links = parent.first["links"]
+      Array(parent_links["parent"]).first
     end
   end
 
