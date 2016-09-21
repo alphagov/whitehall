422f2b22311811e56ea025dcc5d392fc8caadc1a
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-09-21 15:45:11 +0000

    Fetch related mainstream title from content_store
    The view still needs to be able to get the related mainstream title
    so that pieces of related mainstream can still display before migration
    
    I realise this is not an ideal solution, but detailed guides are ready to be
    migrated so this code will only be used for a short time.

--- app/presenters/detailed_guide_presenter.rb
+++ app/presenters/detailed_guide_presenter.rb
@@ -1,6 +1,25 @@
 class DetailedGuidePresenter < Whitehall::Decorators::Decorator
- include EditionPresenterHelper
+  include EditionPresenterHelper
 
- delegate_instance_methods_of DetailedGuide
+  delegate_instance_methods_of DetailedGuide
 
+  def related_mainstream_content_title
+    @related_mainstream_content_title ||= begin
+      content_id = self.model.related_mainstream_content_ids.first
+      item = Whitehall.publishing_api_v2_client.get_content!(content_id)
+      item.title
+    rescue GdsApi::TimedOutException
+      ""
+    end
+  end
+
+  def additional_related_mainstream_content_title
+    @additional_related_mainstream_content_title ||= begin
+      content_id = self.model.related_mainstream_content_ids[1] if self.model.related_mainstream_content_ids[1]
+      item = Whitehall.publishing_api_v2_client.get_content!(content_id)
+      item.title
+    rescue GdsApi::TimedOutException
+      ""
+    end
+  end
 end
