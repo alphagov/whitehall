48d7877ffc1812f4b868e3844949c94743a92eee
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-09-21 14:18:58 +0000

    Refactor Fatality Notice presenter test
    Simplify test setup

--- test/unit/presenters/publishing_api/fatality_notice_presenter_test.rb
+++ test/unit/presenters/publishing_api/fatality_notice_presenter_test.rb
@@ -1,30 +1,29 @@
 require 'test_helper'
 
 class PublishingApi::FatalityNoticePresenterTest < ActiveSupport::TestCase
-  def present(edition)
-    PublishingApi::FatalityNoticePresenter.new(edition)
+  setup do
+    fatality_notice = build(
+      :fatality_notice,
+      title: "Fatality Notice title",
+      summary: "Fatality Notice summary"
+    )
+
+    @presented_fatality_notice = PublishingApi::FatalityNoticePresenter.new(fatality_notice)
   end
 
-  def build_fatality_notice(arguments)
-    build(:fatality_notice, arguments)
-  end
+  test "it delegates the content id" do
+    presented_fatality_notice = PublishingApi::FatalityNoticePresenter.new(
+      stub(content_id: "c07da942-7e01-4809-adda-cc31df098e5b")
+    )
 
-  def build_presenter(arguments)
-    present(build_fatality_notice(arguments))
-  end
-
-  test "it presents the content id" do
-    presented_fatality_notice = present(stub(content_id: "c07da942-7e01-4809-adda-cc31df098e5b"))
     assert_equal "c07da942-7e01-4809-adda-cc31df098e5b", presented_fatality_notice.content_id
   end
 
   test "it presents the title" do
-    presented_fatality_notice = build_presenter(title: "Fatality Notice title")
-    assert_equal "Fatality Notice title", presented_fatality_notice.content[:title]
+    assert_equal "Fatality Notice title", @presented_fatality_notice.content[:title]
   end
 
   test "it presents the summary as the description" do
-    presented_fatality_notice = build_presenter(summary: "Fatality Notice summary")
-    assert_equal "Fatality Notice summary", presented_fatality_notice.content[:description]
+    assert_equal "Fatality Notice summary", @presented_fatality_notice.content[:description]
   end
 end
