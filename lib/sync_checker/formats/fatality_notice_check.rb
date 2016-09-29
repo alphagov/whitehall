d831ef83dafcc5f1be35cf4da9a59fcd58f8bd6d
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2016-09-29 11:37:12 +0000

    Add sync check for Fatality Notices

---
+++ lib/sync_checker/formats/fatality_notice_check.rb
@@ -0,0 +1,20 @@
+module SyncChecker
+  module Formats
+    class FatalityNoticeCheck < EditionBase
+      def root_path
+        "/government/fatalities/"
+      end
+
+      def rendering_app
+        Whitehall::RenderingApp::WHITEHALL_FRONTEND
+      end
+
+      def checks_for_live(locale)
+        super << Checks::LinksCheck.new(
+          "field_of_operation",
+          [edition_expected_in_live.operational_field.content_id]
+        )
+      end
+    end
+  end
+end
