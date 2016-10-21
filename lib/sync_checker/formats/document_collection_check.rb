a01c5a6ff60593a79376468e052fdfb34828e299
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2016-10-21 10:17:54 +0000

    Add initial DocumentCollection sync check

---
+++ lib/sync_checker/formats/document_collection_check.rb
@@ -0,0 +1,45 @@
+module SyncChecker
+  module Formats
+    class DocumentCollectionCheck < EditionBase
+      def root_path
+        "/government/collections/"
+      end
+
+      def rendering_app
+        Whitehall::RenderingApp::WHITEHALL_FRONTEND
+      end
+
+      def checks_for_live(locale)
+        super
+      end
+
+      def expected_details_hash(edition)
+        super.merge(
+          collection_groups: collection_groups(edition)
+        )
+      end
+
+    private
+
+      def top_level_fields_hash(edition, locale)
+        super.merge(
+          { first_published_at: edition.first_published_at }
+        )
+      end
+
+      def collection_groups(edition)
+        edition.groups.map do |group|
+          {
+            title: group.heading,
+            body: govspeak_renderer.govspeak_to_html(group.body),
+            documents: group.documents.collect(&:content_id)
+          }.stringify_keys
+        end
+      end
+
+      def govspeak_renderer
+        @govspeak_renderer ||= Whitehall::GovspeakRenderer.new
+      end
+    end
+  end
+end
