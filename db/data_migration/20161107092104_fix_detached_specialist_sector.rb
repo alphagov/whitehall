de06a38f585e7bdd397e7c35f590b9a17a59f567
Andrew Garner <andrew@andrewgarner.com>
2016-11-08 10:06:01 +0000

    Ensure all Specialist Sectors have a `content_id`
    This migration ensures that all Specialist Sectors have a reference to the
    `content_id` of the associated topic. These associations should already
    exists but somehow have become out sync.
    
    A `content_id` is required so that we can present the topics to the
    Publishing API.

---
+++ db/data_migration/20161107092104_fix_detached_specialist_sector.rb
@@ -0,0 +1,23 @@
+linkable_topics = LinkableTopics.new
+
+linkable_topic_items = linkable_topics.send(:fetch_topics_from_publishing_api)
+linkable_topic_items = linkable_topics.send(:change_separator, linkable_topic_items)
+linkable_topic_items = linkable_topics.send(:select_only_subtopics, linkable_topic_items)
+
+specialist_sectors = SpecialistSector.where(topic_content_id: nil)
+
+puts 'Found %{count} Specialist Sectors with a missing `topic_content_id`' %
+       { count: specialist_sectors.count }
+
+specialist_sectors.each do |specialist_sector|
+  puts '- Processing %{specialist_sector}' % { specialist_sector: specialist_sector.inspect }
+
+  if (topic = linkable_topic_items.find { |item| item['base_path'] == "/topic/#{specialist_sector['tag']}" })
+    puts '  Found topic %{topic}' % { topic: topic.inspect }
+    puts '  %{status} `content_id` %{content_id}' %
+           { content_id: specialist_sector.topic_content_id = topic['content_id'],
+             status: specialist_sector.save ? 'Assigned' : 'Failed to assign' }
+  else
+    puts '  No topic found'
+  end
+end
