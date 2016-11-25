1e95223607b47913d414068ff7d426b2dfaff8dd
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-11-25 13:44:03 +0000

    Configure gds-sso with oauth env vars

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = 'abcdefghjasndjkasndwhitehall'
-  config.oauth_secret = 'secret'
+  config.oauth_id     = ENV.fetch("OAUTH_ID", "abcdefghjasndjkasndwhitehall")
+  config.oauth_secret = ENV.fetch("OAUTH_SECRET", "secret")
   config.oauth_root_url = Plek.find("signon")
 end
