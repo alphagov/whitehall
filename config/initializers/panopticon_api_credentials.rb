b40e492bd27e83350c2ca1982d5cca0e3ba21bc6
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-11-25 13:47:05 +0000

    Configure Panopticon API using env var

--- config/initializers/panopticon_api_credentials.rb
+++ config/initializers/panopticon_api_credentials.rb
@@ -1,5 +1,5 @@
 # This file is overridden on deploy
 
 PANOPTICON_API_CREDENTIALS = {
-  bearer_token: "developmentapicredentials",
+  bearer_token: ENV.fetch("PANOPTICON_BEARER_TOKEN", "developmentapicredentials"),
 }
