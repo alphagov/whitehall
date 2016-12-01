dd24dd8f4c4a188176a106ead3b8fd180ceb84db
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-12-01 10:57:54 +0000

    remove unused method from ClassificationRouteHelper
    - classification_path

--- app/helpers/classification_routes_helper.rb
+++ app/helpers/classification_routes_helper.rb
@@ -1,8 +1,4 @@
 module ClassificationRoutesHelper
-  def classification_path(classification, options = {})
-    polymorphic_path(classification_model_name(classification), options.merge(id: classification))
-  end
-
   def classification_url(classification, options = {})
     polymorphic_url(classification_model_name(classification), options.merge(id: classification))
   end
