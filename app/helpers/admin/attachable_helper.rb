419b183c69bfa780c1f7884f1d45bbe530b0e427
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-12-01 09:41:08 +0000

    Remove replacement_attachment_data_fields
    This method is not being in used.

--- app/helpers/admin/attachable_helper.rb
+++ app/helpers/admin/attachable_helper.rb
@@ -29,23 +29,6 @@ module Admin::AttachableHelper
     ].join.html_safe
   end
 
-  def replacement_attachment_data_fields(fields)
-    return if fields.object.new_record?
-    fields.fields_for(:attachment_data, include_id: false) do |attachment_data_fields|
-      contents = [
-        attachment_data_fields.hidden_field(:to_replace_id, value: attachment_data_fields.object.to_replace_id || attachment_data_fields.object.id),
-        attachment_data_fields.label(:file, 'Replacement'),
-        attachment_data_fields.file_field(:file)
-      ]
-      if attachment_data_fields.object.file_cache.present?
-        text = "#{File.basename(attachment_data_fields.object.file_cache)} already uploaded as replacement"
-        contents << content_tag(:span, text, class: 'already_uploaded')
-      end
-      contents << attachment_data_fields.hidden_field(:file_cache)
-      contents.join.html_safe
-    end
-  end
-
   def consultation_response_form_data_fields(response_form_fields)
     object = response_form_fields.object.consultation_response_form_data
     if object.nil? && !response_form_fields.object.persisted?
