3cbe1c87aa0f890368d433dfd5aa9a174ad3af77
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-12-01 10:57:21 +0000

    Configure Rack::Utils.key_space_limit admin initializer

---
+++ config/initializers/rack_initializer.rb
@@ -0,0 +1,8 @@
+# This initializer is only necessary for the admin configuration of Whitehall.
+# Rack::Utils.key_space_limit is the maximum number of form parameters to parse.
+if ENV.has_key?("KEY_SPACE_LIMIT")
+  # http://stackoverflow.com/a/9123664/61435
+  if Rack::Utils.respond_to?("key_space_limit=")
+    Rack::Utils.key_space_limit = ENV.fetch("KEY_SPACE_LIMIT")
+  end
+end
