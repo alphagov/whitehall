6edcbf191b0fd4485f6e932d9b52641421487d0d
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2016-12-12 14:57:04 +0000

    Republish Statistical Data Sets

--- test/unit/presenters/publishing_api_presenters_test.rb
+++ test/unit/presenters/publishing_api_presenters_test.rb
@@ -44,9 +44,6 @@ class PublishingApiPresentersTest < ActiveSupport::TestCase
 
     assert_equal PublishingApi::GenericEditionPresenter,
       PublishingApiPresenters.presenter_for(CorporateInformationPage.new).class
-
-    assert_equal PublishingApi::GenericEditionPresenter,
-      PublishingApiPresenters.presenter_for(Consultation.new).class
   end
 
   test ".presenter_for returns a Placeholder presenter for an organisation" do
