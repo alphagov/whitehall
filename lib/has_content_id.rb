4ccd34cb858f01133019af15c27d42d6871c24ed
Graham Pengelly <graham@three-tier.com>
2017-01-04 13:43:43 +0000

    Refactor `HasContentId` module
    Moved the creation of the `uuid` into a method that can be overridden in
    classes that include the module. HtmlAttachments need to be able to set
    the content id to that of a previously deleted instance if the titles
    match so this will allow the method to be overriden in that class.

--- lib/has_content_id.rb
+++ lib/has_content_id.rb
@@ -3,8 +3,12 @@ module HasContentId
 
   included do
     before_validation do
-      self.content_id ||= SecureRandom.uuid
+      self.content_id ||= generate_content_id
     end
     validates :content_id, presence: true, format: { with: /[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/ }
+  end
+
+  def generate_content_id
+    SecureRandom.uuid
   end
 end
