8a11521a05b258539e7e7341c7a457e07928a29b
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-01-10 11:43:25 +0000

    Remove non-existent stats datasets

---
+++ db/data_migration/20170109152833_remove_non_existent_stats_datasets.rb
@@ -0,0 +1,10 @@
+# Documents with non existent statistical_data_sets
+doc_ids = [53956,64993,65089,72821]
+non_existent_data_sets = Document.where(id: [71831, 71833])
+
+doc_ids.each do |doc_id|
+  d = Document.find(doc_id)
+  e = d.published_edition
+  e.statistical_data_sets = (e.statistical_data_sets - non_existent_data_sets)
+  e.save!
+end
