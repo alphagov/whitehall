89c018fed8017711962c2b44b82ccc9e0883ad07
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-01-31 14:17:39 +0000

    Send finders to rummager
    This commit changes the `PublishFinder` service to also send finders to rummager for indexing so that they show up in search results.

--- app/services/publish_finder.rb
+++ app/services/publish_finder.rb
@@ -10,6 +10,13 @@ class PublishFinder
   end
 
   def call
+    send_to_publishing_api
+    send_to_rummager
+  end
+
+private
+
+  def send_to_publishing_api
     Whitehall.publishing_api_v2_client.put_content(
       content_id,
       finder_content_item
@@ -17,7 +24,24 @@ class PublishFinder
     Whitehall.publishing_api_v2_client.publish(content_id, "major")
   end
 
-private
+  def send_to_rummager
+    index = Whitehall::SearchIndex.for(:government)
+    index.add(present_for_rummager)
+  end
+
+  def present_for_rummager
+    {
+      _id: finder_content_item.fetch("base_path"),
+      link: finder_content_item.fetch("base_path"),
+      format: "finder",
+      title: finder_content_item.fetch("title"),
+      description: finder_content_item.fetch("description", ""),
+      content_store_document_type: finder_content_item.fetch("document_type"),
+      content_id: content_id,
+      publishing_app: finder_content_item.fetch("publishing_app"),
+      rendering_app: finder_content_item.fetch("rendering_app"),
+    }
+  end
 
   def content_id
     @content_id ||= existing_content_id || SecureRandom.uuid
