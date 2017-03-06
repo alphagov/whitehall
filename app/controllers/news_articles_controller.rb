d9042dc61d79c7cb586581cb3af9bc089290a42d
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2017-03-06 17:25:01 +0000

    Remove presenting news articles

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,17 +1,7 @@
 class NewsArticlesController < DocumentsController
   before_filter :set_analytics_format, only: [:show]
 
-  def show
-    @related_policies = document_related_policies
-    @document = NewsArticlePresenter.new(@document, view_context)
-    set_meta_description(@document.summary)
-  end
-
   private
-
-  def document_class
-    NewsArticle
-  end
 
   def analytics_format
     :news
