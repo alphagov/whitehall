2321f0b640aa52b1d020163da9f61d437fe254d7
Matteo Grassotti <matteo.grassotti@gmail.com>
2017-03-14 12:00:37 +0000

    Remove the tax year (2015-2016) from the slug

---
+++ db/data_migration/20170310085202_reslug_flexibly_accessed_pension.rb
@@ -0,0 +1,15 @@
+require 'gds_api/router'
+
+router = GdsApi::Router.new(Plek.find('router-api'))
+
+old_slug = "flexibly-accessed-pension-payment-repayment-claim-tax-year-2015-2016-p55" 
+new_slug = "flexibly-accessed-pension-payment-repayment-claim-tax-year-p55"
+
+document = Document.find_by(slug: old_slug)
+if document
+  puts "Changing document slug #{old_slug} -> #{new_slug}"
+  document.update_attribute(:slug, new_slug)
+  PublishingApiDocumentRepublishingWorker.perform_async(document.id)
+else
+  puts "Can't find document with slug of #{old_slug}"
+end
