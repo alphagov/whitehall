3cfbfc7aa9c1791eb6c47cc302060db00f7331ad
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-03-29 13:12:50 +0000

    Refactor presenter test
    There's no need to persist the underlying models in
    order to test a presenter so use FactoryGirl#build
    and provide any additional necessary attributes.
    Whitehall tests are slow enough already

--- test/unit/presenters/publishing_api/fatality_notice_presenter_test.rb
+++ test/unit/presenters/publishing_api/fatality_notice_presenter_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class PublishingApi::FatalityNoticePresenterTest < ActiveSupport::TestCase
   setup do
-    @fatality_notice = create(
+    @fatality_notice = build(
       :fatality_notice,
+      document: build(:document, id: 12345, slug: "fatality-notice-title"),
       title: "Fatality Notice title",
       summary: "Fatality Notice summary",
-      first_published_at: @first_published_at = Time.zone.now
+      first_published_at: @first_published_at = Time.zone.now,
+      updated_at: 1.year.ago,
     )
 
     @presented_fatality_notice = PublishingApi::FatalityNoticePresenter.new(@fatality_notice)
