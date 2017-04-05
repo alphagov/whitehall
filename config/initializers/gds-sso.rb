e0d7e2539ea02e2cf5629845fbca29c420f66939
Ruben Arakelyan <ruben@ra.me.uk>
2017-04-05 16:42:39 +0100

    Change how OAuth ID and secret are set
    This commit changes how the OAuth ID and secret (and their fallbacks) are set so that it is consistent with other apps, and to allow the signon-munging script for development to configure signon for this app locally.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = "User"
-  config.oauth_id     = ENV.fetch("OAUTH_ID", "abcdefghjasndjkasndwhitehall")
-  config.oauth_secret = ENV.fetch("OAUTH_SECRET", "secret")
+  config.oauth_id     = ENV['OAUTH_ID'] || "abcdefghjasndjkasndwhitehall"
+  config.oauth_secret = ENV['OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.find("signon")
 end
