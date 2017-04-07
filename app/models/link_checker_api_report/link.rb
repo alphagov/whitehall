ecb02fa491a9eb68949a82f99cace71afe14bef0
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-07 20:21:14 +0100

    Create/update models from Link Checker API payloads
    This takes the batch report payloads from Link Checker API and can
    update/create the models stored in the Whitehall database.

--- app/models/link_checker_api_report/link.rb
+++ app/models/link_checker_api_report/link.rb
@@ -3,4 +3,14 @@ class LinkCheckerApiReport::Link < ActiveRecord::Base
   serialize :check_warnings, Hash
 
   belongs_to :report, class_name: LinkCheckerApiReport
+
+  def self.attributes_from_link_report(payload)
+    {
+      uri: payload.fetch("uri"),
+      status: payload.fetch("status"),
+      checked: payload.fetch("checked"),
+      check_warnings: payload.fetch("warnings", {}),
+      check_errors: payload.fetch("errors", {}),
+    }
+  end
 end
