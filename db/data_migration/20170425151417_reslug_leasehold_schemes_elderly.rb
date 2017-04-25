ceb6d7e012aa1efa9d207a7c674e959a5bf72c48
Chris Carter <chris.carter1@ntlworld.com>
2017-04-25 16:19:42 +0100

    Reslug Leasehold Schemes for the Elderly URL

---
+++ db/data_migration/20170425151417_reslug_leasehold_schemes_elderly.rb
@@ -0,0 +1,3 @@
+document = Document.find_by(slug: "limit-on-the-management-element-of-the-lse-service-charge-for-2016-to-2017")
+
+document.update_attributes(slug: "leasehold-schemes-for-the-elderly-management-fee-limit")
