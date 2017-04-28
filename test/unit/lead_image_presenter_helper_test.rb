748c951108f9f25c7032f838185ee2ddc00cd8b4
Graham Pengelly <graham@three-tier.com>
2017-04-28 11:28:24 +0100

    Use unversioned URL if lead image is an SVG
    SVG images may be used as a lead image if they are the first image in
    some formats. This commit ensures that the generated URL is unversioned
    as uploaded SVGs are not resized on upload and don't have versions.

--- test/unit/lead_image_presenter_helper_test.rb
+++ test/unit/lead_image_presenter_helper_test.rb
@@ -9,8 +9,23 @@ class LeadImagePresenterHelperTest < ActiveSupport::TestCase
 
   test "should use first image with version :s300 if an image is present" do
     presenter = stub("Target", images: [], lead_organisations: [], organisations: []).extend(LeadImagePresenterHelper)
-    image = stub("Image", alt_text: "alt_text")
-    image.expects(:url).with(:s300).returns("url")
+    file = stub("File", content_type: "image/jpg")
+    uploader = stub("Uploader", file: file)
+    image_data = stub("ImageData", file: uploader)
+    image = stub("Image", alt_text: "alt_text", image_data: image_data)
+    uploader.expects(:url).with(:s300).returns("url")
+    presenter.stubs(images: [image])
+    assert_equal 'url', presenter.lead_image_path
+    assert_equal 'alt_text', presenter.lead_image_alt_text
+  end
+
+  test "uses unversioned url if the image is an SVG" do
+    presenter = stub("Target", images: [], lead_organisations: [], organisations: []).extend(LeadImagePresenterHelper)
+    file = stub("File", content_type: "image/svg+xml")
+    uploader = stub("Uploader", file: file)
+    image_data = stub("ImageData", file: uploader)
+    image = stub("Image", alt_text: "alt_text", image_data: image_data)
+    uploader.expects(:url).with.returns("url")
     presenter.stubs(images: [image])
     assert_equal 'url', presenter.lead_image_path
     assert_equal 'alt_text', presenter.lead_image_alt_text
