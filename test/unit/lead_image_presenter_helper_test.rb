5cd3ee0488154e41f28500b7aba2bd31333f8962
Graham Pengelly <graham@three-tier.com>
2017-04-28 11:07:38 +0100

    Actually test lead_image_url
    The existing test didn't actually test that the correct version was
    being requested. This commit updates the test to test that the `s300`
    version is requested specifically.

--- test/unit/lead_image_presenter_helper_test.rb
+++ test/unit/lead_image_presenter_helper_test.rb
@@ -9,7 +9,8 @@ class LeadImagePresenterHelperTest < ActiveSupport::TestCase
 
   test "should use first image with version :s300 if an image is present" do
     presenter = stub("Target", images: [], lead_organisations: [], organisations: []).extend(LeadImagePresenterHelper)
-    image = stub("Image", url: "url", alt_text: "alt_text")
+    image = stub("Image", alt_text: "alt_text")
+    image.expects(:url).with(:s300).returns("url")
     presenter.stubs(images: [image])
     assert_equal 'url', presenter.lead_image_path
     assert_equal 'alt_text', presenter.lead_image_alt_text
