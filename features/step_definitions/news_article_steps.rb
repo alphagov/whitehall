2a3101b349cbef94f0302a2e5d2cb03a0d15245c
David Basalla <davidbasalla@gmail.com>
2017-05-11 14:56:58 +0100

    Add brackets to news_article_step definitions
    To make it easier for code editors to do syntax highlighting, my Sublime was getting confused by single quotes in the regex text.

--- features/step_definitions/news_article_steps.rb
+++ features/step_definitions/news_article_steps.rb
@@ -1,20 +1,20 @@
-Given /^a published news article "([^"]*)" with related published policies "([^"]*)" and "([^"]*)"$/ do |news_article_title, policy_title_1, policy_title_2|
+Given(/^a published news article "([^"]*)" with related published policies "([^"]*)" and "([^"]*)"$/) do |news_article_title, policy_title_1, policy_title_2|
   policies = publishing_api_has_policies([policy_title_1, policy_title_2])
 
   create(:published_news_article, title: news_article_title, policy_content_ids: policies.map {|p| p['content_id']})
 end
 
-Given /^a published news article "([^"]*)" associated with "([^"]*)"$/ do |title, appointee|
+Given(/^a published news article "([^"]*)" associated with "([^"]*)"$/) do |title, appointee|
   person = find_person(appointee)
   appointment = find_person(appointee).current_role_appointments.last
   create(:published_news_article, title: title, role_appointments: [appointment])
 end
 
-Given /^a published news article "([^"]*)" which isn't explicitly associated with "([^"]*)"$/ do |title, thing|
+Given(/^a published news article "([^"]*)" which isn't explicitly associated with "([^"]*)"$/) do |title, thing|
   create(:published_news_article, title: title)
 end
 
-When /^I draft a new news article "([^"]*)"$/ do |title|
+When(/^I draft a new news article "([^"]*)"$/) do |title|
   begin_drafting_news_article title: title, summary: "here's a simple summary"
   within ".images" do
     attach_file "File", jpg_image, match: :first
@@ -23,7 +23,7 @@ When /^I draft a new news article "([^"]*)"$/ do |title|
   click_button "Save"
 end
 
-When /^I draft a new news article "([^"]*)" relating it to the policies "([^"]*)" and "([^"]*)"$/ do |title, first_policy, second_policy|
+When(/^I draft a new news article "([^"]*)" relating it to the policies "([^"]*)" and "([^"]*)"$/) do |title, first_policy, second_policy|
   publishing_api_has_policies([first_policy, second_policy])
 
   begin_drafting_news_article title: title
@@ -32,7 +32,7 @@ When /^I draft a new news article "([^"]*)" relating it to the policies "([^"]*)
   click_button "Save"
 end
 
-When /^I publish a news article "([^"]*)" associated with "([^"]*)"$/ do |title, person_name|
+When(/^I publish a news article "([^"]*)" associated with "([^"]*)"$/) do |title, person_name|
   begin_drafting_news_article title: title
   fill_in_news_article_fields(first_published: Date.today.to_s)
   select person_name, from: "Ministers"
@@ -40,7 +40,7 @@ When /^I publish a news article "([^"]*)" associated with "([^"]*)"$/ do |title,
   publish(force: true)
 end
 
-When /^I publish a news article "([^"]*)" associated with the (topic|topical event) "([^"]*)"$/ do |title, type, topic_name|
+When(/^I publish a news article "([^"]*)" associated with the (topic|topical event) "([^"]*)"$/) do |title, type, topic_name|
   begin_drafting_news_article title: title, skip_topic_selection: (type == 'topic')
 
   if type == 'topic'
@@ -64,33 +64,33 @@ When(/^I publish a news article "(.*?)" associated with the organisation "(.*?)"
   publish(force: true)
 end
 
-When /^I attempt to add the article image into the markdown$/ do
+When(/^I attempt to add the article image into the markdown$/) do
   fill_in "Body", with: "body copy\n!!1\nmore body"
 end
 
-Then /^the news article tag is the same as the person in the text$/ do
+Then(/^the news article tag is the same as the person in the text$/) do
   visit admin_edition_path(NewsArticle.last)
   click_button "Create new edition"
   appointment = NewsArticle.last.role_appointments.first
   assert has_css?("select#edition_role_appointment_ids option[value='#{appointment.id}'][selected=selected]")
 end
 
-Then /^I should see both the news articles for the Deputy Prime Minister role$/ do
+Then(/^I should see both the news articles for the Deputy Prime Minister role$/) do
   assert has_css?(".news_article", text: "News from Don, Deputy PM")
   assert has_css?(".news_article", text: "News from Harriet, Deputy PM")
 end
 
-Then /^I should see both the news articles for Harriet Home$/ do
+Then(/^I should see both the news articles for Harriet Home$/) do
   assert has_css?(".news_article", text: "News from Harriet, Deputy PM")
   assert has_css?(".news_article", text: "News from Harriet, Home Sec")
 end
 
-Then /^I should be informed I shouldn't use this image in the markdown$/ do
+Then(/^I should be informed I shouldn't use this image in the markdown$/) do
   click_on "Edit draft"
   assert has_no_css?("fieldset#image_fields .image input[value='!!1']")
 end
 
-When /^I browse to the announcements index$/ do
+When(/^I browse to the announcements index$/) do
   visit announcements_path
 end
 
@@ -114,7 +114,7 @@ When(/^I draft a French\-only news article of type "([^"]*)" associated with "([
   @news_article = find_news_article_in_locale!(:fr, 'French-only news article')
 end
 
-When /^I publish the French-only news article$/ do
+When(/^I publish the French-only news article$/) do
   visit admin_edition_path(@news_article)
   publish(force: true)
 end
@@ -126,12 +126,12 @@ When(/^I publish a news article "(.*?)" for "(.*?)"$/) do |title, location_name|
   publish(force: true)
 end
 
-Then /^I should see the news article listed in admin with an indication that it is in French$/ do
+Then(/^I should see the news article listed in admin with an indication that it is in French$/) do
   assert_path admin_edition_path(@news_article)
   assert page.has_content?("This document is French-only")
 end
 
-Then /^I should only see the news article on the French version of the public "([^"]*)" location page$/ do |world_location_name|
+Then(/^I should only see the news article on the French version of the public "([^"]*)" location page$/) do |world_location_name|
   world_location = WorldLocation.find_by!(name: world_location_name)
   visit world_location_path(world_location, locale: :fr)
   within record_css_selector(@news_article) do
