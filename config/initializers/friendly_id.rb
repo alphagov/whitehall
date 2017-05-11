f04e750392d4f9389a1b2518cd091c5c0e733566
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-05-11 14:24:13 +0100

    Use friendly_id built-in sequential slugger
    This commit changes the `friendly_id` initializer to use the built-in sequential slugger, which was re-added in version 5.2.0, rather than the whitehall monkey-patch. It keep the custom normaliser method.

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -1,6 +1,6 @@
 FriendlyId.defaults do |config|
   config.base = :name
-  config.use :slugged, :finders, FriendlyId::SequentialSlugs
+  config.use :slugged, :finders, :sequentially_slugged, FriendlyId::CustomNormalise
 
   config.sequence_separator = '--'
 end
