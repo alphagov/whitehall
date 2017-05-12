7bcaaa11ab9b724cfc0ef77f2258d708230e497a
David Basalla <davidbasalla@gmail.com>
2017-05-12 15:01:53 +0000

    Refactor admin edition form js test
    One of the problems with the fixture in this file is that it’s very generic and does not represent any specific page in Whitehall. We attempt to split up the fixture into separate parts so that the tests can load only the HTML fixtures that they are testing and to make the fixture more adaptable and configurable. This is in preparation for adding tests specific to News Articles.

--- test/javascripts/unit/admin/views/editions/_form_test.js
+++ test/javascripts/unit/admin/views/editions/_form_test.js
@@ -1,77 +1,86 @@
-module("admin-edition-form: ", {
-  setup: function() {
-    $('#qunit-fixture').append(
-      '<form id="non-english" class="js-supports-non-english">' +
-        '<fieldset>' +
-          '<div class="checkbox">' +
-            '<label class="checkbox" for="create_foreign_language_only">' +
-              '<input type="checkbox" name="create_foreign_language_only" id="create_foreign_language_only" value="1" /> Create a foreign language only news article' +
-            '</label>' +
-          '</div>' +
-          '<div class="form-group foreign-language-select js-hidden">' +
-            '<label for="edition_primary_locale">Document language</label>' +
-            '<div class="form-inline add-label-margin">' +
-              '<select class="form-control input-md-6" name="edition[primary_locale]" id="edition_primary_locale">' +
-                '<option value="">Choose foreign language...</option>' +
-                '<option value="ar">العربية (Arabic)</option>' +
-                '<option value="cy">Cymraeg (Welsh)</option>' +
-              '</select>' +
-            '</div>' +
-            '<p class="warning">Warning: Foreign language only documents do not allow additional translations.</p>' +
-          '</div>' +
-        '</fieldset>' +
-        '<fieldset>' +
-          '<label for="edition_title">Title</label>' +
-          '<input id="edition_title" name="edition[title]" size="30" type="text" />' +
-        '</fieldset>' +
-        '<fieldset class="first-published-date well">' +
-          '<p class="required">This document <span>*</span></p>' +
-          '<label class="radio" for="edition_previously_published_false">' +
-            '<input id="edition_previously_published_false" name="edition[previously_published]" type="radio" value="true">' +
-            'has never been published before. It is new.' +
-        '</label>' +
-          '<label class="radio" for="edition_previously_published_true">' +
-            '<input id="edition_previously_published_true" name="edition[previously_published]" type="radio" value="false">' +
-            'has previously been published on another website.' +
-        '</label>' +
-          '<div class="js-show-first-published" style="display: none;">' +
-              '<label class="required extra-label" for="edition_first_published_at">Its original publication date was <span>*</span></label>' +
-            '<select class="date" id="edition_first_published_at_1i" name="edition[first_published_at(1i)]">' +
-        '<option value=""></option>' +
-        '<option value="2014">2014</option>' +
-        '<option value="2013">2013</option>' +
-        '<option value="2012">2012</option>' +
+var form =
+  '<form id="non-english" class="js-supports-non-english"></form>'
+
+var foreignLanguageFieldset =
+  '<fieldset class="foreign-language">' +
+    '<div class="checkbox">' +
+      '<label class="checkbox" for="create_foreign_language_only">' +
+        '<input type="checkbox" name="create_foreign_language_only" id="create_foreign_language_only" value="1" /> Create a foreign language only news article' +
+      '</label>' +
+    '</div>' +
+    '<div class="form-group foreign-language-select js-hidden">' +
+      '<label for="edition_primary_locale">Document language</label>' +
+      '<div class="form-inline add-label-margin">' +
+        '<select class="form-control input-md-6" name="edition[primary_locale]" id="edition_primary_locale">' +
+          '<option value="">Choose foreign language...</option>' +
+          '<option value="ar">العربية (Arabic)</option>' +
+          '<option value="cy">Cymraeg (Welsh)</option>' +
+        '</select>' +
+      '</div>' +
+      '<p class="warning">Warning: Foreign language only documents do not allow additional translations.</p>' +
+    '</div>' +
+  '</fieldset>'
+
+var titleFieldset =
+  '<fieldset>' +
+    '<label for="edition_title">Title</label>' +
+    '<input id="edition_title" name="edition[title]" size="30" type="text" />' +
+  '</fieldset>'
+
+var firstPublishedAtFieldset =
+  '<fieldset class="first-published-date well">' +
+    '<p class="required">This document <span>*</span></p>' +
+    '<label class="radio" for="edition_previously_published_false">' +
+      '<input id="edition_previously_published_false" name="edition[previously_published]" type="radio" value="true">' +
+      'has never been published before. It is new.' +
+  '</label>' +
+  '<label class="radio" for="edition_previously_published_true">' +
+    '<input id="edition_previously_published_true" name="edition[previously_published]" type="radio" value="false">' +
+    'has previously been published on another website.' +
+  '</label>' +
+    '<div class="js-show-first-published" style="display: none;">' +
+        '<label class="required extra-label" for="edition_first_published_at">Its original publication date was <span>*</span></label>' +
+        '<select class="date" id="edition_first_published_at_1i" name="edition[first_published_at(1i)]">' +
+          '<option value=""></option>' +
+          '<option value="2014">2014</option>' +
+          '<option value="2013">2013</option>' +
+          '<option value="2012">2012</option>' +
         '</select>' +
         '<select class="date" id="edition_first_published_at_2i" name="edition[first_published_at(2i)]">' +
-        '<option value=""></option>' +
-        '<option value="1">January</option>' +
-        '<option value="2">February</option>' +
-        '<option value="3">March</option>' +
+          '<option value=""></option>' +
+          '<option value="1">January</option>' +
+          '<option value="2">February</option>' +
+          '<option value="3">March</option>' +
         '</select>' +
         '<select class="date" id="edition_first_published_at_3i" name="edition[first_published_at(3i)]">' +
-        '<option value=""></option>' +
-        '<option value="1">1</option>' +
-        '<option value="2">2</option>' +
-        '<option value="3">3</option>' +
+          '<option value=""></option>' +
+          '<option value="1">1</option>' +
+          '<option value="2">2</option>' +
+          '<option value="3">3</option>' +
         '</select>' +
          '— <select class="date" id="edition_first_published_at_4i" name="edition[first_published_at(4i)]">' +
-        '<option value=""></option>' +
-        '<option value="00">00</option>' +
-        '<option value="01">01</option>' +
-        '<option value="02" selected="selected">02</option>' +
-        '<option value="03">03</option>' +
+          '<option value=""></option>' +
+          '<option value="00">00</option>' +
+          '<option value="01">01</option>' +
+          '<option value="02" selected="selected">02</option>' +
+          '<option value="03">03</option>' +
         '</select>' +
          ': <select class="date" id="edition_first_published_at_5i" name="edition[first_published_at(5i)]">' +
-        '<option value=""></option>' +
-        '<option value="00">00</option>' +
-        '<option value="01">01</option>' +
-        '<option value="02">02</option>' +
-        '<option value="03" selected="selected">03</option>' +
+          '<option value=""></option>' +
+          '<option value="00">00</option>' +
+          '<option value="01">01</option>' +
+          '<option value="02">02</option>' +
+          '<option value="03" selected="selected">03</option>' +
         '</select>' +
-            '<span class="explanation">Only complete this field if the document is not new.</span>' +
-          '</div>' +
-          '</fieldset>' +
-      '</form>');
+      '<span class="explanation">Only complete this field if the document is not new.</span>' +
+    '</div>' +
+  '</fieldset>'
+
+module("admin-edition-form-foreign-language: ", {
+  setup: function() {
+    $('#qunit-fixture').append(form)
+    $('form').append(foreignLanguageFieldset)
+    $('form').append(titleFieldset)
 
     GOVUK.adminEditionsForm.init({
       selector: 'form#non-english',
@@ -122,6 +131,19 @@ test("selecting and deselecting right-to-left languages applies the appropriate 
   ok(!$('form#non-english fieldset').hasClass('right-to-left'), 'form fieldsets no longer have "right-to-left" class');
 });
 
+module("admin-edition-form-first-published-at: ", {
+  setup: function() {
+    $('#qunit-fixture').append(form)
+    $('form').append(firstPublishedAtFieldset)
+
+    GOVUK.adminEditionsForm.init({
+      selector: 'form#non-english',
+      right_to_left_locales:["ar"]
+    });
+    $('.js-hidden').hide();
+  }
+});
+
 test("first_published time fields default to 00 if not set", function() {
   // original field values should not be changed
   equal($('#edition_first_published_at_4i').val(), '02', 'hour field has original value');
@@ -145,4 +167,3 @@ test("previously_published radio buttons toggle visibility of first_published da
   $('#edition_previously_published_false').click();
   ok($('.js-show-first-published').is(':hidden'), 'date selector hidden when "document is new" selected');
 });
-
