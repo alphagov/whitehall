67f6c88bfc222cae9aa843c2f44ddc005cd9fe7d
Graham Pengelly <graham@three-tier.com>
2017-05-31 22:30:37 +0100

    Return AB test link for WorldwideOrganisation
    For the worldwide AB test we require that the usual worldwide
    organisation path for embassies that are part of the test redirect to
    the parent location.
    
    When viewing Corporate Information Pages, the header logo is sometimes
    a hyperlink back to the Worldwide Organisation. If we are in the B
    cohort, we want this link to take us to the new (B cohort) Worldwide
    Organisation page.

---
+++ test/unit/helpers/worldwide_organisations_helper_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+class WorldwideOrganisationsHelperTest < ActionView::TestCase
+  test "path returns /government/world/organisations/<slug> for a non test org" do
+    location = create(:world_location, slug: "india")
+    org = create(
+      :worldwide_organisation,
+      slug: "none-test-slug",
+      world_locations: [
+        location
+      ]
+    )
+
+    assert_equal(
+      "/government/world/organisations/none-test-slug",
+      worldwide_organisation_path(org)
+    )
+  end
+
+  test "url returns <host>/government/world/organisations/slug for a non test org" do
+    location = create(:world_location, slug: "india")
+    org = create(
+      :worldwide_organisation,
+      slug: "none-test-slug",
+      world_locations: [
+        location
+      ]
+    )
+
+    assert_equal(
+      "http://test.host/government/world/organisations/none-test-slug",
+      worldwide_organisation_url(org)
+    )
+  end
+
+  test "link for ab test returns /government/world/<location>/<slug> for an org under A/B test" do
+    location = create(:world_location, slug: "india")
+    org = create(
+      :worldwide_organisation,
+      slug: "british-high-commission-new-delhi",
+      world_locations: [
+        location
+      ]
+    )
+
+    assert_equal(
+      "http://test.host/government/world/india/british-high-commission-new-delhi",
+      worldwide_organisation_link_for_ab_test(org)
+    )
+  end
+end
