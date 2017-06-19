ed8efdc4e7476cf7c4065632a824ab7d9a543db5
Tom Gladhill <whoojemaflip@gmail.com>
2017-06-19 14:40:31 +0100

    Keep gov.uk taxonomy class fresh and clean

--- lib/taxonomy/govuk_taxonomy.rb
+++ lib/taxonomy/govuk_taxonomy.rb
@@ -7,16 +7,13 @@ module Taxonomy
 
     def children
       @_children ||= begin
-        @adapter.published_taxon_data
-          .map { |taxon_hash| build_tree(taxon_hash) }
+        @adapter.published_taxon_data.map { |taxon_hash| build_tree(taxon_hash) }
       end
     end
 
     def draft_child_taxons
       @_draft_child_taxons ||= begin
-        @adapter.draft_taxon_data
-          .select { |taxon_hash| visible_to_departmental_editors?(taxon_hash) }
-          .map { |taxon_hash| build_tree(taxon_hash) }
+        @adapter.draft_taxon_data.map { |taxon_hash| build_tree(taxon_hash) }
       end
     end
 
@@ -26,13 +23,7 @@ module Taxonomy
 
   private
 
-    def visible_to_departmental_editors?(taxon_hash)
-      taxon_hash.fetch('details', {})['visible_to_departmental_editors']
-    end
-
     def build_tree(taxon_hash)
-      tree_data = @adapter.tree_data(taxon_hash['content_id'])
-      taxon_hash['expanded_links_hash'] = tree_data
       @tree_builder_class.new(taxon_hash).root_taxon
     end
   end
