956db0fe67a209cc1a3f9f09f4a76184d286a5ed
Chris Patuzzo <chris@patuzzo.co.uk>
2017-08-02 14:40:55 +0100

    Re-work unpublishing features after removing publications#show
    We can no longer test these changes are reflected
    on the show page, but we can still check the data
    has changed.

--- features/step_definitions/unpublishing_published_documents_steps.rb
+++ features/step_definitions/unpublishing_published_documents_steps.rb
@@ -20,40 +20,29 @@ When(/^I unpublish the duplicate, marking it as consolidated into the other page
   end
 end
 
-When(/^I withdraw the publication because it no longer reflects current government policy$/) do
+When(/^I withdraw the publication with the explanation "([^"]*)"$/) do |explanation|
   @publication = Publication.last
   visit admin_edition_path(@publication)
   click_on 'Withdraw or unpublish'
   choose 'Withdraw: no longer current government policy/activity'
-  fill_in 'Public explanation (this is shown on the live site) *', with: 'We no longer believe people should shave'
+  fill_in 'Public explanation (this is shown on the live site) *', with: explanation
   click_button 'Withdraw'
 
   assert_equal :withdrawn, @publication.reload.current_state
 end
 
-When(/^I edit the public explanation for withdrawal$/) do
+When(/^I edit the public explanation for withdrawal to "([^"]*)"$/) do |explanation|
   publication = Publication.last
   visit admin_edition_path(publication)
   click_on 'Edit withdrawal explanation'
-  fill_in 'Public explanation', with: "We believe people should shave, but the government need not enforce a publication for that"
+  fill_in 'Public explanation', with: explanation
   click_button 'Update withdrawal explanation'
 end
 
-Then(/^I should see the updated explanation on the public site$/) do
-  step %{the publication should be marked as withdrawn on the public site}
-end
-
-Then(/^the publication should be marked as withdrawn on the public site$/) do
-  publication = Publication.last
-  visit public_document_path(publication)
-  assert page.has_content?(publication.title)
-  assert page.has_content?('This publication was withdrawn')
-  assert page.has_content?(publication.unpublishing.explanation)
-end
-
-Then(/^I should be redirected to the other page when I view the document on the public site$/) do
-  visit public_document_path(@duplicate_edition)
-  assert_path publication_path(@existing_edition.document)
+Then /^the unpublishing should redirect to the existing edition$/ do
+  unpublishing = @duplicate_edition.unpublishing
+  path = publication_path(@existing_edition.document)
+  assert unpublishing.alternative_url.end_with?(path)
 end
 
 When /^I unpublish the document because it was published in error$/ do
@@ -70,33 +59,32 @@ Then(/^there should be an editorial remark recording the fact that the document 
   assert_equal 'Withdrawn', edition.editorial_remarks.last.body
 end
 
-Then /^I should see that the document was published in error on the public site$/ do
+Then /^there should be an unpublishing explanation of "([^"]*)" and a reason of "([^"]*)"$/ do |explanation, reason_name|
   edition = Edition.last
-  visit public_document_path(edition)
-  assert page.has_no_content?(edition.title)
-  assert page.has_content?('The information on this page has been removed because it was published in error')
-  assert page.has_content?('This page should never have existed')
+  unpublishing = edition.unpublishing
+
+  assert unpublishing.present?
+
+  reason = unpublishing.unpublishing_reason
+
+  assert_equal explanation, unpublishing.explanation
+  assert_equal reason_name, reason.name
 end
 
-Then /^I should see that the document was published in error at the original url$/ do
-  visit publication_path(@original_slug)
-  assert page.has_no_content?(@document.title)
-  assert page.has_content?('The information on this page has been removed because it was published in error')
-  assert page.has_content?('This page should never have existed')
-end
-
-When /^I unpublish the document and ask for a redirect$/ do
+When /^I unpublish the document and ask for a redirect to "([^"]*)"$/ do |url|
   unpublish_edition(Edition.last) do
-    fill_in 'published_in_error_alternative_url', with: Whitehall.url_maker.placeholder_url
+    fill_in 'published_in_error_alternative_url', with: url
     check 'Redirect to URL automatically?'
   end
 end
 
-Then /^I should be redirected to the new url when I view the document on the public site$/ do
+Then /^the unpublishing should redirect to "([^"]*)"$/ do |url|
   edition = Edition.last
 
-  visit public_document_path(edition)
-  assert_current_url edition.unpublishing.alternative_url
+  unpublishing = edition.unpublishing
+
+  assert unpublishing.redirect
+  assert_equal url, unpublishing.alternative_url
 end
 
 Then /^I should not be able to discard the draft resulting from the unpublishing$/ do
--- features/unpublishing-published-documents.feature
+++ features/unpublishing-published-documents.feature
@@ -9,14 +9,7 @@ Feature: Unpublishing published documents
     And a published document "Published by accident" exists
     When I unpublish the document because it was published in error
     Then there should be an editorial remark recording the fact that the document was unpublished
-    And I should see that the document was published in error on the public site
-    And the publication "Published by accident" should no longer be listed on the public site
-
-  Scenario: Unpublishing a document that has had a title change
-    Given I am a managing editor
-    And a published document exists with a slug that does not match the title
-    When I unpublish the document because it was published in error
-    Then I should see that the document was published in error at the original url
+    And there should be an unpublishing explanation of "This page should never have existed" and a reason of "Published in error"
 
   Scenario: Draft resulting from an unpublishing should not be deletable
     Given I am a managing editor
@@ -27,25 +20,24 @@ Feature: Unpublishing published documents
   Scenario: Unpublishing a document and redirecting
     Given I am a managing editor
     And a published document "Published by accident" exists
-    When I unpublish the document and ask for a redirect
-    Then I should be redirected to the new url when I view the document on the public site
+    When I unpublish the document and ask for a redirect to "https://www.test.gov.uk/example"
+    Then the unpublishing should redirect to "https://www.test.gov.uk/example"
 
   Scenario: Consolidating a document into another GOV.UK page
     Given I am a managing editor
     And there is a published document that is a duplicate of another page
     When I unpublish the duplicate, marking it as consolidated into the other page
-    Then I should be redirected to the other page when I view the document on the public site
+    Then the unpublishing should redirect to the existing edition
 
   Scenario: Withdraw a document that is no longer current
     Given I am a managing editor
     And a published publication "Shaving kits for all" exists
-    When I withdraw the publication because it no longer reflects current government policy
-    Then there should be an editorial remark recording the fact that the document was withdrawn
-    And the publication should be marked as withdrawn on the public site
+    When I withdraw the publication with the explanation "Policy change"
+    Then there should be an unpublishing explanation of "Policy change" and a reason of "No longer current government policy/activity"
 
   Scenario: Change the public explanation for archiving a document
     Given I am a managing editor
     And a published publication "Shaving kits for all" exists
-    And I withdraw the publication because it no longer reflects current government policy
-    When I edit the public explanation for withdrawal
-    Then I should see the updated explanation on the public site
+    When I withdraw the publication with the explanation "Policy change"
+    When I edit the public explanation for withdrawal to "The policy has changed"
+    Then there should be an unpublishing explanation of "The policy has changed" and a reason of "No longer current government policy/activity"
