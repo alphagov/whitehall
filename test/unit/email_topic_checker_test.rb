e8a3211c21c63a0bf56d32164a7a9c08feaf7da7
Chris Patuzzo <chris@patuzzo.co.uk>
2017-08-08 10:59:07 +0100

    Pass ‘supertypes’ to /topic-matches endpoint
    The SubscriberListQuery in email-alert-api can
    return additional topics based on the supertypes
    of the edition. We want to make sure we provide
    this endpoint with these supertypes so we get an
    accurate view of which topics are matched.
    
    This commit also includes additional output to
    show which requests are made to email-alert-api
    and govuk-delivery.

--- test/unit/email_topic_checker_test.rb
+++ test/unit/email_topic_checker_test.rb
@@ -8,6 +8,9 @@ class EmailTopicCheckerTest < ActiveSupport::TestCase
     mock_client = mock("EmailAlertApi", topic_matches: { "topics" => ["TOPIC_456"] })
     EmailTopicChecker.any_instance.stubs(email_alert_api: mock_client)
 
+    content_item = { "email_document_supertype" => "foo", "government_document_supertype" => "bar" }
+    Whitehall.content_store.stubs(content_item: content_item)
+
     edition = create(:edition, :published)
     @content_id = edition.document.content_id
   end
@@ -32,6 +35,27 @@ class EmailTopicCheckerTest < ActiveSupport::TestCase
 
   test "lists additional email-alert-api topics" do
     assert_output(/additional email-alert-api topics:\nTOPIC_456/m) do
+      EmailTopicChecker.check(@content_id)
+    end
+  end
+
+  test "lists email-alert-api params" do
+    expected = {
+      links: {},
+      tags: {},
+      document_type: "generic_edition",
+      email_document_supertype: "foo",
+      government_document_supertype: "bar",
+    }
+
+    assert_output(/^email-alert-api params:\n#{expected.to_s}/m) do
+      EmailTopicChecker.check(@content_id)
+    end
+  end
+
+  test "lists govuk-delivery feed urls" do
+    expected = "https://www.test.gov.uk/government/feed"
+    assert_output(/^govuk-delivery feed urls:\n#{expected}/m) do
       EmailTopicChecker.check(@content_id)
     end
   end
