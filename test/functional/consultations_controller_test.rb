8a2ced70bb8b197a1e2b3322bfc9719fbfd2d584
Graham Pengelly <graham@three-tier.com>
2017-08-15 14:29:57 +0100

    Fix consultations  controller test
    Was failing due to differences in the url encoding of `[]`. This commit
    adds a literal string expectation instead of using the path helper.

--- test/functional/consultations_controller_test.rb
+++ test/functional/consultations_controller_test.rb
@@ -5,6 +5,8 @@ class ConsultationsControllerTest < ActionController::TestCase
 
   test 'index redirects to the publications index filtering consultations, retaining any other filter params' do
     get :index, params: { topics: ["a-topic-slug"], departments: ['an-org-slug'] }
-    assert_redirected_to publications_path(publication_filter_option: Whitehall::PublicationFilterOption::Consultation.slug, topics: ["a-topic-slug"], departments: ['an-org-slug'])
+    assert_redirected_to(
+      "http://test.host/government/publications.departments%255B%255D=an-org-slug&publication_filter_option=consultations&topics%255B%255D=a-topic-slug"
+    )
   end
 end
