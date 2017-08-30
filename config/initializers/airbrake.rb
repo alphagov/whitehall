2326e61ca4ea7a7ebf619e978735e6169e9790c0
Chris Patuzzo <chris@patuzzo.co.uk>
2017-08-30 14:51:26 +0100

    Use GOV.UK's fork of Airbrake
    This fixes some compatibility problems and handles deployments:
    https://docs.publishing.service.gov.uk/manual/upgrade-rails.html#airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,14 +1,12 @@
 Airbrake.configure do |config|
-  if ENV['ERRBIT_API_KEY'].present?
-    errbit_uri = Plek.find_uri('errbit')
-    config.project_key = ENV['ERRBIT_API_KEY']
-    config.project_id  = ENV['ERRBIT_API_KEY']
-    config.host        = errbit_uri
-    config.environment = ENV['ERRBIT_ENVIRONMENT_NAME']
+  if ENV.has_key?("ERRBIT_API_KEY")
+    config.api_key          = ENV["ERRBIT_API_KEY"]
+    config.host             = "errbit.#{ENV['GOVUK_APP_DOMAIN']}"
+    config.secure           = true
+    config.environment_name = ENV['ERRBIT_ENVIRONMENT_NAME']
   else
-    # Setting the environment and adding it to ignore_environments causes
-    # Airbrake not to attempt to send notifications.
-    config.environment = 'development'
-    config.ignore_environments << 'development'
+    # Adding production to the development environments causes Airbrake not
+    # to attempt to send notifications.
+    config.development_environments << "production"
   end
 end
