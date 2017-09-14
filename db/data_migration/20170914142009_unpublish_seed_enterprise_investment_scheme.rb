9843935b26fbd62f020bdb2a06c47281ce47483d
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-09-14 15:41:07 +0100

    Unpublish seed-enterprise-investment-scheme-procedures
    Migration to unpublish /guidance/seed-enterprise-investment-scheme-procedures
    and provide an `alternative_url` of
    /guidance/venture-capital-schemes-apply-to-use-the-seed-enterprise-investment-scheme
    as per request in zendesk ticket https://govuk.zendesk.com/agent/tickets/2406584

---
+++ db/data_migration/20170914142009_unpublish_seed_enterprise_investment_scheme.rb
@@ -0,0 +1,18 @@
+# Unpublish guidance/seed-enterprise-investment-scheme-procedures and redirect to guidance/venture-capital-schemes-apply-to-use-the-seed-enterprise-investment-scheme
+document_id = 272247
+detailed_guide = Document.find(document_id)
+
+if detailed_guide.published_edition
+  unpublisher = EditionUnpublisher.new(
+    detailed_guide.published_edition,
+    unpublishing: {
+      unpublishing_reason_id: UnpublishingReason::Consolidated.id,
+      explanation: "Unpublished as consolidated into another GOV.UK page. Editorial Board approval received. Redirected.",
+      alternative_url: "#{Whitehall.public_protocol}://#{Whitehall.public_host}/guidance/venture-capital-schemes-apply-to-use-the-seed-enterprise-investment-scheme"
+    }
+  )
+
+  unpublisher.perform!
+
+  PublishingApiDocumentRepublishingWorker.new.perform(document_id)
+)
