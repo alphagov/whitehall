de7e824f9af4f80628c9b4a73112fb7c1b7a9987
Chris Lowis <chris.lowis@gmail.com>
2017-10-03 13:25:06 +0100

    Make stubs explicit in AssetManagerAndQuarantinedFileStorageTest
    We want to add a feature flag which requires adding a new "never"
    expectation. Making these storage stubs explicit in each test makes
    this change easier.

--- test/unit/whitehall/asset_manager_and_quarantined_file_storage_test.rb
+++ test/unit/whitehall/asset_manager_and_quarantined_file_storage_test.rb
@@ -7,31 +7,35 @@ class Whitehall::AssetManagerAndQuarantinedFileStorageTest < ActiveSupport::Test
     @storage = Whitehall::AssetManagerAndQuarantinedFileStorage.new(uploader)
     @file = CarrierWave::SanitizedFile.new(Tempfile.new('asset'))
 
-    @asset_manager_storage = stub('asset-manager-storage', store!: nil)
+    @asset_manager_storage = stub('asset-manager-storage')
     Whitehall::AssetManagerStorage.stubs(:new).returns(@asset_manager_storage)
-    @quarantined_file_storage = stub('quarantined-file-storage', store!: nil)
+    @quarantined_file_storage = stub('quarantined-file-storage')
     Whitehall::QuarantinedFileStorage.stubs(:new).returns(@quarantined_file_storage)
   end
 
   test 'stores the file using the asset manager storage engine' do
     @asset_manager_storage.expects(:store!).with(@file)
+    @quarantined_file_storage.stubs(:store!)
 
     @storage.store!(@file)
   end
 
   test 'stores the file using the quarantined file storage engine' do
+    @asset_manager_storage.stubs(:store!)
     @quarantined_file_storage.expects(:store!).with(@file)
 
     @storage.store!(@file)
   end
 
   test 'returns the value returned from the quarantined file store' do
+    @asset_manager_storage.stubs(:store!)
     @quarantined_file_storage.stubs(:store!).with(@file).returns('stored-file')
 
     assert_equal 'stored-file', @storage.store!(@file)
   end
 
   test 'retrieves the file from the quarantined file store' do
+    @asset_manager_storage.stubs(:store!)
     @quarantined_file_storage.stubs(:retrieve!).with('identifier').returns('retrieved-file')
 
     assert_equal 'retrieved-file', @storage.retrieve!('identifier')
