d619237a679aef4bd801990a3d297c31feb20171
Chris Patuzzo <chris@patuzzo.co.uk>
2017-10-03 13:33:38 +0000

    Discard drafts of WL news page with wrong base paths

---
+++ db/data_migration/20171003133232_discard_non_english_wl_news.rb
@@ -0,0 +1,9 @@
+# These drafts were created with an incorrect base path structure:
+
+Services.publishing_api.discard_draft("d4515402-9704-4240-a0c5-38455521f3b5", locale: "zh-tw")
+Services.publishing_api.discard_draft("e35a3088-da23-4720-9ffa-87ea93b31880", locale: "ru")
+Services.publishing_api.discard_draft("414cba63-2bae-40ab-8441-4a1f242b1b00", locale: "ar")
+Services.publishing_api.discard_draft("6e751743-5e4c-4964-a318-61d97db76faa", locale: "zh")
+Services.publishing_api.discard_draft("3fb57d53-8f3d-4392-8e19-4a0534c29db5", locale: "ar")
+Services.publishing_api.discard_draft("aa347cd1-2e6c-4557-8c6a-85cd1f4f3090", locale: "it")
+Services.publishing_api.discard_draft("fbbb4746-b26e-4fff-af39-0955a8457926", locale: "es-419")
