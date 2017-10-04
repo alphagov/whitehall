f19e63039369f775e1303a5271dcc7a027026630
Chris Lowis <chris.lowis@gmail.com>
2017-10-04 16:09:53 +0100

    Extract variable

--- lib/whitehall/asset_manager_storage.rb
+++ lib/whitehall/asset_manager_storage.rb
@@ -1,7 +1,8 @@
 class Whitehall::AssetManagerStorage < CarrierWave::Storage::Abstract
   def store!(file)
+    carrrierwave_file = file.to_file
     path = File.join('/government/uploads', uploader.store_path)
-    Services.asset_manager.create_whitehall_asset(file: file.to_file, legacy_url_path: path)
+    Services.asset_manager.create_whitehall_asset(file: carrrierwave_file, legacy_url_path: path)
     file
   end
 
