ba6bd8f25b680fce583a2046a0c0811d24d7517b
Chris Lowis <chris.lowis@gmail.com>
2017-10-05 16:26:11 +0100

    Add unique id to path for temporary asset manager files
    Before this commit there was a possible race condition where two
    assets uploaded at the same time with the same name would be written
    to the same `Whitehall.asset_manager_tmp_dir` directory. By adding a
    UUID to the temporary path we avoid this possibility.
    
    CarrierWave by default does something similar using the ID of the
    CarrierWave model. In this case we want to isolate the use of this
    temporary directory to the AssetManagerStorage class and we don't have
    access to a suitable model id.

--- lib/whitehall/asset_manager_storage.rb
+++ lib/whitehall/asset_manager_storage.rb
@@ -1,7 +1,13 @@
 class Whitehall::AssetManagerStorage < CarrierWave::Storage::Abstract
   def store!(file)
     original_file = file.to_file
-    temporary_location = File.join(Whitehall.asset_manager_tmp_dir, File.basename(original_file))
+    temporary_location = File.join(
+      Whitehall.asset_manager_tmp_dir,
+      SecureRandom.uuid,
+      File.basename(original_file)
+    )
+
+    FileUtils.mkdir_p(File.dirname(temporary_location))
     FileUtils.cp(original_file, temporary_location)
     legacy_url_path = File.join('/government/uploads', uploader.store_path)
     AssetManagerWorker.new.perform(temporary_location, legacy_url_path)
--- test/unit/whitehall/asset_manager_storage_test.rb
+++ test/unit/whitehall/asset_manager_storage_test.rb
@@ -21,7 +21,7 @@ class Whitehall::AssetManagerStorageTest < ActiveSupport::TestCase
   test "creates a sidekiq job using the location of the file in the asset manager tmp directory" do
     @worker.expects(:perform).with do |actual_path, _|
       uploaded_file_name = File.basename(@file.path)
-      expected_path = %r{#{Whitehall.asset_manager_tmp_dir}/#{uploaded_file_name}}
+      expected_path = %r{#{Whitehall.asset_manager_tmp_dir}/[a-z0-9\-]+/#{uploaded_file_name}}
       actual_path =~ expected_path
     end
 
