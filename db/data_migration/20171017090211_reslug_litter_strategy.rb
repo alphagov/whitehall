0577d7c72fb3a98551f646478a77dd996f303f5b
Chris Patuzzo <chris@patuzzo.co.uk>
2017-10-17 10:09:03 +0100

    highways-agency-litter-strategy -> …-england-…
    https://trello.com/c/LU45fBKr/204-some-url-changes
    https://govuk.zendesk.com/agent/tickets/2402743

---
+++ db/data_migration/20171017090211_reslug_litter_strategy.rb
@@ -0,0 +1,12 @@
+old_slug = "highways-agency-litter-strategy"
+new_slug = "highways-england-litter-strategy"
+
+document = Document.find_by!(slug: old_slug)
+edition = document.editions.published.last
+
+Whitehall::SearchIndex.delete(edition)
+
+document.update_attributes!(slug: new_slug)
+PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+
+puts "#{old_slug} -> #{new_slug}"
