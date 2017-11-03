16538c686c0a7e07610f7bbb6bd8e16e24c7ac61
David Basalla <davidbasalla@gmail.com>
2017-11-03 11:17:56 +0000

    Skip auth for show method on Admin Topics Controller
    We want to access this endpoint from ContentTagger, but it being
    an AdminController forces Signon permissions. Disabling auth here
    should be fine as it's only the show method and it's not giving
    away sensitive information.
    
    Original PR for this endpoint: https://github.com/alphagov/whitehall/pull/3346

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -1,4 +1,6 @@
 class Admin::TopicsController < Admin::ClassificationsController
+  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :require_signin_permission!, only: [:show]
 
   def show
     respond_to do |format|
