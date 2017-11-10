4880f79fe72026ea65aa8624e92baf6436792bd1
Chris Roos <chris@seagul.co.uk>
2017-11-10 12:39:30 +0000

    Add integration tests for ConsultationResponseFormData
    We intend to use the `AssetManagerStorage` store for organisation logos
    instead of the `AssetManagerAndQuarantinedFileStorage` store. When we
    make this switch we will lose some coverage of the behaviour of the
    `AssetManagerAndQuarantinedFileStorage` in these integration tests. We
    don't currently have integration tests for the use of the
    `AssetManagerAndQuarantinedFileStorage` for
    `ConsultationResponseFormData` so adding some gives us confidence that
    we can make the change we want.
    
    In the `setup` of `RemovingAConsultationResponseFormData` test we are
    setting an instance variable `@file_path` that is only used in the
    first `test` block. However we noticed that if we do not make an
    explicit call to `@consultation_response_form_data.file` before
    calling `remove_file!` then `Services.asset_manager.delete_asset` is
    not called. We're confident that this doesn't happen in reality but
    has something to do with how CarrierWave loads uploaders on-demand. We
    spent some time trying to fully understand what was causing this in
    CarrierWave but gave up and have accepted that we need to make this
    call to `file` in the `setup`.

--- test/integration/asset_manager_test.rb
+++ test/integration/asset_manager_test.rb
@@ -27,6 +27,31 @@ class AssetManagerIntegrationTest
     end
   end
 
+  class CreatingAConsultationResponseFormData < ActiveSupport::TestCase
+    setup do
+      @filename = 'greenpaper.pdf'
+      @consultation_response_form_data = FactoryGirl.build(
+        :consultation_response_form_data,
+        file: File.open(Rails.root.join('test', 'fixtures', @filename))
+      )
+    end
+
+    test 'sends the consultation response form data file to Asset Manager' do
+      Services.asset_manager.expects(:create_whitehall_asset).with do |args|
+        args[:file].is_a?(File) &&
+          args[:legacy_url_path] =~ /#{@filename}/
+      end
+
+      @consultation_response_form_data.save!
+    end
+
+    test 'saves the consultation response form data file to the file system' do
+      @consultation_response_form_data.save!
+
+      assert File.exist?(@consultation_response_form_data.file.path)
+    end
+  end
+
   class RemovingAnOrganisationLogo < ActiveSupport::TestCase
     setup do
       @organisation = FactoryGirl.create(
@@ -54,6 +79,35 @@ class AssetManagerIntegrationTest
       Services.asset_manager.expects(:delete_asset)
 
       @organisation.remove_logo!
+    end
+  end
+
+  class RemovingAConsultationResponseFormData < ActiveSupport::TestCase
+    setup do
+      @consultation_response_form_data = FactoryGirl.create(
+        :consultation_response_form_data,
+        file: File.open(Rails.root.join('test', 'fixtures', 'greenpaper.pdf'))
+      )
+      VirusScanHelpers.simulate_virus_scan(@consultation_response_form_data.file)
+      @consultation_response_form_data.reload
+      @file_path = @consultation_response_form_data.file.path
+
+      Services.asset_manager.stubs(:whitehall_asset).returns('id' => 'http://asset-manager/assets/asset-id')
+      Services.asset_manager.stubs(:delete_asset)
+    end
+
+    test 'removing a consultation response form data file removes it from the file system' do
+      assert File.exist?(@file_path)
+
+      @consultation_response_form_data.remove_file!
+
+      refute File.exist?(@file_path)
+    end
+
+    test 'removing a consultation response form data file removes it from asset manager' do
+      Services.asset_manager.expects(:delete_asset)
+
+      @consultation_response_form_data.remove_file!
     end
   end
 end
