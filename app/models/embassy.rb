ec845ef425adad14013da44a625997659a2851f1
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-16 13:40:34 +0000

    Rename to hide details of what constitutes an embassy office

--- app/models/embassy.rb
+++ app/models/embassy.rb
@@ -9,7 +9,7 @@ class Embassy
   def_delegator :@world_location, :name
 
   def self.filter_offices(worldwide_organisation)
-    worldwide_organisation.offices.select { |o| embassy_high_commission_or_consulate?(o) }
+    worldwide_organisation.offices.select { |o| embassy_office?(o) }
   end
 
   def offices
@@ -30,9 +30,7 @@ class Embassy
     end
   end
 
-  private
-
-  def self.embassy_high_commission_or_consulate?(office)
+  def self.embassy_office?(office)
     [
       "British Trade and Cultural Office",
       "Consulate",
