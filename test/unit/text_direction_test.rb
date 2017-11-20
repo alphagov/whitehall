4a421d7510a08bdc61dd06c5ade5e09c2797fbcb
Chris Patuzzo <chris@patuzzo.co.uk>
2017-11-20 13:19:57 +0000

    Explicitly set text directions for all locales
    Adds a test that checks all text directions have
    been set to either left-to-right or right-to-left.
    
    As part of this work, I audited all locales to
    check they’d been set correctly. I
    cross-referenced them against these two resources:
    
    https://www.w3.org/International/questions/qa-scripts#directions
    
    https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes

---
+++ test/unit/text_direction_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class TextDirectionTest < ActiveSupport::TestCase
+  paths = Dir[File.join(__dir__, "..", "..", "config", "locales", "*.yml")]
+
+  paths.each do |path|
+    test "explicitly sets text direction for #{File.basename(path)}" do
+      data = YAML.load(File.read(path))
+      locale = data.keys.first
+
+      i18n = data[locale]["i18n"]
+      direction = i18n["direction"] if i18n
+
+      assert_includes %w(ltr rtl), direction
+    end
+  end
+end
