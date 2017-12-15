422b535500ed82ad89e41d8037f675b9c086610b
Graham Pengelly <graham@three-tier.com>
2017-12-15 16:30:14 +0000

    Consistentificate services.rb
    Consistency is next to godliness.

--- lib/services.rb
+++ lib/services.rb
@@ -1,12 +1,12 @@
-require 'gds_api/publishing_api_v2'
-require 'gds_api/asset_manager'
+require "gds_api/publishing_api_v2"
+require "gds_api/asset_manager"
 require "gds_api/email_alert_api"
 
 module Services
   def self.publishing_api
     @publishing_api ||= GdsApi::PublishingApiV2.new(
-      Plek.find('publishing-api'),
-      bearer_token: (ENV['PUBLISHING_API_BEARER_TOKEN'] || 'example'),
+      Plek.find("publishing-api"),
+      bearer_token: ENV.fetch("PUBLISHING_API_BEARER_TOKEN", "example"),
       timeout: 20,
     )
   end
@@ -22,7 +22,7 @@ module Services
   def self.asset_manager
     @asset_manager ||= GdsApi::AssetManager.new(
       Plek.find("asset-manager"),
-      bearer_token: ENV["ASSET_MANAGER_BEARER_TOKEN"] || '12345678',
+      bearer_token: ENV.fetch("ASSET_MANAGER_BEARER_TOKEN", "12345678"),
     )
   end
 
