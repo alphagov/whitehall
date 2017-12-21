b500ad0004b5d540793ef2ee435e657a8758d48a
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2017-12-21 13:44:07 +0000

    Rename HCA statistical data returns URL
    Change https://www.gov.uk/government/publications/statistical-data-return-2012-to-2013-survey-responses to https://www.gov.uk/government/publications/statistical-data-return-survey-responses.

---
+++ db/data_migration/20171221134048_rename_sdr_survey_responses.rb
@@ -0,0 +1,12 @@
+old_slug = "statistical-data-return-2012-to-2013-survey-responses"
+new_slug = "statistical-data-return-survey-responses"
+
+document = Document.find_by!(slug: old_slug)
+edition = document.editions.published.last
+
+Whitehall::SearchIndex.delete(edition)
+
+document.update_attributes!(slug: new_slug)
+PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+
+puts "#{old_slug} -> #{new_slug}"
