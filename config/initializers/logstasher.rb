c377ff26367a5cedba9f27a391887d55ebc948e8
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-12-29 09:51:08 +0000

    Remove logstasher initialiser
    This is handled by govuk_app_config.

--- config/initializers/logstasher.rb
+++
@@ -1,9 +0,0 @@
-if Object.const_defined?('LogStasher') && LogStasher.enabled
-  LogStasher.add_custom_fields do |fields|
-    # Mirrors Nginx request logging, e.g GET /path/here HTTP/1.1
-    fields[:request] = "#{request.request_method} #{request.fullpath} #{request.headers['SERVER_PROTOCOL']}"
-    # Pass the request id to logging
-    fields[:govuk_request_id] = request.headers['GOVUK-Request-Id']
-    fields[:varnish_id] = request.headers['X-Varnish']
-  end
-end
