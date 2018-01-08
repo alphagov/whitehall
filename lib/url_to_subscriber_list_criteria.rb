140ded173b877a8d8cac70eb2921e9fc38da6718
Andrew Garner <andrew@andrewgarner.com>
2018-01-08 15:43:27 +0000

    Fix Performance/RedundantMatch offences

--- lib/url_to_subscriber_list_criteria.rb
+++ lib/url_to_subscriber_list_criteria.rb
@@ -32,11 +32,11 @@ class UrlToSubscriberListCriteria
 
   def map_url_to_hash
     @map_url_to_hash ||= begin
-      result = if @url.path.match(%r{^/government/statistics\.atom$})
+      result = if @url.path =~ %r{^/government/statistics\.atom$}
                  { "links" => from_params, EMAIL_SUPERTYPE => "publications", GOVERNMENT_SUPERTYPE => "statistics" }
-               elsif @url.path.match(%r{^/government/publications\.atom$})
+               elsif @url.path =~ %r{^/government/publications\.atom$}
                  { "links" => from_params, EMAIL_SUPERTYPE => "publications" }
-               elsif @url.path.match(%r{^/government/announcements\.atom$})
+               elsif @url.path =~ %r{^/government/announcements\.atom$}
                  { "links" => from_params, EMAIL_SUPERTYPE => "announcements" }
                elsif (path_match = @url.path.match(%r{^/government/people/(.*)\.atom$}))
                  { "links" => from_params.merge("people" => [path_match[1]]) }
