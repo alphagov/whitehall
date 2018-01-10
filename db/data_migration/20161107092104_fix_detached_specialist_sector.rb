3d115db16c6622cfb28f5ae5a021441fe0e73e42
Andrew Garner <andrew@andrewgarner.com>
2018-01-10 12:39:03 +0000

    Fix Style/FormatStringToken offences

--- db/data_migration/20161107092104_fix_detached_specialist_sector.rb
+++ db/data_migration/20161107092104_fix_detached_specialist_sector.rb
@@ -6,15 +6,15 @@ linkable_topic_items = linkable_topics.send(:select_only_subtopics, linkable_top
 
 specialist_sectors = SpecialistSector.where(topic_content_id: nil)
 
-puts 'Found %{count} Specialist Sectors with a missing `topic_content_id`' %
+puts 'Found %<count>d Specialist Sectors with a missing `topic_content_id`' %
   { count: specialist_sectors.count }
 
 specialist_sectors.each do |specialist_sector|
-  puts '- Processing %{specialist_sector}' % { specialist_sector: specialist_sector.inspect }
+  puts '- Processing %<specialist_sector>s' % { specialist_sector: specialist_sector.inspect }
 
   if (topic = linkable_topic_items.find { |item| item['base_path'] == "/topic/#{specialist_sector['tag']}" })
-    puts '  Found topic %{topic}' % { topic: topic.inspect }
-    puts '  %{status} `content_id` %{content_id}' %
+    puts '  Found topic %<topic>s' % { topic: topic.inspect }
+    puts '  %<status>s `content_id` %<content_id>s' %
       { content_id: specialist_sector.topic_content_id = topic['content_id'],
         status: specialist_sector.save ? 'Assigned' : 'Failed to assign' }
   else
