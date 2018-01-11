bd7936960407e2e6842e9cc566872e39ae37bc81
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-01-11 09:05:27 +0000

    Refactor CreateFromBatchReport class
    Also wrap the database actions in a transaction.

--- app/models/link_checker_api_report/create_from_batch_report.rb
+++ app/models/link_checker_api_report/create_from_batch_report.rb
@@ -1,35 +1,41 @@
 class LinkCheckerApiReport::CreateFromBatchReport
-  def initialize(payload, link_reportable)
+  def initialize(payload, reportable)
     @payload = payload
-    @link_reportable = link_reportable
+    @reportable = reportable
   end
 
   def create
-    report = create_report
-    create_links(report)
-    report
+    ActiveRecord::Base.transaction do
+      report = create_report
+      create_links(report)
+      report
+    end
   end
 
 private
 
-  attr_reader :payload, :link_reportable
+  attr_reader :payload, :reportable
 
   def create_report
     LinkCheckerApiReport.create!(
       batch_id: payload.fetch("id"),
       completed_at: payload.fetch("completed_at"),
-      link_reportable: link_reportable,
+      link_reportable: reportable,
       status: payload.fetch("status"),
     )
   end
 
   def create_links(report)
     payload.fetch("links", []).each_with_index do |link_payload, index|
-      attributes = LinkCheckerApiReport::Link
-        .attributes_from_link_report(link_payload)
-        .merge(ordering: index)
-
-      report.links.create!(attributes)
+      report.links.create!(
+        link_attributes_from_report(link_payload, index)
+      )
     end
+  end
+
+  def link_attributes_from_report(payload, index)
+    LinkCheckerApiReport::Link
+      .attributes_from_link_report(payload)
+      .merge(ordering: index)
   end
 end
