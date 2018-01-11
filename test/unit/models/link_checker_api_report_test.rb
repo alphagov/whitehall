49736eb2173f7faa8ecde4a961f0d650f4506b34
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-01-11 09:28:40 +0000

    Add a test for replacing link checker api reports

---
+++ test/unit/models/link_checker_api_report_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+require "gds_api/test_helpers/link_checker_api"
+
+class LinkCheckerApiReportTest < ActiveSupport::TestCase
+  include GdsApi::TestHelpers::LinkCheckerApi
+
+  test "replaces a batch report if it already exists" do
+    link = "http://www.example.com"
+    publication = create(:publication, body: "[link](#{link})")
+
+    batch_id = 5
+
+    report_payload = link_checker_api_batch_report_hash(
+      id: batch_id,
+      links: [{ uri: link }],
+      status: "completed",
+    ).with_indifferent_access
+
+    LinkCheckerApiReport.create!(
+      batch_id: batch_id,
+      link_reportable: publication,
+      status: "in_progress",
+    )
+
+    LinkCheckerApiReport.create_from_batch_report(report_payload, publication)
+
+    report = LinkCheckerApiReport.find_by(batch_id: batch_id)
+    assert_equal "completed", report.status
+  end
+end
