cc223034f3f73c20f3a81291fcf3a5db1a568764
Tasmin Steer <tasminsteer@hotmail.com>
2018-01-16 16:12:33 +0000

    Remove LinksChecker and related classes

--- config/initializers/authed_hosts_for_link_checker.rb
+++
@@ -1,4 +0,0 @@
-# This ENV var is only populated for the integration deployment environment.
-if ENV.has_key?("BASIC_AUTH_CREDENTIALS")
-  LinksChecker.authed_domains = { "www.#{ENV['GOVUK_APP_DOMAIN']}" => ENV["BASIC_AUTH_CREDENTIALS"] }
-end
