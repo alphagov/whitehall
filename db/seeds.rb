659c0b3ed4f2050852e63a454ca577df457e7afb
Adrian Clay <adrian@madetech.com>
2018-01-23 17:41:04 +0000

    Add more permissions to the seed user
    As part of writing E2E tests for Whitehall we want the user to have
    access to all permissions options, and to have an organisation to
    edit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,9 @@ else
   gds_organisation_id = "af07d5a5-df63-4ddc-9383-6a666845ebe9"
   User.create!(
     name: "Test user",
-    permissions: ["signin", "GDS Admin"],
+    permissions: ["signin", "GDS Admin", "GDS Editor"],
     organisation_content_id: gds_organisation_id,
+    organisation_slug: "test-organisation"
   )
 end
 
