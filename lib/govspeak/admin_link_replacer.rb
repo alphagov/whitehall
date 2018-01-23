6f56faee66bc620f1015b39ada2f0b79312c6604
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2018-01-23 13:30:10 +0000

    Extract class for finding an edition by its Whitehall URL
    Separate this from the logic which replaces these URLs with
    public-facing URLs when rendering govspeak.
    
    This will make it simpler to reuse the URL conversion logic in the link
    checker.

--- lib/govspeak/admin_link_replacer.rb
+++ lib/govspeak/admin_link_replacer.rb
@@ -17,52 +17,27 @@ module Govspeak
       end
     end
 
-    def replacement_html_for_admin_link(anchor, &block)
-      case anchor['href']
-      when ADMIN_ORGANISATION_CIP_PATH
-        convert_link_for_corporate_information_page(anchor, $1, $2, &block)
-      when ADMIN_WORLDWIDE_ORGANISATION_CIP_PATH
-        convert_link_for_worldwide_corporate_information_page(anchor, $1, $2, &block)
-      when ADMIN_EDITION_PATH
-        edition = Edition.unscoped.find_by(id: $1)
-        convert_link_for_edition(anchor, edition, &block)
+    def replacement_html_for_admin_link(anchor)
+      edition = Whitehall::AdminLinkLookup.find_edition(anchor['href'])
+
+      if edition.present? && edition.linkable?
+        public_url = Whitehall.url_maker.public_document_url(edition)
+        new_html = convert_link(anchor, public_url)
       else
-        replace_bad_link(anchor, &block)
+        new_html = anchor.inner_text
       end
-    end
-
-  private
-
-    def convert_link_for_corporate_information_page(anchor, organisation_slug, slug, &block)
-      organisation = Organisation.find_by(slug: organisation_slug)
-      corporate_info_page = organisation.corporate_information_pages.find(slug)
-
-      convert_link_for_edition(anchor, corporate_info_page, &block)
-    end
-
-    def convert_link_for_worldwide_corporate_information_page(anchor, world_org_slug, slug, &block)
-      organisation = WorldwideOrganisation.find_by(slug: world_org_slug)
-      corporate_info_page = organisation.corporate_information_pages.find(slug)
-
-      convert_link_for_edition(anchor, corporate_info_page, &block)
-    end
-
-    def convert_link_for_edition(anchor, edition)
-      new_html = if edition.present? && edition.linkable?
-                   anchor
-                     .dup
-                     .tap { |new_anchor| new_anchor['href'] = Whitehall.url_maker.public_document_url(edition) }
-                     .to_html
-                     .html_safe
-                 else
-                   anchor.inner_text
-                 end
 
       block_given? ? yield(new_html, edition) : new_html
     end
 
-    def replace_bad_link(anchor)
-      block_given? ? yield(anchor.inner_text, nil) : anchor.inner_text
+  private
+
+    def convert_link(anchor, new_url)
+      anchor
+        .dup
+        .tap { |new_anchor| new_anchor['href'] = new_url }
+        .to_html
+        .html_safe
     end
   end
 end
