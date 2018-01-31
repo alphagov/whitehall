ffa7e4479b60b2b278eb749739e67a2a9607b21f
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2018-01-31 16:25:17 +0000

    WIP: Save a placeholder attachment based on details from the CSV

--- lib/import/hmcts_importer.rb
+++ lib/import/hmcts_importer.rb
@@ -31,20 +31,41 @@ module Import
         publication.save!
         puts "Created publication with ID #{publication.id}"
 
-        # TODO: Add attachments
-        attachment_data = AttachmentData.new(file: File.new("/tmp/hello.txt"))
-        file_attachment = FileAttachment.new(
-          title: "Another attachment title",
-          attachment_data: attachment_data,
-          attachable: publication)
-        file_attachment.save!
-
-        puts "Added attachments"
+        create_attachments(publication_data[:attachments], publication)
       end
     end
 
     def self.default_organisation
       @_default_organisation ||= Organisation.find_by(name: "Ministry of Justice")
+    end
+
+    def self.create_attachments(attachments, publication)
+      temp_directory = "/tmp/#{SecureRandom.uuid}"
+      Dir.mkdir(temp_directory)
+      puts "Saving temporary files to #{temp_directory}"
+
+      attachments.each do |attachment|
+        create_attachment(attachment, publication, temp_directory)
+      end
+    end
+
+    def self.create_attachment(attachment, publication, temp_directory)
+      # TODO: Download attachment
+      temp_file_path = "#{temp_directory}/#{attachment[:file_name]}"
+      attachment_file = File.open(temp_file_path, "w") do |file|
+        file.write("Test attachment\n")
+        file.write(attachment[:title])
+      end
+
+      attachment_data = AttachmentData.new(file: File.new(temp_file_path))
+      file_attachment = FileAttachment.new(
+        title: attachment[:title],
+        attachment_data: attachment_data,
+        attachable: publication,
+      )
+      file_attachment.save!
+
+      puts "Added attachment #{temp_file_path}"
     end
   end
 end
