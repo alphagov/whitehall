f46affa32780094d37974f551ddd0804768e21b9
Chris Lowis <chris.lowis@gmail.com>
2018-02-05 08:39:01 -0500

    Use realistic directory names in test
    In practice the directory names that carrierwave creates in
    `@attachments_parent_dir` do not have leading zeros. Using more
    realistic directory names will be less confusing when we add the
    ability to specify a range of directories.

--- test/unit/migrate_assets_to_asset_manager_test.rb
+++ test/unit/migrate_assets_to_asset_manager_test.rb
@@ -100,22 +100,22 @@ class MigrateAttachmentsToAssetManagerTest < ActiveSupport::TestCase
   setup do
     @attachments_parent_dir = Pathname.new(File.join(Whitehall.clean_uploads_root, 'system', 'uploads', 'attachment_data', 'file'))
 
-    FileUtils.mkdir_p(@attachments_parent_dir.join('001'))
-    FileUtils.mkdir_p(@attachments_parent_dir.join('002'))
+    FileUtils.mkdir_p(@attachments_parent_dir.join('100'))
+    FileUtils.mkdir_p(@attachments_parent_dir.join('200'))
 
     @migrator = stub('migrator', perform: nil)
     MigrateAssetsToAssetManager.stubs(:new).returns(@migrator)
   end
 
-  test 'migrates attachment directory 001 and sets attachments as draft' do
-    MigrateAssetsToAssetManager.stubs(:new).with('system/uploads/attachment_data/file/001', true).returns(@migrator)
+  test 'migrates attachment directory 100 and sets attachments as draft' do
+    MigrateAssetsToAssetManager.stubs(:new).with('system/uploads/attachment_data/file/100', true).returns(@migrator)
     @migrator.expects(:perform)
 
     MigrateAssetsToAssetManager.migrate_attachments
   end
 
-  test 'migrates attachment directory 002 and sets attachments as draft' do
-    MigrateAssetsToAssetManager.stubs(:new).with('system/uploads/attachment_data/file/002', true).returns(@migrator)
+  test 'migrates attachment directory 200 and sets attachments as draft' do
+    MigrateAssetsToAssetManager.stubs(:new).with('system/uploads/attachment_data/file/200', true).returns(@migrator)
     @migrator.expects(:perform)
 
     MigrateAssetsToAssetManager.migrate_attachments
