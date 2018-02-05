b1b37db5ea9a58f6d9961b10e4790fb0a012bee3
James Mead <james@floehopper.org>
2018-02-05 11:27:37 +0000

    WIP: Only update asset if draft status has changed

--- app/workers/asset_manager_update_asset_worker.rb
+++ app/workers/asset_manager_update_asset_worker.rb
@@ -1,11 +1,10 @@
 class AssetManagerUpdateAssetWorker < WorkerBase
   def perform(legacy_url_path, draft)
     gds_api_response = Services.asset_manager.whitehall_asset(legacy_url_path)
-    asset_url = gds_api_response['id']
-    asset_id = asset_url[/\/assets\/(.*)/, 1]
-
-    # TODO: We could optimise this by working out whether the draft status
-    # has changed and only update it in the Asset Manager if it has.
-    Services.asset_manager.update_asset(asset_id, draft: draft)
+    unless gds_api_response['draft'] == draft
+      asset_url = gds_api_response['id']
+      asset_id = asset_url[/\/assets\/(.*)/, 1]
+      Services.asset_manager.update_asset(asset_id, draft: draft)
+    end
   end
 end
