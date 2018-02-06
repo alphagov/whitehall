57afaf98e0276a07d6d746c47180b80904b595af
James Mead <james@floehopper.org>
2018-02-06 11:10:03 +0000

    WIP: Use Whitehall::AssetManagerStorage::File#path
    Note that we'll be able to get rid of
    `AttachmentUploader#asset_manager_path` and
    `Whitehall::AssetManagerAndQuarantinedFileStorage::File#asset_manager_path`
    when we switch to using `AssetManagerStorage` only in
    `AttachmentUploader`.

--- app/services/service_listeners/attachment_draft_status_updater.rb
+++ app/services/service_listeners/attachment_draft_status_updater.rb
@@ -10,11 +10,11 @@ module ServiceListeners
       if edition.allows_attachments?
         edition.attachments.select(&:file?).each do |attachment|
           attachment_data = attachment.attachment_data
-          legacy_url_path = attachment_data.file.path
+          legacy_url_path = attachment_data.file.asset_manager_path
           draft = !AttachmentVisibility.new(attachment_data, nil).visible?
           AssetManagerUpdateAssetWorker.perform_async(legacy_url_path, draft)
           attachment_data.file.versions.each_value do |uploader|
-            legacy_url_path = uploader.file.path
+            legacy_url_path = uploader.asset_manager_path
             AssetManagerUpdateAssetWorker.perform_async(legacy_url_path, draft)
           end
         end
