fe83e56b375f52feb68c15720d310698c104f877
Chris Lowis <chris.lowis@gmail.com>
2018-02-07 12:21:57 -0500

    Don't check if incoming HMRC assets exist
    The assets served from this controller are hard-coded to be in under
    the `Whitehall.clean_uploads_root`. The check to see if
    `incoming_upload_exists?` is therefore redundant and can be removed.

--- app/controllers/hmrc_assets_controller.rb
+++ app/controllers/hmrc_assets_controller.rb
@@ -13,18 +13,7 @@ class HmrcAssetsController < ApplicationController
 private
 
   def fail
-    if incoming_upload_exists? upload_path
-      redirect_to_placeholder
-    else
-      render plain: "Not found", status: :not_found
-    end
-  end
-
-  def redirect_to_placeholder
-    # Cache is explicitly 1 minute to prevent the virus redirect beng
-    # cached by CDNs.
-    expires_in(1.minute, public: true)
-    redirect_to placeholder_url
+    render plain: "Not found", status: :not_found
   end
 
   def send_file_for_mime_type
@@ -46,11 +35,6 @@ private
   def upload_path
     basename = [params[:path], params[:format]].compact.join('.')
     File.join(Whitehall.clean_uploads_root, 'uploaded', 'hmrc', basename)
-  end
-
-  def incoming_upload_exists?(path)
-    path = path.sub(Whitehall.clean_uploads_root, Whitehall.incoming_uploads_root)
-    File.exist?(path)
   end
 
   def real_path_for_x_accel_mapping(potentially_symlinked_path)
