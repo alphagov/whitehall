d555c23f4548738d6553992cae300b7e2cc64493
James Mead <james@floehopper.org>
2018-02-07 15:29:55 +0000

    Only update draft status in Asset Manager if has changed
    Since we already have the `draft` status being returned in the response
    from the call to `whitehall_asset` on the Asset Manager API, this
    optimisation doesn't require the code to be much more complex and
    although it might be slightly premature, I think it's worthwhile.
    
    Note again that we're expecting the attribute keys to be strings,
    because even if they are passed as symbols into
    `AssetManagerUpdateAssetWorker.perform_async`, Sidekiq seems to convert
    them into strings when they arrive at
    `AssetManagerUpdateAssetWorker#perform`.

--- test/unit/workers/asset_manager_update_asset_worker_test.rb
+++ test/unit/workers/asset_manager_update_asset_worker_test.rb
@@ -10,16 +10,32 @@ class AssetManagerUpdateAssetWorkerTest < ActiveSupport::TestCase
 
   test 'marks draft asset as published' do
     Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
-      .returns('id' => @asset_url)
+      .returns('id' => @asset_url, 'draft' => true)
     Services.asset_manager.expects(:update_asset).with(@asset_id, 'draft' => false)
+
+    @worker.perform(@legacy_url_path, 'draft' => false)
+  end
+
+  test 'does not mark asset as published if already published' do
+    Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
+      .returns('id' => @asset_url, 'draft' => false)
+    Services.asset_manager.expects(:update_asset).never
 
     @worker.perform(@legacy_url_path, 'draft' => false)
   end
 
   test 'mark published asset as draft' do
     Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
-      .returns('id' => @asset_url)
+      .returns('id' => @asset_url, 'draft' => false)
     Services.asset_manager.expects(:update_asset).with(@asset_id, 'draft' => true)
+
+    @worker.perform(@legacy_url_path, 'draft' => true)
+  end
+
+  test 'does not mark asset as draft if already draft' do
+    Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
+      .returns('id' => @asset_url, 'draft' => true)
+    Services.asset_manager.expects(:update_asset).never
 
     @worker.perform(@legacy_url_path, 'draft' => true)
   end
