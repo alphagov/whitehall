45e8b02329a9bc120f981b041e09b49bb82e1863
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-02-08 10:37:42 +0000

    Use external URLs for Signon
    This uses the new external_url_for method in Plek, which handles
    environments where the URLs for internal and external routing are
    different.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -2,5 +2,5 @@ GDS::SSO.config do |config|
   config.user_model   = "User"
   config.oauth_id     = ENV['OAUTH_ID'] || "abcdefghjasndjkasndwhitehall"
   config.oauth_secret = ENV['OAUTH_SECRET'] || "secret"
-  config.oauth_root_url = Plek.find("signon")
+  config.oauth_root_url = Plek.new.external_url_for("signon")
 end
