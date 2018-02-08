dd875eef11d111ade3770690f354e4ec0aa4a3ef
David Silva <david.silva@digital.cabinet-office.gov.uk>
2018-02-08 11:25:09 +0000

    Fix consultation redirect params
    This commit fixes the current implementation where the paramenters of
    the url were being separated by a "." (dot) instead of a "?" (question
    mark). It also replaced the ascii code "%255B" & "%255D" to the
    appropriate ones which are ""%5B" & "%5D" (both mean "[" & "]") which
    are used to represent an array of the attribute in cause.
    
    This was brought to our attention in the Zendesk Ticket
    https://govuk.zendesk.com/agent/tickets/2576393

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,6 +1,6 @@
 class ConsultationsController < DocumentsController
   def index
     filter_params = params.permit!.except(:controller, :action, :format, :_, :host)
-    redirect_to publications_path(filter_params.merge(publication_filter_option: 'consultations'))
+    redirect_to publications_path(filter_params.merge(publication_filter_option: 'consultations').to_h)
   end
 end
