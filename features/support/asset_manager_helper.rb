b377628195345d63bc547421c9f06d0187c5e409
James Mead <james@floehopper.org>
2018-02-08 11:33:05 +0000

    Stub GdsApi::AssetManager#whitehall_asset for all features
    I'm about to make some changes which would've meant adding this stubbing
    to even more features. Since we're already stubbing all methods on
    GdsApi::AssetManager for all features, it seems reasonable to specify
    this generic return value for one of those stubbed methods for all
    features.

--- features/support/asset_manager_helper.rb
+++ features/support/asset_manager_helper.rb
@@ -1,3 +1,6 @@
 Before do
-  Services.stubs(:asset_manager).returns(stub_everything('asset-manager'))
+  asset_manager = stub_everything('asset-manager')
+  asset_details = { 'id' => 'http://asset-manager/assets/asset-id' }
+  asset_manager.stubs(:whitehall_asset).returns(asset_details)
+  Services.stubs(:asset_manager).returns(asset_manager)
 end
