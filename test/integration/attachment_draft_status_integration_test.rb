28a86c5cca1aac6158640b0162a493403a6a624b
James Mead <james@floehopper.org>
2018-02-11 11:11:35 +0000

    Use let statements to simplify AttachmentDraftStatusIntegrationTest

--- test/integration/attachment_draft_status_integration_test.rb
+++ test/integration/attachment_draft_status_integration_test.rb
@@ -4,11 +4,12 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
 
   context 'when draft document with file attachment is published' do
+    let(:edition) { create(:news_article) }
+
     before do
-      @edition = create(:news_article)
-      @edition.attachments << FactoryBot.build(
+      edition.attachments << FactoryBot.build(
         :file_attachment,
-        attachable: @edition,
+        attachable: edition,
         file: File.open(fixture_path.join('whitepaper.pdf'))
       )
 
@@ -20,17 +21,18 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
       Services.asset_manager.expects(:update_asset).with('asset-id', 'draft' => false)
       Services.asset_manager.expects(:update_asset).with('thumbnail-asset-id', 'draft' => false)
 
-      force_publisher = Whitehall.edition_services.force_publisher(@edition)
+      force_publisher = Whitehall.edition_services.force_publisher(edition)
       assert force_publisher.perform!, force_publisher.failure_reason
     end
   end
 
   context 'when published document with file attachment is unpublished' do
+    let(:edition) { create(:published_news_article) }
+
     before do
-      @edition = create(:published_news_article)
-      @edition.attachments << FactoryBot.build(
+      edition.attachments << FactoryBot.build(
         :file_attachment,
-        attachable: @edition,
+        attachable: edition,
         file: File.open(fixture_path.join('whitepaper.pdf'))
       )
 
@@ -42,7 +44,7 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
       Services.asset_manager.expects(:update_asset).with('asset-id', 'draft' => true)
       Services.asset_manager.expects(:update_asset).with('thumbnail-asset-id', 'draft' => true)
 
-      unpublisher = Whitehall.edition_services.unpublisher(@edition, unpublishing: {
+      unpublisher = Whitehall.edition_services.unpublisher(edition, unpublishing: {
         unpublishing_reason: UnpublishingReason::PublishedInError
       })
       assert unpublisher.perform!, unpublisher.failure_reason
@@ -50,9 +52,11 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
   end
 
   context 'when draft consultation with outcome with file attachment is published' do
+    let(:edition) { create(:draft_consultation) }
+    let(:outcome_attributes) { FactoryBot.attributes_for(:consultation_outcome) }
+    let(:outcome) { edition.create_outcome!(outcome_attributes) }
+
     before do
-      @edition = create(:draft_consultation)
-      outcome = @edition.create_outcome!(FactoryBot.attributes_for(:consultation_outcome))
       outcome.attachments << FactoryBot.build(
         :file_attachment,
         attachable: outcome,
@@ -67,15 +71,17 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
       Services.asset_manager.expects(:update_asset).with('asset-id', 'draft' => false)
       Services.asset_manager.expects(:update_asset).with('thumbnail-asset-id', 'draft' => false)
 
-      force_publisher = Whitehall.edition_services.force_publisher(@edition)
+      force_publisher = Whitehall.edition_services.force_publisher(edition)
       assert force_publisher.perform!, force_publisher.failure_reason
     end
   end
 
   context 'when draft consultation with feedback with file attachment is published' do
+    let(:edition) { create(:draft_consultation) }
+    let(:feedback_attributes) { FactoryBot.attributes_for(:consultation_public_feedback) }
+    let(:feedback) { edition.create_public_feedback!(feedback_attributes) }
+
     before do
-      @edition = create(:draft_consultation)
-      feedback = @edition.create_public_feedback!(FactoryBot.attributes_for(:consultation_public_feedback))
       feedback.attachments << FactoryBot.build(
         :file_attachment,
         attachable: feedback,
@@ -90,16 +96,17 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
       Services.asset_manager.expects(:update_asset).with('asset-id', 'draft' => false)
       Services.asset_manager.expects(:update_asset).with('thumbnail-asset-id', 'draft' => false)
 
-      force_publisher = Whitehall.edition_services.force_publisher(@edition)
+      force_publisher = Whitehall.edition_services.force_publisher(edition)
       assert force_publisher.perform!, force_publisher.failure_reason
     end
   end
 
   context 'when file attachment is added to outcome belonging to published consultation' do
-    before do
-      edition = create(:published_consultation)
-      @outcome = edition.create_outcome!(FactoryBot.attributes_for(:consultation_outcome))
+    let(:edition) { create(:published_consultation) }
+    let(:outcome_attributes) { FactoryBot.attributes_for(:consultation_outcome) }
+    let(:outcome) { edition.create_outcome!(outcome_attributes) }
 
+    before do
       stub_whitehall_asset('whitepaper.pdf', id: 'asset-id', draft: true)
       stub_whitehall_asset('thumbnail_whitepaper.pdf.png', id: 'thumbnail-asset-id', draft: true)
     end
@@ -108,19 +115,19 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
       Services.asset_manager.expects(:update_asset).with('asset-id', 'draft' => false)
       Services.asset_manager.expects(:update_asset).with('thumbnail-asset-id', 'draft' => false)
 
-      @outcome.attachments << FactoryBot.build(
+      outcome.attachments << FactoryBot.build(
         :file_attachment,
-        attachable: @outcome,
+        attachable: outcome,
         file: File.open(fixture_path.join('whitepaper.pdf'))
       )
-      Whitehall.consultation_response_notifier.publish('update', @outcome)
+      Whitehall.consultation_response_notifier.publish('update', outcome)
     end
   end
 
   context 'when file attachment is added to policy group' do
-    before do
-      @policy_group = create(:policy_group)
+    let(:policy_group) { create(:policy_group) }
 
+    before do
       stub_whitehall_asset('whitepaper.pdf', id: 'asset-id', draft: true)
       stub_whitehall_asset('thumbnail_whitepaper.pdf.png', id: 'thumbnail-asset-id', draft: true)
     end
@@ -129,12 +136,12 @@ class AttachmentDraftStatusIntegrationTest < ActiveSupport::TestCase
       Services.asset_manager.expects(:update_asset).with('asset-id', 'draft' => false)
       Services.asset_manager.expects(:update_asset).with('thumbnail-asset-id', 'draft' => false)
 
-      @policy_group.attachments << FactoryBot.build(
+      policy_group.attachments << FactoryBot.build(
         :file_attachment,
-        attachable: @policy_group,
+        attachable: policy_group,
         file: File.open(fixture_path.join('whitepaper.pdf'))
       )
-      Whitehall.policy_group_notifier.publish('update', @policy_group)
+      Whitehall.policy_group_notifier.publish('update', policy_group)
     end
   end
 
