ac3e385c6c5653210bf2ca35647fa199ef0a0c34
Chris Lowis <chris.lowis@gmail.com>
2018-02-20 11:14:46 -0500

    Delegate AssetManagerAndQuarantinedFileStorage#size
    AssetManagerStorage now has an implementation of `size` that
    calls the Asset Manager API. We delegate to that implementation from
    AssetManagerAndQuarantinedFileStorage as this will make it easier to
    completely switch the storage engine over in the future.

--- lib/whitehall/asset_manager_and_quarantined_file_storage.rb
+++ lib/whitehall/asset_manager_and_quarantined_file_storage.rb
@@ -14,7 +14,7 @@ class Whitehall::AssetManagerAndQuarantinedFileStorage < CarrierWave::Storage::A
   end
 
   class File
-    delegate :empty?, :path, :content_type, :filename, :size, to: :@quarantined_file
+    delegate :empty?, :path, :content_type, :filename, to: :@quarantined_file
 
     def initialize(asset_manager_file, quarantined_file)
       @asset_manager_file = asset_manager_file
@@ -32,6 +32,10 @@ class Whitehall::AssetManagerAndQuarantinedFileStorage < CarrierWave::Storage::A
     def delete
       @quarantined_file.delete
       @asset_manager_file.delete
+    end
+
+    def size
+      @asset_manager_file.size
     end
   end
 end
