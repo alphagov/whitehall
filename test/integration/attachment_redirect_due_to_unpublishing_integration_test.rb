fda886c1125edb1de056c8b6135a2c4bd7c467d7
James Mead <james@floehopper.org>
2018-02-22 15:20:34 +0000

    Remove redundant Sidekiq setup from attachment integration test
    Since #3793, all the tests except Cucumber features use the fake test
    mode for Sidekiq. So we can now safely remove this redundant setup &
    teardown code.

--- test/integration/attachment_redirect_due_to_unpublishing_integration_test.rb
+++ test/integration/attachment_redirect_due_to_unpublishing_integration_test.rb
@@ -19,13 +19,6 @@ class AttachmentRedirectDueToUnpublishingIntegrationTest < ActionDispatch::Integ
     attachable.attachments << attachment
     VirusScanHelpers.simulate_virus_scan
     stub_whitehall_asset(filename, id: asset_id)
-
-    @test_mode = Sidekiq::Testing.__test_mode
-    Sidekiq::Testing.fake!
-  end
-
-  after do
-    Sidekiq::Testing.__test_mode = @test_mode
   end
 
   context 'given a published document with file attachment' do
