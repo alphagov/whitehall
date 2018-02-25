ce27fcc42d5e06138755afedf90ff18da66cde68
Chris Lowis <chris.lowis@gmail.com>
2018-02-25 08:27:10 -0500

    Remove unused delegated method FileAttachment#virus_status
    This method is not called from anywhere so can be removed.

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -3,7 +3,7 @@ class FileAttachment < Attachment
 
   delegate :url, :content_type, :pdf?, :csv?, :opendocument?,
     :file_extension, :file_size,
-    :number_of_pages, :file, :filename, :filename_without_extension, :virus_status,
+    :number_of_pages, :file, :filename, :filename_without_extension,
     to: :attachment_data
 
   accepts_nested_attributes_for :attachment_data, reject_if: ->(attributes) { attributes[:file].blank? && attributes[:file_cache].blank? }
