1c1682dfa5520da2a201c85ffef9ffa0be01f4e7
James Mead <james@floehopper.org>
2018-02-27 16:02:08 +0000

    Bring AssetManagerDeleteAssetWorker into line with the update worker
    By calling GdsApi::Response#to_hash on the response from the Asset
    Manager API and renaming the local variable from gds_api_response ->
    attributes.
    
    I think it's important to keep these two worker classes in line with
    each other because there is some duplication between them. Keeping them
    in line with each other will make it easier to remove the duplication
    later.

--- app/workers/asset_manager_delete_asset_worker.rb
+++ app/workers/asset_manager_delete_asset_worker.rb
@@ -1,7 +1,7 @@
 class AssetManagerDeleteAssetWorker < WorkerBase
   def perform(legacy_url_path)
-    gds_api_response = asset_manager.whitehall_asset(legacy_url_path)
-    asset_url = gds_api_response['id']
+    attributes = asset_manager.whitehall_asset(legacy_url_path).to_hash
+    asset_url = attributes['id']
     asset_id = asset_url[/\/assets\/(.*)/, 1]
 
     asset_manager.delete_asset(asset_id)
