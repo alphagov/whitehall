1639d1a34864f684e1bde941cbd752b948df5c75
James Mead <james@floehopper.org>
2018-02-27 18:06:28 +0000

    Introduce ServiceListeners::AttachmentReplacementIdUpdater
    This is similar to the other attachment-related service listeners,
    but it's intended to be used to update the replacement_id of the Asset
    Manager asset corresponding to a Whitehall attachment that is being
    replaced. It differs slightly in that it is instantiated with an
    AttachmentData rather than an Attachment. This is because I'm planning
    to hook this in to the AttachmentData#replace_with! method and so we
    will already know which AttachmentData the update should be performed
    on.
    
    It doesn't look as if there's currently any way to unset the
    AttachmentData#replaced_by_id via the user interface and so I'm not
    handling that scenario for the moment.
    
    I'm not particularly happy with all the duplication between the various
    attachment-related service listeners, but keeping the code as similar as
    possible should make it easier to remove the duplication later.

---
+++ app/services/service_listeners/attachment_replacement_id_updater.rb
@@ -0,0 +1,38 @@
+module ServiceListeners
+  class AttachmentReplacementIdUpdater
+    include Rails.application.routes.url_helpers
+    include PublicDocumentRoutesHelper
+
+    attr_reader :attachment_data, :queue
+
+    def initialize(attachment_data, queue: nil)
+      @attachment_data = attachment_data
+      @queue = queue
+    end
+
+    def update!
+      return unless attachment_data.present?
+
+      return unless attachment_data.replaced_by.present?
+      replacement = attachment_data.replaced_by
+
+      enqueue_job(attachment_data.file, replacement.file)
+      if attachment_data.pdf?
+        enqueue_job(attachment_data.file.thumbnail, replacement.file.thumbnail)
+      end
+    end
+
+  private
+
+    def enqueue_job(uploader, replacement_uploader)
+      legacy_url_path = uploader.asset_manager_path
+      replacement_legacy_url_path = replacement_uploader.asset_manager_path
+      worker.perform_async(legacy_url_path, replacement_legacy_url_path: replacement_legacy_url_path)
+    end
+
+    def worker
+      worker = AssetManagerUpdateAssetWorker
+      queue.present? ? worker.set(queue: queue) : worker
+    end
+  end
+end
