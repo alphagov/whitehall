b0e858aa3b4483e87d913d37ed7f9c3827eef610
Chris Lowis <chris.lowis@gmail.com>
2018-03-09 12:50:42 +0000

    Remove redundant local variable assignments

--- app/workers/asset_manager_attachment_replacement_id_update_worker.rb
+++ app/workers/asset_manager_attachment_replacement_id_update_worker.rb
@@ -11,12 +11,10 @@ class AssetManagerAttachmentReplacementIdUpdateWorker < WorkerBase
       if replacement.pdf?
         legacy_url_path = attachment_data.file.thumbnail.asset_manager_path
         replacement_legacy_url_path = replacement.file.thumbnail.asset_manager_path
-        worker = AssetManagerUpdateAssetWorker
         worker.perform_async(legacy_url_path, replacement_legacy_url_path: replacement_legacy_url_path)
       else
         legacy_url_path = attachment_data.file.thumbnail.asset_manager_path
         replacement_legacy_url_path = replacement.file.asset_manager_path
-        worker = AssetManagerUpdateAssetWorker
         worker.perform_async(legacy_url_path, replacement_legacy_url_path: replacement_legacy_url_path)
       end
     end
