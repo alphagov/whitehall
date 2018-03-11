9b99739374b3d670fe8f38e5f913834f64b71b4a
James Mead <james@floehopper.org>
2018-03-11 13:14:56 +0000

    Inline AttachmentVisibility method into attachment controllers
    I'm planning to get rid of the AttachmentVisibility class altogether.
    This is a small step in that direction.

--- app/controllers/base_attachments_controller.rb
+++ app/controllers/base_attachments_controller.rb
@@ -2,11 +2,11 @@ class BaseAttachmentsController < ApplicationController
 protected
 
   def attachment_visible?
-    upload_exists?(upload_path) && attachment_visibility.visible?
+    upload_exists?(upload_path) && attachment_data.visible_to?(current_user)
   end
 
   def fail
-    if (edition = attachment_visibility.unpublished_edition)
+    if (edition = attachment_data.unpublished_edition)
       redirect_to edition.unpublishing.document_path
     elsif (replacement = attachment_data.replaced_by)
       expires_headers
@@ -46,10 +46,6 @@ protected
 
   def path_to_attachment_or_thumbnail
     attachment_data.file.store_path(file_with_extensions)
-  end
-
-  def attachment_visibility
-    @attachment_visibility ||= AttachmentVisibility.new(attachment_data, current_user)
   end
 
   def file_is_clean?(path)
