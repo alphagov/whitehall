83bd7fee8fcb24bf9a82dcad4782b54c1a4f8425
Chris Lowis <chris.lowis@gmail.com>
2018-03-13 11:35:17 +0000

    Change service listener test to stub new worker
    We have integration tests that ensure that the service listener calls
    the worker correctly so this test can be changed to stub at a higher level.

--- test/unit/services/service_listeners/attachment_redirect_url_updater_test.rb
+++ test/unit/services/service_listeners/attachment_redirect_url_updater_test.rb
@@ -6,12 +6,17 @@ module ServiceListeners
 
     let(:updater) { AttachmentRedirectUrlUpdater.new(attachment_data) }
     let(:attachment_data) { attachment.attachment_data }
+    let(:worker) { mock('asset-manager-attachment-redirect-url-update-worker') }
+
+    before do
+      AssetManagerAttachmentRedirectUrlUpdateWorker.stubs(:new).returns(worker)
+    end
 
     context 'when attachment has no associated attachment data' do
       let(:attachment) { FactoryBot.create(:html_attachment) }
 
       it 'does not update redirect URL of any assets' do
-        AssetManagerUpdateAssetWorker.expects(:perform_async).never
+        worker.expects(:perform).never
 
         updater.update!
       end
