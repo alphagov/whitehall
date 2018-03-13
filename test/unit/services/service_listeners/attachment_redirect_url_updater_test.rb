1b7ad6e6ae66972da94391cd15e22cd688d52a1b
Chris Lowis <chris.lowis@gmail.com>
2018-03-13 11:37:33 +0000

    Add test to ensure worker is called
    We were missing a test case to ensure that the worker is called with
    an attachment data instance is available.

--- test/unit/services/service_listeners/attachment_redirect_url_updater_test.rb
+++ test/unit/services/service_listeners/attachment_redirect_url_updater_test.rb
@@ -12,6 +12,17 @@ module ServiceListeners
       AssetManagerAttachmentRedirectUrlUpdateWorker.stubs(:new).returns(worker)
     end
 
+    context 'when attachment has associated attachment data' do
+      let(:sample_rtf) { File.open(fixture_path.join('sample.rtf')) }
+      let(:attachment) { FactoryBot.create(:file_attachment, file: sample_rtf) }
+
+      it 'updates the redirect URL of any assets' do
+        worker.expects(:perform).with(attachment_data.id)
+
+        updater.update!
+      end
+    end
+
     context 'when attachment has no associated attachment data' do
       let(:attachment) { FactoryBot.create(:html_attachment) }
 
