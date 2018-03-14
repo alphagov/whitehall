bb9eda3135140676de9fc59cacedbdae97c79eec
Chris Lowis <chris.lowis@gmail.com>
2018-03-14 13:40:04 +0000

    Test that service listener calls worker when attachment data present

--- test/unit/services/service_listeners/attachment_access_limited_updater_test.rb
+++ test/unit/services/service_listeners/attachment_access_limited_updater_test.rb
@@ -12,6 +12,17 @@ module ServiceListeners
       AssetManagerAttachmentAccessLimitedWorker.stubs(:new).returns(worker)
     end
 
+    context 'when the attachment has associated attachment data' do
+      let(:sample_rtf) { File.open(fixture_path.join('sample.rtf')) }
+      let(:attachment) { FactoryBot.create(:file_attachment, file: sample_rtf) }
+
+      it 'calls the worker' do
+        worker.expects(:perform).with(attachment_data)
+
+        updater.update!
+      end
+    end
+
     context 'when attachment has no associated attachment data' do
       let(:attachment) { FactoryBot.create(:html_attachment) }
 
