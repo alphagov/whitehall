6e067e9bc11ada71f914828d1ebe67f0570da673
Chris Roos <chris@seagul.co.uk>
2018-03-19 14:50:25 +0000

    Ignore missing files when updating metadata in Asset Manager
    When testing `AssetManagerAttachmentMetadataUpdater.update_range` in
    integration we ran into some problems where `AttachmentData` objects
    didn't have a corresponding file on the file system. The lack of file
    meant that the asset hadn't been created in Asset Manager (because we
    populated Asset Manager using the files on disk) and so we saw
    exceptions when trying to retrieve the asset from Asset Manager (in
    `AssetManagerWorkerHelper#find_asset_by`).
    
    Some of these attachments have been replaced which means that, despite
    not having a file on disk, requests for them are redirected to their
    replacement. We've identified these attachments and are creating assets
    for them in Asset Manager as part of
    https://github.com/alphagov/asset-manager/issues/525. Once these
    records have been created we're confident that it's safe to ignore any
    remaining `AttachmentData` records that don't have files.

--- lib/asset_manager_attachment_metadata_updater.rb
+++ lib/asset_manager_attachment_metadata_updater.rb
@@ -1,6 +1,8 @@
 class AssetManagerAttachmentMetadataUpdater
   def self.update_range(start, finish)
     AttachmentData.find_each(start: start, finish: finish) do |attachment_data|
+      next unless File.exist?(attachment_data.file.path)
+
       [
        AssetManagerAttachmentAccessLimitedWorker,
        AssetManagerAttachmentDeleteWorker,
