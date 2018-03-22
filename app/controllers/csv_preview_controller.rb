56b9eba35534d3d2cd6260461409f0b846c9ed36
James Mead <james@floehopper.org>
2018-03-22 16:11:47 +0000

    Copy BaseAttachmentsController#fail into CsvPreviewController
    It's not obvious that the behaviour in here is all relevant (or even
    correct) for CsvPreviewController#show vs AttachmentsController#show.
    I'm hoping that I can simplify the version of the method in
    CsvPreviewController.

--- app/controllers/csv_preview_controller.rb
+++ app/controllers/csv_preview_controller.rb
@@ -14,4 +14,24 @@ class CsvPreviewController < BaseAttachmentsController
       end
     end
   end
+
+private
+
+  def fail
+    if attachment_data.unpublished?
+      redirect_url = attachment_data.unpublished_edition.unpublishing.document_path
+      redirect_to redirect_url
+    elsif attachment_data.replaced?
+      expires_headers
+      redirect_to attachment_data.replaced_by.url, status: 301
+    elsif incoming_upload_exists? upload_path
+      if image? upload_path
+        redirect_to view_context.path_to_image('thumbnail-placeholder.png')
+      else
+        redirect_to_placeholder
+      end
+    else
+      render plain: "Not found", status: :not_found
+    end
+  end
 end
