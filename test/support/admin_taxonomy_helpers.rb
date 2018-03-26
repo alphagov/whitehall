0500ead5bb2329aea46227da742cfcf211592479
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-03-26 15:03:12 +0000

    Make taxonomy test helpers more generic (for SAs)
    Previous uses of edition (e.g. edition_type) have been replaced with
    model to allow for both Editions and Statistics Announcements (SAs) to
    make use of this helper.
    
    The logic to cope with different types of editions exposing different
    fields has been refactored to a case statement, which makes adding SA
    fields easier.

--- test/support/admin_taxonomy_helpers.rb
+++ test/support/admin_taxonomy_helpers.rb
@@ -1,80 +1,84 @@
-module AdminEditionTaxonomyHelpers
+module AdminTaxonomyHelpers
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def should_allow_tagging_to_taxonomy_for(edition_type)
-      view_test "when edition is not taggable hide taxonomy UI" do
-        edition = create(edition_type)
-        edition_has_no_expanded_links(edition.content_id)
-        get :show, params: { id: edition }
+    def should_allow_tagging_to_taxonomy_for(model_type)
+      view_test "when #{model_type} is not taggable hide taxonomy UI" do
+        model = create(model_type)
+        model_has_no_expanded_links(model.content_id)
+        get :show, params: { id: model }
         refute_select '.taxonomy-topics'
       end
 
-      view_test "when edition is taggable show the taxonomy UI" do
+      view_test "when #{model_type} is taggable show the taxonomy UI" do
         organisation = create(:organisation, content_id: "ebd15ade-73b2-4eaf-b1c3-43034a42eb37")
-        edition = create(edition_type, organisations: [organisation])
+        model = create(model_type, organisations: [organisation])
         login_as(create(:gds_editor, organisation: organisation))
-        edition_has_no_expanded_links(edition.content_id)
-        get :show, params: { id: edition }
+        model_has_no_expanded_links(model.content_id)
+        get :show, params: { id: model }
         assert_select '.taxonomy-topics .btn', "Add topic"
       end
 
-      view_test "when edition is not tagged to the new taxonomy" do
+      view_test "when #{model_type} is not tagged to the new taxonomy" do
         organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
-        edition = create(edition_type, organisations: [organisation])
+        model = create(model_type, organisations: [organisation])
         login_as(create(:gds_editor, organisation: organisation))
-        edition_has_no_expanded_links(edition.content_id)
-        get :show, params: { id: edition }
+        model_has_no_expanded_links(model.content_id)
+        get :show, params: { id: model }
         refute_select '.taxonomy-topics .content'
         assert_select '.taxonomy-topics .no-content', "No topics - please add a topic before publishing"
       end
 
-      view_test "when edition is tagged to the new taxonomy" do
+      view_test "when #{model_type} is tagged to the new taxonomy" do
         organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
-        edition = create(edition_type, organisations: [organisation])
+        model = create(model_type, organisations: [organisation])
         login_as(create(:gds_editor, organisation: organisation))
-        edition_has_expanded_links(edition.content_id)
-        get :show, params: { id: edition }
+        model_has_expanded_links(model.content_id)
+        get :show, params: { id: model }
         refute_select '.taxonomy-topics .no-content'
         assert_select '.taxonomy-topics .content li', "Education, Training and Skills"
         assert_select '.taxonomy-topics .content li', "Primary Education"
       end
     end
 
-    def should_prevent_legacy_tagging_for(edition_type)
-      view_test "when edition is not taggable show legacy UI" do
-        edition = create(edition_type)
+    def should_prevent_legacy_tagging_for(model_type)
+      view_test "when #{model_type} is not taggable show legacy UI" do
+        edition = create(model_type)
         get :new
-        assert_select '#edition_topic_ids'
-        assert_select '#edition_primary_specialist_sector_tag'
-
-        unless [:fatality_notice, :statistical_data_set].include?(edition_type)
-          assert_select '#edition_policy_content_ids'
-        end
+        legacy_tag_fields_for(model_type).each { |field| assert_select field }
       end
 
-      view_test "when edition is taggable hide legacy UI" do
+      view_test "when #{model_type} is taggable hide legacy UI" do
         organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
-        edition = create(edition_type, organisations: [organisation])
+        edition = create(model_type, organisations: [organisation])
         login_as(create(:gds_editor, organisation: organisation))
         get :new
-        refute_select '#edition_policy_content_ids'
-        refute_select '#edition_topic_ids'
-        refute_select '#edition_primary_specialist_sector_tag'
+        legacy_tag_fields_for(model_type).each { |field| refute_select field }
       end
     end
   end
 
 private
 
-  def edition_has_no_expanded_links(content_id)
+  def legacy_tag_fields_for(model_type)
+    case model_type
+    when :fatality_notice, :statistical_data_set
+      %w{#edition_topic_ids #edition_primary_specialist_sector_tag}
+    when :statistics_announcement
+      %w{#statistics_announcement_topic_ids}
+    else
+      %w{#edition_topic_ids #edition_policy_content_ids #edition_primary_specialist_sector_tag}
+    end
+  end
+
+  def model_has_no_expanded_links(content_id)
     publishing_api_has_expanded_links(
       content_id:  content_id,
       expanded_links:  {}
     )
   end
 
-  def edition_has_expanded_links(content_id)
+  def model_has_expanded_links(content_id)
     publishing_api_has_expanded_links(
       content_id:  content_id,
       expanded_links:  {
