32308da7c3e012178765b7e77b056f781e167b0c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-03-27 16:20:42 +0000

    Change edition to model in legacy tag worker
    This worker is used for Editions and Statistics Announcements, so a more
    generic term 'model' is appropriate.

--- app/workers/publishing_api_legacy_tags_worker.rb
+++ app/workers/publishing_api_legacy_tags_worker.rb
@@ -2,48 +2,48 @@ class PublishingApiLegacyTagsWorker
   include Sidekiq::Worker
   API = Services.publishing_api
 
-  def perform(edition_id, edition_class)
-    edition = edition_class.constantize.find(edition_id)
-    previous_version, taxon_ids = get_taxon_ids(edition)
+  def perform(model_id, model_class)
+    model = model_class.constantize.find(model_id)
+    previous_version, taxon_ids = get_taxon_ids(model)
     parent_ids = taxon_ids.flat_map { |id| expand_parents(id) }
     legacy_ids = get_legacy_links(taxon_ids + parent_ids)
 
     links = { policy_areas: [], policies: [], topics: [] }
-    update_topics(links, legacy_ids, edition)
-    update_policies(links, legacy_ids, edition)
-    update_policy_areas(links, legacy_ids, edition)
+    update_topics(links, legacy_ids, model)
+    update_policies(links, legacy_ids, model)
+    update_policy_areas(links, legacy_ids, model)
 
     params = { previous_version: previous_version, links: links }
-    API.patch_links(edition.content_id, params)
+    API.patch_links(model.content_id, params)
   end
 
 private
 
-  def update_topics(links, legacy_ids, edition)
-    return unless edition.respond_to?(:specialist_sector_tags)
+  def update_topics(links, legacy_ids, model)
+    return unless model.respond_to?(:specialist_sector_tags)
     topics = API.get_linkables(document_type: 'topic')
       .to_a.map { |topic| topic["content_id"] } & legacy_ids
 
     links[:topics] += topics
-    edition.primary_specialist_sector_tag = topics.first
-    edition.secondary_specialist_sector_tags = topics.drop(1)
+    model.primary_specialist_sector_tag = topics.first
+    model.secondary_specialist_sector_tags = topics.drop(1)
   end
 
-  def update_policies(links, legacy_ids, edition)
-    return unless edition.respond_to?(:policy_content_ids)
+  def update_policies(links, legacy_ids, model)
+    return unless model.respond_to?(:policy_content_ids)
     policies = Policy.all.map(&:content_id) & legacy_ids
     links[:policies] += policies
 
-    edition.policy_content_ids = policies
+    model.policy_content_ids = policies
 
-    parent_policies = edition.policy_content_ids - policies
+    parent_policies = model.policy_content_ids - policies
     links[:policy_areas] += parent_policies
   end
 
-  def update_policy_areas(links, legacy_ids, edition)
-    return unless edition.respond_to?(:topics)
-    edition.topics = Topic.where(content_id: legacy_ids)
-    links[:policy_areas] += edition.topics.map(&:content_id)
+  def update_policy_areas(links, legacy_ids, model)
+    return unless model.respond_to?(:topics)
+    model.topics = Topic.where(content_id: legacy_ids)
+    links[:policy_areas] += model.topics.map(&:content_id)
   end
 
   def get_legacy_links(taxon_ids)
@@ -51,8 +51,8 @@ private
       .values.flat_map { |ls| ls["links"].fetch("legacy_taxons", []) }
   end
 
-  def get_taxon_ids(edition)
-    response = API.get_links(edition.content_id).to_h
+  def get_taxon_ids(model)
+    response = API.get_links(model.content_id).to_h
     [response["version"], response["links"].fetch("taxons", [])]
   end
 
