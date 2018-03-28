150ea3c5d0794202cd29aca0fc52b947ec2b15c7
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-03-28 11:20:02 +0100

    Extend the associations translation worker
    To support non edition models, such as statistical announcements.

--- app/workers/taxonomy_associations_translation_worker.rb
+++ app/workers/taxonomy_associations_translation_worker.rb
@@ -2,13 +2,16 @@ class TaxonomyAssociationsTranslationWorker < WorkerBase
   sidekiq_options queue: 'publishing_api'
 
   def perform(model_name, id)
-    edition = class_for(model_name).unscoped.find(id)
-    return if edition.nil?
+    model = class_for(model_name).unscoped.find(id)
+    return if model.nil?
+
+    taxon_content_ids = taxon_content_ids_for_model(model)
+    return if taxon_content_ids.nil?
 
     Services.publishing_api.patch_links(
       edition.content_id,
       links: {
-        taxons: applicable_taxon_content_ids(edition)
+        taxons: taxon_content_ids
       }
     )
   end
@@ -19,13 +22,36 @@ private
     model_name.constantize
   end
 
-  def applicable_taxon_content_ids(edition)
+  def taxon_content_ids_for_model(model)
+    if model.is_a?(Edition)
+      applicable_taxon_content_ids_for_edition(model)
+    elsif model.try(:can_publish_to_publishing_api?)
+      applicable_taxon_content_ids_for_links(model)
+    end
+  end
+
+  def applicable_taxon_content_ids_for_edition(edition)
     taxon_content_ids =
       Taxonomy::AssociationsTranslation
         .mapped_taxon_content_ids_for_edition(edition)
 
     logger.info(
       "#{self.class.name}: #{edition.content_id} taxons: "\
+      "#{taxon_content_ids.join(' ,')}"
+    )
+
+    taxon_content_ids
+  end
+
+  def applicable_taxon_content_ids_for_links(model)
+    presenter = PublishingApiPresenters.presenter_for(model)
+
+    taxon_content_ids =
+      Taxonomy::AssociationsTranslation
+        .mapped_taxon_content_ids_for_links(presenter.links)
+
+    logger.info(
+      "#{self.class.name}: #{presenter.content_id} taxons: "\
       "#{taxon_content_ids.join(' ,')}"
     )
 
