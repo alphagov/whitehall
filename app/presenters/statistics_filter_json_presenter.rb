6f4820442425bc839fe8176716ca697523dd2ae1
Michael Walker <mike@barrucadu.co.uk>
2018-04-10 13:41:04 +0100

    Set the atom and email URLs for the statistics filter

--- app/presenters/statistics_filter_json_presenter.rb
+++ app/presenters/statistics_filter_json_presenter.rb
@@ -1,4 +1,9 @@
 class StatisticsFilterJsonPresenter < DocumentFilterPresenter
+  def as_json(options = nil)
+    super.merge atom_feed_url: context.filter_atom_feed_url,
+                email_signup_url: context.new_email_signups_path(email_signup: { feed: context.filter_atom_feed_url })
+  end
+
   def result_type
     "statistic"
   end
