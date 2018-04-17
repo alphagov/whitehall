b437af2a44135ef9c00953cb8f71331f26a65e92
Paul Bowsher <paul.bowsher@gmail.com>
2018-04-17 13:42:58 +0100

    Expand featured items for sub organisations to 6 from 5
    Sub organisation pages used to be designed so that only 5
    featured items would work well on the page. This has changed
    (although it's unclear when), so we can remove the special
    case for sub organisations.

--- app/presenters/organisation_feature_list_presenter.rb
+++ app/presenters/organisation_feature_list_presenter.rb
@@ -4,9 +4,6 @@ class OrganisationFeatureListPresenter < FeatureListPresenter
   def initialize(organisation, view_context)
     @organisation = organisation
     super(@organisation.feature_list_for_locale(I18n.locale), view_context)
-  end
-
-  def limit
-    @organisation.organisation_type.sub_organisation? ? 5 : 6
+    limit_to 6
   end
 end
