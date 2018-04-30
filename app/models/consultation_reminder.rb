0771bc72434eb3b3d801146597d9be21e7f0114b
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-04-30 11:48:48 +0100

    Log obfuscated emails for consultation reminders

--- app/models/consultation_reminder.rb
+++ app/models/consultation_reminder.rb
@@ -13,14 +13,26 @@ class ConsultationReminder
 
     def send_deadline_reminder(weeks_left:)
       Consultation.awaiting_response.closed_on((PUBLISH_DEADLINE - weeks_left).weeks.ago.to_date).each do |consultation|
+        log(consultation)
         Notifications.consultation_deadline_upcoming(consultation, weeks_left: weeks_left).deliver_now
       end
     end
 
     def send_deadline_passed_notification
       Consultation.awaiting_response.closed_on((PUBLISH_DEADLINE.weeks + 1.day).ago.to_date).each do |consultation|
+        log(consultation)
         Notifications.consultation_deadline_passed(consultation).deliver_now
       end
+    end
+
+    def log(consultation)
+      Rails.logger.info("Sending reminder for consultation ##{consultation.id} '#{consultation.title}' to #{obfuscated_email_addresses(consultation)}")
+    end
+
+    def obfuscated_email_addresses(consultation)
+      consultation.authors.uniq.map { |author|
+        author.email.gsub(/^(.{2}).*(.{2})$/, '\1*****\2')
+      }.to_sentence
     end
   end
 end
