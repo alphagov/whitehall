fb8c1ccc62c9ab08bba87366ca95ba3dfd73b4d7
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-05-04 09:38:52 +0100

    Consistent quoting

--- test/unit/workers/asset_manager_delete_asset_worker_test.rb
+++ test/unit/workers/asset_manager_delete_asset_worker_test.rb
@@ -1,16 +1,16 @@
-require 'test_helper'
+require "test_helper"
 
 class AssetManagerDeleteAssetWorkerTest < ActiveSupport::TestCase
   setup do
-    @asset_id = 'asset-id'
+    @asset_id = "asset-id"
     @asset_url = "http://asset-manager/assets/#{@asset_id}"
-    @legacy_url_path = 'legacy-url-path'
+    @legacy_url_path = "legacy-url-path"
     @worker = AssetManagerDeleteAssetWorker.new
   end
 
-  test 'deletes file from asset manager' do
+  test "deletes file from asset manager" do
     @worker.stubs(:find_asset_by).with(@legacy_url_path)
-      .returns('id' => @asset_id)
+      .returns("id" => @asset_id)
     Services.asset_manager.expects(:delete_asset).with(@asset_id)
 
     @worker.perform(@legacy_url_path)
