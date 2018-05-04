c8040b456c0b9e4865ac4b7932ff28ab15c8e3c6
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-05-04 14:04:50 +0000

    got the saving to work

--- app/controllers/admin/edition_legacy_associations_controller.rb
+++ app/controllers/admin/edition_legacy_associations_controller.rb
@@ -3,11 +3,39 @@ class Admin::EditionLegacyAssociationsController < Admin::BaseController
   before_action :enforce_permissions!
   before_action :limit_edition_access!
 
-  def edit
+  def edit; end
 
+  def update
+    @edition.assign_attributes(edition_params)
+    if updater.can_perform? && @edition.save_as(current_user)
+      updater.perform!
+    end
+    redirect_to admin_edition_path(@edition), saved_confirmation_notice
   end
 
-private
+  private
+
+  def saved_confirmation_notice
+    { notice: "The associations have been saved" }
+  end
+
+  def updater
+    @updater ||= Whitehall.edition_services.draft_updater(@edition)
+  end
+
+  def edition_params
+    @edition_params ||= params.fetch(:edition, {}).permit(*permitted_edition_attributes)
+  end
+
+  def permitted_edition_attributes
+    [
+      :primary_specialist_sector_tag,
+      secondary_specialist_sector_tags: [],
+      policy_content_ids: [],
+      topic_ids: [],
+    ]
+  end
+
   def find_edition
     @edition = Edition.find(params[:edition_id])
   end
