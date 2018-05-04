383f23801b0d3906665ce5e5897d8c41829e5eb3
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-05-04 10:12:39 +0100

    Make asset delete worker idempotent

--- app/workers/asset_manager_delete_asset_worker.rb
+++ app/workers/asset_manager_delete_asset_worker.rb
@@ -3,6 +3,8 @@ class AssetManagerDeleteAssetWorker < WorkerBase
 
   def perform(legacy_url_path)
     attributes = find_asset_by(legacy_url_path)
+    return if attributes["deleted"]
+
     asset_manager.delete_asset(attributes['id'])
   end
 end
--- test/unit/workers/asset_manager_delete_asset_worker_test.rb
+++ test/unit/workers/asset_manager_delete_asset_worker_test.rb
@@ -15,4 +15,14 @@ class AssetManagerDeleteAssetWorkerTest < ActiveSupport::TestCase
 
     @worker.perform(@legacy_url_path)
   end
+
+  test "does not attempt a delete if the asset is already deleted" do
+    @worker.stubs(:find_asset_by).with(@legacy_url_path).returns(
+      "id" => @asset_id,
+      "deleted" => true,
+    )
+    Services.asset_manager.expects(:delete_asset).never
+
+    @worker.perform(@legacy_url_path)
+  end
 end
