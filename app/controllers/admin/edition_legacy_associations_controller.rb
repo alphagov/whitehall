99defd9302bdf1c5963d3c84a1e8880dca7ec08c
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-05-08 09:49:02 +0000

    Remove the validation of policy area on save.
    We want to be able to save the edition and tag it outside of the
    edit page.

--- app/controllers/admin/edition_legacy_associations_controller.rb
+++ app/controllers/admin/edition_legacy_associations_controller.rb
@@ -19,7 +19,7 @@ class Admin::EditionLegacyAssociationsController < Admin::BaseController
     redirect_to admin_edition_path(@edition), saved_confirmation_notice
   end
 
-  private
+private
 
   def saved_confirmation_notice
     { notice: "The associations have been saved" }
@@ -36,9 +36,11 @@ class Admin::EditionLegacyAssociationsController < Admin::BaseController
   end
 
   def clean_blank_values(edition_params)
-    edition_params[:policy_content_ids] ?
-      edition_params.merge(policy_content_ids: @edition_params[:policy_content_ids].reject(&:blank?)) :
+    if edition_params[:policy_content_ids]
+      edition_params.merge(policy_content_ids: @edition_params[:policy_content_ids].reject(&:blank?))
+    else
       edition_params
+    end
   end
 
   def permitted_edition_attributes
