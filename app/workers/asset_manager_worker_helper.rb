a8cd7973281c6c732a756ac19901456a87401259
Kevin Dew <kevindew@me.com>
2018-05-08 17:24:16 +0100

    Strip prefix slash from asset manager assets
    The `asset_manager_path` method on an AttachmentData model tends to
    return a path with a slash prefix and unfortunately the corresponding
    method in GdsApi Adapters expects a slash not to be present [1].
    
    This change removes the double slashing from our requests without having
    to change the GDS Api method interface.
    
    [1]: https://github.com/alphagov/gds-api-adapters/blob/bc2df4eb1dd1d3496114778ba01c8d8928365271/lib/gds_api/asset_manager.rb#L139

--- app/workers/asset_manager_worker_helper.rb
+++ app/workers/asset_manager_worker_helper.rb
@@ -5,7 +5,8 @@ module AssetManagerWorkerHelper
   private :asset_manager
 
   def find_asset_by(legacy_url_path)
-    attributes = asset_manager.whitehall_asset(legacy_url_path).to_hash
+    path = legacy_url_path.sub(/^\//, '')
+    attributes = asset_manager.whitehall_asset(path).to_hash
     url = attributes['id']
     id = url[/\/assets\/(.*)/, 1]
     attributes.merge('id' => id, 'url' => url)
