146afc7e55dea90e01a8284b9d56a5774598106c
Kevin Dew <kevindew@me.com>
2018-05-08 17:57:02 +0100

    Raise an error when an asset is not found
    This wraps the GdsApi::HTTPNotFound in a more specific error for when an
    asset is not available on asset manager. This is so this error can be
    caught more easily in workers.

--- app/workers/asset_manager_worker_helper.rb
+++ app/workers/asset_manager_worker_helper.rb
@@ -1,4 +1,11 @@
 module AssetManagerWorkerHelper
+  class AssetManagerAssetNotFound < StandardError
+    def initialize(legacy_url_path)
+      message = "Asset with legacy URL path #{legacy_url_path} is not on Asset Manager"
+      super(message)
+    end
+  end
+
   def asset_manager
     Services.asset_manager
   end
@@ -10,6 +17,9 @@ module AssetManagerWorkerHelper
     url = attributes['id']
     id = url[/\/assets\/(.*)/, 1]
     attributes.merge('id' => id, 'url' => url)
+  rescue GdsApi::HTTPNotFound
+    raise AssetManagerAssetNotFound.new(legacy_url_path)
   end
+
   private :find_asset_by
 end
