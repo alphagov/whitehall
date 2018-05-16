420e5e2edd94b1ade1aeffd7231fcec63d542a8c
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-05-16 13:21:05 +0100

    Separate world from topic taxons in RedisCacheAdapter
    This change adds functionality to cache world taxons separately in Redis.
    
    To populate the new Redis cache with world taxons, the following rake task
    will need to be run after deployment:
    
    `bundle exec rake taxonomy:rebuild_cache`

--- lib/taxonomy/redis_cache_adapter.rb
+++ lib/taxonomy/redis_cache_adapter.rb
@@ -1,6 +1,7 @@
 module Taxonomy
   class RedisCacheAdapter
     TAXONS_CACHE_KEY = "topic_taxonomy_taxons".freeze
+    WORLD_TAXONS_CACHE_KEY = "world_taxonomy_taxons".freeze
 
     def initialize(redis_client: Redis.current, adapter: PublishingApiAdapter.new)
       @redis_client = redis_client
@@ -11,9 +12,18 @@ module Taxonomy
       JSON.parse @redis_client.get(TAXONS_CACHE_KEY)
     end
 
+    def world_taxon_data
+      JSON.parse @redis_client.get(WORLD_TAXONS_CACHE_KEY)
+    end
+
     def rebuild_caches
       data = @adapter.taxon_data
       @redis_client.set TAXONS_CACHE_KEY, JSON.dump(data)
+    end
+
+    def rebuild_world_taxon_caches
+      data = @adapter.world_taxon_data
+      @redis_client.set WORLD_TAXONS_CACHE_KEY, JSON.dump(data)
     end
   end
 end
