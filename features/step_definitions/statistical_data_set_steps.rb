b9656d2f59246f16ea61db9e19fc9c3e4abb704b
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2018-05-18 16:47:06 +0000

    Remove legacy tagging form from statistical data sets

--- features/step_definitions/statistical_data_set_steps.rb
+++ features/step_definitions/statistical_data_set_steps.rb
@@ -1,5 +1,7 @@
 When(/^I draft a new statistical data set "([^"]*)" for organisation "([^"]*)"$/) do |title, organisation_name|
   begin_drafting_statistical_data_set(title: title)
   set_lead_organisation_on_document(Organisation.find_by(name: organisation_name))
-  click_button "Save"
+  click_button "Next"
+  select "Policy 1", from: "Policies"
+  click_button "Save legacy associations"
 end
