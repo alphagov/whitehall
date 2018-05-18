db4d10180d12a9455e4a403d7c6a0fd23df25bf0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-05-18 15:23:01 +0100

    Create a form to tag to the World taxonomy
    This form is where users will tag to the world taxonomy and submit their changes
    The controller to route them to this page, and process the results of the tagging
    will be created in the next commit.
    
    This is a duplicate of TaxonomyTagForm. It was not possible to re-use that
    class as it calls on TopicTaxonomy to embed "invisible taxons" in a hidden
    field in the view.
    
    Right now there aren't any hidden world taxons, so this field isn't needed
    and it didn't feel right to populate the World taxonomy form with data from
    the topic taxonomy.

---
+++ app/models/world_taxonomy_tag_form.rb
@@ -0,0 +1,26 @@
+class WorldTaxonomyTagForm
+  include ActiveModel::Model
+
+  attr_accessor :selected_taxons, :content_id, :previous_version
+
+  def self.load(content_id)
+    begin
+      content_item = Services.publishing_api.get_links(content_id)
+
+      selected_taxons = content_item["links"]["taxons"] || []
+      previous_version = content_item["version"] || 0
+    rescue GdsApi::HTTPNotFound
+      # TODO: This is a workaround, because Publishing API
+      # returns 404 when the document exists but there are no links.
+      # This can be removed when that changes.
+      selected_taxons = []
+      previous_version = 0
+    end
+
+    new(
+      selected_taxons: selected_taxons,
+      content_id: content_id,
+      previous_version: previous_version
+    )
+  end
+end
