3695441cb30dcd83d9d97344e6733da3dc525d1e
Michael Walker <mike@barrucadu.co.uk>
2018-05-21 13:25:53 +0100

    Add a separate Sidekiq queue for asset-manager tasks

--- app/workers/asset_manager_attachment_replacement_id_update_worker.rb
+++ app/workers/asset_manager_attachment_replacement_id_update_worker.rb
@@ -1,4 +1,6 @@
 class AssetManagerAttachmentReplacementIdUpdateWorker < WorkerBase
+  sidekiq_options queue: 'asset_manager'
+
   def perform(attachment_data_id)
     attachment_data = AttachmentData.find_by(id: attachment_data_id)
     return unless attachment_data.present? && attachment_data.replaced?
