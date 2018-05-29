2cf2e7c494e6f7b804844c81d5c22b4a2630a7b0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-05-29 17:23:18 +0100

    Remove `education` from function names
    The functions refer to more than just the education taxonomy
    branch, so the names should be updated to be more generic.

--- app/models/statistics_announcement.rb
+++ app/models/statistics_announcement.rb
@@ -212,12 +212,12 @@ class StatisticsAnnouncement < ApplicationRecord
   def can_be_tagged_to_taxonomy?
     organisations_content_ids = organisations.map(&:content_id)
 
-    organisations_in_education_tagging_beta?(organisations_content_ids)
+    organisations_in_topic_tagging_beta?(organisations_content_ids)
   end
 
 private
 
-  def organisations_in_education_tagging_beta?(org_content_ids)
+  def organisations_in_topic_tagging_beta?(org_content_ids)
     (org_content_ids & Whitehall.organisations_in_tagging_beta).present?
   end
 
