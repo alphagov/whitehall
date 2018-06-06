2e932e0941aa312803b1bf047e0110109ae1040a
Michael Walker <mike@barrucadu.co.uk>
2018-06-06 16:19:24 +0100

    Remove unused AssetManagerAndQuarantinedFileStorage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ require 'whitehall/quarantined_file_storage'
 CarrierWave.configure do |config|
   config.storage_engines[:quarantined_file] = 'Whitehall::QuarantinedFileStorage'
   config.storage_engines[:asset_manager] = 'Whitehall::AssetManagerStorage'
-  config.storage_engines[:asset_manager_and_quarantined_file_storage] = 'Whitehall::AssetManagerAndQuarantinedFileStorage'
   config.storage Whitehall::QuarantinedFileStorage
   config.enable_processing = false if Rails.env.test?
   config.cache_dir = Rails.root.join "carrierwave-tmp"
