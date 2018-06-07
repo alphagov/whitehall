bfc0b1e649132b28555c67f83eb1c80e86d17dc6
Michael Walker <mike@barrucadu.co.uk>
2018-06-07 10:18:58 +0100

    Remove unused VirusScanHelpers.simulate_virus_scan_infected

--- test/support/virus_scan_helpers.rb
+++ test/support/virus_scan_helpers.rb
@@ -28,22 +28,6 @@ module VirusScanHelpers
     end
   end
 
-  def self.simulate_virus_scan_infected(*uploaders)
-    if uploaders.empty?
-      uploaders = AttachmentData.all.map(&:file) + ImageData.all.map(&:file)
-    end
-
-    uploaders.each do |uploader|
-      absolute_path = File.join(Whitehall.incoming_uploads_root, uploader.relative_path)
-      target_dir = File.join(Whitehall.infected_uploads_root, File.dirname(uploader.relative_path))
-      if File.exist?(absolute_path)
-        FileUtils.mkdir_p(target_dir)
-        FileUtils.cp(absolute_path, target_dir)
-        FileUtils.rm(absolute_path)
-      end
-    end
-  end
-
   def self.erase_test_files
     raise "Only use VirusScanHelpers.erase_test_files in test mode" unless Rails.env.test?
     folders = [
