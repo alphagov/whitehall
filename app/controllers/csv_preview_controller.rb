fa7b9bc9d6ad1c1739dbd40e30289b29870fbd4a
Michael Walker <mike@barrucadu.co.uk>
2018-06-08 11:31:48 +0100

    Merge BaseAttachmentsController and CsvPreviewController

--- app/controllers/csv_preview_controller.rb
+++ app/controllers/csv_preview_controller.rb
@@ -1,4 +1,4 @@
-class CsvPreviewController < BaseAttachmentsController
+class CsvPreviewController < ApplicationController
   def show
     respond_to do |format|
       format.html do
@@ -50,5 +50,28 @@ private
 
   def upload_exists?
     @csv_response.status == 206
+  end
+
+  def set_slimmer_template
+    slimmer_template 'chromeless'
+  end
+
+  def attachment_data
+    @attachment_data ||= AttachmentData.find(params[:id])
+  end
+
+  def expires_headers
+    if current_user.nil?
+      expires_in(Whitehall.uploads_cache_max_age, public: true)
+    else
+      expires_now
+    end
+  end
+
+  def redirect_to_placeholder
+    # Cache is explicitly 1 minute to prevent the virus redirect beng
+    # cached by CDNs.
+    expires_in(1.minute, public: true)
+    redirect_to placeholder_url
   end
 end
