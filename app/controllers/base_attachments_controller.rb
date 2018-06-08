aca5bfff911070b6565f56f4bc523320f6350cfc
Michael Walker <mike@barrucadu.co.uk>
2018-06-08 11:17:33 +0100

    Remove unused BaseAttachmentsController methods

--- app/controllers/base_attachments_controller.rb
+++ app/controllers/base_attachments_controller.rb
@@ -1,28 +1,6 @@
 class BaseAttachmentsController < ApplicationController
 protected
 
-  def attachment_visible?
-    upload_exists?(upload_path) && attachment_data.visible_to?(current_user)
-  end
-
-  def fail
-    if attachment_data.unpublished?
-      redirect_url = attachment_data.unpublished_edition.unpublishing.document_path
-      redirect_to redirect_url
-    elsif attachment_data.replaced?
-      expires_headers
-      redirect_to attachment_data.replaced_by.url, status: 301
-    elsif incoming_upload_exists? upload_path
-      if image? upload_path
-        redirect_to view_context.path_to_image('thumbnail-placeholder.png')
-      else
-        redirect_to_placeholder
-      end
-    else
-      render plain: "Not found", status: :not_found
-    end
-  end
-
   def set_slimmer_template
     slimmer_template 'chromeless'
   end
@@ -59,19 +37,10 @@ protected
     ['.jpg', '.jpeg', '.png', '.gif'].include?(File.extname(path))
   end
 
-  def incoming_upload_exists?(path)
-    path = path.sub(Whitehall.clean_uploads_root, Whitehall.incoming_uploads_root)
-    File.exist?(path)
-  end
-
   def redirect_to_placeholder
     # Cache is explicitly 1 minute to prevent the virus redirect beng
     # cached by CDNs.
     expires_in(1.minute, public: true)
     redirect_to placeholder_url
-  end
-
-  def upload_exists?(path)
-    File.exist?(path) && file_is_clean?(path)
   end
 end
