36455080675858206aac47d0f8788ce2c4d27000
Michael Walker <mike@barrucadu.co.uk>
2018-06-08 09:59:32 +0100

    Remove unused QuaratinedFileStorage + shrink SanitizedFile
    There's a kind of circular dependency between the two.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,7 @@
-require 'whitehall/quarantined_file_storage'
+require 'whitehall/carrier_wave/sanitized_file'
 
 CarrierWave.configure do |config|
-  config.storage_engines[:quarantined_file] = 'Whitehall::QuarantinedFileStorage'
   config.storage_engines[:asset_manager] = 'Whitehall::AssetManagerStorage'
-  config.storage Whitehall::QuarantinedFileStorage
   config.enable_processing = false if Rails.env.test?
   config.cache_dir = Rails.root.join "carrierwave-tmp"
   config.validate_integrity = false
