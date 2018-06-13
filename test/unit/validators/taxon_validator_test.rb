d7d8d99003fe11376fea5ce5dd28dc736365f286
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-06-13 10:56:51 +0100

    Update validation to check all editions are tagged
    Update the validation to check that editions created by any
    organisation are tagged to the topic taxonomy.

--- test/unit/validators/taxon_validator_test.rb
+++ test/unit/validators/taxon_validator_test.rb
@@ -7,7 +7,6 @@ class TaxonValidatorTest < ActiveSupport::TestCase
 
   test 'is invalid when edition has not been tagged to a taxon' do
     edition = create(:draft_edition)
-    edition.stubs(:can_be_tagged_to_taxonomy?).returns(true)
 
     publishing_api_has_links(
       "content_id" => edition.content_id,
@@ -33,7 +32,6 @@ class TaxonValidatorTest < ActiveSupport::TestCase
 
   test 'is valid when edition has been tagged to a taxon' do
     edition = create(:draft_edition)
-    edition.stubs(:can_be_tagged_to_taxonomy?).returns(true)
 
     publishing_api_has_links(
       "content_id" => edition.content_id,
