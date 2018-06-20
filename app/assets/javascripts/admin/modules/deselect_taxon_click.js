96e48c8b5302197a3dcd55c517634fc6ff673eb0
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-06-20 13:35:28 +0100

    Rename helper to taxonomyTreeHelper
    To make it clearer what this is.

--- app/assets/javascripts/admin/modules/deselect_taxon_click.js
+++ app/assets/javascripts/admin/modules/deselect_taxon_click.js
@@ -2,14 +2,14 @@
   "use strict";
 
   Modules.DeselectTaxonClick = function () {
-    var helper = window.GOVUK.taxonomyTreeHelper;
+    var taxonomyTreeHelper = window.GOVUK.taxonomyTreeHelper;
     this.start = function (container) {
       var deselectTaxonClick = function () {
         var taxon_name = $(container).closest('.taxon-breadcrumb').find('li:last').text() ;
         $("input[data-taxon-name=\"" + taxon_name + "\"]").each(function(idx, el){
           $(el).prop('checked', false);
-          if (!helper.hasCheckedSiblings(el)){
-            helper.uncheckAncestors(el);
+          if (!taxonomyTreeHelper.hasCheckedSiblings(el)){
+            taxonomyTreeHelper.uncheckAncestors(el);
           }
         });
         $('.topic-tree').trigger('change');
