37d638cb2daaeebe63ce8ecbe174ace6bdc697be
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-06-20 11:03:22 +0100

    Use two space indentation in taxonomy_tree_helper.js

--- app/assets/javascripts/admin/helpers/taxonomy_tree_helper.js
+++ app/assets/javascripts/admin/helpers/taxonomy_tree_helper.js
@@ -1,46 +1,46 @@
 (function () {
-    "use strict";
-    window.GOVUK = window.GOVUK || {};
+  "use strict";
+  window.GOVUK = window.GOVUK || {};
 
-    window.GOVUK.taxonomyTreeHelper = {
-        ancestors: function ancestors(element) {
-            var $parents = $(element).parents('.topics,.topic-tree');
-            return $parents.prev('p').find('input[type="checkbox"]');
-        },
+  window.GOVUK.taxonomyTreeHelper = {
+    ancestors: function ancestors(element) {
+      var $parents = $(element).parents('.topics,.topic-tree');
+      return $parents.prev('p').find('input[type="checkbox"]');
+    },
 
-        descendants: function descendants(element) {
-            return $(element).closest('p').next('.topics').find('input[type="checkbox"]');
-        },
+    descendants: function descendants(element) {
+      return $(element).closest('p').next('.topics').find('input[type="checkbox"]');
+    },
 
-        /**
-         Check all ancestor topics.
-         */
-        checkAncestors: function checkAncestors(element) {
-            this.ancestors(element).prop('checked', true);
-        },
+    /**
+       Check all ancestor topics.
+    */
+    checkAncestors: function checkAncestors(element) {
+      this.ancestors(element).prop('checked', true);
+    },
 
-        /**
-         Uncheck all ancestor topics.
-         */
-        uncheckAncestors: function uncheckAncestors(element) {
-            this.ancestors(element).prop('checked', false);
-        },
+    /**
+       Uncheck all ancestor topics.
+    */
+    uncheckAncestors: function uncheckAncestors(element) {
+      this.ancestors(element).prop('checked', false);
+    },
 
-        /**
-         Uncheck all descendant topics.
-         */
-        uncheckDescendants: function uncheckDescendants(element) {
-            this.descendants(element).prop('checked', false);
-        },
+    /**
+       Uncheck all descendant topics.
+    */
+    uncheckDescendants: function uncheckDescendants(element) {
+      this.descendants(element).prop('checked', false);
+    },
 
-        /**
-         Check if a sibling topic (or its children) are checked.
-         If any of the siblings, children are checked we expect the sibling to be checked too.
-         */
-        hasCheckedSiblings: function hasCheckedSiblings(element) {
-            var $p = $(element).closest('.topics').children('p');
-            var $checkedSiblings = $p.find('input:checked');
-            return $checkedSiblings.length > 0;
-        }
-    };
+    /**
+       Check if a sibling topic (or its children) are checked.
+       If any of the siblings, children are checked we expect the sibling to be checked too.
+    */
+    hasCheckedSiblings: function hasCheckedSiblings(element) {
+      var $p = $(element).closest('.topics').children('p');
+      var $checkedSiblings = $p.find('input:checked');
+      return $checkedSiblings.length > 0;
+    }
+  };
 }());
