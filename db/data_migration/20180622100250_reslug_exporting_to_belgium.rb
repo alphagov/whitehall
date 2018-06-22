e6d3603e804d8df51d71b3c7fee691e17279c3e4
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-06-22 11:03:33 +0100

    Reslug “Exporting to Belgium”
    To get rid of the `--2` suffix.

---
+++ db/data_migration/20180622100250_reslug_exporting_to_belgium.rb
@@ -0,0 +1,12 @@
+old_slug = "exporting-to-belgium--2"
+new_slug = "exporting-to-belgium"
+
+document = Document.find_by!(slug: old_slug)
+edition = document.editions.published.last
+
+Whitehall::SearchIndex.delete(edition)
+
+document.update_attributes!(slug: new_slug)
+PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+
+puts "#{old_slug} -> #{new_slug}"
