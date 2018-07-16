0ee172b82111aa1b0c124d7cf584af42e6156f02
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-07-16 13:49:51 +0100

    Retry sending attachment data if the asset is not yet uploaded

--- app/workers/asset_manager_attachment_data_worker.rb
+++ app/workers/asset_manager_attachment_data_worker.rb
@@ -3,7 +3,10 @@ class AssetManagerAttachmentDataWorker < WorkerBase
 
   def perform(attachment_data_id)
     attachment_data = AttachmentData.find(attachment_data_id)
-    return unless attachment_data.uploaded_to_asset_manager_at
+
+    unless attachment_data.uploaded_to_asset_manager_at
+      raise AttachmentNotYetUploadedError
+    end
 
     [
       AssetManagerAttachmentDraftStatusUpdateWorker,
@@ -18,4 +21,7 @@ class AssetManagerAttachmentDataWorker < WorkerBase
       AssetManagerAttachmentReplacementIdUpdateWorker.new.perform(replaced_attachment_data.id)
     end
   end
+end
+
+class AttachmentNotYetUploadedError < StandardError
 end
