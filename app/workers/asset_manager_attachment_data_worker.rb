fe2b1af0bdd0e84f0aa328bf5cc302a9f65d660b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-07-16 13:52:28 +0100

    Refactor attachment data worker
    This is a small refactor to reduce the number of functions in this
    worker.

--- app/workers/asset_manager_attachment_data_worker.rb
+++ app/workers/asset_manager_attachment_data_worker.rb
@@ -5,35 +5,17 @@ class AssetManagerAttachmentDataWorker < WorkerBase
     attachment_data = AttachmentData.find(attachment_data_id)
     return unless attachment_data.uploaded_to_asset_manager_at
 
-    draft_status_updater attachment_data_id
-    link_header_updater attachment_data_id
-    access_limited_updater attachment_data_id
-    deleter attachment_data_id
-
-    AttachmentData.where(replaced_by: attachment_data).find_each do |data|
-      replacement_id_updater data
+    [
+      AssetManagerAttachmentDraftStatusUpdateWorker,
+      AssetManagerAttachmentLinkHeaderUpdateWorker,
+      AssetManagerAttachmentAccessLimitedWorker,
+      AssetManagerAttachmentDeleteWorker,
+    ].each do |worker|
+      worker.new.perform(attachment_data_id)
     end
-  end
 
-private
-
-  def draft_status_updater(attachment_data_id)
-    AssetManagerAttachmentDraftStatusUpdateWorker.new.perform attachment_data_id
-  end
-
-  def link_header_updater(attachment_data_id)
-    AssetManagerAttachmentLinkHeaderUpdateWorker.new.perform(attachment_data_id)
-  end
-
-  def access_limited_updater(attachment_data_id)
-    AssetManagerAttachmentAccessLimitedWorker.new.perform(attachment_data_id)
-  end
-
-  def replacement_id_updater(attachment_data_id)
-    AssetManagerAttachmentReplacementIdUpdateWorker.new.perform(attachment_data_id)
-  end
-
-  def deleter(attachment_data_id)
-    AssetManagerAttachmentDeleteWorker.new.perform(attachment_data_id)
+    AttachmentData.where(replaced_by: attachment_data).find_each do |replaced_attachment_data|
+      AssetManagerAttachmentReplacementIdUpdateWorker.new.perform(replaced_attachment_data.id)
+    end
   end
 end
