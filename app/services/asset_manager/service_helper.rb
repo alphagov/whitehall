aef4693896e775a698ab4d341e3e9cd7d6d26f2e
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-07-18 08:36:11 +0100

    Refactor AssetManager::ServiceHelper

--- app/services/asset_manager/service_helper.rb
+++ app/services/asset_manager/service_helper.rb
@@ -1,15 +1,15 @@
 module AssetManager::ServiceHelper
   class AssetManagerAssetNotFound < StandardError
     def initialize(legacy_url_path)
-      message = "Asset with legacy URL path #{legacy_url_path} is not on Asset Manager"
-      super(message)
+      super("Asset '#{legacy_url_path}' is not in Asset Manager")
     end
   end
+
+private
 
   def asset_manager
     Services.asset_manager
   end
-  private :asset_manager
 
   def find_asset_by(legacy_url_path)
     path = legacy_url_path.sub(/^\//, '')
@@ -20,6 +20,4 @@ module AssetManager::ServiceHelper
   rescue GdsApi::HTTPNotFound
     raise AssetManagerAssetNotFound.new(legacy_url_path)
   end
-
-  private :find_asset_by
 end
