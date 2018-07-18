69ab740a01e8c0ef29c45e3fec9b21452bcca122
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-07-18 13:21:53 +0100

    Refactor AssetManagerAttachmentRedirectUrlUpdateWorker
    This now calls a `AssetManager::AttachmentRedirectUrlUpdater`
    service.

--- app/workers/asset_manager_attachment_redirect_url_update_worker.rb
+++ app/workers/asset_manager_attachment_redirect_url_update_worker.rb
@@ -1,23 +1,10 @@
 class AssetManagerAttachmentRedirectUrlUpdateWorker < WorkerBase
-  sidekiq_options queue: 'asset_manager'
+  sidekiq_options queue: "asset_manager"
 
   def perform(attachment_data_id)
     attachment_data = AttachmentData.find_by(id: attachment_data_id)
     return unless attachment_data.present?
-    redirect_url = nil
-    if attachment_data.unpublished?
-      redirect_url = attachment_data.unpublished_edition.unpublishing.document_url
-    end
-    enqueue_job(attachment_data, attachment_data.file, redirect_url)
-    if attachment_data.pdf?
-      enqueue_job(attachment_data, attachment_data.file.thumbnail, redirect_url)
-    end
-  end
 
-private
-
-  def enqueue_job(attachment_data, uploader, redirect_url)
-    legacy_url_path = uploader.asset_manager_path
-    AssetManager::AssetUpdater.call(attachment_data, legacy_url_path, 'redirect_url' => redirect_url)
+    AssetManager::AttachmentRedirectUrlUpdater.call(attachment_data)
   end
 end
