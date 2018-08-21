1fa22e9dff2f297085ccfbb24be21ba1df85e747
Steve Laing <steve.laing@gmail.com>
2018-08-21 17:05:55 +0100

    Use govuk_test and selenium chrome driver
    Poltergeist is unmaintained so switch driver.

--- features/step_definitions/image_steps.rb
+++ features/step_definitions/image_steps.rb
@@ -1,6 +1,7 @@
 When(/^I select an image for the (?:detailed guide|publication)$/) do
   within ".images" do
     attach_file "File", jpg_image
+    find(".js-upload-image-input").click # Click event necessary for fieldset cloning.
     fill_in "Alt text", with: "minister of funk", match: :first
   end
 end
