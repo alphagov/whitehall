6e5fd607b38f7ca1342215310d647585037514fe
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2018-10-18 16:28:17 +0100

    Revert "Don't override notify! in ScheduledEditionPublisher"

--- app/services/scheduled_edition_publisher.rb
+++ app/services/scheduled_edition_publisher.rb
@@ -5,6 +5,24 @@ class ScheduledEditionPublisher < EditionPublisher
 
 private
 
+  def notify!
+    super
+  # We cannot afford for documents scheduled for publishing in production to
+  # fail so we catch and continue if there are problems in any of the
+  # registered listeners on scheduled publishing.
+  # Further work is required to make the notification bus more robust.
+  rescue StandardError => e
+    if Rails.env.production?
+      GovukError.notify(e,
+        extra: {
+          error_message: "Exception raised during scheduled publishing attempt: '#{e.message}'",
+          edition_id: edition.id
+        })
+    else
+      raise e
+    end
+  end
+
   def failure_reasons
     @failure_reasons ||= [].tap do |reasons|
       reasons << 'Only scheduled editions can be published with ScheduledEditionPublisher' unless scheduled_for_publication?
