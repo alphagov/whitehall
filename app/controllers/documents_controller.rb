8e7df256b4693c5159223685dd108ddeb01214e7
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-10-23 12:26:14 +0000

    Change search backend depending on filter type

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -9,7 +9,12 @@ class DocumentsController < PublicFacingController
 
 private
 
-  def build_document_filter
+  def build_document_filter(filter_type = nil)
+    search_backend = if filter_type == "announcements"
+                       Whitehall::DocumentFilter::SearchRummager
+                     else
+                       Whitehall::DocumentFilter::AdvancedSearchRummager
+                     end
     search_backend.new(cleaned_document_filter_params)
   end
 
