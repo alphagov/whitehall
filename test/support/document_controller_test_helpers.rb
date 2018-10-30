213885f66c1565f626521600cb9fb7f6840f265d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-10-30 14:52:02 +0000

    Update statistics controller tests

--- test/support/document_controller_test_helpers.rb
+++ test/support/document_controller_test_helpers.rb
@@ -393,14 +393,18 @@ module DocumentControllerTestHelpers
 
     def should_return_json_suitable_for_the_document_filter(document_type)
       include DocumentFilterHelpers
-
+      announcement = %i(news_article speech).include?(document_type)
       view_test "index requested as JSON includes a count of #{document_type}" do
         rummager = stub
-        with_stubbed_rummager(rummager) do
-          rummager.expects(:search).returns('results' =>
-                                              [{ 'format' => document_type.to_s,
-                                                 'content_id' => 1 }])
-
+        with_stubbed_rummager(rummager, announcement) do
+          if announcement
+            rummager.expects(:search).returns('results' =>
+              [{ 'format' => document_type.to_s }])
+          else
+            rummager.expects(:advanced_search).returns('results' =>
+              [{ 'format' => document_type.to_s,
+                 'public_timestamp' => Time.zone.now.to_s }])
+          end
           get :index, format: :json
 
           assert_equal 1, ActiveSupport::JSON.decode(response.body)["count"]
@@ -409,10 +413,13 @@ module DocumentControllerTestHelpers
 
       view_test "index requested as JSON includes the total pages of #{document_type}" do
         rummager = stub
-        with_stubbed_rummager(rummager) do
-          rummager.expects(:search).returns('results' =>
-                                              (0..4).map { |n| { 'format' => document_type.to_s, 'content_id' => n } })
-
+        with_stubbed_rummager(rummager, announcement) do
+          if announcement
+            rummager.expects(:search).returns('results' => (0..4).map { |n| { 'format' => document_type.to_s, 'content_id' => n } })
+          else
+            rummager.expects(:advanced_search).returns('results' =>
+                                                (0..4).map { { 'format' => document_type.to_s, 'public_timestamp' => Time.zone.now.to_s } })
+          end
           with_number_of_documents_per_page(3) do
             get :index, format: :json
           end
@@ -424,8 +431,12 @@ module DocumentControllerTestHelpers
       view_test "index requested as JSON includes the current page of #{document_type}" do
         rummager = stub
         with_stubbed_rummager(rummager) do
-          doc = create(:"published_#{document_type}")
-          rummager.expects(:search).returns('results' => { 'format' => document_type.to_s, 'slug' => doc.slug })
+          if announcement
+            rummager.expects(:search).returns('results' => [{ 'format' => document_type.to_s, 'id' => 1 }])
+          else
+            rummager.expects(:advanced_search).returns('results' =>
+              [{ 'format' => document_type.to_s, 'id' => 1, 'public_timestamp' => Time.zone.now.to_s }])
+          end
 
           get :index, format: :json
           assert_equal 1, ActiveSupport::JSON.decode(response.body)["current_page"]
