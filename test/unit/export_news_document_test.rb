e4e8843190729a498872f72d15949694133e74e7
Kevin Dew <kevindew@me.com>
2018-11-07 21:51:10 +0000

    Improve the ExportNewsDocumentTest
    This makes the test run through more code by actually having a created
    edition. Still not quite sure how far to go with testing this class but
    this at least executes more of the code.

--- test/unit/export_news_document_test.rb
+++ test/unit/export_news_document_test.rb
@@ -2,8 +2,9 @@ require "test_helper"
 
 class ExportNewsDocumentTest < ActiveSupport::TestCase
   test "returns a hash representation of a document" do
-    document = FactoryBot.build(:edition, :with_document).document
+    document = FactoryBot.create(:news_article, :with_document).document
     export = ExportNewsDocument.new(document).call
     assert_instance_of Hash, export
+    assert export["editions"].count == 1
   end
 end
