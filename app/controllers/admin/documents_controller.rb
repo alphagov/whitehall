0af9dc7ab267ff4fba2ae10a4626345bc2a14298
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-01-23 15:44:15 +0000

    Support HTML Attachments with /government/admin/by-content-id/...
    Previously, with the content id for a HTML Attachment, you'd end up on
    the search page, as it's not a Document in Whitehall's internal model.
    
    This change adds support for finding the relevant document for a HTML
    Attachment, meaning that you now end up in a more helpful place.

--- app/controllers/admin/documents_controller.rb
+++ app/controllers/admin/documents_controller.rb
@@ -1,6 +1,12 @@
 class Admin::DocumentsController < Admin::BaseController
   def by_content_id
-    document = Document.find_by(content_id: params[:content_id])
+    document = (
+      Document.find_by(content_id: params[:content_id]) ||
+      # If the content_id doesn't match a document, it could be a HTML
+      # attachment
+      HtmlAttachment.find_by(content_id: params[:content_id])&.attachable
+    )
+
     url_maker = Whitehall::UrlMaker.new(host: Plek.find('whitehall'))
     if document
       redirect_to url_maker.admin_edition_path(document.latest_edition)
