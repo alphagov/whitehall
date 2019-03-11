17309a7339f5e792bb7dd56c4f50861bb79b5fcf
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-03-11 13:29:15 +0000

    Speed up document search
    This change avoids loading any extraneous data when searching for documents.  It's possible that this extra data was in use (or intended to be in use) but only the IDs and title are now used.
    
    This results in an 85% reduction in search times.

--- test/functional/admin/document_searches_controller_test.rb
+++ test/functional/admin/document_searches_controller_test.rb
@@ -22,8 +22,6 @@ class Admin::DocumentSearchesControllerTest < ActionController::TestCase
     assert_equal publication.id, publication_json['id']
     assert_equal publication.document_id, publication_json['document_id']
     assert_equal publication.title, publication_json['title']
-    assert_equal 'publication', publication_json['type']
-    assert_equal publication.display_type, publication_json['display_type']
   end
 
   view_test 'GET #show can filter by edition type and subtype' do
@@ -40,7 +38,5 @@ class Admin::DocumentSearchesControllerTest < ActionController::TestCase
     assert_equal guidance.id, publication_json['id']
     assert_equal guidance.document_id, publication_json['document_id']
     assert_equal guidance.title, publication_json['title']
-    assert_equal 'publication', publication_json['type']
-    assert_equal guidance.display_type, publication_json['display_type']
   end
 end
