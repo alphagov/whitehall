36599c982dea62c132a9c08d964ed56a3a5307d4
Michael Walker <mike@barrucadu.co.uk>
2019-05-15 11:38:55 +0100

    Remove "all" from search parameters sent to finder-frontend
    finder-frontend doesn't support "all", so make sure it's not present.

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -61,11 +61,17 @@ private
       keywords: params['keywords'],
       level_one_taxon: params['taxons'].try(:first) || params['topics'].try(:first),
       level_two_taxon: params['subtaxons'].try(:first),
-      organisations: params['departments'] || params['organisations'],
-      people: params['people'],
-      world_locations: params['world_locations'],
+      organisations: filter_query_array(params['departments'] || params['organisations']),
+      people: filter_query_array(params['people']),
+      world_locations: filter_query_array(params['world_locations']),
       public_timestamp: { from: params['from_date'], to: params['to_date'] }.compact.presence
     }.compact.merge(special_params).to_query
+  end
+
+  def filter_query_array(arr)
+    if arr.respond_to? 'reject'
+      arr.reject { |v| v == 'all' }.compact.presence
+    end
   end
 
   def publication_finder_type
