46a851c07ebd2e23bea69cac4105b9b43353fddd
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-06-05 12:08:05 +0100

    Add environment to inside-government email address
    This might make it easier for users to exclude emails from integration
    as they can filter out the email address.

--- test/unit/services/service_listeners/author_notifier_test.rb
+++ test/unit/services/service_listeners/author_notifier_test.rb
@@ -36,14 +36,31 @@ class ServiceListeners::AuthorNotifierTest < ActiveSupport::TestCase
   end
 
   test 'sets a suitable "from" including a quoted display name' do
-    edition = create(:edition)
+    def notify(environment: nil)
+      GovukAdminTemplate.environment_style = environment
+      GovukAdminTemplate.environment_label = environment.try(:titleize)
 
-    ServiceListeners::AuthorNotifier.new(edition).notify!
+      edition = create(:edition)
+      ServiceListeners::AuthorNotifier.new(edition).notify!
 
-    assert_equal 1, ActionMailer::Base.deliveries.size
+      GovukAdminTemplate.environment_style = nil
+      GovukAdminTemplate.environment_label = nil
+    end
+
+    notify
+    notify(environment: "integration")
+    notify(environment: "production")
+
+    assert_equal 3, ActionMailer::Base.deliveries.size
 
     first_notification = ActionMailer::Base.deliveries.first
     # GovukAdminTemplate.environment_label isn't set in tests, hence the space inside the brackets
-    assert_equal '"[GOV.UK ] GOV.UK publishing" <inside-government@digital.cabinet-office.gov.uk>', first_notification[:from].to_s
+    assert_equal '"[GOV.UK ] GOV.UK publishing" <inside-government+@digital.cabinet-office.gov.uk>', first_notification[:from].to_s
+
+    integration_notification = ActionMailer::Base.deliveries.second
+    assert_equal '"[GOV.UK Integration] GOV.UK publishing" <inside-government+integration@digital.cabinet-office.gov.uk>', integration_notification[:from].to_s
+
+    production_notification = ActionMailer::Base.deliveries.third
+    assert_equal '"GOV.UK publishing" <inside-government@digital.cabinet-office.gov.uk>', production_notification[:from].to_s
   end
 end
