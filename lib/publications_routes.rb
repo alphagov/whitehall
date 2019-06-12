3a396307a3192702ebe58e8b1ed731ebae0459d9
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-06-12 09:37:54 +0000

    Add finder redirect for official document status

--- lib/publications_routes.rb
+++ lib/publications_routes.rb
@@ -1,6 +1,21 @@
 module PublicationsRoutes
   DEFAULT_PUBLICATIONS_PATH = 'search/all'.freeze
   PUBLICATIONS_ROUTES = {
+    'command_and_act_papers' => {
+      base_path: 'official-documents',
+    },
+    'command_papers' => {
+      base_path: 'official-documents',
+      special_params: {
+        content_store_document_type: 'command_papers',
+      }
+    },
+    'act_papers' => {
+      base_path: 'official-documents',
+      special_params: {
+        content_store_document_type: 'act_papers',
+      }
+    },
     'consultations' => {
       base_path: 'search/policy-papers-and-consultations',
       special_params: {
