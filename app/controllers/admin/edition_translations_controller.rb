70d4b2077ee64fd53ce12f38596546bde6b0d7db
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-06-26 13:38:13 +0100

    Don't show supersede actions in document version trails
    These are in the wrong place in the list and are in any case a side effect of the publish action, so we're removing them from the user view.

--- app/controllers/admin/edition_translations_controller.rb
+++ app/controllers/admin/edition_translations_controller.rb
@@ -30,7 +30,7 @@ private
 
   def load_translated_models
     @edition_remarks = @edition.document_remarks_trail.reverse
-    @edition_history = Kaminari.paginate_array(@edition.document_version_trail.reverse).page(params[:page]).per(30)
+    @edition_history = Kaminari.paginate_array(@edition.document_version_trail(superseded: false).reverse).page(params[:page]).per(30)
     @translated_edition = LocalisedModel.new(@edition, translation_locale.code)
   end
 
