e7314bedcee26b3905ebd5de02bcf2bb346b7f12
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-07-18 14:02:41 +0100

    Configure GDS::SSO with caching for bearer token requests
    This'll reduce the number of requests that Whitehall makes to Signon
    for API user information. I think this is particularly relevant when
    Search API makes requests to Whitehall.

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -3,4 +3,5 @@ GDS::SSO.config do |config|
   config.oauth_id     = ENV['OAUTH_ID'] || "abcdefghjasndjkasndwhitehall"
   config.oauth_secret = ENV['OAUTH_SECRET'] || "secret"
   config.oauth_root_url = Plek.new.external_url_for("signon")
+  config.cache = Rails.cache
 end
