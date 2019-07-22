d86305f75197079c823f57d8333555cd416899c4
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-07-22 10:36:57 +0000

    Optionally include locked documents in EditionFilter
    In a previous commit we prevented locked documents from being returned by the
    EditionFilter to avoid them appearing in a list of documents that could be
    featured. This adds a flag to include locked documents in the list returned by
    EditionFilter as we want to display them on the index page.

--- app/models/admin/edition_filter.rb
+++ app/models/admin/edition_filter.rb
@@ -117,7 +117,7 @@ module Admin
       editions = editions.from_date(from_date) if from_date
       editions = editions.to_date(to_date) if to_date
       editions = editions.only_broken_links if only_broken_links
-      editions = editions.without_locked_documents
+      editions = editions.without_locked_documents unless include_locked_documents?
 
       editions = editions.includes(:unpublishing) if include_unpublishing?
       editions = editions.includes(:link_check_reports) if include_link_check_reports?
@@ -236,6 +236,10 @@ module Admin
 
     def only_broken_links
       options[:only_broken_links].present?
+    end
+
+    def include_locked_documents?
+      options[:include_locked_documents]
     end
 
     def include_unpublishing?
