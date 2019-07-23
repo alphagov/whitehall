2ae29c5f0d035ad2c5c804504b5822d3b9b1909b
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-07-23 14:05:15 +0100

    Include public URL for Whitehall admin links in document export API
    Some publishers use Whitehall's admin URLs in their content, instead of
    public URLs.  Whitehall replaces these with public URLs when the document
    is published.
    
    Since Content Publisher has no knowledge of the Whitehall document
    structure, these URLs need to be resolved during the export of content
    from Whitehall.

--- app/controllers/admin/export/document_controller.rb
+++ app/controllers/admin/export/document_controller.rb
@@ -1,4 +1,7 @@
 class Admin::Export::DocumentController < Admin::Export::BaseController
+  include GovspeakHelper
+  include PublicDocumentRoutesHelper
+
   self.responder = Api::Responder
 
   def show
@@ -17,13 +20,31 @@ private
 
   def edition_associations(edition)
     output = {
-               "edition": edition
+               "edition": edition,
+               "associations": {},
+               "whitehall_admin_links": []
              }
-    output[:associations] = {}
     associations = edition.class.reflect_on_all_associations.map(&:name)
     associations.each do |association|
       output[:associations][association] = edition.public_send(association)
     end
+    output[:whitehall_admin_links].concat(resolve_whitehall_admin_links(edition.body))
+    if edition.withdrawn?
+      output[:whitehall_admin_links].concat(resolve_whitehall_admin_links(edition.unpublishing.explanation))
+    end
     output
+  end
+
+  def resolve_whitehall_admin_links(body)
+    whitehall_admin_links(body).map { |link|
+      { "whitehall_admin_url": link, "public_url": public_url_for_admin_link(link) }
+    }
+  end
+
+  def public_url_for_admin_link(url)
+    edition = Whitehall::AdminLinkLookup.find_edition(url)
+    if edition.present?
+      public_document_url(edition)
+    end
   end
 end
