bc0ec8ba48047b7ad82b269ec5f9018f14c3b6c5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-07-24 09:31:08 +0100

    Add test for resolution of Whitehall URLs in export API

--- test/functional/admin/export/document_controller_test.rb
+++ test/functional/admin/export/document_controller_test.rb
@@ -15,4 +15,44 @@ class Admin::Export::DocumentControllerTest < ActionController::TestCase
     get :show, params: { id: '1' }, format: 'json'
     assert_response :forbidden
   end
+
+  test "resolves internal Whitehall URLs in edition body with a public URL" do
+    body = "Some text which contains an [internal link](/government/admin/news/2) to a public document"
+    document = create(:document)
+    create(:edition, document: document, body: body)
+
+    linked_document = create(:document, slug: 'some-article')
+    linked_edition = create(:published_edition, document: linked_document, state: 'published')
+
+    Whitehall::AdminLinkLookup.stubs(:find_edition).with('/government/admin/news/2').returns(linked_edition)
+
+    expected_whitehall_admin_links = [{
+      "whitehall_admin_url" => "/government/admin/news/2",
+      "public_url" => "www.test.gov.uk/government/generic-editions/some-article"
+    }]
+
+    login_as :export_data_user
+    get :show, params: { id: document.id }, format: 'json'
+    assert_equal expected_whitehall_admin_links, json_response['editions'][0]['whitehall_admin_links']
+  end
+
+  test "resolves internal Whitehall URLs in withdrawal explanation with a public URL" do
+    body = "Some text which contains an [internal link](/government/admin/news/2) to a public document"
+    edition = create(:withdrawn_edition)
+    edition.unpublishing.update_attribute(:explanation, body)
+
+    linked_document = create(:document, slug: 'some-article')
+    linked_edition = create(:published_edition, document: linked_document, state: 'published')
+
+    Whitehall::AdminLinkLookup.stubs(:find_edition).with('/government/admin/news/2').returns(linked_edition)
+
+    expected_whitehall_admin_links = [{
+      "whitehall_admin_url" => "/government/admin/news/2",
+      "public_url" => "www.test.gov.uk/government/generic-editions/some-article"
+    }]
+
+    login_as :export_data_user
+    get :show, params: { id: edition.document.id }, format: 'json'
+    assert_equal expected_whitehall_admin_links, json_response['editions'][-1]['whitehall_admin_links']
+  end
 end
