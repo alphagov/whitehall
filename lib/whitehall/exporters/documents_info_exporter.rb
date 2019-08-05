25fc4bf6beadb6b8c6a6e6140fba37355e62c9fe
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-08-05 10:05:28 +0100

    Add documents exporter to return documents information
    This exporter takes an array of `document_ids` and returns the following
    information about each document:
      - the document_id
      - all the locales the document has had
      - all the types the document has had (news_article_type, speech_type, publication_type, corporate_information_page_type)
      - all the lead_organisations of the most recent edition
    
    Trello:
    https://trello.com/c/Tmq0PbYg/1037-create-an-endpoint-for-getting-a-list-of-documents-from-a-publisher

---
+++ lib/whitehall/exporters/documents_info_exporter.rb
@@ -0,0 +1,64 @@
+class Whitehall::Exporters::DocumentsInfoExporter
+  attr_reader :document_ids
+
+  def initialize(document_ids)
+    @document_ids = document_ids
+  end
+
+  def call
+    document_ids.map do |doc_id|
+      {
+        document_id: doc_id,
+        document_information: {
+          locales: locales_hash[doc_id],
+          subtypes: subtypes_hash[doc_id],
+          lead_organisations: lead_orgs_for_latest_edition_hash[doc_id]
+        }
+      }
+    end
+  end
+
+  private
+
+  def locales_hash
+    @locales_hash ||= Edition
+      .joins(:translations)
+      .where(document_id: document_ids)
+      .group(:document_id)
+      .pluck(:document_id, "GROUP_CONCAT(DISTINCT(edition_translations.locale))")
+      .each_with_object({}) { |(k, v), memo| memo[k] = v.split(",") }
+  end
+
+  def subtypes_hash
+    @subtypes_hash ||= subtypes_query.each_with_object({}) do |(document_id, news, speeches, publications, corporate), memo|
+      news_article_types = news.to_s.split(",").map { |id| NewsArticleType.find_by_id(id.to_i)&.key }
+      speech_types = speeches.to_s.split(",").map { |id| SpeechType.find_by_id(id.to_i)&.key }
+      publications_types = publications.to_s.split(",").map { |id| PublicationType.find_by_id(id.to_i)&.key }
+      corporate_types = corporate.to_s.split(",").map { |id| CorporateInformationPageType.find_by_id(id.to_i)&.key }
+      memo[document_id] = [news_article_types, speech_types, publications_types, corporate_types].flatten
+    end
+  end
+
+  def lead_orgs_for_latest_edition_hash
+    @lead_orgs_for_latest_edition_hash ||= Edition
+      .joins("INNER JOIN edition_organisations eo ON eo.edition_id = editions.id")
+      .joins("INNER JOIN organisations o ON o.id = eo.organisation_id")
+      .latest_edition
+      .where(document_id: document_ids)
+      .where(eo: { lead: true })
+      .group(:document_id)
+      .pluck(:document_id, "GROUP_CONCAT(DISTINCT(o.content_id))")
+      .each_with_object({}) { |(k, v), memo| memo[k] = v.split(",") }
+  end
+
+  def subtypes_query
+    Edition
+      .where(document_id: document_ids)
+      .group(:document_id)
+      .pluck(:document_id,
+             "GROUP_CONCAT(DISTINCT(editions.news_article_type_id))",
+             "GROUP_CONCAT(DISTINCT(editions.speech_type_id))",
+             "GROUP_CONCAT(DISTINCT(editions.publication_type_id))",
+             "GROUP_CONCAT(DISTINCT(editions.corporate_information_page_type_id))")
+  end
+end
---
+++ test/unit/whitehall/exporters/documents_info_exporter_test.rb
@@ -0,0 +1,85 @@
+require 'test_helper'
+
+module Whitehall::Exporters
+  class DocumentsInfoExporterTest < ActiveSupport::TestCase
+    test 'returns all subtype information of a document' do
+      org = create(:organisation)
+      document = create(:document)
+      create(
+        :news_article,
+        :superseded,
+        organisations: [org],
+        document: document,
+        news_article_type: NewsArticleType::NewsStory,
+      )
+      create(
+        :news_article,
+        :published,
+        organisations: [org],
+        document: document,
+        news_article_type: NewsArticleType::PressRelease,
+      )
+
+      documents_info_exporter = DocumentsInfoExporter.new(
+        [document.id]
+      )
+
+      assert_equal(
+        %w[news_story press_release],
+        documents_info_exporter.call.first[:document_information][:subtypes]
+      )
+    end
+
+
+    test 'returns all the locales of a document' do
+      org = create(:organisation)
+      news_story = create(
+        :news_article,
+        :published,
+        :translated, primary_locale: 'en', translated_into: %w(ms ar cy),
+        organisations: [org],
+        news_article_type: NewsArticleType::NewsStory,
+      )
+
+      documents_info_exporter = DocumentsInfoExporter.new(
+        [news_story.document_id]
+      )
+
+      assert_same_elements(
+        %w[ar cy en ms],
+        documents_info_exporter.call.first[:document_information][:locales]
+      )
+    end
+
+    test 'returns the lead organisations of the latest edition of a document' do
+      published_edition_org = create(:organisation)
+      published_edition_org_2 = create(:organisation)
+      superseded_edition_org = create(:organisation)
+      document = create(:document)
+      create(
+        :news_article,
+        :superseded,
+        document: document,
+        organisations: [superseded_edition_org],
+        news_article_type: NewsArticleType::NewsStory,
+      )
+
+      create(
+        :news_article,
+        :published,
+        document: document,
+        organisations: [published_edition_org, published_edition_org_2],
+        news_article_type: NewsArticleType::NewsStory,
+      )
+
+      documents_info_exporter = DocumentsInfoExporter.new(
+        [document.id]
+      )
+
+      assert_same_elements(
+        [published_edition_org.content_id, published_edition_org_2.content_id],
+        documents_info_exporter.call.first[:document_information][:lead_organisations]
+      )
+    end
+  end
+end
