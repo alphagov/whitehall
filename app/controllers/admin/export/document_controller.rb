930b3db3033bb247fe119c20da5ab66bf2d49040
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-08-05 11:09:35 +0100

    Create index action in export/document_controller
    This creates a endpoint which returns a json response of documents
    information for a given lead_organisation and edition type.
    
    lead_organisation is needed to find all the documents where the
    lead_organisation passed in is present on the latest edition of each
    document. `type` is used to specify the whitehall edition type of the
    documents you want returning, e.g NewsArticle or Publication.
    
    `lead_organisation` and `type` are mandatory params, `items_per_page`
    and `page_number` are optional which will default to 100 and 1
    respectively.

--- app/controllers/admin/export/document_controller.rb
+++ app/controllers/admin/export/document_controller.rb
@@ -5,4 +5,38 @@ class Admin::Export::DocumentController < Admin::Export::BaseController
     @document = Document.find(params[:id])
     respond_with DocumentExportPresenter.new(@document)
   end
+
+  def index
+    result_set = Whitehall::Exporters::DocumentsInfoExporter.new(paginated_document_ids).call
+
+    respond_with(
+      documents: result_set,
+      page_number: page_number,
+      page_count: result_set.count
+    )
+  end
+
+  private
+
+  def paginated_document_ids
+    Edition
+      .joins("INNER JOIN edition_organisations eo ON eo.edition_id = editions.id")
+      .joins("INNER JOIN organisations o ON o.id = eo.organisation_id")
+      .where(o: { content_id: params.require(:lead_organisation) })
+      .where(eo: { lead: true })
+      .where(type: params.require(:type))
+      .latest_edition
+      .order(:document_id)
+      .page(page_number)
+      .per(items_per_page)
+      .pluck(:document_id)
+  end
+
+  def page_number
+    params.fetch(:page_number, 1)
+  end
+
+  def items_per_page
+    params.fetch(:page_count, 100)
+  end
 end
--- test/functional/admin/export/document_controller_test.rb
+++ test/functional/admin/export/document_controller_test.rb
@@ -15,4 +15,72 @@ class Admin::Export::DocumentControllerTest < ActionController::TestCase
     get :show, params: { id: '1' }, format: 'json'
     assert_response :forbidden
   end
+
+  test "index responds with document information for a lead org and type" do
+    org = create(:organisation)
+    news_story = create(
+      :news_article,
+      organisations: [org],
+      news_article_type: NewsArticleType::NewsStory,
+    )
+
+    login_as :export_data_user
+
+    get :index, params: { lead_organisation: org.content_id,
+                          type: 'NewsArticle' }, format: 'json'
+
+    expected_response =
+      {
+        "documents" => [{
+          "document_id" => news_story.document_id,
+          "document_information" => {
+            "locales" => %w[en],
+            "subtypes" => %w[news_story],
+            "lead_organisations" => [org.content_id]
+          }
+        }],
+        "page_number" => 1,
+        "page_count" => 1,
+        "_response_info" => { "status" => "ok" }
+    }
+
+    assert_equal expected_response, json_response
+  end
+
+
+  test "doesnt return the document where the latest edition is not associated with lead org" do
+    published_edition_org = create(:organisation)
+    draft_edition_org = create(:organisation)
+    document = create(:document)
+    create(
+      :news_article,
+      :published,
+      document: document,
+      organisations: [published_edition_org],
+      news_article_type: NewsArticleType::NewsStory,
+    )
+
+    create(
+      :news_article,
+      :draft,
+      document: document,
+      organisations: [draft_edition_org],
+      news_article_type: NewsArticleType::NewsStory,
+    )
+
+    login_as :export_data_user
+
+    get :index, params: { lead_organisation: published_edition_org.content_id,
+                          type: 'NewsArticle' }, format: 'json'
+
+    expected_response =
+      {
+        "documents" => [],
+        "page_number" => 1,
+        "page_count" => 0,
+        "_response_info" => { "status" => "ok" }
+    }
+
+    assert_equal expected_response, json_response
+  end
 end
