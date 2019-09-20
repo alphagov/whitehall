e13108917f0302eadd17048e3a3f8d13a4f334d8
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2019-09-20 14:45:39 +0100

    Switch page presenter tests to governments api
    The orgs api isn't served from Whitehall anymore.

--- test/unit/presenters/api/page_presenter_test.rb
+++ test/unit/presenters/api/page_presenter_test.rb
@@ -5,7 +5,7 @@ class Api::PagePresenterTest < PresenterTestCase
     stubs_helper_method(:params).returns(
       ActionController::Parameters.new(
         action: "index",
-        controller: "api/organisations"
+        controller: "api/governments"
       )
     )
     @first_result = "a"
@@ -24,7 +24,7 @@ class Api::PagePresenterTest < PresenterTestCase
   test "json includes next page url if next page available" do
     @page.stubs(:last_page?).returns(false)
     @page.stubs(:out_of_range?).returns(false)
-    assert_equal api_organisations_url(page: 3), @presenter.as_json[:next_page_url]
+    assert_equal api_governments_url(page: 3), @presenter.as_json[:next_page_url]
   end
 
   test "json excludes next page url if no next page" do
@@ -34,7 +34,7 @@ class Api::PagePresenterTest < PresenterTestCase
 
   test "json includes previous page url if next page available" do
     @page.stubs(:first_page?).returns(false)
-    assert_equal api_organisations_url(page: 1), @presenter.as_json[:previous_page_url]
+    assert_equal api_governments_url(page: 1), @presenter.as_json[:previous_page_url]
   end
 
   test "json excludes previous page url if no next page" do
@@ -68,7 +68,7 @@ class Api::PagePresenterTest < PresenterTestCase
     self_link = @presenter.links.detect { |(_url, attrs)| attrs['rel'] == 'self' }
     assert self_link
     self_url, = *self_link
-    assert_equal api_organisations_url(page: 2), self_url
+    assert_equal api_governments_url(page: 2), self_url
   end
 
   test 'links include a rel=next url pointing to the next page if it has a next page' do
@@ -77,7 +77,7 @@ class Api::PagePresenterTest < PresenterTestCase
     next_link = @presenter.links.detect { |(_url, attrs)| attrs['rel'] == 'next' }
     assert next_link
     next_url, = *next_link
-    assert_equal api_organisations_url(page: 3), next_url
+    assert_equal api_governments_url(page: 3), next_url
   end
 
   test 'links do not include a rel=next url if it is the last page' do
@@ -98,7 +98,7 @@ class Api::PagePresenterTest < PresenterTestCase
     previous_link = @presenter.links.detect { |(_url, attrs)| attrs['rel'] == 'previous' }
     assert previous_link
     previous_url, = *previous_link
-    assert_equal api_organisations_url(page: 1), previous_url
+    assert_equal api_governments_url(page: 1), previous_url
   end
 
   test 'links do not include a rel=previous url if it is the first page' do
