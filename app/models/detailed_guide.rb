926a9c43bf588cf61cc1c64d6e43af4a32680133
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-10-18 13:00:40 +0100

    Fix url whitespace bug
    Previously if there was any whitespace in the URL for the related
    content fields whitehall would crash throwing the standard 'Something
    went wrong' message. This change strips all the whitespace from the URL
    before parsing.
    
    This problem became apparent when a zendesk came in which was related to
    extra whitespace unknowingly added to the end of a URL.
    
    Zendesk:
    https://govuk.zendesk.com/agent/tickets/3831838

--- app/models/detailed_guide.rb
+++ app/models/detailed_guide.rb
@@ -153,7 +153,7 @@ private
   def parse_base_path_from_related_mainstream_url(url)
     return nil if url.nil? || url.empty?
 
-    parsed_url = URI.parse(url)
+    parsed_url = URI.parse(url.strip)
     url_is_invalid = !["gov.uk", "www.gov.uk"].include?(parsed_url.host)
     return nil if url_is_invalid
 
