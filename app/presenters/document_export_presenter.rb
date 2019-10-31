96270f7d675121f74fa0e4a0270c5468de68a00d
Peter <peter.hartshorn1996@gmail.com>
2019-10-31 12:38:06 +0000

    Reformat DocumentExportPresenter
    This commit changes how DocumentExportPresenter obtains its information
    for a document. Previously the approach was to have conditional logic
    around all the associations a document could have, this will no longer
    work well as we're planning to strip and add data to each field in a bespoke
    way which would generate a hugggggge conditional.
    
    Adding more data in later commits.

--- app/presenters/document_export_presenter.rb
+++ app/presenters/document_export_presenter.rb
@@ -1,6 +1,13 @@
 class DocumentExportPresenter < Whitehall::Decorators::Decorator
   include GovspeakHelper
 
+  DOCUMENT_SUB_TYPES = %i[
+    news_article_type
+    publication_type
+    corporate_information_page_type
+    speech_type
+  ].freeze
+
   def as_json
     {
       document: model,
@@ -12,60 +19,56 @@ class DocumentExportPresenter < Whitehall::Decorators::Decorator
 
   def editions
     model.editions.map do |edition|
-      edition_associations(edition)
+      {
+        edition: edition,
+        government: edition.government,
+        whitehall_admin_links: resolve_whitehall_admin_links(edition),
+        associations: {
+          attachments: complete_attachments(edition),
+          images: complete_images(edition),
+        },
+      }.merge(provide_doctype_information(edition))
     end
   end
 
-  def edition_associations(edition)
-    output = {
-               edition: edition,
-               associations: {},
-             }
+  def complete_images(edition)
+    images = edition.try(:images)
+    return [] unless images
 
-    associations = edition.class.reflect_on_all_associations.map(&:name)
-    associations.each do |association|
-      if association == :images
-        edition_images = edition.public_send(association)
-        output[:associations][association] = complete_images_hash(edition_images)
-      elsif association == :attachments
-        edition_attachments = edition.public_send(association)
-        output[:associations][association] = complete_attachments_hash(edition_attachments)
-      else
-        output[:associations][association] = edition.public_send(association)
-      end
-    end
-
-    provide_doctype_information(edition, output)
-    output[:government] = edition.government
-    output[:whitehall_admin_links] = resolve_whitehall_admin_links(edition.body)
-    if edition.withdrawn?
-      output[:whitehall_admin_links].concat(resolve_whitehall_admin_links(edition.unpublishing.explanation))
-    end
-    output
-  end
-
-  def complete_images_hash(edition_images)
-    edition_images.map do |image|
+    images.map do |image|
       image.as_json(methods: :url)
     end
   end
 
-  def complete_attachments_hash(edition_attachments)
-    edition_attachments.map do |attachment|
+  def complete_attachments(edition)
+    attachments = edition.try(:attachments)
+    return [] unless attachments
+
+    attachments.map do |attachment|
       attachment.as_json(include: :attachment_data, methods: %i[url type]).tap do |json|
         json.merge!("govspeak_content" => attachment.govspeak_content.as_json) if attachment.respond_to?(:govspeak_content)
       end
     end
   end
 
-  def provide_doctype_information(edition, output)
-    %i[news_article_type publication_type corporate_information_page_type speech_type].each do |type|
-      output[type] = edition.public_send(type)&.key if edition.respond_to?(type)
+  def provide_doctype_information(edition)
+    DOCUMENT_SUB_TYPES.each_with_object({}) do |type, memo|
+      memo[type] = edition.public_send(type)&.key if edition.respond_to?(type)
     end
   end
 
-  def resolve_whitehall_admin_links(body)
-    whitehall_admin_links(body).map do |link|
+  def resolve_whitehall_admin_links(edition)
+    links = admin_links(edition.body)
+
+    if edition.withdrawn?
+      links.concat(admin_links(edition.unpublishing.explanation))
+    end
+
+    links
+  end
+
+  def admin_links(text)
+    whitehall_admin_links(text).map do |link|
       { whitehall_admin_url: link, public_url: public_url_for_admin_link(link) }
     end
   end
