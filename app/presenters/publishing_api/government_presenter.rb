41567df94ec0c4396a8054d49b27ba9af8edf485
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-11-22 11:43:42 +0000

    Add a PublishingApi presenter for Governments
    It's unclear if these will be rendered, but just in case, I've set the
    rendering app to government-frontend, and set a base path.

---
+++ app/presenters/publishing_api/government_presenter.rb
@@ -0,0 +1,57 @@
+module PublishingApi
+  class GovernmentPresenter
+    attr_accessor :item
+    attr_accessor :update_type
+
+    def initialize(item, update_type: nil)
+      self.item = item
+      self.update_type = update_type || "major"
+    end
+
+    def content_id
+      item.content_id
+    end
+
+    def content
+      BaseItemPresenter.new(
+        item,
+        title: item.name,
+        update_type: update_type,
+      ).base_attributes.merge(
+        base_path: base_path,
+        details: details,
+        document_type: item.class.name.underscore,
+        public_updated_at: item.updated_at,
+        rendering_app: Whitehall::RenderingApp::GOVERNMENT_FRONTEND,
+        schema_name: schema_name,
+      ).merge(
+        PayloadBuilder::Routes.for(base_path),
+      )
+    end
+
+    def links
+      {}
+    end
+
+  private
+
+    def schema_name
+      "government"
+    end
+
+    def base_path
+      "/government/#{item.slug}"
+    end
+
+    def details
+      {
+        started_on: item.start_date.rfc3339,
+        ended_on: item.end_date&.rfc3339,
+        # Use ended?, rather than current?, as this just checks if the
+        # Government has ended, which should be equivalent, and
+        # doesn't require looking up the details of other governments
+        current: !item.ended?,
+      }
+    end
+  end
+end
---
+++ test/unit/presenters/government_presenter_test.rb
@@ -0,0 +1,54 @@
+require "test_helper"
+
+class GovernmentPresenterTest < PresenterTestCase
+  extend Minitest::Spec::DSL
+
+  setup do
+    @government = build(:government)
+    @presenter = PublishingApi::GovernmentPresenter.new(@government)
+  end
+
+  test "the presenter has the content_id" do
+    assert_equal @government.content_id, @presenter.content_id
+  end
+
+  test "the details has started_on" do
+    assert_equal(
+      @government.start_date.rfc3339,
+      @presenter.content.dig(:details, :started_on),
+    )
+  end
+
+  context "given a previous government" do
+    setup do
+      @government = build(:previous_government, slug: "foo")
+      @presenter = PublishingApi::GovernmentPresenter.new(@government)
+    end
+
+    test "the details has ended_on" do
+      assert_equal(
+        @government.end_date.rfc3339,
+        @presenter.content.dig(:details, :ended_on),
+      )
+    end
+
+    test "current is false" do
+      refute @presenter.content.dig(:details, :current)
+    end
+  end
+
+  context "given a current government" do
+    setup do
+      @government = build(:current_government)
+      @presenter = PublishingApi::GovernmentPresenter.new(@government)
+    end
+
+    test "the details has a null ended_on" do
+      assert_nil @presenter.content.dig(:details, :ended_on)
+    end
+
+    test "current is true" do
+      assert @presenter.content.dig(:details, :current)
+    end
+  end
+end
