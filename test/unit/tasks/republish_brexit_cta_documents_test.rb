232e4bbe7570ff411cf011f88bad89cd4d405a80
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-11-29 16:29:03 +0000

    Add rake task to republish Brexit CTA documents
    When the messaging in the Brexit CTA changes, we will need to republish
    documents that contain `$BrexitCTA` markdown in order to re-present the
    documents with the updated messaging to the Publishing API. This creates a rake
    task to find all documents containing `$BrexitCTA` in the body and send them to
    be republished.

---
+++ test/unit/tasks/republish_brexit_cta_documents_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+require "rake"
+
+class RepublishBrexitCtaDocumentsTest < ActiveSupport::TestCase
+  setup do
+    Rake.application.rake_require "tasks/republish_brexit_cta_documents"
+    Rake::Task.define_task(:environment)
+    Rake::Task["republish_brexit_cta_documents"].reenable
+    $stdout.stubs(:puts)
+  end
+
+  test "it should republish all documents with $BrexitCTA in the body" do
+    edition_one = create(:published_publication, body: "Some content\n\n$BrexitCTA")
+    edition_two = create(:published_publication, body: "$BrexitCTA\n\nSome content")
+    edition_three = create(:published_publication, body: "$CTA\n\nSome other CTA\n\n$CTA")
+
+    PublishingApiDocumentRepublishingWorker.expects(:perform_async_in_queue)
+                                           .with("bulk_republishing", edition_one.document_id)
+                                           .returns(true)
+
+    PublishingApiDocumentRepublishingWorker.expects(:perform_async_in_queue)
+                                           .with("bulk_republishing", edition_two.document_id)
+                                           .returns(true)
+
+    PublishingApiDocumentRepublishingWorker.expects(:perform_async_in_queue)
+                                           .with("bulk_republishing", edition_three.document_id)
+                                           .never
+
+    Rake.application.invoke_task "republish_brexit_cta_documents"
+  end
+end
