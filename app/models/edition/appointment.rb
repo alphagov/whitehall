64165a16eac4e027bd277a6aa1f979fed68e458f
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-01-08 14:09:43 +0000

    Index roles for editions with appointments
    I had to change around the tests a bit because they are using a "fake
    edition".

--- app/models/edition/appointment.rb
+++ app/models/edition/appointment.rb
@@ -25,7 +25,10 @@ module Edition::Appointment
     if person_override?
       super
     else
-      super.merge("people" => [person.slug])
+      super.merge({
+        "people" => [person.slug],
+        "roles" => is_associated_with_a_minister? ? [role.slug] : nil,
+      }.compact)
     end
   end
 end
