933b6dc5f66e7837d6d32a0823b361e78032d0f5
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2020-02-24 15:55:16 +0000

    Untangle the two sets of organisation reslugging tests

--- test/unit/data_hygiene/organisation_reslugger_test.rb
+++ test/unit/data_hygiene/organisation_reslugger_test.rb
@@ -2,13 +2,13 @@ require "test_helper"
 require "gds_api/test_helpers/publishing_api"
 
 module OrganisationResluggerTest
-  module SharedTests
+  class OrganisationTest < ActiveSupport::TestCase
     include GdsApi::TestHelpers::PublishingApi
     extend ActiveSupport::Testing::Declarative
 
     def setup
       stub_any_publishing_api_call
-      @organisation = create_org_and_stub_content_store
+      @organisation = create(:organisation, name: "Old slug")
       WebMock.reset! # clear the Publishing API calls after org creation
       stub_any_publishing_api_call
       @reslugger = DataHygiene::OrganisationReslugger.new(@organisation, "corrected-slug")
@@ -24,6 +24,7 @@ module OrganisationResluggerTest
     end
 
     test "publishes to Publishing API with the new slug" do
+      base_path = "/government/organisations"
       new_base_path = "#{base_path}/corrected-slug"
       new_atom_base_path = "#{base_path}/corrected-slug.atom"
 
@@ -39,8 +40,9 @@ module OrganisationResluggerTest
         stub_publishing_api_publish(content_item.content_id, locale: "en", update_type: nil),
       ]
 
+      stub_any_publishing_api_call
       Sidekiq::Testing.inline! do
-        @reslugger.run!
+        DataHygiene::OrganisationReslugger.new(@organisation, "corrected-slug").run!
       end
 
       assert_all_requested expected_publish_requests
@@ -54,18 +56,6 @@ module OrganisationResluggerTest
     test "adds the new slug from the search index" do
       Whitehall::SearchIndex.expects(:add).with { |org| org.slug == "corrected-slug" }
       @reslugger.run!
-    end
-  end
-
-  class OrganisationTest < ActiveSupport::TestCase
-    include SharedTests
-
-    def create_org_and_stub_content_store
-      create(:organisation, name: "Old slug")
-    end
-
-    def base_path
-      "/government/organisations"
     end
 
     test "updates users belonging to the organisation" do
@@ -87,19 +77,71 @@ module OrganisationResluggerTest
   end
 
   class WorldwideOrganisationTest < ActiveSupport::TestCase
-    include SharedTests
+    include GdsApi::TestHelpers::PublishingApi
+    extend ActiveSupport::Testing::Declarative
 
-    def create_org_and_stub_content_store
-      create(:worldwide_organisation, name: "Old slug")
+    def setup
+      stub_any_publishing_api_call
+      @organisation = create(:worldwide_organisation, name: "Old slug")
+      WebMock.reset! # clear the Publishing API calls after org creation
+      stub_any_publishing_api_call
+      @reslugger = DataHygiene::OrganisationReslugger.new(@organisation, "corrected-slug")
     end
 
-    def base_path
-      "/world/organisations"
+    def teardown
+      WebMock.reset!
+    end
+
+    test "re-slugs the organisation" do
+      @reslugger.run!
+      assert_equal "corrected-slug", @organisation.slug
+    end
+
+    test "publishes to Publishing API with the new slug" do
+      base_path = "/world/organisations"
+      new_base_path = "#{base_path}/corrected-slug"
+      new_atom_base_path = "#{base_path}/corrected-slug.atom"
+
+      stub_any_publishing_api_call
+      organisation = create(:worldwide_organisation, name: "Old slug")
+      WebMock.reset! # clear the Publishing API calls after org creation
+
+      content_item = PublishingApiPresenters.presenter_for(organisation)
+      content = content_item.content
+      content[:base_path] = new_base_path
+      content[:routes][0][:path] = new_base_path
+      content[:routes][1][:path] = new_atom_base_path unless content[:routes][1].nil?
+      content_item.stubs(content: content)
+
+      expected_publish_requests = [
+        stub_publishing_api_put_content(content_item.content_id, content_item.content),
+        stub_publishing_api_publish(content_item.content_id, locale: "en", update_type: nil),
+      ]
+
+      stub_any_publishing_api_call
+      Sidekiq::Testing.inline! do
+        DataHygiene::OrganisationReslugger.new(organisation, "corrected-slug").run!
+      end
+
+      assert_all_requested expected_publish_requests
+    end
+
+    test "deletes the old slug from the search index" do
+      Whitehall::SearchIndex.expects(:delete).with { |org| org.slug == "old-slug" }
+      @reslugger.run!
+    end
+
+    test "adds the new slug from the search index" do
+      Whitehall::SearchIndex.expects(:add).with { |org| org.slug == "corrected-slug" }
+      @reslugger.run!
     end
 
     test "calls update_editions" do
-      @reslugger.expects(:update_editions).once
-      @reslugger.run!
+      organisation = create(:worldwide_organisation, name: "Old slug")
+      reslugger = DataHygiene::OrganisationReslugger.new(organisation, "corrected-slug")
+
+      reslugger.expects(:update_editions).once
+      reslugger.run!
     end
   end
 end
