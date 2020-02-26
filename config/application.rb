bc04da310f2acb2958646e52fc4416d7100cd3d3
Laurent Curau <laurent.curau@digital.cabinet-office.gov.uk>
2020-02-26 15:09:31 +0000

    Deactivate CSS assets sass compression
    https://github.com/alphagov/govuk-frontend/issues/1350

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Whitehall
     # Using a sass css compressor causes a scss file to be processed twice (once
     # to build, once to compress) which breaks the usage of "unquote" to use
     # CSS that has same function names as SCSS such as max
+    # https://github.com/alphagov/govuk-frontend/issues/1350
     config.assets.css_compressor = nil
 
     config.slimmer.wrapper_id = "whitehall-wrapper-slimmer"
