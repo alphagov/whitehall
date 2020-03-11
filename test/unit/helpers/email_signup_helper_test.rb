c51afec4b354e65cf7179bc65a1f2f1ac90ff278
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-03-11 21:48:37 +0000

    Use whitehalls email signup for world locations
    If an atom_feed_url matches a world feed then forward the request to the
    email_signups controller, previously this returned an
    email-alert-frontend url but after a bug was caught this is now being
    updated to use whitehall email_signup flow.
    
    A rescue has been added to catch if the atom_feed_url is invalid e.g
    integer, nil etc, if the url is invalid we'll redirect users to gov.uk's
    homepage.

--- test/unit/helpers/email_signup_helper_test.rb
+++ test/unit/helpers/email_signup_helper_test.rb
@@ -8,10 +8,24 @@ class EmailSignupHelperTest < ActionView::TestCase
     assert_equal expected, result
   end
 
+  test "#email_signup_path returns a whitehall signup url for a world_location" do
+    atom_feed_url = "https://www.gov.uk/world/uk-joint-delegation-to-nato.atom"
+    result = email_signup_path(atom_feed_url)
+    expected = "/government/email-signup/new?email_signup%5Bfeed%5D=https%3A%2F%2Fwww.gov.uk%2Fworld%2Fuk-joint-delegation-to-nato.atom"
+    assert_equal expected, result
+  end
+
   test "#email_signup_path returns a signup path for mhra" do
     atom_feed_url = "https://www.gov.uk/government/organisations/medicines-and-healthcare-products-regulatory-agency.atom"
     result = email_signup_path(atom_feed_url)
     expected = "/government/organisations/medicines-and-healthcare-products-regulatory-agency/email-signup"
+    assert_equal expected, result
+  end
+
+  test "#email_signup_path returns website root if the feed is invalid" do
+    atom_feed_url = nil
+    result = email_signup_path(atom_feed_url)
+    expected = "/"
     assert_equal expected, result
   end
 end
