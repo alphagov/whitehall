d4dd6ceb01e3ef3664efffeed2256b9d215063a2
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2020-03-20 16:40:20 +0000

    Delete LinkCheckerApiReport's without links
    I don't think having reports without any associated links is useful,
    so go ahead and delete these reports as well as deleting the related
    links.
    
    The intent here is to potentially help with the performance of the
    Edition.only_broken_links method, by not having so much data to search
    through.

--- config/schedule.rb
+++ config/schedule.rb
@@ -40,5 +40,5 @@ every taxonomy_cron_rules, roles: [:backend] do
 end
 
 every :day, at: "4am", roles: [:backend] do
-  rake "link_checker:delete_old_report_links"
+  rake "link_checker:delete_old_data"
 end
