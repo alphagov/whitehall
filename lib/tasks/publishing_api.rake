alphagov/whitehall

commit 4b56a10abab800def323e3584e1e46d947232ca3

Fix warnings for top-level rake task methods

lib/tasks/publishing_api.rake
@@ -195,15 +195,6 @@ namespace :publishing_api do
     PublishingApiDocumentRepublishingWorker.new.perform(document.id)
   end
 
-  def ensure_document_with_content_id_doesnt_exist(content_id)
-    document = Document.find_by(content_id: content_id)
-
-    return unless document
-
-    puts "Document with this content ID exists: #{document}"
-    abort "This Rake task is only for unknown content"
-  end
-
   desc "Bulk republishing"
   namespace :bulk_republish do
     desc "Republish all documents of a given type, eg 'NewsArticle'"
@@ -227,7 +218,8 @@ namespace :publishing_api do
     task :dry_run, %i[content_id alternative_path locale] => :environment do |_, args|
       args.with_defaults(locale: "en")
 
-      ensure_document_with_content_id_doesnt_exist(args[:content_id])
+      document = Document.find_by(content_id: content_id)
+      abort "Document with this content ID exists: #{document}" if document
 
       puts "Would send an unpublish request to the Publishing API for #{args[:content_id]} with:"
       puts "  type 'redirect', locale: #{args[:locale]} and alternative_path #{args[:alternative_path].strip}"
@@ -237,7 +229,8 @@ namespace :publishing_api do
     task :real, %i[content_id alternative_path locale] => :environment do |_, args|
       args.with_defaults(locale: "en")
 
-      ensure_document_with_content_id_doesnt_exist(args[:content_id])
+      document = Document.find_by(content_id: content_id)
+      abort "Document with this content ID exists: #{document}" if document
 
       response = Services.publishing_api.unpublish(
         args[:content_id],

lib/tasks/export.rake
@@ -2,10 +2,6 @@ require "csv"
 require "fileutils"
 
 namespace :export do
-  def routes_helper
-    @routes_helper ||= Whitehall.url_maker
-  end
-
   desc "Export mappings (for eg the Transition app to consume)"
   task mappings: :environment do
     # Read off the MySQL slave - we want performance here and
@@ -50,11 +46,11 @@ namespace :export do
             document.slug,
             document.display_type,
             document.latest_edition.state,
-            document.published? ? routes_helper.public_document_url(edition) : nil,
+            document.published? ? Whitehall.url_maker.public_document_url(edition) : nil,
             edition.id,
             edition.title,
             edition.state,
-            routes_helper.admin_edition_url(edition),
+            Whitehall.url_maker.admin_edition_url(edition),
             *edition.authors.uniq.map(&:name),
           ]
         end
@@ -92,8 +88,8 @@ namespace :export do
         org.published_editions.each do |edition|
           csv << [
             org.display_name,
-            routes_helper.public_document_url(edition),
-            routes_helper.admin_edition_url(edition),
+            Whitehall.url_maker.public_document_url(edition),
+            Whitehall.url_maker.admin_edition_url(edition),
             edition.title,
             edition.display_type,
             edition.public_timestamp,

lib/tasks/reslugging.rake
@@ -12,11 +12,8 @@ namespace :reslug do
   - republishes the person to Publishing API (automatically handles the redirect)
   - reindexes all dependent documents in search"
   task :person, %i[old_slug new_slug] => :environment do |_task, args|
-    old_slug = args[:old_slug]
-    new_slug = args[:new_slug]
-    person = Person.find_by!(slug: old_slug)
-
-    DataHygiene::PersonReslugger.new(person, new_slug).run!
+    person = Person.find_by!(slug: args[:old_slug])
+    DataHygiene::PersonReslugger.new(person, args[:new_slug]).run!
   end
 
   desc "Change a role slug (DANGER!).\n
@@ -28,11 +25,8 @@ namespace :reslug do
   - reindexes the role for search
   - republishes the role to Publishing API (automatically handles the redirect)"
   task :role, %i[old_slug new_slug] => :environment do |_task, args|
-    old_slug = args[:old_slug]
-    new_slug = args[:new_slug]
-    role = Role.find_by!(slug: old_slug)
-
-    DataHygiene::RoleReslugger.new(role, new_slug).run!
+    role = Role.find_by!(slug: args[:old_slug])
+    DataHygiene::RoleReslugger.new(role, args[:new_slug]).run!
   end
 
   desc "Change a document's slug in whitehall (DANGER!).\n
@@ -81,14 +75,6 @@ namespace :reslug do
     world_location.editions.published.each(&:update_in_search_index)
   end
 
-  def reslug_organisation(organisation_class, args)
-    old_slug = args[:old_slug]
-    new_slug = args[:new_slug]
-
-    organisation = organisation_class.find_by!(slug: old_slug)
-    DataHygiene::OrganisationReslugger.new(organisation, new_slug).run!
-  end
-
   desc "Change an organisation slug (DANGER!).\n
 
   This rake task changes the slug of an organisation in whitehall.
@@ -99,7 +85,8 @@ namespace :reslug do
   - reindexes the org for search
   - reindexes all dependent documents in search"
   task :organisation, %i[old_slug new_slug] => :environment do |_task, args|
-    reslug_organisation(Organisation, args)
+    organisation = Organisation.find_by!(slug: args[:old_slug])
+    DataHygiene::OrganisationReslugger.new(organisation, args[:new_slug]).run!
   end
 
   desc "Change a worldwide organisation slug (DANGER!).\n
@@ -112,7 +99,8 @@ namespace :reslug do
   - reindexes the org for search
   - reindexes all dependent documents in search"
   task :worldwide_organisation, %i[old_slug new_slug] => :environment do |_task, args|
-    reslug_organisation(WorldwideOrganisation, args)
+    organisation = WorldwideOrganisation.find_by!(slug: args[:old_slug])
+    DataHygiene::OrganisationReslugger.new(organisation, args[:new_slug]).run!
   end
 
   desc "Change the slug of a StatisticsAnnouncement"

lib/tasks/statistics_announcements_intents.rake
@@ -1,8 +1,4 @@
 namespace :statistics_announcements do
-  def no_update_required?(statistics_announcement)
-    statistics_announcement.unpublished? || statistics_announcement.cancelled?
-  end
-
   desc "Updates the publish intent for StatisticsAnnouncements scheduled for release in the future"
   task put_intents_for_scheduled: :environment do
     latest_join_sql = <<-SQL
@@ -19,9 +15,8 @@ namespace :statistics_announcements do
       .where(latest_join_sql)
 
     statistics_announcements.each do |statistics_announcement|
-      unless no_update_required?(statistics_announcement)
-        statistics_announcement.update_publish_intent
-      end
+      no_update_required = statistics_announcement.unpublished? || statistics_announcement.cancelled?
+      statistics_announcement.update_publish_intent unless no_update_required
     end
   end
 end

commit 2485b1a2edf7892c1c62c47f5df8f3ed413df541

Fix the publishing_api:unpublish_with_redirect Rake task
It was broken in 4b56a10abab800def323e3584e1e46d947232ca3.

lib/tasks/publishing_api.rake
@@ -218,7 +218,7 @@ namespace :publishing_api do
     task :dry_run, %i[content_id alternative_path locale] => :environment do |_, args|
       args.with_defaults(locale: "en")
 
-      document = Document.find_by(content_id: content_id)
+      document = Document.find_by(content_id: args[:content_id])
       abort "Document with this content ID exists: #{document}" if document
 
       puts "Would send an unpublish request to the Publishing API for #{args[:content_id]} with:"
@@ -229,7 +229,7 @@ namespace :publishing_api do
     task :real, %i[content_id alternative_path locale] => :environment do |_, args|
       args.with_defaults(locale: "en")
 
-      document = Document.find_by(content_id: content_id)
+      document = Document.find_by(content_id: args[:content_id])
       abort "Document with this content ID exists: #{document}" if document
 
       response = Services.publishing_api.unpublish(