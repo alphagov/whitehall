3ee0120a32fbd8aa60d50b7b646c86eff6c3727b
Michael Walker <mike@barrucadu.co.uk>
2020-05-19 15:23:18 +0100

    Remove the buildings history pages
    These are now published and rendered by government-frontend.

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,23 +1,3 @@
 class HistoriesController < PublicFacingController
   layout "frontend"
-
-  def show
-    if valid_names.include?(params[:id])
-      render template: "histories/#{params[:id].underscore}"
-    else
-      render plain: "Not found", status: :not_found
-    end
-  end
-
-private
-
-  def valid_names
-    %w[
-      king-charles-street
-      lancaster-house
-      10-downing-street
-      11-downing-street
-      1-horse-guards-road
-    ]
-  end
 end
--- features/step_definitions/history_steps.rb
+++ features/step_definitions/history_steps.rb
@@ -5,11 +5,3 @@ end
 Then(/^I should see historic information$/) do
   assert_text "History of the UK government"
 end
-
-When(/^I visit the "([^"]*)" page$/) do |path|
-  visit history_path(path.parameterize)
-end
-
-Then(/^I should see historic information about "([^"]*)"$/) do |name|
-  assert_text name.titlecase
-end
