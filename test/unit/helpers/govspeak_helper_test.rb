dec0bdc89d3a33962e178cc91a52146d859cca32
Kevin Dew <kevindew@me.com>
2020-06-04 23:53:07 +0100

    Remove bogus test
    This test isn't testing any of the logic in the govspeak helper, it
    seems to merely pass because that is what the default is for an
    unpersisted attachment.

--- test/unit/helpers/govspeak_helper_test.rb
+++ test/unit/helpers/govspeak_helper_test.rb
@@ -302,13 +302,6 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, ".govspeak figure.image.embedded img[src='https://some.cdn.com/image.jpg']"
   end
 
-  test "does not prefix embedded attachment urls with asset host so that access to them can be authenticated when previewing draft documents" do
-    Whitehall.stubs(:public_asset_host).returns("https://some.cdn.com")
-    edition = build(:published_publication, :with_file_attachment, body: "!@1")
-    html = govspeak_edition_to_html(edition)
-    assert_select_within_html html, ".govspeak .attachment.embedded a[href^='/'][href$='greenpaper.pdf']"
-  end
-
   test "should remove extra quotes from blockquote text" do
     remover = stub("remover")
     remover.expects(:remove).returns("remover return value")
