cd761f3f97ba3c444168b3cc8badc961fc99d3d4
Andrew White <andrew.white@unboxed.co>
2020-08-06 10:33:32 +0100

    Don't build duplicate feature lists
    There should be only one feature list for a locale so where there aren't
    any in the database it should only build a new one if there isn't already
    a new one waiting to be saved.

--- app/models/featurable.rb
+++ app/models/featurable.rb
@@ -2,15 +2,23 @@ module Featurable
   extend ActiveSupport::Concern
 
   included do
-    has_many :feature_lists, as: :featurable, dependent: :destroy
+    has_many :feature_lists, as: :featurable, dependent: :destroy do
+      def build_for_locale(locale)
+        target.detect(-> { build(locale: locale) }) { |fl| locale.match?(fl.locale) }
+      end
+    end
   end
 
   def feature_list_for_locale(locale)
-    feature_lists.find_by(locale: locale) || feature_lists.build(locale: locale)
+    feature_lists.find_by(locale: locale) || feature_lists.build_for_locale(locale)
   end
 
   def load_or_create_feature_list(locale = nil)
     locale = I18n.default_locale if locale.blank?
     feature_lists.find_by(locale: locale) || feature_lists.create!(locale: locale)
+  end
+
+  def build_feature_list_for_locale(locale)
+    feature_lists.target.select { |feature_list| feature_list.locale == locale }
   end
 end
