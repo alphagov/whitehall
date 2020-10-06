0ab150d39bb9df97e8acdd5b34f2ff9c774a9df1
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-10-06 13:01:36 +0100

    Remove unused step definitions
    These steps aren't referenced anywhere in the feature files and
    should be removed.

--- features/step_definitions/news_article_steps.rb
+++ features/step_definitions/news_article_steps.rb
@@ -11,10 +11,6 @@ Given(/^a published news article "([^"]*)" associated with "([^"]*)"$/) do |titl
   )
 end
 
-Given(/^a published news article "([^"]*)" which isn't explicitly associated with "([^"]*)"$/) do |title, _thing|
-  create(:published_news_article, title: title)
-end
-
 When(/^I draft a new news article "([^"]*)"$/) do |title|
   begin_drafting_news_article title: title, summary: "here's a simple summary"
   within ".images" do
@@ -22,71 +18,6 @@ When(/^I draft a new news article "([^"]*)"$/) do |title|
     fill_in "Alt text", with: "An alternative description", match: :first
   end
   click_button "Save"
-end
-
-When(/^I publish a news article "([^"]*)" associated with "([^"]*)"$/) do |title, person_name|
-  begin_drafting_news_article title: title
-  fill_in_news_article_fields(first_published: Time.zone.today.to_s)
-  select person_name, from: "Ministers"
-  click_button "Save"
-  publish(force: true)
-end
-
-When(/^I publish a news article "([^"]*)" associated with the topical event "([^"]*)"$/) do |title, topic_name|
-  begin_drafting_news_article title: title
-
-  select topic_name, from: "Topical events"
-
-  fill_in_news_article_fields(first_published: Time.zone.today.to_s)
-  click_button "Save"
-  publish(force: true)
-end
-
-When(/^I publish a news article "(.*?)" associated with the organisation "(.*?)"$/) do |title, organisation_name|
-  begin_drafting_news_article title: title
-  fill_in_news_article_fields(first_published: Time.zone.today.to_s)
-  within ".lead-organisations" do
-    select organisation_name, from: "Organisation 1"
-  end
-  click_button "Save"
-  publish(force: true)
-end
-
-When(/^I attempt to add the article image into the markdown$/) do
-  fill_in "Body", with: "body copy\n!!1\nmore body"
-end
-
-Then(/^the news article tag is the same as the person in the text$/) do
-  visit admin_edition_path(NewsArticle.last)
-  click_button "Create new edition"
-  appointment = NewsArticle.last.role_appointments.first
-  assert_selector "select#edition_role_appointment_ids option[value='#{appointment.id}'][selected=selected]"
-end
-
-Then(/^I should see both the news articles for the Deputy Prime Minister role$/) do
-  assert_selector ".news_article", text: "News from Don, Deputy PM"
-  assert_selector ".news_article", text: "News from Harriet, Deputy PM"
-end
-
-Then(/^I should see both the news articles for Harriet Home$/) do
-  assert_text "First article"
-  assert_text "Second article"
-end
-
-Then(/^I should be informed I shouldn't use this image in the markdown$/) do
-  click_on "Edit draft"
-  assert_no_selector "fieldset#image_fields .image input[value='!!1']"
-end
-
-When(/^I browse to the announcements index$/) do
-  stub_content_item_from_content_store_for(announcements_path)
-  visit announcements_path
-end
-
-When(/^I publish a new news article of the type "(.*?)" called "(.*?)"$/) do |announcement_type, title|
-  begin_drafting_news_article(title: title, first_published: Time.zone.today.to_s, announcement_type: announcement_type)
-  click_button "Save"
-  publish(force: true)
 end
 
 When(/^I draft a French\-only "World news story" news article associated with "([^"]*)"$/) do |location_name|
@@ -105,13 +36,6 @@ end
 When(/^I publish the French-only news article$/) do
   stub_publishing_api_links_with_taxons(@news_article.content_id, %w[a-taxon-content-id])
   visit admin_edition_path(@news_article)
-  publish(force: true)
-end
-
-When(/^I publish a news article "(.*?)" for "(.*?)"$/) do |title, location_name|
-  begin_drafting_news_article(title: title, first_published: Time.zone.today.to_s)
-  select location_name, from: "Select the world locations this news article is about"
-  click_button "Save"
   publish(force: true)
 end
 
