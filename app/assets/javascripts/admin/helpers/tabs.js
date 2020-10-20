8aff62ea99bcf1dc84a189465e08990d48ea2458
Kevin Dew <kevindew@me.com>
2020-10-20 20:38:30 +0100

    Fix linting (and logic) issue in tabs js helper
    This file had camel case and unused variable offences, it seems that
    this previously had a mistake when setting the scroll position to use
    both y axis rather than x and y axis. I assume no-one caught this as the
    X axis is rarely scrollable.
    
    I've put this in a separate commit to try help me remember to test this
    individually.

--- app/assets/javascripts/admin/helpers/tabs.js
+++ app/assets/javascripts/admin/helpers/tabs.js
@@ -9,10 +9,10 @@
         $('.nav-tabs a[href=#' + url.split('#')[1] + ']').tab('show')
       }
       $('.nav-tabs a').on('shown', function (e) {
-        var before_shown_scroll_y = window.pageYOffset
-        var before_shown_scroll_x = window.pageXOffset
+        var beforeShownScrollX = window.pageXOffset
+        var beforeShownScrollY = window.pageYOffset
         window.location.hash = e.target.hash
-        window.scrollTo(before_shown_scroll_y, before_shown_scroll_y)
+        window.scrollTo(beforeShownScrollX, beforeShownScrollY)
       })
     }
   }
