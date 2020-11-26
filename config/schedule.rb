59525a572b76910c1d7e805488dcf0e55f23a4e5
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-11-26 15:48:09 +0000

    Warn that errors may occur sending via Notify in integration/staging

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,19 @@
+$LOAD_PATH << "."
+require "active_support"
+require "lib/whitehall"
+
 # default cron env is "/usr/bin:/bin" which is not sufficient as govuk_env is in /usr/local/bin
 env :PATH, "/usr/local/bin:/usr/bin:/bin"
 
 # We need Rake to use our own environment
 job_type :rake, "cd :path && govuk_setenv whitehall bundle exec rake :task --silent :output"
 
-def integration_or_staging?
-  ENV.fetch("GOVUK_WEBSITE_ROOT") =~ /integration|staging/
-end
-
 every :day, at: ["3am", "12:45pm"], roles: [:backend] do
   rake "export:mappings"
 end
 
 def search_index_consultations_cron_rule
-  if integration_or_staging?
+  if Whitehall.integration_or_staging?
     # Don't run near midnight, as this is when the data sync will
     # likely happen, and the task will error
     "0 2-22 * * *"
@@ -27,7 +27,7 @@ every search_index_consultations_cron_rule, roles: [:backend] do
 end
 
 def taxonomy_cron_rules
-  if integration_or_staging?
+  if Whitehall.integration_or_staging?
     # at every 10th minute past the hour between 7am and 8pm
     "*/10 7-20 * * *"
   else
