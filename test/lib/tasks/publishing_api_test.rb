cd5a64008d542984ffe74b4f11d2b92c33ae4634
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-02-22 15:57:29 +0000

    Add more tests to publishing_api_test.rb
    Now that we've run the rake task added in #5995, we no longer need
    the task - or its associated tests - and will remove them in the
    next commit.
    
    However, we don't want to lose the precedent we've set of
    beginning to test our rake tasks. It will be less of a barrier for
    future developers to write test-driven rake tasks if there exists
    a framework within which to write the tests. For that reason, I've
    written some tests for the `publishing_api:bulk_republish:for_organisation`
    rake task, which will mean publishing_api_test.rb itself won't
    be deleted in the next commit.
    
    The hope is that future rake tasks will have tests written for
    them, and perhaps the existing rake tasks will have some more
    tests retrospectively written for them like in this commit.
    
    Trello: https://trello.com/c/Oicf9IVG/2354-5-investigate-fix-withdrawn-explanations-not-updating-in-whitehall

--- test/lib/tasks/publishing_api_test.rb
+++ test/lib/tasks/publishing_api_test.rb
@@ -7,6 +7,36 @@ class PublishingApiRake < ActiveSupport::TestCase
     task.reenable # without this, calling `invoke` does nothing after first test
   end
 
+  describe "republishing all documents of a given organisation" do
+    let(:org) { create(:organisation) }
+    let(:task) { Rake::Task["publishing_api:bulk_republish:for_organisation"] }
+
+    test "Republishes the latest edition for each document owned by the organisation" do
+      edition = create(:published_news_article, organisations: [org])
+
+      PublishingApiDocumentRepublishingWorker.expects(:perform_async_in_queue).with(
+        "bulk_republishing",
+        edition.document.id,
+        true,
+      ).once
+
+      task.invoke(org.slug)
+    end
+
+    test "Ignores documents owned by other organisation" do
+      some_other_org = create(:organisation)
+      edition = create(:published_news_article, organisations: [some_other_org])
+
+      PublishingApiDocumentRepublishingWorker.expects(:perform_async_in_queue).with(
+        "bulk_republishing",
+        edition.document.id,
+        true,
+      ).never
+
+      task.invoke(org.slug)
+    end
+  end
+
   describe "when fixing up a withdrawn document" do
     let(:task) { Rake::Task["publishing_api:bulk_republish:fix_withdrawn_documents"] }
 
