aaf4013e633eb0c9e15aa1516e426d359ab9b53b
Kevin Dew <kevindew@me.com>
2021-03-19 19:11:51 +0000

    Resolve Lint/EmptyFile Rubocop offence
    This offence was caused because this repo had an empty file committed.
    After having a look I decided this file didn't need to exist and changed
    this generator to accommodate this.

--- lib/generators/data_migration/data_migration_generator.rb
+++ lib/generators/data_migration/data_migration_generator.rb
@@ -1,14 +1,8 @@
 require "rails/generators"
 
 class DataMigrationGenerator < Rails::Generators::NamedBase
-  include Rails::Generators::Migration
-  source_root File.expand_path("templates", __dir__)
-
-  def self.next_migration_number(_path)
-    Time.zone.now.utc.strftime("%Y%m%d%H%M%S")
-  end
-
   def create_data_migration
-    migration_template "data_migration.rb", Rails.root.join("db/data_migration", "#{file_name}.rb")
+    prefix = Time.zone.now.utc.strftime("%Y%m%d%H%M%S")
+    create_file Rails.root.join("db/data_migration/#{prefix}_#{file_name}.rb")
   end
 end
