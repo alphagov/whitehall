88bfada197e08515c4112f81f6ea1d0c1905cf34
Chris Yoong <chris.yoong@digital.cabinet-office.gov.uk>
2021-05-11 17:36:11 +0100

    Refactor: Operational fields
    Eg:
    government/fields-of-operation/
    government/fields-of-operation/iraq
    
    Remove shared heading local partial, apply Design System, remove redundant CSS.
    Use gem:
    - Headings
    - Page Title
    - Contents List
    - Org logo
    - List
    Update testing
    
    Removing testing for ID.
    revert
    Refactor: Operation fields
    
    Headings
    Page Title
    Contents List
    Org logo
    
    Maintaining and list elements, slight application of Design System.  Gem list Component cannot be used due to not being able to pass ID (failing tests)
    a
    a
    a

--- app/controllers/operational_fields_controller.rb
+++ app/controllers/operational_fields_controller.rb
@@ -12,5 +12,21 @@ class OperationalFieldsController < PublicFacingController
     @fatality_notices = @operational_field.published_fatality_notices.order("first_published_at desc").map do |fatality_notice|
       FatalityNoticePresenter.new(fatality_notice)
     end
+
+    render locals: { contents_menu: contents_menu }
+  end
+
+  def contents_menu
+    @contents_menu = []
+
+    if @operational_field.description.present?
+      @contents_menu << { href: "#field-of-operation", text: "Field of operation" }
+    end
+
+    if @fatality_notices.any?
+      @contents_menu << { href: "#fatalities", text: "Fatalities" }
+    end
+
+    @contents_menu
   end
 end
