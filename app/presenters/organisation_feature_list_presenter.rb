6b16a2c43461fb7e539c6353e6cc274cd3f11437
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-06-29 15:15:23 +0100

    Remove unused Presenter

--- app/presenters/organisation_feature_list_presenter.rb
+++
@@ -1,9 +0,0 @@
-class OrganisationFeatureListPresenter < FeatureListPresenter
-  delegate_instance_methods_of FeatureList
-
-  def initialize(organisation, view_context)
-    @organisation = organisation
-    super(@organisation.feature_list_for_locale(I18n.locale), view_context)
-    limit_to 6
-  end
-end
