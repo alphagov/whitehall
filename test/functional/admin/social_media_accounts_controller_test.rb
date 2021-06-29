75c59399e143c87ccd978e91a7f590865c7f243d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-06-29 12:19:32 +0100

    Allow editing of social media links in other languages
    This updates the social media links for socialable entities (i.e.
    Organisation and WorldwideOrganisation) to:
    - display a links to edit social media links in all languages that the
      entity is translated into
    - accept `locale` as a parameter when updating the social media links

--- test/functional/admin/social_media_accounts_controller_test.rb
+++ test/functional/admin/social_media_accounts_controller_test.rb
@@ -78,4 +78,40 @@ class Admin::SocialMediaAccountsControllerTest < ActionController::TestCase
     assert_equal "#{social_media_account.service_name} account deleted successfully", flash[:notice]
     assert_not SocialMediaAccount.exists?(social_media_account.id)
   end
+
+  view_test "GET on :index displays edit button only for languages the organisation is translated in" do
+    organisation = create(:organisation, translated_into: %i[fr cy])
+    social_media_account = create(:social_media_account, socialable_id: organisation.id, socialable_type: "Organisation")
+    get :index, params: { organisation_id: organisation.id }
+    assert_select "a[href=?]", edit_admin_organisation_social_media_account_path(organisation_id: organisation.slug, id: social_media_account.id)
+    assert_select "a[href=?]", edit_admin_organisation_social_media_account_path(organisation_id: organisation.slug, id: social_media_account.id, params: { locale: "fr" })
+    assert_select "a[href=?]", edit_admin_organisation_social_media_account_path(organisation_id: organisation.slug, id: social_media_account.id, params: { locale: "cy" })
+    refute_select "a[href=?]", edit_admin_organisation_social_media_account_path(organisation_id: organisation.slug, id: social_media_account.id, params: { locale: "dk" })
+  end
+
+  test "PUT on :update with a locale updates only the translation of the social media account" do
+    worldwide_organisation = create(:worldwide_organisation, translated_into: [:cy])
+    social_media_account = worldwide_organisation.social_media_accounts.create!(social_media_service_id: @social_media_service.id, url: "http://english-url.com", title: "Title in English")
+
+    put :update,
+        params: {
+          id: social_media_account,
+          worldwide_organisation_id: worldwide_organisation,
+          social_media_account: {
+            url: "http://welsh-url.cy",
+            title: "Title in Welsh",
+            locale: "cy",
+          },
+        }
+
+    I18n.with_locale(:en) do
+      assert_equal "http://english-url.com", social_media_account.reload.url
+      assert_equal "Title in English", social_media_account.reload.title
+    end
+
+    I18n.with_locale(:cy) do
+      assert_equal "http://welsh-url.cy", social_media_account.reload.url
+      assert_equal "Title in Welsh", social_media_account.reload.title
+    end
+  end
 end
