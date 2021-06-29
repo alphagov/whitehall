2fe204dba862586265358d4af1e002d44acafa9a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-06-29 10:43:25 +0100

    Add a translated social media links table
    This will allow us to store multiple translations for each social media
    link (e.g. if an organisation has a different social media account for
    non-English content).

---
+++ app/models/social_media_account_translation.rb
@@ -0,0 +1,1 @@
+class SocialMediaAccountTranslation < ApplicationRecord; end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_161500) do
+ActiveRecord::Schema.define(version: 2021_06_16_135328) do
 
   create_table "about_pages", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "topical_event_id"
@@ -933,6 +933,17 @@ ActiveRecord::Schema.define(version: 2021_01_11_161500) do
     t.text "govspeak"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "social_media_account_translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.text "url"
+    t.text "title"
+    t.string "locale", null: false
+    t.integer "social_media_account_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["locale"], name: "index_on_locale"
+    t.index ["social_media_account_id"], name: "index_on_social_media_account"
   end
 
   create_table "social_media_accounts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
