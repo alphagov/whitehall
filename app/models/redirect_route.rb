f2c25bc5ad88e2b4cb8946682948339f15466bc3
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-06-29 14:10:46 +0100

    Refactor Publishing API rake file
    Previously this rake file was quite disorganised with inconsistencies
    like tasks being in the wrong name space.
    
    This commit:
    
    * Moves tasks to the correct namespace
    
    * Renames some tasks to try and gain some naming consistency
    
    * Moves redirect routes and special routes to their own model which will
    help with testing in the next commit

---
+++ app/models/redirect_route.rb
@@ -0,0 +1,11 @@
+class RedirectRoute
+  def self.all
+    [
+      {
+        base_path: "/government/world",
+        destination: "/world",
+        content_id: "36620cf9-00b3-4d51-afab-e05e457061e3",
+      },
+    ]
+  end
+end
---
+++ app/models/special_route.rb
@@ -0,0 +1,44 @@
+class SpecialRoute
+  def self.all
+    [
+      {
+        base_path: "/government",
+        content_id: "4672b1ff-f147-4d49-a5f4-4959588da5a8",
+        title: "Government prefix",
+        description: "The prefix route under which almost all government content is published.",
+      },
+      {
+        base_path: "/government/feed",
+        content_id: "725a346f-9e5b-486d-873d-2b050c126e09",
+        title: "Government feed",
+        description: "This route serves the feed of published content",
+        rendering_app: Whitehall::RenderingApp::COLLECTIONS_FRONTEND,
+        type: "exact",
+      },
+      {
+        base_path: "/courts-tribunals",
+        content_id: "f990c58c-687a-4baf-b1a0-ec2d02c4d654",
+        title: "Courts and tribunals",
+        description: "Courts and tribunals on GOV.UK.",
+      },
+      {
+        base_path: "/api/governments",
+        content_id: "2d5bafcc-2c45-4a84-8fbc-525b75dd6d19",
+        title: "Governments API",
+        description: "API exposing all governments on GOV.UK.",
+      },
+      {
+        base_path: "/api/world-locations",
+        content_id: "2a63b605-77be-4af5-932d-224a054dd5a5",
+        title: "World Locations API",
+        description: "API exposing all world locations on GOV.UK.",
+      },
+      {
+        base_path: "/api/worldwide-organisations",
+        content_id: "736f8a5a-ce6f-4a6f-b0cb-954442aa23c1",
+        title: "Worldwide Organisations API",
+        description: "API exposing all worldwide organisations on GOV.UK.",
+      },
+    ]
+  end
+end
