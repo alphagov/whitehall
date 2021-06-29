66d47381a7f10cc329cb0a197c075300c62fa867
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-06-29 15:06:51 +0100

    Remove unused classes from lib/

--- lib/generators/data_migration/data_migration_generator.rb
+++
@@ -1,8 +0,0 @@
-require "rails/generators"
-
-class DataMigrationGenerator < Rails::Generators::NamedBase
-  def create_data_migration
-    prefix = Time.zone.now.utc.strftime("%Y%m%d%H%M%S")
-    create_file Rails.root.join("db/data_migration/#{prefix}_#{file_name}.rb")
-  end
-end
