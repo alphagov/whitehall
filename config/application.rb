319df0bd5b9d4993ee8e0430ff2fbc009a5cf32a
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2021-06-30 17:58:44 +0100

    Run rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require_relative "boot"
 
 require "rails"
-
 # Pick the frameworks you want:
 require "active_model/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
