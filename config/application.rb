151c18c8142b3bd08c93c76887c61e6523009866
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-08-05 15:30:28 +0100

    Update config.load_defaults to 5.1

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Whitehall
   class Application < Rails::Application
     require "whitehall"
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.1
 
     # Turn off `belongs_to` associations by default. This is turned on by default in Rails 5.0.
     config.active_record.belongs_to_required_by_default = false
