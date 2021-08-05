697c38c422a726c01098b11f3314f289339ffd0c
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-08-05 12:38:24 +0100

    Run app:update on the current rails version in gemfile
    Whilst the gemfile has v6.0.3.7, the application is still using
    the previous version's defaults (5.0)

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 module Whitehall
   class Application < Rails::Application
     require "whitehall"
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
