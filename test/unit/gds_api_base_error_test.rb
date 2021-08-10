c97eaec1fec6a049ba31eb52377243fc240c7e54
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-08-10 08:58:09 +0100

    Rename raven_context to sentry_context
    This was renamed in https://github.com/alphagov/gds-api-adapters/commit/d05134bbd18a33fe946edeb1144cdb97ce6c8153,
    so requires updating the equivalent bit in Whitehall before
    we can bump to gds-api-adapters v72.
    
    This `sentry_context` defines how events/errors are grouped as
    'Issues' in Sentry. The gds-api-adapters implementation groups
    exceptions by type instead of message, so all exceptions like
    `GdsApi::TimedOutException` will get grouped as one error and
    not an error per URL. Whitehall removes this context, to avoid
    grouping unrelated exceptions.
    
    Trello: https://trello.com/c/CV81BgcT/2252-update-whitehall-to-use-govukappconfig-v4-and-latest-gds-api-adapters-3

--- test/unit/gds_api_base_error_test.rb
+++ test/unit/gds_api_base_error_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class GdsApiBaseErrorTest < ActiveSupport::TestCase
-  test "removes raven_context method to avoid grouping unrelated exceptions" do
-    assert_not GdsApi::BaseError.new.respond_to?(:raven_context)
+  test "removes sentry_context method to avoid grouping unrelated exceptions" do
+    assert_not GdsApi::BaseError.new.respond_to?(:sentry_context)
   end
 end
