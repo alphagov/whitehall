d95da9785518d3ba40db412d85395100c6518bdd
Alex J <alexandru.jurubita@digital.cabinet-office.gov.uk>
2021-08-16 16:50:37 +0100

    Swap to `gem_layout`
    Turn off compatibility with govuk_template
    Remove component_support, as this is provided by static and replace `@extend` calls as they are no longer available without the component_support
    Remove feedback component assets, as these are now provided by static as part of the public layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,6 @@
 //
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
-//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
 //
 //= require jquery_ujs
