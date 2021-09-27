01d33678c3b95d9a8676eda201c98cbe311277fb
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-09-27 10:59:55 +0100

    Use translations table for featured links
    This uses the `globalize` gem's `translates` method to make the URL and
    title translatable for featured links.

--- test/unit/featured_link_test.rb
+++ test/unit/featured_link_test.rb
@@ -20,4 +20,25 @@ class FeaturedLinkTest < ActiveSupport::TestCase
     link = build(:featured_link, title: "a title", url: "http://my.example.com/path")
     assert link.valid?
   end
+
+  test "should accept multiple translations" do
+    link = create(:featured_link)
+
+    I18n.with_locale(:en) do
+      link.update(url: "https://example.com", title: "Title in English")
+    end
+    I18n.with_locale(:cy) do
+      link.update(url: "https://example.cy", title: "Title in Welsh")
+    end
+
+    I18n.with_locale(:en) do
+      assert_equal "https://example.com", link.url
+      assert_equal "Title in English", link.title
+    end
+
+    I18n.with_locale(:cy) do
+      assert_equal "https://example.cy", link.url
+      assert_equal "Title in Welsh", link.title
+    end
+  end
 end
