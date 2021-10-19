a73455f552f29cbe1d7179f661a7d632a0e8a327
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-10-19 17:07:02 +0100

    DRY-up extraction of contacts from policy groups
    We now have a method on the PolicyGroup model which allows us to extract
    and store contacts. Therefore we don't need to have exactly the same
    code in the migration.

--- db/migrate/20211019095600_create_policy_group_dependencies.rb
+++ db/migrate/20211019095600_create_policy_group_dependencies.rb
@@ -9,14 +9,6 @@ class CreatePolicyGroupDependencies < ActiveRecord::Migration[6.1]
     add_index :policy_group_dependencies, %i[dependable_id dependable_type policy_group_id],
               unique: true, name: "index_policy_group_dependencies_on_dependable_and_policy_group"
 
-    PolicyGroup.all.each do |policy_group|
-      Govspeak::ContactsExtractor.new(policy_group.description).contacts.uniq.each do |contact|
-        PolicyGroupDependency.create(
-          policy_group_id: policy_group.id,
-          dependable_type: "Contact",
-          dependable_id: contact.id,
-        )
-      end
-    end
+    PolicyGroup.all.map(&:extract_dependencies)
   end
 end
