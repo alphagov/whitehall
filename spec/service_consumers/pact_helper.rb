4f3ea13cc2de7688d394c1a217b39f49a52c9563
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2021-12-06 16:40:43 +0000

    Fix failing Pact test by using correct branch
    This change configures the Pact tests to default to reading the `branch-master` version from the Pact Broker. Previously it was using a temporary branch which no longer exists, which was resulting in 404 Not Found responses from the broker.

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -27,7 +27,7 @@ Pact.service_provider "Whitehall API" do
     else
       base_url = "https://pact-broker.cloudapps.digital"
       path = "pacts/provider/#{url_encode(name)}/consumer/#{url_encode(consumer_name)}"
-      version_modifier = "versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'branch-whitehall-api-pact-tests'))}"
+      version_modifier = "versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'branch-master'))}"
 
       pact_uri("#{base_url}/#{path}/#{version_modifier}")
     end
