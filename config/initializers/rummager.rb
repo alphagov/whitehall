a531e39376ba48c910098e70c9240207e5e0a88a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-12-08 10:02:50 +0000

    Remove FakeRummagerApiForStatisticsAnnouncements
    We have a class named DevelopmentModeStubs::FakeRummagerApiForStatisticsAnnouncements
    which is used to mock Search API (previously known as Rummager) in
    tests.
    
    However it appears this mock is not being used for anything in any of
    the tests (except the test for the mock itself), so it can be removed.
    
    The mock also contains SQL queries that are not compatible with MySQL 8,
    so are blocking us from upgrading.

--- config/initializers/rummager.rb
+++ config/initializers/rummager.rb
@@ -11,14 +11,10 @@ Whitehall.search_client = GdsApi::Search.new(
 )
 
 def statistics_announcement_search_client
-  if Rails.env.test?
-    DevelopmentModeStubs::FakeRummagerApiForStatisticsAnnouncements
-  else
-    GdsApi::Search.new(
-      Plek.find("search") + Whitehall::SearchIndex.government_search_index_path,
-      bearer_token: ENV["RUMMAGER_BEARER_TOKEN"] || "example",
-    )
-  end
+  GdsApi::Search.new(
+    Plek.find("search") + Whitehall::SearchIndex.government_search_index_path,
+    bearer_token: ENV["RUMMAGER_BEARER_TOKEN"] || "example",
+  )
 end
 
 Whitehall.statistics_announcement_search_client = statistics_announcement_search_client
