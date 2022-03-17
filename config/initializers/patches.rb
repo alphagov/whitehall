2d719b50bbd83261a570d9fa61072f8f0d7032f9
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2022-03-17 10:35:29 +0000

    Ignore lib/patches to avoid breaking zeitwerk

--- config/initializers/patches.rb
+++ config/initializers/patches.rb
@@ -1,3 +1,8 @@
+# Naming of files and constants in `lib/patches` is inconsistent with
+# Zeitwerk's expectations, so instead we require them all manually and tell
+# Zeitwerk to ignore the whole directory.
 Dir[Rails.root.join("lib/patches/*.rb")].sort.each do |patch|
   require patch
 end
+
+Rails.autoloaders.main.ignore("lib/patches")
