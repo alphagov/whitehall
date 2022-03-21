29d9f47052165ea04321f7f47b9769cf7d17c303
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2022-03-21 14:36:14 +0000

    Enable unsafe redirect protection and explicitly allow safe occurrences
    By default redirects to other hosts are no longer permitted; but the
    cases of this we have in our code are false positives pointing at our
    own domain.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -25,7 +25,7 @@ private
 
   def redirect_to_research_and_statistics(format = "")
     base_path = "#{Plek.new.website_root}/search/research-and-statistics#{format}"
-    redirect_to("#{base_path}?#{research_and_statistics_query_string}")
+    redirect_to("#{base_path}?#{research_and_statistics_query_string}", allow_other_host: true)
   end
 
   def research_and_statistics_query_string
