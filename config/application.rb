f42671891f64beeaaf8bb80396e8639d6bf53829
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2022-03-22 11:38:42 +0000

    Rotate cookies for Rails 7 upgrade

--- config/application.rb
+++ config/application.rb
@@ -90,5 +90,22 @@ module Whitehall
     # allow overriding the asset host with an enironment variable, useful for
     # when router is proxying to this app but asset proxying isn't set up.
     config.asset_host = ENV["ASSET_HOST"]
+
+    # Rotate SHA1 cookies to SHA256 (the new Rails 7 default)
+    # TODO: Remove this after existing user sessions have been rotated
+    # https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256
+    config.action_dispatch.cookies_rotations.tap do |cookies|
+      salt = config.action_dispatch.authenticated_encrypted_cookie_salt
+      secret_key_base = Whitehall::Application.secrets.secret_key_base
+      next if secret_key_base.blank?
+
+      key_generator = ActiveSupport::KeyGenerator.new(
+        secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+      )
+      key_len = ActiveSupport::MessageEncryptor.key_len
+      secret = key_generator.generate_key(salt, key_len)
+
+      cookies.rotate :encrypted, secret
+    end
   end
 end
