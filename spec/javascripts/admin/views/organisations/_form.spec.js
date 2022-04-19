74930f693b87d23bbf4b006f4868dac4fd6c531d
Kevin Dew <kevindew@me.com>
2022-04-19 19:21:40 +0100

    Port organisations/_form JS tests from QUnit to Jasmine

--- spec/javascripts/admin/views/organisations/_form.spec.js
+++ spec/javascripts/admin/views/organisations/_form.spec.js
@@ -1,64 +1,60 @@
-module('admin-organisations-form', {
-  setup: function () {
-    $('#qunit-fixture').append(
-      '<select id="organisation_govuk_status" name="organisation_govuk_status">' +
-        '<option value="closed">Closed</option>' +
-        '<option value="open" selected="selected">Open</option>' +
-      '</select>' +
-      '<p class="js-closed-organisation-field">' +
-      '<select id="organisation_govuk_closed_status" name="organisation_govuk_closed_status">' +
-        '<option value="merged">Merged</option>' +
-        '<option value="no_longer_exists" selected="selected">No longer exists</option>' +
-      '</select>' +
-      '</p>' +
-      '<p class="js-closed-organisation-field">' +
-        '<label for="organisation_closed_at">Closed at?</label>' +
-        '<input id="test_child">' +
-      '</p>' +
-      '<p class="js-superseded-organisation-field">' +
-        '<label for="organisation_superseding_organisation_ids">Superseding organisations</label>' +
-        '<input id="test_child_2">' +
-      '</p>'
+describe('GOVUK.organisationsForm', function () {
+  var container
+
+  beforeEach(function () {
+    container = $(
+      '<div>' +
+        '<select id="organisation_govuk_status" name="organisation_govuk_status">' +
+          '<option value="closed">Closed</option>' +
+          '<option value="open" selected="selected">Open</option>' +
+        '</select>' +
+        '<p class="js-closed-organisation-field">' +
+        '<select id="organisation_govuk_closed_status" name="organisation_govuk_closed_status">' +
+          '<option value="merged">Merged</option>' +
+          '<option value="no_longer_exists" selected="selected">No longer exists</option>' +
+        '</select>' +
+        '</p>' +
+        '<p class="js-closed-organisation-field">' +
+          '<label for="organisation_closed_at">Closed at?</label>' +
+          '<input id="test_child">' +
+        '</p>' +
+        '<p class="js-superseded-organisation-field">' +
+          '<label for="organisation_superseding_organisation_ids">Superseding organisations</label>' +
+          '<input id="test_child_2">' +
+        '</p>' +
+      '</div>'
     )
-  }
-})
 
-test('Should hide closed organisation fields when organisation is not closed', function () {
-  var $closedGroup = $('.js-closed-organisation-field')
-  GOVUK.organisationsForm.init()
-  ok($closedGroup.is(':hidden'))
-  ok($closedGroup.find('input').is(':disabled'))
-})
+    $(document.body).append(container)
+    GOVUK.organisationsForm.init()
+  })
 
-test('should show closed organisation fields when organisation is closed', function () {
-  var $select = $('#organisation_govuk_status')
-  var $closedGroup = $('.js-closed-organisation-field')
-  GOVUK.organisationsForm.init()
+  afterEach(function () {
+    container.remove()
+  })
 
-  $select.find('option[value=closed]').attr('selected', true)
-  $select.change()
+  it('should hide closed organisation fields when organisation is not closed', function () {
+    expect(container.find('.js-closed-organisation-field').is(':hidden')).toBeTrue()
+    expect(container.find('.js-closed-organisation-field input').is(':disabled')).toBeTrue()
+  })
 
-  ok(!$closedGroup.find('input').is(':disabled'))
-  ok(!$closedGroup.is(':hidden'))
-})
+  it('should show closed organisation fields when organisation is closed', function () {
+    container.find('#organisation_govuk_status').val('closed').trigger('change')
 
-test('Should hide superseded organisation fields when organisation is not closed', function () {
-  var $supersededGroup = $('.js-superseded-organisation-field')
-  GOVUK.organisationsForm.init()
-  ok($supersededGroup.is(':hidden'))
-  ok($supersededGroup.find('input').is(':disabled'))
-})
+    expect(container.find('.js-closed-organisation-field').is(':hidden')).toBeFalse()
+    expect(container.find('.js-closed-organisation-field input').is(':disabled')).toBeFalse()
+  })
 
-test('should show superseded organisation fields when organisation closed status is supersedable', function () {
-  var $closedSelect = $('#organisation_govuk_status')
-  var $closedStatusSelect = $('#organisation_govuk_closed_status')
-  var $supersededGroup = $('.js-superseded-organisation-field')
-  GOVUK.organisationsForm.init()
-  $closedSelect.find('option[value=closed]').attr('selected', true)
-  $closedSelect.change()
-  $closedStatusSelect.find('option[value=merged]').attr('selected', true)
-  $closedStatusSelect.change()
+  it('should hide superseded organisation fields when organisation is not closed', function () {
+    expect(container.find('.js-superseded-organisation-field').is(':hidden')).toBeTrue()
+    expect(container.find('.js-superseded-organisation-field input').is(':disabled')).toBeTrue()
+  })
 
-  ok(!$supersededGroup.is(':hidden'))
-  ok(!$supersededGroup.find('input').is(':disabled'))
+  it('should show superseded organisation fields when organisation closed status is supersedable', function () {
+    container.find('#organisation_govuk_status').val('closed').trigger('change')
+    container.find('#organisation_govuk_closed_status').val('merged').trigger('change')
+
+    expect(container.find('.js-superseded-organisation-field').is(':hidden')).toBeFalse()
+    expect(container.find('.js-superseded-organisation-field input').is(':disabled')).toBeFalse()
+  })
 })
