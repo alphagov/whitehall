c2bd3decf52dd4ebdf081c52be9c57eb3de97964
Kevin Dew <kevindew@me.com>
2022-04-19 19:39:06 +0100

    Port sortable JS tests from QUnit to Jasmine
    We no longer need to stub sortable as it is included in the Whitehall
    admin JS bundle.

--- spec/javascripts/admin/sortable.spec.js
+++ spec/javascripts/admin/sortable.spec.js
@@ -1,24 +1,33 @@
-// jQuery UI comes from the static application
-$.fn.sortable = function () {} // noop
+describe('jQuery.enableSortable', function () {
+  var container, fieldset
 
-module('Sorting options', {
-  setup: function () {
-    this.container = $('<fieldset></fieldset>')
-    var thingOne = $('<div><label for="input_one">one <input name="input_one" type="text" /></label><label for="other_thing_one">other thing: <input name="other_thing_one" type="text" /></label></div>')
-    var thingTwo = $('<div><label for="input_two">two <input name="input_two" type="text" /></label><label for="other_thing_two">other thing: <input name="other_thing_two" type="text" /></label></div>')
-    this.container.append(thingOne)
-    this.container.append(thingTwo)
-    $('#qunit-fixture').append(this.container)
-  }
-})
+  beforeEach(function () {
+    fieldset = $(
+      '<fieldset>' +
+        '<div><label for="input_one">one <input name="input_one" type="text" /></label><label for="other_thing_one">other thing: <input name="other_thing_one" type="text" /></label></div>' +
+        '<div><label for="input_two">two <input name="input_two" type="text" /></label><label for="other_thing_two">other thing: <input name="other_thing_two" type="text" /></label></div>' +
+      '</fieldset>'
+    )
+    container = $('<div />').append(fieldset)
+    $(document.body).append(container)
+  })
 
-test('should build a list from the contents of the labels', function () {
-  this.container.enableSortable()
-  var list = this.container.siblings('ul')
-  equal('one other thing: two other thing: ', list.text()) // I would compare arrays, but Javascript can't do that easily. GAH.
-})
+  afterEach(function () {
+    container.remove()
+  })
 
-test('should hide the input fields', function () {
-  this.container.enableSortable()
-  ok(!this.container.children('input').is(':visible'))
+  it('should build a list from the contents of the labels', function () {
+    fieldset.enableSortable()
+    var listText = fieldset
+      .siblings('ul')
+      .children()
+      .map(function () { return $(this).text() })
+      .get()
+    expect(listText).toEqual(['one other thing: ', 'two other thing: '])
+  })
+
+  it('should hide the input fields', function () {
+    fieldset.enableSortable()
+    expect(fieldset.children('input').is(':visible')).toBeFalse()
+  })
 })
