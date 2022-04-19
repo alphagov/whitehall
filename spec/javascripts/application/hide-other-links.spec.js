9761162e8d08c0dfbba3e1a9f8cba81c34c3177c
Kevin Dew <kevindew@me.com>
2022-04-19 19:47:15 +0100

    Port hide-other-links JS tests from QUnit to Jasmine

--- spec/javascripts/application/hide-other-links.spec.js
+++ spec/javascripts/application/hide-other-links.spec.js
@@ -1,98 +1,105 @@
-module('hide-other-things: hides all but first element in collection', {
-  setup: function () {
-    this.$list = $(
+describe('jQuery.hideOtherLinks', function () {
+  var list
+
+  beforeEach(function () {
+    list = $(
       '<dl>' +
-      '<dt>The Four Main Animals</dt>' +
-      '<dd class="animals js-hide-other-links">' +
-      '<a class="force" href="http://en.wikipedia.org/wiki/dog">Dog</a>, ' +
-      '<a class="force" href="http://en.wikipedia.org/wiki/cat">Cat</a>, ' +
-      '<a href="http://en.wikipedia.org/wiki/cow">Cow</a> and ' +
-      '<a href="http://en.wikipedia.org/wiki/pig">Pig</a>.' +
-      '</dd>' +
-      '<dt>The Four Main Colours</dt>' +
-      '<dd class="colours js-hide-other-links">' +
-      '<span><a href="http://en.wikipedia.org/wiki/red">Red</a></span>, ' +
-      '<span><a href="http://en.wikipedia.org/wiki/green">Green</a></span>, ' +
-      '<span><a href="http://en.wikipedia.org/wiki/blue">Blue</a></span> and ' +
-      '<span><a href="http://en.wikipedia.org/wiki/yello">Yellow</a></span>.' +
-      '</dd>' +
-      '<dt>The Two Main Four Main Things</dt>' +
-      '<dd class="main-things js-hide-other-links">' +
-      '<a href="http://en.wikipedia.org/wiki/animals">Animals</a>, ' +
-      '<a href="http://en.wikipedia.org/wiki/colours">Colours</a>, ' +
-      '</dd>' +
-      '<dt>The Two Main Really Long Words</dt>' +
-      '<dd class="long-words js-hide-other-links">' +
-      '<a href="http://en.wikipedia.org/wiki/Lopado­temacho­selacho­galeo­kranio­leipsano­drim­hypo­trimmato­silphio­parao­melito­katakechy­meno­kichl­epi­kossypho­phatto­perister­alektryon­opte­kephallio­kigklo­peleio­lagoio­siraio­baphe­tragano­pterygon">Lopado­temacho­selacho­galeo­kranio­leipsano­drim­hypo­trimmato­silphio­parao­melito­katakechy­meno­kichl­epi­kossypho­phatto­perister­alektryon­opte­kephallio­kigklo­peleio­lagoio­siraio­baphe­tragano­pterygon</a>, ' +
-      '<a href="http://en.wikipedia.org/wiki/Pneumonoultramicroscopicsilicovolcanoconiosis">Pneumonoultramicroscopicsilicovolcanoconiosis</a>, ' +
-      '</dd>' +
-      '</dl>')
-    $('#qunit-fixture').append(this.$list)
-  }
-})
+        '<dt>The Four Main Animals</dt>' +
+        '<dd class="animals js-hide-other-links">' +
+          '<a class="force" href="http://en.wikipedia.org/wiki/dog">Dog</a>, ' +
+          '<a class="force" href="http://en.wikipedia.org/wiki/cat">Cat</a>, ' +
+          '<a href="http://en.wikipedia.org/wiki/cow">Cow</a> and ' +
+          '<a href="http://en.wikipedia.org/wiki/pig">Pig</a>.' +
+        '</dd>' +
+        '<dt>The Four Main Colours</dt>' +
+        '<dd class="colours js-hide-other-links">' +
+          '<span><a href="http://en.wikipedia.org/wiki/red">Red</a></span>, ' +
+          '<span><a href="http://en.wikipedia.org/wiki/green">Green</a></span>, ' +
+          '<span><a href="http://en.wikipedia.org/wiki/blue">Blue</a></span> and ' +
+          '<span><a href="http://en.wikipedia.org/wiki/yello">Yellow</a></span>.' +
+        '</dd>' +
+        '<dt>The Two Main Four Main Things</dt>' +
+        '<dd class="main-things js-hide-other-links">' +
+          '<a href="http://en.wikipedia.org/wiki/animals">Animals</a>, ' +
+          '<a href="http://en.wikipedia.org/wiki/colours">Colours</a>, ' +
+        '</dd>' +
+        '<dt>The Two Main Really Long Words</dt>' +
+        '<dd class="long-words js-hide-other-links">' +
+          '<a href="http://en.wikipedia.org/wiki/Lopado­temacho­selacho­galeo­kranio­leipsano­drim­hypo­trimmato­silphio­parao­melito­katakechy­meno­kichl­epi­kossypho­phatto­perister­alektryon­opte­kephallio­kigklo­peleio­lagoio­siraio­baphe­tragano­pterygon">Lopado­temacho­selacho­galeo­kranio­leipsano­drim­hypo­trimmato­silphio­parao­melito­katakechy­meno­kichl­epi­kossypho­phatto­perister­alektryon­opte­kephallio­kigklo­peleio­lagoio­siraio­baphe­tragano­pterygon</a>, ' +
+          '<a href="http://en.wikipedia.org/wiki/Pneumonoultramicroscopicsilicovolcanoconiosis">Pneumonoultramicroscopicsilicovolcanoconiosis</a>, ' +
+        '</dd>' +
+      '</dl>'
+    )
+    $(document.body).append(list)
+  })
 
-test('should group elements into other-content span', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok($('.animals .other-content').length > 0 && $('.animals .other-content').children().length === 3)
-})
+  afterEach(function () {
+    list.remove()
+  })
 
-test('should create a link to show hidden content', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok($('.animals .show-other-content').length > 0)
-})
+  it('should group elements into other-content span', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.animals .other-content').children().length).toEqual(3)
+  })
 
-test('created link should show hidden content', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok(!$('.animals .other-content').is(':visible'))
-  $('.show-other-content').click()
-  ok($('.animals .other-content').is(':visible'))
-})
+  it('should create a link to show hidden content', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.animals .show-other-content').length).toEqual(1)
+  })
 
-test('created link should have correct count', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  var otherCount = $('.animals .other-content').find('a').length
-  var linkCount = $('.animals .show-other-content').text().match(/\d+/).pop()
-  ok(parseInt(linkCount, 10) === otherCount)
-})
+  it('should show hidden content when the other-content link is clicked', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.animals .other-content').is(':visible')).toBeFalse()
+    $('.show-other-content').click()
+    expect($('.animals .other-content').is(':visible')).toBeTrue()
+  })
 
-test('check fullstop is preserved', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok($('.animals.js-hide-other-links').text().substr(-1) === '.')
-})
+  it('should have correct count in the link', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    var otherCount = $('.animals .other-content').find('a').length
+    var linkCount = $('.animals .show-other-content').text().match(/\d+/).pop()
+    expect(parseInt(linkCount, 10)).toEqual(otherCount)
+  })
 
-test('check element has correct aria value', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok($('.animals.js-hide-other-links').attr('aria-live') === 'polite')
-})
+  it('should preserve a full stop', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.animals.js-hide-other-links').text().substr(-1)).toEqual('.')
+  })
 
-test('check different elements can be used as wrapper', function () {
-  $('.js-hide-other-links').hideOtherLinks({ linkElement: 'span' })
-  ok($('.colours .other-content').length > 0 && $('.colours .other-content').children().length === 3)
-})
+  it('should set the correct aria value', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.animals.js-hide-other-links').attr('aria-live')).toEqual('polite')
+  })
 
-test('check class can be used to force elements to be visible', function () {
-  $('.js-hide-other-links').hideOtherLinks({ alwaysVisibleClass: '.force' })
-  ok($('.animals .other-content').length > 0 && $('.animals .other-content').children().length === 2)
-})
+  it('should allow different elements to be used as wrapper', function () {
+    $('.js-hide-other-links').hideOtherLinks({ linkElement: 'span' })
+    expect($('.colours .other-content').children().length).toEqual(3)
+  })
 
-test('when there are only two things, they are not hidden', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok($('.main-things .other-content').length === 0)
-})
+  it('should allow a force class to keep an element visibile', function () {
+    $('.js-hide-other-links').hideOtherLinks({ alwaysVisibleClass: '.force' })
+    expect($('.animals .other-content').children().length).toEqual(2)
+  })
 
-test('when there are two really long things, the second is hidden', function () {
-  $('.js-hide-other-links').hideOtherLinks()
-  ok($('.long-words .other-content').children().length === 1)
-})
+  it("shouldn't hide items if there are only 2 links and they are short", function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.main-things .other-content').length).toEqual(0)
+  })
 
-test('when showCount is 2, it shows two things', function () {
-  $('.js-hide-other-links').hideOtherLinks({ showCount: 2 })
-  ok($('.animals').children('a').length === 3)
-  ok($('.animals .other-content').children().length === 2)
-})
+  it('should hide items when there are only 2 links but they are very long', function () {
+    $('.js-hide-other-links').hideOtherLinks()
+    expect($('.long-words .other-content').children().length).toEqual(1)
+  })
 
-test('when showCount is 0, it hides everything', function () {
-  $('.js-hide-other-links').hideOtherLinks({ showCount: 0 })
-  ok($('.animals').children('a').length === 1)
-  ok($('.animals .other-content').children().length === 4)
+  it('should accept showCount to specify amount of links to show', function () {
+    $('.js-hide-other-links').hideOtherLinks({ showCount: 3 })
+    expect($('.animals').children('a:not(.show-other-content)').length).toEqual(3)
+    expect($('.animals .other-content').children().length).toEqual(1)
+  })
+
+  it('when showCount is 0, it hides everything', function () {
+    $('.js-hide-other-links').hideOtherLinks({ showCount: 0 })
+    expect($('.animals').children('a:not(.show-other-content)').length).toEqual(0)
+    expect($('.animals .other-content').children().length).toEqual(4)
+  })
 })
