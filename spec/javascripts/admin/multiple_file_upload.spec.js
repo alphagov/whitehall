69f584e155f5501aa6341163e27439509337ef74
Kevin Dew <kevindew@me.com>
2022-04-19 19:35:40 +0100

    Port multiple_file_upload JS tests from QUnit to Jasmine

--- spec/javascripts/admin/multiple_file_upload.spec.js
+++ spec/javascripts/admin/multiple_file_upload.spec.js
@@ -1,169 +1,133 @@
-module('Uploading multiple files', {
-  setup: function () {
-    this.fieldset = $(
+describe('jquery.enableMultipleFileUploads', function () {
+  var fieldset
+
+  beforeEach(function () {
+    fieldset = $(
       '<fieldset id="image_fields" class="images multiple_file_uploads">' +
-      '<div class="file_upload well">' +
-      '<div class="form-group">' +
-      '<label for="edition_images_attributes_0_image_data_attributes_file">File</label>' +
-      '<input id="edition_images_attributes_0_image_data_attributes_file" name="edition[images_attributes][0][image_data_attributes][file]" type="file" class="js-upload-image-input" />' +
-      '<input id="edition_images_attributes_0_image_data_attributes_file_cache" name="edition[images_attributes][0][image_data_attributes][file_cache]" type="hidden" />' +
-      '</div>' +
-      '<div class="form-group">' +
-      '<label for="edition_images_attributes_0_alt_text">Alt text</label>' +
-      '<input id="edition_images_attributes_0_alt_text" name="edition[images_attributes][0][alt_text]" type="text" />' +
-      '</div>' +
-      '<div class="form-group">' +
-      '<label for="edition_images_attributes_0_caption">Caption</label>' +
-      '<textarea id="edition_images_attributes_0_caption" name="edition[images_attributes][0][caption]"></textarea>' +
-      '</div>' +
-      '</div>' +
-      '</fieldset>')
+        '<div class="file_upload well">' +
+          '<div class="form-group">' +
+            '<label for="edition_images_attributes_0_image_data_attributes_file">File</label>' +
+            '<input id="edition_images_attributes_0_image_data_attributes_file" name="edition[images_attributes][0][image_data_attributes][file]" type="file" class="js-upload-image-input" />' +
+            '<input id="edition_images_attributes_0_image_data_attributes_file_cache" name="edition[images_attributes][0][image_data_attributes][file_cache]" type="hidden" />' +
+          '</div>' +
+          '<div class="form-group">' +
+            '<label for="edition_images_attributes_0_alt_text">Alt text</label>' +
+            '<input id="edition_images_attributes_0_alt_text" name="edition[images_attributes][0][alt_text]" type="text" />' +
+          '</div>' +
+          '<div class="form-group">' +
+            '<label for="edition_images_attributes_0_caption">Caption</label>' +
+            '<textarea id="edition_images_attributes_0_caption" name="edition[images_attributes][0][caption]"></textarea>' +
+          '</div>' +
+        '</div>' +
+      '</fieldset>'
+    )
 
-    $('#qunit-fixture').append(this.fieldset)
-    this.first_file_input = $('#edition_images_attributes_0_image_data_attributes_file')
-    this.fieldset.enableMultipleFileUploads()
-  }
-})
+    $(document.body).append(fieldset)
+  })
 
-var fireClickEventOnLastFileInputOf = function (fieldset) {
-  fieldset.find('input[type=file]:last').click()
-}
+  afterEach(function () {
+    fieldset.remove()
+  })
 
-test('should add a new file input when a file is selected', function () {
-  this.first_file_input.click()
-  equal(this.fieldset.children('.file_upload').length, 2)
-})
+  it('should add a new file input when a file is selected', function () {
+    fieldset.enableMultipleFileUploads()
+    fieldset.find('input[type=file]').click()
+    expect(fieldset.children('.file_upload').length).toEqual(2)
+  })
 
-test('should not add a new file input when a selected file is clicked', function () {
-  this.first_file_input.click()
-  this.first_file_input.click()
-  equal(this.fieldset.children('.file_upload').length, 2)
-})
+  it("shouldn't add extra file inputs if the same one is selected twice", function () {
+    fieldset.enableMultipleFileUploads()
+    var item = fieldset.find('input[type=file]')
+    item.click().click()
+    expect(fieldset.children('.file_upload').length).toEqual(2)
+  })
 
-test('should continue adding new inputs as new files are selected', function () {
-  for (var i = 0; i < 10; i++) {
-    fireClickEventOnLastFileInputOf(this.fieldset)
-  }
-  equal(this.fieldset.children('.file_upload').length, 11)
+  it('should continue adding new inputs as new files are selected', function () {
+    fieldset.enableMultipleFileUploads()
+    for (var i = 0; i < 10; i++) {
+      fieldset.find('input[type=file]:last').click()
+    }
 
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  equal(this.fieldset.children('.file_upload').length, 12)
-})
+    expect(fieldset.children('.file_upload').length).toEqual(11)
+  })
 
-test('should increment the ID and name of the alt text input for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  ok($('#edition_images_attributes_1_alt_text')[0], 'input with id not found')
-  ok($("input[name='edition[images_attributes][1][alt_text]']")[0], 'input with name not found')
+  it('should increment the names of new inputs added', function () {
+    fieldset.enableMultipleFileUploads()
 
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  ok($('#edition_images_attributes_2_alt_text')[0], 'input with id not found')
-  ok($("input[name='edition[images_attributes][2][alt_text]']")[0], 'input with name not found')
-})
+    var elements = [
+      'input[type=file][name="edition[images_attributes][1][image_data_attributes][file]"]',
+      'input[type=hidden][name="edition[images_attributes][1][image_data_attributes][file_cache]"]',
+      'input[type=text][name="edition[images_attributes][1][alt_text]"]',
+      'textarea[name="edition[images_attributes][1][caption]"]'
+    ]
+    expect($(elements.join(',')).length).toEqual(0)
 
-test('should increment the ID and name of the caption textarea for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  ok($('#edition_images_attributes_1_caption')[0], 'textarea with id not found')
-  ok($("textarea[name='edition[images_attributes][1][caption]']")[0], 'textarea with name not found')
+    fieldset.find('input[type=file]').click()
 
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  ok($('#edition_images_attributes_2_caption')[0], 'textarea with id not found')
-  ok($("textarea[name='edition[images_attributes][2][caption]']")[0], 'textarea with name not found')
-})
+    expect($(elements.join(',')).length).toEqual(elements.length)
+  })
 
-test('should increment the referenced ID of the file label for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  var latestInput = this.fieldset.find("label:contains('File'):last")
-  equal(latestInput.attr('for'), 'edition_images_attributes_1_image_data_attributes_file')
+  it('should reset values set on fields', function () {
+    fieldset.enableMultipleFileUploads()
 
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  latestInput = this.fieldset.find("label:contains('File'):last")
-  equal(latestInput.attr('for'), 'edition_images_attributes_2_image_data_attributes_file')
-})
+    fieldset.find('[name="edition[images_attributes][0][image_data_attributes][file_cache]"]').val('not-blank')
+    fieldset.find('[name="edition[images_attributes][0][alt_text]"]').val('not-blank')
+    fieldset.find('[name="edition[images_attributes][0][caption]"]').val('not-blank')
 
-test('should increment the ID and name of the file input for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  var latestInput = this.fieldset.find('input[type=file]:last')[0]
-  equal(latestInput.id, 'edition_images_attributes_1_image_data_attributes_file')
-  equal(latestInput.name, 'edition[images_attributes][1][image_data_attributes][file]')
+    fieldset.find('input[type=file]').click()
 
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  latestInput = this.fieldset.find('input[type=file]:last')[0]
-  equal(latestInput.id, 'edition_images_attributes_2_image_data_attributes_file')
-  equal(latestInput.name, 'edition[images_attributes][2][image_data_attributes][file]')
-})
+    expect(fieldset.find('[name="edition[images_attributes][1][image_data_attributes][file_cache]"]').val()).toEqual('')
+    expect(fieldset.find('[name="edition[images_attributes][1][alt_text]"]').val()).toEqual('')
+    expect(fieldset.find('[name="edition[images_attributes][1][caption]"]').val()).toEqual('')
+  })
 
-test('should increment the ID and name of the hidden cache input for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  var latestInput = this.fieldset.find('input[type=hidden]:last')[0]
-  equal(latestInput.id, 'edition_images_attributes_1_image_data_attributes_file_cache')
-  equal(latestInput.name, 'edition[images_attributes][1][image_data_attributes][file_cache]')
+  it('should reset an already uploaded warning', function () {
+    fieldset.enableMultipleFileUploads()
 
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  latestInput = this.fieldset.find('input[type=hidden]:last')[0]
-  equal(latestInput.id, 'edition_images_attributes_2_image_data_attributes_file_cache')
-  equal(latestInput.name, 'edition[images_attributes][2][image_data_attributes][file_cache]')
-})
+    var alreadyUploaded = $('<span class="already_uploaded">some-file.pdf already uploaded</span>')
+    $('input[type=file]').after(alreadyUploaded)
 
-test('should make the value of the text input blank for each set of new inputs added', function () {
-  this.fieldset.find('input[type=text]:last').val('not-blank')
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  var latestInput = this.fieldset.find('input[type=text]:last')
-  equal(latestInput.val(), '')
-})
+    fieldset.find('input[type=file]').click()
 
-test('should make the value of the caption textarea blank for each set of new inputs added', function () {
-  this.fieldset.find('#edition_images_attributes_0_caption').append('not-blank')
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  equal($('#edition_images_attributes_1_caption').val(), '')
-})
+    expect(fieldset.find('.already_uploaded').length).toEqual(2)
+    expect(fieldset.find('.already_uploaded:last').text()).toEqual('')
+  })
 
-test('should set the value of the hidden cache input to blank for each new input added', function () {
-  $('input[type=hidden]:last').val('not-blank')
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  var latestInput = this.fieldset.find('input[type=hidden]:last')
-  equal(latestInput.val(), '')
-})
+  describe('uploading files after a file field validation error', function () {
+    beforeEach(function () {
+      fieldset.remove()
+      fieldset = $(
+        '<fieldset id="image_fields" class="images multiple_file_uploads">' +
+          '<div class="file_upload well">' +
+            '<div class="field_with_errors">' +
+              '<label for="edition_images_attributes_0_image_data_attributes_file">File</label>' +
+            '</div>' +
+            '<div class="field_with_errors">' +
+              '<input id="edition_images_attributes_0_image_data_attributes_file" name="edition[images_attributes][0][image_data_attributes][file]" type="file" class="js-upload-image-input" />' +
+            '</div>' +
+            '<input id="edition_images_attributes_0_image_data_attributes_file_cache" name="edition[images_attributes][0][image_data_attributes][file_cache]" type="hidden" />' +
+        '</div>' +
+      '</fieldset>'
+      )
 
-test('should set the text of the already_uploaded element to blank for each new input added', function () {
-  var alreadyUploaded = $('<span class="already_uploaded">some-file.pdf already uploaded</span>')
-  $('input[type=file]:last').after(alreadyUploaded)
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  equal(this.fieldset.find('.already_uploaded').length, 2)
-  equal(this.fieldset.find('.already_uploaded:last').text(), '')
-})
+      $(document.body).append(fieldset)
+    })
 
-module('Uploading multiple files after file field validation error', {
-  setup: function () {
-    this.fieldset = $(
-      '<fieldset id="image_fields" class="images multiple_file_uploads">' +
-      '<div class="file_upload well">' +
-      '<div class="field_with_errors">' +
-      '<label for="edition_images_attributes_0_image_data_attributes_file">File</label>' +
-      '</div>' +
-      '<div class="field_with_errors">' +
-      '<input id="edition_images_attributes_0_image_data_attributes_file" name="edition[images_attributes][0][image_data_attributes][file]" type="file" class="js-upload-image-input" />' +
-      '</div>' +
-      '<input id="edition_images_attributes_0_image_data_attributes_file_cache" name="edition[images_attributes][0][image_data_attributes][file_cache]" type="hidden" />' +
-      '</div>' +
-      '</fieldset>')
-    $('#qunit-fixture').append(this.fieldset)
-    this.fieldset.enableMultipleFileUploads()
-  }
-})
+    it('should still allow creating additional file uploads', function () {
+      fieldset.enableMultipleFileUploads()
+      fieldset.find('input[type=file]').click()
+      expect(fieldset.children('.file_upload').length).toEqual(2)
+    })
 
-test('should add a new file input when a file is selected', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  equal(this.fieldset.children('.file_upload').length, 2)
-})
+    it("doesn't copy error wrappers", function () {
+      fieldset.enableMultipleFileUploads()
+      fieldset.find('input[type=file]').click()
 
-test('should copy the file label without error wrapper for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  equal(this.fieldset.find("label:contains('File'):last").parent().hasClass('field_with_errors'), false)
-  equal(this.fieldset.find(".field_with_errors label:contains('File')").length, 1)
-})
+      var labelParent = $('label[for=edition_images_attributes_1_image_data_attributes_file]').parent()
+      var inputParent = $('input[name="edition[images_attributes][1][image_data_attributes][file]"]').parent()
 
-test('should copy the file input without error wrapper for each set of new inputs added', function () {
-  fireClickEventOnLastFileInputOf(this.fieldset)
-  equal(this.fieldset.find('input[type=file]').length, 2)
-  equal(this.fieldset.find('.field_with_errors input[type=file]').length, 1)
+      expect(labelParent.hasClass('field_with_errors')).toBeFalse()
+      expect(inputParent.hasClass('field_with_errors')).toBeFalse()
+    })
+  })
 })
