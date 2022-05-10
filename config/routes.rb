07b605f6dd200c2d13197faecc38a90c5356304c
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-05-10 14:53:45 +0100

    Remove hardcoded MoD Police & Guarding Agency redirect
    This external redirect was hardcoded, but the route
    `/government/organisations/ministry-of-defence-police-and-guarding-agency`
    is rendered by `collections`, so Whitehall will never serve this
    redirect.
    
    It is therefore safe to remove.

--- config/routes.rb
+++ config/routes.rb
@@ -63,9 +63,6 @@ Whitehall::Application.routes.draw do
   get "/auth/failure", to: "admin/base#auth_failure", as: "auth_failure_fixed"
 
   scope Whitehall.router_prefix, shallow_path: Whitehall.router_prefix do
-    external_redirect "/organisations/ministry-of-defence-police-and-guarding-agency",
-                      "http://webarchive.nationalarchives.gov.uk/20121212174735/http://www.mod.uk/DefenceInternet/AboutDefence/WhatWeDo/SecurityandIntelligence/MDPGA/"
-
     root to: redirect("/", prefix: ""), via: :get, as: :main_root
     get "/how-government-works" => "home#how_government_works", as: "how_government_works"
     scope "/get-involved" do
