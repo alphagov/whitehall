0fec48a81667a620e504a6ab93ad97baed6d5800
Kevin Dew <kevindew@me.com>
2022-05-13 13:41:51 +0100

    Upgrade to govuk_app_config 4.6
    govuk_app_config 4.6 includes automatic ignoring of the Sidekiq internal
    exception: Sidekiq::JobRetry::Skip so this can be removed from our code.

--- config/initializers/govuk_errors.rb
+++ config/initializers/govuk_errors.rb
@@ -2,7 +2,6 @@ GovukError.configure do |config|
   config.excluded_exceptions += [
     "AssetManagerAttachmentSetUploadedToWorker::AttachmentDataNotFoundTransient",
     "Redis::CannotConnectError",
-    "Sidekiq::JobRetry::Skip",
   ]
 
   config.data_sync_excluded_exceptions += [
