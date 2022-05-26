1b9c66bb833a4082332028670b7fccd97c7af242
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-05-26 10:18:52 +0100

    Split existing Classification descriptions into two parts
    In a previous commit, we added a migration to create an additional
    'summary' field on Classifcations (only used by Topical Events).
    
    This splits the existing descriptions into the two parts.
    
    The code has been copied from the `TopicalEventsController`'s
    `modify_description` method, to ensure we retain the existing behaviour.

---
+++ db/migrate/20220526090755_split_classification_description.rb
@@ -0,0 +1,15 @@
+class SplitClassificationDescription < ActiveRecord::Migration[7.0]
+  def up
+    Classification.find_each do |classification|
+      split_description = classification.description.split(/[\r\n]+/)
+
+      summary = split_description[0] if split_description&.any?
+      description = split_description[1..split_description.length].join("\r\n\r\n") if split_description.length > 1
+
+      classification.update_columns(
+        summary: summary,
+        description: description,
+      )
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_090436) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_090755) do
   create_table "about_pages", id: :integer, charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "topical_event_id"
     t.string "name"
