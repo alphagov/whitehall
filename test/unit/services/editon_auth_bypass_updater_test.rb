76b364265d05cf95f0cdc81486d321f70162662f
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-06-19 21:43:18 +0100

    Update auth_bypass_id for relevant attachments
    When the auth_bypass_id is updated for an edition, relevant attachments
    also need to be updated.
    
    This commit enforces logic applied in the `asset_manager.rake` tasks.
    Resultantly, file and images are republished. As well as response forms,
    outcomes and public feedback attachments for Consultations.

--- test/unit/services/editon_auth_bypass_updater_test.rb
+++ test/unit/services/editon_auth_bypass_updater_test.rb
@@ -1,24 +1,152 @@
 require "test_helper"
 
 class EditionAuthBypassUpdaterTest < ActiveSupport::TestCase
-  test "#call" do
-    edition = create(:draft_edition)
-    user = create(:user)
-    updater = MiniTest::Mock.new
-    service = EditionAuthBypassUpdater.new(
-      edition: edition,
-      current_user: user,
-      updater: updater,
-    )
+  extend Minitest::Spec::DSL
 
-    uid = SecureRandom.uuid
-    SecureRandom.stubs(uuid: uid)
+  describe "#call" do
+    let(:user) { create(:user) }
+    let(:updater) { MiniTest::Mock.new }
+    let(:uid) { SecureRandom.uuid }
 
-    updater.expect :perform!, true
+    before do
+      updater.expect :perform!, true
+      SecureRandom.stubs(uuid: uid)
+    end
 
-    service.call
+    test "updates the editions auth_bypass_id, saves it with the current user and calls 'perform!' on the updater" do
+      edition = create(:draft_edition)
 
-    assert_equal edition.auth_bypass_id, uid
-    assert_equal edition.edition_authors.last.user, user
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      service.call
+
+      assert_equal edition.auth_bypass_id, uid
+      assert_equal edition.edition_authors.last.user, user
+    end
+
+    test "updates attachments with auth_bypass_ids" do
+      edition = create(:draft_edition)
+      file_attachment = create(:file_attachment, attachable: edition)
+      expected_attributes = { auth_bypass_ids: [uid] }
+
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      AssetManagerUpdateWhitehallAssetWorker.expects(:perform_async_in_queue).with(
+        "asset_manager_updater",
+        file_attachment.attachment_data.to_global_id,
+        expected_attributes,
+      )
+
+      service.call
+    end
+
+    test "updates an image with auth_bypass_id" do
+      edition = create(:draft_case_study)
+      image = create(:image, edition: edition)
+      expected_attributes = { auth_bypass_ids: [uid] }
+
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      AssetManagerUpdateWhitehallAssetWorker.expects(:perform_async_in_queue).with(
+        "asset_manager_updater",
+        image.image_data.to_global_id,
+        expected_attributes,
+      )
+
+      service.call
+    end
+
+    test "does not attempt to update html attachments via the update whitehall asset worker" do
+      edition = create(:draft_edition)
+      create(:html_attachment, attachable: edition)
+
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      AssetManagerUpdateWhitehallAssetWorker.expects(:perform_async_in_queue).never
+
+      service.call
+    end
+
+    test "updates a consultation response form asset with auth_bypass_id" do
+      edition = create(:consultation)
+      participation = create(:consultation_participation, consultation: edition)
+      consultation_response_form = create(:consultation_response_form, consultation_participation: participation)
+
+      expected_attributes = { auth_bypass_ids: [uid] }
+
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      AssetManagerUpdateWhitehallAssetWorker.expects(:perform_async_in_queue).with(
+        "asset_manager_updater",
+        consultation_response_form.consultation_response_form_data.to_global_id,
+        expected_attributes,
+      )
+
+      service.call
+    end
+
+    test "updates a consultation outcome's attachments with auth_bypass_id" do
+      edition = create(:draft_consultation)
+      outcome = create(:consultation_outcome, consultation: edition)
+      file_attachment = create(:file_attachment, attachable: outcome)
+
+      expected_attributes = { auth_bypass_ids: [uid] }
+
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      AssetManagerUpdateWhitehallAssetWorker.expects(:perform_async_in_queue).with(
+        "asset_manager_updater",
+        file_attachment.attachment_data.to_global_id,
+        expected_attributes,
+      )
+
+      service.call
+    end
+
+    test "updates a consultation's public feedback attachments with auth_bypass_id" do
+      edition = create(:draft_consultation)
+      feedback = create(:consultation_public_feedback, consultation: edition)
+      file_attachment = create(:file_attachment, attachable: feedback)
+
+      expected_attributes = { auth_bypass_ids: [uid] }
+
+      service = EditionAuthBypassUpdater.new(
+        edition: edition,
+        current_user: user,
+        updater: updater,
+      )
+
+      AssetManagerUpdateWhitehallAssetWorker.expects(:perform_async_in_queue).with(
+        "asset_manager_updater",
+        file_attachment.attachment_data.to_global_id,
+        expected_attributes,
+      )
+
+      service.call
+    end
   end
 end
