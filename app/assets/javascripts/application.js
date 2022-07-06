5a0af4efe8ebab2d70722323cb86e4f93bd2ebf7
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-07-06 15:55:51 +0100

    Add missing component stylesheets and JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,8 @@
 //
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/details
+//= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/govspeak
 //
 //= require jquery/dist/jquery
