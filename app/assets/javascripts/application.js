1cec8149c924ce465323f463243dc9f8dae14bd5
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-07-07 14:05:34 +0100

    Remove button component JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@
 // the compiled file.
 //
 //= require govuk_publishing_components/lib
-//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/govspeak
