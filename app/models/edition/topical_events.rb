18d5013fdf189daacbf7461c183d67556f774470
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-07-18 10:39:23 +0100

    Rename Edition::Classification model
    The Classification model was solely inherited by TopicalEvent. Therefore
    we renamed Classification to TopicalEvent.
    
    This makes the same change to the Edition::Classification, which were
    only relevant to Topical Events.

--- app/models/edition/topical_events.rb
+++ app/models/edition/topical_events.rb
@@ -1,18 +1,22 @@
 module Edition::TopicalEvents
   extend ActiveSupport::Concern
-  include Edition::Classifications
 
   class Trait < Edition::Traits::Trait
     def process_associations_before_save(edition)
-      edition.classification_featurings = @edition.topical_event_featurings.map do |cf|
-        ClassificationFeaturing.new(cf.attributes.except("id"))
+      edition.topical_event_featurings = @edition.topical_event_featurings.map do |cf|
+        TopicalEventFeaturing.new(cf.attributes.except("id"))
+      end
+
+      edition.topical_event_memberships = @edition.topical_event_memberships.map do |dt|
+        TopicalEventMembership.new(dt.attributes.except("id"))
       end
     end
   end
 
   included do
-    has_many :topical_events, through: :classification_memberships, source: :topical_event
-    has_many :classification_featurings, dependent: :destroy, foreign_key: :edition_id
+    has_many :topical_event_featurings, dependent: :destroy, foreign_key: :edition_id
+    has_many :topical_event_memberships, dependent: :destroy, inverse_of: :edition, foreign_key: :edition_id
+    has_many :topical_events, through: :topical_event_memberships, source: :topical_event
 
     add_trait Trait
   end
