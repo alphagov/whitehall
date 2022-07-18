afede421b2601c9972926774472e075eb4d76f45
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-07-18 13:53:40 +0100

    Rename TopicsHelper
    Topics appear to have been intended to extend from Classification,
    however they are not used.
    
    This helper contains a method that is used by Topical Events.
    
    Therefore renaming the helper and method so it is more correctly named.

---
+++ app/helpers/topical_events_helper.rb
@@ -0,0 +1,7 @@
+module TopicalEventsHelper
+  def topical_event_contents_breakdown(topical_event)
+    capture do
+      concat tag.span(pluralize(topical_event.published_detailed_guides.count, "published detailed guide"))
+    end
+  end
+end
