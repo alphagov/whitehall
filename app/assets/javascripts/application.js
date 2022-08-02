288dc087d9aba026c3d1e8cdc555f97b4ed73bf4
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2022-08-02 16:36:25 +0100

    add bootstrap navigation bar to the govuk design system layout
    This also includes the govuk-admin-layout js and admin js as it includes everything needed for the navigation bar to work.
    
    If you need full layout, use the following css:
    
    ```
    .govuk-width-container--full {
      max-width: 100%;
      margin: 0 govuk-spacing(4);
    }
    
    ```

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/all_components
+//= require govuk-admin-template
+//= require admin
+
+window.GOVUK.navBarHelper.init()
