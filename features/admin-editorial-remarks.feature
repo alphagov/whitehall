f7571eb54dabed45c4876bcfb531433bdf4a09e7
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-08-02 10:07:10 +0100

    Update the cucumber spec for editorial remarks
    This now has a scenario which checks the notes tab is not
    visible on the summary and edit page for editions, and the edit
    page for translations.
    
    Once the flag is pulled out this can be simplified and simply follow
    the happy path with no checks for presence of the tabs.

--- features/admin-editorial-remarks.feature
+++ features/admin-editorial-remarks.feature
@@ -1,6 +1,19 @@
 Feature: Creating and viewing editorial remarks
 
-Scenario: Adding an editorial remark
+Background:
   Given I am a writer
+
+Scenario: Adding an editorial remark
   When I add an editorial remark "Try using a spellchecker" to the document "Badddly Rittin"
   Then my editorial remark should be visible with the document
+
+Scenario: Adding an editorial remark with the View move tabs to endpoints permission
+  Given I have the "View move tabs to endpoints" permission
+  When I visit the edition show page
+  Then the "Notes" tab is not visible
+  When I visit the edit edition page
+  Then the "Notes" tab is not visible
+  When I add a french translation
+  Then the "Notes" tab is not visible
+  When I add an editorial remark "Try using a spellchecker" to the document
+  Then my editorial remark should be visible on the notes index page
--- features/step_definitions/editorial_remarks.rb
+++ features/step_definitions/editorial_remarks.rb
@@ -1,3 +1,42 @@
+When(/^I visit the edition show page$/) do
+  begin_drafting_document type: "case_study", title: "New edition", previously_published: false
+  click_button "Save"
+  @edition = Edition.find_by!(title: "New edition")
+  visit admin_edition_path(@edition)
+end
+
+Then(/^the "([^"]*)" tab is not visible$/) do |tab_name|
+  expect(all(".nav-tabs").map(&:text)).not_to have_content tab_name
+end
+
+When(/^I visit the edit edition page$/) do
+  visit edit_admin_edition_path(@edition)
+end
+
+When(/^I add a french translation$/) do
+  visit admin_edition_path(@edition)
+  click_link "open-add-translation-modal"
+  select "Français", from: "Locale"
+  click_button "Add translation"
+end
+
+When(/^I add an editorial remark "([^"]*)" to the document$/) do |remark_text|
+  @remark_text = remark_text
+
+  visit admin_edition_path(@edition)
+  click_link "Notes"
+  ensure_path admin_edition_editorial_remarks_path(@edition)
+  click_link "Add note"
+  ensure_path new_admin_edition_editorial_remark_path(@edition)
+  fill_in "Remark", with: @remark_text
+  click_button "Add note"
+end
+
+Then(/^my editorial remark should be visible on the notes index page$/) do
+  ensure_path admin_edition_editorial_remarks_path(@edition)
+  expect(page).to have_content(@remark_text)
+end
+
 When(/^I add an editorial remark "([^"]*)" to the document "([^"]*)"$/) do |remark_text, document_title|
   @edition = create(:draft_publication, title: document_title)
   @remark_text = remark_text
