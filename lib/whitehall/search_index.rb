d13ec68ece13520b3376f30681217c284acebea2
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2022-08-04 13:44:19 +0100

    Put FakeRummageableIndex in own file
    Changes to Ruby or Rails, means that classes must be in a file with a
    name that matches the class name, otherwise a TypeError is thrown:
    
    ```
    TypeError (superclass mismatch for class FakeRummageableIndex)
    ```

--- lib/whitehall/search_index.rb
+++ lib/whitehall/search_index.rb
@@ -49,40 +49,4 @@ module Whitehall
       Plek.find("search")
     end
   end
-
-  unless defined?(FakeRummageableIndex)
-    class FakeRummageableIndex < Rummageable::Index
-      class << self
-        attr_accessor :store
-      end
-      @store = nil
-
-      def initialize(url, name, options = {})
-        super
-        @index_name = name
-      end
-
-      def add(entry)
-        add_batch([entry])
-      end
-
-      def add_batch(entries)
-        store.add(entries, @index_name) if store.present?
-      end
-
-      def delete(link)
-        store.delete(link, @index_name) if store.present?
-      end
-
-      def make_request(_method, *_args)
-        raise "Use the in memory index (rather than rummager) in tests"
-      end
-
-    private
-
-      def store
-        self.class.store
-      end
-    end
-  end
 end
