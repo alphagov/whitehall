f538dcbbc7f83e9d953505244901cb12ea0cbfe7
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2022-08-11 15:45:55 +0100

    Display list of activities

--- app/controllers/admin/licences_controller.rb
+++ app/controllers/admin/licences_controller.rb
@@ -5,5 +5,8 @@ class Admin::LicencesController < Admin::BaseController
 
   def edit
     @licence = Licence.find(params[:id])
+    @activity_options = Activity.order(:title).map do |activity|
+      [activity.title, activity.id]
+    end
   end
 end
