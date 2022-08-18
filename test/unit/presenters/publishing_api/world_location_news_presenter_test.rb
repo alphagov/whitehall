6ba1c0d5cb744031d05457be7f40c57d2df0faef
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-08-18 13:20:35 +0100

    Update world location news document type
    This switches from using a placeholder document type to the newly created world_location_news doc type, as part of preparatory work ahead of migrating rendering for this content type away from whitehall.

--- test/unit/presenters/publishing_api/world_location_news_presenter_test.rb
+++ test/unit/presenters/publishing_api/world_location_news_presenter_test.rb
@@ -47,7 +47,7 @@ class PublishingApi::WorldLocationNewsPresenterTest < ActiveSupport::TestCase
         ordered_featured_documents: featured_documents(world_location, WorldLocation::FEATURED_DOCUMENTS_DISPLAY_LIMIT),
         world_location_news_type: "world_location",
       },
-      document_type: "placeholder_world_location_news_page",
+      document_type: "world_location_news",
       public_updated_at: world_location.updated_at,
       rendering_app: "whitehall-frontend",
       schema_name: "world_location_news",
