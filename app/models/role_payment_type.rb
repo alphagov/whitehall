2e95e1c2652c81e83ad76b8f4073bc6711b9d538
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-09-13 14:09:47 +0100

    Add new options for role payment types
    Adds the options:
    - Paid as a whip
    - Paid as a consultant
    
    Requested by a publisher so they can more accurately represent people in their organisation.

--- app/models/role_payment_type.rb
+++ app/models/role_payment_type.rb
@@ -5,4 +5,6 @@ class RolePaymentType
 
   Unpaied = create!(id: 1, name: "Unpaid")
   ParliamentarySecretary = create!(id: 2, name: "Paid as a Parliamentary Secretary")
+  Whip = create!(id: 3, name: "Paid as a whip")
+  Consultant = create!(id: 4, name: "Paid as a consultant")
 end
