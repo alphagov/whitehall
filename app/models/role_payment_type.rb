7a3827172655999e5abfe64e347b03b3a3cfa6d0
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-09-13 15:38:08 +0100

    Sort role payment type options alphabetically

--- app/models/role_payment_type.rb
+++ app/models/role_payment_type.rb
@@ -3,8 +3,8 @@ class RolePaymentType
 
   attr_accessor :id, :name
 
-  Unpaied = create!(id: 1, name: "Unpaid")
+  Consultant = create!(id: 4, name: "Paid as a consultant")
   ParliamentarySecretary = create!(id: 2, name: "Paid as a Parliamentary Secretary")
   Whip = create!(id: 3, name: "Paid as a whip")
-  Consultant = create!(id: 4, name: "Paid as a consultant")
+  Unpaied = create!(id: 1, name: "Unpaid")
 end
