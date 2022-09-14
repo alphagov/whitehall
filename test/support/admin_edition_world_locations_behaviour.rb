04cc3ffa467788bdf53b8d6f2ffbc8fe8281825f
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-09-14 15:37:33 +0100

    Add Duplicate Edition Controller Test Helpers
    These are just duplicates of the legacy files with the "Preview design
    system" permission added to the current user.
    
    As the edit view is amended in the GOV.UK Design System these tests
    will begin to fail as the UI changes.
    
    This should allow us to keep full test coverage and simply delete the
    legacy files once we've shipped the new/edit pages.

---
+++ test/support/admin_edition_world_locations_behaviour.rb
@@ -0,0 +1,95 @@
+module AdminEditionWorldLocationsBehaviour
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def should_allow_association_between_world_locations_and(document_type)
+      setup { give_current_user_preview_design_system_permission }
+      edition_class = class_for(document_type)
+
+      view_test "new displays document form with world locations field" do
+        get :new
+
+        assert_select "form#new_edition" do
+          assert_select "#edition_world_location_ids" do |elements|
+            assert_equal 1, elements.length
+            assert_data_attributes_for_world_locations(
+              element: elements.first,
+              track_label: new_edition_path(document_type),
+            )
+          end
+        end
+      end
+
+      test "creating should create a new document with world locations" do
+        world_location1 = create(:world_location)
+        world_location2 = create(:world_location)
+        attributes = controller_attributes_for(document_type)
+
+        post :create,
+             params: {
+               edition: attributes.merge(
+                 world_location_ids: [world_location1.id, world_location2.id],
+               ),
+             }
+
+        assert document = edition_class.last
+        assert_equal [world_location1, world_location2], document.world_locations
+      end
+
+      view_test "edit displays document form with world locations field" do
+        edition = create(document_type) # rubocop:disable Rails/SaveBang
+        get :edit, params: { id: edition }
+
+        assert_select "form#edit_edition" do
+          assert_select "#edition_world_location_ids" do |elements|
+            assert_equal 1, elements.length
+            assert_data_attributes_for_world_locations(
+              element: elements.first,
+              track_label: edit_edition_path(document_type),
+            )
+          end
+        end
+      end
+
+      test "updating should save modified document attributes with world locations" do
+        world_location1 = create(:world_location)
+        world_location2 = create(:world_location)
+        document = create(document_type, world_locations: [world_location2])
+
+        put :update,
+            params: { id: document,
+                      edition: {
+                        world_location_ids: [world_location1.id],
+                      } }
+
+        document = document.reload
+        assert_equal [world_location1], document.world_locations
+      end
+
+      view_test "updating a stale document should render edit page with conflicting document and its world locations" do
+        document = create(document_type) # rubocop:disable Rails/SaveBang
+        lock_version = document.lock_version
+        document.touch
+
+        put :update, params: { id: document, edition: { lock_version: lock_version } }
+
+        assert_select ".document.conflict" do
+          assert_select "h1", "World locations"
+        end
+      end
+    end
+  end
+
+private
+
+  def give_current_user_preview_design_system_permission
+    @current_user.permissions << "Preview design system"
+  end
+
+  def assert_data_attributes_for_world_locations(element:, track_label:)
+    assert_equal "World locations…", element["data-placeholder"]
+    assert_equal "track-select-click", element["data-module"]
+    assert_equal "worldLocationSelection", element["data-track-category"]
+    assert_equal track_label, element["data-track-label"]
+  end
+end
