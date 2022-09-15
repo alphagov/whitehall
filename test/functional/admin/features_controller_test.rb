6cafcd9134e328cf42d820a172c6ffef0fb1d9a3
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-09-15 10:27:45 +0100

    Add test for republishing WorldLocationNews on feature change
    When a featured document is added or changed, we should expect to see
    the associated `WorldLocationNews` republished.
    
    This does not appear to be happening now so adding a test to confirm
    this (which currently fails).

--- test/functional/admin/features_controller_test.rb
+++ test/functional/admin/features_controller_test.rb
@@ -28,7 +28,7 @@ class Admin::FeaturesControllerTest < ActionController::TestCase
     assert_equal Time.zone.now, feature.ended_at
   end
 
-  test "post :unfeature republishes the organisation" do
+  test "post :unfeature republishes the organisation when the featurable is an organisation" do
     organisation = create(:organisation)
     feature_list = create(:feature_list, featurable: organisation, locale: :en)
     edition = create(:published_speech)
@@ -39,7 +39,19 @@ class Admin::FeaturesControllerTest < ActionController::TestCase
     post :unfeature, params: { feature_list_id: feature_list, id: feature }
   end
 
-  test "post :feature creates a feature and republishes the document & organisation" do
+  test "post :unfeature republishes the world location news when the featurable is a world location news" do
+    world_location_news = build(:world_location_news)
+    create(:world_location, world_location_news: world_location_news)
+    feature_list = create(:feature_list, featurable: world_location_news, locale: :en)
+    edition = create(:published_speech)
+    feature = create(:feature, document: edition.document, feature_list: feature_list)
+
+    Whitehall::PublishingApi.expects(:republish_async).with(world_location_news).once
+
+    post :unfeature, params: { feature_list_id: feature_list, id: feature }
+  end
+
+  test "post :feature creates a feature and republishes the document & organisation when the featurable is an organisation" do
     organisation = create(:organisation)
     feature_list = create(:feature_list, featurable: organisation, locale: :en)
     edition = create(:published_speech)
@@ -52,6 +64,26 @@ class Admin::FeaturesControllerTest < ActionController::TestCase
 
     PublishingApiDocumentRepublishingWorker.expects(:perform_async).with(edition.document_id)
     Whitehall::PublishingApi.expects(:republish_async).with(organisation).once
+
+    post :create, params: { feature_list_id: feature_list.id, feature: params }
+
+    assert_equal edition.document_id, Feature.last.document_id
+  end
+
+  test "post :feature creates a feature and republishes the document & world location news when the featurable is an world location news" do
+    world_location_news = build(:world_location_news)
+    create(:world_location, world_location_news: world_location_news)
+    feature_list = create(:feature_list, featurable: world_location_news, locale: :en)
+    edition = create(:published_speech)
+
+    params = {
+      document_id: edition.document_id,
+      image: upload_fixture("images/960x640_gif.gif"),
+      alt_text: "some text",
+    }
+
+    PublishingApiDocumentRepublishingWorker.expects(:perform_async).with(edition.document_id)
+    Whitehall::PublishingApi.expects(:republish_async).with(world_location_news).once
 
     post :create, params: { feature_list_id: feature_list.id, feature: params }
 
