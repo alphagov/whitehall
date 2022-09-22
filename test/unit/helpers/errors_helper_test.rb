4e3f8ba4da1248f874ed1b719208de284c02d9ce
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-09-22 16:58:53 +0100

    fix error helper

--- test/unit/helpers/errors_helper_test.rb
+++ test/unit/helpers/errors_helper_test.rb
@@ -4,31 +4,41 @@ class ErrorsHelperTest < ActionView::TestCase
   setup do
     @object_with_no_errors = ErrorTestObject.new("title", Time.zone.today)
     @object_with_errors = ErrorTestObject.new(nil, nil)
+    @object_with_unrelated_errors = ErrorTestObject.new("title", nil)
     @object_with_errors.validate
+    @object_with_unrelated_errors.validate
   end
 
-  test "#error_for_input returns nil when there are no error messages" do
+  test "#errors_for_input returns nil when there are no error messages" do
     assert_nil errors_for_input(@object_with_no_errors.errors, :title)
   end
 
-  test "#error_for_input returns errors for the attribute passed in" do
+  test "#errors_for_input returns errors for the attribute passed in" do
     assert_equal errors_for_input(@object_with_errors.errors, :title), "Title can't be blank"
   end
 
-  test "#error_for_input formats the error message when there are multiple errors on a field" do
+  test "#errors_for_input formats the error message when there are multiple errors on a field" do
     assert_equal errors_for_input(@object_with_errors.errors, :date), "Date can't be blank\nDate is invalid"
   end
 
-  test "#error_for returns nil when there are no error messages" do
+  test "#errors_for_input does not return an empty string when object has unrelated error" do
+    assert_nil errors_for_input(@object_with_unrelated_errors.errors, :title)
+  end
+
+  test "#errors_for returns nil when there are no error messages" do
     assert_nil errors_for(@object_with_no_errors.errors, :title)
   end
 
-  test "#error_for returns errors for the attribute passed in" do
+  test "#errors_for returns errors for the attribute passed in" do
     assert_equal errors_for(@object_with_errors.errors, :title), [{ text: "Title can't be blank" }]
   end
 
-  test "#error_for formats the error message when there are multiple errors on a field" do
+  test "#errors_for formats the error message when there are multiple errors on a field" do
     assert_equal errors_for(@object_with_errors.errors, :date), [{ text: "Date can't be blank" }, { text: "Date is invalid" }]
+  end
+
+  test "#errors_for does not return an empty string when object has unrelated error" do
+    assert_nil errors_for_input(@object_with_unrelated_errors.errors, :title)
   end
 
   class ErrorTestObject
