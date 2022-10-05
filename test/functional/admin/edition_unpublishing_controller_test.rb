3398272a61e3657928049f55011f964785551fdf
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2022-10-05 10:53:04 +0100

    Introduce unpublishing controller tests for the new design system
    Update the existing tests to refer to legacy templates and introduce new tests for the new templates

--- test/functional/admin/edition_unpublishing_controller_test.rb
+++ test/functional/admin/edition_unpublishing_controller_test.rb
@@ -10,7 +10,17 @@ class Admin::EditionUnpublishingControllerTest < ActionController::TestCase
     @edition = create(:withdrawn_edition)
   end
 
-  test "#edit loads the unpublishing and renders the unpublish edit template" do
+  test "#edit loads the unpublishing and renders the unpublish legacy edit template" do
+    unpublishing = @edition.unpublishing
+    get :edit, params: { edition_id: @edition.id }
+
+    assert_response :success
+    assert_template :edit_legacy
+    assert_equal unpublishing, assigns(:unpublishing)
+  end
+
+  test "#edit loads the unpublishing and renders the unpublish edit template when the user has the 'Preview design system' permission" do
+    @current_user.permissions << "Preview design system"
     unpublishing = @edition.unpublishing
     get :edit, params: { edition_id: @edition.id }
 
@@ -51,13 +61,23 @@ class Admin::EditionUnpublishingControllerTest < ActionController::TestCase
     assert_equal "this used to say unpublished", @unpublished_edition.unpublishing.reload.explanation
   end
 
-  test "#update shows form with error if the update was not possible" do
+  test "#update shows legacy form with error if the update was not possible" do
+    unpublishing = @edition.unpublishing
+    original_explanation = unpublishing.explanation
+    put :update, params: { edition_id: @edition, unpublishing: { explanation: nil } }
+
+    assert_template :edit_legacy
+    assert_equal "The public explanation could not be updated", flash[:alert]
+    assert_equal original_explanation, unpublishing.reload.explanation
+  end
+
+  test "#update shows form with error if the update was not possible when the user has the 'Preview design system' permission" do
+    @current_user.permissions << "Preview design system"
     unpublishing = @edition.unpublishing
     original_explanation = unpublishing.explanation
     put :update, params: { edition_id: @edition, unpublishing: { explanation: nil } }
 
     assert_template :edit
-    assert_equal "The public explanation could not be updated", flash[:alert]
     assert_equal original_explanation, unpublishing.reload.explanation
   end
 end
