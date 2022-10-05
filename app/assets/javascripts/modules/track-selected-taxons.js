7c5e4e1a80d8606e4daa4af60bb57114e468054e
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2022-10-05 17:36:31 +0100

    Reimplement change selection listener
    This sends an event for each checkbox selection change

--- app/assets/javascripts/modules/track-selected-taxons.js
+++ app/assets/javascripts/modules/track-selected-taxons.js
@@ -8,6 +8,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
   TrackSelectedTaxons.prototype.init = function () {
     this.form.addEventListener('submit', this.onSubmitListener.bind(this))
+    this.addTaxonSelectionEvent()
   }
 
   TrackSelectedTaxons.prototype.onSubmitListener = function (e) {
@@ -22,6 +23,25 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       }
 
       GOVUK.analytics.trackEvent('taxonSelection', taxonPath.join(' > '), { label: window.location.pathname })
+    }
+  }
+
+  TrackSelectedTaxons.prototype.addTaxonSelectionEvent = function () {
+    var taxonCheckboxes = this.form.querySelectorAll('.miller-columns__item .govuk-checkboxes__input')
+
+    for (var index = 0; index < taxonCheckboxes.length; index++) {
+      var checkbox = taxonCheckboxes[index]
+
+      checkbox.addEventListener('click', function (e) {
+        var target = e.currentTarget
+        var label = this.form.querySelector('label[for="' + target.id + '"]').innerText.trim()
+
+        GOVUK.analytics.trackEvent(
+          'pageElementInteraction',
+          target.checked ? 'checkboxClickedOn' : 'checkboxClickedOff',
+          { label: label }
+        )
+      })
     }
   }
 
