21677eb5018b566187515a594c29cf710aac0d0e
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-10-07 12:56:05 +0100

    Revert "Update UrlMaker tests"
    This reverts commit 9386f7762c51703e0b73bd7f8de35e9489381edd.

--- test/unit/whitehall/url_maker_test.rb
+++ test/unit/whitehall/url_maker_test.rb
@@ -4,18 +4,18 @@ module Whitehall
   class UrlMakerTest < ActiveSupport::TestCase
     test "default url options can be set at create time" do
       maker = Whitehall::UrlMaker.new(host: "yahoo.com", protocol: "ftp")
-      assert_equal "ftp://yahoo.com/government/how-government-works", maker.how_government_works_url
+      assert_equal "ftp://yahoo.com/government/get-involved/take-part/woo", maker.take_part_page_url("woo")
     end
 
     test "default url options set on an instance do not interfere with other instances" do
       with_defaults = Whitehall::UrlMaker.new(host: "yahoo.com", protocol: "ftp")
       without_defaults = Whitehall::UrlMaker.new(host: "meh.com", protocol: "gopher")
-      assert_not_equal with_defaults.how_government_works_url, without_defaults.how_government_works_url
+      assert_not_equal with_defaults.get_involved_url, without_defaults.get_involved_url
     end
 
     test "host can be set when using _url helpers" do
       maker = Whitehall::UrlMaker.new
-      assert_equal "http://gov.uk/government/how-government-works", maker.how_government_works_url(host: "gov.uk")
+      assert_equal "http://gov.uk/government/get-involved/take-part/woo", maker.take_part_page_url("woo", host: "gov.uk")
     end
 
     test "the default format can be overridden for a localised resource" do
