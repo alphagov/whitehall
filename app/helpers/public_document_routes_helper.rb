874f51cd0aa5cba5f55fd16c0e4abf721b85ad03
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-10-10 09:11:32 +0100

    Create URL options helper method for public routes helper
    We are currently using Rails automatic route helper methods for forming
    public URLs.
    
    This accepts an optional `options` parameter, which includes things like
    the format, locale and query string parameters.
    
    As we will be switching away from using automatic route helpers for
    pages no longer served by Whitehall, we need to replicate this behaviour
    ourselves.
    
    The `append_url_options` method will be used in later commits to do
    this.

--- app/helpers/public_document_routes_helper.rb
+++ app/helpers/public_document_routes_helper.rb
@@ -126,4 +126,23 @@ private
       self
     end
   end
+
+  def append_url_options(path, options = {})
+    if options[:format] && options[:locale]
+      path = "#{path}.#{options[:locale]}.#{options[:format]}"
+    elsif options[:locale]
+      path = "#{path}.#{options[:locale]}"
+    elsif options[:format]
+      path = "#{path}.#{options[:format]}"
+    end
+
+    if options[:cachebust]
+      query_params = {
+        cachebust: options[:cachebust],
+      }
+      path = "#{path}?#{query_params.to_query}"
+    end
+
+    path
+  end
 end
