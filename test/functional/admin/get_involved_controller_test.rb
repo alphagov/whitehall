4b44f0a51264a7dda750b20ec08abd2dcea8d778
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-10-10 09:08:00 +0100

    Add test for Get Involved admin page
    There is currently no test that loads the admin page for Get Involved.
    
    This meant that an error in loading the page (e.g. by breaking a route
    helper) would not be noticed by running the test suite.
    
    I'm not exactly sure what this page is meant to do, other than provide
    some links to other places, so just adding a basic test that the page
    loads without error (which would be enough to identify a broken route
    helper, for example).

---
+++ test/functional/admin/get_involved_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class Admin::GetInvolvedControllerTest < ActionController::TestCase
+  setup do
+    login_as :gds_editor
+  end
+
+  should_be_an_admin_controller
+
+  test "GET :index returns ok" do
+    get :index
+
+    assert_response :success
+    assert_template "index"
+  end
+end
