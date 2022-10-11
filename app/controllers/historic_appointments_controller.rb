a67a210bc390035f5b77b047aff70bb76dc7b96e
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-10-11 13:21:42 +0100

    Reintroduce historical route changes
    These changes were reverted in
    https://github.com/alphagov/whitehall/pull/6907 as there was an error in
    route generation on the 'Previous Prime Ministers' page.
    
    This issue was fixed in https://github.com/alphagov/whitehall/pull/6905
    and has been tested against the other route changed.
    
    This reverts commit 4342a6cef48461b18e84abf53a8780bea285e42d and
    c66dd67d94fc04b5354b4592f35ce8c464bde325.

--- app/controllers/historic_appointments_controller.rb
+++ app/controllers/historic_appointments_controller.rb
@@ -350,7 +350,7 @@ class HistoricAppointmentsController < PublicFacingController
   end
 
   def show
-    @person = PersonPresenter.new(Person.friendly.find(params[:person_id]), view_context)
+    @person = PersonPresenter.new(Person.friendly.find(params[:id]), view_context)
     @historical_account = @person.historical_accounts.for_role(@role).first
     raise(ActiveRecord::RecordNotFound, "Couldn't find HistoricalAccount for #{@person.inspect}  and #{@role.inspect}") unless @historical_account
   end
@@ -374,11 +374,7 @@ private
   end
 
   def load_role
-    @role = Role.friendly.find(role_id)
-  end
-
-  def role_id
-    Role::HISTORIC_ROLE_PARAM_MAPPINGS[params[:role]]
+    @role = Role.friendly.find("prime-minister")
   end
 
   def previous_appointments
