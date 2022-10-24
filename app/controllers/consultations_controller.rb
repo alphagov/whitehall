a5d1be8b0ee37cfa9e26cb18072e1c99d0632d84
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-10-24 11:24:33 +0100

    Update consultations redirect
    This redirect currently uses the publications route helper, which has
    now been removed.
    
    The previously returned route also issued a redirect to another path, so
    updating the first redirect to now point to the final destination of the
    original redirect.
    
    This allows us to remove usage of the route helper method.

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,9 +1,15 @@
 class ConsultationsController < DocumentsController
   def index
-    filter_params = Whitehall::DocumentFilter::CleanedParams.new(
-      params.except(:controller, :action, :format, :_, :host),
-    ).to_h
+    query_params = {
+      content_store_document_type: %w[
+        open_consultations
+        closed_consultations
+      ],
+    }
 
-    redirect_to publications_path(filter_params.merge(publication_filter_option: "consultations"))
+    query_params["level_one_taxon"] = params[:topics] if params[:topics]
+    query_params["organisations"] = params[:departments] if params[:departments]
+
+    redirect_to "/search/policy-papers-and-consultations?#{query_params.to_query}"
   end
 end
--- test/functional/consultations_controller_test.rb
+++ test/functional/consultations_controller_test.rb
@@ -6,7 +6,7 @@ class ConsultationsControllerTest < ActionController::TestCase
   test "index redirects to the publications index filtering consultations, retaining any other filter params" do
     get :index, params: { topics: %w[a-topic-slug], departments: %w[an-org-slug] }
     assert_redirected_to(
-      "http://test.host/government/publications?departments%5B%5D=an-org-slug&publication_filter_option=consultations&topics%5B%5D=a-topic-slug",
+      "http://test.host/search/policy-papers-and-consultations?content_store_document_type%5B%5D=open_consultations&content_store_document_type%5B%5D=closed_consultations&level_one_taxon%5B%5D=a-topic-slug&organisations%5B%5D=an-org-slug",
     )
   end
 end
