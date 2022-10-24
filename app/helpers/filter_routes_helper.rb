4659a32ce8822ae6ee65331e295c2c906edf266a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-10-24 10:37:10 +0100

    Update route helpers for feeds
    These feeds are no longer rendered by Whitehall, however we currently
    use a Whitehall route helper to generate the URL, which then redirects
    to a Finder Frontend rendered path.
    
    Updating these helpers allows us to link directly to the finder without
    needing the redirect or route helper.
    
    Note the query parameters are different between the Whitehall and Finder
    Frontend finders, so this has been updated here (it was previously done
    by the redirect).

--- app/helpers/filter_routes_helper.rb
+++ app/helpers/filter_routes_helper.rb
@@ -1,10 +1,12 @@
 module FilterRoutesHelper
   def announcements_filter_path(*objects)
-    announcements_path(path_arguments(objects))
+    query_string = path_arguments(objects).to_query
+    "/search/news-and-communications?#{query_string}"
   end
 
   def publications_filter_path(*objects)
-    publications_path(path_arguments(objects))
+    query_string = path_arguments(objects).to_query
+    "/search/all?#{query_string}"
   end
 
   def filter_atom_feed_url
@@ -23,7 +25,7 @@ protected
     objects.reduce({}) do |out, obj|
       case obj
       when Organisation
-        out[:departments] = [obj.slug]
+        out[:organisation] = obj.slug
       when TopicalEvent
         out[:topical_events] = [obj.slug]
       when WorldLocation
@@ -31,6 +33,7 @@ protected
       else
         out = out.merge(obj)
       end
+      out["order"] = "updated-newest"
       out
     end
   end
