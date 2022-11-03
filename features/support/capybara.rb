ccd11d52c313eba644fe7e197c2f5f87d10e417d
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-11-03 17:56:35 +0000

    Allow Capybara to click custom checkbox controls
    Allow Capybara to click a <label> even if its corresponding <input>
    isn't visible on screen.
    
    This needs to be enabled when using custom-styled checkboxes and radios,
    such as those in the GOV.UK Design System.

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -3,6 +3,11 @@
 # so we've increased the default timeout.
 Capybara.default_max_wait_time = 5
 
+# Allow Capybara to click a <label> even if its corresponding <input> isn't visible on screen.
+# This needs to be enabled when using custom-styled checkboxes and radios, such as those
+# in the GOV.UK Design System.
+Capybara.automatic_label_click = true
+
 module ScreenshotHelper
   def screenshot(name = "capybara")
     page.driver.render(Rails.root.join("tmp/#{name}.png"), full: true)
