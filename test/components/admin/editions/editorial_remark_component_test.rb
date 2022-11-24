09a9b0469caf333ce74837196888fb3690984bf7
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-11-24 12:08:35 +0000

    Add View Component for editorial remarks
    This adds a view component which handles rendering an editorial remark.
    This will be used in the history tab on the sidebar.

---
+++ test/components/admin/editions/editorial_remark_component_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Admin::Editions::EditorialRemarkComponentTest < ViewComponent::TestCase
+  test "it constructs output based on the editorial remark when an author is present" do
+    editorial_remark = build_stubbed(:editorial_remark, created_at: Time.zone.local(2020, 1, 1, 11, 11))
+    author = editorial_remark.author
+    render_inline(Admin::Editions::EditorialRemarkComponent.new(editorial_remark:))
+
+    assert_equal page.text, "\n  #{editorial_remark.body}\n  #{author.name}  1 January 2020 11:11am\n\n"
+    assert_equal page.find("li a").text, author.name
+    assert_equal page.find("li a")[:href], "/government/admin/authors/#{author.id}"
+  end
+
+  test "it constructs output based on the editorial remark when an author is absent" do
+    editorial_remark = build_stubbed(:editorial_remark, author: nil, created_at: Time.zone.local(2020, 1, 1, 11, 11))
+
+    render_inline(Admin::Editions::EditorialRemarkComponent.new(editorial_remark:))
+
+    assert_equal page.text, "\n  #{editorial_remark.body}\n  User (removed)  1 January 2020 11:11am\n\n"
+  end
+end
