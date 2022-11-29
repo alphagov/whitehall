14ad180debaecd632b04df05e62174ba1b90259a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-11-29 16:08:41 +0000

    Add `base_path` method for `WorldwideOrganisation`
    This is required to enable us to form the base path of Corporate
    Information Pages correctly.

--- app/models/worldwide_organisation.rb
+++ app/models/worldwide_organisation.rb
@@ -118,4 +118,8 @@ class WorldwideOrganisation < ApplicationRecord
   def office_staff_roles
     roles.occupied.where(type: OFFICE_ROLES.map(&:name))
   end
+
+  def base_path
+    "/world/organisations/#{slug}"
+  end
 end
