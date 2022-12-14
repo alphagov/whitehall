bba13cc1f85039f4c6116c5b03d396918598ec6e
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2022-12-14 17:27:22 +0000

    Move sidebar actions into a ViewComponent
    There was a logic in the siderbar actions template. By moving it into a ViewComponent that code can be better organised and tested.

---
+++ test/components/admin/editions/show/sidebar_actions_component_test.rb
@@ -0,0 +1,197 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Admin::Editions::Show::SidebarActionsComponentTest < ViewComponent::TestCase
+  test "actions for draft edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:draft_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 3
+    assert_selector "button", text: "Submit for 2nd eyes"
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+  end
+
+  test "actions for published edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:published_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 3
+    assert_selector "button", text: "Create new edition"
+    assert_selector "a", text: "View data about page"
+    assert_selector "a", text: "View on website (opens in new tab)"
+  end
+
+  test "actions for submitted edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:submitted_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 2
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+  end
+
+  test "actions for rejected edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:rejected_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 3
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+    assert_selector "button", text: "Submit for 2nd eyes"
+  end
+
+  test "actions for superseded edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:superseded_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_no_selector "app-view-edition-summary__sidebar-actions"
+  end
+
+  test "actions for scheduled edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:scheduled_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 1
+    assert_selector "a", text: "Unschedule"
+  end
+
+  test "actions for unpublished edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:unpublished_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 2
+    assert_selector "a", text: "Edit draft"
+    assert_selector "button", text: "Submit for 2nd eyes"
+  end
+
+  test "actions for withdrawn edition" do
+    current_user = build_stubbed(:user)
+    edition = create(:withdrawn_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 3
+    assert_selector "a", text: "Unwithdraw"
+    assert_selector "a", text: "View data about page"
+    assert_selector "a", text: "View on website (opens in new tab)"
+  end
+
+  test "actions for draft edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:draft_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 4
+    assert_selector "button", text: "Submit for 2nd eyes"
+    assert_selector "a", text: "Force publish"
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+  end
+
+  test "actions for draft edition with a scheduled date as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:draft_edition, scheduled_publication: 1.day.from_now)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 4
+    assert_selector "button", text: "Submit for 2nd eyes"
+    assert_selector "a", text: "Force schedule"
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+  end
+
+  test "actions for published edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:published_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 4
+    assert_selector "button", text: "Create new edition"
+    assert_selector "a", text: "Withdraw or unpublish"
+    assert_selector "a", text: "View data about page"
+    assert_selector "a", text: "View on website (opens in new tab)"
+  end
+
+  test "actions for submitted edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:submitted_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 4
+    assert_selector "button", text: "Publish"
+    assert_selector "button", text: "Reject"
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+  end
+
+  test "actions for submitted edition with a scheduled date as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:submitted_edition, scheduled_publication: 1.day.from_now)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 4
+    assert_selector "button", text: "Schedule"
+    assert_selector "button", text: "Reject"
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+  end
+
+  test "actions for rejected edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:rejected_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 3
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Discard draft"
+    assert_selector "button", text: "Submit for 2nd eyes"
+  end
+
+  test "actions for superseded edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:superseded_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_no_selector "app-view-edition-summary__sidebar-actions"
+  end
+
+  test "actions for scheduled edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:scheduled_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 1
+    assert_selector "a", text: "Unschedule"
+  end
+
+  test "actions for unpublished edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:unpublished_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 3
+    assert_selector "a", text: "Edit draft"
+    assert_selector "a", text: "Force publish"
+    assert_selector "button", text: "Submit for 2nd eyes"
+  end
+
+  test "actions for withdrawn edition as managing editor" do
+    current_user = build_stubbed(:managing_editor)
+    edition = create(:withdrawn_edition)
+    render_inline(Admin::Editions::Show::SidebarActionsComponent.new(edition:, current_user:))
+
+    assert_selector "li", count: 4
+    assert_selector "a", text: "Unwithdraw"
+    assert_selector "a", text: "Edit withdrawal explanation"
+    assert_selector "a", text: "View data about page"
+    assert_selector "a", text: "View on website (opens in new tab)"
+  end
+end
