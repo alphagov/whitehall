fb123dfafa1c4c7c6e3ff448139d14310dbab291
Kevin Dew <kevindew@me.com>
2022-12-28 21:10:10 +0000

    Remove ASSET_HOST env var
    This env var is no longer in active use since the introduction of Static
    proxy [1] and usage of it will soon have problems with the Content
    Security Policy [2].
    
    [1]: https://github.com/alphagov/govuk_app_config/blob/7f060692720df50a27f6845f052b04eae2246226/lib/govuk_app_config/govuk_proxy/static_proxy.rb
    [2]: https://github.com/alphagov/govuk_app_config/pull/274

--- config/application.rb
+++ config/application.rb
@@ -87,10 +87,6 @@ module Whitehall
     # Path within public/ where assets are compiled to
     config.assets.prefix = "/assets/whitehall"
 
-    # allow overriding the asset host with an enironment variable, useful for
-    # when router is proxying to this app but asset proxying isn't set up.
-    config.asset_host = ENV["ASSET_HOST"]
-
     unless Rails.application.secrets.jwt_auth_secret
       raise "JWT auth secret is not configured. See config/secrets.yml"
     end
