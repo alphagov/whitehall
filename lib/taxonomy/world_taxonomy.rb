6cdda8bf2f5aec3b18fa75bc065a9a612a87a8b7
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2023-01-03 13:49:07 +0000

    Adds design system layout for worldwide tagging page
    This change updates the worldwide tagging page to use the design system layout which is rendered when the user has the appropriate permission.

--- lib/taxonomy/world_taxonomy.rb
+++ lib/taxonomy/world_taxonomy.rb
@@ -9,11 +9,26 @@ module Taxonomy
       @all_world_taxons ||= sorted_world_taxons
     end
 
+    def all_world_taxons_transformed(selected_taxon_content_ids = [])
+      transform_taxon(all_world_taxons, selected_taxon_content_ids)
+    end
+
   private
 
     def sorted_world_taxons
       world_taxon_branches.sort_by do |world_taxon|
         [world_taxon.children.count.zero? ? 0 : 1, world_taxon.name]
+      end
+    end
+
+    def transform_taxon(taxons, selected_taxon_content_ids)
+      taxons.map do |taxon|
+        {
+          label: taxon.name,
+          value: taxon.content_id,
+          items: (transform_taxon(taxon.children, selected_taxon_content_ids) if taxon.children),
+          checked: selected_taxon_content_ids.include?(taxon.content_id),
+        }
       end
     end
 
