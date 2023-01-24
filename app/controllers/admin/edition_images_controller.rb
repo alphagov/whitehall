fb65a09aed4bb46bb844c9b682f0bb1289b648c9
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-01-24 09:59:22 +0000

    Add images resource
    This commit adds the images resource which
    will be used for creating the new images workflow.

---
+++ app/controllers/admin/edition_images_controller.rb
@@ -0,0 +1,4 @@
+class Admin::EditionImagesController < Admin::BaseController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -270,6 +270,7 @@ Whitehall::Application.routes.draw do
             post :upload_zip, on: :collection
             get :set_titles, on: :member
           end
+          resources :images, controller: "edition_images", only: %i[index]
         end
 
         get "/editions/:id" => "editions#show"
