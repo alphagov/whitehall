4999047bbe0ac178dbb8cd6143b0797eba2f117e
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-01-27 12:12:54 +0000

    Use GOV.UK Design System layout for world_taxonomy
    This was missed (i think by me) as part of moving the 1.5 relase
    behind the preview next release flag.

--- app/controllers/admin/edition_world_tags_controller.rb
+++ app/controllers/admin/edition_world_tags_controller.rb
@@ -27,7 +27,7 @@ class Admin::EditionWorldTagsController < Admin::BaseController
 private
 
   def get_layout
-    if preview_design_system?(next_release: false)
+    if preview_design_system?(next_release: true)
       "design_system"
     else
       "admin"
