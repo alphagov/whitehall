53913d04b83dc980871cd0219accf837a9753f22
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-01-30 16:48:54 +0000

    Populate images index page
    This commit creates some placeholder content for
    the images index endpoint. This is mostly non-functional
    for the time being, function will be built in at a later
    stage.
    
    The commit also adds some logic to the controller which
    provides the Design System layout, checks the user's
    permissions and manipulates the edition to have an
    empty image if there are no images attached.

--- app/controllers/admin/edition_images_controller.rb
+++ app/controllers/admin/edition_images_controller.rb
@@ -1,4 +1,28 @@
 class Admin::EditionImagesController < Admin::BaseController
-  def index
+  before_action :find_edition
+  before_action :redirect_unless_user_can_preview_images_update
+  before_action :enforce_permissions!
+  layout "design_system"
+
+  def index; end
+
+private
+
+  def find_edition
+    edition = Edition.find(params[:edition_id])
+    @edition = LocalisedModel.new(edition, edition.primary_locale)
+  end
+
+  def redirect_unless_user_can_preview_images_update
+    redirect_to edit_admin_edition_path(@edition) unless current_user.can_preview_images_update?
+  end
+
+  def enforce_permissions!
+    case action_name
+    when "index"
+      enforce_permission!(:see, @edition)
+    else
+      raise Whitehall::Authority::Errors::InvalidAction, action_name
+    end
   end
 end
