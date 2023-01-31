dbfdbeb818a536782bc1b8583d5bfcef1f437a73
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-01-31 09:44:31 +0000

    Ensure links passed in the flash render correctly

--- app/concerns/historic_content_concern.rb
+++ app/concerns/historic_content_concern.rb
@@ -6,7 +6,7 @@ module HistoricContentConcern
     # to redirect with a nice message rather than just "permission denied".
     if @edition.historic? && !can?(:modify, @edition)
       redirect_to [:admin, @edition],
-                  alert: %(This document is in <a href="https://www.gov.uk/guidance/how-to-publish-on-gov-uk/creating-and-updating-pages#history-mode">history mode</a>. Please contact your GOV.UK lead or managing editor if you need to change it.)
+                  alert: %(This document is in <a href="https://www.gov.uk/guidance/how-to-publish-on-gov-uk/creating-and-updating-pages#history-mode" class="govuk-link">history mode</a>. Please contact your GOV.UK lead or managing editor if you need to change it.)
     end
   end
 end
