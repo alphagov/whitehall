4704bf500eee70bb360802af93527b76ca3142fe
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2023-02-15 10:16:44 +0000

    Add a feature test for the display of images

--- features/step_definitions/image_steps.rb
+++ features/step_definitions/image_steps.rb
@@ -1,3 +1,16 @@
+Given("a draft document with images exists") do
+  images = [build(:image), build(:image)]
+  @edition = create(:draft_publication, body: "!!2", images:)
+end
+
+When("I visit the images tab of the the document with images") do
+  visit admin_edition_images_path(@edition)
+end
+
+Then("I should see a list of the images") do
+  expect(page).to have_selector(".app-view-edition-images__details", count: 2)
+end
+
 When(/^I select an image for the (?:detailed guide|publication)$/) do
   within ".images" do
     attach_file "File", jpg_image
