48abf2a1d6d2bc893a16c0fc1c8f73dbef18678e
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-02-24 16:47:02 +0000

    Add pagination to the Corporate Information Index page
    This adds pagination in a like for like way. Rather than adding a new
    component I've used the current Kaminari design and styled it using
    the GOV.UK Design System.
    
    We might decide during design review we either want to:
    
    1. Add a Whitehall Publishing Pagination Component
    2. Extend the Publishing Component Pagination Gem

--- app/controllers/admin/corporate_information_pages_controller.rb
+++ app/controllers/admin/corporate_information_pages_controller.rb
@@ -6,8 +6,8 @@ class Admin::CorporateInformationPagesController < Admin::EditionsController
 
   def index
     params[:state] = "active" # Ensure that state column is displayed.
-    paginator = @organisation.corporate_information_pages.where("state != ?", "superseded").order("corporate_information_page_type_id").page(1).per(100)
-    @filter = FakeEditionFilter.new paginator, "Corporate information pages", false, true
+    @paginator = @organisation.corporate_information_pages.where("state != ?", "superseded").order("corporate_information_page_type_id").page(params["page"].to_i || 1).per(100)
+    @filter = FakeEditionFilter.new @paginator, "Corporate information pages", false, true
 
     render_design_system(:index, :legacy_index, next_release: false)
   end
