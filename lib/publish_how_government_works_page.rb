07a26af1011bae1b9778fba45ccbb40e8ecd47b2
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-03-01 11:26:48 +0000

    Move republishing of How Government Works page
    At the moment, this page gets republished when the other static pages
    are published.
    
    However we plan to start including dynamic content in this page's
    content item.
    
    Therefore moving the republishing so it can be called when required
    (e.g. when an organisation is added or removed) and republished
    on-demand using a rake task.
    
    This is similar to the Past Prime Ministers index page.

---
+++ lib/publish_how_government_works_page.rb
@@ -0,0 +1,8 @@
+class PublishHowGovernmentWorksPage
+  def publish
+    payload = PublishingApi::HowGovernmentWorksPresenter.new
+
+    Services.publishing_api.put_content(payload.content_id, payload.content)
+    Services.publishing_api.publish(payload.content_id, nil)
+  end
+end
