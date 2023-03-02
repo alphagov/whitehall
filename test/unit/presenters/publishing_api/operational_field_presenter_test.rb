23d6f370cac0018199724afb1558f9ab89539d7c
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-03-02 12:30:05 +0000

    Refactor operational field presenter test

--- test/unit/presenters/publishing_api/operational_field_presenter_test.rb
+++ test/unit/presenters/publishing_api/operational_field_presenter_test.rb
@@ -12,47 +12,31 @@ class PublishingApi::OperationalFieldPresenterTest < ActiveSupport::TestCase
     @presented_content = @presented_operational_field.content
   end
 
-  test "it presents a valid placeholder content item" do
+  test "presents an operational field" do
+    expected_hash = {
+      title: "Operational Field name",
+      locale: "en",
+      publishing_app: "whitehall",
+      update_type: "major",
+      base_path: "/government/fields-of-operation/operational-field-name",
+      details: {},
+      document_type: "field_of_operation",
+      rendering_app: "whitehall-frontend",
+      schema_name: "field_of_operation",
+      description: "Operational Field description",
+      routes: [
+        {
+          path: "/government/fields-of-operation/operational-field-name",
+          type: "exact",
+        },
+      ],
+    }
+
+    assert_equal expected_hash, @presented_content
     assert_valid_against_publisher_schema @presented_content, "field_of_operation"
   end
 
   test "it delegates the content id" do
     assert_equal @operational_field.content_id, @presented_operational_field.content_id
-  end
-
-  test "it presents the name as title" do
-    assert_equal "Operational Field name", @presented_content[:title]
-  end
-
-  test "it presents the description" do
-    assert_equal "Operational Field description", @presented_content[:description]
-  end
-
-  test "it presents the base_path" do
-    assert_equal "/government/fields-of-operation/operational-field-name", @presented_content[:base_path]
-  end
-
-  test "it presents the publishing_app as whitehall" do
-    assert_equal "whitehall", @presented_content[:publishing_app]
-  end
-
-  test "it presents the rendering_app as whitehall-frontend" do
-    assert_equal "whitehall-frontend", @presented_content[:rendering_app]
-  end
-
-  test "it presents the schema_name as placeholder" do
-    assert_equal "placeholder", @presented_content[:schema_name]
-  end
-
-  test "it presents the document type as field_of_operation" do
-    assert_equal "field_of_operation", @presented_content[:document_type]
-  end
-
-  test "it presents the global process wide locale as the locale of the operational_field" do
-    assert_equal "en", @presented_content[:locale]
-  end
-
-  test "it presents empty details" do
-    assert_empty @presented_content[:details]
   end
 end
