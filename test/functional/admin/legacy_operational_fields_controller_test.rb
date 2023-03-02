dbaf7e8e7f04660b5e40dbfaec59a8df6d25ef1b
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-03-02 13:16:11 +0000

    Duplicate operational fields controller specs
    Adds the "Preview design system" permission to the user in the
    non-legacy specs so both routes are tested.

--- test/functional/admin/legacy_operational_fields_controller_test.rb
+++ test/functional/admin/legacy_operational_fields_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
-class Admin::OperationalFieldsControllerTest < ActionController::TestCase
+class Admin::LegacyOperationalFieldsControllerTest < ActionController::TestCase
+  tests Admin::OperationalFieldsController
+
   setup do
-    login_as_preview_design_system_user :gds_editor
+    login_as :gds_editor
   end
 
   should_be_an_admin_controller
