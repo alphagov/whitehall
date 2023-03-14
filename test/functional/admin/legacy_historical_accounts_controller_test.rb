5dfad2fcd465c6cf57ee3f34bea465974df5a5a5
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-03-14 14:45:09 +0000

    Duplicate historical accounts controller tests

--- test/functional/admin/legacy_historical_accounts_controller_test.rb
+++ test/functional/admin/legacy_historical_accounts_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
-class Admin::HistoricalAccountsControllerTest < ActionController::TestCase
+class Admin::LegacyHistoricalAccountsControllerTest < ActionController::TestCase
+  tests Admin::HistoricalAccountsController
+
   setup do
-    login_as_preview_design_system_user :writer
+    login_as :writer
     @person = create(:person)
     @role = create(:historic_role)
     @historical_account = create(:historical_account, person: @person, roles: [@role])
