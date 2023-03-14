5a7dce860c6ddd90444012b554761eb57fe1aaee
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-03-14 11:56:50 +0000

    Add person translation index view in the GOV.UK Design System

--- app/controllers/admin/person_translations_controller.rb
+++ app/controllers/admin/person_translations_controller.rb
@@ -5,14 +5,14 @@ class Admin::PersonTranslationsController < Admin::BaseController
   before_action :build_translation_locale, only: %i[confirm_destroy]
 
   def index
-    render :legacy_index
+    render_design_system(:index, :legacy_index, next_release: false)
   end
 
 private
 
   def get_layout
     design_system_actions = %w[confirm_destroy]
-    design_system_actions += %w[edit update] if preview_design_system?(next_release: false)
+    design_system_actions += %w[edit update index] if preview_design_system?(next_release: false)
 
     if design_system_actions.include?(action_name)
       "design_system"
