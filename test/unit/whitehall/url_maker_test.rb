7969a83001f0186de6833d7052edbdbe7d747eb1
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-03-15 07:51:42 +0000

    Update `UrlMaker`tests to use a page rendered by Whitehall
    These tests were previously using the 'How Government Works' page as an
    example for testing `UrlMaker`. Now Whitehall no longer renders this
    page, the route helper method has been removed, so updating to use a
    page that is still rendered by Whitehall.

--- test/unit/whitehall/url_maker_test.rb
+++ test/unit/whitehall/url_maker_test.rb
@@ -4,18 +4,18 @@ module Whitehall
   class UrlMakerTest < ActiveSupport::TestCase
     test "default url options can be set at create time" do
       maker = Whitehall::UrlMaker.new(host: "yahoo.com", protocol: "ftp")
-      assert_equal "ftp://yahoo.com/government/how-government-works", maker.how_government_works_url
+      assert_equal "ftp://yahoo.com/government/ministers", maker.ministerial_roles_url
     end
 
     test "default url options set on an instance do not interfere with other instances" do
       with_defaults = Whitehall::UrlMaker.new(host: "yahoo.com", protocol: "ftp")
       without_defaults = Whitehall::UrlMaker.new(host: "meh.com", protocol: "gopher")
-      assert_not_equal with_defaults.how_government_works_url, without_defaults.how_government_works_url
+      assert_not_equal with_defaults.ministerial_roles_url, without_defaults.ministerial_roles_url
     end
 
     test "host can be set when using _url helpers" do
       maker = Whitehall::UrlMaker.new
-      assert_equal "http://gov.uk/government/how-government-works", maker.how_government_works_url(host: "gov.uk")
+      assert_equal "http://gov.uk/government/ministers", maker.ministerial_roles_url(host: "gov.uk")
     end
 
     test "the default format can be overridden for a localised resource" do
