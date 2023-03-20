5033717959340fda6264cbdf0668bbf19acd0d41
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-03-20 12:11:57 +0000

    Move Government presenter test cases into a context block
    There were some tests outside of a context block, meaning they still
    existed when the contexts were run. This led to a validation error on
    the model as there are overlapping governments.
    
    This was never an issue before as the test was incorrectly set up to not
    write anything to the database, meaning the validation never ran.

--- test/unit/presenters/publishing_api/government_presenter_test.rb
+++ test/unit/presenters/publishing_api/government_presenter_test.rb
@@ -4,25 +4,27 @@ class PublishingApi::GovernmentPresenterTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
 
   describe PublishingApi::GovernmentPresenter do
-    setup do
-      @government = create(:government)
-      @presenter = PublishingApi::GovernmentPresenter.new(@government)
-    end
+    context "given a government" do
+      setup do
+        @government = create(:government)
+        @presenter = PublishingApi::GovernmentPresenter.new(@government)
+      end
 
-    test "the presenter has the content_id" do
-      assert_equal @government.content_id, @presenter.content_id
-    end
+      test "the presenter has the content_id" do
+        assert_equal @government.content_id, @presenter.content_id
+      end
 
-    test "the details has started_on" do
-      assert_equal(
-        @government.start_date.rfc3339,
-        @presenter.content.dig(:details, :started_on),
-      )
-    end
+      test "the details has started_on" do
+        assert_equal(
+          @government.start_date.rfc3339,
+          @presenter.content.dig(:details, :started_on),
+        )
+      end
 
-    test "the presented item is valid against the schema" do
-      assert_valid_against_publisher_schema @presenter.content, "government"
-      assert_valid_against_links_schema({ links: @presenter.links }, "government")
+      test "the presented item is valid against the schema" do
+        assert_valid_against_publisher_schema @presenter.content, "government"
+        assert_valid_against_links_schema({ links: @presenter.links }, "government")
+      end
     end
 
     context "given a previous government" do
