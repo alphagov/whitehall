f9be1bd0d5c283324d25f5315f90d13dfd63a22d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-03-21 14:33:41 +0000

    Remove `EditionCollectionPresenter` and related presenters
    This code is not being used anywhere except for tests, so it can now be
    removed.

--- app/presenters/speech_presenter.rb
+++
@@ -1,9 +0,0 @@
-class SpeechPresenter < Whitehall::Decorators::Decorator
-  include EditionPresenterHelper
-
-  delegate_instance_methods_of Speech
-
-  def delivered_on
-    date_microformat(:delivered_on)
-  end
-end
