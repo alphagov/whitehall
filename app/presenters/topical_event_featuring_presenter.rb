a206dcf8c951901dd745b7c95d77c9e75427cf4d
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2023-03-21 14:31:46 +0000

    remove topical event featuring presenter

--- app/presenters/topical_event_featuring_presenter.rb
+++
@@ -1,7 +0,0 @@
-class TopicalEventFeaturingPresenter < Whitehall::Decorators::Decorator
-  delegate_instance_methods_of TopicalEventFeaturing
-
-  def image_tag(size)
-    model.image.file.url(size || :s630)
-  end
-end
