6a9abbdef52f00209b78fecc1048868100aa5cd4
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2023-03-27 16:28:39 +0100

    Add new create page for Design system
    This moves all of the old bootstrap code to `legacy_*` files

--- app/controllers/admin/take_part_pages_controller.rb
+++ app/controllers/admin/take_part_pages_controller.rb
@@ -12,6 +12,8 @@ class Admin::TakePartPagesController < Admin::BaseController
 
   def new
     @take_part_page = TakePartPage.new
+
+    render_design_system("new", "legacy_new", next_release: false)
   end
 
   def create
@@ -19,7 +21,7 @@ class Admin::TakePartPagesController < Admin::BaseController
     if @take_part_page.save
       redirect_to [:admin, TakePartPage], notice: %(Take part page "#{@take_part_page.title}" created!)
     else
-      render :new
+      render_design_system("new", "legacy_new", next_release: false)
     end
   end
 
@@ -52,7 +54,7 @@ private
 
   def get_layout
     design_system_actions = []
-    design_system_actions += %w[] if preview_design_system?(next_release: false)
+    design_system_actions += %w[new create] if preview_design_system?(next_release: false)
 
     if design_system_actions.include?(action_name)
       "design_system"
