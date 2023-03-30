6e4b270364aba1e3483aba2f54ae41d58fc51ce5
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-03-30 10:56:11 +0100

    Update failing feature specs

--- features/step_definitions/admin_dashboard_steps.rb
+++ features/step_definitions/admin_dashboard_steps.rb
@@ -3,18 +3,34 @@ When(/^I visit the admin dashboard$/) do
 end
 
 Then(/^I should see the draft document "([^"]*)"$/) do |title|
-  edition = Edition.find_by!(title:).document.latest_edition
-  expect(page).to have_selector(".draft-documents #{record_css_selector(edition)}")
+  if using_design_system?
+    expect(all(".govuk-table")[0].all(".govuk-table__cell")[1].text).to eq title
+  else
+    edition = Edition.find_by!(title:).document.latest_edition
+    expect(page).to have_selector(".draft-documents #{record_css_selector(edition)}")
+  end
 end
 
 Then(/^I should see the force published document "([^"]*)"$/) do |title|
-  edition = Edition.find_by!(title:).document.latest_edition
-  expect(page).to have_selector(".force-published-documents #{record_css_selector(edition)}")
+  if using_design_system?
+    expect(all(".govuk-table")[1].all(".govuk-table__cell")[1].text).to eq title
+  else
+    edition = Edition.find_by!(title:).document.latest_edition
+    expect(page).to have_selector(".force-published-documents #{record_css_selector(edition)}")
+  end
 end
 
 Then(/^I should see a link to the content data app$/) do
-  link = find_link("Content Data", href: "https://content-data.test.gov.uk/content")
-  expect("external-link-clicked").to eq(link["data-track-category"])
-  expect("https://content-data.test.gov.uk/content").to eq(link["data-track-action"])
-  expect("Content Data").to eq(link["data-track-label"])
+  ## We track all external link clicks in the design_system layout
+  ## so we don't need to check it for when suing the design system.
+
+  if using_design_system?
+    expect(find_link("Content Data", href: "https://content-data.test.gov.uk/content")).not_to be_nil
+  else
+    link = find_link("Content Data", href: "https://content-data.test.gov.uk/content")
+
+    expect("external-link-clicked").to eq(link["data-track-category"])
+    expect("https://content-data.test.gov.uk/content").to eq(link["data-track-action"])
+    expect("Content Data").to eq(link["data-track-label"])
+  end
 end
