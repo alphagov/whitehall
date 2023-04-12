2bed00787d3ffc5eba2ebd87acbb2de0ab0437a8
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2023-04-12 14:41:49 +0100

    Render Field of Operation pages in Government Frontend

--- test/unit/presenters/publishing_api/operational_field_presenter_test.rb
+++ test/unit/presenters/publishing_api/operational_field_presenter_test.rb
@@ -32,7 +32,7 @@ class PublishingApi::OperationalFieldPresenterTest < ActiveSupport::TestCase
       base_path: "/government/fields-of-operation/operational-field-name",
       details: {},
       document_type: "field_of_operation",
-      rendering_app: "whitehall-frontend",
+      rendering_app: "government-frontend",
       schema_name: "field_of_operation",
       description: Whitehall::GovspeakRenderer.new.govspeak_to_html(@operational_field.description),
       routes: [
