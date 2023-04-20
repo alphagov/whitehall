2cacf83faa3a7d57fed6f2f6aafeb2f48d1e5469
Chris Roos <chris.roos@digital.cabinet-office.gov.uk>
2023-04-20 12:49:25 +0100

    Add tests for Embassy#remote_office
    We think these new tests explain more clearly what is being tested and
    rely on a single interface (#remote_office) into the underlying
    implementation, so we're also removing the explicit tests for
    `remote_office_country` so we can subsequently make it private (the
    tests are the only place that calls this method outside of the Embassy
    class).
    
    We've decided to inline the setup into each test for now to make it
    clearer to us where setup is shared and where it differs. We hope to
    factor this out as appropriate later as we continue to increase the
    test coverage.

--- test/unit/models/embassy_test.rb
+++ test/unit/models/embassy_test.rb
@@ -1,47 +1,10 @@
 require "test_helper"
 
 class EmbassyTest < ActiveSupport::TestCase
-  setup do
-    @location = create(:world_location, :with_worldwide_organisations, name: "Narnia")
-    @organisation = @location.worldwide_organisations.first
-
-    @organisation.main_office = create(
-      :worldwide_office,
-      title: "British Embassy Narnia",
-      worldwide_organisation: @organisation,
-      worldwide_office_type: WorldwideOfficeType::Embassy,
-      contact: create(:contact, street_address: "The woods", country: @location),
-    )
-  end
-
   test "it delegates to the world location" do
-    embassy = Embassy.new(@location)
+    location = create(:world_location, :with_worldwide_organisations, name: "Narnia")
+    embassy = Embassy.new(location)
     assert_equal "Narnia", embassy.name
-  end
-
-  test "remote_services_office and remote_services_country" do
-    legoland = create(:world_location, name: "Legoland")
-
-    toytown = create(:world_location, :with_worldwide_organisations, name: "Narnia")
-    toytown_org = toytown.worldwide_organisations.first
-    toytown_org.main_office = nil
-    contact = create(
-      :contact,
-      title: "British Embassy Legoland",
-      street_address: "1 Brick Lane",
-      country: legoland,
-    )
-    toytown_org.offices << create(
-      :worldwide_office,
-      title: "British Consular Services Legoland",
-      contact:,
-      worldwide_organisation: toytown_org,
-      worldwide_office_type: WorldwideOfficeType::Embassy,
-    )
-
-    location = Embassy.new(toytown)
-
-    assert_equal "Legoland", location.remote_services_country.name
   end
 
   test '#can_assist_british_nationals? returns true if the world location is a special case' do
@@ -70,5 +33,60 @@ class EmbassyTest < ActiveSupport::TestCase
     embassy = Embassy.new(location)
 
     refute embassy.can_assist_british_nationals?
+  end
+
+  test '#remote_office returns special case data where world location is a special case' do
+    location_name = 'Central African Republic'
+    location = build(:world_location, name: location_name)
+    embassy = Embassy.new(location)
+
+    assert Embassy::SPECIAL_CASES.has_key?(location_name)
+
+    expected = Embassy::RemoteOffice.new(
+      name: "Foreign, Commonwealth and Development Office",
+      location: "the UK",
+      path: "/government/organisations/foreign-commonwealth-development-office",
+    )
+    assert_equal expected, embassy.remote_office
+  end
+
+  test '#remote_office returns details of remote office where world location is not a special case' do
+    location = create(:world_location, name: 'location-1')
+    organisation = create(:worldwide_organisation,
+                          world_locations: [location],
+                          name: 'org-name',
+                          slug: 'org-slug')
+    other_location = create(:world_location, name: 'location-2')
+    contact = create(:contact, street_address: 'street-address', country: other_location)
+    office = create(:worldwide_office,
+                    contact: contact,
+                    worldwide_organisation: organisation,
+                    worldwide_office_type: WorldwideOfficeType::EMBASSY_OFFICE_TYPES.first,
+                   )
+    embassy = Embassy.new(location)
+
+    expected = Embassy::RemoteOffice.new(
+      name: "org-name",
+      location: other_location,
+      path: "/world/organisations/org-slug",
+    )
+    assert_equal expected, embassy.remote_office
+  end
+
+  test '#remote_office returns nil when the world location is not a special case nor does it have all of its offices outside of the world location' do
+    location = create(:world_location, name: 'location-1')
+    organisation = create(:worldwide_organisation,
+                          world_locations: [location],
+                          name: 'org-name',
+                          slug: 'org-slug')
+    contact = create(:contact, street_address: 'street-address', country: location)
+    office = create(:worldwide_office,
+                    contact: contact,
+                    worldwide_organisation: organisation,
+                    worldwide_office_type: WorldwideOfficeType::EMBASSY_OFFICE_TYPES.first,
+                  )
+    embassy = Embassy.new(location)
+
+    assert_nil embassy.remote_office
   end
 end
