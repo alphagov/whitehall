ce1c60bb5acc8af9d2a1e98bc971766136034337
Chris Whitehouse <chris.whitehouse@digital.cabinet-office.gov.uk>
2023-04-24 17:38:08 +0100

    Add cancel statistics announcment into the GOV.UK Design System

--- app/controllers/admin/statistics_announcements_controller.rb
+++ app/controllers/admin/statistics_announcements_controller.rb
@@ -47,14 +47,14 @@ class Admin::StatisticsAnnouncementsController < Admin::BaseController
   end
 
   def cancel
-    render :legacy_cancel
+    render_design_system("cancel", "legacy_cancel", next_release: false)
   end
 
   def publish_cancellation
     if @statistics_announcement.cancel!(params[:statistics_announcement][:cancellation_reason], current_user)
       redirect_to [:admin, @statistics_announcement], notice: "Announcement has been cancelled"
     else
-      render :legacy_cancel
+      render_design_system("cancel", "legacy_cancel", next_release: false)
     end
   end
 
@@ -135,7 +135,7 @@ private
 
   def get_layout
     design_system_actions = []
-    design_system_actions += %w[edit update new create] if preview_design_system?(next_release: false)
+    design_system_actions += %w[edit update new create cancel publish_cancellation] if preview_design_system?(next_release: false)
 
     if design_system_actions.include?(action_name)
       "design_system"
