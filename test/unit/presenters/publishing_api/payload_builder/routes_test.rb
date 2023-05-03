f4b67cb4f1ea1bb04deb7b2968fdd6cf2641187a
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-05-03 11:51:22 +0100

    Add unit test for PublishingApi::PayloadBuilder::Routes
    The additional_routes logic was added in this commit [1], but its effect
    was only tested in the PublishingApi::OrganisationPresenter test. I
    think it's useful to include a more specific unit test for this Routes
    class, particularly because I'm about to make changes to it.
    
    I've also tweaked the name of the original test method to make its
    purpose clearer and to differentiate it from the new test method.
    
    [1]: 381856144a7797277f9690184685bffd698019af

--- test/unit/presenters/publishing_api/payload_builder/routes_test.rb
+++ test/unit/presenters/publishing_api/payload_builder/routes_test.rb
@@ -3,10 +3,22 @@ require "test_helper"
 module PublishingApi
   module PayloadBuilder
     class RoutesTest < ActiveSupport::TestCase
-      test "returns a routes payload in the correct form" do
+      test "returns a routes payload without additional routes" do
         base_path = "some/base/path"
 
         assert_equal({ routes: [{ path: base_path, type: "exact" }] }, Routes.for(base_path))
+      end
+
+      test "returns a routes payload with additional routes" do
+        base_path = "some/base/path"
+        additional_routes = %w[atom rss]
+        expected_routes = [
+          { path: base_path, type: "exact" },
+          { path: "#{base_path}.atom", type: "exact" },
+          { path: "#{base_path}.rss", type: "exact" },
+        ]
+
+        assert_equal({ routes: expected_routes }, Routes.for(base_path, additional_routes:))
       end
     end
   end
