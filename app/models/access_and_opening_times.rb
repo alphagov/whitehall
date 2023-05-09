ab15e3ac1a6c49062920d8791cecf33307a08b89
Joseph Kempster <jkempster34@gmail.com>
2023-05-09 14:00:51 +0100

    Remove AccessAndOpeningTimes model
    This class is no longer used, and the table associated with the model is to be
    removed.

--- app/models/access_and_opening_times.rb
+++
@@ -1,8 +0,0 @@
-class AccessAndOpeningTimes < ApplicationRecord
-  belongs_to :accessible, polymorphic: true
-
-  validates_with SafeHtmlValidator
-  validates_with NoFootnotesInGovspeakValidator, attribute: :body
-
-  validates :body, presence: true
-end
