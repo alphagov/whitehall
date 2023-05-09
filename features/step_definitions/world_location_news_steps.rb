8fe41b4430ed8b384f2bbaf14da4c8e5dab6822d
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-05-09 13:17:02 +0100

    Add reorder featured documents feature spec
    This is the first of several commits that adds in feature specs for
    the world_location_news#features page.
    
    This page seems to have been previously untested.

--- features/step_definitions/world_location_news_steps.rb
+++ features/step_definitions/world_location_news_steps.rb
@@ -2,7 +2,12 @@ Given(/^no world locations exist$/) do
   WorldLocation.delete_all
 end
 
-When(/^I visit the world location news page$/) do
+And(/^a world location news exists$/) do
+  @world_location = build(:world_location, slug: "foo")
+  @world_location_news = create(:world_location_news, world_location: @world_location)
+end
+
+When(/^I visit the world location news index page$/) do
   visit admin_world_location_news_index_path
 end
 
@@ -12,4 +17,39 @@ end
 
 Then(/^I should see the "([^"]*)" message$/) do |message|
   expect(page).to have_selector("p", text: "#{message}.")
+end
+
+Given(/^the world location has a feature list with (\d+) featured (?:document|documents)$/) do |count|
+  @feature_list = create(:feature_list, featurable: @world_location_news)
+
+  count.times do |i|
+    edition = create(:edition, :published, title: "Document #{i + 1}")
+
+    create(:feature, feature_list: @feature_list, document: edition.document)
+  end
+end
+
+When(/^I visit the world location news page$/) do
+  visit features_admin_world_location_news_path(@world_location, locale: I18n.default_locale)
+end
+
+And(/^I set the order of the featured documents to:$/) do |featured_documents_order|
+  visit features_admin_world_location_news_path(@world_location, locale: I18n.default_locale)
+  click_link "Reorder documents"
+
+  featured_documents_order.hashes.each do |hash|
+    feature = @feature_list.features.select { |f| f.to_s == hash[:title] }.first
+    fill_in "ordering[#{feature.id}]", with: hash[:order]
+  end
+
+  click_button "Update order"
+end
+
+Then(/^the featured documents should be in the following order:$/) do |featured_documents_titles|
+  document_titles = all("table td:first").map(&:text)
+
+  featured_documents_titles.hashes.each_with_index do |hash, index|
+    feature = @feature_list.features.select { |f| f.to_s == hash[:title] }.first
+    expect(feature.to_s).to eq(document_titles[index])
+  end
 end
--- features/world-location-news.feature
+++ features/world-location-news.feature
@@ -1,9 +1,24 @@
+@design-system-only
 Feature: Administering world location news information
-  @design-system-only
+  Background:
+    Given I am a GDS admin
+    And a world location news exists
+
   Scenario: Viewing the list presents no world location news message, when no news exists
     Given no world locations exist
-    And I am a GDS admin
-    When I visit the world location news page
+    When I visit the world location news index page
     Then I should see the "No active world location news" message
     When I click the Inactive tab
     Then I should see the "No inactive world location news" message
+
+  Scenario: Reordering currently featured documents
+    Given the world location has a feature list with 2 featured documents
+    When I visit the world location news page
+    And I set the order of the featured documents to:
+      | title      | order |
+      | Document 2 | 0     |
+      | Document 1 | 1     |
+    Then the featured documents should be in the following order:
+      | title      |
+      | Document 2 |
+      | Document 1 |
