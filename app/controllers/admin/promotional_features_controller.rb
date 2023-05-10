46f87554a0172dbe82e1c7e085b08832d5ee6880
farahTW <farah.mehmood@digital.cabinet-office.gov.uk>
2023-05-10 16:09:48 +0100

    Added index to design system actions and created new index html with gds design

--- app/controllers/admin/promotional_features_controller.rb
+++ app/controllers/admin/promotional_features_controller.rb
@@ -66,7 +66,7 @@ private
 
   def get_layout
     design_system_actions = %w[reorder update_order]
-    design_system_actions += %w[edit confirm_destroy] if preview_design_system?(next_release: false)
+    design_system_actions += %w[edit confirm_destroy index] if preview_design_system?(next_release: false)
     if design_system_actions.include?(action_name)
       "design_system"
     else
