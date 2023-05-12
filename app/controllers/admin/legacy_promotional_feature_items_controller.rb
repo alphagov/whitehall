2581eccfc2fe967b2c23e856a733536475390198
farahTW <farah.mehmood@digital.cabinet-office.gov.uk>
2023-05-12 16:21:22 +0100

    Duplicated controller test and added preview design system with new confirm_destroy end point in the controller file

--- app/controllers/admin/legacy_promotional_feature_items_controller.rb
+++ app/controllers/admin/legacy_promotional_feature_items_controller.rb
@@ -1,9 +1,9 @@
 class Admin::PromotionalFeatureItemsController < Admin::BaseController
   before_action :load_organisation
   before_action :load_promotional_feature
-  before_action :load_promotional_feature_item, only: %i[edit update destroy confirm_destroy]
+  before_action :load_promotional_feature_item, only: %i[edit update destroy]
   before_action :clean_image_or_youtube_video_url_param, only: %i[create update]
-  layout :get_layout
+
   def new
     @promotional_feature_item = @promotional_feature.promotional_feature_items.build
     @promotional_feature_item.links.build
@@ -44,16 +44,9 @@ class Admin::PromotionalFeatureItemsController < Admin::BaseController
     Whitehall::PublishingApi.republish_async(@organisation)
     redirect_to_feature "Feature item deleted."
   end
-  def confirm_destroy; end
+
 private
-  def get_layout
-    design_system_actions = %w[confirm_destroy]
-    if preview_design_system?(next_release: false) && design_system_actions.include?(action_name)
-      "design_system"
-    else
-      "admin"
-    end
-  end
+
   def load_organisation
     @organisation = Organisation.allowed_promotional.find(params[:organisation_id])
   end
--- test/functional/admin/legacy_promotional_feature_items_controller_test.rb
+++ test/functional/admin/legacy_promotional_feature_items_controller_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 class Admin::PromotionalFeatureItemsControllerTest < ActionController::TestCase
+  tests Admin::PromotionalFeatureItemsController
   setup do
-    login_as_preview_design_system_user :writer
+    login_as :writer
     @promotional_feature = create(:promotional_feature)
     @organisation = @promotional_feature.organisation
   end
