4c3d40a35693eae6d6e1a5f088dc71b83e488ff0
Chris Whitehouse <chris.whitehouse@digital.cabinet-office.gov.uk>
2023-05-15 15:11:16 +0100

    Add pagination to search results

--- app/controllers/admin/statistics_announcement_publications_controller.rb
+++ app/controllers/admin/statistics_announcement_publications_controller.rb
@@ -3,7 +3,9 @@ class Admin::StatisticsAnnouncementPublicationsController < Admin::BaseControlle
   layout "design_system"
 
   def index
-    get_editions
+    if params[:search].present?
+      filter
+    end
   end
 
   def connect
@@ -12,17 +14,41 @@ class Admin::StatisticsAnnouncementPublicationsController < Admin::BaseControlle
     if @statistics_announcement.save
       redirect_to [:admin, @statistics_announcement], notice: "Announcement updated successfully"
     else
-      get_editions
+      filter
       render :index
     end
   end
 
 private
 
+  def filter
+    @filter ||= Admin::EditionFilter.new(get_editions, current_user, edition_filter_options)
+  end
+
   def get_editions
-    if params[:search].present?
-      @editions = Edition.statistical_publications.with_title_containing(params[:search])
-    end
+    Edition.statistical_publications.with_title_containing(params[:search])
+  end
+
+  def params_filters
+    params.slice(:type, :state, :organisation, :author, :page, :title, :world_location, :from_date, :to_date, :only_broken_links)
+          .permit!
+          .to_h
+  end
+
+  def params_filters_with_default_state
+    params_filters.reverse_merge("state" => "active")
+  end
+
+  def edition_filter_options
+    filter_options = params_filters_with_default_state
+                       .symbolize_keys
+                       .merge(
+                         include_unpublishing: true,
+                         include_link_check_reports: true,
+                         include_last_author: true,
+                       )
+
+    filter_options.merge(per_page: Admin::EditionFilter::GOVUK_DESIGN_SYSTEM_PER_PAGE)
   end
 
   def find_statistics_announcement
--- test/functional/admin/statistics_announcement_publications_controller_test.rb
+++ test/functional/admin/statistics_announcement_publications_controller_test.rb
@@ -6,7 +6,7 @@ class Admin::StatisticsAnnouncementPublicationsControllerTest < ActionController
     @announcement = create(:statistics_announcement)
     @statistics_publication = create(:published_statistics)
     @generic_publication = create(:publication)
-    @search_string = "publication-title"
+    @search = "publication-title"
   end
 
   should_be_an_admin_controller
@@ -20,11 +20,37 @@ class Admin::StatisticsAnnouncementPublicationsControllerTest < ActionController
     refute_select ".govuk-table"
   end
 
-  view_test "GET :index with search value renders search bar and list of statistical publications only" do
-    get :index, params: { statistics_announcement_id: @announcement, search: @search_string }
+  test "GET :index will only return statistical publications" do
+    Admin::EditionFilter.expects(:new).with([@statistics_publication], anything, anything)
+
+    get :index, params: { statistics_announcement_id: @announcement, search: @search }
+  end
+
+  test "GET :index will filter by title containing search param" do
+    create(:published_statistics, title: "something else")
+
+    Admin::EditionFilter.expects(:new).with([@statistics_publication], anything, anything)
+
+    get :index, params: { statistics_announcement_id: @announcement, search: @search }
+  end
+
+  view_test "GET :index with search value renders paginated results" do
+    editions = []
+    16.times { editions << @statistics_publication }
+
+    stub_filter = stub_edition_filter({ editions:, options: { per_page: 15 } })
+    Admin::EditionFilter.stubs(:new).returns(stub_filter)
+
+    get :index, params: { statistics_announcement_id: @announcement, search: @search }
 
     assert_response :success
-    has_search_results_table
+    assert_template :index
+    assert_select "input[name='search']"
+    assert_select ".govuk-heading-s", "16 documents"
+    assert_select ".govuk-table" do
+      assert_select "tr", count: 15
+    end
+    assert_select "nav.govuk-pagination"
   end
 
   test "GET :connect will add a document to a statistics announcement" do
@@ -35,24 +61,39 @@ class Admin::StatisticsAnnouncementPublicationsControllerTest < ActionController
   end
 
   view_test "GET :connect will handle an error in adding not a statistics publication to statistics announcement" do
-    get :connect, params: { statistics_announcement_id: @announcement, publication_id: @generic_publication, search: @search_string }
+    stub_filter = stub_edition_filter({ editions: [@statistics_publication], options: { per_page: 15 } })
+    Admin::EditionFilter.stubs(:new).returns(stub_filter)
+
+    get :connect, params: { statistics_announcement_id: @announcement, publication_id: @generic_publication, search: @search }
 
     assert_response :success
     has_search_results_table
     assert_select "ul.govuk-error-summary__list a[data-track-action='statistics announcement-error'][data-track-label=\"Publication type does not match: must be statistics\"]", text: "Publication type does not match: must be statistics"
   end
 
-  private
+private
 
   def has_search_results_table
     assert_template :index
     assert_select "input[name='search']"
-    assert_select ".govuk-table__caption--m", "1 document"
+    assert_select ".govuk-heading-s", "1 document"
     assert_select ".govuk-table" do
       assert_select "tr", count: 1
       assert_select "td", @statistics_publication.title
       assert_select "a[href=?]", admin_publication_path(@statistics_publication), text: "View"
-      assert_select "a[href=?]", admin_statistics_announcement_publication_connect_path(@announcement, @statistics_publication, search: @search_string), text: "Connect"
+      assert_select "a[href=?]", admin_statistics_announcement_publication_connect_path(@announcement, @statistics_publication, search: @search), text: "Connect"
     end
+  end
+
+  def stub_edition_filter(attributes = {})
+    default_attributes = {
+      editions: Kaminari.paginate_array(attributes[:editions] || [], limit: attributes[:options][:per_page]).page(1),
+      page_title: "",
+      edition_state: "",
+      valid?: true,
+      options: {},
+      hide_type: false,
+    }
+    stub("edition filter", default_attributes.merge(attributes.except(:editions)))
   end
 end
