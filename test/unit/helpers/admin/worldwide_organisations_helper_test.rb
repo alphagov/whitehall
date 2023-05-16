dc65063d359bd36dd96e4fb21a621109363cf899
James Mead <james.mead@digital.cabinet-office.gov.uk>
2023-05-16 14:24:11 +0100

    Add test for Admin::WorldwideOrganisationsHelper#worldwide_organisation_tabs
    I'm planning to add an Attachments tab to this in a later commit. This
    will give me more confidence to do that and should make it clearer
    what's changing.

--- test/unit/helpers/admin/worldwide_organisations_helper_test.rb
+++ test/unit/helpers/admin/worldwide_organisations_helper_test.rb
@@ -1,6 +1,21 @@
 require "test_helper"
 
 class Admin::WorldwideOrganisationsHelperTest < ActionView::TestCase
+  test "#worldwide_organisation_tabs returns relevant tabs" do
+    worldwide_organisation = build_stubbed(:worldwide_organisation)
+
+    expected_output = [
+      "Details",
+      "Translations",
+      "Offices",
+      "Access and opening times",
+      "Social media accounts",
+      "Corporate information pages",
+    ]
+
+    assert_equal expected_output, worldwide_organisation_tabs(worldwide_organisation).keys
+  end
+
   test "#worldwide_organisation_nav_items returns an array of hashes with the correct items" do
     worldwide_organisation = build_stubbed(:worldwide_organisation)
     current_path = admin_worldwide_organisation_path(worldwide_organisation)
