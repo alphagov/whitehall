9d8c3780c6b76d9ba44377a3d32785f36eb57a25
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-05-22 15:23:10 +0100

    Update currently_featured_tab_component to work with featurings
    When building this i hadn't realised that there is a features page
    for topical events. Instead of features belonging to a feature list,
    which in turn belongs to a world locations news or org, featurables
    belong to topical events.
    
    They have a lot more in common than different so i'm going to retain
    the currently_featured_tab_components core behaviour, but tweak
    it so that it works for both features and featurings.
    
    This updates the currently_featured_tab_component to do this. I'm going
    to add the two new components which handle rendering the featured
    documents for both into their own commits so it's a bit easier to
    review.

--- app/components/admin/currently_featured_tab_component.rb
+++ app/components/admin/currently_featured_tab_component.rb
@@ -1,138 +1,41 @@
 # frozen_string_literal: true
 
 class Admin::CurrentlyFeaturedTabComponent < ViewComponent::Base
-  include Admin::EditionRoutesHelper
-  include Admin::OrganisationHelper
+  attr_reader :features, :featurings, :maximum_featured_documents
 
-  attr_reader :features, :maximum_featured_documents
-
-  def initialize(features:, maximum_featured_documents:)
-    @features = features || []
+  def initialize(maximum_featured_documents:, features: [], featurings: [])
+    @features = features
+    @featurings = featurings
     @maximum_featured_documents = maximum_featured_documents
   end
 
 private
 
-  def live_features
-    @live_features ||= features.slice(0, maximum_featured_documents)
+  def featured
+    @featured ||= (features.presence || featurings)
   end
 
-  def remaining_features
-    @remaining_features ||= features - live_features
+  def live
+    @live ||= featured.slice(0, maximum_featured_documents)
   end
 
-  def table(caption, features)
-    tag.div(class: "govuk-table--with-actions") do
-      render "govuk_publishing_components/components/table", {
-        caption:,
-        caption_classes: "govuk-heading-s",
-        head: [
-          {
-            text: "Title",
-          },
-          {
-            text: "Type",
-          },
-          {
-            text: "Published",
-          },
-          {
-            text: tag.span("Actions", class: "govuk-visually-hidden"),
-          },
-        ],
-        rows: rows(features),
-      }
-    end
+  def remaining
+    @remaining ||= featured - live
   end
 
-  def rows(features)
-    features.map do |feature|
-      [
-        title_row(feature),
-        type_row(feature),
-        published_row(feature),
-        actions_row(feature),
-      ]
-    end
-  end
-
-  def title_row(feature)
-    {
-      text: tag.p(title(feature), class: "govuk-!-font-weight-bold govuk-!-margin-0"),
-    }
-  end
-
-  def title(feature)
-    if feature.document&.live_edition.present?
-      feature
-    elsif feature.topical_event.present?
-      feature.topical_event
-    elsif feature.offsite_link.present?
-      feature.offsite_link
+  def table(caption, featured)
+    if features.present?
+      render Admin::Features::FeaturedDocumentsTableComponent.new(caption:, features: featured)
     else
-      feature
+      render Admin::TopicalEvents::Featurings::FeaturedDocumentsTableComponent.new(caption:, featurings: featured)
     end
   end
 
-  def type_row(feature)
-    {
-      text: type(feature),
-    }
-  end
-
-  def type(feature)
-    if feature.document&.live_edition.present?
-      "#{feature.document.live_edition.type.titleize} (document)"
-    elsif feature.topical_event.present?
-      "Topical Event"
-    elsif feature.offsite_link.present?
-      "#{feature.offsite_link.humanized_link_type} (offsite link)"
+  def reorder_path
+    if features.present?
+      reorder_admin_feature_list_path(features.first.feature_list)
     else
-      ""
-    end
-  end
-
-  def published_row(feature)
-    {
-      text: published_at(feature),
-    }
-  end
-
-  def published_at(feature)
-    if feature.document&.live_edition.present?
-      localize(feature.document.live_edition.major_change_published_at.to_date)
-    elsif feature.topical_event.present?
-      topical_event_dates_string(feature.topical_event)
-    elsif feature.offsite_link.present?
-      (localize(feature.offsite_link.date.to_date) if feature.offsite_link.date) || ""
-    else
-      ""
-    end
-  end
-
-  def actions_row(feature)
-    {
-      text: sanitize(edit_link(feature) + unfeature_link(feature)),
-    }
-  end
-
-  def edit_link(feature)
-    if feature.document&.live_edition.present?
-      link_to(sanitize("Edit #{tag.span(feature, class: 'govuk-visually-hidden')}"), admin_edition_path(feature.document.live_edition), class: "govuk-link")
-    elsif feature.topical_event.present?
-      link_to(sanitize("Edit #{tag.span(feature.topical_event, class: 'govuk-visually-hidden')}"), edit_admin_topical_event_path(feature.topical_event), class: "govuk-link")
-    elsif feature.offsite_link.present?
-      link_to(sanitize("Edit #{tag.span(feature.offsite_link, class: 'govuk-visually-hidden')}"), polymorphic_path([:edit, :admin, feature.offsite_link.parent, feature.offsite_link]), class: "govuk-link")
-    else
-      ""
-    end
-  end
-
-  def unfeature_link(feature)
-    if feature.document&.live_edition.present? || feature.topical_event.present? || feature.offsite_link.present?
-      link_to(sanitize("Unfeature #{tag.span(title(feature), class: 'govuk-visually-hidden')}"), confirm_unfeature_admin_feature_list_feature_path(feature.feature_list, feature), class: "gem-link--destructive govuk-!-margin-left-2")
-    else
-      ""
+      reorder_admin_topical_event_topical_event_featurings_path(featurings.first.topical_event)
     end
   end
 end
