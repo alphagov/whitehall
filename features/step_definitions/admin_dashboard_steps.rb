dd4458d874e31ff85358c50ed27a5751a5ec21a4
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-05-23 17:44:49 +0100

    Reorder table columns and remove state column

--- features/step_definitions/admin_dashboard_steps.rb
+++ features/step_definitions/admin_dashboard_steps.rb
@@ -4,7 +4,7 @@ end
 
 Then(/^I should see the draft document "([^"]*)"$/) do |title|
   if using_design_system?
-    expect(all(".govuk-table")[0].all(".govuk-table__cell")[1].text).to eq title
+    expect(all(".govuk-table")[0].all(".govuk-table__cell")[0].text).to eq title
   else
     edition = Edition.find_by!(title:).document.latest_edition
     expect(page).to have_selector(".draft-documents #{record_css_selector(edition)}")
@@ -13,7 +13,7 @@ end
 
 Then(/^I should see the force published document "([^"]*)"$/) do |title|
   if using_design_system?
-    expect(all(".govuk-table")[1].all(".govuk-table__cell")[1].text).to eq title
+    expect(all(".govuk-table")[1].all(".govuk-table__cell")[0].text).to eq title
   else
     edition = Edition.find_by!(title:).document.latest_edition
     expect(page).to have_selector(".force-published-documents #{record_css_selector(edition)}")
@@ -22,7 +22,7 @@ end
 
 Then(/^I should see a link to the content data app$/) do
   ## We track all external link clicks in the design_system layout
-  ## so we don't need to check it for when suing the design system.
+  ## so we don't need to check it for when using the design system.
 
   if using_design_system?
     expect(find_link("Content Data", href: "https://content-data.test.gov.uk/content")).not_to be_nil
