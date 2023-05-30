5eb482b72efec241fe26aaf2423edbd24a5ef18a
Chris Roos <chris@seagul.co.uk>
2023-05-30 17:04:14 +0100

    Delete attachments from Worldwide Org About pages
    In the commit titled "Move attachments from about page to
    WorldwideOrganisation" we move attachments from published About
    Corporate Information Pages to the corresponding Worldwide Organisation.
    
    In this commit we're updating the tidy-up migration so that as well as
    deleting the now redundant About Corporate Information Pages we also
    delete any attachments associated with those pages. The `has_many
    :attachments` comes from the `Attachable` concern and I've copied enough
    of the `Attachment` class to get everything to work.
    
    The output of this destructive migration is very verbose in case we need
    to recover any of the data. An example of the output:
    
    Processing DeleteWorldwideOrganisationAboutPages::WorldwideOrganisation 503 - uk-science-innovation-network-in-colombia
      Deleting CorporateInformationPage 923435 - About us
        Deleting FileAttachment 3234866 - UK Science and Innovation Network Country Snapshot - Colombia
      Deleting CorporateInformationPage 1265622 - About us
        Deleting FileAttachment 6017156 - UK Science and Innovation Network Country Snapshot - Colombia
      Deleting CorporateInformationPage 1427071 - About us
    
    I ran this against a reasonably recent version of the Whitehall data in
    development and it took about 15 minutes. The summary printed at the end
    of the migration was:
    
    == Summary
    
    === Before
    
    WorldwideOrganisations: 435
    About Us Corporate Information Pages: 10742
    Attachments: 2602
    
    === After
    
    WorldwideOrganisations: 435
    About Us Corporate Information Pages: 0
    Attachments: 0

--- db/pending_migration_cleanups/20230516160033_delete_worldwide_organisation_about_pages.rb
+++ db/pending_migration_cleanups/20230516160033_delete_worldwide_organisation_about_pages.rb
@@ -2,8 +2,30 @@ class DeleteWorldwideOrganisationAboutPages < ActiveRecord::Migration[7.0]
   class Edition < ApplicationRecord
   end
 
+  class Attachment < ApplicationRecord
+    scope :not_deleted, -> { where(deleted: false) }
+
+    def delete
+      update_column(:deleted, true)
+    end
+
+    def destroy
+      callbacks_result = transaction do
+        run_callbacks(:destroy) do
+          delete
+        end
+      end
+      callbacks_result ? self : false
+    end
+  end
+
   class CorporateInformationPage < Edition
     ABOUT_US_TYPE_ID = 20
+
+    has_many :attachments,
+             -> { not_deleted.order("attachments.ordering, attachments.id") },
+             as: :attachable,
+             inverse_of: :attachable
   end
 
   class WorldwideOrganisation < ApplicationRecord
@@ -16,9 +38,38 @@ class DeleteWorldwideOrganisationAboutPages < ActiveRecord::Migration[7.0]
   end
 
   def up
+    worldwide_organisations = WorldwideOrganisation.all
+    about_us_pages = worldwide_organisations.flat_map(&:about_us_page_editions)
+    about_us_pages_attachments = about_us_pages.flat_map(&:attachments)
+    worldwide_organisations_count_before = worldwide_organisations.count
+    about_us_pages_count_before = about_us_pages.count
+    about_us_pages_attachments_count_before = about_us_pages_attachments.count
+
     WorldwideOrganisation.all.each do |worldwide_organisation|
-      worldwide_organisation.about_us_page_editions.destroy_all
+      write "Processing #{worldwide_organisation.class} #{worldwide_organisation.id} - #{worldwide_organisation.slug}"
+      worldwide_organisation.about_us_page_editions.each do |page|
+        write "  Deleting #{page.class} #{page.id} - #{page.title}"
+        page.attachments.each do |attachment|
+          write "    Deleting #{attachment.class} #{attachment.id} - #{attachment.title}"
+          attachment.destroy # rubocop:disable Rails/SaveBang
+        end
+        page.destroy!
+      end
     end
+
+    write "## Summary"
+    write "### Before"
+    write "WorldwideOrganisations: #{worldwide_organisations_count_before}"
+    write "About Us Corporate Information Pages: #{about_us_pages_count_before}"
+    write "Attachments: #{about_us_pages_attachments_count_before}"
+
+    write "### After"
+    worldwide_organisations = WorldwideOrganisation.all
+    about_us_pages = worldwide_organisations.flat_map(&:about_us_page_editions)
+    about_us_pages_attachments = about_us_pages.flat_map(&:attachments)
+    write "WorldwideOrganisations: #{worldwide_organisations.count}"
+    write "About Us Corporate Information Pages: #{about_us_pages.count}"
+    write "Attachments: #{about_us_pages_attachments.count}"
   end
 
   def down
