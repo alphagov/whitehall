3a881ef72fabf81024c551f715f153f89c36db7e
Joseph Kempster <jkempster34@gmail.com>
2023-05-30 18:08:22 +0100

    Update the features for Worldwide Office access information
    Once rendering has been removed from Whitehall, these features will fail.
    
    However, they are helpful to ensure that the office is associated with the
    correct access information. 
    
    This updates them to check the edit page instead of the public page; this is
    now the only page that displays access information.

--- features/step_definitions/worldwide_organisation_steps.rb
+++ features/step_definitions/worldwide_organisation_steps.rb
@@ -147,17 +147,20 @@ When(/^I add default access information to the worldwide organisation$/) do
   click_button "Save"
 end
 
-Then(/^I should see the default access information on the public "([^"]*)" office page$/) do |office_name|
+Then(/^I should see the default access information on the edit "([^"]*)" office page$/) do |office_name|
   worldwide_organisation = WorldwideOrganisation.last
   worldwide_office = WorldwideOffice.joins(contact: :translations).where(contact_translations: { title: office_name }).first
-  visit worldwide_organisation.public_path
-  within record_css_selector(worldwide_office) do
-    click_link "Access and opening times"
-  end
+  visit edit_admin_worldwide_organisation_worldwide_office_access_and_opening_time_path(worldwide_organisation, worldwide_office)
 
-  within ".body" do
-    expect(page).to have_content("Default body information")
-  end
+  expect(page).to have_content("Default body information")
+end
+
+Then(/^I should see custom access information on the edit "([^"]*)" office page$/) do |office_name|
+  worldwide_organisation = WorldwideOrganisation.last
+  worldwide_office = WorldwideOffice.joins(contact: :translations).where(contact_translations: { title: office_name }).first
+  visit edit_admin_worldwide_organisation_worldwide_office_access_and_opening_time_path(worldwide_organisation, worldwide_office)
+
+  expect(page).to have_content("Custom body information")
 end
 
 Given(/^a worldwide organisation "([^"]*)" with default access information$/) do |name|
@@ -190,19 +193,6 @@ When(/^I give "([^"]*)" custom access information$/) do |office_name|
 
   fill_in "Body", with: "Custom body information"
   click_button "Save"
-end
-
-Then(/^I should see the custom access information on the public "([^"]*)" office page$/) do |office_name|
-  worldwide_organisation = WorldwideOrganisation.last
-  worldwide_office = WorldwideOffice.joins(contact: :translations).where(contact_translations: { title: office_name }).first
-  visit worldwide_organisation.public_path
-  within record_css_selector(worldwide_office) do
-    click_link "Access and opening times"
-  end
-
-  within ".body" do
-    expect(page).to have_content("Custom body information")
-  end
 end
 
 Then(/^I should see the updated default access information$/) do
