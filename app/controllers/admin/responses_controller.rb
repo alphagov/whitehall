b98e5d17080ccc4288761f6677f659e23a254d99
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-05-30 17:40:53 +0100

    Remove Bootstrap implementation of response pages
     Now 1.7 has been released we can cleanup the legacy code.
     This removes the views, associated partials, JS and styling for the
     response#edit and show pages

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -4,11 +4,10 @@ class Admin::ResponsesController < Admin::BaseController
   before_action :enforce_edition_permissions!
   before_action :prevent_modification_of_unmodifiable_edition
   before_action :find_response, only: %i[edit update]
-  layout :get_layout
+  layout "design_system"
 
   def show
     @response = response_class.find_by(edition_id: @edition) || response_class.new(published_on: Time.zone.today)
-    render_design_system("show", "show_legacy", next_release: true)
   end
 
   def create
@@ -17,34 +16,21 @@ class Admin::ResponsesController < Admin::BaseController
     if @response.save
       redirect_to [:admin, @edition, @response.singular_routing_symbol], notice: "#{@response.friendly_name.capitalize} saved"
     else
-      render_design_system("show", "show_legacy", next_release: true)
+      render :show
     end
   end
 
-  def edit
-    render_design_system("edit", "edit_legacy", next_release: true)
-  end
+  def edit; end
 
   def update
     if @response.update(response_params)
       redirect_to [:admin, @edition, @response.singular_routing_symbol], notice: "#{@response.friendly_name.capitalize} updated"
     else
-      render_design_system("edit", "edit_legacy", next_release: true)
+      render :edit
     end
   end
 
 private
-
-  def get_layout
-    design_system_actions = []
-    design_system_actions += %w[show create edit update] if preview_design_system?(next_release: true)
-
-    if design_system_actions.include?(action_name)
-      "design_system"
-    else
-      "admin"
-    end
-  end
 
   def find_consultation
     @edition = Consultation.find(params[:consultation_id])
