4faf9e6ca089501b960e9e91954ff73c4da257fb
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-05-30 17:12:55 +0100

    Remove Bootstrap implementation of dashboard index page
    Now 1.7 has been released we can cleanup the legacy code.
    This removes the view, associated partial and styling for the dashboard
    index page.

--- features/step_definitions/admin_dashboard_steps.rb
+++ features/step_definitions/admin_dashboard_steps.rb
@@ -3,34 +3,13 @@ When(/^I visit the admin dashboard$/) do
 end
 
 Then(/^I should see the draft document "([^"]*)"$/) do |title|
-  if using_design_system?
-    expect(all(".govuk-table")[0].all(".govuk-table__cell")[0].text).to eq title
-  else
-    edition = Edition.find_by!(title:).document.latest_edition
-    expect(page).to have_selector(".draft-documents #{record_css_selector(edition)}")
-  end
+  expect(all(".govuk-table")[0].all(".govuk-table__cell")[0].text).to eq title
 end
 
 Then(/^I should see the force published document "([^"]*)"$/) do |title|
-  if using_design_system?
-    expect(all(".govuk-table")[1].all(".govuk-table__cell")[0].text).to eq title
-  else
-    edition = Edition.find_by!(title:).document.latest_edition
-    expect(page).to have_selector(".force-published-documents #{record_css_selector(edition)}")
-  end
+  expect(all(".govuk-table")[1].all(".govuk-table__cell")[0].text).to eq title
 end
 
 Then(/^I should see a link to the content data app$/) do
-  ## We track all external link clicks in the design_system layout
-  ## so we don't need to check it for when using the design system.
-
-  if using_design_system?
-    expect(find_link("Content Data", href: "https://content-data.test.gov.uk/content")).not_to be_nil
-  else
-    link = find_link("Content Data", href: "https://content-data.test.gov.uk/content")
-
-    expect("external-link-clicked").to eq(link["data-track-category"])
-    expect("https://content-data.test.gov.uk/content").to eq(link["data-track-action"])
-    expect("Content Data").to eq(link["data-track-label"])
-  end
+  expect(find_link("Content Data", href: "https://content-data.test.gov.uk/content")).not_to be_nil
 end
