784feeaed6d44c677ee7a57942908970765ee7ba
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-06-08 12:03:04 +0100

    Ensure integration tests test Boostrap and GDS Implementation
    This duplicates a couple of integration tests that failed when we
    overrode the preview_design_system? method to always return true
    for pages going out in the 1.9 release.
    
    It also fixes an issue on the world location translation edit page
    where divs were being closed inside the form despite being opened
    before it.
    
    This caused some bizarre behaviour where the id of the world location
    news was not being passed to the controller in integration tests despite
    working on production.

--- test/integration/world_location_integration_test.rb
+++ test/integration/world_location_integration_test.rb
@@ -10,9 +10,9 @@ class WorldLocationIntegrationTest < ActionDispatch::IntegrationTest
 
   before do
     organisation = create(:organisation)
-    managing_editor = create(:managing_editor, organisation:, uid: "user-uid")
+    @managing_editor = create(:managing_editor, organisation:, uid: "user-uid")
 
-    login_as managing_editor
+    login_as @managing_editor
   end
 
   setup do
@@ -115,6 +115,22 @@ class WorldLocationIntegrationTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "when updating a world location news with translations, does not make any calls to search api with design system" do
+    @managing_editor.permissions << "Preview next release"
+
+    Sidekiq::Testing.inline! do
+      visit admin_world_location_news_path(@world_location_news)
+      click_link "Translations"
+      click_link "Edit fr"
+      new_mission_statement = "un mission différent"
+      fill_in "world_location_news_mission_statement", with: new_mission_statement
+
+      Whitehall::FakeRummageableIndex.any_instance.expects(:add).never
+
+      click_on "Save"
+    end
+  end
+
   test "when updating the english news page, other translations retains their original values" do
     Sidekiq::Testing.inline! do
       visit edit_admin_world_location_news_path(@world_location_news)
@@ -139,6 +155,27 @@ class WorldLocationIntegrationTest < ActionDispatch::IntegrationTest
       visit admin_world_location_news_path(@world_location_news)
       click_link "Translations"
       click_link "Français"
+      new_mission_statement = "un mission différent"
+      fill_in "world_location_news_mission_statement", with: new_mission_statement
+      new_title = "un titre différent"
+      fill_in "Title", with: new_title
+
+      PresentPageToPublishingApi.any_instance.stubs(:publish).with(PublishingApi::EmbassiesIndexPresenter)
+      Services.publishing_api.expects(:put_content).once.with(@world_location_news.content_id, put_content_hash_containing("en", @original_english_title, @original_english_mission_statement))
+      Services.publishing_api.expects(:put_content).at_least_once.with(@world_location_news.content_id, put_content_hash_containing("fr", new_title, new_mission_statement))
+      Services.publishing_api.expects(:publish).at_least_once
+
+      click_on "Save"
+    end
+  end
+
+  test "when updating a non-english news page, the english version retains its original values with design system" do
+    @managing_editor.permissions << "Preview next release"
+
+    Sidekiq::Testing.inline! do
+      visit admin_world_location_news_path(@world_location_news)
+      click_link "Translations"
+      click_link "Edit fr"
       new_mission_statement = "un mission différent"
       fill_in "world_location_news_mission_statement", with: new_mission_statement
       new_title = "un titre différent"
