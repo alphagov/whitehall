ab8d8c9123328ef824485679046abd686fc817e7
Joseph Kempster <jkempster34@gmail.com>
2023-06-12 11:52:51 +0100

    Republish Worldwide Organisation on Corporate Information Pages changes
    Currently, Organisations are republished when a related Corporate Information
    Pages changes, but Worldwide Organisations are not. Now that we publish
    Worldwide Corporate Information Pages, we want to change this.
    
    This is important because the Worldwide Organisation content item surfaces
    information about related Corporate Information Pages on it's `details` rather
    than it's `links` (though the links are present as well, they aren't used. This
    is consistent with the Organisation content item).

--- app/models/corporate_information_page.rb
+++ app/models/corporate_information_page.rb
@@ -2,7 +2,7 @@ class CorporateInformationPage < Edition
   include ::Attachable
   include Searchable
 
-  after_commit :republish_organisation_to_publishing_api
+  after_commit :republish_owning_organisation_to_publishing_api
   after_commit :republish_about_page_to_publishing_api, unless: :about_page?
   after_save :reindex_organisation_in_search_index, if: :about_page?
 
@@ -31,8 +31,8 @@ class CorporateInformationPage < Edition
   scope :with_organisation_govuk_status, ->(status) { joins(:organisation).where(organisations: { govuk_status: status }) }
   scope :accessible_documents_policy, -> { where(corporate_information_page_type_id: CorporateInformationPageType::AccessibleDocumentsPolicy.id) }
 
-  def republish_organisation_to_publishing_api
-    Whitehall::PublishingApi.republish_async(owning_organisation) if owning_organisation.is_a?(Organisation)
+  def republish_owning_organisation_to_publishing_api
+    Whitehall::PublishingApi.republish_async(owning_organisation) if owning_organisation.present?
   end
 
   def republish_about_page_to_publishing_api
