e76416c09b220468c133671074c896e1a07a7266
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-06-19 11:31:35 +0100

    Stringify keys in EditionAuthBypassUpdater
    Sidekiq is blowing up when a new shareable preview link
    is generated which has images or attachments.
    
    This is due to an arg being passed using a hash with
    symbols rather than strings.
    
    Stringifying the keys resolves this issue.

--- app/services/edition_auth_bypass_updater.rb
+++ app/services/edition_auth_bypass_updater.rb
@@ -22,7 +22,7 @@ private
   def update_file_attachments(edition)
     return unless edition.respond_to?(:attachments) && edition.attachments.files.present?
 
-    new_attributes = { auth_bypass_ids: [edition.auth_bypass_id] }
+    new_attributes = { "auth_bypass_ids" => [edition.auth_bypass_id] }
 
     edition.attachments.files.each do |file_attachment|
       attachment_data_id = file_attachment.attachment_data.id
@@ -33,7 +33,7 @@ private
   def update_image_attachments(edition)
     edition.images.each do |image|
       image_data_id = image.image_data.id
-      new_attributes = { auth_bypass_ids: [edition.auth_bypass_id] }
+      new_attributes = { "auth_bypass_ids" => [edition.auth_bypass_id] }
       AssetManagerUpdateWhitehallAssetWorker.perform_async_in_queue("asset_manager_updater", "ImageData", image_data_id, new_attributes)
     end
   end
@@ -44,14 +44,14 @@ private
     if edition.consultation_participation&.consultation_response_form.present?
       response_form = edition.consultation_participation.consultation_response_form
       response_form_data_id = response_form.consultation_response_form_data.id
-      new_attributes = { auth_bypass_ids: [edition.auth_bypass_id] }
+      new_attributes = { "auth_bypass_ids" => [edition.auth_bypass_id] }
 
       AssetManagerUpdateWhitehallAssetWorker.perform_async_in_queue("asset_manager_updater", "ConsultationResponseFormData", response_form_data_id, new_attributes)
     end
 
     if edition.outcome.present?
       edition.outcome.attachments.files.each do |file_attachment|
-        new_attributes = { auth_bypass_ids: [edition.auth_bypass_id] }
+        new_attributes = { "auth_bypass_ids" => [edition.auth_bypass_id] }
         attachment_data_id = file_attachment.attachment_data.id
         AssetManagerUpdateWhitehallAssetWorker.perform_async_in_queue("asset_manager_updater", "AttachmentData", attachment_data_id, new_attributes)
       end
@@ -59,7 +59,7 @@ private
 
     if edition.public_feedback.present?
       edition.public_feedback.attachments.files.each do |file_attachment|
-        new_attributes = { auth_bypass_ids: [edition.auth_bypass_id] }
+        new_attributes = { "auth_bypass_ids" => [edition.auth_bypass_id] }
         attachment_data_id = file_attachment.attachment_data.id
         AssetManagerUpdateWhitehallAssetWorker.perform_async_in_queue("asset_manager_updater", "AttachmentData", attachment_data_id, new_attributes)
       end
