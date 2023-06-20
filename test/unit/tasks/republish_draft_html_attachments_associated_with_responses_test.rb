7a6bd2f744e849fb78bfb5b98c5d3afb033eeb62
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2023-06-20 10:35:38 +0100

    Fix a spelling mistake in rake task

--- test/unit/tasks/republish_draft_html_attachments_associated_with_responses_test.rb
+++ test/unit/tasks/republish_draft_html_attachments_associated_with_responses_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 require "rake"
 
-class RepublishDraftHtmlAttachmentsWithAssoicatedResponsesRake < ActiveSupport::TestCase
+class RepublishDraftHtmlAttachmentsWithAssociatedResponsesRake < ActiveSupport::TestCase
   test "it republishes documents with an associated draft response which has an html attachment" do
     draft_consultation_with_outcome = create(:draft_consultation)
     draft_outcome = create(:consultation_outcome, consultation: draft_consultation_with_outcome)
