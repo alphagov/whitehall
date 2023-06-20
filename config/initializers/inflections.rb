e58c35f5b88e7ef21d6480a96af43160dee7060b
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2023-06-20 14:18:54 +0100

    Add call for evidence custom inflection
    The plural for of call for evidence should be calls for evidence, which
    is not what the standard rails pluralisation rules give me.
    
    We have added a custom inflection so we can name and load our files
    correctly and so the pluralising will be correct on the frontend.
    
    Co-authored-by: Minno Dang <minno.dang@digital.cabinet-office.gov.uk>
    Co-authored-by: Barbara Slawinska <barbara.slawinska@digital.cabinet-office.gov.uk>

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -9,9 +9,10 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   #   inflect.irregular 'person', 'people'
   #   inflect.uncountable %w( fish sheep )
   inflect.acronym "FOI"
-
   inflect.plural %r{^(Minister)}, '\0s'
   inflect.plural %r{^Chancellor}, '\0s'
+  inflect.plural %r{\A(call)(.*evidence)\z}i, '\1s\2'
+  inflect.singular %r{\A(call)s(.*evidence)\z}i, '\1\2'
 end
 
 # These inflection rules are supported but not enabled by default:
