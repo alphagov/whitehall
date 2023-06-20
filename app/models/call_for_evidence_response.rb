421cf459209e96f68a9d22a5631b21131c15872e
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2023-06-20 14:21:19 +0100

    Create models for responses to call for evidence
    Creates the basic model for call_for_evidence_responses this is a
    prerequisite for create call_for_evidence_outcomes. The model follows
    the functionality in consultation_responses as we are mirroring the
    behaviour.
    
    Co-authored-by: Minno Dang <minno.dang@digital.cabinet-office.gov.uk>
    Co-authored-by: Barbara Slawinska <barbara.slawinska@digital.cabinet-office.gov.uk>

---
+++ app/models/call_for_evidence_response.rb
@@ -0,0 +1,53 @@
+class CallForEvidenceResponse < ApplicationRecord
+  include Attachable
+
+  belongs_to :call_for_evidence, foreign_key: :edition_id
+
+  validates :published_on, recent_date: true
+  validates :summary, presence: { unless: :has_attachments }
+  validates_with SafeHtmlValidator
+  validates_with NoFootnotesInGovspeakValidator, attribute: :summary
+  delegate :auth_bypass_id, to: :call_for_evidence
+
+  def access_limited_object
+    call_for_evidence
+  end
+
+  delegate :access_limited?, to: :parent_attachable
+
+  delegate :organisations, to: :parent_attachable
+
+  delegate :alternative_format_contact_email, to: :call_for_evidence
+
+  delegate :publicly_visible?, to: :parent_attachable
+
+  delegate :accessible_to?, to: :parent_attachable
+
+  delegate :unpublished?, to: :parent_attachable
+
+  delegate :unpublished_edition, to: :parent_attachable
+
+  def can_order_attachments?
+    true
+  end
+
+  def allows_html_attachments?
+    true
+  end
+
+  def path_name
+    to_model.class.name.underscore
+  end
+
+  delegate :public_timestamp, :first_published_version?, :slug, :document, :images, :content_id, to: :call_for_evidence
+
+private
+
+  def has_attachments
+    attachments.any?
+  end
+
+  def parent_attachable
+    call_for_evidence || Attachable::Null.new
+  end
+end
