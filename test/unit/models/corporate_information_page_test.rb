7c5f793c56fc47e4bd6aa07839885d4387fc9b2b
Joseph Kempster <jkempster34@gmail.com>
2023-06-26 11:26:22 +0100

    Update the base path of the Worldwide Corporate Information about pages
    The worldwide corporate information about pages differ from the organisation
    corporate information about pages in that they are not rendered on a separate
    page.
    
    Currently, we attempt to prevent worldwide corporate information about pages
    from being published at all. When they do get published, they raise an error as
    the base_path conflicts with the existing content item for the worldwide
    organisation itself.
    
    Instead, we have decided to intentionally publish all worldwide corporate
    information about pages as `placeholder` items with the base_path
    `about/about`. This means we can avoid the conflicting base_path errors that
    occur when they are published.
    
    This:
    - Updates the base_path for corporate information pages as described above.
    - Removes a guard statement that was added some time ago in an attempt to
    prevent worldwide corporate information about pages from being published.

--- test/unit/models/corporate_information_page_test.rb
+++ test/unit/models/corporate_information_page_test.rb
@@ -26,8 +26,50 @@ class CorporateInformationPageTest < ActiveSupport::TestCase
     assert_not build(:corporate_information_page).previously_published
   end
 
-  test "base path is nil when neither organisation or worldwide organisation is present" do
+  test "base_path is nil when neither organisation or worldwide organisation is present" do
     corporate_information_page = create(:corporate_information_page, organisation: nil, worldwide_organisation: nil)
     assert_nil corporate_information_page.base_path
+  end
+
+  test "base_path appends the Corporate Information Page path to the associated Organisation base_path" do
+    organisation = create(:organisation)
+    corporate_information_page = create(
+      :corporate_information_page,
+      organisation:,
+    )
+
+    assert_equal "/government/organisations/#{organisation.name}/about/#{corporate_information_page.slug}", corporate_information_page.base_path
+  end
+
+  test "base_path appends /about to the associated Organisation base_path when about page" do
+    organisation = create(:organisation)
+    corporate_information_page = create(
+      :about_corporate_information_page,
+      organisation:,
+    )
+
+    assert_equal "/government/organisations/#{organisation.name}/about", corporate_information_page.base_path
+  end
+
+  test "base_path appends Corporate Information Page path to the associated WorldwideOrganisation base_path" do
+    worldwide_organisation = create(:worldwide_organisation)
+    corporate_information_page = create(
+      :corporate_information_page,
+      organisation: nil,
+      worldwide_organisation:,
+    )
+
+    assert_equal "/world/organisations/#{worldwide_organisation.name}/about/#{corporate_information_page.slug}", corporate_information_page.base_path
+  end
+
+  test "#base_path appends /about/about to WorldwideOrganisation base_path when about page" do
+    worldwide_organisation = create(:worldwide_organisation)
+    corporate_information_page = create(
+      :about_corporate_information_page,
+      organisation: nil,
+      worldwide_organisation:,
+    )
+
+    assert_equal "/world/organisations/#{worldwide_organisation.name}/about/about", corporate_information_page.base_path
   end
 end
