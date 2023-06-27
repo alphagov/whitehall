66a4af510c64681c0b23731c794d4d87462be49d
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-06-27 16:44:15 +0100

    Extend translated trait to social media accounts factory

--- test/factories/social_media_accounts.rb
+++ test/factories/social_media_accounts.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :social_media_account do
+  factory :social_media_account, traits: [:translated] do
     social_media_service
     url { "http://example.com" }
   end
