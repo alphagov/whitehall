0f9d0aeb9b5720ed5ca8f453901a503977b6df11
jkempster34 <47089130+jkempster34@users.noreply.github.com>
2023-06-30 16:07:12 +0100

    Revert "Switch rendering application for Worldwide Organisations"

--- app/presenters/publishing_api/worldwide_organisation_presenter.rb
+++ app/presenters/publishing_api/worldwide_organisation_presenter.rb
@@ -34,7 +34,7 @@ module PublishingApi
         },
         document_type: item.class.name.underscore,
         public_updated_at: item.updated_at,
-        rendering_app: Whitehall::RenderingApp::GOVERNMENT_FRONTEND,
+        rendering_app: Whitehall::RenderingApp::WHITEHALL_FRONTEND,
         schema_name: "worldwide_organisation",
       )
       content.merge!(PayloadBuilder::PolymorphicPath.for(item))
