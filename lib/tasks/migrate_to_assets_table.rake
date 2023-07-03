Havoooo/UK-Government

Created rake task for AttachmentData migration to Assets
In order to be able to populate Asset with Attachment details we
want to request asset_manager_id from asset-manager and save them along
with other detail like attachment_data_id. This rake task intends to do
this.

Co-authored-by: Iris Lau Iris.lau@digital.cabinet-office.gov.uk

lib/tasks/migrate_to_assets_table.rake
@@ -1,0 +1,32 @@
+desc "Create Asset relationship for AttachmentData"
+
+task create_asset_relationship: :environment do
+  def asset_manager
+    Services.asset_manager
+  end
+
+  def save_asset_id_to_assets(model_id, version, asset_manager_id)
+    asset = Asset.new(asset_manager_id:, attachment_data_id: model_id, version:)
+    asset.save!
+  end
+
+  def get_asset_manager_id(legacy_url_path)
+    path = legacy_url_path.sub(/^\//, "")
+    response_hash = asset_manager.whitehall_asset(path)
+    asset_manager_id = response_hash["id"]
+    asset_manager_id[/\/assets\/(.*)/, 1]
+  end
+
+  attachment_data = AttachmentData.all
+
+  attachment_data.each do |attachment|
+    asset_manager_id = get_asset_manager_id(attachment.file.path)
+    save_asset_id_to_assets(attachment.id, Asset.versions["original"], asset_manager_id)
+
+    if (attachment.pdf?)
+      asset_manager_id_for_thumbnail = get_asset_manager_id(attachment.file.thumbnail.asset_manager_path)
+      save_asset_id_to_assets(attachment.id, Asset.versions["thumbnail"], asset_manager_id_for_thumbnail)
+    end
+  end
+end
+

alphagov/whitehall

lib/tasks/migrate_to_assets_table.rake
@@ -1,0 +1,32 @@
+desc "Create Asset relationship for AttachmentData"
+
+task create_asset_relationship: :environment do
+  def asset_manager
+    Services.asset_manager
+  end
+
+  def save_asset_id_to_assets(model_id, variant, asset_manager_id)
+    asset = Asset.new(asset_manager_id:, attachment_data_id: model_id, variant:)
+    asset.save!
+  end
+
+  def get_asset_manager_id(legacy_url_path)
+    path = legacy_url_path.sub(/^\//, "")
+    response_hash = asset_manager.whitehall_asset(path)
+    asset_manager_id = response_hash["id"]
+    asset_manager_id[/\/assets\/(.*)/, 1]
+  end
+
+  attachment_data = AttachmentData.all
+
+  attachment_data.each do |attachment|
+    asset_manager_id = get_asset_manager_id(attachment.file.path)
+    save_asset_id_to_assets(attachment.id, Asset.variants["original"], asset_manager_id)
+
+    if (attachment.pdf?)
+      asset_manager_id_for_thumbnail = get_asset_manager_id(attachment.file.thumbnail.asset_manager_path)
+      save_asset_id_to_assets(attachment.id, Asset.variants["thumbnail"], asset_manager_id_for_thumbnail)
+    end
+  end
+end
+