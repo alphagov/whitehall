be2e67a086e3cf0bca73d3a9a57e7413896cf07c
syed-ali-tw <syed.ali@digital.cabinet-office.gov.uk>
2023-07-03 10:47:00 +0100

    Handle asset not found error and code refactor

--- app/workers/create_asset_relationship_worker.rb
+++ app/workers/create_asset_relationship_worker.rb
@@ -1,11 +1,10 @@
-
 class CreateAssetRelationshipWorker < WorkerBase
   def asset_manager
     Services.asset_manager
   end
 
-  def save_asset_id_to_assets(model_id, version, asset_manager_id)
-    asset = Asset.new(asset_manager_id:, attachment_data_id: model_id, version:)
+  def save_asset_id_to_assets(model_id, variant, asset_manager_id)
+    asset = Asset.new(asset_manager_id:, attachment_data_id: model_id, variant:)
     asset.save!
   end
 
@@ -18,23 +17,45 @@ class CreateAssetRelationshipWorker < WorkerBase
 
   def perform
     logger.info("CreateAssetRelationshipWorker start!")
-    attachment_data = AttachmentData.all
-    logger.info "no. of attachment data found: "+attachment_data.count.to_s
-    attachment_data.each do |attachment|
-      begin
-        path = attachment.file.path
-        asset_manager_id = get_asset_manager_id(path)
-        save_asset_id_to_assets(attachment.id, Asset.versions["original"], asset_manager_id)
+    counter_start = 2000
+    counter_end = 10000
+    attachment_data = AttachmentData.where(id:counter_start..counter_end)
+    logger.info "no. of attachment data found: #{attachment_data.count.to_s}"
+    logger.info "Creating Asset for #{counter_end} records  "
 
-        if (attachment.pdf?)
-          path = attachment.file.thumbnail.asset_manager_path
-          asset_manager_id_for_thumbnail = get_asset_manager_id(attachment.file.thumbnail.asset_manager_path)
-          save_asset_id_to_assets(attachment.id, Asset.versions["thumbnail"], asset_manager_id_for_thumbnail)
+    count = counter_start
+    attachment_data.each do |attachment|
+      break if count == counter_end
+
+      begin
+        path = save_asset_original(attachment)
+
+        if attachment.pdf?
+          path = save_asset_thumbnail(attachment)
         end
+
+        count += 1
       rescue GdsApi::HTTPNotFound
-          logger.warn "asset attachment id:#{attachment.id.to_s} not found in AM for legacy url path: "+ path
+        logger.warn "asset attachment id:#{attachment.id.to_s} not found in AM for legacy url path: #{path}"
       end
     end
+    logger.info("Created #{count} assets")
     logger.info("CreateAssetRelationshipWorker finish!")
+  end
+
+  private
+
+  def save_asset_thumbnail(attachment)
+    path = attachment.file.thumbnail.asset_manager_path
+    asset_manager_id_for_thumbnail = get_asset_manager_id(attachment.file.thumbnail.asset_manager_path)
+    save_asset_id_to_assets(attachment.id, Asset.variants["thumbnail"], asset_manager_id_for_thumbnail)
+    path
+  end
+
+  def save_asset_original(attachment)
+    path = attachment.file.path
+    asset_manager_id = get_asset_manager_id(path)
+    save_asset_id_to_assets(attachment.id, Asset.variants["original"], asset_manager_id)
+    path
   end
 end
