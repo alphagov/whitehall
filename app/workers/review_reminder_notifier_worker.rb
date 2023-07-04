25eb40d03ca4b5d495583287257033ea9d429fff
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-07-04 14:13:05 +0100

    Add worker to send review reminders async

---
+++ app/workers/review_reminder_notifier_worker.rb
@@ -0,0 +1,18 @@
+class ReviewReminderNotifierWorker < WorkerBase
+  def perform(id)
+    review_reminder = ReviewReminder.find(id)
+    return if review_reminder.reminder_sent_at.present?
+
+    edition = review_reminder.document.latest_edition
+    email_address = review_reminder.email_address
+
+    ActiveRecord::Base.transaction do
+      MailNotifications.review_reminder(
+        edition,
+        recipient_address: email_address,
+      ).deliver_now
+
+      review_reminder.update!(reminder_sent_at: Time.zone.now)
+    end
+  end
+end
