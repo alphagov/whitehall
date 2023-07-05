Havoooo/UK-Government

Introduce use_non_legacy_endpoints flag in attachments controller
The USE_NON_LEGACY_ENDPOINTS permission on the current user is used to set
a flag on the AttachmentData at the controller level.

The AttachmentData instance is then checked in the asset_manager_storage
and if the flag is true we then save an Asset for the corresponding file
the `store!` method has been called with.

test/functional/admin/attachments_controller_test.rb
@@ -136,8 +136,8 @@ class Admin::AttachmentsControllerTest < ActionController::TestCase
     attachment = valid_html_attachment_params
 
     Whitehall::PublishingApi
-    .expects(:save_draft)
-    .with(@edition)
+      .expects(:save_draft)
+      .with(@edition)
 
     Whitehall::PublishingApi
       .expects(:save_draft)
@@ -150,8 +150,8 @@ class Admin::AttachmentsControllerTest < ActionController::TestCase
     attachment = valid_file_attachment_params
 
     Whitehall::PublishingApi
-    .expects(:save_draft)
-    .with(@edition)
+      .expects(:save_draft)
+      .with(@edition)
 
     Whitehall::PublishingApi
       .expects(:save_draft)
@@ -169,10 +169,20 @@ class Admin::AttachmentsControllerTest < ActionController::TestCase
     assert_equal 0, attachable.reload.attachments.size
   end
 
-  test "POST :create triggers a job to be queued to store the attachment in Asset Manager" do
+  test "use_non_legacy_endpoints is false - POST :create triggers a job to be queued to store the attachment in Asset Manager" do
     attachment = valid_file_attachment_params
 
-    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, anything, anything, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+
+    post :create, params: { edition_id: @edition.id, type: "file", attachment: }
+  end
+
+  test "use_non_legacy_endpoints is true - POST :create triggers a job to be queued to store the attachment in Asset Manager" do
+    login_as_use_non_legacy_endpoints_user :gds_editor
+    attachment = valid_file_attachment_params
+    version = Asset.versions[:original]
+
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, kind_of(Integer), version, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
 
     post :create, params: { edition_id: @edition.id, type: "file", attachment: }
   end
@@ -344,10 +354,29 @@ class Admin::AttachmentsControllerTest < ActionController::TestCase
         }
   end
 
-  test "PUT :update with a file triggers a job to be queued to store the attachment in Asset Manager" do
+  test "use_non_legacy_endpoints is false - PUT :update with a file triggers a job to be queued to store the attachment in Asset Manager" do
     attachment = create(:file_attachment, attachable: @edition)
 
-    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, anything, anything, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+
+    put :update,
+        params: {
+          edition_id: @edition,
+          id: attachment.id,
+          attachment: {
+            attachment_data_attributes: {
+              file: upload_fixture("whitepaper.pdf"),
+            },
+          },
+        }
+  end
+
+  test "use_non_legacy_endpoints is true -  PUT :update with a file triggers a job to be queued to store the attachment in Asset Manager" do
+    login_as_use_non_legacy_endpoints_user :gds_editor
+    attachment = create(:file_attachment, attachable: @edition)
+    version = Asset.versions[:original]
+
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, kind_of(Integer), version, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
 
     put :update,
         params: {

test/unit/whitehall/asset_manager_storage_test.rb
@@ -117,29 +117,54 @@ class Whitehall::AssetManagerStorageTest < ActiveSupport::TestCase
   describe "invokes worker with necessary arguments to create an asset record" do
     context "attachment is a file" do
       setup do
-        @model = AttachmentData.new(id: 55, file: @file)
+        file = File.open(fixture_path.join("greenpaper.pdf"))
+        @model = build(:attachment_data, file:)
         @uploader.stubs(:model).returns(@model)
       end
 
-      test "calls worker with model_id and default origin asset_version" do
-        version = Asset.versions[:original]
+      context "use_non_legacy_endpoints permission is true" do
+        setup do
+          @model.use_non_legacy_endpoints = true
+        end
 
-        AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, version, anything, anything, anything, anything)
+        test "calls worker with model_id and default origin asset_version" do
+          version = Asset.versions[:original]
 
-        @uploader.store!(@file)
+          AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, version, anything, anything, anything, anything)
+
+          @uploader.store!(@file)
+        end
+
+        test "calls worker with model_id and version/thumbnail asset_version" do
+          version = Asset.versions[:thumbnail]
+          @uploader.stubs(:store_path).returns("asset-path/thumbnail_file_name")
+
+          AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, version, anything, anything, anything, anything)
+
+          @uploader.store!(@file)
+        end
       end
 
-      test "calls worker with model_id and version/thumbnail asset_version" do
-        version = Asset.versions[:thumbnail]
-        @uploader.stubs(:store_path).returns("asset-path/thumbnail_file_name")
+      context "use_non_legacy_endpoints permission is false" do
+        setup do
+          @model.use_non_legacy_endpoints = false
+        end
 
-        AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, version, anything, anything, anything, anything)
+        test "calls worker with nil model_id and asset_version" do
+          AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, nil, nil, anything, anything)
 
-        @uploader.store!(@file)
+          @uploader.store!(@file)
+        end
       end
     end
 
     context "attachment is not a file" do
+      setup do
+        file = File.open(fixture_path.join("big-cheese.960x640.jpg"))
+        @model = build(:image_data, file:)
+        @uploader.stubs(:model).returns(@model)
+      end
+
       test "calls worker with nil model_id and asset_version" do
         AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, nil, nil, anything, anything)
 

alphagov/whitehall

Co-authored-by: Syed Ali syed.ali@digital.cabinet-office.gov.uk

test/functional/admin/attachments_controller_test.rb
@@ -169,10 +169,20 @@ class Admin::AttachmentsControllerTest < ActionController::TestCase
     assert_equal 0, attachable.reload.attachments.size
   end
 
-  test "POST :create triggers a job to be queued to store the attachment in Asset Manager" do
+  test "use_non_legacy_endpoints is false - POST :create triggers a job to be queued to store the attachment in Asset Manager" do
     attachment = valid_file_attachment_params
 
-    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, anything, anything, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+
+    post :create, params: { edition_id: @edition.id, type: "file", attachment: }
+  end
+
+  test "use_non_legacy_endpoints is true - POST :create triggers a job to be queued to store the attachment in Asset Manager" do
+    login_as_use_non_legacy_endpoints_user :gds_editor
+    attachment = valid_file_attachment_params
+    variant = Asset.variants[:original]
+
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, kind_of(Integer), variant, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
 
     post :create, params: { edition_id: @edition.id, type: "file", attachment: }
   end
@@ -344,10 +354,29 @@ class Admin::AttachmentsControllerTest < ActionController::TestCase
         }
   end
 
-  test "PUT :update with a file triggers a job to be queued to store the attachment in Asset Manager" do
+  test "use_non_legacy_endpoints is false - PUT :update with a file triggers a job to be queued to store the attachment in Asset Manager" do
     attachment = create(:file_attachment, attachable: @edition)
 
-    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, anything, anything, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
+
+    put :update,
+        params: {
+          edition_id: @edition,
+          id: attachment.id,
+          attachment: {
+            attachment_data_attributes: {
+              file: upload_fixture("whitepaper.pdf"),
+            },
+          },
+        }
+  end
+
+  test "use_non_legacy_endpoints is true -  PUT :update with a file triggers a job to be queued to store the attachment in Asset Manager" do
+    login_as_use_non_legacy_endpoints_user :gds_editor
+    attachment = create(:file_attachment, attachable: @edition)
+    variant = Asset.variants[:original]
+
+    AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, kind_of(Integer), variant, anything, @edition.class.to_s, @edition.id, [@edition.auth_bypass_id])
 
     put :update,
         params: {

test/unit/whitehall/asset_manager_storage_test.rb
@@ -121,25 +121,48 @@ class Whitehall::AssetManagerStorageTest < ActiveSupport::TestCase
         @uploader.stubs(:model).returns(@model)
       end
 
-      test "calls worker with model_id and default origin asset_variant" do
-        variant = Asset.variants[:original]
+      context "use_non_legacy_endpoints permission is true" do
+        setup do
+          @model.use_non_legacy_endpoints = true
+        end
 
-        AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, variant, anything, anything, anything, anything)
+        test "calls worker with model_id and default origin asset_variant" do
+          variant = Asset.variants[:original]
 
-        @uploader.store!(@file)
+          AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, variant, anything, anything, anything, anything)
+
+          @uploader.store!(@file)
+        end
+
+        test "calls worker with model_id and variant/thumbnail asset_variant" do
+          variant = Asset.variants[:thumbnail]
+          @uploader.stubs(:store_path).returns("asset-path/thumbnail_file_name")
+
+          AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, variant, anything, anything, anything, anything)
+
+          @uploader.store!(@file)
+        end
       end
 
-      test "calls worker with model_id and variant/thumbnail asset_variant" do
-        variant = Asset.variants[:thumbnail]
-        @uploader.stubs(:store_path).returns("asset-path/thumbnail_file_name")
+      context "use_non_legacy_endpoints permission is false" do
+        setup do
+          @model.use_non_legacy_endpoints = false
+        end
 
-        AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, @model.id, variant, anything, anything, anything, anything)
+        test "calls worker with nil model_id and asset_variant" do
+          AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, nil, nil, anything, anything)
 
-        @uploader.store!(@file)
+          @uploader.store!(@file)
+        end
       end
     end
 
     context "attachment is not a file" do
+      setup do
+        model = create(:image_data)
+        @uploader.stubs(:model).returns(model)
+      end
+
       test "calls worker with nil model_id and asset_variant" do
         AssetManagerCreateWhitehallAssetWorker.expects(:perform_async).with(anything, anything, nil, nil, nil, nil, anything, anything)
 