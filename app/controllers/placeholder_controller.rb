be4069d930d60c7974b8baf919c03b642cb4df0c
Cristina <579522+CristinaRO@users.noreply.github.com>
2023-07-18 13:50:58 +0100

    Remove code for rendering /government/placeholder

--- app/controllers/placeholder_controller.rb
+++
@@ -1,9 +0,0 @@
-class PlaceholderController < PublicFacingController
-  include ActionView::Helpers::AssetTagHelper
-
-  def show; end
-
-  def placeholder_image
-    redirect_to view_context.path_to_image("thumbnail-placeholder.png")
-  end
-end
