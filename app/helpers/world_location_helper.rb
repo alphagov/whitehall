069b7fc0c46416e52c6667572a1adf65926cd07f
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-07-24 16:40:12 +0100

    Add `sort` method to `WorldLocationHelper`
    This will allow us to get a sorted list of World Locations without them
    being grouped by first letter.

--- app/helpers/world_location_helper.rb
+++ app/helpers/world_location_helper.rb
@@ -7,4 +7,11 @@ module WorldLocationHelper
       .sort
       .map { |(letter, names_and_locations)| [letter, names_and_locations.map { |_transliterated_name, location| location }] }
   end
+
+  def sort(locations)
+    locations
+      .map { |location| [ActiveSupport::Inflector.transliterate(location.name_without_prefix), location] }
+      .sort_by { |transliterated_name, _location| transliterated_name }
+      .map { |_transliterated_name, location| location }
+  end
 end
--- test/unit/app/helpers/world_location_helper_test.rb
+++ test/unit/app/helpers/world_location_helper_test.rb
@@ -58,4 +58,45 @@ class WorldLocationHelperTest < ActionView::TestCase
       assert_equal expected_order, group_and_sort(unsorted)
     end
   end
+
+  context "#sort" do
+    test "sorts correctly where special characters are used anywhere other than the first character" do
+      world_location_1 = create(:world_location, name: "Cow")
+      world_location_2 = create(:world_location, name: "Côt")
+      unsorted = [world_location_1, world_location_2]
+
+      expected_order = [
+        world_location_2,
+        world_location_1,
+      ]
+
+      assert_equal expected_order, sort(unsorted)
+    end
+
+    test "sorts correctly where special characters are used as the first character" do
+      world_location_1 = create(:world_location, name: "England")
+      world_location_2 = create(:world_location, name: "Éire")
+      unsorted = [world_location_1, world_location_2]
+
+      expected_order = [
+        world_location_2,
+        world_location_1,
+      ]
+
+      assert_equal expected_order, sort(unsorted)
+    end
+
+    test "ignores `The` prefix when sorting" do
+      world_location_1 = create(:world_location, name: "Hungary")
+      world_location_2 = create(:world_location, name: "The Gambia")
+      unsorted = [world_location_1, world_location_2]
+
+      expected_order = [
+        world_location_2,
+        world_location_1,
+      ]
+
+      assert_equal expected_order, sort(unsorted)
+    end
+  end
 end
