5e6df851c64d1995f2496ea7a4b3e9de9e12e065
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-07-24 08:06:54 +0100

    Add topical events tabs to organisation features page
    This was missed when I ported over the org features page by accident.
    I've added a feature test as this should be tested.

--- app/helpers/admin/features_helper.rb
+++ app/helpers/admin/features_helper.rb
@@ -7,6 +7,14 @@ module Admin::FeaturesHelper
     end
   end
 
+  def featurable_topical_events_for_feature_list(featurable_topical_events, feature_list)
+    @featurable_topical_events_for_feature_list ||= featurable_topical_events.reject do |topical_event|
+      feature_list.features.current.detect do |feature|
+        feature.topical_event == topical_event
+      end
+    end
+  end
+
   def featurable_editions_for_feature_list(editions, feature_list)
     @featurable_editions_for_feature_list ||= editions.reject do |edition|
       localised_edition = LocalisedModel.new(edition, feature_list.locale)
