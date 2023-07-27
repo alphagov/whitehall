a1ac82f9b207fe54c1da145034f2bf1216ee9424
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-07-27 12:19:39 +0100

    Add confirm destroy worldwide office translation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -176,7 +176,9 @@ Whitehall::Application.routes.draw do
             end
             post :reorder_for_home_page, on: :collection
             resource :access_and_opening_time, path: "access_info", except: %i[index show new]
-            resources :translations, controller: "worldwide_office_translations", only: %i[create edit update destroy]
+            resources :translations, controller: "worldwide_office_translations", only: %i[create edit update destroy] do
+              get :confirm_destroy, on: :member
+            end
           end
           resources :corporate_information_pages do
             resources :translations, controller: "corporate_information_pages_translations"
