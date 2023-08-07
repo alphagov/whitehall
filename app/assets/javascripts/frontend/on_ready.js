85cfe3921467a0c769aa78a7931a3f9c5a9982dd
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2023-08-07 16:47:07 +0100

    Remove filter list items
    No longer used

--- app/assets/javascripts/frontend/on_ready.js
+++ app/assets/javascripts/frontend/on_ready.js
@@ -3,7 +3,6 @@ jQuery(function ($) {
   $('.govspeak').enhanceYoutubeVideoLinks()
 
   GOVUK.hideDepartmentChildren.init()
-  GOVUK.filterListItems.init()
   GOVUK.showHide.init()
 })
 // These want images to be loaded before they run so the page height doesn't change.
