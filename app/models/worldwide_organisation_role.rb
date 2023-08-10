9467da5fad05f02e471affa49178d918b93a4051
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-08-10 10:47:08 +0100

    Republish linked WorldwideOrganisation on role update
    When an assignment of a role to a Worldwide Organisation is updated, we
    need to republish the associated Worldwide Organisation to Publishing
    API.
    
    This was missed when we originally migrated these pages out of
    Whitehall.

--- app/models/worldwide_organisation_role.rb
+++ app/models/worldwide_organisation_role.rb
@@ -3,4 +3,11 @@ class WorldwideOrganisationRole < ApplicationRecord
   belongs_to :role
 
   validates :worldwide_organisation, :role, presence: true
+
+  after_save :republish_worldwide_organisation_to_publishing_api
+  after_destroy :republish_worldwide_organisation_to_publishing_api
+
+  def republish_worldwide_organisation_to_publishing_api
+    Whitehall::PublishingApi.republish_async(worldwide_organisation)
+  end
 end
