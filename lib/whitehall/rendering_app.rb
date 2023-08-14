a51052c15c763e5915d31b3470b11fe5bd2d6852
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-08-14 10:23:59 +0100

    Remove `WHITEHALL_FRONTEND` constant
    This is not being used anywhere.

--- lib/whitehall/rendering_app.rb
+++ lib/whitehall/rendering_app.rb
@@ -1,6 +1,5 @@
 module Whitehall
   module RenderingApp
-    WHITEHALL_FRONTEND = "whitehall-frontend".freeze
     GOVERNMENT_FRONTEND = "government-frontend".freeze
     COLLECTIONS_FRONTEND = "collections".freeze
   end
