b15c1fdaa02f3b4b8e0d7c315bd7fb35fee2a84b
davidtrussler <mail@davidtrussler.net>
2023-08-15 12:28:21 +0100

    Release-1.13 - updates the get_layout methods in the worldwide organisations controller for card-315-Details-page

--- test/functional/admin/legacy_worldwide_organisations_controller_test.rb
+++ test/functional/admin/legacy_worldwide_organisations_controller_test.rb
@@ -7,7 +7,6 @@ class Admin::LegacyWorldwideOrganisationsControllerTest < ActionController::Test
   end
 
   should_be_an_admin_controller
-  should_render_bootstrap_implementation_with_preview_next_release
 
   test "shows a list of worldwide organisations" do
     organisation = create(:worldwide_organisation)
