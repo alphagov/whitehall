1fcb3c42120d65bd3cb03be9c8f19746bc824261
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-08-17 12:28:23 +0100

    Add migration to backfill current_release_date_id
    This backfills the new column. Unfortunately, we need to use update_column
    here to skip validations as quite a few announcments are invalid.
    
    It may well be better anyway though as if we used update, we would
    essentially be bulk updating all announcments and sending them off to
    the Publishing API.

---
+++ db/data_migration/20230817104012_backfill_current_release_date_id.rb
@@ -0,0 +1,12 @@
+class BackfillCurrentReleaseDateId < ActiveRecord::Migration[7.0]
+  def change
+    count = 0
+    total = StatisticsAnnouncement.count
+    StatisticsAnnouncement.find_each do |statistics_announcement|
+      latest = statistics_announcement.statistics_announcement_dates.reverse_order
+      statistics_announcement.update_column(:current_release_date_id, latest.pick(:id))
+      count += 1
+      puts "#{count}/#{total} completed"
+    end
+  end
+end
