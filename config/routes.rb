62cd741d742f8eea64d015f8a279a675c1d6abf6
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-08-21 15:58:01 +0100

    Remove access and opening times endpoint
    This is no longer used as we've removed the ability for a worldwide
    organisation to have default access and opening times.

--- config/routes.rb
+++ config/routes.rb
@@ -145,7 +145,6 @@ Whitehall::Application.routes.draw do
           end
           get :reorder, on: :collection
           post :reorder_for_home_page, on: :collection
-          resource :access_and_opening_time, path: "access_info", except: %i[index show new]
           resources :translations, controller: "worldwide_office_translations", only: %i[create edit update destroy index] do
             get :confirm_destroy, on: :member
           end
