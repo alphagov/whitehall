8d8a3aabb46be0fec0b8f6c25ccdabd9c4b2dd7d
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2023-08-24 17:40:26 +0100

    Add ESLint config for JavaScript Standard Style
    Move from standardx to standard configured through `.estlintrc.js`. This updates us to the latest version of standard and opens the door for using prettier in future.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'standard',
+  ignorePatterns: [
+    'app/assets/javascripts/vendor/',
+    'app/assets/javascripts/admin_legacy/**/*',
+    'spec/javascripts/admin_legacy/**/*'
+  ],
+  env: {
+    browser: true,
+    jquery: true,
+    jasmine: true,
+    es6: true
+  },
+  globals: {
+    GOVUK: 'readonly',
+    GOVUKAdmin: 'readonly'
+  }
+}
