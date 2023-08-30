e41d3f0020d0c72e56d1726b664b6964c632b840
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-08-30 11:15:05 +0100

    Add document collection group memberships index route

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Whitehall::Application.routes.draw do
                    as: :members,
                    path: "members",
                    only: [:destroy]
+         resources :document_collection_group_memberships, path: "members", only: %i[index]
         end
         post "whitehall-member" => "document_collection_group_memberships#create_whitehall_member", as: :new_whitehall_member
         post "non-whitehall-member" => "document_collection_group_memberships#create_non_whitehall_member", as: :new_non_whitehall_member
