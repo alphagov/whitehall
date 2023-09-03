48701ad251c6004fe12757a92df0e73f36bec4ce
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2023-09-03 21:52:06 +0100

    Introduce factories for assets
    Assets are now created for AttachmentData (file attachments).
    These factories will replace the usual factories once the migration
    is done to populate assets for all attachments.
    
    Co-authored-by: Syed Ali syed.ali@digital.cabinet-office.gov.uk

--- test/factories/attachment_data.rb
+++ test/factories/attachment_data.rb
@@ -4,6 +4,25 @@ FactoryBot.define do
     uploaded_to_asset_manager_at { Time.zone.now }
   end
 
+  factory :attachment_data_with_assets, parent: :attachment_data do
+    use_non_legacy_endpoints { true }
+
+    after(:build) do |attachment_data|
+      attachment_data.assets << build(:asset, asset_manager_id: "asset_manager_id_original", variant: Asset.variants[:original])
+      attachment_data.assets << build(:asset, asset_manager_id: "asset_manager_id_thumbnail", variant: Asset.variants[:thumbnail])
+    end
+  end
+
+  factory :attachment_data_with_asset, class: AttachmentData do
+    file { File.open(Rails.root.join("test/fixtures/sample.docx")) }
+    uploaded_to_asset_manager_at { Time.zone.now }
+    use_non_legacy_endpoints { true }
+
+    after(:build) do |attachment_data|
+      attachment_data.assets << build(:asset, asset_manager_id: "asset_manager_id", variant: Asset.variants[:original])
+    end
+  end
+
   factory :image_attachment_data, parent: :attachment_data do
     file { File.open(Rails.root.join("test/fixtures/minister-of-funk.960x640.jpg")) }
   end
