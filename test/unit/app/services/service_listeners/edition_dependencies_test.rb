7beb54b354d96f9948bdad42d0b25bb6aed16115
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2023-09-22 10:10:57 +0100

    Remove tests of process for updating dependencies of unpublished editions
    Now that we require publishers to create a new edition if they want to republish an unpublished document, these tests no longer make sense

--- test/unit/app/services/service_listeners/edition_dependencies_test.rb
+++ test/unit/app/services/service_listeners/edition_dependencies_test.rb
@@ -52,49 +52,6 @@ class ServiceListeners::EditionDependenciesTest < ActiveSupport::TestCase
         assert Whitehall.edition_services.send(service_name, subsequent_edition_of_dependable_speech).perform!
       end
     end
-
-    # given a depended-upon edition is published, then unpublished.
-    # its title is updated and it's published, causing an updated slug.
-    # we need to republish the dependent edition to reflect the updated slug.
-    # NOTE: this doesn't cover the case where a subsequent edition of the depended-upon
-    # edition changes the title/slug, leaving an outdated slug in the dependent edition.
-    test "unpublishing a depended-upon edition and #{transition}ing it again should cause dependent editions to be republished" do
-      Sidekiq::Testing.inline! do
-        disable_publishes_to_publishing_api do
-          dependable_speech, dependent_article = create_article_dependent_on_speech
-
-          expect_publishing(dependable_speech)
-          expect_republishing(dependent_article)
-          assert Whitehall.edition_services.send(service_name, dependable_speech).perform!
-
-          dependable_speech.unpublishing = create(:unpublishing, edition: dependable_speech)
-
-          # stub unpublishing worker to avoid the extra save draft calls
-          PublishingApiUnpublishingWorker.stubs(:perform_async)
-          assert Whitehall.edition_services.unpublisher(dependable_speech).perform!
-
-          dependable_speech.title = "New speech title"
-          dependable_speech.submit!
-
-          expect_publishing(dependable_speech)
-          expect_republishing(dependent_article)
-          assert Whitehall.edition_services.send(service_name, dependable_speech).perform!
-        end
-      end
-    end
-  end
-
-  # no need to republish an unpublished edition when its dependencies change, so
-  test "unpublishing a depended-upon edition destroys links with its dependencies" do
-    edition = create(:published_news_article)
-    edition.depended_upon_contacts << create(:contact)
-    edition.depended_upon_editions << create(:speech)
-
-    edition.unpublishing = create(:unpublishing)
-    assert Whitehall.edition_services.unpublisher(edition).perform!
-
-    assert_empty edition.depended_upon_contacts.reload
-    assert_empty edition.depended_upon_editions.reload
   end
 
   def create_article_dependent_on_speech
