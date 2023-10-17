648d7fa8ca148f64a22bdc7a76e84ec088ecab05
Tuomas Nylund <tuomas.nylund@thoughtworks.com>
2023-10-17 17:49:59 +0100

    Delete assets when Asset is deleted from Asset Manager
    Except for AttachmentData because they are soft deleted in Whitehall and Asset Manager.
    
    Images and logos are considered less valuable resources than File attachments. They therefore
    do not carry the same level of traceability and recoverability requirements as Attachments.
    
    Whitehall deletes Image and Logos from the Asset table and considers the asset data mutable in Whitehall.
    
    Co-authored-by: Syed Ali <syed.ali@digital.cabinet-office.gov.uk>
    Co-authored-by: Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>

--- test/unit/app/workers/asset_manager_delete_asset_worker_test.rb
+++ test/unit/app/workers/asset_manager_delete_asset_worker_test.rb
@@ -2,14 +2,39 @@ require "test_helper"
 
 class AssetManagerDeleteAssetWorkerTest < ActiveSupport::TestCase
   setup do
-    @legacy_url_path = "legacy-url-path"
+    @asset = create(:image_data).assets.first
+    @asset_manager_id = @asset.asset_manager_id
     @worker = AssetManagerDeleteAssetWorker.new
-    @asset_manager_id = "asset_manager_id"
   end
 
   test "it calls AssetManager::AssetDeleter" do
-    AssetManager::AssetDeleter.expects(:call).with(@legacy_url_path, @asset_manager_id)
+    AssetManager::AssetDeleter.expects(:call).with("legacy-url-path", "any-asset-id")
 
-    @worker.perform(@legacy_url_path, @asset_manager_id)
+    @worker.perform("legacy-url-path", "any-asset-id")
+  end
+
+  test "it raises an error if the asset deletion fails for an unknown reason" do
+    expected_error = GdsApi::HTTPServerError.new(500)
+    AssetManager::AssetDeleter.expects(:call).raises(expected_error)
+
+    assert_raises(GdsApi::HTTPServerError) { @worker.perform(nil, @asset_manager_id) }
+  end
+
+  test "it deletes the Asset from Asset table" do
+    AssetManager::AssetDeleter.stubs(:call)
+
+    @worker.perform(nil, @asset_manager_id)
+
+    assert_not Asset.where(asset_manager_id: @asset_manager_id).exists?
+  end
+
+  test "it deletes the Asset from Asset table even if the asset is not found in Asset Manager" do
+    expected_error = AssetManager::ServiceHelper::AssetNotFound.new(@asset_manager_id)
+    AssetManager::AssetDeleter.expects(:call).raises(expected_error)
+    Logger.any_instance.stubs(:info).with("Asset #{@asset_manager_id} has already been deleted from Asset Manager")
+
+    @worker.perform(nil, @asset_manager_id)
+
+    assert_not Asset.where(asset_manager_id: @asset_manager_id).exists?
   end
 end
