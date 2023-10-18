77ea6b293a0c027bb69aaf5664db25447f2f8756
davidtrussler <mail@davidtrussler.net>
2023-10-18 17:24:26 +0100

    Add test for "more" page

---
+++ test/functional/admin/more_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class Admin::MoreControllerTest < ActionController::TestCase
+  setup do
+    login_as_preview_design_system_user :writer
+  end
+
+  view_test "GET #index renders the 'More' page with a correctly formatted list of links" do
+    get :index
+
+    assert_response :success
+    assert_select "h1.govuk-heading-xl", text: "More"
+    assert_select ".govuk-list"
+    assert_select "a.govuk-link", text: "Departments & agencies"
+  end
+end
