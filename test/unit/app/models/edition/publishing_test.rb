alphagov/whitehall

commit 4eb10f4d87c247a5be99e7cab3281d262dfcd3d8

Put unpublished editions into an unpublished state instead of reverting them to the draft state
This is intended to reduce confusion for users, who have reported that the behaviour does not match their expectations. The current behaviour also means that we have thousands of draft editions that we do not expect will ever be published again. This is inconsistent with other publishing apps.

In addition, the reversion to draft state makes it difficult to synchronise the content with the publishing API. Simply putting the edition into a dedicated "unpublished" state and requiring users to supersede the edition as usual makes the synchronisation logic much simpler, as we no longer need to check if the draft edition was once in the published state or send a new draft to the publishing API when we unpublish an edition.

app/models/edition/publishing.rb
@@ -38,13 +38,9 @@ module Edition::Publishing
   end
 
   def change_note_required?
-    if deleted? || superseded?
-      false
-    elsif draft? && new_record?
-      false
-    else
-      other_editions.published.exists?
-    end
+    return false if new_record?
+
+    draft? && previous_edition.present?
   end
 
   def change_note_present!
@@ -96,10 +92,6 @@ module Edition::Publishing
   def reset_version_numbers
     self.published_major_version = previous_edition.try(:published_major_version)
     self.published_minor_version = previous_edition.try(:published_minor_version)
-  end
-
-  def unpublished?
-    !publicly_visible? && unpublishing.present?
   end
 
   def unpublished_edition

test/unit/app/models/edition/publishing_test.rb
@@ -13,6 +13,12 @@ class Edition::PublishingChangeNoteTest < ActiveSupport::TestCase
     assert_not edition.valid?
   end
 
+  test "a draft is invalid without change note once saved if an unpublished edition already exists" do
+    unpublished_edition = create(:unpublished_edition)
+    edition = create(:draft_edition, change_note: nil, minor_change: false, document: unpublished_edition.document)
+    assert_not edition.valid?
+  end
+
   test "is valid without change note if no published edition already exists" do
     edition = create(:draft_edition, change_note: nil, minor_change: false)
     assert edition.valid?
@@ -125,34 +131,14 @@ class Edition::PublishingTest < ActiveSupport::TestCase
     assert edition.errors[:base].include?("This document has not been force-published")
   end
 
-  test "#unpublished? returns false if publicly visible" do
-    published_edition = build(:published_edition)
-
-    assert_not published_edition.unpublished?
-  end
-
-  test "#unpublished? returns false if no unpublishing exists" do
-    draft_edition = build(:draft_edition)
-
-    assert_not draft_edition.unpublished?
-  end
-
-  test "#unpublished? returns true if not publicly visible and unpublishing exists" do
-    unpublishing = build(:unpublishing)
-    draft_edition_with_unpublishing = build(:draft_edition, unpublishing:)
-
-    assert draft_edition_with_unpublishing.unpublished?
-  end
-
   test "#unpublished_edition returns nil if not unpublished" do
     edition = build(:published_edition)
 
     assert_nil edition.unpublished_edition
   end
 
-  test "#unpublished_edition returns unpublished edition if unpublished" do
-    edition = create(:draft_edition, :with_document)
-    edition.build_unpublishing(edition:)
+  test "#unpublished_edition returns itself if unpublished" do
+    edition = create(:unpublished_edition)
 
     assert_equal edition, edition.unpublished_edition
   end

app/components/admin/editions/show/sidebar_actions_component.rb
@@ -40,7 +40,7 @@ class Admin::Editions::Show::SidebarActionsComponent < ViewComponent::Base
 private
 
   def add_create_action
-    if @edition.is_latest_edition? && @edition.published?
+    if @edition.is_latest_edition? && @edition.can_supersede?
       actions << form_with(url: revise_admin_edition_path(@edition.id), method: :post, data: {
         module: "prevent-multiple-form-submissions",
       }) do

app/helpers/admin/editions_helper.rb
@@ -110,6 +110,7 @@ module Admin::EditionsHelper
       %w[Published published],
       ["Force published (not reviewed)", "force_published"],
       %w[Withdrawn withdrawn],
+      %w[Unpublished unpublished],
     ]
   end
 

app/models/edition.rb
@@ -58,10 +58,10 @@ class Edition < ApplicationRecord
   validates :first_published_at, inclusion: { in: proc { Date.parse("1900-01-01")..Time.zone.now }, message: "must be between 1/1/1900 and the present" }, if: :draft?, allow_blank: true
   validates :scheduled_publication, inclusion: { in: proc { Time.zone.now.. }, message: "must be in the future" }, if: :draft?, allow_blank: true
 
-  UNMODIFIABLE_STATES = %w[scheduled published superseded deleted].freeze
+  UNMODIFIABLE_STATES = %w[scheduled published superseded deleted unpublished].freeze
   FROZEN_STATES = %w[superseded deleted].freeze
   PRE_PUBLICATION_STATES = %w[draft submitted rejected scheduled].freeze
-  POST_PUBLICATION_STATES = %w[published superseded withdrawn].freeze
+  POST_PUBLICATION_STATES = %w[published superseded withdrawn unpublished].freeze
   PUBLICLY_VISIBLE_STATES = %w[published withdrawn].freeze
 
   scope :with_title_or_summary_containing,
@@ -142,7 +142,7 @@ class Edition < ApplicationRecord
     end
 
     def being_unpublished?(previous_state, current_state)
-      previous_state == "published" && %w[draft withdrawn].include?(current_state)
+      previous_state == "published" && %w[unpublished withdrawn].include?(current_state)
     end
   end
 
@@ -478,7 +478,7 @@ EXISTS (
       lock!
       if allow_creating_draft_from_deleted_edition
         raise "Edition not in the deleted state" unless deleted?
-      elsif !published?
+      elsif !can_supersede?
         raise "Cannot create new edition based on edition in the #{state} state"
       end
 

app/models/edition/workflow.rb
@@ -11,7 +11,7 @@ module Edition::Workflow
     end
 
     def valid_state?(state)
-      %w[active draft submitted rejected published scheduled force_published withdrawn not_published].include?(state)
+      %w[active draft submitted rejected published scheduled force_published withdrawn not_published unpublished].include?(state)
     end
   end
 
@@ -29,6 +29,7 @@ module Edition::Workflow
       state :superseded
       state :deleted
       state :withdrawn
+      state :unpublished
 
       event :delete do
         transitions from: %i[draft submitted rejected], to: :deleted
@@ -63,11 +64,11 @@ module Edition::Workflow
       end
 
       event :unpublish do
-        transitions from: %i[published draft], to: :draft
+        transitions from: %i[published unpublished], to: :unpublished
       end
 
       event :supersede, success: :destroy_associations_with_edition_dependencies_and_dependants do
-        transitions from: :published, to: :superseded
+        transitions from: %i[published unpublished], to: :superseded
       end
 
       event :withdraw do

app/services/edition_publisher.rb
@@ -43,7 +43,7 @@ private
 
   def editions_to_supersede
     edition.document.editions
-      .published
+      .where(state: %i[published unpublished])
       .excluding(edition)
   end
 

app/services/edition_unpublisher.rb
@@ -7,8 +7,6 @@ class EditionUnpublisher < EditionService
   def failure_reason
     @failure_reason ||= if !can_transition?
                           "An edition that is #{edition.current_state} cannot be #{past_participle}"
-                        elsif (other_edition = edition.other_editions.in_pre_publication_state.first)
-                          "There is already a #{other_edition.state} edition of this document. You must discard it before you can #{verb} this edition."
                         elsif edition.unpublishing.blank?
                           "The reason for unpublishing must be present"
                         elsif !edition.unpublishing.valid?

commit ec0514a63826715f53e8caf8e31ed3b1971eee99

Ensure change note input field is visible for all pre-publication documents
Users were reporting that the change note field was not visible on submitted documents. This change ensures that the change note input is visible on all pre publication documents, fixing a bug introduced in 4eb10f4.

app/models/edition/publishing.rb
@@ -40,7 +40,7 @@ module Edition::Publishing
   def change_note_required?
     return false if new_record?
 
-    draft? && previous_edition.present?
+    pre_publication? && previous_edition.present?
   end
 
   def change_note_present!

test/unit/app/models/edition/publishing_test.rb
@@ -13,6 +13,18 @@ class Edition::PublishingChangeNoteTest < ActiveSupport::TestCase
     assert_not edition.valid?
   end
 
+  test "a submitted edition is invalid without change note once saved if a published edition already exists" do
+    published_edition = create(:published_edition)
+    edition = create(:submitted_edition, change_note: nil, minor_change: false, document: published_edition.document)
+    assert_not edition.valid?
+  end
+
+  test "a rejected edition is invalid without change note once saved if a published edition already exists" do
+    published_edition = create(:published_edition)
+    edition = create(:rejected_edition, change_note: nil, minor_change: false, document: published_edition.document)
+    assert_not edition.valid?
+  end
+
   test "a draft is invalid without change note once saved if an unpublished edition already exists" do
     unpublished_edition = create(:unpublished_edition)
     edition = create(:draft_edition, change_note: nil, minor_change: false, document: unpublished_edition.document)