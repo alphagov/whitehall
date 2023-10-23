2185189888b2a283aed78ccab6e353b2203d9927
davidtrussler <mail@davidtrussler.net>
2023-10-23 11:17:12 +0100

    Add "forbidden" toggle for users without design_system permissions

--- app/controllers/admin/more_controller.rb
+++ app/controllers/admin/more_controller.rb
@@ -1,5 +1,22 @@
 class Admin::MoreController < Admin::BaseController
-  layout "design_system"
+  before_action :check_new_design_system_permissions, only: %i[index]
+  layout :get_layout
 
   def index; end
+
+private
+
+  def check_new_design_system_permissions
+    forbidden! unless new_design_system?
+  end
+
+  def get_layout
+    design_system_actions = %w[index] if preview_design_system?(next_release: false)
+
+    if design_system_actions&.include?(action_name)
+      "design_system"
+    else
+      "admin"
+    end
+  end
 end
