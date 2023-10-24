34db8da5c83eaacf82a1546a75118407cebc6aa1
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2023-10-24 09:47:09 +0100

    Add featured image data relationship to topical events
    FeaturedImageData will now be the intermediary class managing topical event
    logos. This is now consistent with the way we typically implement attachments,
    as is the case with `AttachmentData` and `ImageData`.
    
    Adding an intermediary class rather than allowing the mount_uploader to sit on the
    topical event, means we are also enabling future additions of other attachment types.
    With the new Asset model in place, the intermediary class is needed to distinguish
    the assets by assetable id.
    
    Future changes will bring together under the same intermediary `_Data` class all
    document types that have attachments handled by the FeaturedImageUploader since
    they share versions and validation rules.

--- app/models/featured_image_data.rb
+++ app/models/featured_image_data.rb
@@ -1,12 +1,10 @@
 class FeaturedImageData < ApplicationRecord
   mount_uploader :file, FeaturedImageUploader, mount_on: :carrierwave_image
 
-  validates :file, presence: true, if: :image_changed?
-  validates_with ImageValidator, size: [960, 640], if: :image_changed?
+  has_many :assets,
+           as: :assetable,
+           inverse_of: :assetable
 
-private
-
-  def image_changed?
-    changes["carrierwave_image"].present?
-  end
+  validates :file, presence: true
+  validates_with ImageValidator, size: [960, 640]
 end
