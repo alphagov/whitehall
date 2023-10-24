alphagov/whitehall

commit 4eb10f4d87c247a5be99e7cab3281d262dfcd3d8

Put unpublished editions into an unpublished state instead of reverting them to the draft state
This is intended to reduce confusion for users, who have reported that the behaviour does not match their expectations. The current behaviour also means that we have thousands of draft editions that we do not expect will ever be published again. This is inconsistent with other publishing apps.

In addition, the reversion to draft state makes it difficult to synchronise the content with the publishing API. Simply putting the edition into a dedicated "unpublished" state and requiring users to supersede the edition as usual makes the synchronisation logic much simpler, as we no longer need to check if the draft edition was once in the published state or send a new draft to the publishing API when we unpublish an edition.

test/integration/attachment_redirect_due_to_unpublishing_integration_test.rb
@@ -58,8 +58,6 @@ class AttachmentRedirectDueToUnpublishingIntegrationTest < ActionDispatch::Integ
         new_attachment = build(:file_attachment, attachable:, file:)
         attachable.attachments << new_attachment
         new_attachment.save!
-
-        refute_sets_redirect_url_in_asset_manager
       end
     end
 
@@ -131,32 +129,6 @@ class AttachmentRedirectDueToUnpublishingIntegrationTest < ActionDispatch::Integ
         visit admin_consultation_path(edition)
         withdraw_document
         refute_sets_redirect_url_in_asset_manager
-      end
-    end
-
-    context "given an unpublished document with file attachment" do
-      let(:edition) { create(:news_article, :unpublished) }
-
-      it "resets redirect URI for attachment in Asset Manager when document is published" do
-        stub_publishing_api_links_with_taxons(edition.content_id, [topic_taxon["content_id"]])
-
-        visit admin_news_article_path(edition)
-        force_publish_document
-        assert_sets_redirect_url_in_asset_manager_to nil
-      end
-    end
-
-    context "given an unpublished consultation with outcome with file attachment" do
-      let(:edition) { create(:consultation, :unpublished) }
-      let(:outcome_attributes) { attributes_for(:consultation_outcome) }
-      let(:attachable) { edition.create_outcome!(outcome_attributes) }
-
-      it "resets redirect URI for attachment in Asset Manager when document is published" do
-        stub_publishing_api_links_with_taxons(edition.content_id, [topic_taxon["content_id"]])
-
-        visit admin_consultation_path(edition)
-        force_publish_document
-        assert_sets_redirect_url_in_asset_manager_to nil
       end
     end
 

app/components/admin/editions/show/sidebar_actions_component.rb
@@ -40,7 +40,7 @@ class Admin::Editions::Show::SidebarActionsComponent < ViewComponent::Base
 private
 
   def add_create_action
-    if @edition.is_latest_edition? && @edition.published?
+    if @edition.is_latest_edition? && @edition.can_supersede?
       actions << form_with(url: revise_admin_edition_path(@edition.id), method: :post, data: {
         module: "prevent-multiple-form-submissions",
       }) do

app/helpers/admin/editions_helper.rb
@@ -110,6 +110,7 @@ module Admin::EditionsHelper
       %w[Published published],
       ["Force published (not reviewed)", "force_published"],
       %w[Withdrawn withdrawn],
+      %w[Unpublished unpublished],
     ]
   end
 

app/models/edition.rb
@@ -58,10 +58,10 @@ class Edition < ApplicationRecord
   validates :first_published_at, inclusion: { in: proc { Date.parse("1900-01-01")..Time.zone.now }, message: "must be between 1/1/1900 and the present" }, if: :draft?, allow_blank: true
   validates :scheduled_publication, inclusion: { in: proc { Time.zone.now.. }, message: "must be in the future" }, if: :draft?, allow_blank: true
 
-  UNMODIFIABLE_STATES = %w[scheduled published superseded deleted].freeze
+  UNMODIFIABLE_STATES = %w[scheduled published superseded deleted unpublished].freeze
   FROZEN_STATES = %w[superseded deleted].freeze
   PRE_PUBLICATION_STATES = %w[draft submitted rejected scheduled].freeze
-  POST_PUBLICATION_STATES = %w[published superseded withdrawn].freeze
+  POST_PUBLICATION_STATES = %w[published superseded withdrawn unpublished].freeze
   PUBLICLY_VISIBLE_STATES = %w[published withdrawn].freeze
 
   scope :with_title_or_summary_containing,
@@ -142,7 +142,7 @@ class Edition < ApplicationRecord
     end
 
     def being_unpublished?(previous_state, current_state)
-      previous_state == "published" && %w[draft withdrawn].include?(current_state)
+      previous_state == "published" && %w[unpublished withdrawn].include?(current_state)
     end
   end
 
@@ -478,7 +478,7 @@ EXISTS (
       lock!
       if allow_creating_draft_from_deleted_edition
         raise "Edition not in the deleted state" unless deleted?
-      elsif !published?
+      elsif !can_supersede?
         raise "Cannot create new edition based on edition in the #{state} state"
       end
 

app/models/edition/publishing.rb
@@ -38,13 +38,9 @@ module Edition::Publishing
   end
 
   def change_note_required?
-    if deleted? || superseded?
-      false
-    elsif draft? && new_record?
-      false
-    else
-      other_editions.published.exists?
-    end
+    return false if new_record?
+
+    draft? && previous_edition.present?
   end
 
   def change_note_present!
@@ -96,10 +92,6 @@ module Edition::Publishing
   def reset_version_numbers
     self.published_major_version = previous_edition.try(:published_major_version)
     self.published_minor_version = previous_edition.try(:published_minor_version)
-  end
-
-  def unpublished?
-    !publicly_visible? && unpublishing.present?
   end
 
   def unpublished_edition

app/models/edition/workflow.rb
@@ -11,7 +11,7 @@ module Edition::Workflow
     end
 
     def valid_state?(state)
-      %w[active draft submitted rejected published scheduled force_published withdrawn not_published].include?(state)
+      %w[active draft submitted rejected published scheduled force_published withdrawn not_published unpublished].include?(state)
     end
   end
 
@@ -29,6 +29,7 @@ module Edition::Workflow
       state :superseded
       state :deleted
       state :withdrawn
+      state :unpublished
 
       event :delete do
         transitions from: %i[draft submitted rejected], to: :deleted
@@ -63,11 +64,11 @@ module Edition::Workflow
       end
 
       event :unpublish do
-        transitions from: %i[published draft], to: :draft
+        transitions from: %i[published unpublished], to: :unpublished
       end
 
       event :supersede, success: :destroy_associations_with_edition_dependencies_and_dependants do
-        transitions from: :published, to: :superseded
+        transitions from: %i[published unpublished], to: :superseded
       end
 
       event :withdraw do

app/services/edition_publisher.rb
@@ -43,7 +43,7 @@ private
 
   def editions_to_supersede
     edition.document.editions
-      .published
+      .where(state: %i[published unpublished])
       .excluding(edition)
   end
 

app/services/edition_unpublisher.rb
@@ -7,8 +7,6 @@ class EditionUnpublisher < EditionService
   def failure_reason
     @failure_reason ||= if !can_transition?
                           "An edition that is #{edition.current_state} cannot be #{past_participle}"
-                        elsif (other_edition = edition.other_editions.in_pre_publication_state.first)
-                          "There is already a #{other_edition.state} edition of this document. You must discard it before you can #{verb} this edition."
                         elsif edition.unpublishing.blank?
                           "The reason for unpublishing must be present"
                         elsif !edition.unpublishing.valid?

commit e4f0b5109d13469e208b5605df33785cd6d1d883

Fix unpublished edition attachment redirects when draft edition exists
This fixes a bug introduced in 4eb10f4. In that commit we stopped preventing users from unpublishing a document if a draft edition exists. However, the attachment redirect URL update behaviour assumed that the latest attachable would always be the unpublished one and failed to update the redirect if it found a draft.

The new behaviour checks for the existence of an unpublished attachable and updates the redirect if it finds one. There can only ever be a single unpublished attachment at the moment, so this is a safe approach.

app/models/attachment_data.rb
@@ -102,7 +102,7 @@ class AttachmentData < ApplicationRecord
 
   delegate :access_limited_object, to: :last_attachable
 
-  delegate :unpublished?, to: :last_attachable
+  delegate :unpublished?, to: :unpublished_attachable
 
   def replaced?
     replaced_by.present?
@@ -151,12 +151,20 @@ class AttachmentData < ApplicationRecord
     last_attachment.attachable || Attachable::Null.new
   end
 
+  def unpublished_attachable
+    unpublished_attachment&.attachable || Attachable::Null.new
+  end
+
   def significant_attachment(**args)
     last_publicly_visible_attachment || last_attachment(**args)
   end
 
   def last_attachment(**args)
     filtered_attachments(**args).last || Attachment::Null.new
+  end
+
+  def unpublished_attachment
+    attachments.reverse.detect { |a| a.attachable&.unpublished? }
   end
 
   def last_publicly_visible_attachment

app/services/asset_manager/attachment_updater.rb
@@ -55,7 +55,7 @@ class AssetManager::AttachmentUpdater
   def self.get_redirect_url(attachment_data)
     return nil unless attachment_data.unpublished?
 
-    attachment_data.last_attachable.unpublishing.document_url
+    attachment_data.unpublished_attachable.unpublishing.document_url
   end
 
   def self.get_replacement_id(replaced_attachment_data, variant)

test/integration/attachment_redirect_due_to_unpublishing_integration_test.rb
@@ -61,6 +61,17 @@ class AttachmentRedirectDueToUnpublishingIntegrationTest < ActionDispatch::Integ
       end
     end
 
+    context "given a published document with file attachment and a draft" do
+      let(:edition) { create(:published_news_article) }
+      let!(:draft) { edition.create_draft(edition.creator) }
+
+      it "sets redirect URL for attachment in Asset Manager when document is unpublished" do
+        visit admin_news_article_path(edition)
+        unpublish_document_published_in_error
+        assert_sets_redirect_url_in_asset_manager_to redirect_url
+      end
+    end
+
     context "given a published document with HTML attachment" do
       let(:edition) { create(:published_publication, :with_html_attachment) }
 

test/unit/app/services/asset_manager/attachment_updater_test.rb
@@ -169,7 +169,7 @@ class AssetManager::AttachmentUpdaterTest < ActiveSupport::TestCase
 
       before do
         attachment_data.stubs(:unpublished?).returns(unpublished)
-        attachment_data.stubs(:last_attachable).returns(unpublished_edition)
+        attachment_data.stubs(:unpublished_attachable).returns(unpublished_edition)
       end
 
       it "updates redirect URL for all assets" do