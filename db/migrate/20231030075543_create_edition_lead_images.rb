5d91e546b2dd40a2a5d54c23935b8d2c8e140a7d
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-10-26 16:19:11 +0100

    Add edition_lead_image joins table and model associations
    We're going to add in functionality for users to select a lead image.
    
    This creates a lead_image table, which contains and image_id and edition_id
    and adds the relevant associations in the models.
    
    This should be a has_one as an edition should only ever be able to have
    one lead image. I've added a unique constraint to on edition_id to
    enforce this.

---
+++ db/migrate/20231030075543_create_edition_lead_images.rb
@@ -0,0 +1,11 @@
+class CreateEditionLeadImages < ActiveRecord::Migration[7.0]
+  def change
+    create_table :edition_lead_images do |t|
+      t.integer "edition_id", foreign_key: true
+      t.integer "image_id", foreign_key: true
+      t.index %w[edition_id], name: "index_lead_image_on_edition_id", unique: true
+
+      t.timestamps
+    end
+  end
+end
