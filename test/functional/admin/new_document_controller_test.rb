bd6ee862b338b9b7976cdc1192f9e3518a9975db
Rodney Johnson <rodney.johnson@digital.cabinet-office.gov.uk>
2023-10-27 10:05:25 +0100

    This commit is implementing the technical review comments, specifically improving the accessibility features of the index page as well as ensuring we have a test that handles the redirect of all the radio buttons.

--- test/functional/admin/new_document_controller_test.rb
+++ test/functional/admin/new_document_controller_test.rb
@@ -9,12 +9,10 @@ class Admin::NewDocumentControllerTest < ActionController::TestCase
     get :index
 
     assert_response :success
-    assert_select "h1.govuk-heading-xl", text: "New document"
-
+    assert_select "h1.gem-c-radio__heading-text", text: "New document"
     radio_button_values.each do |value|
       assert_select_radio_button(value)
     end
-
     assert_select ".govuk-inset-text", text: "Check the content types guidance if you need more help in choosing a content type."
   end
 
@@ -24,12 +22,16 @@ class Admin::NewDocumentControllerTest < ActionController::TestCase
     assert_response :forbidden
   end
 
-  test "POST #new_document_options_redirect redirects to #call-for-evidence if search option passed is call-for-evidence" do
-    request_params = {
-      "new_document_options": "call-for-evidence",
-    }
-    post :new_document_options_redirect, params: request_params
-    assert_redirected_to new_admin_call_for_evidence_path
+  test "POST #new_document_options_redirect redirects each radio buttons to their expected paths" do
+    redirect_options.each do |selected_option, expected_path|
+      request_params = {
+        "new_document_options": selected_option,
+      }
+
+      post :new_document_options_redirect, params: request_params
+
+      assert_redirected_to expected_path
+    end
   end
 
   test "when no radio buttons are selected a flash notice is shown and the user remains on the index page" do
@@ -38,6 +40,7 @@ class Admin::NewDocumentControllerTest < ActionController::TestCase
     }
 
     post :new_document_options_redirect, params: request_params
+
     assert_redirected_to admin_new_document_path
     assert_equal flash[:alert], "Please select a new document option"
   end
@@ -50,5 +53,20 @@ private
 
   def assert_select_radio_button(value)
     assert_select ".govuk-radios__item input[type=radio][name=new_document_options][value=#{value}]", count: 1
+  end
+
+  def redirect_options
+    {
+      "call-for-evidence": new_admin_call_for_evidence_path,
+      "case-study": new_admin_case_study_path,
+      "consultation": new_admin_consultation_path,
+      "detailed-guide": new_admin_detailed_guide_path,
+      "document-collection": new_admin_document_collection_path,
+      "fatality-notice": new_admin_fatality_notice_path,
+      "news-article": new_admin_news_article_path,
+      "publication": new_admin_publication_path,
+      "speech": new_admin_speech_path,
+      "statistical-data-set": new_admin_statistical_data_set_path,
+    }
   end
 end
