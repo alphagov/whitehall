c76ccd6d2d587f670fd355a3588049967b23a367
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2023-11-03 16:18:15 +0000

    Fix contact-related tests
    The contacts_test.rb model test file was failing because it was not
     requiring the test helper (not sure why this has not been a problem up
     until now…).
    
    During the investigation we noticed that there was a `contact_test.rb`
     and a `contacts_test.rb` that were both testing the `Contact` model, so
     we have merged them together.

--- test/unit/app/models/contact_test.rb
+++ test/unit/app/models/contact_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "publishes_to_publishing_api"
 
 class ContactTest < ActiveSupport::TestCase
   test "should be invalid without a description" do
@@ -203,5 +204,18 @@ class ContactTest < ActiveSupport::TestCase
     Whitehall::PublishingApi.expects(:republish_async).with(worldwide_office).once
 
     worldwide_office.contact.destroy!
+  end
+
+  test "destroy deletes related contacts" do
+    # This test uses organisations as a candidate, but any object with this module
+    # can be used here. Ideally a seperate stub ActiveRecord object would be used.
+    organisation = create(:organisation)
+    contact = create(:contact, contactable: organisation)
+    organisation.destroy!
+    assert_nil Contact.find_by(id: contact.id)
+  end
+
+  test "publishes to the publishing api" do
+    assert Contact.included_modules.include? PublishesToPublishingApi
   end
 end
