5eca54cc92bb3e39226b963b70e2bee0b29d4256
Minno Dang <mndang@thoughtworks.com>
2023-11-09 17:47:08 +0000

    Fix Orphan Document scenario for Document Collection
    Document Collection Groups assume that documents added to the document groups always have a latest_edition attached to it since users are only allowed to search for and add published documents to the group. However, since there is behaviour to unpublish and delete the edition, the document is left orphaned, which is currently causing server errors for users who have previously added this document to their Document Collection Group.
    
    Fix is to render the unavailable document so that the user would know to remove this orphaned document relationship from their document collection group.
    
    Co-Authored-By: Nikin Nagewadia <nikin.nagewadia@digital.cabinet-office.gov.uk>

--- app/helpers/admin/document_collection_group_memberships_helper.rb
+++ app/helpers/admin/document_collection_group_memberships_helper.rb
@@ -1,17 +1,23 @@
 module Admin::DocumentCollectionGroupMembershipsHelper
   def document_collection_group_member_title(membership)
-    if membership.non_whitehall_link
-      membership.non_whitehall_link.title
-    else
-      membership.document.latest_edition.title
-    end
+    return membership.non_whitehall_link.title if membership.non_whitehall_link
+
+    membership.document.latest_edition.title
   end
 
   def document_collection_group_member_url(membership)
-    if membership.non_whitehall_link
-      Plek.website_root + membership.non_whitehall_link.base_path
-    else
-      membership.document.latest_edition.public_url
+    return Plek.website_root + membership.non_whitehall_link.base_path if membership.non_whitehall_link
+
+    membership.document.latest_edition.public_url
+  end
+
+  def document_collection_group_member_unavailable?(membership)
+    !membership.non_whitehall_link && !membership.document&.latest_edition
+  end
+
+  def unavailable_document_count(memberships)
+    memberships.count do |membership|
+      document_collection_group_member_unavailable?(membership)
     end
   end
 end
