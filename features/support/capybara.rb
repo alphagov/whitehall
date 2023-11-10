0013bdc99f9226cf4bebfc0539658e9ac7a9b3e4
Tuomas Nylund <tuomas.nylund@thoughtworks.com>
2023-11-10 13:24:18 +0000

    increase capybara wait times because ci workflow execution began to fail on github

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,7 +1,7 @@
 # On the CI box we have seen intermittent failures.
 # We think this may be due to timeouts (the default is 2 secs),
 # so we've increased the default timeout.
-Capybara.default_max_wait_time = 5
+Capybara.default_max_wait_time = 8
 
 # Allow Capybara to click a <label> even if its corresponding <input> isn't visible on screen.
 # This needs to be enabled when using custom-styled checkboxes and radios, such as those
