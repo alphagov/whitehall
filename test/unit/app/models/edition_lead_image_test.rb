fe59525d0e07d187a3b020ca913853460f1f1430
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-11-15 10:27:11 +0000

    Add data migration to replace lead images which are SVGs
    After a quick bit of investigation i can see that 5 pretty old draft
    editions have had lead images created in the db which use SVGs.
    
    This migration deletes any lead images which are SVGs and replaces them
    with the latest non-svg image.
    
    All of these editions only actually have 1 image so all that will happen
    is they'll be removed as the lead image, but i've whacked the logic
    in around updating the lead image in case anyones local data is in
    a different state.

--- test/unit/app/models/edition_lead_image_test.rb
+++ test/unit/app/models/edition_lead_image_test.rb
@@ -14,7 +14,7 @@ class EditionLeadImageTest < ActiveSupport::TestCase
   test "is invalid when the image is an svg" do
     svg_image_data = build(:image_data, file: File.open(Rails.root.join("test/fixtures/images/test-svg.svg")))
     image = build(:image, image_data: svg_image_data)
-    edition_lead_image = build(:edition_lead_image, image: image)
+    edition_lead_image = build(:edition_lead_image, image:)
 
     assert_not edition_lead_image.valid?
     assert_equal "Lead image can not be an SVG.", edition_lead_image.errors.first.full_message
