4eea21f7ee0980437d8e6531f508dc3b39b0a4fd
farahTW <farah.mehmood@digital.cabinet-office.gov.uk>
2023-11-21 15:10:47 +0000

    Removes document searches legacy files
    These files are no longer in use after transition to design system.

--- app/helpers/admin/document_searches_helper.rb
+++
@@ -1,9 +0,0 @@
-module Admin::DocumentSearchesHelper
-  def public_time_for_edition(edition)
-    if edition.public_timestamp.present?
-      absolute_date(edition.public_timestamp)
-    else
-      "(#{edition.state.humanize})"
-    end
-  end
-end
