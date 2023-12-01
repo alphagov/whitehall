2f4fafbb518509509fa1f225f0a62082879b9859
Iris Lau <iris.lau@digital.cabinet-office.gov.uk>
2023-12-01 15:26:15 +0000

    Cleanup legacy url path service helper

--- test/unit/app/services/asset_manager/service_helper_test.rb
+++ test/unit/app/services/asset_manager/service_helper_test.rb
@@ -6,47 +6,8 @@ class AssetManager::ServiceHelperTest < ActiveSupport::TestCase
   setup do
     @asset_manager_id = "asset-id"
     @asset_url = "http://asset-manager/assets/#{@asset_manager_id}"
-    @legacy_url_path = "legacy-url-path"
     @worker = Object.new
     @worker.extend(AssetManager::ServiceHelper)
-  end
-
-  describe "find_asset_by" do
-    test "returns attributes including asset ID" do
-      Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
-              .returns(gds_api_response("id" => @asset_url))
-
-      attributes = @worker.send(:find_asset_by, @legacy_url_path)
-
-      assert_equal @asset_manager_id, attributes["id"]
-    end
-
-    test "returns attributes including asset URL" do
-      Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
-              .returns(gds_api_response("id" => @asset_url))
-
-      attributes = @worker.send(:find_asset_by, @legacy_url_path)
-
-      assert_equal @asset_url, attributes["url"]
-    end
-
-    test "returns other attributes" do
-      Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
-              .returns(gds_api_response("id" => @asset_url, "key" => "value"))
-
-      attributes = @worker.send(:find_asset_by, @legacy_url_path)
-
-      assert_equal "value", attributes["key"]
-    end
-
-    test "raises AssetNotFound when an asset is not available" do
-      Services.asset_manager.stubs(:whitehall_asset).with(@legacy_url_path)
-              .raises(GdsApi::HTTPNotFound.new(404))
-
-      assert_raises AssetManager::ServiceHelper::AssetNotFound do
-        @worker.send(:find_asset_by, @legacy_url_path)
-      end
-    end
   end
 
   describe "find_asset_by_id" do
