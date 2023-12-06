3868a1f700e27189ca211320eaa0236f43825392
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-12-06 21:12:33 +0000

    Use stubbed model for UserOrderableClass tests

--- test/unit/app/models/user_orderable_class_test.rb
+++ test/unit/app/models/user_orderable_class_test.rb
@@ -1,39 +1,52 @@
 require "test_helper"
 
 class UserOrderableClassTest < ActiveSupport::TestCase
+  class StubModel
+    include ActiveModel::Model
+    include UserOrderableClass
+
+    attr_accessor :id, :ordering
+
+    def self.transaction
+      yield
+    end
+
+    def self.find; end
+
+    def update_column; end
+
+    def update!; end
+  end
+
+  setup do
+    @model1 = StubModel.new(id: 1, ordering: 1)
+    @model2 = StubModel.new(id: 2, ordering: 2)
+
+    StubModel.stubs(:find).with(@model1.id).returns(@model1)
+    StubModel.stubs(:find).with(@model2.id).returns(@model2)
+  end
+
   test "#reorder_without_callbacks! reorders the ordering of the collection using #update_column based on the params and column name passed in" do
-    org1 = create(:ministerial_department, ministerial_ordering: 1)
-    org2 = create(:ministerial_department, ministerial_ordering: 2)
+    @model2.expects(:update_column).with(:ordering, "1").once
+    @model1.expects(:update_column).with(:ordering, "2").once
 
-    Organisation.stubs(:find).with(org1.id).returns(org1)
-    Organisation.stubs(:find).with(org2.id).returns(org2)
-
-    org2.expects(:update_column).with(:ministerial_ordering, "1").once
-    org1.expects(:update_column).with(:ministerial_ordering, "2").once
-
-    Organisation.reorder_without_callbacks!(
+    StubModel.reorder_without_callbacks!(
       {
-        org2.id => "1",
-        org1.id => "2",
+        @model2.id => "1",
+        @model1.id => "2",
       },
-      :ministerial_ordering,
+      :ordering,
     )
   end
 
   test "#reorder! reorders the collection using #update! based on the params passed in" do
-    take_part_page1 = create(:take_part_page, ordering: 1)
-    take_part_page2 = create(:take_part_page, ordering: 2)
+    @model2.expects(:update!).with(ordering: "1").once
+    @model1.expects(:update!).with(ordering: "2").once
 
-    TakePartPage.stubs(:find).with(take_part_page1.id).returns(take_part_page1)
-    TakePartPage.stubs(:find).with(take_part_page2.id).returns(take_part_page2)
-
-    take_part_page2.expects(:update!).with(ordering: "1").once
-    take_part_page1.expects(:update!).with(ordering: "2").once
-
-    TakePartPage.reorder!(
+    StubModel.reorder!(
       {
-        take_part_page2.id => "1",
-        take_part_page1.id => "2",
+        @model2.id => "1",
+        @model1.id => "2",
       },
       :ordering,
     )
