f060b769a2a8d9c7894ced425a54f37632f84ab4
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2023-12-18 13:19:31 +0000

    Remove access limited from edition
    There have been cases of users accidentally removing their own access by not setting
    the correct organisations, thus making it impossible for themselves to publish.
    Adding rake task to temporarily remove access limited from editions so that the users
    can fix the organisation.

---
+++ test/unit/lib/tasks/remove_access_limiting_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+require "rake"
+
+class RemoveAccessLimitingTest < ActiveSupport::TestCase
+  teardown do
+    Rake::Task["remove_access_limiting"].reenable
+  end
+
+  test "sets access limited to false for selected edition" do
+    edition = create(:draft_edition, :access_limited)
+
+    assert_output(/Access limited successfully set to false for edition of ID #{edition.id}./) { Rake.application.invoke_task("remove_access_limiting[#{edition.id}]") }
+
+    edition.reload
+    assert_equal false, edition.access_limited
+  end
+
+  test "raises error if edition cannot be found" do
+    assert_raises(StandardError, "Cannot find edition of ID 12345.") do
+      Rake.application.invoke_task("remove_access_limiting[12345]")
+    end
+  end
+end
