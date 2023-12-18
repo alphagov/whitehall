cced5f2a554f6c1f774143063cb0c742f9891ef9
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-12-18 12:12:21 +0000

    Remove send call on Role to prevent injection attack
    In the organisation roles controller #send is used directly on the
    Role class.
    
    This provides an attack vector for scripting. Particularly because it's
    using #send in place of #public_send so it has access to the private
    interface too.
    
    There's some hidden complexity here because the type param is actually one
    of 6 scopes called on the Role model.
    
    This is a pretty rough and ready fix and there are probably better solutions.
    I'd love to give this whole bit of the codebase a refactor as there's
    a bunch of weird stuff going on, but for now i think this is probably
    the simplest way to resolve the vunerability.
    
    Ideally, i think the type query param would move to the path so we can
    use constraints to match on type
    https://guides.rubyonrails.org/routing.html#specifying-constraints
    
    We could also potenitally add a separate reorder endpoint for each type
    like we did for the CabinetMinistersController. But i think that's
    probably a bit beyond the scope of this investigation & fix.
    
    I'll be adding a card to the backlog to come back and look at this bit of
    the codebase which i'll link in the PR.

--- app/controllers/admin/organisation_people_controller.rb
+++ app/controllers/admin/organisation_people_controller.rb
@@ -4,17 +4,23 @@ class Admin::OrganisationPeopleController < Admin::BaseController
 
   def index
     @render_reorder = can?(:edit, @organisation)
-    @ministerial_organisation_roles = organisation_roles(:ministerial)
-    @management_organisation_roles = organisation_roles(:management)
-    @traffic_commissioner_organisation_roles = organisation_roles(:traffic_commissioner)
-    @military_organisation_roles = organisation_roles(:military)
-    @special_representative_organisation_roles = organisation_roles(:special_representative)
-    @chief_professional_officer_roles = organisation_roles(:chief_professional_officer)
+    @ministerial_organisation_roles = organisation_roles("ministerial")
+    @management_organisation_roles = organisation_roles("management")
+    @traffic_commissioner_organisation_roles = organisation_roles("traffic_commissioner")
+    @military_organisation_roles = organisation_roles("military")
+    @special_representative_organisation_roles = organisation_roles("special_representative")
+    @chief_professional_officer_roles = organisation_roles("chief_professional_officer")
   end
 
   def reorder
     type = params[:type]
-    @organisation_roles = organisation_roles(type)
+    valid_types = %w[ministerial special_representative management traffic_commissioner chief_professional_officer military]
+
+    if valid_types.exclude?(type)
+      render "admin/errors/not_found", status: :not_found
+    else
+      @organisation_roles = organisation_roles(type)
+    end
   end
 
   def order
@@ -27,8 +33,28 @@ class Admin::OrganisationPeopleController < Admin::BaseController
 private
 
   def organisation_roles(type)
-    @organisation.organisation_roles.joins(:role)
-                 .merge(Role.send(type)).order(:ordering)
+    @organisation
+    .organisation_roles
+    .joins(:role)
+    .merge(roles_for_type(type))
+    .order(:ordering)
+  end
+
+  def roles_for_type(type)
+    case type
+    when "ministerial"
+      Role.ministerial
+    when "management"
+      Role.management
+    when "traffic_commissioner"
+      Role.traffic_commissioner
+    when "military"
+      Role.military
+    when "special_representative"
+      Role.special_representative
+    when "chief_professional_officer"
+      Role.chief_professional_officer
+    end
   end
 
   def enforce_permissions!
