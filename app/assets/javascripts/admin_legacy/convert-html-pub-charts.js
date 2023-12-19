d05c7efdb73a9fb02e2ed6e6145b9ee83c810f63
Minno Dang <mndang@thoughtworks.com>
2023-12-19 14:09:22 +0000

    Remove Govspeak to charts JS
    This is possibly left over from when Whitehall was still rendering frontend pages.
    The front end does seem to be doing something special with {barchart} annotation in govspeak so will leave the govspeak helper as is

--- app/assets/javascripts/admin_legacy/convert-html-pub-charts.js
+++
@@ -1,8 +0,0 @@
-function govspeakBarcharts () {
-  $('.js-barchart-table').each(function () {
-    $.magnaCharta($(this), {
-      toggleText: 'Change between chart and table'
-    })
-  })
-}
-$(govspeakBarcharts)
