fc288fbb28548cf86679a75d2d8779ddcf7e2da4
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-01-12 15:32:04 +0000

    Add test for image cropper component
    - this is a bit of a placeholder as the component doesn't render in the component guide so I'm not sure how it's supposed to operate
    - also had to add some accessibility criteria (again placeholder, need more detail) as this is a required field

---
+++ test/components/image_cropper_test.rb
@@ -0,0 +1,24 @@
+require "component_test_helper"
+
+class ImagecropperComponentTest < ComponentTestCase
+  def component_name
+    "image_cropper"
+  end
+
+  test "errors when no parameters given" do
+    assert_raises do
+      render_component({})
+    end
+  end
+
+  test "renders the basic component" do
+    render_component({
+      name: "name",
+      src: "src",
+      filename: "filename",
+      type: "type",
+    })
+
+    assert_select ".app-c-image-cropper"
+  end
+end
