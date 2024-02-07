842a7a81c78e5c47cb80cb6821b911b0b5191da6
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2024-02-07 16:43:21 +0000

    Inline and simplify asset creation worker code

--- app/workers/asset_manager_create_asset_worker.rb
+++ app/workers/asset_manager_create_asset_worker.rb
@@ -9,44 +9,22 @@ class AssetManagerCreateAssetWorker < WorkerBase
   def perform(temporary_location, asset_params, auth_bypass_ids = [])
     return unless File.exist?(temporary_location)
 
-    file = File.open(temporary_location)
-
     assetable_id, assetable_type, asset_variant = asset_params.values_at("assetable_id", "assetable_type", "asset_variant")
+    assetable = assetable_type.constantize.where(id: assetable_id).first
 
-    return logger.info("Assetable #{assetable_type} of id #{assetable_id} does not exist") unless assetable_type.constantize.where(id: assetable_id).exists?
+    return logger.info("Assetable #{assetable_type} of id #{assetable_id} does not exist") if assetable.nil?
 
-    asset_options = { file:, auth_bypass_ids:, draft: false }
-
-    response = create_asset(asset_options)
-    save_asset(assetable_id, assetable_type, asset_variant, response.asset_manager_id, response.filename)
-
-    enqueue_downstream_service_updates(assetable_id, assetable_type)
-
+    file = File.open(temporary_location)
+    response = create_asset({ file:, auth_bypass_ids:, draft: false })
     file.close
     FileUtils.rm(file)
     FileUtils.rmdir(File.dirname(file))
-  end
 
-private
-
-  def enqueue_downstream_service_updates(assetable_id, assetable_type)
-    assetable = assetable_type.constantize.find(assetable_id)
-    assetable.republish_on_assets_ready if assetable.respond_to? :republish_on_assets_ready
-  end
-
-  def get_authorised_organisation_ids(attachable_model_class, attachable_model_id)
-    if attachable_model_class && attachable_model_id
-      attachable_model = attachable_model_class.constantize.find(attachable_model_id)
-      if attachable_model.respond_to?(:access_limited?) && attachable_model.access_limited?
-        AssetManagerAccessLimitation.for(attachable_model)
-      end
-    end
-  end
-
-  def save_asset(assetable_id, assetable_type, variant, asset_manager_id, filename)
-    asset = Asset.where(assetable_id:, assetable_type:, variant:).first_or_initialize
-    asset.asset_manager_id = asset_manager_id
-    asset.filename = filename
+    asset = assetable.assets.where(variant: asset_variant).first_or_initialize
+    asset.asset_manager_id = response.asset_manager_id
+    asset.filename = response.filename
     asset.save!
+
+    assetable.republish_on_assets_ready if assetable.respond_to? :republish_on_assets_ready
   end
 end
