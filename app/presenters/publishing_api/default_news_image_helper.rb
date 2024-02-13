5463e9ed67ba66cce81bf7d5a8eee89005bdc851
Meyric Rawlings <meyric.rawlings@digital.cabinet-office.gov.uk>
2024-02-13 10:59:13 +0000

    Extract default new image presentation methods
    Both Organisation and WorldwideOrganisation use default_news_image, but
    work in different ways. We want to make them consistent.
    
    Here we extract the two methods that are used in the preseter into a
    module so they can be resused.
    
    We also rename the methods to help distihguish between the methods on
    the internal representation of the object, Organisation here, and the
    presentation method.
    
    We have also added some additional coverage around the behaviour.

---
+++ app/presenters/publishing_api/default_news_image_helper.rb
@@ -0,0 +1,16 @@
+module PublishingApi::DefaultNewsImageHelper
+private
+
+  def present_default_news_image(item)
+    return unless item.default_news_image && item.default_news_image.all_asset_variants_uploaded?
+
+    {
+      url: default_news_image_url(item, :s300),
+      high_resolution_url: default_news_image_url(item, :s960),
+    }
+  end
+
+  def default_news_image_url(item, size = nil)
+    size ? item.default_news_image.url(size) : item.default_news_image.url
+  end
+end
