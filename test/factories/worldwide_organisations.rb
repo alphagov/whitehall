bbf13cd036756aecabc97153bf3361f94c515fd4
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-02-14 09:59:20 +0000

    Add home page offices to worldwide organisation test
    We were not including home page offices in the worldwide organisation
    presenter tests, therefore meaning the code for these was untested.

--- test/factories/worldwide_organisations.rb
+++ test/factories/worldwide_organisations.rb
@@ -14,9 +14,16 @@ FactoryBot.define do
       end
     end
 
-    trait(:with_office) do
+    trait(:with_main_office) do
       after :create do |organisation, _evaluator|
         FactoryBot.create(:worldwide_office, worldwide_organisation: organisation)
+      end
+    end
+
+    trait(:with_home_page_offices) do
+      after :create do |organisation, _evaluator|
+        worldwide_office = create(:worldwide_office, worldwide_organisation: organisation)
+        organisation.add_office_to_home_page!(worldwide_office)
       end
     end
 
