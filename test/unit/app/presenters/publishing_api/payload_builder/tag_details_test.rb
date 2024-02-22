96947f3b77fb48abf2e9f03e33f03fcd6c375432
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2024-02-22 15:37:27 +0000

    Remove specialist topics from publishing_api tag_details
    Since we have retired specialist topics they are no longer needed to send
    to the publishing API to enable emails to be sent.

--- test/unit/app/presenters/publishing_api/payload_builder/tag_details_test.rb
+++ test/unit/app/presenters/publishing_api/payload_builder/tag_details_test.rb
@@ -4,14 +4,10 @@ module PublishingApi
   module PayloadBuilder
     class TagDetailsTest < ActiveSupport::TestCase
       test "returns tag details for the item" do
-        stubbed_item = stub(
-          primary_specialist_sector_tag: "ss_tag_1",
-          secondary_specialist_sector_tags: %w[ss_tag_1 ss_tag_3],
-        )
+        stubbed_item = stub
         expected_hash = {
           tags: {
             browse_pages: [],
-            topics: %w[ss_tag_1 ss_tag_1 ss_tag_3],
           },
         }
 
