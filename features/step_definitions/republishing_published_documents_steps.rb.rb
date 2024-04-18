24916128091540cd899a0e871efb01628818bde7
Ynda Jas <yndajas@gmail.com>
2024-04-18 13:00:16 +0100

    Fix republishing link ID
    We shifted to using a `.map` here but left in a page-specific ID: this
    makes it generic/specific to whichever item is within a given `.map`
    iteration

--- features/step_definitions/republishing_published_documents_steps.rb.rb
+++ features/step_definitions/republishing_published_documents_steps.rb.rb
@@ -4,7 +4,7 @@ end
 
 When(/^I request a republish of the "Past prime ministers" page$/) do
   visit admin_republishing_index_path
-  find("#republish-past-prime-ministers-index").click
+  find("#republish-past-prime-ministers").click
   click_button("Confirm republishing")
 end
 
