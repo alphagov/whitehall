7c06a0c9886b3fa3b63fa60d6a4ca8928ae8b93a
Ynda Jas <yndajas@gmail.com>
2024-04-18 12:47:48 +0100

    Reorder republishing controller actions
    This makes them more chronological

--- app/controllers/admin/republishing_controller.rb
+++ app/controllers/admin/republishing_controller.rb
@@ -5,6 +5,15 @@ class Admin::RepublishingController < Admin::BaseController
     @republishable_pages = republishable_pages
   end
 
+  def confirm_page
+    page_to_republish = republishable_pages.find { |page| page[:slug] == params[:page_slug] }
+
+    return render "admin/errors/not_found", status: :not_found unless page_to_republish
+
+    @title = page_to_republish[:title]
+    @republishing_path = admin_republishing_page_republish_path(page_to_republish[:slug])
+  end
+
   def republish_page
     page_to_republish = republishable_pages.find { |page| page[:slug] == params[:page_slug] }
 
@@ -13,15 +22,6 @@ class Admin::RepublishingController < Admin::BaseController
     PresentPageToPublishingApiWorker.perform_async(page_to_republish[:presenter])
     flash[:notice] = "'#{page_to_republish[:title]}' page has been scheduled for republishing"
     redirect_to(admin_republishing_index_path)
-  end
-
-  def confirm_page
-    page_to_republish = republishable_pages.find { |page| page[:slug] == params[:page_slug] }
-
-    return render "admin/errors/not_found", status: :not_found unless page_to_republish
-
-    @title = page_to_republish[:title]
-    @republishing_path = admin_republishing_page_republish_path(page_to_republish[:slug])
   end
 
 private
