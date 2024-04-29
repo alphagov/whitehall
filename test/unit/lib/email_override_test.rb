faf95b823f7def32cce9c6a052ecdc079d138e9d
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2024-04-29 12:50:23 +0100

    fix 3
    you didn't need all these stubs, the only time the task is fetching anything from publishing api
    is @taxon_content_item = Services.publishing_api.get_content(@taxon_content_id).to_h so that's
    all you need to stub.

--- test/unit/lib/email_override_test.rb
+++ test/unit/lib/email_override_test.rb
@@ -4,21 +4,11 @@ class EmailOverrideTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
   include TaxonomyHelper
   setup do
-    # stub_any_publishing_api_call
-    stub_taxonomy_with_selected_taxons
-    stub_taxonomy_with_all_taxons
     stub_publishing_api_has_item(content_id: work_taxon_content_id, title: work_taxon_parent["title"])
-
     @document_collection = create(:document_collection, :draft)
-    # stub_taxonomy_with_all_taxons
   end
 
   test "changes the email overide attribute to a content_id" do
-    taxons = { "title" => "title", "base_path" => "/foo", "content_id" => "work_taxon_content_id" }
-    links = { "meets_user_needs" => %w[123] }
-    stub_publishing_api_expanded_links_with_taxons(@document_collection.content_id, [taxons])
-    stub_publishing_api_has_links({ content_id: @document_collection.content_id, links: })
-
     email_overrider = EmailOveride::EmailOverride.new(document_collection_id: @document_collection.id, taxon_content_id: work_taxon_content_id, dry_run: false)
 
     # Rake.application.invoke_task("set_email_override:real[#{document_collection.id},#{root_taxon_content_id}]")
