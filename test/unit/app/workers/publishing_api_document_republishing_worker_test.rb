02384477cae70ca0a825b6b17b08677a05d96431
George Eaton <george@dxw.com>
2024-04-30 16:48:08 +0100

    Refactor republishing worker tests
    When working on a new feature that relies on the republishing worker, we
    found the worker difficult to parse and extend. This commit refactors
    the tests to:
    - be grouped by context
    - assert that actions happen in the correct sequence - this was tested
      in [an earlier version of the suite][1]
    - mock ActiveRecord calls per a suggestion from [an earlier commit][1]
    - include coverage for when a draft edition is invalid
    
    [1]: https://github.com/alphagov/whitehall/commit/56195d57139b80b376379ccac286dfc9002d44ce
    
    Co-authored-by: George <george@dxw.com>
    Co-authored-by: Ynda Jas <yndajas@gmail.com>

--- test/unit/app/workers/publishing_api_document_republishing_worker_test.rb
+++ test/unit/app/workers/publishing_api_document_republishing_worker_test.rb
@@ -4,104 +4,321 @@ require "gds_api/test_helpers/publishing_api"
 class PublishingApiDocumentRepublishingWorkerTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
 
+  let(:unpublished_edition_a) { build(:unpublished_edition) }
+  let(:unpublished_edition_b) { build(:unpublished_edition) }
+  let(:withdrawn_edition) { build(:withdrawn_edition) }
+  let(:published_edition) { build(:published_edition) }
+  let(:draft_edition) { build(:draft_edition) }
+
+  let(:document) { build(:document, live_edition:, pre_publication_edition:) }
+
+  setup do
+    Document.stubs(:find).returns(document)
+  end
+
   context "#perform" do
-    test "does nothing when the document only has superseded editions" do
-      document = create(:document, editions: [build(:superseded_edition)])
+    context "when the document only has superseded editions" do
+      let(:live_edition) { nil }
+      let(:pre_publication_edition) { nil }
 
-      Whitehall::PublishingApi.expects(:publish).never
-      Whitehall::PublishingApi.expects(:save_draft).never
-      Whitehall::PublishingApi.expects(:patch_links).never
-      PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
-      ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).never
+      setup { document.editions.stubs(:unpublished).returns([]) }
 
-      PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+      it "does nothing" do
+        Whitehall::PublishingApi.expects(:publish).never
+        Whitehall::PublishingApi.expects(:save_draft).never
+        Whitehall::PublishingApi.expects(:patch_links).never
+        PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+        ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).never
+
+        PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+      end
     end
 
-    context "when there are non-superseded editions" do
-      test "unpublishes the latest unpublished edition when the document has been unpublished" do
-        first_unpublished_edition = build(:unpublished_edition)
-        last_unpublished_edition = build(:unpublished_edition)
-        document = create(:document, editions: [first_unpublished_edition, last_unpublished_edition])
+    context "when the document has one or more non-superseded editions" do
+      context "that are unpublished" do
+        let(:live_edition) { nil }
+        let(:pre_publication_edition) { nil }
 
-        PublishingApiUnpublishingWorker.any_instance.expects(:perform).with(last_unpublished_edition.unpublishing.id, last_unpublished_edition.draft?).once
-        ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(last_unpublished_edition, "republish").once
-        Whitehall::PublishingApi.expects(:save_draft).never
-        Whitehall::PublishingApi.expects(:publish).never
-        Whitehall::PublishingApi.expects(:patch_links).never
+        setup { document.editions.stubs(:unpublished).returns([unpublished_edition_a, unpublished_edition_b]) }
 
-        PublishingApiDocumentRepublishingWorker.new.perform(document.id)
-      end
+        it "refreshes the latest unpublished edition" do
+          sequence = sequence("unpublish; handle attachments")
 
-      test "unpublishes the latest unpublished edition and draft edition when the document has been unpublished and there's also a draft edition" do
-        first_unpublished_edition = build(:unpublished_edition)
-        last_unpublished_edition = build(:unpublished_edition)
-        draft_edition = build(:draft_edition)
-        document = create(:document, editions: [first_unpublished_edition, last_unpublished_edition, draft_edition])
+          PublishingApiUnpublishingWorker
+            .any_instance
+            .expects(:perform)
+            .with(unpublished_edition_b.unpublishing.id, unpublished_edition_b.draft?)
+            .once
+            .in_sequence(sequence)
+          ServiceListeners::PublishingApiAssociatedDocuments
+            .expects(:process)
+            .with(unpublished_edition_b, "republish")
+            .once
+            .in_sequence(sequence)
 
-        PublishingApiUnpublishingWorker.any_instance.expects(:perform).with(last_unpublished_edition.unpublishing.id, last_unpublished_edition.draft?).once
-        ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(last_unpublished_edition, "republish").once
-        Whitehall::PublishingApi.expects(:save_draft).with(draft_edition, "republish", bulk_publishing: false).once
-        ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(draft_edition, "republish").once
-        Whitehall::PublishingApi.expects(:publish).never
-        Whitehall::PublishingApi.expects(:patch_links).never
-
-        PublishingApiDocumentRepublishingWorker.new.perform(document.id)
-      end
-
-      test "republishes then unpublishes the live edition when the document has not been unpublished but has been withdrawn" do
-        live_withdrawn_edition = build(:withdrawn_edition)
-        document = create(:document, live_edition: live_withdrawn_edition, editions: [live_withdrawn_edition])
-
-        Whitehall::PublishingApi.expects(:publish).with(live_withdrawn_edition, "republish", bulk_publishing: false).once
-        ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(live_withdrawn_edition, "republish").once
-        PublishingApiUnpublishingWorker.any_instance.expects(:perform).with(live_withdrawn_edition.unpublishing.id, live_withdrawn_edition.draft?).once
-        ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(live_withdrawn_edition, "republish").once
-        Whitehall::PublishingApi.expects(:save_draft).never
-        Whitehall::PublishingApi.expects(:patch_links).never
-
-        PublishingApiDocumentRepublishingWorker.new.perform(document.id)
-      end
-
-      context "when the document has not been unpublished or withdrawn" do
-        test "patches links then republishes the draft edition when there's only a draft edition" do
-          draft_edition = build(:draft_edition)
-          document = create(:document, editions: [draft_edition])
-
-          Whitehall::PublishingApi.expects(:patch_links).with(draft_edition, bulk_publishing: false).once
-          Whitehall::PublishingApi.expects(:save_draft).with(draft_edition, "republish", bulk_publishing: false).once
-          ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(draft_edition, "republish").once
-          Whitehall::PublishingApi.expects(:publish).never
-          PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
-
-          PublishingApiDocumentRepublishingWorker.new.perform(document.id)
-        end
-
-        test "patches links then republishes the live edition when there's only a live edition" do
-          live_edition = build(:published_edition)
-          document = create(:document, live_edition:, editions: [live_edition])
-
-          Whitehall::PublishingApi.expects(:patch_links).with(live_edition, bulk_publishing: false).once
-          Whitehall::PublishingApi.expects(:publish).with(live_edition, "republish", bulk_publishing: false).once
-          ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(live_edition, "republish").once
           Whitehall::PublishingApi.expects(:save_draft).never
-          PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+          Whitehall::PublishingApi.expects(:publish).never
+          Whitehall::PublishingApi.expects(:patch_links).never
 
           PublishingApiDocumentRepublishingWorker.new.perform(document.id)
         end
 
-        test "patches links with the live edition then republishes the live edition then republishes the draft edition when there's both a live and draft edition" do
-          live_edition = build(:published_edition)
-          draft_edition = build(:draft_edition)
-          document = create(:document, live_edition:, editions: [live_edition, draft_edition])
+        context "and there's also a draft edition" do
+          let(:live_edition) { nil }
+          let(:pre_publication_edition) { draft_edition }
 
-          Whitehall::PublishingApi.expects(:patch_links).with(live_edition, bulk_publishing: false).once
-          Whitehall::PublishingApi.expects(:publish).with(live_edition, "republish", bulk_publishing: false).once
-          ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(live_edition, "republish").once
-          Whitehall::PublishingApi.expects(:save_draft).with(draft_edition, "republish", bulk_publishing: false).once
-          ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(draft_edition, "republish").once
-          PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+          setup { document.editions.stubs(:unpublished).returns([unpublished_edition_a, unpublished_edition_b]) }
+
+          it "refreshes the latest unpublished edition and the draft edition" do
+            sequence = sequence("unpublish; handle unpublished attachments; save draft; handle draft attachments")
+
+            PublishingApiUnpublishingWorker
+              .any_instance
+              .expects(:perform)
+              .with(unpublished_edition_b.unpublishing.id, unpublished_edition_b.draft?)
+              .once
+              .in_sequence(sequence)
+            ServiceListeners::PublishingApiAssociatedDocuments
+              .expects(:process)
+              .with(unpublished_edition_b, "republish")
+              .once
+              .in_sequence(sequence)
+            Whitehall::PublishingApi
+              .expects(:save_draft)
+              .with(draft_edition, "republish", bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            ServiceListeners::PublishingApiAssociatedDocuments
+              .expects(:process)
+              .with(draft_edition, "republish")
+              .once
+              .in_sequence(sequence)
+
+            Whitehall::PublishingApi.expects(:publish).never
+            Whitehall::PublishingApi.expects(:patch_links).never
+
+            PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+          end
+
+          context "but the draft edition is invalid" do
+            setup { draft_edition.stubs(:valid?).returns(false) }
+
+            it "refreshes the latest unpublished edition but not the draft edition" do
+              sequence = sequence("unpublish; handle unpublished attachments")
+
+              PublishingApiUnpublishingWorker
+                .any_instance
+                .expects(:perform)
+                .with(unpublished_edition_b.unpublishing.id, unpublished_edition_b.draft?)
+                .once
+                .in_sequence(sequence)
+              ServiceListeners::PublishingApiAssociatedDocuments
+                .expects(:process)
+                .with(unpublished_edition_b, "republish")
+                .once
+                .in_sequence(sequence)
+
+              Whitehall::PublishingApi.expects(:save_draft).never
+              ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(draft_edition, "republish").never
+              Whitehall::PublishingApi.expects(:publish).never
+              Whitehall::PublishingApi.expects(:patch_links).never
+
+              PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+            end
+          end
+        end
+      end
+
+      context "and the live edition is withdrawn" do
+        let(:live_edition) { withdrawn_edition }
+        let(:pre_publication_edition) { nil }
+
+        setup { document.editions.stubs(:unpublished).returns([]) }
+
+        it "refreshes the withdrawn edition" do
+          sequence = sequence("republish; handle attachments; unpublish; handle attachments")
+
+          Whitehall::PublishingApi
+            .expects(:publish)
+            .with(withdrawn_edition, "republish", bulk_publishing: false)
+            .once
+            .in_sequence(sequence)
+          ServiceListeners::PublishingApiAssociatedDocuments
+            .expects(:process)
+            .with(withdrawn_edition, "republish")
+            .once
+            .in_sequence(sequence)
+          PublishingApiUnpublishingWorker
+            .any_instance
+            .expects(:perform)
+            .with(withdrawn_edition.unpublishing.id, withdrawn_edition.draft?)
+            .once
+            .in_sequence(sequence)
+          ServiceListeners::PublishingApiAssociatedDocuments
+            .expects(:process)
+            .with(withdrawn_edition, "republish")
+            .once
+            .in_sequence(sequence)
+
+          Whitehall::PublishingApi.expects(:save_draft).never
+          Whitehall::PublishingApi.expects(:patch_links).never
 
           PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+        end
+      end
+
+      context "none of which are unpublished or withdrawn" do
+        let(:live_edition) { nil }
+        let(:pre_publication_edition) { draft_edition }
+
+        context "and there's only a draft edition" do
+          setup { document.editions.stubs(:unpublished).returns([]) }
+
+          it "patches links for, then refreshes, the draft edition" do
+            sequence = sequence("patch links; save; handle attachments")
+
+            Whitehall::PublishingApi
+              .expects(:patch_links)
+              .with(draft_edition, bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            Whitehall::PublishingApi
+              .expects(:save_draft)
+              .with(draft_edition, "republish", bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            ServiceListeners::PublishingApiAssociatedDocuments
+              .expects(:process)
+              .with(draft_edition, "republish")
+              .once
+              .in_sequence(sequence)
+
+            Whitehall::PublishingApi.expects(:publish).never
+            PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+
+            PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+          end
+
+          context "but the draft edition is invalid" do
+            setup { draft_edition.stubs(:valid?).returns(false) }
+
+            it "patches links for, but doesn't refresh, the draft edition" do
+              Whitehall::PublishingApi
+                .expects(:patch_links)
+                .with(draft_edition, bulk_publishing: false)
+                .once
+
+              Whitehall::PublishingApi.expects(:save_draft).never
+              ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).never
+              Whitehall::PublishingApi.expects(:publish).never
+              PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+
+              PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+            end
+          end
+        end
+
+        context "and there's only a published edition" do
+          let(:live_edition) { published_edition }
+          let(:pre_publication_edition) { nil }
+
+          setup { document.editions.stubs(:unpublished).returns([]) }
+
+          it "patches links for, then refreshes, the published edition" do
+            sequence = sequence("patch links; republish; handle attachments")
+
+            Whitehall::PublishingApi
+              .expects(:patch_links)
+              .with(published_edition, bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            Whitehall::PublishingApi
+              .expects(:publish)
+              .with(published_edition, "republish", bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            ServiceListeners::PublishingApiAssociatedDocuments
+              .expects(:process)
+              .with(published_edition, "republish")
+              .once
+              .in_sequence(sequence)
+
+            Whitehall::PublishingApi.expects(:save_draft).never
+            PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+
+            PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+          end
+        end
+
+        context "and there's both a published and draft edition" do
+          let(:live_edition) { published_edition }
+          let(:pre_publication_edition) { draft_edition }
+
+          setup { document.editions.stubs(:unpublished).returns([]) }
+
+          it "patches links for the published edition, then refreshes the published then draft editions" do
+            sequence = sequence("patch links; republish live edition; handle live attachments; save draft; handle draft attachments")
+
+            Whitehall::PublishingApi
+              .expects(:patch_links)
+              .with(published_edition, bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            Whitehall::PublishingApi
+              .expects(:publish)
+              .with(published_edition, "republish", bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            ServiceListeners::PublishingApiAssociatedDocuments
+              .expects(:process)
+              .with(published_edition, "republish")
+              .once
+              .in_sequence(sequence)
+            Whitehall::PublishingApi
+              .expects(:save_draft)
+              .with(draft_edition, "republish", bulk_publishing: false)
+              .once
+              .in_sequence(sequence)
+            ServiceListeners::PublishingApiAssociatedDocuments
+              .expects(:process)
+              .with(draft_edition, "republish")
+              .once
+              .in_sequence(sequence)
+
+            PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+
+            PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+          end
+
+          context "but the draft edition is invalid" do
+            setup { draft_edition.stubs(:valid?).returns(false) }
+
+            it "patches links for, then refreshes, the published edition, but not the draft edition" do
+              sequence = sequence("patch links; republish live edition; handle live attachments")
+
+              Whitehall::PublishingApi
+                .expects(:patch_links)
+                .with(published_edition, bulk_publishing: false)
+                .once
+                .in_sequence(sequence)
+              Whitehall::PublishingApi
+                .expects(:publish)
+                .with(published_edition, "republish", bulk_publishing: false)
+                .once
+                .in_sequence(sequence)
+              ServiceListeners::PublishingApiAssociatedDocuments
+                .expects(:process)
+                .with(published_edition, "republish")
+                .once
+                .in_sequence(sequence)
+
+              Whitehall::PublishingApi.expects(:save_draft).never
+              ServiceListeners::PublishingApiAssociatedDocuments.expects(:process).with(draft_edition, "republish").never
+              PublishingApiUnpublishingWorker.any_instance.expects(:perform).never
+
+              PublishingApiDocumentRepublishingWorker.new.perform(document.id)
+            end
+          end
         end
       end
     end
