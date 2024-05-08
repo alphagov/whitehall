abda7e8f9af3e0334f8181f7766dfd9da534fbc8
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-05-08 10:08:50 +0100

    Allow links in editorial remarks
    This will allow links to be included in editorial remarks, e.g. when
    logging where a corporate information page has been migrated from.

--- test/components/admin/editions/editorial_remark_component_test.rb
+++ test/components/admin/editions/editorial_remark_component_test.rb
@@ -22,4 +22,18 @@ class Admin::Editions::EditorialRemarkComponentTest < ViewComponent::TestCase
     assert_equal page.all("p")[0].text.strip, editorial_remark.body
     assert_equal page.all("p")[1].text.strip, "1 January 2020 11:11am by User (removed)"
   end
+
+  test "it includes HTML for links as actual links" do
+    editorial_remark = build_stubbed(:editorial_remark, body: "Remark with <a href=\"/foo\">a link</a>.")
+    render_inline(Admin::Editions::EditorialRemarkComponent.new(editorial_remark:))
+
+    assert_equal page.all("a").first.text, "a link"
+  end
+
+  test "it does not render other HTML" do
+    editorial_remark = build_stubbed(:editorial_remark, body: "Remark with <script>a dodgy script</script>.")
+    render_inline(Admin::Editions::EditorialRemarkComponent.new(editorial_remark:))
+
+    assert_empty page.all("script")
+  end
 end
