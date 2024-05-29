2e9bdcaf899414b4ca784e2f3ad1f5d7e729fa8a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-05-29 13:45:53 +0100

    Present `auth_bypass_ids` for editionable worldwide organisation offices
    These values were missed from the presenter. By sending them to
    Publishing API, we are able to offer a shareable preview feature to
    publishers.
    
    We are using the `auth_bypass_id` from the parents worldwide
    organisation to create a seemless journey where users only need a single
    token to see all offices that are part of the organisation.
    
    This presenter is used for worldwide offices that are associated with
    both editionable and non-editionable worldwide organisations. Therefore
    adding a test case for editionable worldwide organisations that was
    missing.

--- app/presenters/publishing_api/worldwide_office_presenter.rb
+++ app/presenters/publishing_api/worldwide_office_presenter.rb
@@ -29,6 +29,7 @@ module PublishingApi
         schema_name: "worldwide_office",
       )
 
+      content.merge!(auth_bypass_ids: [item.edition.auth_bypass_id]) if item.edition
       content.merge!(PayloadBuilder::PolymorphicPath.for(item))
     end
 
