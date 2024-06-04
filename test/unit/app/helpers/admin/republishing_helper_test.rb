2e1ab3fc2859a39e686fbb41f323605472ccb59a
Ynda Jas <yndajas@gmail.com>
2024-06-04 13:00:07 +0100

    Relocate method-specific test setup
    We don't seem to be able to use `context` here, perhaps because of it
    inherited from `ActionView::TestCase` instead of
    `ActiveSupport::TestCase`. This setup is specific to this method, so it
    isn't relevant to tests of other methods

--- test/unit/app/helpers/admin/republishing_helper_test.rb
+++ test/unit/app/helpers/admin/republishing_helper_test.rb
@@ -1,16 +1,16 @@
 require "test_helper"
 
 class Admin::RepublishingHelperTest < ActionView::TestCase
-  setup do
-    @first_row = republishing_index_bulk_republishing_rows.first
-  end
-
   test "#republishing_index_bulk_republishing_rows capitalises the first letter of the bulk content type" do
-    assert_equal @first_row.first[:text], "All documents"
+    first_bulk_content_type = republishing_index_bulk_republishing_rows.first.first[:text]
+
+    assert_equal first_bulk_content_type, "All documents"
   end
 
   test "#republishing_index_bulk_republishing_rows creates a link to the specific bulk republishing confirmation page" do
+    first_link = republishing_index_bulk_republishing_rows.first[1][:text]
     expected_link = '<a id="all-documents" class="govuk-link" href="/government/admin/republishing/bulk/all-documents/confirm">Republish <span class="govuk-visually-hidden">all documents</span></a>'
-    assert_equal @first_row[1][:text], expected_link
+
+    assert_equal first_link, expected_link
   end
 end
