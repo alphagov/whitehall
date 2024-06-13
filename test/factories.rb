12e1f5022f4785f67d170c3ee35147cd5c210f4d
pezholio <pezholio@gmail.com>
2024-06-13 09:44:06 +0100

    Include all tests and factories from packages
    To improve visibility, tests will be included in their relevant
    pacakages, so we’ll need to ensure all test-related files are
    included during the test run

--- test/factories.rb
+++ test/factories.rb
@@ -2,4 +2,7 @@ def image_fixture_file
   @image_fixture_file ||= File.open(Rails.root.join("test/fixtures/minister-of-funk.960x640.jpg"))
 end
 
-Dir[Rails.root.join("test/factories/*.rb")].sort.each { |f| require f }
+[
+  Dir[Rails.root.join("test/factories/*.rb")],
+  Dir[Rails.root.join("packages/**/test/factories/*.rb")],
+].flatten.sort.each { |f| require f }
