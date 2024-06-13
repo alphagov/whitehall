c2ae68b8a0933cfcdb9c4d09f69304d09ade5cd4
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-06-13 15:41:48 +0100

    Revert custom visual editor exit tracking
    Now that we are tracking all buttons we can remove the custom tracking for the visual editor exit button, maintaining only the custom event and tool names.

--- features/step_definitions/visual_editor_steps.rb
+++ features/step_definitions/visual_editor_steps.rb
@@ -82,7 +82,7 @@ And(/^I save the HTML attachment$/) do
 end
 
 And(/^I exit the visual editor experience$/) do
-  click_on I18n.t("admin.visual_editor.exit.button")
+  click_on "Continue editing in markdown"
 end
 
 When(/^I edit a pre-existing publication$/) do
