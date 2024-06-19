a578dcd7ba04bffc70e15698b31646a22f5ad736
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-06-19 14:11:12 +0100

    Make Publishing API calls directly instead of going via worker
    We're seeing occasional race condition issues in Publishing API,
    whereby the `payload_version` in Content Store is ahead of what's
    being sent from Publishing API. We suspect this happens when
    putting content, patching links and publishing content happens in
    quick succession.
    
    In hindsight, the patch/publish calls only need to happen when
    reshuffle mode is toggled. The link patching only needs to happen
    in the necessary callbacks, e.g. when ministers have been
    reordered.
    
    This commit reduces the number of calls we make to Publishing API,
    and should mean we can delete a bunch of (now unused) code in the
    next commit.

--- test/unit/app/models/sitewide_setting_test.rb
+++ test/unit/app/models/sitewide_setting_test.rb
@@ -1,14 +1,17 @@
 require "test_helper"
 
 class SitewideSettingTest < ActiveSupport::TestCase
+  def setup
+    @content_id = "324e4708-2285-40a0-b3aa-cb13af14ec5f"
+  end
+
   test "enabling reshuffle mode republishes custom ministers index and how government work pages" do
     setting = create(:sitewide_setting, key: :minister_reshuffle_mode, on: true)
 
-    mock_worker = mock
-    PresentPageToPublishingApiWorker.stubs(:new).returns(mock_worker)
-    mock_worker.expects(:perform).with("PublishingApi::HowGovernmentWorksEnableReshufflePresenter", true).once
-    mock_worker.expects(:perform).with("PublishingApi::MinistersIndexEnableReshufflePresenter", true).once
-    mock_worker.expects(:perform).with("PublishingApi::MinistersIndexPresenter", false).once
+    Services.publishing_api.expects(:put_content).with(@content_id, PublishingApi::MinistersIndexEnableReshufflePresenter.new.content).once
+    Services.publishing_api.expects(:publish).with(@content_id, nil, locale: "en").once
+    Services.publishing_api.expects(:put_content).with(@content_id, PublishingApi::MinistersIndexPresenter.new.content).once
+    PresentPageToPublishingApiWorker.expects(:perform_async).with("PublishingApi::HowGovernmentWorksEnableReshufflePresenter").once
 
     setting.republish_downstream_if_reshuffle
   end
@@ -16,8 +19,8 @@ class SitewideSettingTest < ActiveSupport::TestCase
   test "disabling reshuffle mode republishes ministers index and how government work pages" do
     setting = create(:sitewide_setting, key: :minister_reshuffle_mode, on: false)
 
-    PresentPageToPublishingApiWorker.expects(:perform_async).with("PublishingApi::HowGovernmentWorksPresenter", true).once
-    PresentPageToPublishingApiWorker.expects(:perform_async).with("PublishingApi::MinistersIndexPresenter", true).once
+    PresentPageToPublishingApiWorker.expects(:perform_async).with("PublishingApi::HowGovernmentWorksPresenter").once
+    Services.publishing_api.expects(:publish).with(@content_id, nil, locale: "en").once
 
     setting.republish_downstream_if_reshuffle
   end
