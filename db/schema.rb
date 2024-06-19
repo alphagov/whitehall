359c1ea93722c792f44874d2e8412528dfa21c69
Ynda Jas <yndajas@gmail.com>
2024-06-19 13:50:35 +0100

    Add `content_ids` to `RepublishingEvent`
    These will be used to capture the documents requested for republishing
    for `all_documents_by_content_ids` events
    
    One of the main reasons for capturing identifying information for bulk
    republishing events that require extra input is to notify users when a
    matching event was last triggered (this is a planned iteration of the
    UI), so that they can make an informed decision on whether to request a
    new republishing. This is perhaps less useful for this purpose, since we
    could have partially matching arrays of content IDs across requests, and
    we probably don't want to go as far as searching through all events of
    this type to find the last appearance for each content ID in a list.
    (For other republishing events that require extra input, there's a
    single identifier for the whole event: a content ID, an organisation, or
    a type.) However, this will still contribute to a detailed, searchable
    record of which content has been requested for republishing and when
    
    This is represented as JSON in the database as we're using MySQL2, which
    doesn't support array columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_18_123401) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_164903) do
   create_table "assets", charset: "utf8mb3", force: :cascade do |t|
     t.string "asset_manager_id", null: false
     t.string "variant", null: false
@@ -867,6 +867,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_123401) do
     t.integer "bulk_content_type"
     t.string "content_type"
     t.string "organisation_id"
+    t.json "content_ids"
     t.index ["user_id"], name: "index_republishing_events_on_user_id"
   end
 
--- test/unit/app/models/republishing_event_test.rb
+++ test/unit/app/models/republishing_event_test.rb
@@ -51,5 +51,48 @@ class RepublishingEventTest < ActiveSupport::TestCase
         end
       end
     end
+
+    describe "content_ids" do
+      context "for an `all_documents_by_content_ids` bulk republishing event" do
+        test "should be valid if content IDs is a non-empty array" do
+          event = build(:republishing_event, :bulk, bulk_content_type: "all_documents_by_content_ids", content_ids: %w[1234 5678])
+          assert event.valid?
+        end
+
+        test "should be invalid if content IDs is a hash" do
+          event = build(:republishing_event, :bulk, bulk_content_type: "all_documents_by_content_ids", content_ids: {})
+          assert_not event.valid?
+          assert_includes event.errors.full_messages, "Content ids is not an array"
+        end
+
+        test "should be invalid if content IDs is `nil`" do
+          event = build(:republishing_event, :bulk, bulk_content_type: "all_documents_by_content_ids", content_ids: nil)
+          assert_not event.valid?
+          assert_includes event.errors.full_messages, "Content ids is not an array"
+        end
+
+        test "should be invalid if content IDs is an empty array" do
+          event = build(:republishing_event, :bulk, bulk_content_type: "all_documents_by_content_ids", content_ids: [])
+          assert_not event.valid?
+          assert_includes event.errors.full_messages, "Content ids is not a non-empty array"
+        end
+
+        test "should be invalid if content IDs is an array of integers" do
+          event = build(:republishing_event, :bulk, bulk_content_type: "all_documents_by_content_ids", content_ids: [1234, 5678])
+          assert_not event.valid?
+          assert_includes event.errors.full_messages, "Content ids is not a non-empty array of strings"
+        end
+      end
+
+      context "for any other republishing event" do
+        test "must not be present" do
+          bulk_event = build(:republishing_event, :bulk, bulk_content_type: "all_documents", content_ids: %w[1234 5678])
+          assert_not bulk_event.valid?
+
+          non_bulk_event = build(:republishing_event, content_ids: %w[1234 5678])
+          assert_not non_bulk_event.valid?
+        end
+      end
+    end
   end
 end
