118291bbc1e659eff10c738c7b90a420c4039790
Harriet H-W <harriethw@gmail.com>
2024-06-28 16:10:07 +0100

    add an index endpoint for Content Block Editions
    adds a simple JSON response listing all Content
    Block Editions.

--- lib/engines/content_object_store/config/routes.rb
+++ lib/engines/content_object_store/config/routes.rb
@@ -2,5 +2,7 @@ ContentObjectStore::Engine.routes.draw do
   namespace :content_object_store, path: "/" do
     resources :health_check, path: "health-check", only: %i[index]
     root to: "health_check#index", via: :get
+
+    resources :content_block_editions, path: "content-block-editions", only: %i[index]
   end
 end
---
+++ lib/engines/content_object_store/test/integration/content_block_editions_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+require "capybara/rails"
+
+class ContentBlockEditionsTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  test "#index returns all Content Block Editions" do
+    content_block_document = create(:content_block_document)
+    create(
+      :content_block_edition,
+      details: '"email_address":"example@example.com"',
+      content_block_document_id: content_block_document.id,
+    )
+    visit "/government/admin/content-object-store/content-block-editions"
+    assert_text '"email_address":"example@example.com"'
+  end
+end
