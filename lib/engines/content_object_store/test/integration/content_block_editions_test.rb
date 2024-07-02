43d8be9d5a2815dfd59c6cb50971e4e863aca649
pezholio <pezholio@gmail.com>
2024-07-02 16:41:30 +0100

    List all valid schemas if a block_type is blank
    When accessing the `content-block-editions/new` route, present the user
    with a list of valid object types to choose from

--- lib/engines/content_object_store/test/integration/content_block_editions_test.rb
+++ lib/engines/content_object_store/test/integration/content_block_editions_test.rb
@@ -3,15 +3,34 @@ require "capybara/rails"
 
 class ContentBlockEditionsTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
+  extend Minitest::Spec::DSL
 
-  test "#index returns all Content Block Editions" do
-    content_block_document = create(:content_block_document)
-    create(
-      :content_block_edition,
-      details: '"email_address":"example@example.com"',
-      content_block_document_id: content_block_document.id,
-    )
-    visit "/government/admin/content-object-store/content-block-editions"
-    assert_text '"email_address":"example@example.com"'
+  describe "#index" do
+    test "it returns all Content Block Editions" do
+      content_block_document = create(:content_block_document)
+      create(
+        :content_block_edition,
+        details: '"email_address":"example@example.com"',
+        content_block_document_id: content_block_document.id,
+      )
+      visit "/government/admin/content-object-store/content-block-editions"
+      assert_text '"email_address":"example@example.com"'
+    end
+  end
+
+  describe "#new" do
+    test "it shows a list of all the valid block types" do
+      schemas = [
+        ContentObjectStore::Schema.new("content_block_foo"),
+        ContentObjectStore::Schema.new("content_block_bar"),
+      ]
+
+      ContentObjectStore::SchemaService.expects(:valid_schemas).returns(schemas)
+
+      visit "/government/admin/content-object-store/content-block-editions/new"
+
+      assert_text schemas[0].name
+      assert_text schemas[1].name
+    end
   end
 end
