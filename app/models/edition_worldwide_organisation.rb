07aff47107592865af2f7ad9a3c753c6d82e1f15
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-07-04 09:24:33 +0100

    Remove `EditionWorldwideOrganisation`
    We no longer need to associate editions with worldwide organisations,
    as they don't exist.
    
    Other mechanisms exist for associating editionable worldwide
    organisations with editions.

--- app/models/edition_worldwide_organisation.rb
+++
@@ -1,6 +0,0 @@
-class EditionWorldwideOrganisation < ApplicationRecord
-  belongs_to :edition
-  belongs_to :worldwide_organisation, inverse_of: :edition_worldwide_organisations
-
-  validates :edition, :worldwide_organisation, presence: true
-end
