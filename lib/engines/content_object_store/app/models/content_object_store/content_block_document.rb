221e104f33a1498ea8f1c81cefcf79a53caa1648
pezholio <pezholio@gmail.com>
2024-07-08 15:14:57 +0100

    Remove `SCHEMA_PREFIX` constant from Document
    This isn’t used anywhere

--- lib/engines/content_object_store/app/models/content_object_store/content_block_document.rb
+++ lib/engines/content_object_store/app/models/content_object_store/content_block_document.rb
@@ -1,6 +1,4 @@
 class ContentObjectStore::ContentBlockDocument < ApplicationRecord
-  SCHEMA_PREFIX = "content_block".freeze
-
   has_many :content_block_editions,
            -> { order(created_at: :asc, id: :asc) },
            inverse_of: :content_block_document
