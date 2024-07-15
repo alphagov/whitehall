febe35c552831cb8b79cb4db9ca5d8985b904f0a
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-07-15 15:11:44 +0100

    Add GA4 tracking handler to visual editor buttons
    Trigger a GA4 tracking event when the buttons on the Visual Editor toolbar are clicked.

--- spec/javascripts/admin/modules/ga4-visual-editor-event-handlers.spec.js
+++ spec/javascripts/admin/modules/ga4-visual-editor-event-handlers.spec.js
@@ -35,4 +35,43 @@ describe('GOVUK.Modules.Ga4VisualEditorEventHandlers', function () {
       'event_data'
     )
   })
+
+  it('triggers ga4 tracking on visualEditorButtonClick event', function () {
+    document.title = 'Title - Text'
+    const button = document.createElement('button')
+    document.body.appendChild(button)
+
+    const mockGa4SendData = spyOn(
+      window.GOVUK.analyticsGa4.core,
+      'applySchemaAndSendData'
+    )
+
+    const ga4VisualEditorEventHandlers =
+      new GOVUK.Modules.Ga4VisualEditorEventHandlers(document)
+    ga4VisualEditorEventHandlers.init()
+    button.dispatchEvent(
+      new CustomEvent('visualEditorButtonClick', {
+        bubbles: true,
+        detail: {
+          buttonText: 'BUTTON'
+        }
+      })
+    )
+
+    const expectedAttributes = {
+      event_name: 'select_content',
+      type: 'generic_link',
+      text: 'BUTTON',
+      external: 'false',
+      method: 'primary click',
+      section: 'Title',
+      action: 'select',
+      tool_name: 'Visual Editor'
+    }
+
+    expect(mockGa4SendData).toHaveBeenCalledWith(
+      expectedAttributes,
+      'event_data'
+    )
+  })
 })
