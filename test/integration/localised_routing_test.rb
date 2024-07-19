7621fb05a954f28b63f63686a8a60cf56c0c39d0
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2024-07-19 11:08:38 +0100

    Revert "Update `LocalisedRoutingTest`"
    This reverts commit 2b9fafc8fddeff0ed143c8029d356e011f0838a4.

--- test/integration/localised_routing_test.rb
+++ test/integration/localised_routing_test.rb
@@ -2,37 +2,37 @@ require "test_helper"
 
 class RoutingLocaleTest < ActionDispatch::IntegrationTest
   test "#show with no locale" do
-    worldwide_organisation = create(:editionable_worldwide_organisation)
+    worldwide_organisation = create(:worldwide_organisation)
     assert_equal "/world/organisations/#{worldwide_organisation.slug}",
                  worldwide_organisation.public_path
   end
 
   test "#show with english locale doesn't includes the locale in the path" do
-    worldwide_organisation = create(:editionable_worldwide_organisation)
+    worldwide_organisation = create(:worldwide_organisation)
     assert_equal "/world/organisations/#{worldwide_organisation.slug}",
                  worldwide_organisation.public_path(locale: "en")
   end
 
   test "#show with non-english locale includes the locale in the path" do
-    worldwide_organisation = I18n.with_locale(:dr) { create(:editionable_worldwide_organisation) }
+    worldwide_organisation = I18n.with_locale(:dr) { create(:worldwide_organisation) }
     assert_equal "/world/organisations/#{worldwide_organisation.slug}.dr",
                  worldwide_organisation.public_path(locale: "dr")
   end
 
   test "#show with a format includes it in the path" do
-    worldwide_organisation = create(:editionable_worldwide_organisation)
+    worldwide_organisation = create(:worldwide_organisation)
     assert_equal "/world/organisations/#{worldwide_organisation.slug}.json",
                  worldwide_organisation.public_path(format: "json")
   end
 
   test "#show with a english locale and a format includes the format in the path" do
-    worldwide_organisation = create(:editionable_worldwide_organisation)
+    worldwide_organisation = create(:worldwide_organisation)
     assert_equal "/world/organisations/#{worldwide_organisation.slug}.json",
                  worldwide_organisation.public_path(locale: "en", format: "json")
   end
 
   test "#show with a non-english locale and a format includes the locale and format in the path" do
-    worldwide_organisation = I18n.with_locale(:cy) { create(:editionable_worldwide_organisation) }
+    worldwide_organisation = I18n.with_locale(:cy) { create(:worldwide_organisation) }
     assert_equal "/world/organisations/#{worldwide_organisation.slug}.cy.json",
                  worldwide_organisation.public_path(locale: "cy", format: "json")
   end
