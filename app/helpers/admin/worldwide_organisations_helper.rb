2b7029fac54a9511426920e7ba345c3a178e1eb0
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2024-07-19 11:08:51 +0100

    Revert "Remove `worldwide_office_shown_on_home_page_text`"
    This reverts commit fe7ae57019fff719d099a6d5f268d014a6734bbe.

--- app/helpers/admin/worldwide_organisations_helper.rb
+++ app/helpers/admin/worldwide_organisations_helper.rb
@@ -1,4 +1,14 @@
 module Admin::WorldwideOrganisationsHelper
+  def worldwide_office_shown_on_home_page_text(worldwide_organisation, worldwide_office)
+    if worldwide_organisation.is_main_office?(worldwide_office)
+      "Yes (as main office)"
+    elsif worldwide_organisation.office_shown_on_home_page?(worldwide_office)
+      "Yes"
+    else
+      "No"
+    end
+  end
+
   def ordered_worldwide_offices(worldwide_organisation)
     ([worldwide_organisation.main_office] + [worldwide_organisation.home_page_offices] + [worldwide_organisation.other_offices]).flatten.uniq
   end
