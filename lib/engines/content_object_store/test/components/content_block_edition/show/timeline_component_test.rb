9d88271cf12bff35df40013f1ec702ffd95abd81
Harriet H-W <harriethw@gmail.com>
2024-07-22 17:51:25 +0100

    add a timeline component
    This lifts the styling from the MoJ's timeline component [1] with
    an added 'Latest' label.
    
    We'd like to use it to display the audit trail for Documents in a clear
    and accessible way.
    
    [1] https://design-patterns.service.justice.gov.uk/components/timeline/

---
+++ lib/engines/content_object_store/test/components/content_block_edition/show/timeline_component_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class ContentObjectStore::ContentBlockEdition::Show::TimelineComponentTest < ViewComponent::TestCase
+  test "renders a timeline component with a created event" do
+    @user = create(:user)
+    @content_block_edition = create(:content_block_edition)
+    @content_block_version = create(
+      :content_block_version,
+      item: @content_block_edition,
+      whodunnit: @user.id,
+    )
+
+    render_inline(ContentObjectStore::ContentBlockEdition::Show::TimelineComponent.new(
+                    content_block_versions: [@content_block_version],
+                  ))
+
+    assert_selector ".timeline__item", count: 1
+    assert_selector ".timeline__title", text: "Email address created"
+    assert_selector ".timeline__byline", text: "by #{@user.name}"
+    assert_selector "time[datetime='#{@content_block_version.created_at.iso8601}']",
+                    text: @content_block_version.created_at.strftime("%d %B %Y at %I:%M%P")
+  end
+end
