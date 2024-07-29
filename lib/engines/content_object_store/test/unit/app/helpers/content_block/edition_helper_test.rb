93a30d4a924b3ae710ca87549043bff107602936
pezholio <pezholio@gmail.com>
2024-07-29 15:46:44 +0100

    Namespace helpers

--- lib/engines/content_object_store/test/unit/app/helpers/content_block/edition_helper_test.rb
+++ lib/engines/content_object_store/test/unit/app/helpers/content_block/edition_helper_test.rb
@@ -1,17 +1,17 @@
 require "test_helper"
 
-class ContentObjectStore::ContentBlockEditionHelperTest < ActionView::TestCase
+class ContentObjectStore::ContentBlock::EditionHelperTest < ActionView::TestCase
   extend Minitest::Spec::DSL
 
   include Rails.application.routes.url_helpers
-  include ContentObjectStore::ContentBlockEditionHelper
+  include ContentObjectStore::ContentBlock::EditionHelper
 
   describe "link_to_new_block_type" do
     test "it generates a link to create a new block type" do
       schema = build(:content_block_schema, :email_address, body: {})
 
       link = link_to_new_block_type(schema)
-      expected = link_to schema.name, "/government/admin/content-object-store/content-block-editions/#{schema.parameter}/new"
+      expected = link_to schema.name, "/government/admin/content-object-store/content-block/editions/#{schema.parameter}/new"
 
       assert_equal link, expected
     end
