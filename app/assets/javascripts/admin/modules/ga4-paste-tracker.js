69f0d994917e8169bd09d87682454b536012de64
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-07-30 15:59:45 +0100

    Introduce paste tracking
    Add a js module to send GA4 tracking data when the user pastes content

---
+++ app/assets/javascripts/admin/modules/ga4-paste-tracker.js
@@ -0,0 +1,23 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {}
+;(function (Modules) {
+  function Ga4PasteTracker(module) {
+    this.module = module
+  }
+
+  Ga4PasteTracker.prototype.init = function () {
+    window.addEventListener('paste', this.trackPaste.bind(this))
+  }
+
+  Ga4PasteTracker.prototype.trackPaste = function (event) {
+    const data = {
+      event_name: 'paste',
+      type: 'paste',
+      action: 'paste',
+      method: 'browser paste'
+    }
+    window.GOVUK.analyticsGa4.core.applySchemaAndSendData(data, 'event_data')
+  }
+
+  Modules.Ga4PasteTracker = Ga4PasteTracker
+})(window.GOVUK.Modules)
---
+++ spec/javascripts/admin/modules/ga4-paste-tracker.js
@@ -0,0 +1,26 @@
+describe('GOVUK.Modules.Ga4PasteTracker', function () {
+  it('triggers ga4 tracking on paste event', function () {
+    const mockGa4SendData = spyOn(
+      window.GOVUK.analyticsGa4.core,
+      'applySchemaAndSendData'
+    )
+
+    const ga4PasteTracker = new GOVUK.Modules.Ga4VisualEditorEventHandlers(
+      document
+    )
+    ga4PasteTracker.init()
+    window.dispatchEvent(new ClipboardEvent('paste', {}))
+
+    const expectedAttributes = {
+      event_name: 'paste',
+      type: 'paste',
+      action: 'paste',
+      method: 'browser paste'
+    }
+
+    expect(mockGa4SendData).toHaveBeenCalledWith(
+      expectedAttributes,
+      'event_data'
+    )
+  })
+})
