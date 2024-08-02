2d5f35654d2aafd479d489b8b35aa183d1b5b510
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2024-08-02 17:03:01 +0100

    Started building out news article links

--- app/controllers/api/news_articles_controller.rb
+++ app/controllers/api/news_articles_controller.rb
@@ -1,7 +1,7 @@
 class Api::NewsArticlesController < ApplicationController
   def show
     document = Document.find_by(slug: params[:slug])
-    return render "admin/errors/not_found", status: :not_found unless document and document.document_type == 'NewsArticle'
+    return render "admin/errors/not_found", status: :not_found unless document && (document.document_type == "NewsArticle")
 
     presenter = Api::NewsArticlePresenter.new(document.live_edition)
     render json: presenter.content
