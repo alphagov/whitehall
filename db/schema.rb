63c85a8b3b75007fb54c697817b28fc4c4995e94
Harriet H-W <harriethw@gmail.com>
2024-08-16 14:44:42 +0100

    add scheduled_publication datetime to CB Edition
    We want to allow scheduled publication of Content
    Blocks.
    
    This is the Whitehall way of adding the datetime an
    Edition is scheduled for. we will use the `state`
    of an Edition to determine whether an Edition is
    still scheduled, rather than checking this field.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_112802) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_29_130419) do
   create_table "assets", charset: "utf8mb3", force: :cascade do |t|
     t.string "asset_manager_id", null: false
     t.string "variant", null: false
@@ -227,6 +227,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_112802) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "state", default: "draft", null: false
+    t.datetime "scheduled_publication", precision: nil
     t.index ["document_id"], name: "index_content_block_editions_on_document_id"
   end
 
