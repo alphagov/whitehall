3fe0c6324656ee42999df48d51be8a4fd815875b
Tom Hipkin <tomhipkin@gmail.com>
2024-08-20 14:16:09 +0100

    View component for presenting embedded content
    We introduce a new view component that with our new naming of “embed” which will eventually replace `LinkedEditionsTableComponent`.
    
    This component doesn’t support pagination yet. We’ve lost that temporarily during the migration to a new endpoint.

---
+++ lib/engines/content_object_store/test/components/content_block/document/show/host_editions_table_component_test.rb
@@ -0,0 +1,103 @@
+require "test_helper"
+
+class ContentObjectStore::ContentBlock::Document::Show::HostEditionsTableComponentTest < ViewComponent::TestCase
+  extend Minitest::Spec::DSL
+  include Rails.application.routes.url_helpers
+
+  let(:described_class) { ContentObjectStore::ContentBlock::Document::Show::HostEditionsTableComponent }
+
+  let(:host_content_items) do
+    [
+      ContentObjectStore::EmbeddedContentItem.new(
+        title: "Some title",
+        base_path: "/foo",
+        document_type: "document_type",
+        publishing_organisation: {
+          "content_id" => SecureRandom.uuid,
+          "title" => "bar",
+          "base_path" => "/bar",
+        },
+      ),
+    ]
+  end
+
+  it "renders embedded editions" do
+    caption = "Some caption"
+
+    render_inline(
+      described_class.new(
+        caption:,
+        host_content_items:,
+      ),
+    )
+
+    assert_selector ".govuk-table__caption", text: caption
+
+    assert_selector "tbody .govuk-table__row", count: 1
+
+    assert_selector "tbody .govuk-table__cell", text: host_content_items[0].title
+    assert_selector "tbody .govuk-table__cell", text: host_content_items[0].document_type.humanize
+    assert_selector "tbody .govuk-table__cell", text: host_content_items[0].publishing_organisation["title"]
+  end
+
+  context "when the organisation does NOT exist within Whitehall" do
+    it "does not link to the organisation" do
+      render_inline(
+        described_class.new(
+          caption: anything,
+          host_content_items:,
+        ),
+      )
+      assert_no_selector "tbody .govuk-table__cell a", text: host_content_items[0].publishing_organisation["title"]
+    end
+  end
+
+  context "when the organisation DOES exist within Whitehall" do
+    it "links to the organisation instead of printing the name" do
+      organisation = create(:organisation, content_id: host_content_items[0].publishing_organisation["content_id"], name: host_content_items[0].publishing_organisation["title"])
+
+      expected_href = admin_organisation_path(organisation)
+
+      render_inline(
+        described_class.new(
+          caption: anything,
+          host_content_items:,
+        ),
+      )
+      assert_selector "tbody .govuk-table__cell a",
+                      text: host_content_items[0].publishing_organisation["title"]
+
+      assert_link host_content_items[0].publishing_organisation["title"], href: expected_href
+    end
+  end
+
+  context "when the organisation recieved does not have a title or base_path" do
+    let(:host_content_items) do
+      [
+        ContentObjectStore::EmbeddedContentItem.new(
+          title: "Some title",
+          base_path: "/foo",
+          document_type: "document_type",
+          publishing_organisation: {
+            "content_id" => SecureRandom.uuid,
+            "title" => nil,
+            "base_path" => nil,
+          },
+        ),
+      ]
+    end
+
+    it "presents 'Not set'" do
+      caption = "Some caption"
+
+      render_inline(
+        described_class.new(
+          caption:,
+          host_content_items:,
+        ),
+      )
+
+      assert_selector "tbody .govuk-table__cell", text: "Not set"
+    end
+  end
+end
