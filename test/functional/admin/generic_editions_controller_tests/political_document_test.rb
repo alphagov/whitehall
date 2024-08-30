c4edb4c7f3afc6415195de33b5939b59c783b3a4
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2024-08-30 14:14:42 +0100

    Revert "Add an interface for GDS editors and admins to override the government associated with an edition"
    This reverts commit 22c5e18f3e34086929c7e73006666026ce705b4f.
    
    Because we weren't rendering the hidden `political` field for non-managing editor users, they were creating editions with null values in the political column. This then failed Publishing API validation.

--- test/functional/admin/generic_editions_controller_tests/political_document_test.rb
+++ test/functional/admin/generic_editions_controller_tests/political_document_test.rb
@@ -31,12 +31,26 @@ class Admin::GenericEditionsController::PolticalDocumentsTest < ActionController
     assert_equal previous_government, new_draft.reload.government
   end
 
-  view_test "displays the history mode form controls for privileged users " do
+  view_test "displays the political checkbox for privileged users " do
+    create(:current_government)
     login_as :managing_editor
+    published_edition = create(:published_news_article, first_published_at: 2.days.ago)
+    new_draft = create(:news_article, document: published_edition.document, first_published_at: 2.days.ago)
+    get :edit, params: { id: new_draft }
+    assert_select "#edition_political"
+  end
+
+  view_test "doesn't display the political checkbox for non-privileged users " do
     published_edition = create(:published_news_article)
     new_draft = create(:news_article, document: published_edition.document)
     get :edit, params: { id: new_draft }
-    assert_select "#edition_political"
+    assert_select "#edition_political", count: 0
+  end
+
+  view_test "doesn't display the political checkbox on creation" do
+    login_as :managing_editor
+    get :new
+    assert_select "#edition_political", count: 0
   end
 
   def edit_historic_document
