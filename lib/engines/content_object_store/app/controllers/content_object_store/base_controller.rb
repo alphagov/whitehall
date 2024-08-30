1fbb8cca0888e3ac8a861af86f27ef6c4c5e9971
Harriet H-W <harriethw@gmail.com>
2024-08-30 12:47:41 +0100

    refactor shared controller methods into base controller

--- lib/engines/content_object_store/app/controllers/content_object_store/base_controller.rb
+++ lib/engines/content_object_store/app/controllers/content_object_store/base_controller.rb
@@ -4,4 +4,28 @@ class ContentObjectStore::BaseController < Admin::BaseController
   def check_object_store_feature_flag
     forbidden! unless Flipflop.content_object_store?
   end
+
+  def scheduled_publication_params
+    params.require(:scheduled_at).permit("scheduled_publication(1i)",
+                                         "scheduled_publication(2i)",
+                                         "scheduled_publication(3i)",
+                                         "scheduled_publication(4i)",
+                                         "scheduled_publication(5i)")
+  end
+
+  def edition_params
+    params.require(:content_block_edition)
+          .permit(
+            :organisation_id,
+            :creator,
+            "scheduled_publication(1i)",
+            "scheduled_publication(2i)",
+            "scheduled_publication(3i)",
+            "scheduled_publication(4i)",
+            "scheduled_publication(5i)",
+            document_attributes: %w[title block_type],
+            details: @schema.fields,
+          )
+          .merge!(creator: current_user)
+  end
 end
--- lib/engines/content_object_store/app/controllers/content_object_store/content_block/workflow_controller.rb
+++ lib/engines/content_object_store/app/controllers/content_object_store/content_block/workflow_controller.rb
@@ -41,30 +41,4 @@ class ContentObjectStore::ContentBlock::WorkflowController < ContentObjectStore:
 
     render "content_object_store/content_block/editions/schedule_publishing"
   end
-
-private
-
-  def scheduled_publication_params
-    params.require(:scheduled_at).permit("scheduled_publication(1i)",
-                                         "scheduled_publication(2i)",
-                                         "scheduled_publication(3i)",
-                                         "scheduled_publication(4i)",
-                                         "scheduled_publication(5i)")
-  end
-
-  def edition_params
-    params.require(:content_block_edition)
-          .permit(
-            :organisation_id,
-            :creator,
-            "scheduled_publication(1i)",
-            "scheduled_publication(2i)",
-            "scheduled_publication(3i)",
-            "scheduled_publication(4i)",
-            "scheduled_publication(5i)",
-            document_attributes: %w[title block_type],
-            details: @schema.fields,
-          )
-          .merge!(creator: current_user)
-  end
 end
