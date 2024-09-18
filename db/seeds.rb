774fb8b8183853f0fef82afbd711da0a78133058
Harriet H-W <harriethw@gmail.com>
2024-09-18 11:46:38 +0100

    add Sidekiq Admin permissions to Test User

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ if User.where(name: "Test user").blank?
   gds_organisation_id = "af07d5a5-df63-4ddc-9383-6a666845ebe9"
   User.create!(
     name: "Test user",
-    permissions: ["signin", "GDS Admin", "GDS Editor", "Managing Editor", "Export data"],
+    permissions: ["signin", "GDS Admin", "GDS Editor", "Managing Editor", "Export data", "Sidekiq Admin"],
     organisation_content_id: gds_organisation_id,
     organisation_slug: "government-digital-service",
   )
