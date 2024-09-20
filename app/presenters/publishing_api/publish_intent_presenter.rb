3895f0ab94d4f58628e0c576f80a6f06e02aba05
Harriet H-W <harriethw@gmail.com>
2024-09-20 11:03:15 +0100

    add optional `publishing_app` param in PublishIntentPresenter
    we want to send Publish Intents for Host Content (e.g. Editions that
    are using embedded content/Content Blocks) [1] that is published
    by other apps - this allows an optional app to be passed in, but retains
    the default Whitehall value in order not to break existing functionality.
    
    [1] https://github.com/alphagov/whitehall/pull/9447

--- app/presenters/publishing_api/publish_intent_presenter.rb
+++ app/presenters/publishing_api/publish_intent_presenter.rb
@@ -1,14 +1,15 @@
 module PublishingApi
   class PublishIntentPresenter
-    def initialize(base_path, publish_timestamp)
+    def initialize(base_path, publish_timestamp, publishing_app = Whitehall::PublishingApp::WHITEHALL)
       @base_path = base_path
       @publish_timestamp = publish_timestamp
+      @publishing_app = publishing_app
     end
 
     def as_json
       {
         publish_time: @publish_timestamp,
-        publishing_app: Whitehall::PublishingApp::WHITEHALL,
+        publishing_app: @publishing_app,
         rendering_app: Whitehall::RenderingApp::GOVERNMENT_FRONTEND,
         routes: [{ path: @base_path, type: "exact" }],
       }
