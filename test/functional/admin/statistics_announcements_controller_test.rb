53f80c2e926283804740b41a6543635cdcb07d42
Minno Dang <mndang@thoughtworks.com>
2024-10-07 09:56:28 +0100

    Re-arranging tests for Statistics Announcements Controller
    Unfortunately adding the MiniTest DSL and describe blocks or context blocks did not work - there was a lot of failures with setups and undefined variables. I've opted for adding comments instead to section out the tests to their respective test methods.

--- test/functional/admin/statistics_announcements_controller_test.rb
+++ test/functional/admin/statistics_announcements_controller_test.rb
@@ -9,13 +9,7 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     stub_taxonomy_with_world_taxons
   end
 
-  view_test "GET :new renders a new announcement form" do
-    get :new
-
-    assert_response :success
-    assert_select "input[name='statistics_announcement[title]']"
-  end
-
+  # ==== GET :index ====
   test "GET :index defaults to future-dated announcements by the current user's organisation" do
     @future_announcement = create(
       :statistics_announcement,
@@ -41,6 +35,15 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  # ==== GET :new ====
+  view_test "GET :new renders a new announcement form" do
+    get :new
+
+    assert_response :success
+    assert_select "input[name='statistics_announcement[title]']"
+  end
+
+  # ==== POST :create ====
   test "POST :create saves the announcement to the database and redirects to the dashboard with provisional date" do
     post :create,
          params: {
@@ -99,6 +102,7 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_not StatisticsAnnouncement.any?
   end
 
+  # ==== GET :show ====
   view_test "GET :show renders the details of the announcement" do
     announcement = create(:statistics_announcement)
     stub_publishing_api_expanded_links_with_taxons(announcement.content_id, [])
@@ -108,6 +112,59 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_select "h1.govuk-heading-xl", text: announcement.title
   end
 
+  view_test "GET :show renders when announcement is not tagged to the new taxonomy" do
+    sfa_organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
+
+    announcement = create(
+      :statistics_announcement,
+      organisations: [sfa_organisation],
+    )
+
+    login_as(:user)
+
+    announcement_has_no_expanded_links(announcement.content_id)
+    get :show, params: { id: announcement }
+
+    assert_select ".govuk-warning-text", /You need to add topic tags before you can publish this document./
+    refute_select ".govuk-breadcrumbs__list"
+  end
+
+  view_test "GET :show renders when announcement is tagged to the new taxonomy" do
+    sfa_organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
+
+    announcement = create(
+      :statistics_announcement,
+      organisations: [sfa_organisation],
+    )
+
+    login_as(:user)
+
+    announcement_has_expanded_links(announcement.content_id)
+
+    get :show, params: { id: announcement }
+
+    refute_select ".govuk-warning-text"
+    assert_select ".govuk-breadcrumbs__list-item", "Education, Training and Skills"
+    assert_select ".govuk-breadcrumbs__list-item", "Primary Education"
+  end
+
+  view_test "GET :show show a link to tag to the new taxonomy" do
+    dfe_organisation = create(:organisation, content_id: "ebd15ade-73b2-4eaf-b1c3-43034a42eb37")
+
+    announcement = create(
+      :statistics_announcement,
+      organisations: [dfe_organisation],
+    )
+
+    login_as(:user)
+
+    announcement_has_no_expanded_links(announcement.content_id)
+    get :show, params: { id: announcement }
+
+    assert_select "a[href='#{edit_admin_statistics_announcement_tags_path(announcement.id)}']", "Add tags"
+  end
+
+  # ==== GET :edit ====
   view_test "GET :edit renders the edit form for the  announcement" do
     announcement = create(:statistics_announcement)
     get :edit, params: { id: announcement.id }
@@ -116,6 +173,7 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_select "input[name='statistics_announcement[title]'][value='#{announcement.title}']"
   end
 
+  # ==== PUT :update ====
   test "PUT :update saves changes to the announcement" do
     announcement = create(:statistics_announcement)
     put :update, params: { id: announcement.id, statistics_announcement: { title: "New announcement title" } }
@@ -132,6 +190,7 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_select "ul.govuk-error-summary__list a", text: "Title can't be blank"
   end
 
+  # ==== POST :publish_cancellation ====
   test "POST :publish_cancellation cancels the announcement" do
     announcement = create(:statistics_announcement)
     post :publish_cancellation,
@@ -144,6 +203,20 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert announcement.reload.cancelled?
     assert_equal "Reason", announcement.cancellation_reason
     assert_equal Time.zone.now, announcement.cancelled_at
+  end
+
+  test "POST :publish_cancellation cannot cancel cancelled announcements" do
+    announcement = create(:cancelled_statistics_announcement)
+
+    get :cancel, params: { id: announcement }
+    assert_redirected_to [:admin, announcement]
+
+    post :publish_cancellation,
+         params: {
+           id: announcement.id,
+           statistics_announcement: { cancellation_reason: "Reason" },
+         }
+    assert_redirected_to [:admin, announcement]
   end
 
   view_test "POST :publish_cancellation re-renders cancellation form if changes are not valid" do
@@ -159,6 +232,7 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_select "ul.govuk-error-summary__list a", text: "Cancellation reason must be provided when cancelling an announcement"
   end
 
+  # ==== PATCH :update_cancel_reason ====
   test "PATCH :update_cancel_reason updates the cancellation message" do
     announcement = create(:cancelled_statistics_announcement)
     patch :update_cancel_reason,
@@ -183,72 +257,6 @@ class Admin::StatisticsAnnouncementsControllerTest < ActionController::TestCase
     assert_response :success
     assert_template :cancel_reason
     assert_select "ul.govuk-error-summary__list a", text: "Cancellation reason must be provided when cancelling an announcement"
-  end
-
-  test "cancelled announcements cannot be cancelled" do
-    announcement = create(:cancelled_statistics_announcement)
-
-    get :cancel, params: { id: announcement }
-    assert_redirected_to [:admin, announcement]
-
-    post :publish_cancellation,
-         params: {
-           id: announcement.id,
-           statistics_announcement: { cancellation_reason: "Reason" },
-         }
-    assert_redirected_to [:admin, announcement]
-  end
-
-  view_test "show a link to tag to the new taxonomy" do
-    dfe_organisation = create(:organisation, content_id: "ebd15ade-73b2-4eaf-b1c3-43034a42eb37")
-
-    announcement = create(
-      :statistics_announcement,
-      organisations: [dfe_organisation],
-    )
-
-    login_as(:user)
-
-    announcement_has_no_expanded_links(announcement.content_id)
-    get :show, params: { id: announcement }
-
-    assert_select "a[href='#{edit_admin_statistics_announcement_tags_path(announcement.id)}']", "Add tags"
-  end
-
-  view_test "when announcement is not tagged to the new taxonomy" do
-    sfa_organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
-
-    announcement = create(
-      :statistics_announcement,
-      organisations: [sfa_organisation],
-    )
-
-    login_as(:user)
-
-    announcement_has_no_expanded_links(announcement.content_id)
-    get :show, params: { id: announcement }
-
-    assert_select ".govuk-warning-text", /You need to add topic tags before you can publish this document./
-    refute_select ".govuk-breadcrumbs__list"
-  end
-
-  view_test "when announcement is tagged to the new taxonomy" do
-    sfa_organisation = create(:organisation, content_id: "3e5a6924-b369-4eb3-8b06-3c0814701de4")
-
-    announcement = create(
-      :statistics_announcement,
-      organisations: [sfa_organisation],
-    )
-
-    login_as(:user)
-
-    announcement_has_expanded_links(announcement.content_id)
-
-    get :show, params: { id: announcement }
-
-    refute_select ".govuk-warning-text"
-    assert_select ".govuk-breadcrumbs__list-item", "Education, Training and Skills"
-    assert_select ".govuk-breadcrumbs__list-item", "Primary Education"
   end
 
 private
