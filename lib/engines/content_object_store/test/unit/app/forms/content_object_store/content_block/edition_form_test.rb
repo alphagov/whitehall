7aac4829b629c1da1159c542af443cd27aaff65b
pezholio <pezholio@gmail.com>
2024-10-08 09:41:11 +0100

    Update the edition form to support new and update
    This adds a factory method - `.for`, which returns a particular class
    depending if the request is for a new edition without a document, which
    means we’re creating a brand new block, or an edition with a document,
    which means we’re updating a block.

--- lib/engines/content_object_store/test/unit/app/forms/content_object_store/content_block/edition_form_test.rb
+++ lib/engines/content_object_store/test/unit/app/forms/content_object_store/content_block/edition_form_test.rb
@@ -5,25 +5,68 @@ class ContentObjectStore::ContentBlock::EditionFormTest < ActiveSupport::TestCas
 
   include ContentObjectStore::Engine.routes.url_helpers
 
-  let(:content_block_edition) { create(:content_block_edition, :email_address) }
   let(:schema) { build(:content_block_schema, :email_address, body: { "properties" => { "foo" => "", "bar" => "" } }) }
   let(:result) do
-    ContentObjectStore::ContentBlock::EditionForm.new(
+    ContentObjectStore::ContentBlock::EditionForm.for(
       content_block_edition:,
       schema:,
     )
   end
 
-  it "initializes with the correct attributes from the schema" do
-    expected_attributes = { "foo" => nil, "bar" => nil }
+  describe "when initialized for an edition with an existing document" do
+    let(:content_block_document) { build(:content_block_document, id: 123) }
+    let(:content_block_edition) { build(:content_block_edition, :email_address, document: content_block_document) }
 
-    assert_equal content_block_edition, result.content_block_edition
-    assert_equal schema, result.schema
-    assert_equal expected_attributes, result.attributes
+    let(:result) do
+      ContentObjectStore::ContentBlock::EditionForm.for(
+        content_block_edition:,
+        schema:,
+      )
+    end
+
+    it "initializes with the correct attributes from the schema" do
+      expected_attributes = { "foo" => nil, "bar" => nil }
+
+      assert_equal content_block_edition, result.content_block_edition
+      assert_equal schema, result.schema
+      assert_equal expected_attributes, result.attributes
+    end
+
+    it "sets the correct title" do
+      assert_equal "Change Email address", result.title
+    end
+
+    it "sets the correct urls" do
+      assert_equal content_object_store_content_block_document_path(id: content_block_edition.document.id), result.back_path
+      assert_equal content_object_store_content_block_document_editions_path(document_id: content_block_edition.document.id), result.url
+    end
   end
 
-  it "sets the correct urls" do
-    assert_equal content_object_store_content_block_document_path(id: content_block_edition.document.id), result.back_path
-    assert_equal content_object_store_content_block_document_editions_path(document_id: content_block_edition.document.id), result.url
+  describe "when initialized for an edition without an existing document" do
+    let(:content_block_edition) { build(:content_block_edition, :email_address, document: nil) }
+
+    let(:result) do
+      ContentObjectStore::ContentBlock::EditionForm.for(
+        content_block_edition:,
+        schema:,
+      )
+    end
+
+    it "initializes with the correct attributes from the schema" do
+      expected_attributes = { "foo" => nil, "bar" => nil }
+
+      assert_equal content_block_edition, result.content_block_edition
+      assert_equal schema, result.schema
+      assert_equal expected_attributes, result.attributes
+    end
+
+    it "sets the correct title" do
+      assert_equal "Create a new Email address", result.title
+    end
+
+    it "sets the correct urls" do
+      assert_equal new_content_object_store_content_block_document_path, result.back_path
+      assert_equal content_object_store_content_block_editions_path, result.url
+    end
   end
 end
