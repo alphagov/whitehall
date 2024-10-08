5a213e5efe243c0e095e21287e2f933e8446f20a
pezholio <pezholio@gmail.com>
2024-10-08 09:02:23 +0100

    Update CreateEditionService to accept document ID
    If the document_id is provided, we create an edition for that document,
    rather than create a brand new document.

--- lib/engines/content_object_store/test/unit/app/services/create_edition_service_test.rb
+++ lib/engines/content_object_store/test/unit/app/services/create_edition_service_test.rb
@@ -7,10 +7,11 @@ class ContentObjectStore::CreateEditionServiceTest < ActiveSupport::TestCase
     let(:content_id) { "49453854-d8fd-41da-ad4c-f99dbac601c3" }
     let(:organisation_id) { "f67b4350-35c2-46a8-babf-e39f5a4f2a7e" }
     let(:schema) { build(:content_block_schema, block_type: "content_block_type", body: { "properties" => { "foo" => "", "bar" => "" } }) }
+    let(:new_title) { "New Title" }
     let(:edition_params) do
       {
         document_attributes: {
-          title: "Some Title",
+          title: new_title,
           block_type: "email_address",
         }.with_indifferent_access,
         details: {
@@ -32,12 +33,12 @@ class ContentObjectStore::CreateEditionServiceTest < ActiveSupport::TestCase
                                             .returns(schema)
     end
 
-    test "returns a ContentBlockEdition" do
+    it "returns a ContentBlockEdition" do
       result = ContentObjectStore::CreateEditionService.new(schema).call(edition_params)
       assert_instance_of ContentObjectStore::ContentBlock::Edition, result
     end
 
-    test "it creates a ContentBlockEdition in Whitehall" do
+    it "creates a ContentBlockEdition in Whitehall" do
       assert_changes -> { ContentObjectStore::ContentBlock::Document.count }, from: 0, to: 1 do
         assert_changes -> { ContentObjectStore::ContentBlock::Edition.count }, from: 0, to: 1 do
           ContentObjectStore::CreateEditionService.new(schema).call(edition_params)
@@ -47,7 +48,7 @@ class ContentObjectStore::CreateEditionServiceTest < ActiveSupport::TestCase
       new_document = ContentObjectStore::ContentBlock::Document.find_by!(content_id:)
       new_edition = new_document.editions.first
 
-      assert_equal edition_params[:document_attributes][:title], new_document.title
+      assert_equal new_title, new_document.title
       assert_equal edition_params[:document_attributes][:block_type], new_document.block_type
       assert_equal edition_params[:details], new_edition.details
       assert_equal new_edition.document_id, new_document.id
@@ -55,17 +56,25 @@ class ContentObjectStore::CreateEditionServiceTest < ActiveSupport::TestCase
       assert_equal new_document.latest_edition_id, new_edition.id
     end
 
-    # test "if the Whitehall creation fails, no call to the Publishing API is made" do
-    #   exception = ArgumentError.new("Cannot find schema for block_type")
-    #   raises_exception = ->(*_args) { raise exception }
-    #
-    #   Services.publishing_api.expects(:put_content).never
-    #
-    #   ContentObjectStore::ContentBlock::Edition.stub :create!, raises_exception do
-    #     assert_raises(ArgumentError) do
-    #       ContentObjectStore::CreateEditionService.new(schema).call(nil, {})
-    #     end
-    #   end
-    # end
+    describe "when a document id is provided" do
+      let(:document) { create(:content_block_document, :email_address) }
+      let!(:previous_edition) { create(:content_block_edition, :email_address, document:) }
+
+      it "creates a new edition for that document" do
+        assert_no_changes -> { ContentObjectStore::ContentBlock::Document.count } do
+          assert_changes -> { document.reload.editions.count }, from: 1, to: 2 do
+            ContentObjectStore::CreateEditionService.new(schema).call(edition_params, document_id: document.id)
+          end
+        end
+
+        new_edition = document.editions.last
+
+        assert_equal new_title, document.title
+        assert_equal edition_params[:details], new_edition.details
+        assert_equal new_edition.document_id, document.id
+
+        assert_equal document.latest_edition_id, new_edition.id
+      end
+    end
   end
 end
