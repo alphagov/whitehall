f3e4b3040e1126983badbcf33ca41eff4794d1d5
Harriet H-W <harriethw@gmail.com>
2024-10-10 10:02:52 +0100

    correct name of ContentBlockManager engine in migration
    although it is not good practice to edit past
    migrations, we think this is safe to do as it
    doesn't change the actual migration, and not
    changing it could cause problems/confusion in the
    future.

--- db/migrate/20240725144121_add_latest_edition_info_to_content_block_documents.rb
+++ db/migrate/20240725144121_add_latest_edition_info_to_content_block_documents.rb
@@ -1,6 +1,6 @@
 class AddLatestEditionInfoToContentBlockDocuments < ActiveRecord::Migration[7.1]
   def up
-    ContentObjectStore::ContentBlock::Document.find_each do |document|
+    ContentBlockManager::ContentBlock::Document.find_each do |document|
       if document.latest_edition_id.nil? || document.live_edition_id.nil?
         document.update!(
           latest_edition_id: document.content_block_editions.last.id,
@@ -11,7 +11,7 @@ class AddLatestEditionInfoToContentBlockDocuments < ActiveRecord::Migration[7.1]
   end
 
   def down
-    ContentObjectStore::ContentBlock::Document.update_all(
+    ContentBlockManager::ContentBlock::Document.update_all(
       latest_edition_id: nil,
       live_edition_id: nil,
     )
