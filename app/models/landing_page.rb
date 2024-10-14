fb5cea2c31a485929b44fd81764fd3e7cc0005d1
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-14 15:12:30 +0100

    Add validation to model

--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -1,9 +1,17 @@
 class LandingPage < ApplicationRecord
   after_save :publish_landing_page
+  after_destroy :unpublish_landing_page
+
+  validates :title, :description, presence: true
+  validates :base_path, presence: true, uniqueness: true
 
 private
 
   def publish_landing_page
     PublishLandingPage.call(self)
+  end
+
+  def unpublish_landing_page
+    # TODO
   end
 end
