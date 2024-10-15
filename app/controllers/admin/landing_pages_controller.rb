e66f84fc3439ba69405d2c28126dd3d67032379e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-10-15 15:10:30 +0100

    Prevent any actions on landing pages for non-admins

--- app/controllers/admin/landing_pages_controller.rb
+++ app/controllers/admin/landing_pages_controller.rb
@@ -1,7 +1,13 @@
 class Admin::LandingPagesController < Admin::EditionsController
+  before_action :enforce_edition_permissions!
+
 private
 
   def edition_class
     LandingPage
+  end
+
+  def enforce_edition_permissions!
+    enforce_permission!(:update, @edition)
   end
 end
