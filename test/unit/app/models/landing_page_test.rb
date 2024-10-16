5b0e85c3d67fa035f0c722bdd29271c1b3ba98ce
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-10-16 11:53:29 +0100

    Update model test to improve validation coverage

--- test/unit/app/models/landing_page_test.rb
+++ test/unit/app/models/landing_page_test.rb
@@ -10,4 +10,20 @@ class LandingPageTest < ActiveSupport::TestCase
     landing_page = build(:landing_page, document:)
     assert_equal landing_page.base_path, "/landing-page/test"
   end
+
+  test "landing-page is not valid if base_path is already in use" do
+    create(:document, slug: "/landing-page/test")
+
+    document = build(:document, slug: "/landing-page/test")
+    landing_page = build(:landing_page, document:)
+    assert_not landing_page.valid?
+    assert_equal :base_path, landing_page.errors.first.attribute
+  end
+
+  test "landing-page is not valid if body is not YAML with a root of blocks: " do
+    document = build(:document, slug: "/landing-page/test")
+    landing_page = build(:landing_page, document:, body: "blinks:")
+    assert_not landing_page.valid?
+    assert_equal :body, landing_page.errors.first.attribute
+  end
 end
