99cc2fa619c0f32e000764e1c357ba7e51dd27ca
pezholio <pezholio@gmail.com>
2024-10-17 16:26:50 +0100

    Send `last_edited_by_editor_id` to Publishing API
    This is a new field that will allow us to identify the last person who
    has edited an Edition within the Publishing API.
    
    I’ve added this to the `BaseItemPresenter` rather than each individual
    Presenter, because there is already a lot of duplication in the Edition
    shaped presenters and doing it this way:
    
    a) DRYs things up; and
    b) Gives us flexibility to fetch last edited users from other objects
    in future.

--- test/unit/app/presenters/publishing_api/base_item_presenter_test.rb
+++ test/unit/app/presenters/publishing_api/base_item_presenter_test.rb
@@ -2,19 +2,43 @@ require "test_helper"
 
 module PublishingApi
   class BaseItemPresenterTest < ActiveSupport::TestCase
-    test "it returns the base set of attributes needed by all documents sent to the publishing API" do
-      stubbed_item = stub(title: "A title")
+    extend Minitest::Spec::DSL
 
-      presenter = PublishingApi::BaseItemPresenter.new(stubbed_item, update_type: "major", locale: "fr")
-      expected_hash = {
-        title: stubbed_item.title,
-        locale: "fr",
-        publishing_app: Whitehall::PublishingApp::WHITEHALL,
-        redirects: [],
-        update_type: "major",
-      }
+    describe ".base_attributes" do
+      let(:presenter) { PublishingApi::BaseItemPresenter.new(stubbed_item, update_type: "major", locale: "fr") }
+      let(:expected_hash) do
+        {
+          title: stubbed_item.title,
+          locale: "fr",
+          publishing_app: Whitehall::PublishingApp::WHITEHALL,
+          redirects: [],
+          update_type: "major",
+        }
+      end
 
-      assert_equal presenter.base_attributes, expected_hash
+      context "when the item does not respond to last_author" do
+        let(:stubbed_item) { stub(title: "A title") }
+        let(:last_edited_by_editor_id) { nil }
+
+        it "returns the base set of attributes needed by all documents sent to the publishing API" do
+          assert_equal presenter.base_attributes, expected_hash
+        end
+      end
+
+      context "when the item responds to last_author" do
+        let(:last_edited_by_editor_id) { SecureRandom.uuid }
+        let(:last_author) { stub(uid: last_edited_by_editor_id) }
+        let(:stubbed_item) { stub(title: "A title", last_author:) }
+        let(:updated_expected_hash) do
+          expected_hash.merge(
+            last_edited_by_editor_id:,
+          )
+        end
+
+        it "returns the base set of attributes needed by all documents sent to the publishing API" do
+          assert_equal presenter.base_attributes, updated_expected_hash
+        end
+      end
     end
   end
 end
