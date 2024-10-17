76fbd05cc468ba4aa81d0fb0a1543f5c23c1e305
Harriet H-W <harriethw@gmail.com>
2024-10-17 10:41:08 +0100

    use log.info in DeleteDraftContentBlocksWorker
    this is correct level of logging to appear in remote enviornments.

--- lib/engines/content_block_manager/app/workers/content_block_manager/delete_draft_content_blocks_worker.rb
+++ lib/engines/content_block_manager/app/workers/content_block_manager/delete_draft_content_blocks_worker.rb
@@ -6,12 +6,12 @@ module ContentBlockManager
 
     def perform
       draft_content_editions = ContentBlockManager::ContentBlock::Edition.draft.where("created_at < ?", 60.days.ago).limit(100)
-      puts "Starting to delete #{draft_content_editions.count} draft Content Blocks"
+      logger.info("[delete-draft-content-blocks-debug] Starting to delete #{draft_content_editions.count} draft Content Blocks")
       draft_content_editions.each do |draft_content_edition|
-        puts "Deleting draft Content Block Edition #{draft_content_edition.id}"
+        logger.info("[delete-draft-content-blocks-debug] Deleting draft Content Block Edition #{draft_content_edition.id}")
         ContentBlockManager::DeleteEditionService.new.call(draft_content_edition)
       end
-      puts "Finished deleting draft Content Blocks"
+      logger.info("[delete-draft-content-blocks-debug] Finished deleting draft Content Blocks")
     end
   end
 end
