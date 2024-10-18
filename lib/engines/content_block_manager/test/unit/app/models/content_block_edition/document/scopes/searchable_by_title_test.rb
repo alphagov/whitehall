c24d19d631c6bb747a0188bde0161c976c77b400
Harriet H-W <harriethw@gmail.com>
2024-10-18 15:53:23 +0100

    allow users to search title by keywords not exact phrase
    using the same method as Whitehall meant if a user
    put more than one word in the search it would
    treat that as an entire phrase, but splitting the
    string in the Scope class and then doing a
    lookahead in the regex means it will match the
    words in any order.

--- lib/engines/content_block_manager/test/unit/app/models/content_block_edition/document/scopes/searchable_by_title_test.rb
+++ lib/engines/content_block_manager/test/unit/app/models/content_block_edition/document/scopes/searchable_by_title_test.rb
@@ -10,5 +10,12 @@ class ContentBlockManager::SearchableByTitleTest < ActiveSupport::TestCase
       _document_without_first_keyword = create(:content_block_document, :email_address, title: "this document is about muppets")
       assert_equal [document_with_first_keyword], ContentBlockManager::ContentBlock::Document.with_title("klingons")
     end
+
+    test "should find documents with title containing keywords not in order" do
+      document_with_first_keyword = create(:content_block_document, :email_address, title: "klingons and such")
+      _edition_with_first_keyword = create(:content_block_edition, :email_address, document: document_with_first_keyword)
+      _document_without_first_keyword = create(:content_block_document, :email_address, title: "muppets and such")
+      assert_equal [document_with_first_keyword], ContentBlockManager::ContentBlock::Document.with_title("such klingons")
+    end
   end
 end
