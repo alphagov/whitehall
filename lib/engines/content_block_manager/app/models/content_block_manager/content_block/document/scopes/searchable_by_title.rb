ce20934e9d982df7837431a69fd6360d816a5497
Harriet H-W <harriethw@gmail.com>
2024-10-18 15:53:23 +0100

    allow users to search title by keywords not exact phrase
    using the same method as Whitehall meant if a user
    put more than one word in the search it would
    treat that as an entire phrase, but splitting the
    string in the Scope class and then doing a
    lookahead in the regex means it will match the
    words in any order.

--- lib/engines/content_block_manager/app/models/content_block_manager/content_block/document/scopes/searchable_by_title.rb
+++ lib/engines/content_block_manager/app/models/content_block_manager/content_block/document/scopes/searchable_by_title.rb
@@ -4,8 +4,13 @@ module ContentBlockManager
 
     included do
       scope :with_title,
-            lambda { |*keywords|
-              pattern = "(#{keywords.map { |k| Regexp.escape(k) }.join('|')})"
+            lambda { |keywords|
+              split_keywords = keywords.split
+              pattern = ""
+              split_keywords.map do |k|
+                escaped_word = Regexp.escape(k)
+                pattern += "(?=.*#{escaped_word})"
+              end
               where("title REGEXP :pattern", pattern:)
             }
     end
