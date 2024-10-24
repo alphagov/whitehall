de90d1ed1e32327ce40c3091e5af9eb18c19b106
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-10-24 16:16:53 +0100

    Make republishable_content_types less clever
    Using `Edition.descendants` makes some assumptions about the inheritance
    tree of Edition which might not always be true.
    
    Specifically, at the moment the only classes that get published are
    leaf nodes in the tree - classes without any descendants. In another
    branch, I would like to make a new LandingPage class which inherits
    DocumentCollection. Both LandingPage and DocumentCollection should be
    publishable, so they should both appear in the republishable documents
    list.
    
    The nice thing about the old approach was that if we add models in the
    future, they'd be automatically included (but the tests would fail
    because they had a hardcoded list which would need updating).
    
    I've switched it around, so that the production code hardcodes the
    models (which is nice and simple and easy to reason about), but the
    tests look at the descendants. We'll still get a failing test if we add
    a model that's missing from the hardcoded list, because the tests will
    spot the new model.

--- test/unit/app/helpers/admin/republishing_helper_test.rb
+++ test/unit/app/helpers/admin/republishing_helper_test.rb
@@ -2,20 +2,26 @@ require "test_helper"
 
 class Admin::RepublishingHelperTest < ActionView::TestCase
   test "#republishable_content_types returns a sorted list combining valid document types and other publishable content types" do
-    # we need to eager load here to ensure we have all the models
     Rails.application.eager_load!
 
-    expected_content_types = omnipresent_content_types.sort
-    result_minus_test_types = republishable_content_types.reject { |type| content_types[:test_specific].include? type }
+    # Ignore "abstract" Edition descendants
+    unexpected_content_types = %w[Announcement GenericEdition Publicationesque SearchableEdition]
 
-    assert_equal expected_content_types, result_minus_test_types
+    expected_content_types = (
+      Edition.descendants.map(&:to_s) + non_editionable_content_types - unexpected_content_types
+    ).reject { _1.include?("Test::") }.sort
+
+    assert_equal expected_content_types, republishable_content_types
   end
 
   test "#non_editionable_content_types returns a list of non-editionable content types" do
-    # we need to eager load here to ensure we have all the models
     Rails.application.eager_load!
+    expected_non_editionable_models = ApplicationRecord.subclasses
+      .select { |subclass| subclass.included_modules.include? PublishesToPublishingApi }
+      .map(&:to_s)
+      .sort
 
-    assert_equal content_types[:omnipresent_non_editionable], non_editionable_content_types.sort
+    assert_equal expected_non_editionable_models, non_editionable_content_types.sort
   end
 
   test "#republishing_index_bulk_republishing_rows capitalises the first letter of the bulk content type" do
@@ -45,9 +51,6 @@ class Admin::RepublishingHelperTest < ActionView::TestCase
   end
 
   test "#republishable_content_types_select_options creates select options from republishable_content_types" do
-    # we need to eager load here to ensure we have all the models
-    Rails.application.eager_load!
-
     options = republishable_content_types_select_options
 
     assert_includes options, {
@@ -125,46 +128,4 @@ class Admin::RepublishingHelperTest < ActionView::TestCase
 
     assert_equal expected_rows, confirm_documents_by_content_ids_edition_rows([document_a, document_b])
   end
-end
-
-def omnipresent_content_types
-  content_types[:omnipresent_editionable].concat(content_types[:omnipresent_non_editionable])
-end
-
-def content_types
-  { omnipresent_editionable: %w[CallForEvidence
-                                CaseStudy
-                                Consultation
-                                CorporateInformationPage
-                                DetailedGuide
-                                DocumentCollection
-                                WorldwideOrganisation
-                                FatalityNotice
-                                LandingPage
-                                NewsArticle
-                                Publication
-                                Speech
-                                StatisticalDataSet],
-    omnipresent_non_editionable: %w[Contact
-                                    Government
-                                    HistoricalAccount
-                                    OperationalField
-                                    Organisation
-                                    Person
-                                    PolicyGroup
-                                    Role
-                                    RoleAppointment
-                                    StatisticsAnnouncement
-                                    TakePartPage
-                                    TopicalEvent
-                                    TopicalEventAboutPage
-                                    WorldLocationNews],
-    test_specific: %w[GenericEdition
-                      SearchableEdition
-                      Edition::AlternativeFormatProviderTest::EditionWithAlternativeFormat
-                      Edition::AppointmentTest::EditionWithAppointment
-                      Edition::WorldwideOrganisationTest::EditionWithWorldwideOrganisations
-                      Edition::ImagesTest::EditionWithImages
-                      Edition::StatisticalDataSetsTest::EditionWithStatisticalDataSets
-                      Edition::LimitedAccessTest::LimitedByDefaultEdition] }
 end
