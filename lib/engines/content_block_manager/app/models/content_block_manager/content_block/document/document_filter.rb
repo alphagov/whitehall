fd217cfeb7e06f2d129d669a690e84e8bfe24cdb
Harriet H-W <harriethw@gmail.com>
2024-10-25 12:26:19 +0100

    add lead_organisation filter to Content Block Document Filter

--- lib/engines/content_block_manager/app/models/content_block_manager/content_block/document/document_filter.rb
+++ lib/engines/content_block_manager/app/models/content_block_manager/content_block/document/document_filter.rb
@@ -9,6 +9,7 @@ module ContentBlockManager
       documents = documents.live
       documents = documents.with_keyword(@filters[:keyword]) if @filters[:keyword].present?
       documents = documents.where(block_type: @filters[:block_type]) if @filters[:block_type].present?
+      documents = documents.with_lead_organisation(@filters[:lead_organisation]) if @filters[:lead_organisation].present?
       documents
     end
   end
