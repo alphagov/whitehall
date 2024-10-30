a24fe7c38f5d6b2bd2576a5edea780178cd09297
pezholio <pezholio@gmail.com>
2024-10-30 07:45:41 +0000

    Add a local service for pageviews
    This ensures we don’t need to call BigQuery in, for example, local
    environments. In the next commit, we’ll specify the class to call based
    on the presence/absence of the environment variables required to call
    BigQuery.

--- lib/engines/content_block_manager/app/services/content_block_manager/page_views_service.rb
+++ lib/engines/content_block_manager/app/services/content_block_manager/page_views_service.rb
@@ -3,20 +3,6 @@ require "googleauth"
 
 module ContentBlockManager
   class PageViewsService
-    SQL = <<~SQL.freeze
-      SELECT cleaned_page_location,
-      COUNT (DISTINCT ga_sessionid) as unique_pageviews FROM
-      (
-        SELECT cleaned_page_location, ga_sessionid
-        FROM `ga4-analytics-352613.flattened_dataset.partitioned_flattened_events`
-        WHERE event_name = "page_view"
-        AND cleaned_page_location IN UNNEST(@paths)
-        AND event_date BETWEEN @start_date AND @end_date
-      )
-      GROUP BY cleaned_page_location
-    SQL
-    SCOPE = ["https://www.googleapis.com/auth/bigquery"].freeze
-
     attr_reader :paths, :start_date, :end_date
 
     def initialize(paths:)
@@ -25,33 +11,57 @@ module ContentBlockManager
       @start_date = @end_date - 30.days
     end
 
-    def call
-      results.map do |row|
-        PageView.new(path: row[:cleaned_page_location], page_views: row[:unique_pageviews])
+    class Local < PageViewsService
+      def call
+        paths.map do |path|
+          PageView.new(path:, page_views: rand(10_000..2_000_000))
+        end
       end
     end
 
-  private
+    class Google < PageViewsService
+      SQL = <<~SQL.freeze
+        SELECT cleaned_page_location,
+        COUNT (DISTINCT ga_sessionid) as unique_pageviews FROM
+        (
+          SELECT cleaned_page_location, ga_sessionid
+          FROM `ga4-analytics-352613.flattened_dataset.partitioned_flattened_events`
+          WHERE event_name = "page_view"
+          AND cleaned_page_location IN UNNEST(@paths)
+          AND event_date BETWEEN @start_date AND @end_date
+        )
+        GROUP BY cleaned_page_location
+      SQL
+      SCOPE = ["https://www.googleapis.com/auth/bigquery"].freeze
 
-    def results
-      @results ||= bigquery.query SQL, params: { paths:, start_date: start_date.iso8601, end_date: end_date.iso8601 }
-    end
+      def call
+        results.map do |row|
+          PageView.new(path: row[:cleaned_page_location], page_views: row[:unique_pageviews])
+        end
+      end
 
-    def bigquery
-      @bigquery ||= Google::Cloud::Bigquery.new(
-        project_id: ENV["BIGQUERY_PROJECT_ID"],
-        credentials: Google::Auth::ServiceAccountCredentials.make_creds(
-          json_key_io: StringIO.new(credentials.to_json),
-          scope: SCOPE,
-        ),
-      )
-    end
+    private
 
-    def credentials
-      {
-        "client_email" => ENV["BIGQUERY_CLIENT_EMAIL"],
-        "private_key" => ENV["BIGQUERY_PRIVATE_KEY"],
-      }
+      def results
+        @results ||= bigquery.query SQL, params: { paths:, start_date: start_date.iso8601, end_date: end_date.iso8601 }
+      end
+
+      def bigquery
+        @bigquery ||= ::Google::Cloud::Bigquery.new(
+          project_id: ENV["BIGQUERY_PROJECT_ID"],
+          credentials: ::Google::Auth::ServiceAccountCredentials.make_creds(
+            json_key_io: StringIO.new(credentials.to_json),
+            scope: SCOPE,
+          ),
+        )
+      end
+
+      def credentials
+        {
+          "client_email" => ENV["BIGQUERY_CLIENT_EMAIL"],
+          "private_key" => ENV["BIGQUERY_PRIVATE_KEY"],
+        }
+      end
     end
   end
 end
--- lib/engines/content_block_manager/test/unit/app/services/page_views_service_test.rb
+++ lib/engines/content_block_manager/test/unit/app/services/page_views_service_test.rb
@@ -5,69 +5,88 @@ require "googleauth"
 class ContentBlockManager::PageViewsServiceTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
 
-  describe "#call" do
-    let(:client_email) { "foo@example.com" }
-    let(:private_key) { "PRIVATE_KEY" }
-    let(:project_id) { "some-project-id" }
-    let(:credentials) do
-      {
-        "client_email" => client_email,
-        "private_key" => private_key,
-      }
-    end
+  let(:paths) { %w[foo bar] }
+  let(:start_date) { "2024-01-01" }
+  let(:end_date) { "2024-01-31" }
 
-    let(:creds_stub) { stub }
-    let(:bigquery_stub) { stub }
-    let(:stub_response) do
-      [
-        {
-          cleaned_page_location: "foo",
-          unique_pageviews: 123,
-        },
-        {
-          cleaned_page_location: "bar",
-          unique_pageviews: 345,
-        },
-      ]
-    end
+  before do
+    travel_to Date.parse(end_date)
+  end
 
-    let(:paths) { %w[foo bar] }
-    let(:start_date) { "2024-01-01" }
-    let(:end_date) { "2024-01-31" }
-
-    def stub_creds
-      Google::Auth::ServiceAccountCredentials.stubs(:make_creds).with { |args|
-        args[:json_key_io].read == StringIO.new(credentials.to_json).read &&
-          args[:scope] == ContentBlockManager::PageViewsService::SCOPE
-      }.returns(creds_stub)
-    end
-
-    def stub_big_query
-      Google::Cloud::Bigquery.stubs(:new).with(
-        project_id:,
-        credentials: creds_stub,
-      ).returns(bigquery_stub)
-      bigquery_stub.stubs(:query).with(
-        ContentBlockManager::PageViewsService::SQL,
-        params: { paths:, start_date:, end_date: },
-      ).returns(stub_response)
-    end
-
-    before do
-      travel_to Date.parse(end_date)
-      stub_creds
-      stub_big_query
-    end
-
-    it "returns results from BigQuery" do
-      ClimateControl.modify BIGQUERY_PROJECT_ID: project_id, BIGQUERY_CLIENT_EMAIL: client_email, BIGQUERY_PRIVATE_KEY: private_key do
-        result = ContentBlockManager::PageViewsService.new(paths:).call
+  describe "ContentBlockManager::PageViewsService::Local" do
+    describe "#call" do
+      it "returns random results for each path" do
+        result = ContentBlockManager::PageViewsService::Local.new(paths:).call
 
         assert_equal result.count, 2
         assert_equal result.first.path, "foo"
-        assert_equal result.first.page_views, 123
+        assert_includes 10_000..2_000_000, result.first.page_views
         assert_equal result.last.path, "bar"
-        assert_equal result.last.page_views, 345
+        assert_includes 10_000..2_000_000, result.last.page_views
+      end
+    end
+  end
+
+  describe "ContentBlockManager::PageViewsService::Google" do
+    describe "#call" do
+      let(:client_email) { "foo@example.com" }
+      let(:private_key) { "PRIVATE_KEY" }
+      let(:project_id) { "some-project-id" }
+      let(:credentials) do
+        {
+          "client_email" => client_email,
+          "private_key" => private_key,
+        }
+      end
+
+      let(:creds_stub) { stub }
+      let(:bigquery_stub) { stub }
+      let(:stub_response) do
+        [
+          {
+            cleaned_page_location: "foo",
+            unique_pageviews: 123,
+          },
+          {
+            cleaned_page_location: "bar",
+            unique_pageviews: 345,
+          },
+        ]
+      end
+
+      def stub_creds
+        Google::Auth::ServiceAccountCredentials.stubs(:make_creds).with { |args|
+          args[:json_key_io].read == StringIO.new(credentials.to_json).read &&
+            args[:scope] == ContentBlockManager::PageViewsService::Google::SCOPE
+        }.returns(creds_stub)
+      end
+
+      def stub_big_query
+        Google::Cloud::Bigquery.stubs(:new).with(
+          project_id:,
+          credentials: creds_stub,
+          ).returns(bigquery_stub)
+        bigquery_stub.stubs(:query).with(
+          ContentBlockManager::PageViewsService::Google::SQL,
+          params: { paths:, start_date:, end_date: },
+          ).returns(stub_response)
+      end
+
+      before do
+        stub_creds
+        stub_big_query
+      end
+
+      it "returns results from BigQuery" do
+        ClimateControl.modify BIGQUERY_PROJECT_ID: project_id, BIGQUERY_CLIENT_EMAIL: client_email, BIGQUERY_PRIVATE_KEY: private_key do
+          result = ContentBlockManager::PageViewsService::Google.new(paths:).call
+
+          assert_equal result.count, 2
+          assert_equal result.first.path, "foo"
+          assert_equal result.first.page_views, 123
+          assert_equal result.last.path, "bar"
+          assert_equal result.last.page_views, 345
+        end
       end
     end
   end
