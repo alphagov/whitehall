cfe78a8b7087d5f9c0e17151ae78a0f32c8315e7
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-11-05 13:35:36 +0000

    Ensure that image_kind attribute is assigned first
    ... and test the other methods in the mixin

--- app/models/concerns/image_kind.rb
+++ app/models/concerns/image_kind.rb
@@ -2,6 +2,18 @@ module ImageKind
   extend ActiveSupport::Concern
 
   included do
+    def assign_attributes(attributes)
+      # It's important that the image_kind attribute is assigned first, as it is intended to be used by
+      # carrierwave uploaders when they are mounted to work out which image versions to use.
+      image_kind = attributes.delete(:image_kind)
+      ordered_attributes = if image_kind.present?
+                             { image_kind:, **attributes }
+                           else
+                             attributes
+                           end
+      super ordered_attributes
+    end
+
     def image_kind
       attributes.fetch("image_kind", "default")
     end
