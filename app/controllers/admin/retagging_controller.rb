ce26f05fcbb9a55971c932fe920a418cdd1370da
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-11-15 15:30:17 +0000

    fixup

--- app/controllers/admin/retagging_controller.rb
+++ app/controllers/admin/retagging_controller.rb
@@ -5,6 +5,6 @@ class Admin::RetaggingController < Admin::BaseController
 
 private
   def enforce_permissions!
-    enforce_permission!(:administer, :retag_documents)
+    enforce_permission!(:administer, :retag_content)
   end
 end
