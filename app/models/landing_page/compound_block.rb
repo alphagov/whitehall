38c9c2ab87f2cd2046856d4b75ce64b956ea4882
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-11-27 16:49:47 +0000

    Add comment explaining compound blocks

--- app/models/landing_page/compound_block.rb
+++ app/models/landing_page/compound_block.rb
@@ -1,3 +1,18 @@
+# Compound blocks are blocks which have some of their own configuration,
+# alongside a list of child blocks. For example:
+#
+# - type: hero
+#   image: ...
+#   hero_content:
+#     blocks:
+#       - type: govspeak
+#         content: some text
+#
+# hero blocks are Compound blocks because they have their own config (image) as
+# well as children (hero_content -> blocks).
+#
+# Compound blocks have an extra key above the blocks (hero_content, featured_content, card_content etc.)
+# This is referred to here as the "content_block_key"
 class LandingPage::CompoundBlock < LandingPage::BaseBlock
   include ActiveModel::API
 
