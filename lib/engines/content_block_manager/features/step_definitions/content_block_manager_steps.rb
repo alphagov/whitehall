2396ca03414aee450b9639fde134ee3a8bb5fdb1
Harriet H-W <harriethw@gmail.com>
2024-11-27 11:16:38 +0000

    add block type to subheading of preview

--- lib/engines/content_block_manager/features/step_definitions/content_block_manager_steps.rb
+++ lib/engines/content_block_manager/features/step_definitions/content_block_manager_steps.rb
@@ -516,7 +516,7 @@ end
 
 Then("the preview page opens in a new tab") do
   page.switch_to_window(page.windows.last)
-  assert_text "Preview content block"
+  assert_text "Preview email address"
   assert_text "Instances: 1"
   assert_text "Email address: changed@example.com"
   within_frame "preview" do
