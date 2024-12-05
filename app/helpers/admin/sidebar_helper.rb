a36e06937f72470a88a7680f91b1eff4d7421904
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-05 09:44:02 +0000

    WIP

--- app/helpers/admin/sidebar_helper.rb
+++ app/helpers/admin/sidebar_helper.rb
@@ -2,6 +2,7 @@ module Admin::SidebarHelper
   def simple_formatting_sidebar(options = {})
     sidebar_content = []
     sidebar_content << render("admin/editions/govspeak_help", options)
+    sidebar_content << render("admin/editions/landing_page_help", options) if options[:show_landing_page_help]
     sidebar_content << render("admin/editions/style_guidance", options)
     raw sidebar_content.join("\n")
   end
