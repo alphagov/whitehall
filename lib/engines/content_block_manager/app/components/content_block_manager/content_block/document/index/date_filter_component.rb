b922ba8c3785b248dc039582c06dc5325775b716
pezholio <pezholio@gmail.com>
2024-12-10 13:00:32 +0000

    add values from params to date filter
    passing the filters to the date component so it
    can check what its values should be.

--- lib/engines/content_block_manager/app/components/content_block_manager/content_block/document/index/date_filter_component.rb
+++ lib/engines/content_block_manager/app/components/content_block_manager/content_block/document/index/date_filter_component.rb
@@ -1,3 +1,13 @@
 class ContentBlockManager::ContentBlock::Document::Index::DateFilterComponent < ViewComponent::Base
-  def initialize; end
+  def initialize(filters: nil)
+    @filters = filters
+  end
+
+private
+
+  attr_reader :filters
+
+  def date_value(date, date_part)
+    filters&.dig(date, date_part)
+  end
 end
--- lib/engines/content_block_manager/test/components/content_block/document/index/date_filter_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/document/index/date_filter_component_test.rb
@@ -5,12 +5,36 @@ class ContentBlockManager::ContentBlock::Document::Index::DateFilterComponentTes
 
   it "renders from and to dates" do
     render_inline(ContentBlockManager::ContentBlock::Document::Index::DateFilterComponent.new)
-    assert_selector "input[name='last_updated[from(1i)]']"
-    assert_selector "input[name='last_updated[from(2i)]']"
-    assert_selector "input[name='last_updated[from(3i)]']"
+    assert_selector "input[name='last_updated_from[1i]']"
+    assert_selector "input[name='last_updated_from[2i]']"
+    assert_selector "input[name='last_updated_from[3i]']"
 
-    assert_selector "input[name='last_updated[to(1i)]']"
-    assert_selector "input[name='last_updated[to(2i)]']"
-    assert_selector "input[name='last_updated[to(3i)]']"
+    assert_selector "input[name='last_updated_to[1i]']"
+    assert_selector "input[name='last_updated_to[2i]']"
+    assert_selector "input[name='last_updated_to[3i]']"
+  end
+
+  it "keeps the values from the filter params" do
+    filters = {
+      last_updated_from: {
+        "3i" => "1",
+        "2i" => "2",
+        "1i" => "2025",
+      },
+      last_updated_to: {
+        "3i" => "3",
+        "2i" => "4",
+        "1i" => "2026",
+      },
+    }
+    render_inline(ContentBlockManager::ContentBlock::Document::Index::DateFilterComponent.new(filters:))
+
+    assert_selector "input[name='last_updated_from[3i]'][value=1]"
+    assert_selector "input[name='last_updated_from[2i]'][value='2']"
+    assert_selector "input[name='last_updated_from[1i]'][value='2025']"
+
+    assert_selector "input[name='last_updated_to[3i]'][value='3']"
+    assert_selector "input[name='last_updated_to[2i]'][value='4']"
+    assert_selector "input[name='last_updated_to[1i]'][value='2026']"
   end
 end
