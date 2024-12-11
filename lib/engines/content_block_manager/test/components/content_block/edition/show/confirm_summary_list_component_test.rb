d5f2cae7f05f9c6f93287b140bcb488765cd82a3
Harriet H-W <harriethw@gmail.com>
2024-12-11 12:14:14 +0000

    add scheduled date if edition scheduled on review page

--- lib/engines/content_block_manager/test/components/content_block/edition/show/confirm_summary_list_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/edition/show/confirm_summary_list_component_test.rb
@@ -45,4 +45,30 @@ class ContentBlockManager::ContentBlockEdition::Show::ConfirmSummaryListComponen
     assert_selector ".govuk-summary-list__key", text: "Instructions to publishers"
     assert_selector ".govuk-summary-list__value", text: "None"
   end
+
+  describe "when the content block is scheduled" do
+    it "shows the scheduled date time" do
+      organisation = create(:organisation, name: "Department for Example")
+
+      content_block_document = create(:content_block_document, :email_address, title: "Some title")
+
+      content_block_edition = create(
+        :content_block_edition,
+        :email_address,
+        details: { "interesting_fact" => "value of fact" },
+        organisation:,
+        document: content_block_document,
+        scheduled_publication: 2.days.from_now,
+      )
+
+      content_block_edition.schedule!
+
+      render_inline(ContentBlockManager::ContentBlockEdition::Show::ConfirmSummaryListComponent.new(
+                      content_block_edition:,
+                    ))
+
+      assert_selector ".govuk-summary-list__key", text: "Scheduled date and time"
+      assert_selector ".govuk-summary-list__value", text: I18n.l(content_block_edition.scheduled_publication, format: :long_ordinal)
+    end
+  end
 end
