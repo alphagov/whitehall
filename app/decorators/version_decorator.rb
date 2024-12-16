03d05679306eda5e06ac87c3c4596babe7edcca9
pezholio <pezholio@gmail.com>
2024-12-16 15:37:34 +0000

    Use a decorator instead of a presenter
    This better matches what we’re trying to do, and also moves us out
    of the `Query` namespace, as we’ll soon be handling decorating versions
    elsewhere.

---
+++ app/decorators/version_decorator.rb
@@ -0,0 +1,33 @@
+class VersionDecorator < SimpleDelegator
+  def initialize(version, is_first_edition: false, previous_version: nil)
+    @is_first_edition = is_first_edition
+    @preloaded_previous_version = previous_version
+    super(version)
+  end
+
+  def ==(other)
+    self.class == other.class &&
+      id == other.id &&
+      action == other.action
+  end
+
+  def actor
+    user
+  end
+
+  def action
+    case event
+    when "create"
+      @is_first_edition ? "created" : "editioned"
+    else
+      previous_version&.state != state ? state : "updated"
+    end
+  end
+
+  private
+
+  def previous_version
+    # we can avoid n+1 queries by using our preloaded_prev_version
+    @previous_version ||= @preloaded_previous_version || previous
+  end
+end
--- test/unit/app/decorators/version_decorator_test.rb
+++ test/unit/app/decorators/version_decorator_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Queries::VersionPresenterTest < ActiveSupport::TestCase
+class VersionDecoratorTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
 
   let(:user) { build(:user) }
@@ -9,17 +9,26 @@ class Queries::VersionPresenterTest < ActiveSupport::TestCase
   let(:is_first_edition) { false }
   let(:previous_version) { build(:version, user:, item: edition) }
 
-  let(:presenter) { Queries::VersionPresenter.new(version, is_first_edition:, previous_version:) }
+  let(:decorator) { VersionDecorator.new(version, is_first_edition:, previous_version:) }
 
-  describe ".model_name" do
-    it "returns the model name" do
-      assert_equal Queries::VersionPresenter.model_name, ActiveModel::Name.new(Version, nil)
+  describe "#==" do
+    it "is true if the class, ID and action are the same" do
+      other_decorator = VersionDecorator.new(version, is_first_edition:, previous_version:)
+
+      assert decorator == other_decorator
+    end
+
+    it "is not true if the class ID and action are different" do
+      other_version = build(:version, user:, item: edition, id: 444)
+      other_decorator = VersionDecorator.new(other_version, is_first_edition:, previous_version:)
+
+      assert_not decorator == other_decorator
     end
   end
 
   describe "#actor" do
     it "returns the version's user" do
-      assert_equal presenter.actor, version.user
+      assert_equal decorator.actor, version.user
     end
   end
 
@@ -28,14 +37,14 @@ class Queries::VersionPresenterTest < ActiveSupport::TestCase
       let(:version) { build(:version, user:, item: edition, event: "create") }
 
       it "returns `editioned`" do
-        assert_equal presenter.action, "editioned"
+        assert_equal decorator.action, "editioned"
       end
 
       context "and the edition is the first edition" do
         let(:is_first_edition) { true }
 
         it "returns `created`" do
-          assert_equal presenter.action, "created"
+          assert_equal decorator.action, "created"
         end
       end
     end
@@ -45,7 +54,7 @@ class Queries::VersionPresenterTest < ActiveSupport::TestCase
       let(:previous_version) { build(:version, user:, item: edition, state: "some_state") }
 
       it "returns `updated`" do
-        assert_equal presenter.action, "updated"
+        assert_equal decorator.action, "updated"
       end
     end
 
@@ -54,7 +63,7 @@ class Queries::VersionPresenterTest < ActiveSupport::TestCase
       let(:previous_version) { build(:version, user:, item: edition, state: "state1") }
 
       it "returns the version's state" do
-        assert_equal presenter.action, version.state
+        assert_equal decorator.action, version.state
       end
     end
 
@@ -65,7 +74,7 @@ class Queries::VersionPresenterTest < ActiveSupport::TestCase
         loaded_version = build(:version, user:, item: edition)
         version.expects(:previous).returns(loaded_version)
 
-        presenter.action
+        decorator.action
       end
     end
   end
