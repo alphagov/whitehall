fedc0f0cfac84c161978beda13488a449317e708
pezholio <pezholio@gmail.com>
2025-01-06 16:22:10 +0000

    Add a method to fetch editors given a UUID array
    This calls the Signon API’s `get_users` endpoint and casts the results
    to an array of `Editor` objects. If an organisation is present, it
    returns an associated organisation too.

--- lib/engines/content_block_manager/app/models/content_block_manager/host_content_item/editor.rb
+++ lib/engines/content_block_manager/app/models/content_block_manager/host_content_item/editor.rb
@@ -1,6 +1,16 @@
 module ContentBlockManager
   class HostContentItem
     class Editor < Data.define(:uid, :name, :email, :organisation)
+      def self.with_uuids(uuids)
+        Services.signon_api_client.get_users(uuids:).map do |user|
+          new(
+            uid: user["uid"],
+            name: user["name"],
+            email: user["email"],
+            organisation: ContentBlockManager::HostContentItem::Editor::Organisation.from_user_hash(user),
+          )
+        end
+      end
     end
   end
 end
--- lib/engines/content_block_manager/app/models/content_block_manager/host_content_item/editor/organisation.rb
+++ lib/engines/content_block_manager/app/models/content_block_manager/host_content_item/editor/organisation.rb
@@ -2,6 +2,15 @@ module ContentBlockManager
   class HostContentItem
     class Editor
       class Organisation < Data.define(:content_id, :name, :slug)
+        def self.from_user_hash(user)
+          if user["organisation"].present?
+            new(
+              content_id: user["organisation"]["content_id"],
+              name: user["organisation"]["name"],
+              slug: user["organisation"]["slug"],
+            )
+          end
+        end
       end
     end
   end
