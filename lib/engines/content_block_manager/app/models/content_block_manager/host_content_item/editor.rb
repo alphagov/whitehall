578aab7cfe53b4c420ae02f471d753550404fd93
pezholio <pezholio@gmail.com>
2025-01-06 16:20:15 +0000

    Add Editor and Organisation classes for Host items
    This will record assoiated editors and organisations for a given content
    item

---
+++ lib/engines/content_block_manager/app/models/content_block_manager/host_content_item/editor.rb
@@ -0,0 +1,6 @@
+module ContentBlockManager
+  class HostContentItem
+    class Editor < Data.define(:uid, :name, :email, :organisation)
+    end
+  end
+end
---
+++ lib/engines/content_block_manager/app/models/content_block_manager/host_content_item/editor/organisation.rb
@@ -0,0 +1,8 @@
+module ContentBlockManager
+  class HostContentItem
+    class Editor
+      class Organisation < Data.define(:content_id, :name, :slug)
+      end
+    end
+  end
+end
