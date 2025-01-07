b0cd5edee09ec9b16eaf33eefd2074435d2e20ae
Rebecca Pearce <17481621+beccapearce@users.noreply.github.com>
2025-01-07 12:25:05 +0000

    Revert "Remove uses of advisory "

--- lib/govspeak/embedded_content_patterns.rb
+++ lib/govspeak/embedded_content_patterns.rb
@@ -4,6 +4,5 @@ module Govspeak
     ADMIN_EDITION_PATH = %r{/admin/(?:#{Whitehall.edition_route_path_segments.join('|')})/(\d+)}
     ADMIN_ORGANISATION_CIP_PATH = %r{/admin/organisations/([\w-]+)/corporate_information_pages/(\d+)}
     ADMIN_WORLDWIDE_ORGANISATION_CIP_PATH = %r{/admin/worldwide_organisations/([\w-]+)/corporate_information_pages/(\d+)}
-    ADVISORY = /(^@)([\s\S]*?)(@?)(?=(?:^\$CTA|\r?\n\r?\n|^@|$))/m
   end
 end
