dedbac114c53b71092133188a065bd2213cd0d6f
pezholio <pezholio@gmail.com>
2025-01-10 11:32:33 +0000

    Add HostContentItem fetching logic to the model
    This will allow us to keep the fetching of an object tightly coupled
    to the object itself.

---
+++ lib/engines/content_block_manager/test/unit/app/models/host_content_item/items_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class ContentBlockManager::HostContentItem::ItemsTest < ActiveSupport::TestCase
+  extend Minitest::Spec::DSL
+
+  let(:items) { build_list(:host_content_item, 5) }
+  let(:total) { 12 }
+  let(:total_pages) { 2 }
+  let(:rollup) { build(:rollup) }
+  let(:host_content_items) { build(:host_content_items, items:, total:, total_pages:, rollup:) }
+
+  it "delegates array methods to items" do
+    ([].methods - Object.methods).each do |method|
+      assert host_content_items.respond_to?(method)
+    end
+
+    host_content_items.each_with_index do |item, i|
+      assert_equal item, items[i]
+    end
+  end
+
+  it "returns page count values" do
+    assert_equal host_content_items.total, total
+    assert_equal host_content_items.total_pages, total_pages
+  end
+end
