eb2165d52bbc68477747140dc277d4b940fa1234
pezholio <pezholio@gmail.com>
2025-01-14 10:32:46 +0000

    Allow validation of arrays in a details hash
    This tweaks the validator to remove blank fields from arrays of hashes,
    as well a customise the errors so we retain the pointer to the error,
    but ensure the error message reads correctly, i.e instead of
    `My array 0 foo cannot be blank` it reads `Foo cannot be blank`, whilst
    still pointing to `my_array_0_foo`

--- lib/engines/content_block_manager/test/unit/app/validators/details_validator_test.rb
+++ lib/engines/content_block_manager/test/unit/app/validators/details_validator_test.rb
@@ -3,59 +3,159 @@ require "test_helper"
 class ContentBlockManager::DetailsValidatorTest < ActiveSupport::TestCase
   extend Minitest::Spec::DSL
 
-  let(:body) do
-    {
-      "type" => "object",
-      "required" => %w[foo bar],
-      "additionalProperties" => false,
-      "properties" => {
-        "foo" => {
-          "type" => "string",
-          "format" => "email",
-        },
-        "bar" => {
-          "type" => "string",
-          "format" => "date",
-        },
-      },
-    }
-  end
-
   let(:schema) { build(:content_block_schema, body:) }
 
-  test "it validates the presence of fields" do
-    content_block_edition = build(
-      :content_block_edition,
-      :email_address,
-      details: {
-        foo: "",
-        bar: "",
-      },
-      schema:,
-    )
+  describe "with a non-nested schema" do
+    let(:body) do
+      {
+        "type" => "object",
+        "required" => %w[foo bar],
+        "additionalProperties" => false,
+        "properties" => {
+          "foo" => {
+            "type" => "string",
+            "format" => "email",
+          },
+          "bar" => {
+            "type" => "string",
+            "format" => "date",
+          },
+        },
+      }
+    end
 
-    assert_equal content_block_edition.valid?, false
-    assert_equal content_block_edition.errors.full_messages, [
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.blank", attribute: "Foo"),
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.blank", attribute: "Bar"),
-    ]
+    it "validates the presence of fields" do
+      content_block_edition = build(
+        :content_block_edition,
+        :email_address,
+        details: {
+          foo: "",
+          bar: "",
+        },
+        schema:,
+      )
+
+      assert_equal content_block_edition.valid?, false
+      assert_equal content_block_edition.errors.messages, {
+        details_foo: [I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Foo")],
+        details_bar: [I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Bar")],
+      }
+      assert_equal content_block_edition.errors.full_messages, [
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Foo"),
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Bar"),
+      ]
+    end
+
+    it "validates the format of fields" do
+      content_block_edition = build(
+        :content_block_edition,
+        :email_address,
+        details: {
+          foo: "dddd",
+          bar: "ffff",
+        },
+        schema:,
+      )
+
+      assert_equal content_block_edition.valid?, false
+      assert_equal content_block_edition.errors.messages, {
+        details_foo: [I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.invalid", attribute: "Foo")],
+        details_bar: [I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.invalid", attribute: "Bar")],
+      }
+      assert_equal content_block_edition.errors.full_messages, [
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.invalid", attribute: "Foo"),
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.invalid", attribute: "Bar"),
+      ]
+    end
   end
 
-  test "it validates the format of fields" do
-    content_block_edition = build(
-      :content_block_edition,
-      :email_address,
-      details: {
-        foo: "dddd",
-        bar: "ffff",
-      },
-      schema:,
-    )
+  describe "with a nested schema" do
+    let(:body) do
+      {
+        "type" => "object",
+        "required" => %w[my_array],
+        "additionalProperties" => false,
+        "properties" => {
+          "my_array" => {
+            "type" => "array",
+            "items" => {
+              "type" => "object",
+              "required" => %w[foo bar],
+              "additionalProperties" => false,
+              "properties" => {
+                "foo" => {
+                  "type" => "string",
+                  "format" => "email",
+                },
+                "bar" => {
+                  "type" => "string",
+                  "format" => "date",
+                },
+              },
+            },
+          },
+        },
+      }
+    end
 
-    assert_equal content_block_edition.valid?, false
-    assert_equal content_block_edition.errors.full_messages, [
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.invalid", attribute: "Foo"),
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.invalid", attribute: "Bar"),
-    ]
+    it "validates the presence of an array" do
+      content_block_edition = build(
+        :content_block_edition,
+        :email_address,
+        details: {
+          my_array: [],
+        },
+        schema:,
+      )
+
+      assert_equal content_block_edition.valid?, false
+      assert_equal content_block_edition.errors.full_messages, [
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "My array"),
+      ]
+    end
+
+    it "validates the presence of fields in an array" do
+      content_block_edition = build(
+        :content_block_edition,
+        :email_address,
+        details: {
+          my_array: [
+            foo: "",
+            bar: "",
+          ],
+        },
+        schema:,
+      )
+
+      assert_equal content_block_edition.valid?, false
+      assert_equal content_block_edition.errors.messages, {
+        details_my_array_0_foo: [I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Foo")],
+        details_my_array_0_bar: [I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Bar")],
+      }
+      assert_equal content_block_edition.errors.full_messages, [
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Foo"),
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.blank", attribute: "Bar"),
+      ]
+    end
+
+    it "validates the format of fields in an array" do
+      content_block_edition = build(
+        :content_block_edition,
+        :email_address,
+        details: {
+          my_array: [
+            foo: "dddd",
+            bar: "ffff",
+          ],
+        },
+        schema:,
+      )
+
+      assert_equal content_block_edition.valid?, false
+      assert_equal content_block_edition.errors.full_messages, [
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.invalid", attribute: "Foo"),
+        I18n.t("#{ContentBlockManager::DetailsValidator::BASE_TRANSLATION_PATH}.invalid", attribute: "Bar"),
+      ]
+    end
   end
 end
