b1fe3b8ed094d1aef01a52589210dcd6eabed13b
pezholio <pezholio@gmail.com>
2025-01-15 17:08:52 +0000

    Use the form component in the form

--- lib/engines/content_block_manager/test/support/integration_test_helpers.rb
+++ lib/engines/content_block_manager/test/support/integration_test_helpers.rb
@@ -1,6 +1,17 @@
 module ContentBlockManager::IntegrationTestHelpers
   def stub_request_for_schema(block_type)
-    schema = stub(id: "content_block_type", fields: %w[foo bar], name: "schema", body: {}, block_type:)
+    schema = stub(
+      id: "content_block_type",
+      fields: %w[foo bar],
+      name: "schema",
+      body: {
+        "properties" => {
+          "foo" => { "type" => "string" },
+          "bar" => { "type" => "string" },
+        },
+      },
+      block_type:,
+    )
     ContentBlockManager::ContentBlock::Schema.stubs(:find_by_block_type).with(block_type).returns(schema)
     schema
   end
