544fa51b7b6c3f18a3b0a4d7ed3f3d8612357150
Harriet H-W <harriethw@gmail.com>
2025-01-28 11:37:32 +0000

    refactor doc timeline test

--- lib/engines/content_block_manager/test/components/content_block/document/show/document_timeline_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/document/show/document_timeline_component_test.rb
@@ -4,42 +4,44 @@ class ContentBlockManager::ContentBlock::Document::Show::DocumentTimelineCompone
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::UrlHelper
   include ApplicationHelper
+  extend Minitest::Spec::DSL
+  
+  let(:user) { create(:user) }
 
   test "renders a timeline component with events in correct order" do
-    @user = create(:user)
-    @version_1 = create(
+    version_1 = create(
       :content_block_version,
       event: "created",
-      whodunnit: @user.id,
+      whodunnit: user.id,
     )
-    @version_2 = create(
+    version_2 = create(
       :content_block_version,
       event: "updated",
-      whodunnit: @user.id,
+      whodunnit: user.id,
       state: "published",
     )
-    @version_3 = create(
+    version_3 = create(
       :content_block_version,
       event: "updated",
-      whodunnit: @user.id,
+      whodunnit: user.id,
       state: "scheduled",
     )
 
     render_inline(ContentBlockManager::ContentBlock::Document::Show::DocumentTimelineComponent.new(
-                    content_block_versions: [@version_3, @version_2, @version_1],
+                    content_block_versions: [version_3, version_2, version_1],
                   ))
 
     assert_selector ".timeline__item", count: 2
 
     assert_equal "Email address scheduled", page.all(".timeline__title")[0].text
-    assert_equal "by #{linked_author(@user, { class: 'govuk-link' })}", page.all(".timeline__byline")[0].native.inner_html
-    assert_equal  I18n.l(@version_3.created_at, format: :long_ordinal),
-                  page.all("time[datetime='#{@version_3.created_at.iso8601}']")[1].text
+    assert_equal "by #{linked_author(user, { class: 'govuk-link' })}", page.all(".timeline__byline")[0].native.inner_html
+    assert_equal  I18n.l(version_3.created_at, format: :long_ordinal),
+                  page.all("time[datetime='#{version_3.created_at.iso8601}']")[1].text
 
     assert_equal "Email address published", page.all(".timeline__title")[1].text
-    assert_equal "by #{linked_author(@user, { class: 'govuk-link' })}", page.all(".timeline__byline")[1].native.inner_html
-    assert_equal  I18n.l(@version_2.created_at, format: :long_ordinal),
-                  page.all("time[datetime='#{@version_2.created_at.iso8601}']")[1].text
+    assert_equal "by #{linked_author(user, { class: 'govuk-link' })}", page.all(".timeline__byline")[1].native.inner_html
+    assert_equal  I18n.l(version_2.created_at, format: :long_ordinal),
+                  page.all("time[datetime='#{version_2.created_at.iso8601}']")[1].text
 
     assert_no_selector ".govuk-table"
   end
@@ -62,17 +64,16 @@ class ContentBlockManager::ContentBlock::Document::Show::DocumentTimelineCompone
         "previous_value": "old instructions",
       },
     ]
-    @user = create(:user)
-    @version = create(
+    version = create(
       :content_block_version,
       event: "updated",
-      whodunnit: @user.id,
+      whodunnit: user.id,
       state: "scheduled",
       field_diffs: field_diffs,
     )
 
     render_inline(ContentBlockManager::ContentBlock::Document::Show::DocumentTimelineComponent.new(
-                    content_block_versions: [@version],
+                    content_block_versions: [version],
                   ))
 
     assert_equal "old title", page.all("td")[0].text
