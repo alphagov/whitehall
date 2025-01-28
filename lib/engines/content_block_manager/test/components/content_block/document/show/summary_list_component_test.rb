8eb10b7241788a12e711ae7861c0c89d7eb7f2a7
pezholio <pezholio@gmail.com>
2025-01-28 11:54:12 +0000

    Show change note when viewing a block
    Again, if the change is minor, we don’t show the change note

--- lib/engines/content_block_manager/test/components/content_block/document/show/summary_list_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/document/show/summary_list_component_test.rb
@@ -16,6 +16,7 @@ class ContentBlockManager::ContentBlock::Document::Show::SummaryListComponentTes
       state: "published",
       updated_at: 1.day.ago,
       internal_change_note: "Some internal change note",
+      major_change: false,
     )
   end
   let(:content_block_document) { content_block_edition.document }
@@ -23,7 +24,7 @@ class ContentBlockManager::ContentBlock::Document::Show::SummaryListComponentTes
   it "renders a published content block correctly" do
     render_inline(ContentBlockManager::ContentBlock::Document::Show::SummaryListComponent.new(content_block_document:))
 
-    assert_selector ".govuk-summary-list__row", count: 9
+    assert_selector ".govuk-summary-list__row", count: 10
     assert_selector ".govuk-summary-list__actions", count: 1
 
     assert_selector ".govuk-summary-list__key", text: "Email address details"
@@ -50,6 +51,9 @@ class ContentBlockManager::ContentBlock::Document::Show::SummaryListComponentTes
     assert_selector ".govuk-summary-list__key", text: "Instructions to publishers"
     assert_selector ".govuk-summary-list__value", text: "None"
 
+    assert_selector ".govuk-summary-list__key", text: "Do users have to know the content has changed?"
+    assert_selector ".govuk-summary-list__value", text: "No"
+
     assert_selector ".govuk-summary-list__row[data-module='copy-embed-code']", text: "Embed code"
     assert_selector ".govuk-summary-list__row[data-embed-code='#{content_block_document.embed_code}']", text: "Embed code"
     assert_selector ".govuk-summary-list__key", text: "Embed code"
@@ -61,7 +65,7 @@ class ContentBlockManager::ContentBlock::Document::Show::SummaryListComponentTes
 
     render_inline(ContentBlockManager::ContentBlock::Document::Show::SummaryListComponent.new(content_block_document:))
 
-    assert_selector ".govuk-summary-list__row", count: 9
+    assert_selector ".govuk-summary-list__row", count: 10
 
     assert_selector ".govuk-summary-list__key", text: "Status"
     assert_selector ".govuk-summary-list__value", text: "Scheduled for publication at #{I18n.l(content_block_edition.scheduled_publication, format: :long_ordinal)}"
@@ -77,6 +81,23 @@ class ContentBlockManager::ContentBlock::Document::Show::SummaryListComponentTes
 
       assert_selector ".govuk-summary-list__key", text: "Instructions to publishers"
       assert_selector ".govuk-summary-list__value", text: "instructions"
+    end
+  end
+
+  describe "when there is a major change" do
+    it "renders the change note" do
+      content_block_document.latest_edition.major_change = true
+      content_block_document.latest_edition.change_note = "change note"
+
+      render_inline(ContentBlockManager::ContentBlock::Document::Show::SummaryListComponent.new(content_block_document:))
+
+      assert_selector ".govuk-summary-list__row", count: 11
+
+      assert_selector ".govuk-summary-list__key", text: "Do users have to know the content has changed?"
+      assert_selector ".govuk-summary-list__value", text: "Yes"
+
+      assert_selector ".govuk-summary-list__key", text: "Public change note"
+      assert_selector ".govuk-summary-list__value", text: "change note"
     end
   end
 end
