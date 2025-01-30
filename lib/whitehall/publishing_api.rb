40e99dd1486378bdfacbbd2440776275d74042b1
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-01-30 11:52:45 +0000

    Add `unpublish_sync` method to Whitehall::PublishingApi
    We'll be calling this in the next commit.

--- lib/whitehall/publishing_api.rb
+++ lib/whitehall/publishing_api.rb
@@ -154,6 +154,10 @@ module Whitehall
       PublishingApiUnpublishingWorker.perform_async(unpublishing.id)
     end
 
+    def self.unpublish_sync(unpublishing)
+      PublishingApiUnpublishingWorker.new.perform(unpublishing.id)
+    end
+
     def self.save_draft_redirect_async(base_path, redirects, locale = I18n.default_locale.to_s)
       PublishingApiRedirectWorker.perform_async(
         base_path,
