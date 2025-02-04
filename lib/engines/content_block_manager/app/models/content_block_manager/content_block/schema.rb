4f79214513287596af67766d464232174391eab7
pezholio <pezholio@gmail.com>
2025-02-04 16:08:00 +0000

    Allow embedded objects to be returned in schema
    This allows us to pull out an embedded schema from any properties that
    have a `type` of `object`. As we’re making some assumptions about the
    shape of a hash, we raise an error if the shape is not what we expect.
    
    Additionally, we remove any embedded objects from the `fields` method.

--- lib/engines/content_block_manager/app/models/content_block_manager/content_block/schema.rb
+++ lib/engines/content_block_manager/app/models/content_block_manager/content_block/schema.rb
@@ -3,11 +3,29 @@ module ContentBlockManager
     class Schema
       SCHEMA_PREFIX = "content_block".freeze
 
-      VALID_SCHEMAS = %w[email_address postal_address].freeze
+      VALID_SCHEMAS = %w[email_address postal_address pension].freeze
       private_constant :VALID_SCHEMAS
 
-      def self.valid_schemas
-        VALID_SCHEMAS
+      class << self
+        def valid_schemas
+          VALID_SCHEMAS
+        end
+
+        def all
+          @all ||= Services.publishing_api.get_schemas.select { |k, _v|
+            is_valid_schema?(k)
+          }.map { |id, full_schema|
+            full_schema.dig("definitions", "details")&.yield_self { |schema| new(id, schema) }
+          }.compact
+        end
+
+        def find_by_block_type(block_type)
+          all.find { |schema| schema.block_type == block_type } || raise(ArgumentError, "Cannot find schema for #{block_type}")
+        end
+
+        def is_valid_schema?(key)
+          key.start_with?(SCHEMA_PREFIX) && key.end_with?(*valid_schemas)
+        end
       end
 
       attr_reader :id, :body
@@ -26,7 +44,11 @@ module ContentBlockManager
       end
 
       def fields
-        @body["properties"].keys
+        (@body["properties"].to_a - embedded_objects.to_a).to_h.keys
+      end
+
+      def subschema(name)
+        subschemas.find { |s| s.id == name }
       end
 
       def permitted_params
@@ -37,20 +59,29 @@ module ContentBlockManager
         @block_type ||= id.delete_prefix("#{SCHEMA_PREFIX}_")
       end
 
-      def self.all
-        @all ||= Services.publishing_api.get_schemas.select { |k, _v|
-          is_valid_schema?(k)
-        }.map { |id, full_schema|
-          full_schema.dig("definitions", "details")&.yield_self { |schema| new(id, schema) }
-        }.compact
+      class EmbeddedSchema < Schema
+        def initialize(id, body)
+          body = body["patternProperties"]&.values&.first || raise(ArgumentError, "Subschema `#{id}` is invalid")
+          super(id, body)
+        end
+
+        def fields
+          @body["properties"].keys.sort_by { |field| @body["order"]&.index(field) }
+        end
+
+        def block_type
+          @id
+        end
       end
 
-      def self.find_by_block_type(block_type)
-        all.find { |schema| schema.block_type == block_type } || raise(ArgumentError, "Cannot find schema for #{block_type}")
+    private
+
+      def embedded_objects
+        @body["properties"].select { |_k, v| v["type"] == "object" }
       end
 
-      def self.is_valid_schema?(key)
-        key.start_with?(SCHEMA_PREFIX) && key.end_with?(*valid_schemas)
+      def subschemas
+        @subschemas ||= embedded_objects.map { |object| EmbeddedSchema.new(*object) }
       end
     end
   end
