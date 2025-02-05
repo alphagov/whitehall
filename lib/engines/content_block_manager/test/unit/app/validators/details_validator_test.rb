0c6dc53eda047012e253286bc9cae1f817ec234a
pezholio <pezholio@gmail.com>
2025-02-05 14:52:59 +0000

    Update validator to handle nested params
    This updates the validator to marshal the errors into the correct format
    if nested attributes do not meet the validation.

--- lib/engines/content_block_manager/test/unit/app/validators/details_validator_test.rb
+++ lib/engines/content_block_manager/test/unit/app/validators/details_validator_test.rb
@@ -17,6 +17,24 @@ class ContentBlockManager::DetailsValidatorTest < ActiveSupport::TestCase
           "type" => "string",
           "format" => "date",
         },
+        "things" => {
+          "type" => "object",
+          "patternProperties" => {
+            "^[a-z0-9]+(?:-[a-z0-9]+)*$" => {
+              "type" => "object",
+              "required" => %w[my_string],
+              "properties" => {
+                "my_string" => {
+                  "type" => "string",
+                },
+                "something_else" => {
+                  "type" => "string",
+                  "format" => "email",
+                },
+              },
+            },
+          },
+        },
       },
     }
   end
@@ -35,10 +53,10 @@ class ContentBlockManager::DetailsValidatorTest < ActiveSupport::TestCase
     )
 
     assert_equal content_block_edition.valid?, false
-    assert_equal content_block_edition.errors.full_messages, [
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.blank", attribute: "Foo"),
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.blank", attribute: "Bar"),
-    ]
+    errors = content_block_edition.errors
+
+    assert_error errors:, key: :details_foo, type: "blank", attribute: "Foo"
+    assert_error errors:, key: :details_bar, type: "blank", attribute: "Bar"
   end
 
   test "it validates the format of fields" do
@@ -53,9 +71,63 @@ class ContentBlockManager::DetailsValidatorTest < ActiveSupport::TestCase
     )
 
     assert_equal content_block_edition.valid?, false
-    assert_equal content_block_edition.errors.full_messages, [
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.invalid", attribute: "Foo"),
-      I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.invalid", attribute: "Bar"),
-    ]
+    errors = content_block_edition.errors
+
+    assert_equal errors.count, 2
+    assert_error errors:, key: :details_foo, type: "invalid", attribute: "Foo"
+    assert_error errors:, key: :details_bar, type: "invalid", attribute: "Bar"
+  end
+
+  it "validates the presence of nested fields in nested objects" do
+    content_block_edition = build(
+      :content_block_edition,
+      :email_address,
+      details: {
+        foo: "foo@example.com",
+        bar: "2022-01-01",
+        things: {
+          "something-else": {
+            my_string: "",
+            something_else: "",
+          },
+        },
+      },
+      schema:,
+    )
+
+    assert_equal content_block_edition.valid?, false
+
+    errors = content_block_edition.errors
+
+    assert_equal errors.count, 1
+    assert_error errors:, key: :details_things_my_string, type: "blank", attribute: "My string"
+  end
+
+  it "validates the format of nested fields in nested objects" do
+    content_block_edition = build(
+      :content_block_edition,
+      :email_address,
+      details: {
+        foo: "foo@example.com",
+        bar: "2022-01-01",
+        things: {
+          "something-else": {
+            my_string: "something",
+            something_else: "Not an email",
+          },
+        },
+      },
+      schema:,
+    )
+
+    assert_equal content_block_edition.valid?, false
+
+    errors = content_block_edition.errors
+
+    assert_error errors:, key: :details_things_something_else, type: "invalid", attribute: "Something else"
+  end
+
+  def assert_error(errors:, key:, type:, attribute:)
+    assert_equal errors[key], [I18n.t("activerecord.errors.models.content_block_manager/content_block/edition.#{type}", attribute:)]
   end
 end
