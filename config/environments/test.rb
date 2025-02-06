256e5039c1a9fb4eadf50ef625ae8ff9727d63b2
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-02-06 09:46:46 +0000

    Upgrade to Rails 8

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,11 +17,11 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV["CI"].present?
 
-  # Configure public file server for tests with Cache-Control for performance.
+  # Configure public file server for tests with cache-control for performance.
   config.public_file_server.enabled = true
-  config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{1.hour.to_i}" }
+  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.hour.to_i}" }
 
-  # Show full error reports and disable caching.
+  # Show full error reports.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.action_controller.action_on_unpermitted_parameters = :raise
@@ -42,9 +42,8 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Unlike controllers, the mailer instance doesn't have any context about the
-  # incoming request so you'll need to provide the :host parameter yourself.
-  config.action_mailer.default_url_options = { host: "www.example.com" }
+  # Set host to be used by links generated in mailer templates.
+  config.action_mailer.default_url_options = { host: "example.com" }
 
   config.log_level = (ENV["LOG_LEVEL"].presence || :debug).to_sym
 
