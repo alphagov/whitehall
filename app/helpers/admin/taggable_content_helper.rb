0a2f1b732f499d8009279647007acd31f6210cb6
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-02-19 16:16:18 +0000

    Change how TaggableContentHelper caches queries
    The query caching (added in #1712 and #1728) is important to
    retain as, according to the linked PRs, it causes considerable
    savings in page load times for the affected pages.
    
    However, in one of the next commits we'll be changing the structure
    of the returned data to include not only the `text` and `value` of
    each option, but also its `selected` status. We don't want to
    cache the `selected` status as that would mean for example we're
    showing the same "Lead Organisation" selected item across all
    five "Lead organisations" dropdowns on the "New document" page.
    
    So, we're keeping the caching, but we're just going to cache the
    expensive bit - the database result. The `map` calls are very
    quick in comparison and don't need to be cached.

--- app/helpers/admin/taggable_content_helper.rb
+++ app/helpers/admin/taggable_content_helper.rb
@@ -5,8 +5,8 @@ module Admin::TaggableContentHelper
   # Each element of the array consists of two values: the select_name and the
   # ID of the organisation
   def taggable_organisations_container
-    Rails.cache.fetch(taggable_organisations_cache_digest, expires_in: 1.day) do
-      Organisation.with_translations.order("organisation_translations.name").map { |o| [o.select_name, o.id] }
+    cached_taggable_organisations.map do |o|
+      [o.select_name, o.id]
     end
   end
 
@@ -16,16 +16,14 @@ module Admin::TaggableContentHelper
   # role, the date the role was held if it's in the past, and the organisations
   # the person belongs to) and the ID of the role appointment.
   def taggable_ministerial_role_appointments_container
-    Rails.cache.fetch(taggable_ministerial_role_appointments_cache_digest, expires_in: 1.day) do
-      role_appointments_container_for(RoleAppointment.for_ministerial_roles)
+    cached_taggable_ministerial_role_appointments.map do |appointment|
+      [role_appointment_label(appointment), appointment.id]
     end
   end
 
   def taggable_needs_container
-    Rails.cache.fetch("need.linkables", expires_in: 1.minute) do
-      Services.publishing_api.get_linkables(document_type: "need").to_a.map do |need|
-        need.values_at("title", "content_id")
-      end
+    Services.publishing_api.get_linkables(document_type: "need").to_a.map do |need|
+      need.values_at("title", "content_id")
     end
   rescue GdsApi::TimedOutException, GdsApi::HTTPServerError
     stale_data = Rails.cache.fetch("need.linkables")
@@ -40,8 +38,8 @@ module Admin::TaggableContentHelper
   # held if it's in the past, and the organisations the person belongs to) and
   # the ID of the role appointment.
   def taggable_role_appointments_container
-    Rails.cache.fetch(taggable_role_appointments_cache_digest, expires_in: 1.day) do
-      role_appointments_container_for(RoleAppointment)
+    cached_taggable_role_appointments.map do |appointment|
+      [role_appointment_label(appointment), appointment.id]
     end
   end
 
@@ -49,8 +47,8 @@ module Admin::TaggableContentHelper
   # guides. Each element of the array consists of two values: the guide title
   # and its ID.
   def taggable_detailed_guides_container
-    Rails.cache.fetch(taggable_detailed_guides_cache_digest, expires_in: 1.day) do
-      DetailedGuide.alphabetical.latest_edition.active.map { |d| [d.title, d.id] }
+    cached_taggable_detailed_guides.map do |d|
+      [d.title, d.id]
     end
   end
 
@@ -58,26 +56,24 @@ module Admin::TaggableContentHelper
   # data sets. Each elements of the array consists of two values: the data
   # set title and its ID.
   def taggable_statistical_data_sets_container
-    Rails.cache.fetch(taggable_statistical_data_sets_cache_digest, expires_in: 1.day) do
-      StatisticalDataSet.with_translations.latest_edition.map do |data_set|
-        [data_set.title, data_set.document_id]
-      end
+    cached_taggable_statistical_data_sets.map do |data_set|
+      [data_set.title, data_set.document_id]
     end
   end
 
   # Returns an Array that represents the taggable world locations. Each element
   # of the array consists of two values: the location name and its ID
   def taggable_world_locations_container
-    Rails.cache.fetch(taggable_world_locations_cache_digest, expires_in: 1.day) do
-      WorldLocation.ordered_by_name.where(active: true).map { |w| [w.name, w.id] }
+    cached_taggable_world_locations.map do |w|
+      [w.name, w.id]
     end
   end
 
   # Returns an Array that represents the taggable roles. Each element of the
   # array consists of two values: the role name and its ID
   def taggable_roles_container
-    Rails.cache.fetch(taggable_roles_cache_digest, expires_in: 1.day) do
-      Role.order(:name).map { |w| [w.name, w.id] }
+    cached_taggable_roles.map do |w|
+      [w.name, w.id]
     end
   end
 
@@ -85,10 +81,8 @@ module Admin::TaggableContentHelper
   # Each element of the array consists of two values: the label (organisation
   # and the email address if avaiable) and the ID of the organisation.
   def taggable_alternative_format_providers_container
-    Rails.cache.fetch(taggable_alternative_format_providers_cache_digest, expires_in: 1.day) do
-      Organisation.alphabetical.map do |o|
-        ["#{o.name} (#{o.alternative_format_contact_email.presence || '-'})", o.id]
-      end
+    cached_taggable_alternative_format_providers.map do |o|
+      ["#{o.name} (#{o.alternative_format_contact_email.presence || '-'})", o.id]
     end
   end
 
@@ -96,8 +90,14 @@ module Admin::TaggableContentHelper
   # Each element of the array consists of two values: the name of the worldwide
   # organisation and its ID.
   def taggable_worldwide_organisations_container
-    Rails.cache.fetch(taggable_worldwide_organisations_cache_digest, expires_in: 1.day) do
-      WorldwideOrganisation.with_translations.latest_edition.map { |wo| [wo.title, wo.document.id] }
+    cached_taggable_worldwide_organisations.map do |wo|
+      [wo.title, wo.document.id]
+    end
+  end
+
+  def cached_taggable_organisations
+    Rails.cache.fetch(taggable_organisations_cache_digest, expires_in: 1.day) do
+      Organisation.with_translations.order("organisation_translations.name")
     end
   end
 
@@ -115,6 +115,12 @@ module Admin::TaggableContentHelper
     @taggable_organisations_cache_digest ||= calculate_digest(Organisation.order(:id), "organisations")
   end
 
+  def cached_taggable_ministerial_role_appointments
+    Rails.cache.fetch(taggable_ministerial_role_appointments_cache_digest, expires_in: 1.day) do
+      role_appointments_container_for(RoleAppointment.for_ministerial_roles)
+    end
+  end
+
   # Returns an MD5 digest representing the current set of taggable ministerial
   # role appointments. This will change if any role appointments are added or
   # changed, and also if an occupied MinisterialRole is updated.
@@ -128,11 +134,23 @@ module Admin::TaggableContentHelper
     )
   end
 
+  def cached_taggable_role_appointments
+    Rails.cache.fetch(taggable_role_appointments_cache_digest, expires_in: 1.day) do
+      role_appointments_container_for(RoleAppointment)
+    end
+  end
+
   # Returns an MD5 digest representing the current set of taggable ministerial
   # role appointments. This will change if any role appointments are added or
   # changed, and also if an occupied Role is updated.
   def taggable_role_appointments_cache_digest
     @taggable_role_appointments_cache_digest ||= calculate_digest(RoleAppointment.order(:id), "role-appointments")
+  end
+
+  def cached_taggable_detailed_guides
+    Rails.cache.fetch(taggable_detailed_guides_cache_digest, expires_in: 1.day) do
+      DetailedGuide.alphabetical.latest_edition.active
+    end
   end
 
   # Returns an MD5 digest representing all the detailed guides. This wil change
@@ -141,10 +159,22 @@ module Admin::TaggableContentHelper
     @taggable_detailed_guides_cache_digest ||= calculate_digest(Document.where(document_type: "DetailedGuide").order(:id), "detailed-guides")
   end
 
+  def cached_taggable_statistical_data_sets
+    Rails.cache.fetch(taggable_statistical_data_sets_cache_digest, expires_in: 1.day) do
+      StatisticalDataSet.with_translations.latest_edition
+    end
+  end
+
   # Returns an MD5 digest representing the taggable statistical data sets. This
   # will change if any statistical data set is added or updated.
   def taggable_statistical_data_sets_cache_digest
     @taggable_statistical_data_sets_cache_digest ||= calculate_digest(Document.where(document_type: "StatisticalDataSet").order(:id), "statistical-data-sets")
+  end
+
+  def cached_taggable_world_locations
+    Rails.cache.fetch(taggable_world_locations_cache_digest, expires_in: 1.day) do
+      WorldLocation.ordered_by_name.where(active: true)
+    end
   end
 
   # Returns an MD5 digest representing the taggable world locations. This will
@@ -153,10 +183,22 @@ module Admin::TaggableContentHelper
     @taggable_world_locations_cache_digest ||= calculate_digest(WorldLocation.order(:id), "world-locations")
   end
 
+  def cached_taggable_roles
+    Rails.cache.fetch(taggable_roles_cache_digest, expires_in: 1.day) do
+      Role.order(:name)
+    end
+  end
+
   # Returns an MD5 digest representing the taggable roles. This will
   # change if any world locations are added or updated.
   def taggable_roles_cache_digest
     @taggable_roles_cache_digest ||= calculate_digest(Role.order(:id), "roles")
+  end
+
+  def cached_taggable_alternative_format_providers
+    Rails.cache.fetch(taggable_alternative_format_providers_cache_digest, expires_in: 1.day) do
+      Organisation.alphabetical
+    end
   end
 
   # Returns an MD5 digest representing the taggable alternative format
@@ -164,6 +206,12 @@ module Admin::TaggableContentHelper
   # changed.
   def taggable_alternative_format_providers_cache_digest
     @taggable_alternative_format_providers_cache_digest ||= calculate_digest(Organisation.order(:id), "alternative-format-providers")
+  end
+
+  def cached_taggable_worldwide_organisations
+    Rails.cache.fetch(taggable_worldwide_organisations_cache_digest, expires_in: 1.day) do
+      WorldwideOrganisation.with_translations.latest_edition
+    end
   end
 
   # Returns an MD5 digest representing the taggable worldwide organisations. This
@@ -184,7 +232,7 @@ private
       .includes(:person)
       .with_translations_for(:organisations)
       .with_translations_for(:role)
-      .ascending_start_date.map { |appointment| [role_appointment_label(appointment), appointment.id] }
+      .ascending_start_date
   end
 
   def role_appointment_label(appointment)
