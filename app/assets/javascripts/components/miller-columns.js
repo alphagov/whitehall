290b75a14ba9196a94fd0353036f53802060807f
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-02-25 08:15:42 +0000

    Swap miller-columns-element 'autocomplete' for select_with_search
    miller-columns-element was the last thing that was depending on
    accessible-autocomplete-multiselect, preventing us from removing
    the dependency from the repo. But as this commit shows, it's
    relatively straightforward to swap this out for select_with_search
    and we can therefore remove accessible-autocomplete-multiselect
    in the next commit.
    
    The end result is a near identical experience for the user:
    
    | Old component | New component|
    |------|----|
    |![1-landing-old](https://github.com/user-attachments/assets/7e6f09fa-4494-4f1a-affa-e23e3bc92f27)|![1-landing-new](https://github.com/user-attachments/assets/81ff66bb-94ba-4c3d-8b32-947393d53dfc)|
    |On page load|On page load - we've lost the magnifying glass, but gained some hint text|
    |![1-searching-old](https://github.com/user-attachments/assets/4d426a7d-850f-4649-89d7-575cb122aa5c)|![1-searching-new](https://github.com/user-attachments/assets/ee7f4538-f9f9-4852-ad6d-abac61daa6fa)|
    |Searching|Searching - we've lost some highlighting, but otherwise identical|
    |![1-selected-old](https://github.com/user-attachments/assets/1ea5556f-e42c-4c18-9e71-77915dd524b6)|![1-selected-new](https://github.com/user-attachments/assets/a0759c9b-aaed-418c-bfb3-108f3ef3fef2)|
    |Selected|Selected - the end result is identical|

--- app/assets/javascripts/components/miller-columns.js
+++ app/assets/javascripts/components/miller-columns.js
@@ -1,3 +1,4 @@
+//= require choices.js/public/assets/scripts/choices.min.js
 //= require miller-columns-element
 'use strict'
 window.GOVUK = window.GOVUK || {}
@@ -13,80 +14,42 @@ window.GOVUK.Modules = window.GOVUK.Modules || {}
   }
 
   MillerColumns.prototype.initSearch = function () {
-    const element = this.module.querySelector(
-      '#js-app-c-miller-columns__search'
-    )
     const input = this.module.querySelector(
-      '#js-app-c-miller-columns__search-input'
+      '#app-c-miller-columns__search-input'
     )
-    const millerColumns = this.module.querySelector('miller-columns')
+    const placeholderOption = input.querySelector(
+      'option[value=""]:first-child'
+    )
 
-    if (!window.accessibleAutocomplete) {
-      element.parentNode.removeChild(element)
-      return
+    if (placeholderOption && placeholderOption.textContent === '') {
+      placeholderOption.textContent = 'Search for topics'
     }
-
-    const topics = millerColumns.taxonomy.flattenedTopics
-    const topicSuggestions = []
-
-    topics.forEach(function (topic) {
-      topicSuggestions.push({
-        topic,
-        highlightedTopicName: topic.topicName.replace(/<\/?mark>/gm, ''), // strip existing <mark> tags
-        breadcrumbs: topic.topicNames
-      })
-    })
-
-    if (!topicSuggestions.length) {
-      element.parentNode.removeChild(element)
-      return
-    }
-
-    // prettier-ignore
-    new window.accessibleAutocomplete({ // eslint-disable-line no-new, new-cap
-      id: input.id,
-      name: input.name,
-      element,
-      minLength: 3,
-      autoselect: false,
-      source: function (query, syncResults) {
-        const results = topicSuggestions
-        const resultMatcher = function (result) {
-          const topicName = result.topic.topicName
-          const indexOf = topicName.toLowerCase().indexOf(query.toLowerCase())
-          const resultContainsQuery = indexOf !== -1
-          if (resultContainsQuery) {
-            // Wrap query in <mark> tags
-            const queryRegex = new RegExp('(' + query + ')', 'ig')
-            result.highlightedTopicName = topicName.replace(queryRegex, '<mark>$1</mark>')
-          }
-          return resultContainsQuery
-        }
-
-        syncResults(query ? results.filter(resultMatcher) : [])
-      },
-      templates: {
-        inputValue: function (result) {
-          return ''
-        },
-        suggestion: function (result) {
-          let suggestionsBreadcrumbs
-          if (result && result.breadcrumbs) {
-            result.breadcrumbs[result.breadcrumbs.length - 1] = result.highlightedTopicName
-            suggestionsBreadcrumbs = result.breadcrumbs.join(' › ')
-          }
-          return suggestionsBreadcrumbs
-        }
-      },
-      onConfirm: function (result) {
-        if (result && !result.topic.selected && !result.topic.selectedChildren.length) {
-          millerColumns.taxonomy.topicClicked(result.topic)
-        }
+    const choices = new window.Choices(input, {
+      allowHTML: true,
+      shouldSort: false, // show options and groups in the order they were given
+      itemSelectText: '',
+      searchResultLimit: 100,
+      // https://fusejs.io/api/options.html
+      fuseOptions: {
+        ignoreLocation: true, // matches any part of the string
+        threshold: 0 // only matches when characters are sequential
       }
     })
+    choices.passedElement.element.addEventListener(
+      'choice',
+      function (event) {
+        // check the corresponding box in miller-columns
+        document
+          .querySelector("input[value='" + event.detail.value + "']")
+          .click()
 
-    element.classList.add('app-c-autocomplete--search')
-    input.parentNode.removeChild(input)
+        // Remove the 'chosen' item so that it continues to be available
+        // in search, and so that it isn't made visible below the search
+        // area like we'd normally want it to be.
+        choices.removeActiveItems()
+      },
+      false
+    )
   }
 
   Modules.MillerColumns = MillerColumns
