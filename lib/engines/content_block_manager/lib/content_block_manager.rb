5b5f8be93338f6ac2444d5f662c89925f2f90c47
pezholio <pezholio@gmail.com>
2025-02-25 11:47:54 +0000

    Add local config for ContentBlockManager
    This allows us to centralise some of the configuration values, similar
    to how it’s done in `lib/whitehall.rb`

--- lib/engines/content_block_manager/lib/content_block_manager.rb
+++ lib/engines/content_block_manager/lib/content_block_manager.rb
@@ -1,4 +1,13 @@
-require_relative "content_block_manager/engine"
-
 module ContentBlockManager
+  def self.product_name
+    "Content Block Manager"
+  end
+
+  def self.support_url
+    "#{Plek.external_url_for('support')}/general_request/new"
+  end
+
+  def self.router_prefix
+    "/content-block-manager"
+  end
 end
