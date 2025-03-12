78b6589bc26650c8bf337b04de1c3d9167233a7b
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-03-12 09:53:55 +0000

    Remove use of 'capture_subprocess_io'
    These were getting in the way of my `require 'pry'; binding.pry`
    calls. They're also unnecessary. According to
    [its own documentation](https://www.rubydoc.info/gems/minitest/5.10.3/Minitest%2FAssertions:capture_subprocess_io),
    "this method is approximately 10x slower than #capture_io so
    only use it when you need to test the output of a subprocess".
    
    We don't test the output of the sub-process. I imagine we only
    include this line because we want to suppress the debugger output
    from the link checker. We can find another way of doing this if it
    becomes annoying.

--- test/unit/app/sidekiq/check_organisation_links_worker_test.rb
+++ test/unit/app/sidekiq/check_organisation_links_worker_test.rb
@@ -21,7 +21,7 @@ class CheckOrganisationLinksWorkerTest < ActiveSupport::TestCase
   test "when given an organisation it only calls LinkCheckerApiService with editions for that organisation" do
     new_edition = stub_published_publication
 
-    capture_subprocess_io { CheckOrganisationLinksWorker.new.perform(@hmrc.id) }
+    CheckOrganisationLinksWorker.new.perform(@hmrc.id)
 
     assert_equal 1, LinkCheckerApiReport.count
     assert_requested new_edition
@@ -69,7 +69,7 @@ class CheckOrganisationLinksWorkerTest < ActiveSupport::TestCase
       body: "no links here",
     )
 
-    capture_subprocess_io { CheckOrganisationLinksWorker.new.perform(org.id) }
+    CheckOrganisationLinksWorker.new.perform(org.id)
     assert_equal(1, publication.link_check_reports.count)
     assert_equal(false, publication.link_check_reports.last.has_problems?) # TODO: delete this line
     assert_equal(false, publication.link_check_report.has_problems?)
@@ -80,7 +80,7 @@ private
   def assert_report_count_increased
     stub_organisation_edition_limit(2) do
       assert_difference "LinkCheckerApiReport.count", 2 do
-        capture_subprocess_io { CheckOrganisationLinksWorker.new.perform(@hmrc.id) }
+        CheckOrganisationLinksWorker.new.perform(@hmrc.id)
       end
     end
   end
