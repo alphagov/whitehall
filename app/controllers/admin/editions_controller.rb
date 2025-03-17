ee9969572e52e8ba83513fafd2af9c5c6cb5f05a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-03-17 17:10:04 +0000

    Add 'Only dangerous links' filter on search page
    MVP for resolving any mass 'dangerous domain' links in Whitehall:
    surface them in search results for manual resolution.
    
    Trello: https://trello.com/c/tmnht4P1

--- app/controllers/admin/editions_controller.rb
+++ app/controllers/admin/editions_controller.rb
@@ -417,7 +417,7 @@ private
   end
 
   def params_filters
-    params.slice(:type, :state, :organisation, :author, :page, :title, :world_location, :from_date, :to_date, :only_broken_links, :review_overdue)
+    params.slice(:type, :state, :organisation, :author, :page, :title, :world_location, :from_date, :to_date, :only_broken_links, :only_danger_links, :review_overdue)
           .permit!
           .to_h
   end
