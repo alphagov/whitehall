f951628fa0289e9ddf618fe60979747ed7a91124
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-03-24 15:01:08 +0000

    Don't retry `RemoveDangerousLinksWorker` workers

--- app/sidekiq/remove_dangerous_links_worker.rb
+++ app/sidekiq/remove_dangerous_links_worker.rb
@@ -1,6 +1,17 @@
 class RemoveDangerousLinksWorker < WorkerBase
   sidekiq_options queue: "publishing_api"
 
+  # Don't retry this job if it fails, because:
+  # 1. It's all 'internal' - so won't fail if a third party API is down,
+  #    and any failure is unlikely to resolve itself on a retry.
+  # 2. There are currently some cases where `ArgumentError` is raised
+  #    'legitimately'. E.g. for 'withdrawn' editions, for which we
+  #    haven't yet written the code to auto-remove dangerous links.
+  #    NB, they're logged as errors (rather than logs in Kibana) because
+  #    they're still 'actionable' errors that should be resolved on a
+  #    case by case basis.
+  sidekiq_options retry: 0
+
   def perform(edition_id)
     edition = find_and_validate_edition(edition_id)
 
