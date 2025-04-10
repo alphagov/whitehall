4852a44c0ce05b6ea1a6bc251197bda81189b41e
Harriet H-W <harriethw@gmail.com>
2025-04-10 14:04:21 +0100

    Change header title from ‘Dashboard’ to ‘Blocks’
    As per design decision.

--- lib/engines/content_block_manager/features/step_definitions/content_block_manager_steps.rb
+++ lib/engines/content_block_manager/features/step_definitions/content_block_manager_steps.rb
@@ -443,7 +443,7 @@ Then(/^I should see the object store's home page title$/) do
 end
 
 And(/^I should see the object store's navigation$/) do
-  expect(page).to have_selector("a.govuk-header__link[href='#{content_block_manager.content_block_manager_root_path}']", text: "Dashboard")
+  expect(page).to have_selector("a.govuk-header__link[href='#{content_block_manager.content_block_manager_root_path}']", text: "Blocks")
 end
 
 And("I should see the object store's phase banner") do
