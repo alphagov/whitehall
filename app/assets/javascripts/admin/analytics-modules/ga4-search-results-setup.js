8404240df66d6148b2c915554853d7be5eaddf83
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-04-22 17:32:08 +0100

    Add `Ga4EcommerceTracker` to search pages
    Implement new `ga4-search-results-setup.js` and add to `application.js`.
    
    `ga4-search-results-setup.js` runs
    `window.GOVUK.analyticsGa4.Ga4EcommerceTracker.init()`
    which fires a `search_results` event if expected data attributes are
    present on a page.
    
    If the page has tabs, then the search results could be in a tab and not
    visible when the user views the page. `ga4-search-results-setup.js` ensures
    that the `search_results` event only fires if search results are
    visible.

---
+++ app/assets/javascripts/admin/analytics-modules/ga4-search-results-setup.js
@@ -0,0 +1,104 @@
+'use strict'
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.analyticsGa4 = window.GOVUK.analyticsGa4 || {}
+window.GOVUK.analyticsGa4.analyticsModules =
+  window.GOVUK.analyticsGa4.analyticsModules || {}
+;(function (Modules) {
+  Modules.Ga4SearchResultsSetup = {
+    init: function (noHashChange) {
+      if (!document.querySelector('[data-ga4-ecommerce]')) {
+        return
+      }
+
+      // if we have multiple tabs with search results
+      // then remove the attributes so that we only capture
+      // the search results in the visible tab
+      Array.from(
+        document.querySelectorAll(
+          '[data-module~="govuk-tabs"] [data-ga4-ecommerce]'
+        )
+      ).forEach((search) => {
+        search.removeAttribute('data-ga4-ecommerce')
+        search.setAttribute('data-search-track', true)
+      })
+
+      this.trackEcommerce()
+
+      if (!noHashChange) {
+        this.eventListener = this.trackEcommerce.bind(this)
+
+        window.addEventListener('hashchange', this.eventListener)
+
+        this.clearHashChange()
+      }
+    },
+
+    clearHashChange: function () {
+      // remove hashchange listener
+      // if no more search to track
+      if (
+        !document.querySelectorAll(
+          '[data-module~="govuk-tabs"] [data-search-track]'
+        ).length
+      ) {
+        window.removeEventListener('hashchange', this.eventListener)
+      } else {
+        window.addEventListener('hashchange', this.clearHashChange.bind(this), {
+          once: true
+        })
+      }
+    },
+
+    trackEcommerce: function () {
+      // this is because we have an instance of a search result
+      // page within a tab component and we only want to fire a
+      // search result event if the tab with the search result
+      // is visible
+
+      const hash = window.location.hash
+
+      let searchTarget = document.querySelector('[data-ga4-ecommerce]')
+
+      if (
+        !searchTarget &&
+        document.querySelector('[data-module~="govuk-tabs"]')
+      ) {
+        if (hash.length) {
+          searchTarget = document.querySelector(
+            `[data-module~="govuk-tabs"] ${hash} [data-search-track]`
+          )
+        } else {
+          // if search result is on the first tab
+          // and the page is loaded without an anchor
+
+          // get the first instance of a search in a tab
+          // on the page if it exists
+          searchTarget = document.querySelector(
+            '[data-module~="govuk-tabs"] section [data-search-track]'
+          )
+
+          if (searchTarget) {
+            const sectionOfSearchTarget = searchTarget.closest('section')
+
+            const sectionIndexOfSearchTarget = Array.from(
+              searchTarget
+                .closest('[data-module~="govuk-tabs"]')
+                .querySelectorAll('section')
+            ).indexOf(sectionOfSearchTarget)
+
+            searchTarget = !sectionIndexOfSearchTarget && searchTarget
+          }
+        }
+      }
+
+      if (searchTarget) {
+        searchTarget.removeAttribute('data-search-track')
+        searchTarget.setAttribute('data-ga4-ecommerce', true)
+
+        window.GOVUK.analyticsGa4.Ga4EcommerceTracker.init()
+
+        searchTarget.removeAttribute('data-ga4-ecommerce')
+      }
+    }
+  }
+})(window.GOVUK.analyticsGa4.analyticsModules)
