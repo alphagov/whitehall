fa771349da95a065540a669ef654600cd05cd855
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-05-06 11:36:12 +0100

    `Ga4IndexSetup` changes to index calculation
    -`Ga4IndexSetup` adds index to fieldset instead of individual
    inputs within the fieldset.
    -`Ga4IndexSetup` now sets `index` to a JSON containing the
    `index_section` and `index_section_count`. This is for the
    `Ga4FinderTracker`.
    - `Ga4IndexSetup` now sets `data-ga4-filter-parent` which
    is a required attribute for `Ga4FinderTracker`.

--- spec/javascripts/admin/analytics-modules/ga4-index-section-setup.spec.js
+++ spec/javascripts/admin/analytics-modules/ga4-index-section-setup.spec.js
@@ -1,5 +1,13 @@
 describe('GOVUK.analyticsGa4.analyticsModules.GA4IndexSectionEventHandlers', function () {
-  let container, form, input, select, excludedParent, excludedInput
+  let container,
+    form,
+    input,
+    radioInput,
+    hiddenInput,
+    fieldset,
+    select,
+    excludedParent,
+    excludedInput
 
   beforeEach(function () {
     document.title = 'Document title'
@@ -16,6 +24,14 @@ describe('GOVUK.analyticsGa4.analyticsModules.GA4IndexSectionEventHandlers', fun
     excludedInput = document.createElement('input')
     excludedParent.appendChild(excludedInput)
     form.appendChild(excludedParent)
+    fieldset = document.createElement('fieldset')
+    radioInput = document.createElement('input')
+    radioInput.type = 'radio'
+    fieldset.appendChild(radioInput)
+    form.appendChild(fieldset)
+    hiddenInput = document.createElement('input')
+    hiddenInput.type = 'hidden'
+    form.appendChild(hiddenInput)
     container.appendChild(form)
     document.body.appendChild(container)
   })
@@ -25,15 +41,54 @@ describe('GOVUK.analyticsGa4.analyticsModules.GA4IndexSectionEventHandlers', fun
       GOVUK.analyticsGa4.analyticsModules.Ga4IndexSectionSetup
     Ga4IndexSectionSetup.init()
 
-    expect(input.dataset.ga4IndexSection).toEqual('0')
-    expect(select.dataset.ga4IndexSection).toEqual('1')
+    expect(input.dataset.ga4Index).toEqual(
+      JSON.stringify({ index_section: '0', index_section_count: '3' })
+    )
+    expect(select.dataset.ga4Index).toEqual(
+      JSON.stringify({ index_section: '1', index_section_count: '3' })
+    )
+    expect(fieldset.dataset.ga4Index).toEqual(
+      JSON.stringify({ index_section: '2', index_section_count: '3' })
+    )
   })
 
-  it('does not add ga4-index-section data attributes to select and input fields in an excludedParent', function () {
+  it('adds a ga4-filter-parent data attribute to select and input fields', function () {
     const Ga4IndexSectionSetup =
       GOVUK.analyticsGa4.analyticsModules.Ga4IndexSectionSetup
     Ga4IndexSectionSetup.init()
 
-    expect(excludedInput.dataset.ga4IndexSection).not.toBeDefined()
+    expect(input.dataset.ga4FilterParent).toEqual(String(true))
+    expect(select.dataset.ga4FilterParent).toEqual(String(true))
+    expect(fieldset.dataset.ga4FilterParent).toEqual(String(true))
+  })
+
+  describe('does not add ga4-index-section data attributes to', function () {
+    it('select and input fields in an excludedParent', function () {
+      const Ga4IndexSectionSetup =
+        GOVUK.analyticsGa4.analyticsModules.Ga4IndexSectionSetup
+      Ga4IndexSectionSetup.init()
+
+      expect(excludedInput.dataset.ga4Index).not.toBeDefined()
+    })
+
+    it('radio button within fieldset', function () {
+      const Ga4IndexSectionSetup =
+        GOVUK.analyticsGa4.analyticsModules.Ga4IndexSectionSetup
+      Ga4IndexSectionSetup.init()
+
+      expect(radioInput.dataset.ga4Index).not.toBeDefined()
+    })
+
+    it('hidden input', function () {
+      const Ga4IndexSectionSetup =
+        GOVUK.analyticsGa4.analyticsModules.Ga4IndexSectionSetup
+      Ga4IndexSectionSetup.init()
+
+      expect(hiddenInput.dataset.ga4Index).not.toBeDefined()
+    })
+  })
+
+  afterEach(function () {
+    container.remove()
   })
 })
