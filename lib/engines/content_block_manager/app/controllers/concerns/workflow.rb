e9e3b85563e4a6b447bbfd08980d981a6faad0e2
pezholio <pezholio@gmail.com>
2025-05-07 16:19:53 +0100

    Remove unused subschema code

--- lib/engines/content_block_manager/app/controllers/concerns/workflow.rb
+++ lib/engines/content_block_manager/app/controllers/concerns/workflow.rb
@@ -1,7 +1,5 @@
 module Workflow
   class Step < Data.define(:name, :show_action, :update_action, :included_in_create_journey)
-    SUBSCHEMA_PREFIX = "embedded_".freeze
-
     ALL = [
       Step.new(:edit_draft, :edit_draft, :update_draft, true),
       Step.new(:embedded_objects, :embedded_objects, :redirect_to_next_step, true),
@@ -12,9 +10,5 @@ module Workflow
       Step.new(:review, :review, :validate_review_page, true),
       Step.new(:confirmation, :confirmation, nil, true),
     ].freeze
-
-    def is_subschema?
-      name.to_s.start_with?(SUBSCHEMA_PREFIX)
-    end
   end
 end
