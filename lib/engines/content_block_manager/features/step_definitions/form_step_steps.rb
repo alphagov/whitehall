a58cb0d3a369b8cb9aeeff39f056524a23d06c1e
pezholio <pezholio@gmail.com>
2025-05-13 15:02:04 +0100

    Remove rollup from preview page
    Users we tested this with found it confusing and
    disorientating.

--- lib/engines/content_block_manager/features/step_definitions/form_step_steps.rb
+++ lib/engines/content_block_manager/features/step_definitions/form_step_steps.rb
@@ -6,7 +6,6 @@ Then(/^I should be on the "([^"]*)" step$/) do |step|
     should_show_edit_form(object_type: @content_block.document.block_type)
   when "review_links"
     should_show_dependent_content(object_type: @content_block.document.block_type)
-    should_show_rollup_data
   when "schedule_publishing"
     should_show_publish_form
   when "review"
