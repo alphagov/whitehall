e6aa9e71353a889de1ae8af913d3a5640b69ccb8
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-05-27 16:20:40 +0100

    Add `Ga4FormSetup`
    For forms that are not search forms, `Ga4FormSetup` adds required
    `data-ga4-form` attribute for `Ga4FormTracker` to track them. It
    also adds an `onSubmit` event listener to change the `action` of
    the form event.

---
+++ app/assets/javascripts/admin/analytics-modules/ga4-form-setup.js
@@ -0,0 +1,71 @@
+'use strict'
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.analyticsGa4 = window.GOVUK.analyticsGa4 || {}
+window.GOVUK.analyticsGa4.analyticsModules =
+  window.GOVUK.analyticsGa4.analyticsModules || {}
+;(function (Modules) {
+  Modules.Ga4FormSetup = {
+    init: function () {
+      const $modules = document.querySelectorAll(
+        "[data-module~='ga4-form-setup']"
+      )
+
+      $modules.forEach(($module) => {
+        const forms = $module.querySelectorAll(
+          "form:not([data-module~='ga4-finder-tracker'])"
+        )
+
+        forms.forEach(function (form) {
+          const sectionContainer = form.closest('[data-ga4-section]')
+          const documentTypeContainer = form.closest('[data-ga4-document-type]')
+
+          let eventData = {
+            event_name: 'form_response',
+            action: 'Save'
+          }
+
+          if (sectionContainer) {
+            eventData.section =
+              sectionContainer.getAttribute('data-ga4-section')
+          }
+
+          if (documentTypeContainer) {
+            const [type, toolName] =
+              documentTypeContainer.dataset.ga4DocumentType.split('-')
+
+            eventData = {
+              ...eventData,
+              type,
+              tool_name: toolName
+            }
+          }
+
+          form.setAttribute('data-ga4-form', JSON.stringify(eventData))
+
+          form.addEventListener('submit', this.onSubmit)
+        }, this)
+
+        new window.GOVUK.Modules.Ga4FormTracker($module).init()
+      })
+    },
+
+    onSubmit: (event) => {
+      // on forms we have multiple submit buttons so need to "guess"
+      // the action from the focused element on submit
+
+      const form = event.target.closest('form')
+
+      const activeElement = document.activeElement
+
+      try {
+        const dataGa4Form = JSON.parse(form.getAttribute('data-ga4-form'))
+
+        dataGa4Form.action = activeElement.textContent
+
+        form.setAttribute('data-ga4-form', JSON.stringify(dataGa4Form))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+})(window.GOVUK.analyticsGa4.analyticsModules)
