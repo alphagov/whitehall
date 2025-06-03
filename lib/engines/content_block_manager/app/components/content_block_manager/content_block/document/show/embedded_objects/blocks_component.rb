96b183bc54cefb5f2ad727da3ea19e1352539cca
pezholio <pezholio@gmail.com>
2025-06-03 15:10:44 +0100

    Show an entire block when embeddable

--- lib/engines/content_block_manager/app/components/content_block_manager/content_block/document/show/embedded_objects/blocks_component.rb
+++ lib/engines/content_block_manager/app/components/content_block_manager/content_block/document/show/embedded_objects/blocks_component.rb
@@ -13,13 +13,25 @@ private
   attr_reader :items, :object_type, :object_title, :content_block_document
 
   def rows
-    items.map do |key, value|
-      {
+    rows = schema.embeddable_as_block? ? [block_row] : []
+
+    items.each do |key, value|
+      rows << {
         key: key.titleize,
         value: content_for_row(key, value),
         data: data_attributes_for_row(key),
       }
     end
+
+    rows
+  end
+
+  def block_row
+    {
+      key: object_type.singularize.titleize,
+      value: content_for_block_row,
+      data: data_attributes_for_block_row,
+    }
   end
 
   def content_for_row(key, value)
@@ -29,14 +41,32 @@ private
   end
 
   def data_attributes_for_row(key)
-    attributes = {
+    {
       testid: (object_title.parameterize + "_#{key}").underscore,
+      **copy_embed_code_data_attributes("#{object_type}/#{object_title}/#{key}", content_block_document),
     }
-    attributes.merge!(copy_embed_code_data_attributes("#{object_type}/#{object_title}/#{key}", content_block_document))
-    attributes
+  end
+
+  def content_for_block_row
+    content = content_tag(:p,
+                          content_block_edition.render(content_block_document.embed_code_for_field("#{object_type}/#{object_title}")),
+                          class: "app-c-embedded-objects-blocks-component__content")
+    content << content_tag(:p, content_block_document.embed_code_for_field("#{object_type}/#{object_title}"), class: "app-c-embedded-objects-blocks-component__embed-code")
+    content
+  end
+
+  def data_attributes_for_block_row
+    {
+      testid: object_title.parameterize.underscore,
+      **copy_embed_code_data_attributes("#{object_type}/#{object_title}", content_block_document),
+    }
   end
 
   def schema
-    @schema ||= content_block_document.schema
+    @schema ||= content_block_document.schema.subschema(object_type)
+  end
+
+  def content_block_edition
+    @content_block_edition ||= content_block_document.latest_edition
   end
 end
--- lib/engines/content_block_manager/test/components/content_block/document/show/embedded_objects/blocks_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/document/show/embedded_objects/blocks_component_test.rb
@@ -11,8 +11,19 @@ class ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::Blocks
   end
   let(:object_type) { "something" }
   let(:object_title) { "else" }
+  let(:embeddable_as_block) { false }
 
+  let(:content_block_edition) { build(:content_block_edition, :pension) }
   let(:content_block_document) { build(:content_block_document, :pension) }
+
+  let(:schema) { stub("schema") }
+  let(:subschema) { stub("schema", embeddable_as_block?: embeddable_as_block) }
+
+  before do
+    content_block_document.stubs(:schema).returns(schema)
+    content_block_document.stubs(:latest_edition).returns(content_block_edition)
+    schema.stubs(:subschema).with(object_type).returns(subschema)
+  end
 
   let(:component) do
     ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::BlocksComponent.new(
@@ -39,6 +50,42 @@ class ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::Blocks
       row.assert_selector ".govuk-summary-list__value" do |col|
         col.assert_selector ".app-c-embedded-objects-blocks-component__content", text: "buzz"
         col.assert_selector ".app-c-embedded-objects-blocks-component__embed-code", text: content_block_document.embed_code_for_field("#{object_type}/#{object_title}/fizz")
+      end
+    end
+  end
+
+  describe "when the block type is embeddable as a block" do
+    let(:embeddable_as_block) { true }
+
+    it "returns the block alongside the other fields" do
+      content_block_edition.expects(:render).with(
+        content_block_document.embed_code_for_field("#{object_type}/#{object_title}"),
+      ).returns("BLOCK_RESPONSE")
+
+      render_inline component
+
+      assert_selector ".app-c-embedded-objects-blocks-component [data-testid='else']" do |row|
+        row.assert_selector ".govuk-summary-list__key", text: "Something"
+        row.assert_selector ".govuk-summary-list__value" do |col|
+          col.assert_selector ".app-c-embedded-objects-blocks-component__content", text: "BLOCK_RESPONSE"
+          col.assert_selector ".app-c-embedded-objects-blocks-component__embed-code", text: content_block_document.embed_code_for_field("#{object_type}/#{object_title}")
+        end
+      end
+
+      assert_selector ".app-c-embedded-objects-blocks-component [data-testid='else_foo']" do |row|
+        row.assert_selector ".govuk-summary-list__key", text: "Foo"
+        row.assert_selector ".govuk-summary-list__value" do |col|
+          col.assert_selector ".app-c-embedded-objects-blocks-component__content", text: "bar"
+          col.assert_selector ".app-c-embedded-objects-blocks-component__embed-code", text: content_block_document.embed_code_for_field("#{object_type}/#{object_title}/foo")
+        end
+      end
+
+      assert_selector ".app-c-embedded-objects-blocks-component [data-testid='else_fizz']" do |row|
+        row.assert_selector ".govuk-summary-list__key", text: "Fizz"
+        row.assert_selector ".govuk-summary-list__value" do |col|
+          col.assert_selector ".app-c-embedded-objects-blocks-component__content", text: "buzz"
+          col.assert_selector ".app-c-embedded-objects-blocks-component__embed-code", text: content_block_document.embed_code_for_field("#{object_type}/#{object_title}/fizz")
+        end
       end
     end
   end
