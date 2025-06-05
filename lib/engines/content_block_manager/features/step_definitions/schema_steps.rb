3c90dc4133689ecc472c186dccc8056de5b904e0
pezholio <pezholio@gmail.com>
2025-06-05 13:24:07 +0100

    Add a button to add a grouped subschema item
    I’ve also added a feature spec to ensure the entire flow
    works

--- lib/engines/content_block_manager/features/step_definitions/schema_steps.rb
+++ lib/engines/content_block_manager/features/step_definitions/schema_steps.rb
@@ -32,6 +32,13 @@ When("I click on the {string} schema") do |schema_id|
   click_save_and_continue
 end
 
+When("I click on the {string} subschema") do |schema_id|
+  schema = @schemas.values.last
+  subschema = schema.subschema(schema_id)
+  choose subschema.name.singularize
+  click_save_and_continue
+end
+
 Then("I should see a form for the schema") do
   expect(page).to have_text("Create #{@schema.name.downcase}")
 end
@@ -39,5 +46,22 @@ end
 Then("I should see all the schemas listed") do
   @schemas.values.each do |schema|
     expect(page).to have_content(schema.name)
+  end
+end
+
+Then("I should see all the subschemas for {string} listed") do |group|
+  schema = @schemas.values.last
+  schema&.subschemas_for_group(group)&.each do |subschema|
+    expect(page).to have_content(subschema.name.singularize)
+  end
+end
+
+And("the schema {string} has a group {string} with the following subschemas:") do |block_type, group, table|
+  subschemas = table.raw.first
+  schema = @schemas[block_type]
+
+  subschemas.each do |subschema_id|
+    subschema = schema.subschema(subschema_id)
+    subschema.stubs(:group).returns(group)
   end
 end
