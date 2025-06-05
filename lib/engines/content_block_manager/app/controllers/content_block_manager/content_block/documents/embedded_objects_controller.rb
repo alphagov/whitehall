7d2c71f8a000ab77e4ed3660e275d8ff30a6f1b5
pezholio <pezholio@gmail.com>
2025-06-05 10:39:11 +0100

    Add a redirect after selecting a subschema
    If no subschema is selected, we redirect back to the select schema
    page with an error

--- lib/engines/content_block_manager/app/controllers/content_block_manager/content_block/documents/embedded_objects_controller.rb
+++ lib/engines/content_block_manager/app/controllers/content_block_manager/content_block/documents/embedded_objects_controller.rb
@@ -1,7 +1,7 @@
 class ContentBlockManager::ContentBlock::Documents::EmbeddedObjectsController < ContentBlockManager::BaseController
   include EmbeddedObjects
 
-  before_action :initialize_document_and_schema
+  before_action :initialize_document
 
   def new
     @content_block_edition = @content_block_document.latest_edition
@@ -21,6 +21,7 @@ class ContentBlockManager::ContentBlock::Documents::EmbeddedObjectsController < 
   end
 
   def create
+    initialize_schemas
     @content_block_edition = @content_block_document.latest_edition.clone_edition(creator: current_user)
     @params = object_params(@subschema).dig(:details, @subschema.block_type)
     @content_block_edition.add_object_to_details(@subschema.block_type, @params)
@@ -35,13 +36,26 @@ class ContentBlockManager::ContentBlock::Documents::EmbeddedObjectsController < 
     render :new
   end
 
+  def new_embedded_objects_options_redirect
+    @content_block_document = ContentBlockManager::ContentBlock::Document.find(params[:document_id])
+
+    if params[:object_type].present?
+      redirect_to content_block_manager.new_content_block_manager_content_block_document_embedded_object_path(@content_block_document, object_type: params.require(:object_type))
+    else
+      redirect_to content_block_manager.new_content_block_manager_content_block_document_embedded_object_path(@content_block_document, group: params.require(:group)), flash: { error: I18n.t("activerecord.errors.models.content_block_manager/content_block/document.attributes.block_type.blank") }
+    end
+  end
+
 private
 
-  def initialize_document_and_schema
+  def initialize_document
     @content_block_document = ContentBlockManager::ContentBlock::Document.find(params[:document_id])
-    if params[:object_type]
-      get_schema_and_subschema(@content_block_document.block_type, params[:object_type])
-      render "admin/errors/not_found", status: :not_found unless @subschema
+  end
+
+  def initialize_schemas
+    get_schema_and_subschema(@content_block_document.block_type, params[:object_type])
+    unless @subschema
+      render "admin/errors/not_found", status: :not_found and return
     end
   end
 end
