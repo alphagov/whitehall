e46776b615c44db6725325a21d3cd52ea985574e
pezholio <pezholio@gmail.com>
2025-06-05 13:21:15 +0100

    Add an argument to not render an add button
    This will allow us to skip adding a button to add a new object
    type when rendering grouped subschemas.

--- lib/engines/content_block_manager/test/components/content_block/document/show/embedded_objects/subschema_items_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/document/show/embedded_objects/subschema_items_component_test.rb
@@ -46,18 +46,7 @@ class ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::Subsch
   end
 
   describe "rendering" do
-    it "renders a button to add an item, and a SummaryListComponent for each object" do
-      button_stub = "BUTTON"
-
-      component.expects(:render).with("govuk_publishing_components/components/button", {
-        text: "Add an embedded type",
-        href: new_content_block_manager_content_block_document_embedded_object_path(
-          content_block_edition.document,
-          object_type: subschema.id,
-        ),
-        margin_bottom: 6,
-      }).returns(button_stub)
-
+    it "renders a SummaryListComponent for each object" do
       summary_list_stub_1 = "my-embedded-object"
       summary_list_stub_2 = "my-other-embedded-object"
 
@@ -75,12 +64,48 @@ class ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::Subsch
 
       component.expects(:render).with(summary_list_stub_1).returns(summary_list_stub_1)
       component.expects(:render).with(summary_list_stub_2).returns(summary_list_stub_2)
+      component.expects(:render).at_least_once.with("govuk_publishing_components/components/button", anything)
+
+      render_inline(component)
+
+      assert_text summary_list_stub_1
+      assert_text summary_list_stub_2
+    end
+
+    it "renders a button to add an object" do
+      button_stub = "BUTTON"
+
+      component.expects(:render).with("govuk_publishing_components/components/button", {
+        text: "Add an embedded type",
+        href: new_content_block_manager_content_block_document_embedded_object_path(
+          content_block_edition.document,
+          object_type: subschema.id,
+        ),
+        margin_bottom: 6,
+      }).returns(button_stub)
+
+      component.expects(:render).at_least_once.with(anything)
 
       render_inline(component)
 
       assert_text button_stub
-      assert_text summary_list_stub_1
-      assert_text summary_list_stub_2
+    end
+
+    describe "when show_button is set to false" do
+      let(:component) do
+        ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::SubschemaItemsComponent.new(
+          content_block_edition:,
+          subschema:,
+          show_button: false,
+        )
+      end
+
+      it "does not render a button" do
+        component.expects(:render).at_least_once.with(anything)
+        component.expects(:render).with("govuk_publishing_components/components/button", anything).never
+
+        render_inline(component)
+      end
     end
   end
 end
