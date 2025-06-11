6e1e9568cdb196f66542fa0478a7cf04d9027853
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-06-11 17:51:43 +0100

    Add `extracted_contact_ids` method
    We want to be able to extract ALL of the contact IDs referenced
    in the passed GovSpeak body, but the existing `valid_contacts` method
    returns an array of Contacts that exist (silently dropping any
    that don't).
    
    This will be used in the next commit.

--- test/unit/lib/govspeak/contacts_extractor_test.rb
+++ test/unit/lib/govspeak/contacts_extractor_test.rb
@@ -4,8 +4,8 @@ class ContactsExtractorTest < ActiveSupport::TestCase
   test "can collect all the embedded contacts into a list of Contacts in order" do
     contact1 = build(:contact)
     contact2 = build(:contact)
-    Contact.stubs(:find_by).with(id: "1").returns(contact1)
-    Contact.stubs(:find_by).with(id: "2").returns(contact2)
+    Contact.stubs(:find_by).with(id: 1).returns(contact1)
+    Contact.stubs(:find_by).with(id: 2).returns(contact2)
 
     input = "We have an office at [Contact:2] but deliveries go to [Contact:1]"
 
@@ -15,7 +15,7 @@ class ContactsExtractorTest < ActiveSupport::TestCase
 
   test "will remove duplicate contacts" do
     contact1 = build(:contact)
-    Contact.stubs(:find_by).with(id: "1").returns(contact1)
+    Contact.stubs(:find_by).with(id: 1).returns(contact1)
 
     input = "Our office at [Contact:1] is brilliant, you should come for a cup of tea. Remeber the address is [Contact:1]"
 
@@ -24,11 +24,18 @@ class ContactsExtractorTest < ActiveSupport::TestCase
   end
 
   test "will silently remove contact references that do not resolve to a Contact" do
-    Contact.stubs(:find_by).with(id: "1").returns(nil)
+    Contact.stubs(:find_by).with(id: 1).returns(nil)
 
     input = "Our office used to be at [Contact:1] but we moved"
 
     embedded_contacts = Govspeak::ContactsExtractor.new(input).valid_contacts
     assert_equal [], embedded_contacts
+  end
+
+  test "extracted_contact_ids method returns all IDs extracted from the input, regardless of whether Contact exists or not" do
+    input = "[Contact:99999999]"
+
+    embedded_contacts = Govspeak::ContactsExtractor.new(input).extracted_contact_ids
+    assert_equal [99_999_999], embedded_contacts
   end
 end
