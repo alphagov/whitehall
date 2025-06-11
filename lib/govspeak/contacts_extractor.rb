947f04c8595cc421122806443fe21d66f6738c9f
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-06-11 17:51:43 +0100

    Add `extracted_contact_ids` method
    We want to be able to extract ALL of the contact IDs referenced
    in the passed GovSpeak body, but the existing `valid_contacts` method
    returns an array of Contacts that exist (silently dropping any
    that don't).
    
    This will be used in the next commit.

--- lib/govspeak/contacts_extractor.rb
+++ lib/govspeak/contacts_extractor.rb
@@ -1,18 +1,19 @@
 module Govspeak
   class ContactsExtractor
     def initialize(govspeak)
-      @govspeak = govspeak
+      @govspeak = govspeak || ""
     end
 
     def valid_contacts
-      return [] if @govspeak.blank?
+      extracted_contact_ids.map { |contact_id| Contact.find_by(id: contact_id) }.compact
+    end
 
+    def extracted_contact_ids
       # scan yields an array of capture groups for each match
       # so "[Contact:1] is now [Contact:2]" => [["1"], ["2"]]
       @govspeak.scan(EmbeddedContentPatterns::CONTACT).map { |capture|
-        contact_id = capture.first
-        Contact.find_by(id: contact_id)
-      }.compact.uniq
+        capture.first.to_i
+      }.uniq
     end
   end
 end
