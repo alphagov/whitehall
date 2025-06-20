a621a1da8c65202e4e8b3b408b3e5238e2598973
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-06-20 17:23:16 +0100

    Add govspeak validation for govspeak flexible page content

--- app/models/flexible_page_type.rb
+++ app/models/flexible_page_type.rb
@@ -39,7 +39,12 @@ class FlexiblePageType
   end
 
   def validator
-    JSONSchemer.schema(@schema)
+    formats = {
+      "govspeak" => proc do |instance|
+        instance.is_a?(String) && Govspeak::HtmlValidator.new(instance).valid?
+      end,
+    }
+    JSONSchemer.schema(@schema, formats:)
   end
 
   def publishing_api_payload_builder(page_content)
