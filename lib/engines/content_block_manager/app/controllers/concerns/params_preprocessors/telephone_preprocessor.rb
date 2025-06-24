d71ecf8c9d1ff94b524b6315bfd5c4f14c76901d
pezholio <pezholio@gmail.com>
2025-06-24 17:01:57 +0100

    Add a preprocessor to handle opening hours
    This introduces the concept of a preprocessor for different object
    types, which will allow us to alter what is saved if the forms fields
    have a different data shape to what Publishing API expects.
    
    Because our opening hours time fields will have multiple select boxes,
    we need to consume each select box and join them together to make a
    single field. This will then be validated using a regex from the schema,
    so any missing fields will result in an invalid time.
    
    Similarly, if the `hours_available` checkbox is left unchecked, we
    should strip any submitted opening hours, as we don’t want these to
    be saved.

---
+++ lib/engines/content_block_manager/app/controllers/concerns/params_preprocessors/telephone_preprocessor.rb
@@ -0,0 +1,42 @@
+class ParamsPreprocessors::TelephonePreprocessor
+  def initialize(params)
+    @params = params
+  end
+
+  def processed_params
+    process!
+    params
+  end
+
+  def process!
+    if params.dig("content_block/edition", "details", "telephones", "opening_hours")
+      params["hours_available"] ? format_opening_hours : strip_opening_hours
+    end
+  end
+
+private
+
+  attr_accessor :params
+
+  def format_opening_hours
+    params["content_block/edition"]["details"]["telephones"]["opening_hours"].map! do |hours|
+      {
+        "day_from" => hours["day_from"],
+        "day_to" => hours["day_to"],
+        "time_from" => format_time(hours, "time_from"),
+        "time_to" => format_time(hours, "time_to"),
+      }
+    end
+  end
+
+  def strip_opening_hours
+    params["content_block/edition"]["details"]["telephones"]["opening_hours"] = []
+  end
+
+  def format_time(hours, prefix)
+    h = hours["#{prefix}(h)"]
+    m = hours["#{prefix}(m)"]
+    meridian = hours["#{prefix}(meridian)"]
+    "#{h}:#{m}#{meridian}"
+  end
+end
