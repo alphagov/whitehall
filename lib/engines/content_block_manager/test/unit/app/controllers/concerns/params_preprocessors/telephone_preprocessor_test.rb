5c8e051bf6cd5876b946549ecd7ca7d84f0bde09
pezholio <pezholio@gmail.com>
2025-06-24 17:01:57 +0100

    Add a preprocessor to handle opening hours
    This introduces the concept of a preprocessor for different object
    types, which will allow us to alter what is saved if the forms fields
    have a different data shape to what Publishing API expects.
    
    Because our opening hours time fields will have multiple select boxes,
    we need to consume each select box and join them together to make a
    single field. This will then be validated using a regex from the schema,
    so any missing fields will result in an invalid time.
    
    Similarly, if the `hours_available` checkbox is left unchecked, we
    should strip any submitted opening hours, as we don’t want these to
    be saved.

---
+++ lib/engines/content_block_manager/test/unit/app/controllers/concerns/params_preprocessors/telephone_preprocessor_test.rb
@@ -0,0 +1,84 @@
+require "test_helper"
+
+class ParamsPreprocessors::TelephonePreprocessorTest < ActiveSupport::TestCase
+  extend Minitest::Spec::DSL
+
+  let(:opening_hours) do
+    [
+      {
+        "day_from" => "Monday",
+        "day_to" => "Friday",
+        "time_from(h)" => "9",
+        "time_from(m)" => "00",
+        "time_from(meridian)" => "AM",
+        "time_to(h)" => "5",
+        "time_to(m)" => "30",
+        "time_to(meridian)" => "PM",
+      },
+    ]
+  end
+
+  let(:details) do
+    {
+      "telephones" => {
+        "opening_hours" => opening_hours,
+      },
+    }
+  end
+
+  let(:params) do
+    {
+      "hours_available" => hours_available,
+      "content_block/edition" => {
+        "details" => details,
+      },
+    }
+  end
+
+  describe "when hours_available is set" do
+    let(:hours_available) { "1" }
+
+    it "formats the opening hours correctly" do
+      result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
+      expected_result = {
+        "hours_available" => "1",
+        "content_block/edition" => {
+          "details" => {
+            "telephones" => {
+              "opening_hours" => [
+                {
+                  "day_from" => "Monday",
+                  "day_to" => "Friday",
+                  "time_from" => "9:00AM",
+                  "time_to" => "5:30PM",
+                },
+              ],
+            },
+          },
+        },
+      }
+
+      assert_equal result, expected_result
+    end
+  end
+
+  describe "when hours_available is not set" do
+    let(:hours_available) { nil }
+
+    it "clears the opening_hours array" do
+      result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
+      expected_result = {
+        "hours_available" => nil,
+        "content_block/edition" => {
+          "details" => {
+            "telephones" => {
+              "opening_hours" => [],
+            },
+          },
+        },
+      }
+
+      assert_equal result, expected_result
+    end
+  end
+end
