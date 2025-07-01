daa8686573b9399fc45141d40fd193a280102aab
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-07-01 12:28:40 +0100

    Add `content_id` to tracked events
    `content_id` is a new attribute for `event_data`. It is added to the
    root container in `data-ga4-content-id` to be picked up by trackers.
    
    `Ga4FormSetup` picks up `data-ga4-content-id` and adds it to
    `data-ga4-form`. This includes `content_id` in form submission events.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -142,4 +142,12 @@ module ApplicationHelper
   def starts_with_vowel?(word_or_phrase)
     "aeiou".include?(word_or_phrase.downcase[0])
   end
+
+  def get_content_id(edition)
+    return if edition.nil?
+
+    return unless edition.respond_to?("content_id")
+
+    edition.content_id
+  end
 end
