582f9d36a8fda56b048c896f911b1ac920867a5a
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-07-03 12:19:39 +0100

    Order metadata on embedded object using schema field_order
    The order in which fields are listed in a form is defined in:
    `content_block_manager/config/content_block_manager.yml`
    e.g.
    
    ```
    field_order:
      - title
      - email_address
      - subject
      - body
      - description
    ```
    
    and applied with sorting code in
    `ContenBlockManager::ContentBlock::Schema`.
    
    In this commit we implement similar code in the
    `MetadataComponent` to use the same `field_order` to sort the
    rows in the metadata summary list. In this way the user will
    see the fields of the embedded object in the same order on
    both the form and display views.
    
    Until we have more information from users to confirm or deny
    that it's appropriate to reuse the `field_order` in this
    way, we won't try to DRY up this code.

--- lib/engines/content_block_manager/app/components/content_block_manager/content_block/document/show/embedded_objects/metadata_component.rb
+++ lib/engines/content_block_manager/app/components/content_block_manager/content_block/document/show/embedded_objects/metadata_component.rb
@@ -11,11 +11,29 @@ private
   attr_reader :items
 
   def rows
+    unordered_rows.sort_by { |row| row_ordering_rule(row) }
+  end
+
+  def unordered_rows
     items.map do |key, value|
       {
         field: humanized_label(key, @object_type),
         value: translated_value(value),
       }
     end
+  end
+
+  def row_ordering_rule(row)
+    if field_order
+      # If a field order is found in the config, order by the index. If a field is not found, put it to the end
+      field_order.index(row.fetch(:field).downcase) || Float::INFINITY
+    else
+      # By default, order with title first
+      row.fetch(:field).downcase == "title" ? 0 : 1
+    end
+  end
+
+  def field_order
+    @schema.config["field_order"]
   end
 end
--- lib/engines/content_block_manager/test/components/content_block/document/show/embedded_objects/metadata_component_test.rb
+++ lib/engines/content_block_manager/test/components/content_block/document/show/embedded_objects/metadata_component_test.rb
@@ -55,25 +55,60 @@ class ContentBlockManager::ContentBlock::Document::Show::EmbeddedObjects::Metada
     )
   end
 
-  it "renders a summary list with the expected attributes" do
-    component.expects(:render).with(
-      "govuk_publishing_components/components/summary_list", {
-        items: [
-          {
-            field: "Foo",
-            value: "bar",
-          },
-          {
-            field: "Fizz",
-            value: "buzz",
-          },
-        ],
+  before do
+    schema.stubs(:config).returns(schema_config)
+  end
+
+  context "when NO field order is defined" do
+    it "renders a summary list with the expected attributes with no field ordering" do
+      component.expects(:render).with(
+        "govuk_publishing_components/components/summary_list", {
+          items: [
+            {
+              field: "Foo",
+              value: "bar",
+            },
+            {
+              field: "Fizz",
+              value: "buzz",
+            },
+          ],
+        }
+      ).returns("STUB_RESPONSE")
+
+      render_inline component
+
+      assert_text "STUB_RESPONSE"
+    end
+  end
+
+  context "when a field order IS defined" do
+    let(:schema_config) do
+      {
+        "field_order" => %w[fizz foo],
       }
-    ).returns("STUB_RESPONSE")
+    end
 
-    render_inline component
+    it "renders a summary list with the defined field ordering (case insensitive)" do
+      component.expects(:render).with(
+        "govuk_publishing_components/components/summary_list", {
+          items: [
+            {
+              field: "Fizz",
+              value: "buzz",
+            },
+            {
+              field: "Foo",
+              value: "bar",
+            },
+          ],
+        }
+      ).returns("STUB_RESPONSE")
 
-    assert_text "STUB_RESPONSE"
+      render_inline component
+
+      assert_text "STUB_RESPONSE"
+    end
   end
 
   describe "when there is a translated field label" do
