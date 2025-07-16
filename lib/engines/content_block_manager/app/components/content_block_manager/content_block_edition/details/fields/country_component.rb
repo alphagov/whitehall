3af0f9b6bbdafbdfadfdc06762d720e5cb918a23
pezholio <pezholio@gmail.com>
2025-07-16 14:20:10 +0100

    CountryComponent includes UK as the first option
    This overrides the `blank_option` method to return the UK as an option,
    and overrides `enum` to return all the options apart from the UK, so it
    does not show twice.

--- lib/engines/content_block_manager/app/components/content_block_manager/content_block_edition/details/fields/country_component.rb
+++ lib/engines/content_block_manager/app/components/content_block_manager/content_block_edition/details/fields/country_component.rb
@@ -1,6 +1,18 @@
 class ContentBlockManager::ContentBlockEdition::Details::Fields::CountryComponent < ContentBlockManager::ContentBlockEdition::Details::Fields::EnumComponent
+  BLANK_OPTION = "United Kingdom".freeze
+
   def initialize(**args)
     countries = WorldLocation.geographical.map(&:name)
     super(**args.merge(enum: countries))
+  end
+
+private
+
+  def enum
+    @enum.excluding(blank_option)
+  end
+
+  def blank_option
+    BLANK_OPTION
   end
 end
