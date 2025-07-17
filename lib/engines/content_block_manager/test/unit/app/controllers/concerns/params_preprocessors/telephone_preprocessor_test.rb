ee7868256d80f63ac7366aa0ad34116c08a3ef3e
pezholio <pezholio@gmail.com>
2025-07-17 11:10:01 +0100

    Update preprocessor to handle call charges
    This also removes the other attributes if `show_call_charges_info_url`
    is not set.

--- lib/engines/content_block_manager/test/unit/app/controllers/concerns/params_preprocessors/telephone_preprocessor_test.rb
+++ lib/engines/content_block_manager/test/unit/app/controllers/concerns/params_preprocessors/telephone_preprocessor_test.rb
@@ -19,10 +19,22 @@ class ParamsPreprocessors::TelephonePreprocessorTest < ActiveSupport::TestCase
     ]
   end
 
+  let(:show_call_charges_info_url) { "true" }
+  let(:hours_available) { "1" }
+
+  let(:call_charges) do
+    {
+      "show_call_charges_info_url" => show_call_charges_info_url,
+      "label" => "Label",
+      "call_charges_info_url" => "https://example.com",
+    }
+  end
+
   let(:details) do
     {
       "telephones" => {
         "opening_hours" => opening_hours,
+        "call_charges" => call_charges,
       },
     }
   end
@@ -36,51 +48,61 @@ class ParamsPreprocessors::TelephonePreprocessorTest < ActiveSupport::TestCase
     }
   end
 
-  describe "when hours_available is set" do
-    let(:hours_available) { "1" }
+  describe "opening hours processing" do
+    describe "when hours_available is set" do
+      let(:hours_available) { "1" }
 
-    it "formats the opening hours correctly" do
-      result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
-      expected_result = {
-        "hours_available" => "1",
-        "content_block/edition" => {
-          "details" => {
-            "telephones" => {
-              "opening_hours" => [
-                {
-                  "day_from" => "Monday",
-                  "day_to" => "Friday",
-                  "time_from" => "9:00AM",
-                  "time_to" => "5:30PM",
-                  "_destroy" => "0",
-                },
-              ],
-            },
+      it "formats the opening hours correctly" do
+        result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
+
+        assert_equal result["hours_available"], "1"
+        assert_equal result["content_block/edition"]["details"]["telephones"]["opening_hours"], [
+          {
+            "day_from" => "Monday",
+            "day_to" => "Friday",
+            "time_from" => "9:00AM",
+            "time_to" => "5:30PM",
+            "_destroy" => "0",
           },
-        },
-      }
+        ]
+      end
+    end
 
-      assert_equal result, expected_result
+    describe "when hours_available is not set" do
+      let(:hours_available) { nil }
+
+      it "clears the opening_hours array" do
+        result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
+
+        assert_equal result["hours_available"], nil
+        assert_equal result["content_block/edition"]["details"]["telephones"]["opening_hours"], []
+      end
     end
   end
 
-  describe "when hours_available is not set" do
-    let(:hours_available) { nil }
+  describe "call charges processing" do
+    describe "when show_call_charges_info_url is set to a `true` string" do
+      let(:show_call_charges_info_url) { "true" }
 
-    it "clears the opening_hours array" do
-      result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
-      expected_result = {
-        "hours_available" => nil,
-        "content_block/edition" => {
-          "details" => {
-            "telephones" => {
-              "opening_hours" => [],
-            },
-          },
-        },
-      }
+      it "converts the string to a boolean" do
+        result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
 
-      assert_equal result, expected_result
+        assert_equal result["content_block/edition"]["details"]["telephones"]["call_charges"], {
+          "show_call_charges_info_url" => true,
+          "label" => "Label",
+          "call_charges_info_url" => "https://example.com",
+        }
+      end
+    end
+
+    describe "when show_call_charges_info_url is empty" do
+      let(:show_call_charges_info_url) { "" }
+
+      it "empties the call charges object" do
+        result = ParamsPreprocessors::TelephonePreprocessor.new(params).processed_params
+
+        assert_equal result["content_block/edition"]["details"]["telephones"]["call_charges"], {}
+      end
     end
   end
 end
