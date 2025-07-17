89b93c5fa3f1d131a1c8a24c353583eeea9c750c
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2025-07-17 15:06:44 +0100

    Revert "Change GovSpeak Contact Validator to check HTML Attachment validation from parent edition"
    This reverts commit 8ffd9ac43646f3dc0c574674e5fa43262dc7af90.

--- app/validators/govspeak_contact_embed_validator.rb
+++ app/validators/govspeak_contact_embed_validator.rb
@@ -4,14 +4,5 @@ class GovspeakContactEmbedValidator < ActiveModel::Validator
       contact = Contact.find_by(id: contact_id)
       record.errors.add(:base, "Contact ID #{contact_id} doesn't exist") unless contact
     end
-
-    if record.respond_to?(:html_attachments)
-      record.html_attachments.each do |html_attachment|
-        Govspeak::ContactsExtractor.new(html_attachment.body).extracted_contact_ids.each do |contact_id|
-          contact = Contact.find_by(id: contact_id)
-          html_attachment.errors.add(:base, "Contact ID #{contact_id} doesn't exist") unless contact
-        end
-      end
-    end
   end
 end
