9a77583b4308fc3da7ffa450c2e33d9946c1486f
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2025-07-18 14:17:21 +0100

    WIP

--- app/validators/govspeak_contact_embed_validator.rb
+++ app/validators/govspeak_contact_embed_validator.rb
@@ -1,8 +1,23 @@
 class GovspeakContactEmbedValidator < ActiveModel::Validator
   def validate(record)
+    return if record.is_a?(Edition) && record.validation_context != :publish
+
     Govspeak::ContactsExtractor.new(record.body).extracted_contact_ids.each do |contact_id|
-      contact = Contact.find_by(id: contact_id)
-      record.errors.add(:base, "Contact ID #{contact_id} doesn't exist") unless contact
+      next if Contact.exists?(id: contact_id)
+      
+      record.errors.add(:base, build_error_message(record, contact_id))
+    end
+  end
+
+private
+
+  def build_error_message(record, contact_id)
+    case record
+    when HtmlAttachment
+      identifier = record.title.present? ? "'#{record.title}'" : record.id
+      "HTML Attachment #{identifier} invalid: Contact ID #{contact_id} doesn't exist"
+    else
+      "Contact ID #{contact_id} doesn't exist"
     end
   end
 end
--- test/unit/app/validators/govspeak_contact_embed_validator_test.rb
+++ test/unit/app/validators/govspeak_contact_embed_validator_test.rb
@@ -24,11 +24,63 @@ class GovspeakContactEmbedValidatorTest < ActiveSupport::TestCase
     bad_id = "9999999999999"
     test_model = Edition.new(body: "[Contact:#{bad_id}]")
 
+    test_model.valid?(:publish)
+
+    contact_errors = test_model.errors.select { |error| error.message.include?("Contact ID") }
+    assert_equal 1, contact_errors.size
+    assert_equal ["Contact ID #{bad_id} doesn't exist"], contact_errors.map(&:message)
+  end
+
+  test "should be valid if Edition contains a Contact that doesn't exist but not in publish context" do
+    bad_id = "9999999999999"
+    test_model = Edition.new(body: "[Contact:#{bad_id}]")
+
+    test_model.valid?
+
+    contact_errors = test_model.errors.select { |error| error.message.include?("Contact ID") }
+    assert_empty contact_errors
+  end
+
+  test "should be invalid if HTML attachment contains a Contact that doesn't exist" do
+    bad_id = "9999999999999"
+    test_model = build(:html_attachment, body: "[Contact:#{bad_id}]")
+    test_model.id = 12345
+
     GovspeakContactEmbedValidator.new.validate(test_model)
 
     assert_equal 1, test_model.errors.size
-    assert_equal [
-      "Contact ID #{bad_id} doesn't exist",
-    ], test_model.errors.map(&:type)
+    assert_equal ["HTML Attachment 'html-attachment-title-1' invalid: Contact ID #{bad_id} doesn't exist"], test_model.errors.map(&:type)
+  end
+
+  test "should use HTML attachment title in error message when title is present" do
+    bad_id = "9999999999999"
+    test_model = build(:html_attachment, title: "Annual Report 2024", body: "[Contact:#{bad_id}]")
+
+    GovspeakContactEmbedValidator.new.validate(test_model)
+
+    assert_equal 1, test_model.errors.size
+    assert_equal ["HTML Attachment 'Annual Report 2024' invalid: Contact ID #{bad_id} doesn't exist"], test_model.errors.map(&:type)
+  end
+
+  test "should use HTML attachment ID in error message when title is blank" do
+    bad_id = "9999999999999"
+    test_model = build(:html_attachment, title: "", body: "[Contact:#{bad_id}]")
+    test_model.id = 67890
+
+    GovspeakContactEmbedValidator.new.validate(test_model)
+
+    assert_equal 1, test_model.errors.size
+    assert_equal ["HTML Attachment #{test_model.id} invalid: Contact ID #{bad_id} doesn't exist"], test_model.errors.map(&:type)
+  end
+
+  test "should use HTML attachment ID in error message when title is nil" do
+    bad_id = "9999999999999"
+    test_model = build(:html_attachment, title: nil, body: "[Contact:#{bad_id}]")
+    test_model.id = 13579
+
+    GovspeakContactEmbedValidator.new.validate(test_model)
+
+    assert_equal 1, test_model.errors.size
+    assert_equal ["HTML Attachment #{test_model.id} invalid: Contact ID #{bad_id} doesn't exist"], test_model.errors.map(&:type)
   end
 end
