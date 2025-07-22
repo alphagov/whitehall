d20bd01c4c1bc89de0bf550f33526cdfa60d4831
pezholio <pezholio@gmail.com>
2025-07-22 12:01:17 +0100

    Add helper to return redirect URL for a subschema
    If a subschema has a group, then we’ll want to redirect to a group page,
    rather than a page for a specific subschema.

--- lib/engines/content_block_manager/test/unit/app/helpers/content_block_manager/content_block/schema_helper_test.rb
+++ lib/engines/content_block_manager/test/unit/app/helpers/content_block_manager/content_block/schema_helper_test.rb
@@ -1,9 +1,8 @@
 require "test_helper"
 
-class ContentBlockManager::ContentBlock::SchemaHelperTest < ActiveSupport::TestCase
+class ContentBlockManager::ContentBlock::SchemaHelperTest < ActionView::TestCase
   extend Minitest::Spec::DSL
-
-  include ContentBlockManager::ContentBlock::SchemaHelper
+  include Rails.application.routes.url_helpers
 
   let(:schema) { stub(:schema) }
 
@@ -45,6 +44,27 @@ class ContentBlockManager::ContentBlock::SchemaHelperTest < ActiveSupport::TestC
   describe "#ungrouped_subschemas" do
     it "returns all ungrouped subschemas" do
       assert_equal ungrouped_subschemas(schema), subschemas_without_groups
+    end
+  end
+
+  describe "#redirect_url_for_subschema" do
+    let(:content_block_edition) { build_stubbed(:content_block_edition, :contact) }
+    let(:subschema) { stub(:subschema, group:, id: "my_subschema") }
+
+    context "when the subschema has a group" do
+      let(:group) { nil }
+
+      it "should generate a url with the subschema's step" do
+        assert_equal redirect_url_for_subschema(subschema, content_block_edition), content_block_manager.content_block_manager_content_block_workflow_path(content_block_edition, step: "#{Workflow::Step::SUBSCHEMA_PREFIX}#{subschema.id}")
+      end
+    end
+
+    context "when the subschema has no group" do
+      let(:group) { "some_group" }
+
+      it "should generate a url with the subschema's group" do
+        assert_equal redirect_url_for_subschema(subschema, content_block_edition), content_block_manager.content_block_manager_content_block_workflow_path(content_block_edition, step: "#{Workflow::Step::GROUP_PREFIX}#{subschema.group}")
+      end
     end
   end
 end
