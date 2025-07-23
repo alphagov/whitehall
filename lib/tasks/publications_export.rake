f928eb11edc697d010ed78de644c7e4e055e862d
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2025-07-23 16:15:58 +0100

    export attachment for publication

--- lib/tasks/publications_export.rake
+++ lib/tasks/publications_export.rake
@@ -3,13 +3,22 @@ namespace :publications do
   task export_for_document_collection: :environment do
     require "csv"
 
-    filepath = Rails.root.join("publictions_export.csv")
+    filepath = Rails.root.join("publications_export.csv")
     puts "Exporting publications to #{filepath}..."
 
     CSV.open(filepath, 'w') do |csv|
-      publlication = Publication.first
-      csv << %w"title summary body"
-      csv << [publlication.title, publlication.summary, publlication.body] if publlication
+      publication = Publication.first
+      csv << %w"title summary body attachment_title attachment_filename attachment_url attachment_created_at attachment_updated_at"
+      csv << [
+        publication.title,
+        publication.summary,
+        publication.body,
+        publication.attachments[0].title,
+        publication.attachments[0].filename,
+        publication.attachments[0].url,
+        publication.attachments[0].created_at,
+        publication.attachments[0].updated_at,
+      ] if publication
     end
   end
 end
--- test/unit/lib/tasks/export_publication_data_test.rb
+++ test/unit/lib/tasks/export_publication_data_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 require "rake"
 class ExportPublicationDataTest < ActiveSupport::TestCase
-  CSV_PATH = Rails.root.join("publictions_export.csv")
+  CSV_PATH = Rails.root.join("publications_export.csv")
 
   teardown do
     File.delete(CSV_PATH) if File.exist?(CSV_PATH)
@@ -9,7 +9,8 @@ class ExportPublicationDataTest < ActiveSupport::TestCase
   end
 
   test "export_for_document_collection writes publications to a CSV file" do
-    publication = create(:publication, title: "Test Publication", summary: "This is a test publication.", body: "Detailed body content.")
+    publication = create(:publication, :with_file_attachment)
+
     Rake.application.invoke_task "publications:export_for_document_collection"
 
     assert File.exist?(CSV_PATH), "CSV file should be created"
@@ -18,5 +19,10 @@ class ExportPublicationDataTest < ActiveSupport::TestCase
     assert_equal publication.title, csv[0]['title']
     assert_equal publication.summary, csv[0]['summary']
     assert_equal publication.body, csv[0]['body']
+    assert_equal publication.attachments[0].title, csv[0]['attachment_title']
+    assert_equal publication.attachments[0].filename, csv[0]['attachment_filename']
+    assert_equal publication.attachments[0].url, csv[0]['attachment_url']
+    assert_equal publication.attachments[0].created_at, csv[0]['attachment_created_at']
+    assert_equal publication.attachments[0].updated_at, csv[0]['attachment_updated_at']
   end
 end
