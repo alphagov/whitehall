1369a8443f196b21e087daaf3e35525bf498767c
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2025-07-23 17:05:01 +0100

    test only the first pdf attachment is exported

--- test/unit/lib/tasks/export_publication_data_test.rb
+++ test/unit/lib/tasks/export_publication_data_test.rb
@@ -72,4 +72,19 @@ class ExportPublicationDataTest < ActiveSupport::TestCase
     assert_equal additional_attachment.url, csv[0]['attachment_url']
   end
 
+  test "export_for_document_collection gets the first pdf attachment if there are multiple" do
+    publication = create(:publication, :with_html_attachment)
+    additional_attachment1 = create(:file_attachment, attachable: publication)
+    create(:file_attachment, attachable: publication)
+
+    Rake.application.invoke_task "publications:export_for_document_collection"
+
+    csv = CSV.read(CSV_PATH, headers: true)
+    assert_equal 1, csv.size
+    assert_equal additional_attachment1.title, csv[0]['attachment_title']
+    assert_equal additional_attachment1.filename, csv[0]['attachment_filename']
+    assert_equal additional_attachment1.url, csv[0]['attachment_url']
+  end
+
+
 end
