66e49a553b9190a9cd28791352a1ce8765ad7439
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-08-01 14:34:43 +0100

    Enforce 'absolute path' GOV.UK paths and disallow full GOV.UK URLs
    Publishing API does not allow redirect values to fully qualified
    GOV.UK URLs. The resulting UX is very poor - Whitehall happily
    reports that the document has redirected as it should, whereas in
    reality, Publishing API has responded with a 422 and not processed
    the redirect.
    
    See Sentry error:
    https://govuk.sentry.io/issues/6340746667/?project=202259&query=&referrer=issue-stream
    
    So now we enforce the same rule as Publishing API: that redirects
    to GOV.UK URLs should be given absolute paths rather than fully
    qualified URLs. Host-less URLs, such as `example.com`, continue
    to fail as they should.

--- test/unit/app/validators/uri_validator_test.rb
+++ test/unit/app/validators/uri_validator_test.rb
@@ -20,6 +20,17 @@ class UriValidatorTest < ActiveSupport::TestCase
     assert feature_link.errors.empty?
   end
 
+  test "validates internal GOV.UK URLs" do
+    feature_link = validate(PromotionalFeatureLink.new(url: "/foo"))
+    assert feature_link.errors.empty?
+  end
+
+  # See https://github.com/alphagov/publishing-api/blob/537a4d62960e6fc7ea7ffa4e245dba006172b5df/app/validators/routes_and_redirects_validator.rb#L201-L202
+  test "absolute GOV.UK URLs are not valid (they're rejected by Publishing API)" do
+    feature_link = validate(PromotionalFeatureLink.new(url: "https://www.gov.uk/foo"))
+    assert_equal ["is not valid. A redirect to a page on GOV.UK should not be specified with a full url (e.g. use '/example' rather than 'https://www.gov.uk/example')"], feature_link.errors[:url]
+  end
+
   test "non-http(s) URLs are not valid" do
     feature_link = validate(PromotionalFeatureLink.new(url: "ftp://example.com"))
     assert_equal ["is not valid. Make sure it starts with http(s)"], feature_link.errors[:url]
