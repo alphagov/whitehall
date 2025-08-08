401fcf1b641f44bf787ab365a0c32d8ce10b1f10
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-08-08 14:51:17 +0100

    Move attachment type specific code to controller
    To remove branching logic within `AttachmentController`, move the logic
    specific to handling specific attachment types to the controller of that
    attachment type.

--- app/controllers/admin/file_attachments_controller.rb
+++ app/controllers/admin/file_attachments_controller.rb
@@ -3,10 +3,44 @@ class Admin::FileAttachmentsController < Admin::AttachmentsController
 
   def edit; end
 
+  def update
+    if save_attachment
+      flash[:notice] = "Attachment '#{attachment.title}' updated"
+
+      if attachment.filename_changed? && attachable.allows_inline_attachments?
+        flash[:notice] += ". You must replace the attachment markdown with the new markdown below."
+      end
+
+      save_and_redirect
+    else
+      render :edit
+    end
+  end
+
+  def attachment_params
+    attachment_params = params.fetch(:attachment, {}).permit(
+      :title,
+      :locale,
+      :isbn,
+      :unique_reference,
+      :command_paper_number,
+      :unnumbered_command_paper,
+      :hoc_paper_number,
+      :unnumbered_hoc_paper,
+      :parliamentary_session,
+      :accessible,
+      attachment_data_attributes: %i[file to_replace_id file_cache],
+    ).merge(attachable:)
+
+    clear_file_cache(attachment_params)
+  end
+
 private
 
-  def redirect_to_attachments_index
-    redirect_to attachable_attachments_path(attachable)
+  def update_attachment_params
+    super
+
+    attachment.attachment_data.attachable = attachable
   end
 
   def clear_file_cache(attachment_params)
@@ -15,12 +49,5 @@ private
     end
 
     attachment_params
-  end
-
-  def build_attachment
-    FileAttachment.new(attachment_params).tap do |file_attachment|
-      file_attachment.build_attachment_data unless file_attachment.attachment_data
-      file_attachment.attachment_data.attachable = attachable
-    end
   end
 end
--- app/controllers/admin/html_attachments_controller.rb
+++ app/controllers/admin/html_attachments_controller.rb
@@ -5,6 +5,32 @@ class Admin::HtmlAttachmentsController < Admin::AttachmentsController
 
 private
 
+  def attachment_params
+    params.fetch(:attachment, {}).permit(
+      :title,
+      :locale,
+      :isbn,
+      :unique_reference,
+      :command_paper_number,
+      :unnumbered_command_paper,
+      :hoc_paper_number,
+      :unnumbered_hoc_paper,
+      :parliamentary_session,
+      :visual_editor,
+      govspeak_content_attributes: %i[id body manually_numbered_headings],
+    ).merge(attachable:)
+  end
+
+  def save_attachment
+    super
+
+    if attachment.valid? && !attachable_is_an_edition?
+      Whitehall::PublishingApi.save_draft(attachment)
+    end
+
+    attachment.valid?
+  end
+
   def build_attachment
     HtmlAttachment.new(attachment_params).tap do |attachment|
       attachment.build_govspeak_content if attachment.govspeak_content.blank?
