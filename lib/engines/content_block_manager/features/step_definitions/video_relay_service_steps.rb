51eceea86d7c692b0c15b1b89cd9ecea16d053fb
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-12 10:28:43 +0100

    Show Govspeak 'Preview' in action in cucumber
    For now we just demonstrate that the JS is wired up
    correctly: when clicking the 'Preview' button the user sees
    the govspeak preview pane, populated with the initial value
    of "Generating preview...".
    
    It would be nice to see the HTML returned by the Whitehall
    preview service. The JS (`GOVUK.Modules.GovspeakEditor`) sends
    a `POST` to:
    
    `http://whitehall-admin.dev.gov.uk/government/admin/preview`
    
    and receives an HTML fragment in response e.g.
    
    ```html
    section>
      <article class="document">
        <div class="body">
          <div class="govspeak">
            <p>
              Rich <strong>content</strong>
            </p>
          </div>
        </div>
      </article>
    </section>
    ```
    
    I haven't figure out where this request is being swallowed
    up but probably it's sufficient to show that the govspeak
    editor widget is set up correctly?

--- lib/engines/content_block_manager/features/step_definitions/video_relay_service_steps.rb
+++ lib/engines/content_block_manager/features/step_definitions/video_relay_service_steps.rb
@@ -5,6 +5,7 @@ end
 Given("I provide custom video relay service info where available") do
   within(".app-c-content-block-manager-video-relay-service-component") do
     fill_in(label_for("prefix"), with: "**Custom** prefix: 12345 then")
+    should_be_able_to_preview_the_govspeak_enabled_field
     fill_in(label_for("telephone_number"), with: "01777 123 1234")
   end
 end
@@ -25,4 +26,14 @@ end
 
 def label_for(field_name)
   I18n.t("content_block_edition.details.labels.telephones.video_relay_service.#{field_name}")
+end
+
+def should_be_able_to_preview_the_govspeak_enabled_field
+  click_button("Preview")
+  preliminary_preview_text = page.find(".app-c-govspeak-editor__preview p").text
+
+  assert_equal(
+    "Generating preview, please wait.",
+    preliminary_preview_text,
+  )
 end
