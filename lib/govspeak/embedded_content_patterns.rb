7741469720f1ca370e5f258f0a066933c0dd83be
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-08-21 17:26:48 +0100

    Allow content ID based [Contact:x] shortcodes
    This is what Publishing API will use to render the embed.
    Publishing API has no concept of the integer IDs we use within
    Whitehall (i.e. [Contact:123]).

--- lib/govspeak/embedded_content_patterns.rb
+++ lib/govspeak/embedded_content_patterns.rb
@@ -1,6 +1,6 @@
 module Govspeak
   module EmbeddedContentPatterns
-    CONTACT = /\[Contact:([0-9]+)\]/
+    CONTACT = /\[Contact:(.+)\]/
     ADMIN_EDITION_PATH = %r{/admin/(?:#{Whitehall.edition_route_path_segments.join('|')})/(\d+)}
     ADMIN_ORGANISATION_CIP_PATH = %r{/admin/organisations/([\w-]+)/corporate_information_pages/(\d+)}
     ADMIN_WORLDWIDE_ORGANISATION_CIP_PATH = %r{/admin/worldwide_organisations/([\w-]+)/corporate_information_pages/(\d+)}
