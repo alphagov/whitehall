b07a965a71fbe03ec8b011f12ddd3864e6a8d805
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-08-21 14:37:46 +0100

    Display helpful HTML attachment error on edition summary page
    We had to fix a few tests that were passing when they shouldn't have been. In particular, it wasn't possible to create a new draft of a document with an invalid HTML attachment. That has now been fixed by skipping validation during new draft creation, which we think is a safe choice.
    
    We added a more human-friendly label for the HTML attachments edition attribute and improved the message added by the govspeak contact embedding validator.
    
    We had to forcibly disable association validation for attachments and html_attachments on the attachable trait. This was because, by default, when an association is loaded and a custom validation context is in use (in our case :publish), Rails will validate the models in the association, which is somewhat unexpected behaviour. This resulted in generic messages such as "attachments is invalid" being displayed to users, which were not very useful. See https://github.com/rails/rails/issues/45209 for more information.

--- test/unit/app/validators/govspeak_contact_embed_validator_test.rb
+++ test/unit/app/validators/govspeak_contact_embed_validator_test.rb
@@ -44,16 +44,25 @@ class GovspeakContactEmbedValidatorTest < ActiveSupport::TestCase
     assert_equal 0, edition.html_attachments.first.errors.size
   end
 
-  test "should be invalid if the HTML attachment contains a Contact that doesn't exist" do
+  test "should be invalid if the HTML attachment contains a Contact that has been deleted" do
     contact = create(:contact)
-    bad_id = "9999999999999"
-    edition = create(:case_study, body: "[Contact:#{contact.id}]")
-
-    edition.html_attachments = [create(:html_attachment, body: "[Contact:#{bad_id}]")]
+    edition = create(:case_study, html_attachments: [create(:html_attachment, body: "[Contact:#{contact.id}]")])
+    contact.destroy!
 
     GovspeakContactEmbedValidator.new.validate(edition)
 
-    assert_equal 0, edition.errors.size
-    assert_equal 1, edition.html_attachments.first.errors.size
+    assert_equal 1, edition.errors.where(:html_attachments).size
+  end
+
+  test "should add a specific error message if the HTML attachment contains a Contact that doesn't exist" do
+    bad_id = "9999999999999"
+    html_attachment = build(:html_attachment, title: "Test Doc", body: "[Contact:#{bad_id}]")
+    edition = build(:case_study, html_attachments: [html_attachment])
+
+    GovspeakContactEmbedValidator.new.validate(edition)
+
+    error_message = edition.errors[:html_attachments].first
+    expected_message = '"Test Doc" - embedded Contact (ID 9999999999999) doesn\'t exist'
+    assert_includes error_message, expected_message
   end
 end
