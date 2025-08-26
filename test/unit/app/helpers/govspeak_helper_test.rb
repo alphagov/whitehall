c299dd235252843ba6bd534bb29dc9f1cc95a86d
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-08-26 12:22:37 +0100

    Move govspeak_helper_test to RSpec-like syntax
    We'll be using a `describe` block in the next commit.

--- test/unit/app/helpers/govspeak_helper_test.rb
+++ test/unit/app/helpers/govspeak_helper_test.rb
@@ -1,47 +1,49 @@
 require "test_helper"
 
 class GovspeakHelperTest < ActionView::TestCase
-  test "should not alter urls to other sites" do
+  extend Minitest::Spec::DSL
+
+  it "should not alter urls to other sites" do
     html = govspeak_to_html("no [change](http://external.example.com/page.html)")
     assert_select_within_html html, "a[href=?]", "http://external.example.com/page.html", text: "change"
   end
 
-  test "should not alter mailto urls" do
+  it "should not alter mailto urls" do
     html = govspeak_to_html("no [change](mailto:dave@example.com)")
     assert_select_within_html html, "a[href=?]", "mailto:dave@example.com", text: "change"
   end
 
-  test "should not alter invalid urls" do
+  it "should not alter invalid urls" do
     html = govspeak_to_html("no [change](not a valid url)")
     assert_select_within_html html, "a[href=?]", "not%20a%20valid%20url", text: "change"
   end
 
-  test "should not alter partial urls" do
+  it "should not alter partial urls" do
     html = govspeak_to_html("no [change](http://)")
     assert_select_within_html html, "a[href=?]", "http://", text: "change"
   end
 
-  test "should wrap output with a govspeak class" do
+  it "should wrap output with a govspeak class" do
     html = govspeak_to_html("govspeak-text")
     assert_select_within_html html, ".govspeak", text: "govspeak-text"
   end
 
-  test "should mark the govspeak output as html safe" do
+  it "should mark the govspeak output as html safe" do
     html = govspeak_to_html("govspeak-text")
     assert html.html_safe?
   end
 
-  test "should produce UTF-8 for HTML entities" do
+  it "should produce UTF-8 for HTML entities" do
     html = govspeak_to_html("a ['funny'](/url) thing")
     assert_select_within_html html, "a", text: "‘funny’"
   end
 
-  test "does not change css class on buttons" do
+  it "does not change css class on buttons" do
     html = govspeak_to_html("{button}[Link text](https://www.gov.uk){/button}")
     assert_select_within_html html, "a.govuk-button", "Link text"
   end
 
-  test "should not mark admin links as 'external'" do
+  it "should not mark admin links as 'external'" do
     speech = create(:published_speech)
     url = admin_speech_url(speech, host: Whitehall.admin_host)
     govspeak = "this and [that](#{url}) yeah?"
@@ -49,7 +51,7 @@ class GovspeakHelperTest < ActionView::TestCase
     refute_select_within_html html, "a[rel='external']", text: "that"
   end
 
-  test "should not mark public site links as 'external'" do
+  it "should not mark public site links as 'external'" do
     speech = create(:published_speech)
     url = admin_speech_url(speech, host: Whitehall.public_host)
     govspeak = "this and [that](#{url}) yeah?"
@@ -57,7 +59,7 @@ class GovspeakHelperTest < ActionView::TestCase
     refute_select_within_html html, "a[rel='external']", text: "that"
   end
 
-  test "should rewrite admin links for editions" do
+  it "should rewrite admin links for editions" do
     speech = create(:published_speech)
     admin_path = admin_speech_path(speech)
     public_url = speech.public_url
@@ -67,13 +69,13 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, "a[href='#{public_url}']", text: "that"
   end
 
-  test "should only extract level two headers by default" do
+  it "should only extract level two headers by default" do
     text = "# Heading 1\n\n## Heading 2\n\n### Heading 3"
     headers = govspeak_headers(text)
     assert_equal [Govspeak::Header.new("Heading 2", 2, "heading-2")], headers
   end
 
-  test "should extract header hierarchy from level 2+3 headings" do
+  it "should extract header hierarchy from level 2+3 headings" do
     text = "# Heading 1\n\n## Heading 2a\n\n### Heading 3a\n\n### Heading 3b\n\n#### Ignored heading\n\n## Heading 2b"
     headers = govspeak_header_hierarchy(text)
     assert_equal [
@@ -92,18 +94,18 @@ class GovspeakHelperTest < ActionView::TestCase
                  headers
   end
 
-  test "should raise exception when extracting header hierarchy with orphaned level 3 headings" do
+  it "should raise exception when extracting header hierarchy with orphaned level 3 headings" do
     e = assert_raise(Govspeak::OrphanedHeadingError) { govspeak_header_hierarchy("### Heading 3") }
     assert_equal "Heading 3", e.heading
   end
 
-  test "should convert single document to govspeak" do
+  it "should convert single document to govspeak" do
     document = build(:published_news_article, body: "## test")
     html = govspeak_edition_to_html(document)
     assert_select_within_html html, "h2"
   end
 
-  test "should return an empty string if nil edition" do
+  it "should return an empty string if nil edition" do
     assert_equal "", govspeak_edition_to_html(nil)
   end
 
@@ -116,7 +118,7 @@ class GovspeakHelperTest < ActionView::TestCase
     "legacy syntax" => "!@1",
     "Govspeak syntax" => "[Attachment: greenpaper.pdf]",
   }.each do |name, embed_code|
-    test "should embed block attachments using #{name}" do
+    it "should embed block attachments using #{name}" do
       body = "#Heading\n\n#{embed_code}\n\n##Subheading"
       document = edition_with_attachment(body:)
       html = govspeak_edition_to_html(document)
@@ -133,7 +135,7 @@ class GovspeakHelperTest < ActionView::TestCase
     "legacy syntax" => "[InlineAttachment:1]",
     "Govspeak syntax" => "[AttachmentLink: greenpaper.pdf]",
   }.each do |name, embed_code|
-    test "should embed attachment links inline using #{name}" do
+    it "should embed attachment links inline using #{name}" do
       body = "#Heading\n\nText about my #{embed_code}."
       document = edition_with_attachment(body:)
       html = govspeak_edition_to_html(document)
@@ -146,7 +148,7 @@ class GovspeakHelperTest < ActionView::TestCase
     end
   end
 
-  test "should ignore missing block attachments" do
+  it "should ignore missing block attachments" do
     text = "#Heading\n\n!@2\n\n##Subheading"
     document = build(:published_detailed_guide, :with_file_attachment, body: text)
     html = govspeak_edition_to_html(document)
@@ -155,7 +157,7 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, "h2"
   end
 
-  test "should ignore missing inline attachments" do
+  it "should ignore missing inline attachments" do
     text = "#Heading\n\nText about my [InlineAttachment:2]."
     document = build(:published_detailed_guide, :with_file_attachment, body: text)
     html = govspeak_edition_to_html(document)
@@ -163,7 +165,7 @@ class GovspeakHelperTest < ActionView::TestCase
     refute_select_within_html html, ".gem-c-attachment-link"
   end
 
-  test "should ignore file attachments with missing asset variants" do
+  it "should ignore file attachments with missing asset variants" do
     embed_code = "[Attachment: greenpaper.pdf]"
     body = "#Heading\n\n#{embed_code}\n\n##Subheading"
     attachment = build(:file_attachment_with_no_assets, title: "Green paper")
@@ -173,7 +175,7 @@ class GovspeakHelperTest < ActionView::TestCase
     refute_select_within_html html, ".gem-c-attachment"
   end
 
-  test "should ignore images with missing asset variants" do
+  it "should ignore images with missing asset variants" do
     embed_code = "[Image: minister-of-funk.960x640.jpg]"
     body = "#Heading\n\n#{embed_code}\n\n##Subheading"
     image = build(:image_with_no_assets)
@@ -185,7 +187,7 @@ class GovspeakHelperTest < ActionView::TestCase
     refute_select_within_html html, ".image.embedded"
   end
 
-  test "should ignore images with image kinds which do not permit use in govspeak embeds" do
+  it "should ignore images with image kinds which do not permit use in govspeak embeds" do
     embed_code = "[Image: minister-of-funk.960x640.jpg]"
     body = "#Heading\n\n#{embed_code}\n\n##Subheading"
     image = build(:image)
@@ -204,14 +206,14 @@ class GovspeakHelperTest < ActionView::TestCase
     refute_select_within_html html, ".image.embedded"
   end
 
-  test "should not convert documents with no block attachments" do
+  it "should not convert documents with no block attachments" do
     text = "#Heading\n\n!@2"
     document = build(:published_detailed_guide, body: text)
     html = govspeak_edition_to_html(document)
     refute_select_within_html html, ".gem-c-attachment"
   end
 
-  test "should not convert documents with no inline attachments" do
+  it "should not convert documents with no inline attachments" do
     text = "#Heading\n\nText about my [InlineAttachment:2]."
     document = build(:published_detailed_guide, body: text)
     html = govspeak_edition_to_html(document)
@@ -223,7 +225,7 @@ class GovspeakHelperTest < ActionView::TestCase
     "Govspeak syntax" => "#heading\n\n[Attachment: greenpaper.pdf]\n\n[Attachment: sample.csv]",
     "a mixture of both legacy and Govspeak syntax" => "#heading\n\n[Attachment: greenpaper.pdf]\n\n!@2",
   }.each do |name, document_body|
-    test "should convert multiple block attachments using #{name}" do
+    it "should convert multiple block attachments using #{name}" do
       document = build(
         :published_detailed_guide,
         body: document_body,
@@ -246,7 +248,7 @@ class GovspeakHelperTest < ActionView::TestCase
     "Govspeak syntax" => "#Heading\n\nText about my [AttachmentLink: greenpaper.pdf] and [AttachmentLink: sample.csv].",
     "a mixture of both legacy and Govspeak syntax" => "#Heading\n\nText about my [AttachmentLink: greenpaper.pdf] and [InlineAttachment:2].",
   }.each do |name, document_body|
-    test "should convert multiple inline attachments using #{name}" do
+    it "should convert multiple inline attachments using #{name}" do
       document = build(
         :published_detailed_guide,
         body: document_body,
@@ -264,7 +266,7 @@ class GovspeakHelperTest < ActionView::TestCase
     end
   end
 
-  test "should not escape embedded attachment when attachment embed code only separated by one newline from a previous paragraph" do
+  it "should not escape embedded attachment when attachment embed code only separated by one newline from a previous paragraph" do
     text = "para\n!@1"
     document = build(:published_detailed_guide, :with_file_attachment, body: text)
     html = govspeak_edition_to_html(document)
@@ -272,7 +274,7 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, ".gem-c-attachment__thumbnail"
   end
 
-  test "embeds images using !!number syntax" do
+  it "embeds images using !!number syntax" do
     edition = build(:published_news_article, body: "!!1")
     image_data = create(:image_data, id: 1)
     edition.stubs(:images).returns([OpenStruct.new(alt_text: "My Alt", url: "https://some.cdn.com/image.jpg", filename: "image.jpg", image_data: ImageData.find(image_data.id))])
@@ -280,7 +282,7 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, ".govspeak figure.image.embedded img[src='https://some.cdn.com/image.jpg']"
   end
 
-  test "embeds images using [Image:] syntax" do
+  it "embeds images using [Image:] syntax" do
     edition = build(:published_news_article, body: "[Image: image.jpg]")
     image_data = create(:image_data, id: 1)
     edition.stubs(:images).returns([OpenStruct.new(alt_text: "My Alt", url: "https://some.cdn.com/image.jpg", filename: "image.jpg", image_data: ImageData.find(image_data.id))])
@@ -288,13 +290,13 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, ".govspeak figure.image.embedded img[src='https://some.cdn.com/image.jpg']"
   end
 
-  test "adds numbers to h2 headings" do
+  it "adds numbers to h2 headings" do
     input = "# main\n\n## first\n\n## second"
     output = '<div class="govspeak"><h1 id="main">main</h1> <h2 id="first"> <span class="number">1. </span>first</h2> <h2 id="second"> <span class="number">2. </span>second</h2></div>'
     assert_equivalent_html output, govspeak_to_html(input, heading_numbering: :auto).gsub(/\s+/, " ")
   end
 
-  test "adds sub-numbers to h3 tags" do
+  it "adds sub-numbers to h3 tags" do
     input = "## first\n\n### first point one\n\n### first point two\n\n## second\n\n### second point one"
     expected_output1 = '<h2 id="first"> <span class="number">1. </span>first</h2>'
     expected_output_1a = '<h3 id="first-point-one"> <span class="number">1.1 </span>first point one</h3>'
@@ -309,25 +311,25 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_match %r{#{expected_output_2a}}, actual_output
   end
 
-  test "adds manual numbering to heading tags" do
+  it "adds manual numbering to heading tags" do
     input = "## 1. Main\n\n## 2. Second\n\n### Sub heading without a number\n\n## 42.12 Out of sequence\n\n## 0GD Not all numeric characters"
     expected_output = '<div class="govspeak"><h2 id="main"> <span class="number">1. </span> Main</h2> <h2 id="second"> <span class="number">2. </span> Second</h2> <h3 id="sub-heading-without-a-number">Sub heading without a number</h3> <h2 id="out-of-sequence"> <span class="number">42.12 </span> Out of sequence</h2> <h2 id="gd-not-all-numeric-characters"> <span class="number">0GD </span> Not all numeric characters</h2></div>'
     assert_equivalent_html expected_output, govspeak_to_html(input, heading_numbering: :manual).gsub(/\s+/, " ")
   end
 
-  test "leaves heading numbers not occuring at the start of the heading text alone when using manual heading numbering" do
+  it "leaves heading numbers not occuring at the start of the heading text alone when using manual heading numbering" do
     input = "## Number 8"
     result = Nokogiri::HTML::DocumentFragment.parse(govspeak_to_html(input, heading_numbering: :manual))
     assert_equal "Number 8", result.css("h2").first.text
   end
 
-  test "should not corrupt character encoding of numbered headings" do
+  it "should not corrupt character encoding of numbered headings" do
     input = "# café"
     actual_output = govspeak_to_html(input, heading_numbering: :auto)
     assert actual_output.include?("café</h1>")
   end
 
-  test "converts [Contact:<id>] into a rendering of contacts/_contact for the Contact with id = <id>" do
+  it "converts [Contact:<id>] into a rendering of contacts/_contact for the Contact with id = <id>" do
     contact = build(:contact)
     Contact.stubs(:find_by).with(id: "1").returns(contact)
     input = "[Contact:1]"
@@ -336,7 +338,7 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_equivalent_html "<div class=\"govspeak\">#{contact_html}</div>", output
   end
 
-  test "converts [Contact:<id>] into a rendering of contacts/_contact for the Contact with id = <id> with defined header level" do
+  it "converts [Contact:<id>] into a rendering of contacts/_contact for the Contact with id = <id> with defined header level" do
     contact = build(:contact)
     Contact.stubs(:find_by).with(id: "1").returns(contact)
     input = "[Contact:1]"
@@ -345,14 +347,14 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_equivalent_html "<div class=\"govspeak\">#{contact_html}</div>", output
   end
 
-  test "silently converts [Contact:<id>] into nothing if there is no Contact with id = <id>" do
+  it "silently converts [Contact:<id>] into nothing if there is no Contact with id = <id>" do
     Contact.stubs(:find_by).with(id: "1").returns(nil)
     input = "[Contact:1]"
     output = govspeak_to_html(input)
     assert_equivalent_html "<div class=\"govspeak\"></div>", output
   end
 
-  test "will use the html version of the contact partial, even if the view context is for a different format" do
+  it "will use the html version of the contact partial, even if the view context is for a different format" do
     contact = build(:contact)
     Contact.stubs(:find_by).with(id: "1").returns(contact)
     input = "[Contact:1]"
@@ -363,13 +365,13 @@ class GovspeakHelperTest < ActionView::TestCase
     end
   end
 
-  test "should convert a HTML attachment" do
+  it "should convert a HTML attachment" do
     html_attachment = create(:html_attachment, body: "## A heading")
     html = govspeak_html_attachment_to_html(html_attachment)
     assert_select_within_html html, ".govspeak h2"
   end
 
-  test "HTML attachments inherit images from their parent edition" do
+  it "HTML attachments inherit images from their parent edition" do
     edition = create(:published_publication, images: [build(:image)])
     body = "[Image: minister-of-funk.960x640.jpg]"
     html_attachment = create(:html_attachment, attachable: edition, body:)
@@ -377,26 +379,26 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_select_within_html html, ".govspeak figure.image.embedded img[src='#{edition.images.first.url}']"
   end
 
-  test "HTML attachments can embed images using !!number syntax" do
+  it "HTML attachments can embed images using !!number syntax" do
     edition = create(:published_publication, images: [build(:image)])
     html_attachment = create(:html_attachment, attachable: edition, body: "!!1")
     html = govspeak_html_attachment_to_html(html_attachment)
     assert_select_within_html html, ".govspeak figure.image.embedded img[src='#{edition.images.first.url}']"
   end
 
-  test "HTML attachment with automatically numbered headings" do
+  it "HTML attachment with automatically numbered headings" do
     html_attachment = create(:html_attachment, body: "## A heading", manually_numbered_headings: false)
     html = govspeak_html_attachment_to_html(html_attachment)
     assert_select_within_html html, ".govspeak h2", text: "1. A heading", count: 1
   end
 
-  test "HTML attachment with manually numbered headings" do
+  it "HTML attachment with manually numbered headings" do
     html_attachment = create(:html_attachment, body: "## A heading", manually_numbered_headings: true)
     html = govspeak_html_attachment_to_html(html_attachment)
     assert_select_within_html html, ".govspeak h2", text: "A heading", count: 1
   end
 
-  test "HTML attachments cannot embed attachments from their parent edition" do
+  it "HTML attachments cannot embed attachments from their parent edition" do
     body = <<~MARKDOWN
       Every way to embed an attachment:
 
@@ -419,7 +421,7 @@ class GovspeakHelperTest < ActionView::TestCase
     assert_equivalent_html '<div class="govspeak"><p>Every way to embed an attachment:</p></div>', html
   end
 
-  test "uses locale of HTML attachment" do
+  it "uses locale of HTML attachment" do
     body = <<~MARKDOWN
       Footnote[^1]
 
