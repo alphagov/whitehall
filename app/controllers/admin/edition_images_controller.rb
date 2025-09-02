415a42e5764fbc8f9cbdb7c5de9fba3bee83c83f
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-09-02 08:31:44 +0100

    Save `crop_data` to `ImageData`
    Allows images that are too big to be saved. When this happens
    the user will be asked to crop the image on the `index` page. This
    change means that images can be recropped, the cropping data can be
    used in other contexts and multiple images can be uploaded without
    validation errors if all/some of them are too big.

--- app/controllers/admin/edition_images_controller.rb
+++ app/controllers/admin/edition_images_controller.rb
@@ -16,7 +16,21 @@ class Admin::EditionImagesController < Admin::BaseController
   end
 
   def update
-    if image.update(params.require(:image).permit(:caption, :alt_text))
+    image.assign_attributes(image_params)
+
+    if image_data_params["crop_data"].present?
+      image_data = image.image_data
+      new_image_data = ImageData.new
+      new_image_data.to_replace_id = image_data.id
+      new_image_data.assign_attributes(image_data_params)
+      new_image_data.file.download! image_data.file.url
+      new_image_data.save!
+      image.image_data = new_image_data
+    end
+
+    image.image_data.validate_on_image = image
+
+    if image.save
       PublishingApiDocumentRepublishingWorker.perform_async(@edition.document_id)
       redirect_to admin_edition_images_path(@edition), notice: "#{image.image_data.carrierwave_image} details updated"
     else
@@ -29,43 +43,31 @@ class Admin::EditionImagesController < Admin::BaseController
     @new_image.build_image_data(image_params["image_data"])
 
     @new_image.image_data.validate_on_image = @new_image
-    # so that auth_bypass_id is discoverable by AssetManagerStorage
+
     @new_image.image_data.images << @new_image
 
     if @new_image.save
       @edition.update_lead_image if @edition.can_have_custom_lead_image?
       PublishingApiDocumentRepublishingWorker.perform_async(@edition.document_id)
-      redirect_to edit_admin_edition_image_path(@edition, @new_image.id), notice: "#{@new_image.filename} successfully uploaded"
-    elsif new_image_needs_cropping?
-      image_kind_config = @new_image.image_data.image_kind_config
-      @valid_width = image_kind_config.valid_width
-      @valid_height = image_kind_config.valid_height
-      @data_url = image_data_url
-      render :crop
+      flash.notice = "#{@new_image.filename} successfully uploaded"
     else
-      @new_image.errors.delete(:"image_data.file", :too_large)
       # Remove @new_image from @edition.images array, otherwise the view will render it in the 'Uploaded images' list
       @edition.images.delete(@new_image)
-      render :index
     end
+
+    render :index
   end
 
   def edit
-    image = Image.find(params[:id])
-    flash.now.notice = "The image is being processed. Try refreshing the page." unless image&.image_data&.all_asset_variants_uploaded?
+    flash.now.notice = "The image is being processed. Try refreshing the page." unless image&.image_data&.original_uploaded?
   end
 
 private
 
-  def new_image_needs_cropping?
-    @new_image.errors.of_kind?(:"image_data.file", :too_large) && @new_image.errors.size == 1
+  def image_kind_config
+    image.image_data.image_kind_config
   end
-
-  def image_data_url
-    file = @new_image.image_data.file
-    image_data = Base64.strict_encode64(file.read)
-    "data:#{file.content_type};base64,#{image_data}"
-  end
+  helper_method :image_kind_config
 
   def image
     @image ||= find_image
@@ -92,7 +94,11 @@ private
     end
   end
 
+  def image_data_params
+    params.fetch(:image, {}).fetch(:image_data, {}).permit(:file, :image_kind, crop_data: %i[x y width height])
+  end
+
   def image_params
-    params.fetch(:image, {}).permit(image_data: %i[file image_kind])
+    params.fetch(:image, {}).except(:image_data).permit(:caption, :alt_text, image_data: %i[crop_data file image_kind])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_01_125325) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_22_102338) do
   create_table "assets", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "asset_manager_id", null: false
     t.string "variant", null: false
@@ -591,6 +591,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_01_125325) do
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
     t.string "image_kind", default: "default", null: false
+    t.json "dimensions"
+    t.json "crop_data"
+    t.integer "replaced_by_id"
   end
 
   create_table "images", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
