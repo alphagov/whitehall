35e659276a907db27ecf7dd38d6d7d278842e278
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-09-11 19:23:38 +0100

    Fetch content blocks from Publishing API
    ... rather than from local db.
    
    We:
    
    - pass a list of the `content_id_aliases` for which we want to
      retrieve content blocks. This behaviour was added to
      Publishing API for this purpose in [PR 3593][].
    
    - specify that only blocks in the `published` state should
      be returned by the Publishing API
    
    [PR 3593]:
    https://github.com/alphagov/publishing-api/commit/135d3dc34dcf4bd9525c5bf0802a16b50cd69c59

--- app/services/content_block/find_and_replace_embed_codes_service.rb
+++ app/services/content_block/find_and_replace_embed_codes_service.rb
@@ -7,11 +7,14 @@ module ContentBlock
     def call
       embed_content_references.uniq.each do |reference|
         content_block = content_blocks.find do |c|
-          c.document.content_id == reference.identifier || c.document.content_id_alias == reference.identifier
+          c.fetch("content_id_aliases").first.fetch("name") == reference.identifier
         end
         next if content_block.nil?
 
-        html.gsub!(reference.embed_code, content_block.render(reference.embed_code))
+        html.gsub!(
+          reference.embed_code,
+          render_block(content_block: content_block, embed_code: reference.embed_code),
+        )
       end
 
       html
@@ -29,16 +32,32 @@ module ContentBlock
       @embed_content_references ||= ContentBlockTools::ContentBlockReference.find_all_in_document(html)
     end
 
-    def identifiers
-      embed_content_references.map(&:identifier)
+    def unique_identifiers
+      embed_content_references.uniq.map(&:identifier)
     end
 
     def content_blocks
-      @content_blocks ||= begin
-        scope = ContentBlockManager::ContentBlock::Edition.current_versions
-        scope.where(document: { content_id: identifiers })
-             .or(scope.where(document: { content_id_alias: identifiers }))
-      end
+      @content_blocks ||= get_content_items_from_publishing_api
+    end
+
+    def get_content_items_from_publishing_api
+      Services
+        .publishing_api
+        .get_content_items(
+          content_id_aliases: unique_identifiers,
+          fields: %w[title content_id content_id_aliases details document_type],
+          states: %w[published],
+        )["results"]
+    end
+
+    def render_block(content_block:, embed_code:)
+      ContentBlockTools::ContentBlock.new(
+        document_type: content_block.fetch("document_type"),
+        content_id: content_block.fetch("content_id"),
+        title: content_block.fetch("title"),
+        details: content_block.fetch("details"),
+        embed_code: embed_code,
+      ).render
     end
   end
 end
