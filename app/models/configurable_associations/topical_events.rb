6f67b81d4050870ed096c0119344faab558a9cb5
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2025-09-11 16:32:50 +0100

    Move caching into Topical Events association

--- app/models/configurable_associations/topical_events.rb
+++ app/models/configurable_associations/topical_events.rb
@@ -16,6 +16,16 @@ module ConfigurableAssociations
       @association.map(&:content_id)
     end
 
+    def options_query
+      TopicalEvent.order(:name)
+    end
+
+    def template_cache_digest
+      topical_event_timestamps = options_query.pluck(:updated_at)
+      update_timestamps = topical_event_timestamps.map(&:to_i).join
+      Digest::MD5.hexdigest "topical-events-#{update_timestamps}"
+    end
+
     def to_partial_path
       "admin/configurable_associations/topical_events"
     end
