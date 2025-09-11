1580aed86a219c390ae92a46973289ec59eb9e67
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-09-11 11:16:14 +0100

    Move ministerial role appointment options list build into role appointments association
    We want to keep code related to association value selection colocated as far as possible, so we move the querying and caching behaviour for loading the available ministerial appointments into the association object.

--- app/models/configurable_associations/role_appointments.rb
+++ app/models/configurable_associations/role_appointments.rb
@@ -13,6 +13,19 @@ module ConfigurableAssociations
       @association.ids
     end
 
+    def scoped_appointments
+      RoleAppointment.for_ministerial_roles
+        .includes(:person)
+        .includes(organisations: :translations)
+        .ascending_start_date
+    end
+
+    def template_cache_digest
+      role_appointment_timestamps = scoped_appointments.pluck(:updated_at)
+      update_timestamps = role_appointment_timestamps.map(&:to_i).join
+      Digest::MD5.hexdigest "role-appointments-#{update_timestamps}"
+    end
+
     def to_partial_path
       "admin/configurable_associations/role_appointments"
     end
