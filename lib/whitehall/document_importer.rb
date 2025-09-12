2dacb883b5675a08118f9176b9159a04787ca52a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-09-12 14:07:12 +0100

    Add DocumentImporter class and tests for basic properties
    This class will be called by a worker in a later commit, for
    doing the actual import. This first commit just sets up the
    basics: slug, title, body, state, timestamps etc. In subsequent
    commits we'll tackle the trickier aspects that need importing.
    
    Note that I originally started using Timecop to overwrite the
    system time at the point of import, so that documents were being
    imported 'at the time' they were created. But there was a major
    problem with this: Timecop pollutes time globally and is only
    intended to be used in test suites. Consequences:
    
    1. Running the bulk import (batching lots of Sidekiq workers)
       didn't work as expected, as several time overrides would happen
       in parallel and thus documents would be given unpredictable
       created_at dates.
    2. During the bulk import with all the time overwriting going on,
       there was every chance that some unrelated live production
       activity could be affected and its timestamps effectively
       corrupted.
    
    Instead, I now import the docs as-is and then updated the created_at
    values wherever needed, after the fact.

---
+++ lib/whitehall/document_importer.rb
@@ -0,0 +1,75 @@
+require "open-uri"
+require "pdf-reader"
+
+class Whitehall::DocumentImporter
+  def self.import!(data)
+    edition = create_base_edition!(data)
+    # Whitehall's in-house 'AuditTrail' is used to populate the timeline in
+    # the sidebar, so we need to overwrite the created_at date.
+    edition.most_recent_version.update!(created_at: data["created_at"])
+
+    edition.document.update_columns(
+      created_at: data["created_at"],
+      content_id: data["content_id"],
+      slug: data["base_path"].split("/").last,
+    )
+    edition.document
+  end
+
+  def self.create_base_edition!(data)
+    user = User.find_by(email: data["created_by"]) || Whitehall::DocumentImporter.robot_user
+    AuditTrail.acting_as(user) do
+      edition = StandardEdition.new(
+        configurable_document_type: data["document_type"],
+        state: derived_state(data["state"]),
+        title: data["title"],
+        summary: data["summary"],
+        block_content: {
+          "body" => data["body"],
+        },
+        political: data["political"],
+        alternative_format_provider_id: Organisation.find_by(content_id: data["tags"]["primary_publishing_organisation"]).id,
+      )
+      edition.creator = user
+      set_publishing_metadata(edition, data)
+      edition.save!
+      return edition
+    end
+  end
+
+  def self.derived_state(state)
+    case state
+    when "published", "published_but_needs_2i"
+      "published"
+    when "withdrawn"
+      "withdrawn"
+    else
+      raise "Unsupported state: #{state}"
+    end
+  end
+
+  def self.set_publishing_metadata(edition, data)
+    edition.created_at = data["created_at"]
+    edition.first_published_at = data["first_published_at"]
+    edition.previously_published = data["created_at"] > data["first_published_at"]
+
+    # We're only importing a single 'flattened' edition
+    # from Content Publisher. This doesn't play nicely with the
+    # Edition model's `set_public_timestamp` callback, which checks
+    #  if this is the "first published version" (defined in `Edition::Publishing`),
+    # which is true if `published_major_version` is `nil` or `1`.
+    # It would then set `public_timestamp` to `first_published_at`,
+    # which is not the correct timestamp if there was a subsequent
+    # major version.
+    # By setting `published_major_version` to a minimum of `2`, the
+    # `set_public_timestamp` falls back to `major_change_published_at`,
+    # which we've set to match the latest public changenote timestamp
+    # associated with the edition, which is the correct value.
+    edition.published_major_version = 2
+    edition.major_change_published_at = data["change_notes"].first["public_timestamp"]
+  end
+
+  def self.robot_user
+    User.find_by(name: "Scheduled Publishing Robot")
+  end
+end
