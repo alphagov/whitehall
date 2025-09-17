677f1a96b9cbadafddcd2bc4168e0a23a98be660
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2025-09-17 17:45:50 +0100

    Add organisations association to publishing api payload

--- app/models/configurable_associations/organisations.rb
+++ app/models/configurable_associations/organisations.rb
@@ -7,6 +7,10 @@ module ConfigurableAssociations
 
     attr_reader :errors
 
+    def publishing_api_links_key
+      :organisations
+    end
+
     def selected_lead_organisation_id_at(lead_organisation_index)
       @association.select { |edition_org| lead_organisation_selector(lead_organisation_index, edition_org) }
                   .first.try(:organisation_id)
@@ -14,6 +18,11 @@ module ConfigurableAssociations
 
     def selected_supporting_organisation_ids
       @association.reject(&:lead?).map(&:organisation_id)
+    end
+
+    def selected_content_ids
+      @association.preload(:organisation)
+      @association.map { |edition_org| edition_org.organisation.content_id }
     end
 
     def options_query
