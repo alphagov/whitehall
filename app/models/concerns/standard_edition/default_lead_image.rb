46bfc04e03b7e1e730ec7ce267ebaec12b432fd8
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-10-20 13:11:34 +0100

    Check that organisation methods are available
    Just like we check for worldwide organisations, we should check for organisations and lead organisations. It could be that the associations have been missed out from whatever type of edition (such as `StandardEdition`) is trying to implement the lead image.
    
    Alternatively, we could include the Organisations concern here, but that's included as an association, conditionally, so including it here would make that conditional inclusion redundant.

--- app/models/concerns/standard_edition/default_lead_image.rb
+++ app/models/concerns/standard_edition/default_lead_image.rb
@@ -2,9 +2,9 @@ module StandardEdition::DefaultLeadImage
   extend ActiveSupport::Concern
 
   def default_lead_image
-    if lead_organisations.any? && lead_organisations.first.default_news_image
+    if respond_to?(:lead_organisations) && lead_organisations.any? && lead_organisations.first.default_news_image
       lead_organisations.first.default_news_image
-    elsif organisations.any? && organisations.first.default_news_image
+    elsif respond_to?(:organisations) && organisations.any? && organisations.first.default_news_image
       organisations.first.default_news_image
     elsif respond_to?(:worldwide_organisations) && published_worldwide_organisations.any? && published_worldwide_organisations.first.default_news_image
       published_worldwide_organisations.first.default_news_image
