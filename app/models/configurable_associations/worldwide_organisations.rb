dc43a0ceeab5065c18c4038cbf41ad0694e24a8d
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-10-23 14:24:17 +0100

    Render worldwide organisation association select

--- app/models/configurable_associations/worldwide_organisations.rb
+++ app/models/configurable_associations/worldwide_organisations.rb
@@ -1,5 +1,7 @@
 module ConfigurableAssociations
   class WorldwideOrganisations
+    include Admin::TaggableContentHelper
+
     def initialize(association, errors)
       @association = association
       @errors = errors
@@ -9,6 +11,14 @@ module ConfigurableAssociations
       {
         worldwide_organisations: @association.map { |edition_worldwide_org| edition_worldwide_org.document.content_id },
       }
+    end
+
+    def options
+      taggable_worldwide_organisations_container(@association)
+    end
+
+    def to_partial_path
+      "admin/configurable_associations/worldwide_organisations"
     end
   end
 end
--- test/unit/app/models/configurable_associations/worldwide_organisations_test.rb
+++ test/unit/app/models/configurable_associations/worldwide_organisations_test.rb
@@ -14,3 +14,18 @@ class WorldwideOrganisationsTest < ActiveSupport::TestCase
     assert_equal expected_links, worldwide_organisations_association.links
   end
 end
+
+class WorldWideOrganisationsRenderingTest < ActionView::TestCase
+  test "it renders the worldwide organisations form control" do
+    worldwide_organisations = [create(:worldwide_organisation, title: "WWO 2"), create(:worldwide_organisation, title: "WWO 1")]
+    edition = build(:draft_standard_edition)
+    edition.edition_worldwide_organisations.build([{ document: worldwide_organisations.first.document }, { document: worldwide_organisations.last.document }])
+
+    render ConfigurableAssociations::WorldwideOrganisations.new(edition.edition_worldwide_organisations, edition.errors)
+
+    assert_dom "label", text: "Worldwide organisations"
+    worldwide_organisations.each do |worldwide_organisation|
+      assert_dom "option", text: worldwide_organisation.name
+    end
+  end
+end
