uk-gov-mirror/alphagov.whitehall

Support searching config-driven documents by their parent doctype
Whitehall has historically allowed filtering by 'schema' in
addition to 'document type', i.e. choosing "News Articles" from
the dropdown should surface News Stories, Press Releases,
Government Responses and World News Stories.

In #10672, we extended Whitehall search to include both 'legacy'
and 'config-driven' news articles when searching by document type,
i.e. choosing "Press Releases" would surface both legacy Press
Releases and config-driven (StandardEdition) Press Releases. But
it didn't support surfacing config-driven documents in the schema
level search. This commit fixes that. It arguably makes the
search logic easier to follow now too, as everything is part of
the same predicates/Arel based approach instead of returning
early in the special case of a standalone doc type (e.g. Consultation)
being filtered.

Jira: https://gov-uk.atlassian.net/browse/WHIT-2640

app/models/admin/edition_filter.rb
@@ -158,34 +158,47 @@ module Admin
     end
 
     def scope_by_type(editions, type)
-      type_class_name = type.sub(/_\d+$/, "").classify
-      return editions.where(type: type_class_name) if EDITION_TYPE_LOOKUP[type_class_name]
-
-      # `type` (e.g. `press_release`) is not in the EDITION_TYPE_LOOKUP, so
-      # this is either a StandardEdition type, or a "subtype" of a legacy
-      # document type. So we:
-      #
       # 1. Check `ConfigurableDocumentType` to see if the `type` is a known
       #    type of StandardEdition.
-      # 2. In addition, loop through all the legacy types:
+      # 2. In addition, check `ConfigurableDocumentType` to see if `type` is
+      #    a known 'schema' (e.g. "news_article") that maps to multiple
+      #    subtypes (which would all be in scope).
+      # 3. In addition, check if `type` matches any legacy 'standalone' types
+      #    (e.g. "CaseStudy") directly.
+      # 4. In addition, loop through all the legacy 'schema' types:
       #    NewsArticleType, PublicationType, SpeechType
       #    Dynamically check their subtypes to see if it matches the `type`.
       #
-      # We could find editions for BOTH of the above, so we can't just
-      # build on the same `edition.where` call. We need to build some
-      # OR-clauses (from a fresh base relation for the same model) and
-      # then merge that back into the incoming Relation. We do that by
-      # building predicates on the editions table only (Arel), then adding
-      # them with a single `where`.
+      # We need to build some OR-clauses (from a fresh base relation for the
+      # same model) and then merge that back into the incoming Relation so
+      # that the search is performed efficiently.
+      # We do that by building predicates on the editions table only (Arel),
+      # then adding them with a single `where`.
+
       table = Edition.arel_table
       predicates = []
 
-      # StandardEdition with configurable_document_type
+      # 1. StandardEdition with configurable_document_type
       if ConfigurableDocumentType.all_keys.include?(type)
         predicates << table[:type].eq("StandardEdition").and(table[:configurable_document_type].eq(type))
       end
 
-      # Legacy subtypes (TODO: delete this block and refactor this method when the legacy types are removed)
+      # 2. StandardEdition with schema mapping to multiple types
+      # e.g. "news_article" => ["news_story", "press_release", ...]
+      matching_types = ConfigurableDocumentType.all.select { |cdt| cdt.settings["publishing_api_schema_name"] == type }
+      if matching_types.any?
+        predicates << table[:type].eq("StandardEdition").and(
+          table[:configurable_document_type].in(matching_types.map(&:key)),
+        )
+      end
+
+      # 3. Legacy standalone types
+      type_class_name = type.sub(/_\d+$/, "").classify
+      if EDITION_TYPE_LOOKUP[type_class_name]
+        predicates << table[:type].eq(type_class_name)
+      end
+
+      # 4. Legacy subtypes (TODO: delete this block and refactor this method when these legacy types are removed)
       %w[NewsArticle Publication Speech].each do |parent_type|
         type_class = "#{parent_type}Type".constantize
         if (subtype = type_class.all.select { |k| k.key == type }.first)

test/unit/app/models/admin/edition_filter_test.rb
@@ -171,6 +171,22 @@ class Admin::EditionFilterTest < ActiveSupport::TestCase
     assert_equal [edition], Admin::EditionFilter.new(Edition, @current_user, type: "test_type").editions
   end
 
+  test "should filter by news article category" do
+    news_story_document_type = build_configurable_document_type("news_story", { "settings" => { "publishing_api_schema_name" => "news_article" } })
+    press_release_document_type = build_configurable_document_type("press_release", { "settings" => { "publishing_api_schema_name" => "news_article" } })
+    subtypes = news_story_document_type.deep_merge(press_release_document_type)
+    ConfigurableDocumentType.setup_test_types(subtypes)
+
+    # config-driven sub-types
+    config_driven_news_article = create(:draft_standard_edition, configurable_document_type: "news_story")
+    config_driven_press_release = create(:draft_standard_edition, configurable_document_type: "press_release")
+    # legacy sub-types
+    news_story = create(:news_article, news_article_type: NewsArticleType::NewsStory)
+    press_release = create(:news_article, news_article_type: NewsArticleType::PressRelease)
+
+    assert_equal [config_driven_news_article, config_driven_press_release, news_story, press_release], Admin::EditionFilter.new(Edition, @current_user, type: "news_article").editions.sort_by(&:id)
+  end
+
   test "should filter by news article sub-type" do
     _news_story   = create(:news_article, news_article_type: NewsArticleType::NewsStory)
     press_release = create(:news_article, news_article_type: NewsArticleType::PressRelease)

alphagov/whitehall

app/models/admin/edition_filter.rb
@@ -158,34 +158,47 @@ module Admin
     end
 
     def scope_by_type(editions, type)
-      type_class_name = type.sub(/_\d+$/, "").classify
-      return editions.where(type: type_class_name) if EDITION_TYPE_LOOKUP[type_class_name]
-
-      # `type` (e.g. `press_release`) is not in the EDITION_TYPE_LOOKUP, so
-      # this is either a StandardEdition type, or a "subtype" of a legacy
-      # document type. So we:
-      #
       # 1. Check `ConfigurableDocumentType` to see if the `type` is a known
       #    type of StandardEdition.
-      # 2. In addition, loop through all the legacy types:
+      # 2. In addition, check `ConfigurableDocumentType` to see if `type` is
+      #    a known 'schema' (e.g. "news_article") that maps to multiple
+      #    subtypes (which would all be in scope).
+      # 3. In addition, check if `type` matches any legacy 'standalone' types
+      #    (e.g. "CaseStudy") directly.
+      # 4. In addition, loop through all the legacy 'schema' types:
       #    NewsArticleType, PublicationType, SpeechType
       #    Dynamically check their subtypes to see if it matches the `type`.
       #
-      # We could find editions for BOTH of the above, so we can't just
-      # build on the same `edition.where` call. We need to build some
-      # OR-clauses (from a fresh base relation for the same model) and
-      # then merge that back into the incoming Relation. We do that by
-      # building predicates on the editions table only (Arel), then adding
-      # them with a single `where`.
+      # We need to build some OR-clauses (from a fresh base relation for the
+      # same model) and then merge that back into the incoming Relation so
+      # that the search is performed efficiently.
+      # We do that by building predicates on the editions table only (Arel),
+      # then adding them with a single `where`.
+
       table = Edition.arel_table
       predicates = []
 
-      # StandardEdition with configurable_document_type
+      # 1. StandardEdition with configurable_document_type
       if ConfigurableDocumentType.all_keys.include?(type)
         predicates << table[:type].eq("StandardEdition").and(table[:configurable_document_type].eq(type))
       end
 
-      # Legacy subtypes (TODO: delete this block and refactor this method when the legacy types are removed)
+      # 2. StandardEdition with schema mapping to multiple types
+      # e.g. "news_article" => ["news_story", "press_release", ...]
+      matching_types = ConfigurableDocumentType.all.select { |cdt| cdt.settings["configurable_document_group"] == type }
+      if matching_types.any?
+        predicates << table[:type].eq("StandardEdition").and(
+          table[:configurable_document_type].in(matching_types.map(&:key)),
+        )
+      end
+
+      # 3. Legacy standalone types
+      type_class_name = type.sub(/_\d+$/, "").classify
+      if EDITION_TYPE_LOOKUP[type_class_name]
+        predicates << table[:type].eq(type_class_name)
+      end
+
+      # 4. Legacy subtypes (TODO: delete this block and refactor this method when these legacy types are removed)
       %w[NewsArticle Publication Speech].each do |parent_type|
         type_class = "#{parent_type}Type".constantize
         if (subtype = type_class.all.select { |k| k.key == type }.first)

test/unit/app/models/admin/edition_filter_test.rb
@@ -171,6 +171,22 @@ class Admin::EditionFilterTest < ActiveSupport::TestCase
     assert_equal [edition], Admin::EditionFilter.new(Edition, @current_user, type: "test_type").editions
   end
 
+  test "should filter by news article category" do
+    news_story_document_type = build_configurable_document_type("news_story", { "settings" => { "configurable_document_group" => "news_article" } })
+    press_release_document_type = build_configurable_document_type("press_release", { "settings" => { "configurable_document_group" => "news_article" } })
+    subtypes = news_story_document_type.deep_merge(press_release_document_type)
+    ConfigurableDocumentType.setup_test_types(subtypes)
+
+    # config-driven sub-types
+    config_driven_news_article = create(:draft_standard_edition, configurable_document_type: "news_story")
+    config_driven_press_release = create(:draft_standard_edition, configurable_document_type: "press_release")
+    # legacy sub-types
+    news_story = create(:news_article, news_article_type: NewsArticleType::NewsStory)
+    press_release = create(:news_article, news_article_type: NewsArticleType::PressRelease)
+
+    assert_equal [config_driven_news_article, config_driven_press_release, news_story, press_release], Admin::EditionFilter.new(Edition, @current_user, type: "news_article").editions.sort_by(&:id)
+  end
+
   test "should filter by news article sub-type" do
     _news_story   = create(:news_article, news_article_type: NewsArticleType::NewsStory)
     press_release = create(:news_article, news_article_type: NewsArticleType::PressRelease)