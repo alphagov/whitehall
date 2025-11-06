39879352db83d1eeae70081682fce01e0197e27a
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-11-06 16:44:48 +0000

    Enable users to edit the redirect URL for an unpublishing
    We occasionally get support tickets from users requesting that we change the redirect URL for an unpublished document. This commit allows users to update the redirect via the Whitehall interface.

--- test/functional/admin/edition_unpublishing_controller_test.rb
+++ test/functional/admin/edition_unpublishing_controller_test.rb
@@ -7,56 +7,96 @@ class Admin::EditionUnpublishingControllerTest < ActionController::TestCase
   def setup
     @user = create(:managing_editor)
     login_as(@user)
-    @edition = create(:withdrawn_edition)
   end
 
-  test "#edit loads the unpublishing and renders the unpublish legacy edit template" do
-    unpublishing = @edition.unpublishing
-    get :edit, params: { edition_id: @edition.id }
+  view_test "#edit loads the unpublishing and renders the edit form" do
+    edition = create(:edition, :published_in_error_redirect)
+    get :edit, params: { edition_id: edition.id }
 
     assert_response :success
     assert_template :edit
-    assert_equal unpublishing, assigns(:unpublishing)
+    assert_equal edition.unpublishing, assigns(:unpublishing)
+    assert_dom "textarea[name=\"unpublishing[explanation]\"]"
+    assert_dom "input[name=\"unpublishing[alternative_url]\"]"
+  end
+
+  view_test "#edit loads the consolidated unpublishing and renders the edit form" do
+    edition = create(:edition, :consolidated_redirect)
+    get :edit, params: { edition_id: edition.id }
+
+    assert_response :success
+    assert_template :edit
+    assert_equal edition.unpublishing, assigns(:unpublishing)
+    refute_dom "textarea[name=\"unpublishing[explanation]\"]"
+    assert_dom "input[name=\"unpublishing[alternative_url]\"]"
+  end
+
+  view_test "#edit loads the withdrawal and renders the edit form" do
+    edition = create(:edition, :withdrawn)
+    get :edit, params: { edition_id: edition.id }
+
+    assert_response :success
+    assert_template :edit
+    assert_equal edition.unpublishing, assigns(:unpublishing)
+    assert_dom "textarea[name=\"unpublishing[explanation]\"]"
+    refute_dom "input[name=\"unpublishing[alternative_url]\"]"
   end
 
   test "#update updates the withdrawal and redirects to admin policy page" do
+    edition = create(:edition, :withdrawn)
     Whitehall.edition_services
       .expects(:withdrawer)
-      .with(@edition, user: @user)
+      .with(edition, user: @user)
       .returns(withdrawer = stub)
 
     withdrawer.expects(:perform!)
 
-    put :update, params: { edition_id: @edition.id, unpublishing: { explanation: "this used to say withdrawn" } }
+    put :update, params: { edition_id: edition.id, unpublishing: { explanation: "this used to say withdrawn" } }
 
-    assert_redirected_to @controller.admin_edition_path(@edition)
-    assert_equal "The public explanation was updated", flash[:notice]
-    assert_equal "this used to say withdrawn", @edition.unpublishing.reload.explanation
+    assert_redirected_to @controller.admin_edition_path(edition)
+    assert_equal "The withdrawal was updated", flash[:notice]
+    assert_equal "this used to say withdrawn", edition.unpublishing.reload.explanation
   end
 
   test "#update updates the unpublishing and redirects to admin policy page" do
-    @unpublished_edition = create(:unpublished_edition)
+    edition = create(:edition, :published_in_error_redirect)
 
     Whitehall.edition_services
       .expects(:unpublisher)
-      .with(@unpublished_edition)
+      .with(edition)
       .returns(unpublisher = stub)
 
     unpublisher.expects(:perform!)
 
-    put :update, params: { edition_id: @unpublished_edition.id, unpublishing: { explanation: "this used to say unpublished" } }
+    put :update, params: { edition_id: edition.id,
+                           unpublishing: {
+                             explanation: "this used to say unpublished",
+                             alternative_url: "https://gov.uk/some-page",
+                           } }
 
-    assert_redirected_to @controller.admin_edition_path(@unpublished_edition)
-    assert_equal "The public explanation was updated", flash[:notice]
-    assert_equal "this used to say unpublished", @unpublished_edition.unpublishing.reload.explanation
+    assert_redirected_to @controller.admin_edition_path(edition)
+    assert_equal "The unpublishing was updated", flash[:notice]
+    assert_equal "this used to say unpublished", edition.unpublishing.reload.explanation
+    assert_equal "https://gov.uk/some-page", edition.unpublishing.reload.alternative_url
   end
 
-  test "#update shows legacy form with error if the update was not possible" do
-    unpublishing = @edition.unpublishing
-    original_explanation = unpublishing.explanation
-    put :update, params: { edition_id: @edition, unpublishing: { explanation: nil } }
+  view_test "#updating the withdrawal shows the error message if the update was not possible" do
+    edition = create(:edition, :withdrawn)
+    original_explanation = edition.unpublishing.explanation
+    put :update, params: { edition_id: edition, unpublishing: { explanation: "" } }
 
     assert_template :edit
-    assert_equal original_explanation, unpublishing.reload.explanation
+    assert_equal original_explanation, edition.unpublishing.reload.explanation
+
+    error = assigns(:unpublishing).errors.where("explanation").first
+    assert_dom ".govuk-error-message", text: "Error: #{error.full_message}"
+  end
+
+  view_test "#updating the unpublishing shows the error message if the update was not possible" do
+    edition = create(:edition, :published_in_error_redirect)
+    put :update, params: { edition_id: edition.id, unpublishing: { explanation: "", alternative_url: "not a URL" } }
+
+    errors = assigns(:unpublishing).errors.where(:alternative_url)
+    assert_dom ".govuk-error-message", text: "Error: #{errors.map(&:full_message).join}"
   end
 end
