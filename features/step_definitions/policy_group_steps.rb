bd8f968c36d601fc018d6b6fac00b1ab35277ddf
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2025-11-13 14:01:46 +0000

    Move methods inline into policy_group_steps
    These methods were only used in one step definition, so let's
    move them inline.

--- features/step_definitions/policy_group_steps.rb
+++ features/step_definitions/policy_group_steps.rb
@@ -20,3 +20,17 @@ Then(/^I should be able to add attachments to the policy group "(.*?)"$/) do |gr
   attachment = upload_pdf_to_policy_group(group)
   insert_attachment_markdown_into_policy_group_description(attachment, group)
 end
+
+def upload_pdf_to_policy_group(group)
+  visit edit_admin_policy_group_path(group)
+  first(:link, "Attachments").click
+  upload_new_attachment(pdf_attachment, "A PDF attachment")
+end
+
+def insert_attachment_markdown_into_policy_group_description(attachment, group)
+  visit admin_policy_group_attachments_path(group)
+  markdown = first("li:contains('#{attachment.title}') input").value
+  visit edit_admin_policy_group_path(group)
+  fill_in "Description", with: "#{group.description}\n\n#{markdown}"
+  click_button "Save"
+end
