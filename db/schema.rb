848a666e8a6ec443548ac3016d6be8b0bf935d14
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2025-11-18 08:52:17 +0000

    Remove alt_text column from images table
    This migration removes the alt_text column from the images table
    as it is no longer used after the code cleanup in the previous PR.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_27_103851) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_18_085108) do
   create_table "assets", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "asset_manager_id", null: false
     t.string "variant", null: false
@@ -600,7 +600,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_103851) do
   create_table "images", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "image_data_id"
     t.integer "edition_id"
-    t.string "alt_text"
     t.text "caption", size: :medium
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
