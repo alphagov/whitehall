a11ab9752193ee72b47df7ebe2340b648ac07a9e
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-12-16 08:17:03 +0000

    Initialize document type with forms and presenters
    This ensures we can define a different type setup in future commits, one that is aware of new properties such as `presenters` and `forms`.

--- test/support/configurable_document_type_helper.rb
+++ test/support/configurable_document_type_helper.rb
@@ -30,4 +30,47 @@ module ConfigurableDocumentTypeHelper
       }.deep_merge(attributes),
     }
   end
+
+  def build_configurable_document_type_with_forms(type, attributes = {})
+    {
+      type => {
+        "key" => type,
+        "title" => "Test type",
+        "forms" => {
+          "test_attribute" => {
+            "fields" => {
+              "field_attribute" => {
+                "title" => "Test Attribute",
+                "block" => "govspeak",
+              },
+            },
+          },
+        },
+        "schema" => {
+          "attributes" => {
+            "field_attribute" => {
+              "type" => "string",
+            },
+          },
+        },
+        "presenters" => {
+          "publishing_api" => {
+            "field_attribute" => "string",
+          },
+        },
+        "associations" => [],
+        "settings" => {
+          "base_path_prefix" => "/government/test",
+          "publishing_api_schema_name" => "test_article",
+          "publishing_api_document_type" => "test_story",
+          "rendering_app" => "frontend",
+          "images_enabled" => false,
+          "organisations" => nil,
+          "backdating_enabled" => false,
+          "history_mode_enabled" => false,
+          "translations_enabled" => false,
+        },
+      }.deep_merge(attributes),
+    }
+  end
 end
