43dab3c0512d95eaba5f50020eb5cf54f822e09b
kashifatcha <kashif.atcha@digital.cabinet-office.gov.uk>
2025-12-16 15:42:55 +0000

    Change all_stylesheets to component_guide_stylesheet

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -2,7 +2,7 @@ APP_STYLESHEETS = {
   "application.scss" => "application.css",
 }.freeze
 
-all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.component_guide_stylesheet)
 Rails.application.config.dartsass.builds = all_stylesheets
 
 Rails.application.config.dartsass.build_options << " --quiet-deps"
