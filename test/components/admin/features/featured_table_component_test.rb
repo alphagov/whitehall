bfd63219ab485b0985fe193da988c0d9ed64cf28
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-12-18 13:43:11 +0000

    Change type display for document features tab
    This is to use the more explicit (sub)type where applicable, e.g. "Press release" and not "Publication".

--- test/components/admin/features/featured_table_component_test.rb
+++ test/components/admin/features/featured_table_component_test.rb
@@ -13,15 +13,32 @@ class Admin::Features::FeaturedDocumentsTableComponentTest < ViewComponent::Test
 
   test "renders the correct row when the feature list item belongs to a document with a live edition" do
     document = build(:document)
-    edition = build_stubbed(:news_article, :published)
+    edition = build_stubbed(:news_article, :published, news_article_type: NewsArticleType::PressRelease)
     document.stubs(:live_edition).returns(edition)
     feature = build_stubbed(:feature, document:, feature_list: @feature_list)
     title = edition.title
 
     render_inline(Admin::Features::FeaturedDocumentsTableComponent.new(caption: "caption", features: [feature]))
-
     assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[0].text, title
-    assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[1].text, "News Article (document)"
+    assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[1].text, "Press release (document)"
+    assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[2].text, I18n.localize(edition.major_change_published_at.to_date)
+
+    actions_column = page.all(".govuk-table .govuk-table__row .govuk-table__cell")[3]
+    actions_column.assert_selector "a[href='#{admin_edition_path(edition)}']", text: "Edit #{title}"
+    actions_column.assert_selector "a[href='#{confirm_unfeature_admin_feature_list_feature_path(@feature_list, feature)}']", text: "Unfeature #{title}"
+  end
+
+  test "renders the correct row when the feature list item belongs to a document with a live standard edition" do
+    ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type"))
+    edition = build_stubbed(:published_standard_edition)
+    document = build(:document)
+    document.stubs(:live_edition).returns(edition)
+    feature = build_stubbed(:feature, document:, feature_list: @feature_list)
+    title = edition.title
+
+    render_inline(Admin::Features::FeaturedDocumentsTableComponent.new(caption: "caption", features: [feature]))
+    assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[0].text, title
+    assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[1].text, "Test type (document)"
     assert_equal page.all(".govuk-table .govuk-table__row .govuk-table__cell")[2].text, I18n.localize(edition.major_change_published_at.to_date)
 
     actions_column = page.all(".govuk-table .govuk-table__row .govuk-table__cell")[3]
