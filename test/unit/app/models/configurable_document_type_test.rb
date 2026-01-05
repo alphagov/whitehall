d074cecb5a60c6e767081fe26b00fe64e5828a71
eYinka <olayinka.oladele@digital.cabinet-office.gov.uk>
2026-01-05 13:18:22 +0000

    Fix test naming: use . for class methods and # for instance methods

--- test/unit/app/models/configurable_document_type_test.rb
+++ test/unit/app/models/configurable_document_type_test.rb
@@ -1,17 +1,17 @@
 require "test_helper"
 
 class ConfigurableDocumentTypeTest < ActiveSupport::TestCase
-  test "#find raises an error if the type is not specified" do
+  test ".find raises an error if the type is not specified" do
     error = assert_raises(ConfigurableDocumentType::NotFoundError) { ConfigurableDocumentType.find(nil) }
     assert_equal "No document type specified", error.message
   end
 
-  test "#find raises an error if the type is not found" do
+  test ".find raises an error if the type is not found" do
     error = assert_raises(ConfigurableDocumentType::NotFoundError) { ConfigurableDocumentType.find("non_existent_type") }
     assert_equal "No document type found for 'non_existent_type'", error.message
   end
 
-  test ".properties_for_edit_screen returns properties for a given edit screen" do
+  test "#properties_for_edit_screen returns properties for a given edit screen" do
     body_property = {
       "title" => "Body",
       "description" => "The main content of the page",
