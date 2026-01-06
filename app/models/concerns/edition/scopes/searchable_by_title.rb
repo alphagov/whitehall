1e1d894b4c44607b7f7fb448928cba9a5ad26999
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-01-06 07:09:59 +0000

    Remove unused `with_title_or_summary_containing` scope
    This scope name was introduced in 3228e519a5da262a4d2de3c94e57516c78e46a72
    
    It was written to support this file, which no longer exists:
    https://github.com/alphagov/whitehall/blob/3228e519a5da262a4d2de3c94e57516c78e46a72/lib/whitehall/document_filter/mysql.rb#L105

--- app/models/concerns/edition/scopes/searchable_by_title.rb
+++ app/models/concerns/edition/scopes/searchable_by_title.rb
@@ -2,12 +2,6 @@ module Edition::Scopes::SearchableByTitle
   extend ActiveSupport::Concern
 
   included do
-    scope :with_title_or_summary_containing,
-          lambda { |*keywords|
-            pattern = "(#{keywords.map { |k| Regexp.escape(k) }.join('|')})"
-            in_default_locale.where("edition_translations.title REGEXP :pattern OR edition_translations.summary REGEXP :pattern", pattern:)
-          }
-
     scope :with_title_containing,
           lambda { |keywords|
             escaped_like_expression = keywords.gsub(/([%_])/, "%" => '\\%', "_" => '\\_')
