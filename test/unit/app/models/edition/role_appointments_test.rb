3cfe0a7659477fbc5f9f3ab8a9f074acacc3f6bd
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2026-01-06 16:01:54 +0000

    Remove news article association from role appointments
    Originally introduced in 8a846ec4, when people and roles were rendered by whitehall.
    
    Feature tests at the time expected:
    ```
    When I visit the minister page for "Deputy Prime Minister"
    Then I should see both the news articles for the Deputy Prime Minister role
    ```
    
    It is no longer actively used. Only speeches are shown on role appointments pages now, publisher-side.
    People and roles and rendered by collections, with an "Announcements" feed being dynamically fetched, not sent by whitehall.
    
    If needed in the future, the association can be rewritten for standard editions, using the now available scope `with_news_article_document_type`.
    
    Also replaced news article factory usage in tests, and added some standard edition coverage.
    
    Co-authored-by: Alex Newton <alex.newton@digital.cabinet-office.gov.uk>

--- test/unit/app/models/edition/role_appointments_test.rb
+++ test/unit/app/models/edition/role_appointments_test.rb
@@ -6,7 +6,19 @@ class Edition::RoleAppointmentsTest < ActiveSupport::TestCase
       create(:role_appointment),
       create(:role_appointment),
     ]
-    published = create(:published_news_article, role_appointments: appointments)
+    published = create(:published_publication, role_appointments: appointments)
+
+    assert_equal appointments, published.create_draft(create(:user)).role_appointments
+  end
+
+  test "re-drafting a standard edition with role appointments copies the appointments" do
+    ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type"))
+    appointments = [
+      create(:role_appointment),
+      create(:role_appointment),
+    ]
+    published = create(:published_standard_edition, role_appointments: appointments)
+
     assert_equal appointments, published.create_draft(create(:user)).role_appointments
   end
 end
