456d6aeba483aad9faeb7a9beec3baf0cfe0d6a6
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2026-01-08 14:16:51 +0000

    Do not render topical event documents association input if configurable documents are disabled

--- test/unit/app/models/configurable_associations/topical_event_documents_test.rb
+++ test/unit/app/models/configurable_associations/topical_event_documents_test.rb
@@ -15,6 +15,32 @@ class TopicalEventsDocumentsTest < ActiveSupport::TestCase
 end
 
 class TopicalEventsRenderingTest < ActionView::TestCase
+  setup do
+    @test_strategy ||= Flipflop::FeatureSet.current.test!
+    @test_strategy.switch!(:configurable_document_types, true)
+  end
+
+  teardown do
+    @test_strategy.switch!(:configurable_document_types, false)
+  end
+
+  test "it does not render the topical events form control if configurable documents are disabled" do
+    @test_strategy.switch!(:configurable_document_types, false)
+
+    ConfigurableDocumentType.setup_test_types(build_configurable_document_type("topical_event"))
+    topical_events = create_list(:published_standard_edition, 3, configurable_document_type: "topical_event")
+    edition = build(:draft_standard_edition, {
+      topical_event_documents: [topical_events.first.document, topical_events.last.document],
+    })
+
+    topical_events_association = ConfigurableAssociations::TopicalEventDocuments.new(edition.topical_event_documents)
+    render topical_events_association
+
+    assert_dom "label", text: "Topical events (experimental)", count: 0
+
+    @test_strategy.switch!(:configurable_document_types, true)
+  end
+
   test "it renders topical events form control" do
     ConfigurableDocumentType.setup_test_types(build_configurable_document_type("topical_event"))
     topical_events = create_list(:published_standard_edition, 3, configurable_document_type: "topical_event")
