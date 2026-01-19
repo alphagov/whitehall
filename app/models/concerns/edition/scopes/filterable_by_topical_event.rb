ae74a3569f9773a9b70eb0dff640a765392086f2
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2026-01-19 14:57:05 +0000

    Add features to Standard Editions

--- app/models/concerns/edition/scopes/filterable_by_topical_event.rb
+++ app/models/concerns/edition/scopes/filterable_by_topical_event.rb
@@ -4,7 +4,7 @@ module Edition::Scopes::FilterableByTopicalEvent
   included do
     # NOTE: this scope becomes redundant once Admin::EditionFilterer is backed by an admin-only search_api index
     scope :with_topical_event, lambda { |topical_event|
-      joins("INNER JOIN topical_event_memberships ON topical_event_memberships.edition_id = editions.id")
+      joins("LEFT OUTER JOIN topical_event_memberships ON topical_event_memberships.edition_id = editions.id")
         .where("topical_event_memberships.topical_event_id" => topical_event.id)
     }
   end
