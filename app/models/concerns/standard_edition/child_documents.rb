ad14a1513b76651b9087e9924dea30dad7fd7de1
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-01-22 08:34:07 +0000

    Add 'EditionRelationships' table to capture parent/child relationship

--- app/models/concerns/standard_edition/child_documents.rb
+++ app/models/concerns/standard_edition/child_documents.rb
@@ -1,11 +1,34 @@
 module StandardEdition::ChildDocuments
   extend ActiveSupport::Concern
 
+  included do
+    has_many :child_relationships,
+             class_name: "EditionRelationship",
+             foreign_key: :parent_edition_id,
+             inverse_of: :parent_edition,
+             dependent: :destroy
+
+    has_many :children,
+             through: :child_relationships,
+             source: :child_edition
+
+    has_one :parent_relationship,
+            class_name: "EditionRelationship",
+            foreign_key: :child_edition_id,
+            inverse_of: :child_edition,
+            dependent: :restrict_with_error
+
+    has_one :parent_edition,
+            through: :parent_relationship,
+            source: :parent_edition
+  end
+
   def allows_child_documents?
     type_instance.child_documents
   end
 
   def child_documents
-    []
+    rels = child_relationships.order(:position)
+    Edition.where(id: rels.select(:child_edition_id))
   end
 end
