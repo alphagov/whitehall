b556a7974287a9edfd43634d28c8ae78ffdb24ba
Patrick Cartlidge <patrickcartlidge@gmail.com>
2026-01-23 12:25:12 +0000

    Add required attributes to document schema
    Make `title` and `description` required attributes in the
    cofigurable document type schema.

--- test/unit/app/models/configurable_document_types/validate_configurable_document_schemas_test.rb
+++ test/unit/app/models/configurable_document_types/validate_configurable_document_schemas_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ValidateConfigurableDocumentSchemasTest < ActiveSupport::TestCase
+  extend Minitest::Spec::DSL
+
   test "validates that all configurable document schemas are valid according to the schema" do
     document_type_files = Dir.glob(Rails.root.join("app/models/configurable_document_types/*.json"))
 
@@ -10,6 +12,17 @@ class ValidateConfigurableDocumentSchemasTest < ActiveSupport::TestCase
       errors = SchemaValidator.for(document)
 
       assert errors.empty?, "Schema validation errors for #{File.basename(file_path)}:\n#{errors.join("\n")}"
+    end
+  end
+
+  context "validates root level mandatory fields" do
+    %w[title description].each do |key|
+      it "will cause a validation error if not defined" do
+        document = JSON.parse(File.read("test/fixtures/test_schema.json"))
+        assert SchemaValidator.for(document).empty?
+        document.delete(key)
+        assert_equal SchemaValidator.for(document).first, "object at root is missing required properties: #{key}"
+      end
     end
   end
 end
