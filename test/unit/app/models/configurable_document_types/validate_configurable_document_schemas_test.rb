02657f0d801d7d7d48b13e70637a0d2beeadbafa
Patrick Cartlidge <patrickcartlidge@gmail.com>
2026-01-26 14:47:17 +0000

    Use `json-schema` instead of `json_schemer`

--- test/unit/app/models/configurable_document_types/validate_configurable_document_schemas_test.rb
+++ test/unit/app/models/configurable_document_types/validate_configurable_document_schemas_test.rb
@@ -1,19 +1,19 @@
 require "test_helper"
-require "json_schemer"
+require "json-schema"
 
 class ValidateConfigurableDocumentSchemasTest < ActiveSupport::TestCase
   test "validates that all configurable document schemas are valid" do
     schema_path = Rails.root.join("public/configurable-document-type.schema.json")
     schema = JSON.parse(File.read(schema_path))
-    validator = JSONSchemer.schema(schema)
 
     document_type_files = Dir.glob(Rails.root.join("app/models/configurable_document_types/*.json"))
 
     document_type_files.each do |file_path|
       document = JSON.parse(File.read(file_path))
-      errors = validator.validate(document).to_a
 
-      assert errors.empty?, "Schema validation errors for #{File.basename(file_path)}:\n#{errors.map { |e| e['error'] }.join("\n")}"
+      assert_nothing_raised do
+        JSON::Validator.validate(schema, document)
+      end
     end
   end
 end
