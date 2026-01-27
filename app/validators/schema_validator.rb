df2f5701205893dffcffb73fd70f5682b8459651
Patrick Cartlidge <patrickcartlidge@gmail.com>
2026-01-27 12:46:13 +0000

    Add `SchemaValidator`
    Create new validator for handling validation of configurable document
    schema. Allows us to extend validation beyond the limitations of JSON
    Schema.

---
+++ app/validators/schema_validator.rb
@@ -0,0 +1,28 @@
+require "json_schemer"
+
+class SchemaValidator
+  attr_reader :errors
+
+  def initialize(document)
+    @document = document
+
+    schema_path = Rails.root.join("public/configurable-document-type.schema.json")
+    schema = JSON.parse(File.read(schema_path))
+
+    @schema_validator = JSONSchemer.schema(schema)
+
+    @errors = []
+  end
+
+  def self.for(document)
+    new(document).call
+  end
+
+  def call
+    validate
+  end
+
+  def validate
+    @errors += @schema_validator.validate(@document).to_a.map { |e| e["error"] }
+  end
+end
--- test/unit/app/models/configurable_document_types/validate_configurable_document_schemas_test.rb
+++ test/unit/app/models/configurable_document_types/validate_configurable_document_schemas_test.rb
@@ -1,19 +1,15 @@
 require "test_helper"
-require "json_schemer"
 
 class ValidateConfigurableDocumentSchemasTest < ActiveSupport::TestCase
-  test "validates that all configurable document schemas are valid" do
-    schema_path = Rails.root.join("public/configurable-document-type.schema.json")
-    schema = JSON.parse(File.read(schema_path))
-    validator = JSONSchemer.schema(schema)
-
+  test "validates that all configurable document schemas are valid according to the schema" do
     document_type_files = Dir.glob(Rails.root.join("app/models/configurable_document_types/*.json"))
 
     document_type_files.each do |file_path|
       document = JSON.parse(File.read(file_path))
-      errors = validator.validate(document).to_a
 
-      assert errors.empty?, "Schema validation errors for #{File.basename(file_path)}:\n#{errors.map { |e| e['error'] }.join("\n")}"
+      errors = SchemaValidator.for(document)
+
+      assert errors.empty?, "Schema validation errors for #{File.basename(file_path)}:\n#{errors.join("\n")}"
     end
   end
 end
