100337de712dc9f15c17af78e46469a1fd6b2057
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-02-02 07:58:10 +0000

    Implement base path that inherits prefix from its parent

--- app/models/concerns/standard_edition/child_documents.rb
+++ app/models/concerns/standard_edition/child_documents.rb
@@ -27,6 +27,14 @@ module StandardEdition::ChildDocuments
     parent_edition.present?
   end
 
+  def child_document_base_path_override
+    if is_child_document?
+      parent_path = parent_edition.base_path
+      fixed_path = type_instance.settings["fixed_path"]
+      fixed_path.sub("$INHERITED", parent_path)
+    end
+  end
+
   def allows_child_documents?
     type_instance.child_documents
   end
