45b10281a6af40c925991d5c20984f92199eb876
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-02-02 09:11:24 +0000

    Revert "Spike auto-inheriting organisations"
    This reverts commit 3d5bd1fbde1b26e2c7116eafa626ff49abecd90d.
    
    It feels unclean architecturally. The true fix is likely that we'll
    want to send the Organisations at publish time, inheriting at the
    PRESENTER level rather than trying to model it all before then.

--- app/models/edition_relationship.rb
+++ app/models/edition_relationship.rb
@@ -5,17 +5,9 @@ class EditionRelationship < ApplicationRecord
 
   validate :not_self
 
-  after_create :inherit_from_parent!
-
 private
 
   def not_self
     errors.add(:child_edition, "cannot equal parent") if parent_edition_id == child_edition_id
-  end
-
-  def inherit_from_parent!
-    child_edition.with_lock do
-      child_edition.inherit_associations_from_parent(parent_edition)
-    end
   end
 end
