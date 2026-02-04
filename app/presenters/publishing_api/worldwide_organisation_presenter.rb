f92745b5e9194530f0081c8efac76cb5120ab39a
AshGDS <8880610+AshGDS@users.noreply.github.com>
2026-02-04 11:08:28 +0000

    Revert "Change worldwide_organisation rendering app to frontend"

--- app/presenters/publishing_api/worldwide_organisation_presenter.rb
+++ app/presenters/publishing_api/worldwide_organisation_presenter.rb
@@ -29,7 +29,7 @@ module PublishingApi
         document_type:,
         links: edition_links,
         public_updated_at: item.updated_at,
-        rendering_app: Whitehall::RenderingApp::FRONTEND,
+        rendering_app: Whitehall::RenderingApp::GOVERNMENT_FRONTEND,
         schema_name: "worldwide_organisation",
         auth_bypass_ids: [item.auth_bypass_id],
       )
