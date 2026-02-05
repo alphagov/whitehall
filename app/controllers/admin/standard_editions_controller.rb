46b03c73fdf7e93bab081b52b9495e5c8bf34627
eYinka <olayinka.oladele@digital.cabinet-office.gov.uk>
2026-02-05 12:00:41 +0000

    Exclude already featured documents from the documents search

--- app/controllers/admin/standard_editions_controller.rb
+++ app/controllers/admin/standard_editions_controller.rb
@@ -48,11 +48,15 @@ class Admin::StandardEditionsController < Admin::EditionsController
                           )
 
     @filter = Admin::EditionFilter.new(Edition, current_user, filter_params)
-    @tagged_editions = Edition.published.with_translations.joins("LEFT OUTER JOIN edition_links ON edition_links.edition_id = editions.id")
-                                      .where(edition_links: { document_id: @edition.document_id })
-                                      .order("editions.created_at DESC")
-                                      .page(params[:page])
-                                      .per(Admin::EditionFilter::GOVUK_DESIGN_SYSTEM_PER_PAGE)
+    featured_document_ids = @feature_list.features.pluck(:document_id)
+    @tagged_editions = Edition.published
+                              .with_translations
+                              .joins("INNER JOIN edition_links ON edition_links.edition_id = editions.id")
+                              .where(edition_links: { document_id: @edition.document_id })
+                              .where.not(document_id: featured_document_ids)
+                              .reorder("editions.created_at DESC")
+                              .page(params[:page])
+                              .per(Admin::EditionFilter::GOVUK_DESIGN_SYSTEM_PER_PAGE)
 
     render :features
   end
