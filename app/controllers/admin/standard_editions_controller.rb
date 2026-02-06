fee111c71d5718c71c657706366614cdfd72e6e8
eYinka <olayinka.oladele@digital.cabinet-office.gov.uk>
2026-02-06 15:02:30 +0000

    Add filtering to documents search on the standard edition features page

--- app/controllers/admin/standard_editions_controller.rb
+++ app/controllers/admin/standard_editions_controller.rb
@@ -39,7 +39,7 @@ class Admin::StandardEditionsController < Admin::EditionsController
     @feature_list = @edition.load_or_create_feature_list(params[:locale])
     @locale = Locale.new(params[:locale] || :en)
 
-    filter_params = params.slice(:page, :type, :author, :title)
+    filter_params = params.slice(:page, :type, :author, :organisation, :title)
                           .permit!
                           .to_h
                           .merge(
@@ -47,14 +47,16 @@ class Admin::StandardEditionsController < Admin::EditionsController
                             per_page: Admin::EditionFilter::GOVUK_DESIGN_SYSTEM_PER_PAGE,
                           )
 
-    @filter = Admin::EditionFilter.new(Edition, current_user, filter_params)
-    @tagged_editions = Edition.published
+    @tagged_editions_query = Edition.published
                               .with_translations(@feature_list.locale)
                               .joins("INNER JOIN edition_links ON edition_links.edition_id = editions.id")
                               .where(edition_links: { document_id: @edition.document_id })
                               .reorder("editions.created_at DESC")
                               .page(params[:page])
                               .per(Admin::EditionFilter::GOVUK_DESIGN_SYSTEM_PER_PAGE)
+
+    @filter = Admin::EditionFilter.new(@tagged_editions_query, current_user, filter_params)
+    @tagged_editions = @filter.editions(@feature_list.locale)
 
     render :features
   end
