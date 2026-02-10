efeb5b74d86bc3417d36f84d28dbd5946e235789
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-02-10 16:39:53 +0000

    Duplicate feature lists when saving new edition of the 'featurable'
    This is necessary as explored in
    https://app.mural.co/t/govukdelivery7534/m/govukdelivery7534/1770375442463/94590f435b718f119ea6de6cc0486bf8d19f4dc2

--- app/models/concerns/edition/featurable.rb
+++ app/models/concerns/edition/featurable.rb
@@ -1,12 +1,19 @@
 module Edition::Featurable
   extend ActiveSupport::Concern
 
+  class Trait < Edition::Traits::Trait
+    def process_associations_after_save(edition)
+      edition.feature_lists = @edition.feature_lists.map(&:deep_clone)
+    end
+  end
+
   included do
     has_many :feature_lists, as: :featurable, dependent: :destroy do
       def build_for_locale(locale)
         target.detect(-> { build(locale:) }) { |fl| locale.match?(fl.locale) }
       end
     end
+    add_trait Trait
   end
 
   def feature_list_for_locale(locale)
