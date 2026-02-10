8992b9e64e9d07aeeac30fc32eb6f32c4a913880
eYinka <olayinka.oladele@digital.cabinet-office.gov.uk>
2026-02-10 17:20:55 +0000

    Move standard editions featured documents limit to a constant
    This brings it into line with Organisations / World Location News
    usage of Features, and avoids a 'magic number' in the template.

--- app/models/standard_edition.rb
+++ app/models/standard_edition.rb
@@ -12,6 +12,8 @@ class StandardEdition < Edition
   include HasBlockContent
   include StandardEdition::DefaultLeadImage
 
+  FEATURED_DOCUMENTS_DISPLAY_LIMIT = 6
+
   validates :configurable_document_type, presence: true, inclusion: { in: -> { ConfigurableDocumentType.all_keys } }
 
   scope :with_news_article_document_type, -> { where(configurable_document_type: ConfigurableDocumentType.where_group("news_article").map(&:key)) }
