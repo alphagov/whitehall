164c3dbfd37f1795b97fbf0a76a65bde187ef9ad
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-02-23 09:32:30 +0000

    Split out Parent/Child responsibilities into different concerns
    Makes it much easier to read

--- app/models/concerns/standard_edition/parent_document.rb
+++ app/models/concerns/standard_edition/parent_document.rb
@@ -1,4 +1,4 @@
-module StandardEdition::ChildDocuments
+module StandardEdition::ParentDocument
   extend ActiveSupport::Concern
 
   included do
@@ -12,33 +12,11 @@ module StandardEdition::ChildDocuments
              through: :child_relationships,
              source: :child_edition
 
-    has_one :parent_relationship,
-            class_name: "EditionRelationship",
-            foreign_key: :child_edition_id,
-            inverse_of: :child_edition,
-            dependent: :restrict_with_error
-
-    has_one :parent_edition,
-            through: :parent_relationship,
-            source: :parent_edition
-
     after_save :copy_inherited_associations_to_children, if: :is_parent_document?
   end
 
   def is_parent_document?
     children.any?
-  end
-
-  def is_child_document?
-    parent_edition.present?
-  end
-
-  def child_document_base_path_override
-    if is_child_document?
-      parent_path = parent_edition.base_path
-      fixed_path = type_instance.settings["fixed_path"]
-      fixed_path.sub("$INHERITED", parent_path)
-    end
   end
 
   def allows_child_documents?
@@ -48,12 +26,6 @@ module StandardEdition::ChildDocuments
   def child_documents
     rels = child_relationships.order(:position)
     Edition.where(id: rels.select(:child_edition_id))
-  end
-
-  def requires_taxon?
-    return false if is_child_document?
-
-    super
   end
 
   def copy_inherited_associations_to_children
--- app/models/standard_edition.rb
+++ app/models/standard_edition.rb
@@ -11,7 +11,8 @@ class StandardEdition < Edition
   include Edition::WorldwideOrganisations
   include HasBlockContent
   include StandardEdition::DefaultLeadImage
-  include StandardEdition::ChildDocuments
+  include StandardEdition::ParentDocument
+  include StandardEdition::ChildDocument
 
   FEATURED_DOCUMENTS_DISPLAY_LIMIT = 6
 
