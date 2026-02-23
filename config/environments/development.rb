d262ce1929522a1e22708fa01452673f4ceb65d6
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-02-23 09:18:37 +0000

    Disable SQL query logs locally
    These are VERY noisy and make it difficult to spot debug / puts
    statements when manually testing things.
    
    We should disable this by default and only re-enable it when
    debugging database queries / bottlenecks.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Disable noisy SQL query logs
+  config.log_level = :info
+
   # Show full error reports.
   config.consider_all_requests_local = true
   config.action_controller.action_on_unpermitted_parameters = :raise
