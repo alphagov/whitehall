82d6f26451d098dd983ecd243ab6f94c0ced261d
Patrick Cartlidge <patrickcartlidge@gmail.com>
2026-02-25 15:46:43 +0000

    Support images of different aspect ratios
    Changes to cropping calculation and the cropping component
    for cases in which an `image_kind` has a versions with are
    different aspect ratios. Includes calculation of correct
    height, width, left and top based on the scale of the selected
    cropping area and presenting to the user the different views
    of a single image.

--- spec/javascripts/components/image-cropper-spec.js
+++ spec/javascripts/components/image-cropper-spec.js
@@ -3,20 +3,45 @@ describe('GOVUK.Modules.ImageCropper', () => {
 
   const width = 960
   const height = 640
+  const imageWidth = width
+  const imageHeight = 660
+  const versions = [
+    {
+      name: 'desktop',
+      width: 1024,
+      height: 1024,
+      from_version: null
+    },
+    {
+      name: 'tablet',
+      width: 1536,
+      height: 864,
+      from_version: null
+    },
+    {
+      name: 'mobile',
+      width: 640,
+      height: 360,
+      from_version: null
+    }
+  ]
 
-  beforeEach((done) => {
+  const initCropperHTML = () => {
     const canvas = document.createElement('canvas')
-    canvas.setAttribute('width', 960)
-    canvas.setAttribute('height', 660)
+    canvas.setAttribute('width', imageWidth)
+    canvas.setAttribute('height', imageHeight)
     const src = canvas.toDataURL('image/png')
 
     component = document.createElement('div')
     component.setAttribute('data-filename', 'icon.png')
     component.setAttribute('data-type', 'image/png')
-    component.setAttribute('data-width', width)
-    component.setAttribute('data-height', height)
+    component.setAttribute('data-width', imageWidth)
+    component.setAttribute('data-height', imageHeight)
     component.setAttribute('data-x', 0)
     component.setAttribute('data-y', 0)
+    component.setAttribute('data-target-width', width)
+    component.setAttribute('data-target-height', height)
+    component.setAttribute('data-versions', '[]')
     component.setAttribute('class', 'app-c-image-cropper')
     component.innerHTML = `
       <input class="js-cropped-image-input" name="[x]" hidden>
@@ -31,56 +56,111 @@ describe('GOVUK.Modules.ImageCropper', () => {
     form = document.createElement('form')
     form.appendChild(component)
     document.body.append(form)
+  }
 
-    // Initializing the module immediately after appending the form results in
-    // the cropper not properly reading the DOM, the following timeout prevents
-    // that from occuring.
-    window.setTimeout(() => {
-      module = new GOVUK.Modules.ImageCropper(component)
-      module.init()
+  describe('without versions', () => {
+    beforeEach((done) => {
+      initCropperHTML()
+      window.setTimeout(() => {
+        module = new GOVUK.Modules.ImageCropper(component)
+        module.init()
 
+        const image = document.querySelector('.app-c-image-cropper__image')
+        image.addEventListener('ready', () => done())
+      }, 1)
+    })
+
+    afterEach(() => form.remove())
+
+    it('should add cropping data to input when crop takes place', (done) => {
       const image = document.querySelector('.app-c-image-cropper__image')
-      image.addEventListener('ready', () => done())
-    }, 1)
-  })
-
-  afterEach(() => form.remove())
-
-  it('should add cropping data to input when crop takes place', (done) => {
-    const image = document.querySelector('.app-c-image-cropper__image')
-    image.addEventListener('crop', () => {
-      const inputX = document.querySelector(
-        '.js-cropped-image-input[name="[x]"]'
-      )
-      const inputY = document.querySelector(
-        '.js-cropped-image-input[name="[y]"]'
-      )
-      const inputWidth = document.querySelector(
-        '.js-cropped-image-input[name="[width]"]'
-      )
-      const inputHeight = document.querySelector(
-        '.js-cropped-image-input[name="[height]"]'
-      )
-      expect(inputX.value).toBe('0')
-      expect(inputY.value).toBe('0')
-      expect(inputWidth.value).toBe(`${width}`)
-      expect(inputHeight.value).toBe(`${height}`)
-      done()
+      image.addEventListener('crop', () => {
+        const inputX = document.querySelector(
+          '.js-cropped-image-input[name="[x]"]'
+        )
+        const inputY = document.querySelector(
+          '.js-cropped-image-input[name="[y]"]'
+        )
+        const inputWidth = document.querySelector(
+          '.js-cropped-image-input[name="[width]"]'
+        )
+        const inputHeight = document.querySelector(
+          '.js-cropped-image-input[name="[height]"]'
+        )
+        expect(inputX.value).toBe('0')
+        expect(inputY.value).toBe(`${(imageHeight - height) / 2}`)
+        expect(inputWidth.value).toBe(`${width}`)
+        expect(inputHeight.value).toBe(`${height}`)
+        done()
+      })
+      image.dispatchEvent(new CustomEvent('crop'))
     })
-    image.dispatchEvent(new CustomEvent('crop'))
-  })
 
-  it('should update the aria label as the selection is controlled using the keyboard', (done) => {
-    expect(document.querySelector('.app-c-image-cropper').ariaLabel).toBe(
-      'Image to be cropped. All of the image is selected.'
-    )
-    const image = document.querySelector('.app-c-image-cropper__image')
-    image.addEventListener('crop', () => {
+    it('should update the aria label as the selection is controlled using the keyboard', (done) => {
       expect(document.querySelector('.app-c-image-cropper').ariaLabel).toBe(
-        'Image to be cropped. 90% of the image, centered on the top left is selected.'
+        'Image to be cropped. All of the image is selected.'
+      )
+      const image = document.querySelector('.app-c-image-cropper__image')
+      image.addEventListener('crop', () => {
+        expect(document.querySelector('.app-c-image-cropper').ariaLabel).toBe(
+          'Image to be cropped. 90% of the image, centered on the top left is selected.'
+        )
+        done()
+      })
+      component.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 189 }))
+    })
+  })
+
+  describe('with versions', () => {
+    beforeEach((done) => {
+      initCropperHTML()
+      component.innerHTML += `
+        <div class="app-c-image-cropper__image-information" hidden>
+          <h2 class="govuk-heading-m">Crop boxes</h2>
+          <ul class="govuk-list"></ul>
+        </div>
+      `
+      component.setAttribute('data-versions', JSON.stringify(versions))
+
+      window.setTimeout(() => {
+        module = new GOVUK.Modules.ImageCropper(component)
+        module.init()
+
+        const image = document.querySelector('.app-c-image-cropper__image')
+        image.addEventListener('ready', () => done())
+      }, 1)
+    })
+
+    afterEach(() => form.remove())
+
+    it('should display multiple cropboxes', (done) => {
+      expect(document.querySelectorAll('.cropper-crop-box').length).toBe(
+        versions.length + 1
       )
       done()
     })
-    component.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 189 }))
+
+    it('should display legend for each cropbox', (done) => {
+      const cropKeys = document.querySelectorAll(
+        '.app-c-image-cropper__crop-key'
+      )
+      expect(cropKeys.length).toBe(versions.length)
+      cropKeys.forEach((cropKey, index) => {
+        const keyColour = cropKey.querySelector(
+          '.app-c-image-cropper__crop-key-colour'
+        )
+        const cropBox = document.querySelector(
+          `#cropbox-${cropKey.dataset.cropBox}`
+        )
+
+        expect(cropKey.innerText.toLowerCase()).toBe(
+          versions[index].name.toLowerCase()
+        )
+        expect(cropBox.style.outlineColor).toBe(keyColour.style.borderColor)
+        expect(cropBox.style.outlineStyle).toBe(keyColour.style.borderStyle)
+      })
+
+      done()
+    })
   })
 })
