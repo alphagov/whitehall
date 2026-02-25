df47cf409b7848ac7ee43b550e1e1baaf6abdcd8
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-02-25 12:19:08 +0000

    fixup db drop lint

--- db/migrate/20260225080000_drop_edition_relationships.rb
+++ db/migrate/20260225080000_drop_edition_relationships.rb
@@ -1,5 +1,9 @@
 class DropEditionRelationships < ActiveRecord::Migration[8.0]
-  def change
+  def up
     drop_table :edition_relationships, if_exists: true
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
