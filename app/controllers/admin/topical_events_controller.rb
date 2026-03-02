bdc864f6da85856019b5fb01fdc9eb576fd53ac2
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-03-02 15:32:40 +0000

    Remove code that manually purges deleted social media accounts
    We have this already with our implementation of the add another
    component for config-driven topical events:
    
    ```
    PublishingApi::StandardEditionPresenter.new(Edition.find(2)).content
    
    ...
     :details=>{:body=>"<div class=\"govspeak\"><p>.,</p>\n</div>", :social_media_links=>[], :emphasised_organisations=>["af07d5a5-df63-4ddc-9383-6a666845ebe9"], :ordered_featured_documents=>[]},
    ...
    ```
    
    ^ this was after deleting a social media link in the UI.

--- app/controllers/admin/topical_events_controller.rb
+++ app/controllers/admin/topical_events_controller.rb
@@ -3,7 +3,6 @@ class Admin::TopicalEventsController < Admin::BaseController
   before_action :load_object, only: %i[show edit confirm_destroy destroy]
   before_action :build_object, only: [:new]
   before_action :build_associated_objects, only: %i[new edit]
-  before_action :destroy_blank_social_media_accounts, only: %i[create update]
 
   def show; end
 
@@ -60,16 +59,6 @@ class Admin::TopicalEventsController < Admin::BaseController
 
   def build_associated_objects
     @topical_event.build_logo if @topical_event.logo.blank?
-  end
-
-  def destroy_blank_social_media_accounts
-    if params[:topical_event][:social_media_accounts_attributes]
-      params[:topical_event][:social_media_accounts_attributes].each_pair do |_key, account|
-        if account[:social_media_service_id].blank? && account[:url].blank?
-          account[:_destroy] = "1"
-        end
-      end
-    end
   end
 
   def object_params
