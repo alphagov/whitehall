01f9179b0f15b6b636e41fea635de9cb06997d4a
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2026-03-03 17:28:57 +0000

    Include filename in image error messages
    Prepend the uploaded filename to image validation errors so users can
    identify which file failed when uploading multiple images. CarrierWave
    integrity errors (too small, unreadable, missing kind) now include the
    filename directly, and a new after_validation callback on ImageData
    prepends the filename to any remaining file errors that don't already
    include it.

--- test/functional/admin/edition_images_controller_test.rb
+++ test/functional/admin/edition_images_controller_test.rb
@@ -414,7 +414,7 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
 
     topical_event_header_kind = Whitehall.image_kinds.fetch("topical_event_header")
     assert_template "admin/edition_images/new"
-    assert_select ".govuk-error-summary li", "Image data file is too small. Select an image that is at least #{topical_event_header_kind.valid_width} pixels wide and at least #{topical_event_header_kind.valid_height} pixels tall"
+    assert_select ".govuk-error-summary li", "Image data file \"50x33_gif.gif\" is too small. Select an image that is at least #{topical_event_header_kind.valid_width} pixels wide and at least #{topical_event_header_kind.valid_height} pixels tall"
   end
 
   view_test "POST :create shows a validation error if one 'single' usage image has a duplicated filename, using the 'new' template" do
@@ -445,7 +445,7 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
     post :create, params: { edition_id: edition.id, usage: "logo", image_kind: "topical_event_logo", images: [{ image_data_attributes: { file: } }] }
 
     assert_template "admin/edition_images/new"
-    assert_select ".govuk-error-summary li", "Image data file name is not unique. All your file names must be different. Do not use special characters to create another version of the same file name."
+    assert_select ".govuk-error-summary li", "Image data file \"test-svg.svg\" name is not unique. All your file names must be different. Do not use special characters to create another version of the same file name."
   end
 
   view_test "POST :create shows a validation error if one 'multiple' usage embeddable image is too small, using the 'index' template" do
@@ -457,7 +457,7 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
 
     default_kind = Whitehall.image_kinds.fetch("default")
     assert_template "admin/edition_images/index"
-    assert_select ".govuk-error-summary li", "Image data file is too small. Select an image that is at least #{default_kind.valid_width} pixels wide and at least #{default_kind.valid_height} pixels tall"
+    assert_select ".govuk-error-summary li", "Image data file \"50x33_gif.gif\" is too small. Select an image that is at least #{default_kind.valid_width} pixels wide and at least #{default_kind.valid_height} pixels tall"
   end
 
   view_test "POST :create shows a validation error if one 'multiple' usage embeddable image has a duplicated filename, using the 'index' template" do
@@ -469,7 +469,7 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
     post :create, params: { edition_id: edition.id, usage: "govspeak_embed", image_kind: "default", images: [{ image_data_attributes: { file: } }] }
 
     assert_template "admin/edition_images/index"
-    assert_select ".govuk-error-summary li", "Image data file name is not unique. All your file names must be different. Do not use special characters to create another version of the same file name."
+    assert_select ".govuk-error-summary li", "Image data file \"960x640_gif.gif\" name is not unique. All your file names must be different. Do not use special characters to create another version of the same file name."
   end
 
   view_test "POST :create shows a validation error if one 'multiple' usage non-embeddable image is too small, using the 'index' template" do
@@ -495,7 +495,7 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
 
     hero_desktop_kind = Whitehall.image_kinds.fetch("hero_desktop")
     assert_template "admin/edition_images/index"
-    assert_select ".govuk-error-summary li", "Image data file is too small. Select an image that is at least #{hero_desktop_kind.valid_width} pixels wide and at least #{hero_desktop_kind.valid_height} pixels tall"
+    assert_select ".govuk-error-summary li", "Image data file \"hero_image_mobile_2x.png\" is too small. Select an image that is at least #{hero_desktop_kind.valid_width} pixels wide and at least #{hero_desktop_kind.valid_height} pixels tall"
   end
 
   view_test "POST :create shows a validation error if one 'multiple' usage non-embeddable image has a duplicated filename, using the 'index' template" do
@@ -521,7 +521,7 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
     post :create, params: { edition_id: edition.id, usage: "hero", image_kind: "hero_mobile", images: [{ image_data_attributes: { file: } }] }
 
     assert_template "admin/edition_images/index"
-    assert_select ".govuk-error-summary li", "Image data file name is not unique. All your file names must be different. Do not use special characters to create another version of the same file name."
+    assert_select ".govuk-error-summary li", "Image data file \"hero_image_mobile_2x.png\" name is not unique. All your file names must be different. Do not use special characters to create another version of the same file name."
   end
 
   view_test "POST :create re-renders the correct usage 'new' template on validation errors" do
@@ -545,9 +545,10 @@ class Admin::EditionImagesControllerTest < ActionController::TestCase
 
     post :create, params: { edition_id: edition.id, usage: "hero", image_kind: "hero_desktop", images: [{ image_data_attributes: { file: } }] }
 
+    hero_desktop_kind = Whitehall.image_kinds.fetch("hero_desktop")
     assert_template "admin/edition_images/new"
     assert_select "label", "Upload hero image"
-    assert_select ".govuk-error-summary li", /Image data file is too small/
+    assert_select ".govuk-error-summary li", "Image data file \"960x640_jpeg.jpg\" is too small. Select an image that is at least #{hero_desktop_kind.valid_width} pixels wide and at least #{hero_desktop_kind.valid_height} pixels tall"
   end
 
   test "POST :create returns 422 for non-permitted usage" do
