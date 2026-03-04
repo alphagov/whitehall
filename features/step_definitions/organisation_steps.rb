033b9a661ca841bdbd00821e4dd42c81b400cb20
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-03-04 16:50:50 +0000

    Make Organisations' topical event tagging test use 'config-driven'
    Organisations will soon be expected to be tagged to config-driven
    topical events rather than legacy topical events. This commit
    fixes up the test to use the new style of topical events, to
    minimise the amount of tests that will need reworking when we
    switch off the legacy ones.
    
    We've also kept a legacy test but marked it for deletion post
    migration.

--- features/step_definitions/organisation_steps.rb
+++ features/step_definitions/organisation_steps.rb
@@ -26,15 +26,12 @@ Given(/^I set the alternative format contact email of "([^"]*)" to "([^"]*)"$/) 
 end
 
 When(/^I add a new organisation called "([^"]*)"$/) do |organisation_name|
-  create(:topical_event, name: "Jazz Bizniz")
-
   visit new_admin_organisation_path
 
   fill_in "Name", with: organisation_name
   fill_in "Acronym", with: organisation_name.split(" ").collect { |word| word.chars.first }.join
   fill_in "Logo formatted name", with: organisation_name
   select "Ministerial department", from: "Organisation type"
-  select "Jazz Bizniz", from: "organisation_topical_event_ids_0"
   selector = ".app-view-organisation__featured_links .js-add-another__fieldset"
   within selector, match: :first do
     expect(page).to_not have_content("English:")
@@ -237,7 +234,11 @@ Then(/^the roles should be in the following order:$/) do |roles|
 end
 
 Given(/^an active topical event called "([^"]*)" exists$/) do |name|
-  @topical_event = create(:topical_event, :active, name:)
+  create(:topical_event, :active, name:)
+end
+
+Given(/^a config-driven document called "([^"]*)" exists$/) do |name|
+  create(:published_standard_edition, configurable_document_type: "test_type", title: name, lead_organisations: [@organisation])
 end
 
 And(/^I visit the the organisation feature page for "([^"]*)"$/) do |name|
