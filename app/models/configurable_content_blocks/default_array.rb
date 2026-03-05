bb332d9eff1095867984e73b9e49c5f3c4c86acb
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2026-03-05 17:01:59 +0000

    Fix array field error labels
    Previously, error labels for array fields were not linking to the correct element ID because the array index was not being included in the path.
    
    This was caused by a naive implementation of the document configuration traversal which walked the graph of the raw configuration JSON rather than the hydrated document object.
    
    This commit provides a new implementation which walks the graph of block objects to build the error paths object. This is an improvement, because it means we only have one data structure for navigating the configuration graph.
    
    Ideally we would also use the enumerator behaviour from the default object and array blocks to perform rendering using a visitor, but I haven't quite worked out how to do that yet.

--- app/models/configurable_content_blocks/default_array.rb
+++ app/models/configurable_content_blocks/default_array.rb
@@ -1,5 +1,7 @@
 module ConfigurableContentBlocks
   class DefaultArray < BaseBlock
+    include Enumerable
+
     def field_blocks(index)
       @config["fields"].values.map do |field_config|
         ConfigurableDocumentType::CONTENT_BLOCKS[field_config["block"]].new(@edition, field_config, @path.push([index, *field_config["attribute_path"]]))
@@ -8,6 +10,19 @@ module ConfigurableContentBlocks
 
     def items
       value || []
+    end
+
+    def each(&block)
+      block.call(self)
+      items.map.with_index do |_item, index|
+        field_blocks(index).each do |field_block|
+          if field_block.respond_to? :each
+            field_block.each(&block)
+          else
+            block.call(field_block)
+          end
+        end
+      end
     end
 
   private
--- app/models/configurable_content_blocks/default_object.rb
+++ app/models/configurable_content_blocks/default_object.rb
@@ -1,5 +1,11 @@
 module ConfigurableContentBlocks
   class DefaultObject < BaseBlock
+    include Enumerable
+
+    def self.root_block_for(edition, form = nil)
+      new(edition, edition.type_instance.form(form).merge("root" => true), ConfigurableContentBlocks::Path.new)
+    end
+
     def root?
       @config["root"]
     end
@@ -7,6 +13,17 @@ module ConfigurableContentBlocks
     def field_blocks
       @config["fields"].values.map do |field_config|
         ConfigurableDocumentType::CONTENT_BLOCKS[field_config["block"]].new(@edition, field_config, @path.push(field_config["attribute_path"]))
+      end
+    end
+
+    def each(&block)
+      block.call(self)
+      field_blocks.each do |field_block|
+        if field_block.respond_to? :each
+          field_block.each(&block)
+        else
+          block.call(field_block)
+        end
       end
     end
 
