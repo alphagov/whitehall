f86173c4d6962145da6f510b7c9d8025ee9c087d
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-03-06 16:57:33 +0000

    Change Searchable tests to link to Statistics Announcements
    ...rather than to Topical Events, which will be deleted shortly.

--- test/unit/app/models/searchable_test.rb
+++ test/unit/app/models/searchable_test.rb
@@ -5,12 +5,12 @@ class SearchableTest < ActiveSupport::TestCase
   # as all the searchable definition is in one place (and it doesn't
   # lend itself to redefinition)
   class SearchableTestTopic < ApplicationRecord
-    self.table_name = "topical_events"
+    self.table_name = "statistics_announcements"
 
     include Searchable
-    searchable link: :name, only: :publicly_visible, index_after: [:save], unindex_after: [:destroy]
+    searchable link: :title, only: :publicly_visible, index_after: [:save], unindex_after: [:destroy]
 
-    scope :publicly_visible, -> { where(state: %w[published withdrawn]) }
+    scope :publicly_visible, -> { where(publishing_state: %w[published withdrawn]) }
   end
 
   def setup
@@ -18,31 +18,31 @@ class SearchableTest < ActiveSupport::TestCase
   end
 
   test "will not request indexing on save if it is not in searchable_instances" do
-    searchable_test_topic = SearchableTestTopic.new(name: "woo", state: "draft")
+    searchable_test_topic = SearchableTestTopic.new(title: "woo", publishing_state: "draft", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).never
     searchable_test_topic.save!
   end
 
   test "will request indexing on save if it is in searchable_instances" do
-    searchable_test_topic = SearchableTestTopic.create!(name: "woo", state: "published")
+    searchable_test_topic = SearchableTestTopic.create!(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).with(searchable_test_topic)
     searchable_test_topic.save!
   end
 
   test "will request indexing on save if it is in searchable_instances and withrawn" do
-    searchable_test_topic = SearchableTestTopic.create!(name: "woo", state: "withdrawn")
+    searchable_test_topic = SearchableTestTopic.create!(title: "woo", publishing_state: "withdrawn", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).with(searchable_test_topic)
     searchable_test_topic.save!
   end
 
   test "will request deletion on destruction even if it is not in searchable_instances" do
-    searchable_test_topic = SearchableTestTopic.create!(name: "woo", state: "draft")
+    searchable_test_topic = SearchableTestTopic.create!(title: "woo", publishing_state: "draft", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:delete).with(searchable_test_topic)
     searchable_test_topic.destroy!
   end
 
   test "will request deletion on destruction if it is contained in searchable_instances" do
-    searchable_test_topic = SearchableTestTopic.create!(name: "woo", state: "published")
+    searchable_test_topic = SearchableTestTopic.create!(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:delete).with(searchable_test_topic)
     searchable_test_topic.destroy!
   end
@@ -50,7 +50,7 @@ class SearchableTest < ActiveSupport::TestCase
   test "will only request indexing of things that are included in the SearchApiPresenters.searchable_classes property" do
     non_existent_class = Class.new
     SearchApiPresenters.stubs(:searchable_classes).returns([non_existent_class])
-    searchable_test_topic = SearchableTestTopic.new(name: "woo", state: "published")
+    searchable_test_topic = SearchableTestTopic.new(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).never
     searchable_test_topic.save!
   end
@@ -58,30 +58,30 @@ class SearchableTest < ActiveSupport::TestCase
   test "#reindex_all will not request indexing for an instance whose class is not in SearchApiPresenters.searchable_classes" do
     non_existent_class = Class.new
     SearchApiPresenters.stubs(:searchable_classes).returns([non_existent_class])
-    SearchableTestTopic.create!(name: "woo", state: "published")
+    SearchableTestTopic.create!(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).never
     SearchableTestTopic.reindex_all
   end
 
   test "#reindex_all will respect the scopes it is prefixed with" do
-    searchable_test_topic1 = SearchableTestTopic.create!(name: "woo", state: "published")
-    searchable_test_topic2 = SearchableTestTopic.create!(name: "moo", state: "published")
+    searchable_test_topic1 = SearchableTestTopic.create!(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
+    searchable_test_topic2 = SearchableTestTopic.create!(title: "moo", publishing_state: "published", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).with(searchable_test_topic1).never
     Whitehall::SearchIndex.expects(:add).with(searchable_test_topic2)
-    SearchableTestTopic.where(name: "moo").reindex_all
+    SearchableTestTopic.where(title: "moo").reindex_all
   end
 
   test "#reindex_all will request indexing for each searchable instance" do
-    searchable_test_topic1 = SearchableTestTopic.create!(name: "woo", state: "draft")
-    searchable_test_topic2 = SearchableTestTopic.create!(name: "woo", state: "published")
+    searchable_test_topic1 = SearchableTestTopic.create!(title: "woo", publishing_state: "draft", content_id: SecureRandom.uuid)
+    searchable_test_topic2 = SearchableTestTopic.create!(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
     Whitehall::SearchIndex.expects(:add).with(searchable_test_topic1).never
     Whitehall::SearchIndex.expects(:add).with(searchable_test_topic2)
     SearchableTestTopic.reindex_all
   end
 
   test "#searchable_instances uses the searchable_options[:only] proc to find instances that can be searched" do
-    draft_topic = SearchableTestTopic.create!(name: "woo", state: "draft")
-    published_topic = SearchableTestTopic.create!(name: "woo", state: "published")
+    draft_topic = SearchableTestTopic.create!(title: "woo", publishing_state: "draft", content_id: SecureRandom.uuid)
+    published_topic = SearchableTestTopic.create!(title: "woo", publishing_state: "published", content_id: SecureRandom.uuid)
 
     searchable_topics = SearchableTestTopic.searchable_instances
     assert searchable_topics.include?(published_topic)
