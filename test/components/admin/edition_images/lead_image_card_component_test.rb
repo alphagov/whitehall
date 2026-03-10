903b44696e75778edfe01274459e058528f478b4
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-03-10 13:26:35 +0000

    Add 'Use default image' option for when fallback image deleted

--- test/components/admin/edition_images/lead_image_card_component_test.rb
+++ test/components/admin/edition_images/lead_image_card_component_test.rb
@@ -29,6 +29,17 @@ class Admin::EditionImages::LeadImageCardComponentTest < ViewComponent::TestCase
     assert_selector ".govuk-link[href='#{confirm_toggle_default_lead_image_behaviour_admin_edition_images_path(edition)}']", text: "Delete"
   end
 
+  test "summary card actions contains 'Add image' and 'Use default image' options if no custom or default image is present" do
+    ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type", lead_image_usage_test_type))
+    edition = build_stubbed(:standard_edition, image_display_option: "no_image")
+    lead_usage = edition.permitted_image_usages.find { |usage| usage.key == "lead" }
+
+    render_inline(Admin::EditionImages::LeadImageCardComponent.new(edition:, image: nil, image_usage: lead_usage))
+
+    assert_selector ".govuk-link[href='#{new_admin_edition_image_path(edition_id: edition.id, usage: lead_usage.key)}']", text: "Add image"
+    assert_selector ".govuk-link[href='#{confirm_toggle_default_lead_image_behaviour_admin_edition_images_path(edition)}']", text: "Use default image"
+  end
+
   test "there are no summary card actions if the edition isn't editable" do
     ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type", lead_image_usage_test_type))
     edition = build_stubbed(:published_standard_edition)
