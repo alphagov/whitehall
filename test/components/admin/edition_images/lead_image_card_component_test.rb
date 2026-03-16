ede1f9382d4010513ba65155ac727e960f778741
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-03-16 08:22:57 +0000

    Change label from Add to Replace
    When editions are created and they get the default lead image,
    the link to 'Add' a lead image is a little confusing, as there is
    already one - the default.
    
    The designs in the ticket suggest we use the word 'Replace'
    instead.

--- test/components/admin/edition_images/lead_image_card_component_test.rb
+++ test/components/admin/edition_images/lead_image_card_component_test.rb
@@ -18,14 +18,14 @@ class Admin::EditionImages::LeadImageCardComponentTest < ViewComponent::TestCase
     assert_selector ".govuk-link[href='#{confirm_destroy_admin_edition_image_path(edition, image)}']", text: "Delete"
   end
 
-  test "summary card actions contains Add option if image not present" do
+  test "summary card actions contains Replace option if image not present" do
     ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type", lead_image_usage_test_type))
     edition = build_stubbed(:draft_standard_edition)
     lead_usage = edition.permitted_image_usages.find { |usage| usage.key == "lead" }
 
     render_inline(Admin::EditionImages::LeadImageCardComponent.new(edition:, image: nil, image_usage: lead_usage))
 
-    assert_selector ".govuk-link[href='#{new_admin_edition_image_path(edition_id: edition.id, usage: lead_usage.key)}']", text: "Add"
+    assert_selector ".govuk-link[href='#{new_admin_edition_image_path(edition_id: edition.id, usage: lead_usage.key)}']", text: "Replace"
   end
 
   test "there are no summary card actions if the edition isn't editable" do
