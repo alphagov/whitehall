6fb77f7875fe65ee3cd4f8c7996f9472a318ab97
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-03-19 14:06:08 +0000

    Remove GOVERNMENT_FRONTEND from rendering app options.

--- lib/whitehall/rendering_app.rb
+++ lib/whitehall/rendering_app.rb
@@ -1,6 +1,5 @@
 module Whitehall
   module RenderingApp
-    GOVERNMENT_FRONTEND = "government-frontend".freeze
     COLLECTIONS_FRONTEND = "collections".freeze
     FRONTEND = "frontend".freeze
   end
