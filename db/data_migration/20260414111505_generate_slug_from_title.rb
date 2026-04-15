0b5e33775674e4fa4b6d3f0e1ef2a4c640fe0fd4
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2026-04-15 15:08:19 +0100

    Update slug generation code to skip callbacks
    We got a stale object error in production. We're not entirely sure what caused it, but this should eliminate some of the likely responsible behaviours.
    
    The original data migration has not run in production, so it's safe to update the file.

--- db/data_migration/20260414111505_generate_slug_from_title.rb
+++ db/data_migration/20260414111505_generate_slug_from_title.rb
@@ -1,9 +1,12 @@
 total_processed = 0
 
-Edition.skip_callback(:update, :after, :republish_topical_event_to_publishing_api)
 Edition.in_pre_publication_state.find_each(batch_size: 1000) do |edition|
   edition.set_slug
-  total_processed += 1 if edition.save(validate: false)
+  total_processed += 1 if edition.update_columns(
+    slug: edition.slug,
+    slug_from_title: edition.slug_from_title,
+    touch: false,
+  )
 
   puts "Processed #{total_processed} editions" if (total_processed % 5000).zero?
 end
