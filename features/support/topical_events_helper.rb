05f67b629ef77779f6d01e65e4bfbbc600f505fe
leimdorfer <september21@gmail.com>
2026-04-23 18:21:56 +0100

    correcting lint suggestion and removing a redundant test for creating legacy TEs

--- features/support/topical_events_helper.rb
+++ features/support/topical_events_helper.rb
@@ -4,25 +4,6 @@ ParameterType(
   transformer: ->(section) { section },
 )
 module TopicalEventsHelper
-  def create_topical_event_and_stub_in_content_store(options = {})
-    visit admin_root_path
-    click_link "More"
-    click_link "Topical events"
-    click_link "Create topical event"
-    fill_in "Name", with: options[:name] || "topic-name"
-    fill_in "Description", with: options[:description] || "topic-description"
-    fill_in "Summary", with: options[:description] || "topic-summary"
-    within "#topical_event_start_date" do
-      fill_in_date_fields(options[:start_date] || 1.day.ago.to_s)
-    end
-    within "#topical_event_end_date" do
-      fill_in_date_fields(options[:end_date] || 1.month.from_now.to_s)
-    end
-
-    click_button "Save"
-
-    stub_topical_event_in_content_store(options[:name])
-  end
 
   def stub_topical_event_in_content_store(name)
     content_item = {
