d96ac74d72aec1d186de39cdd9d62f35ebc3900d
eYinka <olayinka.oladele@digital.cabinet-office.gov.uk>
2026-04-24 12:04:46 +0100

    Update search scope for slug
    Since the introduction of `slug_override`, editions using an overridden
    slug were not found when searching by that slug value. I've extended the slug
    search condition to also check `slug_override`.

--- test/unit/app/models/edition_test.rb
+++ test/unit/app/models/edition_test.rb
@@ -512,6 +512,12 @@ class EditionTest < ActiveSupport::TestCase
     assert_equal [edition_with_first_keyword], Edition.with_title_containing("klingons-rule")
   end
 
+  test "should find editions with slug_override containing keyword" do
+    edition_with_slug_override = create(:edition, title: "klingons rule", slug_override: "another-slug")
+    _edition_without_match = create(:edition, title: "this document is about muppets")
+    assert_equal [edition_with_slug_override], Edition.with_title_containing("another-slug")
+  end
+
   test "should find editions with title containing regular expression characters" do
     edition_with_nasty_characters = create(:edition, title: "title with [stuff in brackets]")
     assert_equal [edition_with_nasty_characters], Edition.with_title_containing("[stuff")
