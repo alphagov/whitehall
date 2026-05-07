d5750a3e4f595148518d58ea9f144ac63a1e9026
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-05-07 16:08:13 +0100

    Add child document creation journey end-to-end
    Starts a feature file describing creating a child document from the
    parent.
    
    Over the next few commits, we'll introduce the agreed business
    rules so that we can keep the documents in a reasonable synchronised
    state (avoiding orphaned children, or children that are live when
    the parent is unpublished, and so on).
    
    NB: I used the same design for surfacing Child Documents on the
    document summary page, as exists for listing corporate information
    pages on Organisations, since it seems useful to surface the state
    of the child documents here too. It uses the same 'fake edition
    filter' approach as that.

---
+++ features/standard-edition-child-pages.feature
@@ -0,0 +1,18 @@
+Feature: Standard Editions - Child Pages
+
+  Scenario: Creating a new draft configurable document
+    Given I am a writer
+    And the configurable document types feature flag is enabled
+    And the test configurable document type group is defined
+    When I draft a new parent configurable document
+    Then I should see a "Child documents" section on the document summary page
+    And when I click the link "Add child document"
+    Then I am taken to the new child document page
+    And I see only the relevant child document types
+    And when I choose a child document type
+    Then I am on the document creation screen
+    And I can see a "You are creating a child document" callout
+    And when I fill in and create the child document
+    Then I am taken to the summary page of my child document
+    And I can see a "This is a child document" callout
+    And it links back to the parent document
---
+++ features/step_definitions/standard_edition_child_pages_steps.rb
@@ -0,0 +1,65 @@
+When("I draft a new parent configurable document") do
+  @standard_edition = create(
+    :draft_standard_edition,
+    configurable_document_type: "test_type_parent",
+    title: "Parent document",
+    summary: "This is the parent document",
+    primary_locale: "en",
+    block_content: {
+      "body" => "This is the parent body",
+    },
+  )
+
+  visit admin_standard_edition_path(@standard_edition)
+end
+
+Then(/^I should see a "(.+)" section on the document summary page$/) do |section_title|
+  expect(page).to have_css("h2.gem-c-heading__text", text: section_title)
+end
+
+And(/^when I click the link "(.+)"$/) do |link_text|
+  click_link(link_text)
+end
+
+Then("I am taken to the new child document page") do
+  assert_current_path(/^\/government\/admin\/child_documents\/choose_type\?parent_edition_id=#{@standard_edition.id}$/)
+end
+
+Then("I see only the relevant child document types") do
+  within(".govuk-radios") do
+    expect(page).to have_css("label", text: "Test configurable document type child")
+    expect(page).to have_css("input[type='radio']", count: 1)
+  end
+end
+
+Then("when I choose a child document type") do
+  choose("Test configurable document type child")
+  click_button("Next")
+end
+
+Then("I am on the document creation screen") do
+  assert_current_path(/^\/government\/admin\/standard-editions\/new\?parent_edition_id=#{@standard_edition.id}&configurable_document_type=topical_event_about_page$/)
+end
+
+Then(/^I can see a "(.+)" callout$/) do |callout|
+  assert page.has_css?(".govuk-inset-text", text: callout)
+end
+
+Then("when I fill in and create the child document") do
+  fill_in "Title", with: "Child document"
+  fill_in "Summary", with: "This is the child document"
+  fill_in "Body", with: "This is the child body"
+
+  click_button "Save and go to document summary"
+end
+
+Then("I am taken to the summary page of my child document") do
+  @child_edition = StandardEdition.last
+  assert_current_path(admin_standard_edition_path(@child_edition))
+end
+
+Then("it links back to the parent document") do
+  within ".govuk-inset-text" do
+    expect(page).to have_link(@standard_edition.title, href: "/government/admin/editions/#{@standard_edition.id}")
+  end
+end
