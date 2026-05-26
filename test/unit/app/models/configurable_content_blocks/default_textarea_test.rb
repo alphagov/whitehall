3e789047599963af0421a6588e8f3cad8c2eb85b
ChrisBAshton <christopher.ashton@dsit.gov.uk>
2026-05-26 09:52:54 +0100

    Add DefaultTextarea component
    Sometimes we want a multi-line input that isn't Govspeak and
    should not have the 'inline preview' feature.

---
+++ test/unit/app/models/configurable_content_blocks/default_textarea_test.rb
@@ -0,0 +1,86 @@
+require "test_helper"
+
+class ConfigurableContentBlocks::DefaultTextareaRenderingTest < ActionView::TestCase
+  include ConfigurableContentBlockSharedTests
+
+  setup do
+    @field = {
+      "block" => "default_textarea",
+      "title" => "Test attribute",
+      "description" => "A test attribute",
+      "attribute_path" => %w[block_content test_attribute],
+      "translatable" => true,
+    }
+    @path = Path.new(%w[block_content test_attribute])
+    ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type", {
+      "forms" => {
+        "documents" => {
+          "fields" => {
+            "test_attribute" => @field,
+          },
+        },
+      },
+      "schema" => {
+        "attributes" => {
+          "test_attribute" => {
+            "type" => "string",
+          },
+        },
+      },
+    }))
+    @edition = StandardEdition.new(
+      configurable_document_type: "test_type",
+      block_content: { "test_attribute" => "foo" },
+    )
+    @block = ConfigurableContentBlocks::DefaultTextarea.new(@edition, @field, @path)
+  end
+
+  test "the form label is equal to the attribute title" do
+    render @block
+    assert_dom "label", text: @field["title"]
+  end
+
+  test "it adds a required message to the label when the attribute is required" do
+    @field["required"] = true
+    render @block
+    assert_dom "label", text: "#{@field['title']} (required)"
+  end
+
+  test "it sets the textarea name correctly" do
+    render @block
+    assert_dom "textarea[name=?]", "edition[block_content][test_attribute]"
+  end
+
+  test "it sets the textarea value based on the content" do
+    render @block
+    assert_dom "textarea", text: @edition.block_content["test_attribute"]
+  end
+
+  test "it sets the hint text based on the description" do
+    render @block
+    assert_dom ".govuk-hint", text: @field["description"]
+  end
+
+  test "it sets the direction on the input to right to left when the current locale is Arabic" do
+    with_locale(:ar) do
+      render @block
+    end
+    assert_dom "textarea[dir=\"rtl\"]"
+  end
+
+  test "it renders the primary locale content under the textarea when the current locale is different from the primary locale" do
+    with_locale(:es) do
+      render @block
+    end
+
+    assert_dom ".govuk-details__text", text: @edition.block_content["test_attribute"]
+  end
+
+  test "it renders any validation errors when they are present" do
+    messages = %w[foo bar]
+    messages.each { |m| @edition.errors.add(:test_attribute, m) }
+
+    render @block
+    assert_dom ".govuk-error-message", "Error: #{messages.map { |m| "Test attribute #{m}" }.join}"
+  end
+end
