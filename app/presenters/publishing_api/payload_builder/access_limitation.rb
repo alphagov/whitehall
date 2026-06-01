9f222d0c800d7f9d3b9bd4428b268aee6dd305be
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2026-06-01 13:44:30 +0100

    Build payload for access limiting

--- app/presenters/publishing_api/payload_builder/access_limitation.rb
+++ app/presenters/publishing_api/payload_builder/access_limitation.rb
@@ -4,7 +4,32 @@ module PublishingApi
       def self.for(item)
         return {} unless item.access_limited? && !item.publicly_visible?
 
-        { access_limited: { organisations: item.organisations.pluck(:content_id).uniq } }
+        access_limited = {}
+
+        if item.respond_to?(:named_users?) && item.named_users?
+          emails = item.edition_user_accesses.pluck(:email)
+
+          uids = emails.filter_map { |email|
+            begin
+              if Services.signon_api_client.respond_to?(:user_by_email)
+                response = Services.signon_api_client.user_by_email(email)
+                response && response["uid"]
+              end
+            rescue StandardError => e
+              Rails.logger.error("Signon UID lookup failed for #{email}: #{e.message}")
+              nil
+            end
+          }.uniq
+
+          access_limited[:users] = uids unless uids.empty?
+        else
+          organisations = item.organisations.pluck(:content_id).uniq
+          access_limited[:organisations] = organisations unless organisations.empty?
+        end
+
+        return {} if access_limited.empty?
+
+        { access_limited: access_limited }
       end
     end
   end
--- test/unit/app/presenters/publishing_api/payload_builder/access_limitation_test.rb
+++ test/unit/app/presenters/publishing_api/payload_builder/access_limitation_test.rb
@@ -3,39 +3,105 @@ require "test_helper"
 module PublishingApi
   module PayloadBuilder
     class AccessLimitationTest < ActiveSupport::TestCase
+      def build_item(overrides = {})
+        defaults = {
+          access_limited?: true,
+          publicly_visible?: false,
+          named_users?: false,
+        }
+
+        stub(defaults.merge(overrides))
+      end
+
+      def stub_signon(uid:)
+        response = uid ? { "uid" => uid } : nil
+
+        Services.stubs(:signon_api_client).returns(
+          stub(user_by_email: response),
+        )
+      end
+
+      def access_limited_result(item)
+        AccessLimitation.for(item)[:access_limited]
+      end
+
       test "returns access limitation data for the item" do
         organisation = create(:organisation)
 
-        stubbed_item = stub(
-          access_limited?: true,
-          publicly_visible?: false,
+        item = build_item(
           organisations: [organisation],
+          organisations?: true,
         )
-        expected_hash = {
-          access_limited: {
-            organisations: [organisation.content_id],
-          },
-        }
 
-        assert_equal AccessLimitation.for(stubbed_item), expected_hash
+        assert_equal(
+          { organisations: [organisation.content_id] },
+          access_limited_result(item),
+        )
       end
 
-      test "it returns an empty hash if the item is not access limited" do
-        stubbed_item = stub(access_limited?: false, publicly_visible?: false)
+      test "returns only organisations when access limited by organisation" do
+        org1 = create(:organisation)
+        org2 = create(:organisation)
 
-        assert_equal({}, AccessLimitation.for(stubbed_item))
+        item = build_item(
+          organisations: [org1, org2],
+          organisations?: true,
+        )
+
+        result = access_limited_result(item)
+
+        assert_equal(
+          [org1.content_id, org2.content_id].sort,
+          result[:organisations].sort,
+        )
+
+        assert_nil result[:users]
       end
 
-      test "it returns an empty hash if the item is publicly visible" do
-        stubbed_item = stub(access_limited?: true, publicly_visible?: true)
+      test "returns user uids for named users" do
+        item = build_item(
+          named_users?: true,
+          edition_user_accesses: stub(pluck: ["user@example.com"]),
+        )
 
-        assert_equal({}, AccessLimitation.for(stubbed_item))
+        stub_signon(uid: "uid-1")
+
+        assert_equal(
+          { users: %w[uid-1] },
+          access_limited_result(item),
+        )
       end
 
-      test "it returns an empty hash if the item is publicly visible and not access limited" do
-        stubbed_item = stub(access_limited?: false, publicly_visible?: true)
+      test "returns empty hash when named users resolve to no UIDs" do
+        item = build_item(
+          named_users?: true,
+          edition_user_accesses: stub(pluck: ["unknown@example.com"]),
+        )
 
-        assert_equal({}, AccessLimitation.for(stubbed_item))
+        stub_signon(uid: nil)
+
+        assert_equal({}, AccessLimitation.for(item))
+      end
+
+      test "returns empty hash if not access limited" do
+        item = build_item(access_limited?: false)
+
+        assert_equal({}, AccessLimitation.for(item))
+      end
+
+      test "returns empty hash if publicly visible" do
+        item = build_item(publicly_visible?: true)
+
+        assert_equal({}, AccessLimitation.for(item))
+      end
+
+      test "returns empty hash if publicly visible and not access limited" do
+        item = build_item(
+          access_limited?: false,
+          publicly_visible?: true,
+        )
+
+        assert_equal({}, AccessLimitation.for(item))
       end
     end
   end
