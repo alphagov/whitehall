alphagov/whitehall

commit 6e46574e354cf70927bfd806620a4e2d12cdcc3e

Condolidated all methods that check if Organisations can be enabled for a document type.

app/models/speech.rb
@@ -57,6 +57,10 @@ class Speech < Edition
     PublishingApi::SpeechPresenter
   end
 
+  def organisation_association_enabled?
+    !can_have_some_invalid_data? && person_override.blank?
+  end
+
 private
 
   def date_for_government
@@ -65,9 +69,5 @@ private
     else
       super
     end
-  end
-
-  def skip_organisation_validation?
-    can_have_some_invalid_data? || person_override.present?
   end
 end

app/controllers/admin/edition_access_limited_controller.rb
@@ -66,7 +66,7 @@ private
   end
 
   def changed?
-    if @edition.can_be_related_to_organisations?
+    if @edition.organisation_association_enabled?
       @edition.changed? || @edition.edition_organisations != Edition.find(params[:id]).edition_organisations
     else
       @edition.changed?

app/controllers/admin/editions_controller.rb
@@ -385,7 +385,7 @@ private
   end
 
   def build_default_organisation
-    if @edition.can_be_related_to_organisations?
+    if @edition.organisation_association_enabled?
       @edition.edition_organisations.build(lead_ordering: 0, lead: true, organisation: current_user.organisation)
     end
   end

app/controllers/admin/standard_editions_controller.rb
@@ -1,6 +1,6 @@
 # Trying to figure out how configurable document types work? Check the documentation: `docs/configurable_document_types.md`!
 class Admin::StandardEditionsController < Admin::EditionsController
-  before_action :prevent_access_when_disabled
+  prepend_before_action :prevent_access_when_disabled
   def choose_type
     @permitted_configurable_document_types = ConfigurableDocumentType.all.select { |type| can?(current_user, type) }
   end

app/models/concerns/edition/base_permission_methods.rb
@@ -9,11 +9,11 @@ module Edition::BasePermissionMethods
     can_be_associated_with_role_appointments?
     can_be_associated_with_worldwide_organisations?
     can_be_fact_checked?
-    can_be_related_to_organisations?
     can_apply_to_subset_of_nations?
     can_be_grouped_in_collections?
     is_associated_with_a_minister?
     can_be_tagged_to_worldwide_taxonomy?
+    organisation_association_enabled?
   ].each do |method|
     define_method(method) { false }
   end

app/models/concerns/edition/organisations.rb
@@ -13,8 +13,8 @@ module Edition::Organisations
     has_many :edition_organisations, foreign_key: :edition_id, dependent: :destroy, autosave: true, validate: false
     has_many :organisations, -> { includes(:translations) }, through: :edition_organisations, validate: false
 
-    validate :at_least_one_lead_organisation
-    validate :no_duplication_of_organisations
+    validate :at_least_one_lead_organisation, if: :organisation_association_enabled?
+    validate :no_duplication_of_organisations, if: :organisation_association_enabled?
 
     add_trait Trait
   end
@@ -59,16 +59,12 @@ module Edition::Organisations
     assign_organisation_ids(new_supporting_organisation_ids, false)
   end
 
-  def can_be_related_to_organisations?
-    true
-  end
-
   def can_have_supporting_organisations?
     true
   end
 
-  def skip_organisation_validation?
-    false
+  def organisation_association_enabled?
+    true
   end
 
   def limits_access_via_organisations?
@@ -78,7 +74,7 @@ module Edition::Organisations
 private
 
   def at_least_one_lead_organisation
-    if !skip_organisation_validation? && !edition_organisations.detect(&:lead?)
+    unless edition_organisations.detect(&:lead?)
       errors.add(:lead_organisations, "at least one required")
     end
   end

app/models/corporate_information_page.rb
@@ -56,10 +56,6 @@ class CorporateInformationPage < Edition
     false
   end
 
-  def skip_organisation_validation?
-    true
-  end
-
   def translatable?
     !non_english_edition?
   end

app/models/news_article.rb
@@ -65,8 +65,8 @@ class NewsArticle < Edition
     !world_news_story?
   end
 
-  def skip_organisation_validation?
-    world_news_story?
+  def organisation_association_enabled?
+    !world_news_story?
   end
 
   def skip_world_location_validation?

commit 6144db91c429b254d338d63d9dbbc17f985621c8

Update speech to override `required?` method for organisation validation
This fixes some issues introduced in `6e46574e`, causing the organisations fields to disappear from the form when a value for the person override is provided (the "Speaker does not have a profile on GOV.UK" field is populated). With these changes, the validations for organisations are separated from the logic that checks if organisations are enabled, ensuring we continue to render the organisation fields.

What that commit did:
- replace `can_be_related_to_organisations?` with  `organisation_association_enabled?`
- but also replace `skip_organisation_validation?` with `organisation_association_enabled?`

Speeches used to override `skip_organisation_validation?` but they would always keep `can_be_related_to_organisations?` true from the `Edition::Organisations` concern value. This got changed in commit `6e46574e``:
- the method `organisation_association_enabled?` started conditionally rendering fields in the `_organisation_fields.html.erb` partial, but also conditionally validating the lead organisations for presence
- speeches with a `person_override` would not require lead orgs, but they'd also render without the organisations fields at all, because the logic for enabled and required organisations got merged into a single method.

The cases affected in that commit were news article, corporate information pages, speeches and standard editions. News articles are now standard editions and work as expected due to the implicit separation of types and declarations of associations in the json schemas. Corporate information pages never render org fields, nor do they include the `Organisations` concern, so their `organisation_association_enabled?` should always be false, which currently comes from the value in `base_permission_methods.rb`. Speeches were the only remaining problematic case.

app/models/speech.rb
@@ -52,7 +52,7 @@ class Speech < Edition
     PublishingApi::SpeechPresenter
   end
 
-  def organisation_association_enabled?
+  def lead_organisation_association_required?
     !can_have_some_invalid_data? && person_override.blank?
   end
 

test/unit/app/models/speech_test.rb
@@ -44,11 +44,21 @@ class SpeechTest < ActiveSupport::TestCase
     assert_not speech.valid?
   end
 
-  test "does not require an organisation or role appointment if a person_override is given" do
+  test "requires an organisation and displays organisation fields, if a person_override and role_appointment are not given" do
+    speech = build(:speech, person_override: nil, role_appointment: nil, create_default_organisation: false)
+
+    assert_not speech.valid?
+    assert speech.errors.include?(:lead_organisation_ids)
+    assert speech.organisation_association_enabled?
+    assert speech.lead_organisation_association_required?
+  end
+
+  test "does not require an organisation or role appointment if a person_override is given, and displays org fields" do
     speech = build(:speech, person_override: "The Queen", role_appointment: nil, create_default_organisation: false)
-    assert speech.person_override?
-    speech.save!
-    assert_equal [], speech.reload.organisations
+
+    assert speech.valid?
+    assert speech.organisation_association_enabled?
+    assert_not speech.lead_organisation_association_required?
   end
 
   test "#display_type returns en locale values" do