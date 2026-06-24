8a6253bf5042bc328d40c968331193ed7ab88f06
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-06-24 07:16:57 +0100

    Filter out duplicate topical event content IDs
    During the migration we may have both a TopicalEvent and a Document
    sharing the same content ID, and both having an association with
    the same featured document. We need to filter out any duplicates.

--- app/presenters/publishing_api/payload_builder/topical_events.rb
+++ app/presenters/publishing_api/payload_builder/topical_events.rb
@@ -10,7 +10,7 @@ module PublishingApi
         legacy_topical_events = item.topical_events.pluck(:content_id)
         config_driven_topical_events = item.topical_event_documents.pluck(:content_id)
         {
-          topical_events: legacy_topical_events + config_driven_topical_events,
+          topical_events: (legacy_topical_events + config_driven_topical_events).uniq,
         }
       end
     end
