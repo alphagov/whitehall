6792a8688e76d7dc24692c3b4d24eda2487a2385
Laura Ghiorghisor <laura.ghiorghisor@dsit.gov.uk>
2026-07-19 20:51:59 +0100

    Add test coverage for the election rake task
    Added:
    - Unfulfilled expectations around states other than `published`. The marking should also include withdrawn and unpublished content.
    - Test covering the un-marking behaviour; the existing rake task can be used to set the political flag to either true or false, based on what the `PoliticalContentIdentifier` evaluates to.
    - Characterisation test covering that editions that were manually made political will be reverted by the 'un-marking' task run. This is not ideal, as a user's manual choice should likely be respected when we revert the political marker.

--- test/unit/lib/tasks/election_identify_political_content_test.rb
+++ test/unit/lib/tasks/election_identify_political_content_test.rb
@@ -26,15 +26,77 @@ class IdentifyPoliticalContentFor < ActiveSupport::TestCase
     end
 
     it "marks eligible published editions of documents first published after the specified date and any associated drafts as political" do
-      document = create(:document)
-      published_edition = create(:publication, :published, document:, first_published_at: "01-01-2023")
-      draft_edition = create(:publication, :draft, document:)
-      organisation.editions = [published_edition, draft_edition]
+      published_document = create(:document)
+      published_edition = create(:publication, :published, document: published_document, first_published_at: "01-01-2023")
+      draft_of_published_edition = create(:publication, :draft, document: published_document)
+      withdrawn_document = create(:document)
+      withdrawn_edition = create(:publication, :withdrawn, document: withdrawn_document, first_published_at: "01-01-2023")
+      draft_of_withdrawn_edition = create(:publication, :draft, document: withdrawn_document)
+      unpublished_document = create(:document)
+      unpublished_edition = create(:publication, :unpublished, document: unpublished_document, first_published_at: "01-01-2023")
+      draft_of_unpublished_edition = create(:publication, :draft, document: unpublished_document)
+      organisation.editions = [published_edition, draft_of_published_edition, withdrawn_edition, draft_of_withdrawn_edition, unpublished_edition, draft_of_unpublished_edition]
       organisation.save!
 
+      # All editions are "potentially" political by virtue of their type
+      assert([published_edition, withdrawn_edition, unpublished_edition].pluck(:publication_type).each { |type| PoliticalContentIdentifier::POLITICAL_PUBLICATION_TYPES.include?(type) })
+
       capture_io { task.invoke(organisation.slug, date) }
+
       assert published_edition.reload.political
-      assert draft_edition.reload.political
+      assert draft_of_published_edition.reload.political
+      # These states are not included
+      assert_not withdrawn_edition.reload.political
+      assert_not draft_of_withdrawn_edition.reload.political
+      assert_not unpublished_edition.reload.political
+      assert_not draft_of_unpublished_edition.reload.political
+    end
+
+    it "unmarks eligible published editions of documents first published after the specified date as not political" do
+      organisation.update!(political: false)
+      published_document = create(:document)
+      published_edition = create(:publication, :published, document: published_document, first_published_at: "01-01-2023", political: true)
+      draft_of_published_edition = create(:publication, :draft, document: published_document, political: true)
+      withdrawn_document = create(:document)
+      withdrawn_edition = create(:publication, :withdrawn, document: withdrawn_document, first_published_at: "01-01-2023", political: true)
+      draft_of_withdrawn_edition = create(:publication, :draft, document: withdrawn_document, political: true)
+      unpublished_document = create(:document)
+      unpublished_edition = create(:publication, :unpublished, document: unpublished_document, first_published_at: "01-01-2023", political: true)
+      draft_of_unpublished_edition = create(:publication, :draft, document: unpublished_document, political: true)
+      organisation.editions = [published_edition, draft_of_published_edition, withdrawn_edition, draft_of_withdrawn_edition, unpublished_edition, draft_of_unpublished_edition]
+      organisation.save!
+
+      # All editions are "potentially" political by virtue of their type
+      assert([published_edition, withdrawn_edition, unpublished_edition].pluck(:publication_type).each { |type| PoliticalContentIdentifier::POLITICAL_PUBLICATION_TYPES.include?(type) })
+
+      capture_io { task.invoke(organisation.slug, date) }
+
+      assert_not published_edition.reload.political
+      assert_not draft_of_published_edition.reload.political
+      # These states are not included
+      assert withdrawn_edition.reload.political
+      assert draft_of_withdrawn_edition.reload.political
+      assert unpublished_edition.reload.political
+      assert draft_of_unpublished_edition.reload.political
+    end
+
+    # This is a characterisation test. Ideally, manually set political markers would not be affected by the task.
+    it "unmarks political flag set by the user for non-political document types" do
+      organisation.update!(political: true)
+      published_document = create(:document)
+      non_political_publication_type = PublicationType.all.detect { |type| PoliticalContentIdentifier::POLITICAL_PUBLICATION_TYPES.exclude?(type) }
+      published_edition = create(:publication, :published, publication_type: non_political_publication_type, document: published_document, first_published_at: "01-01-2023", political: true)
+      organisation.editions = [published_edition]
+      organisation.save!
+
+      # Confirm that the edition was manually set to political
+      assert_not PoliticalContentIdentifier.political?(published_edition)
+      assert published_edition.political
+
+      organisation.update!(political: false)
+      capture_io { task.invoke(organisation.slug, date) }
+
+      assert_not published_edition.reload.political
     end
 
     it "does not mark editions unrelated to documents first published before the specified date as political" do
