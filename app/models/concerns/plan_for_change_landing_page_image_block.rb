9e0299099a4dd996745d5ca5604a4524bb3e5d84
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-07-24 16:49:45 +0100

    Suppress validation on Plan for Change Landing Page pages
    These are now deprecated and will redirect to the National Archives.
    In the meantime, if we need to publish anything to these routes,
    we don't want to be tripped up by validation errors.

--- app/models/concerns/plan_for_change_landing_page_image_block.rb
+++ app/models/concerns/plan_for_change_landing_page_image_block.rb
@@ -11,14 +11,14 @@ module PlanForChangeLandingPageImageBlock
   included do
     attr_reader :desktop_image, :tablet_image, :mobile_image, :alt
 
-    validates :desktop_image, :tablet_image, :mobile_image, presence: true
-    validates_each :desktop_image, :tablet_image, :mobile_image do |record, attr, value|
-      next if value.blank?
+    # validates :desktop_image, :tablet_image, :mobile_image, presence: true
+    # validates_each :desktop_image, :tablet_image, :mobile_image do |record, attr, value|
+    #   next if value.blank?
 
-      expected_kind = EXPECTED_IMAGE_KINDS.fetch(attr)
-      actual_kind = value.image_data.image_kind
-      record.errors.add(attr, "is of the wrong image kind: #{actual_kind}") if actual_kind != expected_kind
-    end
+    #   expected_kind = EXPECTED_IMAGE_KINDS.fetch(attr)
+    #   actual_kind = value.image_data.image_kind
+    #   record.errors.add(attr, "is of the wrong image kind: #{actual_kind}") if actual_kind != expected_kind
+    # end
 
     def present_image
       {
@@ -31,12 +31,12 @@ module PlanForChangeLandingPageImageBlock
 
     def present_image_sources
       {
-        "desktop" => desktop_image.url(:landing_page_desktop_1x),
-        "desktop_2x" => desktop_image.url(:landing_page_desktop_2x),
-        "tablet" => tablet_image.url(:landing_page_tablet_1x),
-        "tablet_2x" => tablet_image.url(:landing_page_tablet_2x),
-        "mobile" => mobile_image.url(:landing_page_mobile_1x),
-        "mobile_2x" => mobile_image.url(:landing_page_mobile_2x),
+        "desktop" => desktop_image&.url(:landing_page_desktop_1x),
+        "desktop_2x" => desktop_image&.url(:landing_page_desktop_2x),
+        "tablet" => tablet_image&.url(:landing_page_tablet_1x),
+        "tablet_2x" => tablet_image&.url(:landing_page_tablet_2x),
+        "mobile" => mobile_image&.url(:landing_page_mobile_1x),
+        "mobile_2x" => mobile_image&.url(:landing_page_mobile_2x),
       }
     end
 
