48dd9039e0101a299f3ce0bac0ff8ffd4ee31407
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-07-30 14:22:08 +0100

    Backfill test for "publish redirect on Publication publish" logic
    This logic was added in 2016 in ec88bba4fae569ecd02de4eab8e64789fc03f1d6
    and covered by an integration test:
    https://github.com/alphagov/whitehall/blob/a6c5b68a5d83c61083026721bf8669cf34216627/test/integration/statistics_announcement_test.rb#L160-L168
    
    But it can also be expressed as a unit test, which we'll expand on
    in the next commit.

--- test/unit/app/models/statistics_announcement_test.rb
+++ test/unit/app/models/statistics_announcement_test.rb
@@ -7,6 +7,13 @@ class StatisticsAnnouncementTest < ActiveSupport::TestCase
     assert_equal PublicationType::OfficialStatistics, announcement.publication_type
   end
 
+  test "publish_redirect_to_publication publishes a redirect to the publication URL (on touch) if the publication is published" do
+    announcement = create(:statistics_announcement)
+    publication = create(:published_statistics, publication_type_id: PublicationType::OfficialStatistics.id, statistics_announcement: announcement)
+    Whitehall::PublishingApi.expects(:publish_redirect_async).with(announcement.content_id, publication.base_path)
+    announcement.touch
+  end
+
   test "only statistical publication types are valid" do
     assert build(:statistics_announcement, publication_type_id: PublicationType::OfficialStatistics.id).valid?
     assert build(:statistics_announcement, publication_type_id: PublicationType::NationalStatistics.id).valid?
