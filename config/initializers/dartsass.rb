841ef53aaf3720acc57c0c5d9efaabfbf2b7bd95
David Sauntson <david.sauntson@digital.cabinet-office.gov.uk>
2026-08-05 13:21:47 +0100

    Silence `@import` deprecation warnings
    We will need to wait for `govuk-frontend` (and subsequently `govuk_publishing_gem`) to remove the usage of `@import` rules before we can deal with these warnings.  The frontend team have this on their roadmap for v7 (https://github.com/alphagov/govuk-frontend/milestone/89),so it will be a while before we can address this issue properly in whitehall.  In the meantime we can silence these warnings.  We are currently on v1.120 (in this branch) and the deprecation is not happening to v3 of dartsass so there is plenty of time.

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -6,3 +6,4 @@ all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.compon
 Rails.application.config.dartsass.builds = all_stylesheets
 
 Rails.application.config.dartsass.build_options << " --quiet-deps"
+Rails.application.config.dartsass.build_options << " --silence-deprecation=import"
