28744526937e72bee77405109c5b3c596d86da1a
Jamie Stamp <jamie.stamp@digital.cabinet-office.gov.uk>
2026-08-18 09:27:31 +0100

    Update the radio button options for access-limiting
    Updates the options for access limiting to:
    
    Limit access:
      - No – make available to all publishers
      - Yes – limit access to specific organisations
      - Yes – limit access to named publishers

--- features/step_definitions/admin_limit_access_steps.rb
+++ features/step_definitions/admin_limit_access_steps.rb
@@ -31,7 +31,7 @@ When(/^I set the Lead organisation to an org I am not in$/) do
 end
 
 When(/^I choose organisation access limiting$/) do
-  choose "Limit access to the following organisations"
+  choose "Yes – limit access to specific organisations"
 end
 
 When(/^I select "([^"]*)" as an access limiting organisation$/) do |org_name|
