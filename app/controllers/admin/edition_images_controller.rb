8bdb633d42ab10029ebccd8436cda0fad54fa604
Patrick Cartlidge <patrick.cartlidge@digital.cabinet-office.gov.uk>
2026-08-19 16:17:46 +0100

    revert

--- app/controllers/admin/edition_images_controller.rb
+++ app/controllers/admin/edition_images_controller.rb
@@ -39,7 +39,9 @@ class Admin::EditionImagesController < Admin::BaseController
   def update
     image.assign_attributes(image_params)
 
-    cookie = request.headers["Cookie"]  
+    # cookie = request.headers["Cookie"]
+
+    # binding.pry
 
     if image_data_params["crop_data"].present?
       image_data = image.image_data
@@ -47,10 +49,10 @@ class Admin::EditionImagesController < Admin::BaseController
       new_image_data.images << image
       new_image_data.to_replace_id = image_data.id
       new_image_data.assign_attributes(image_data_params)
-      # new_image_data.file = image_data_to_file(image_data)
-      new_image_data.file.download!(image_data.file.url, {
-        "Cookie" => cookie
-      })
+      new_image_data.file = image_data_to_file(image_data)
+      # new_image_data.file.download!(image_data.file.url, {
+      #   "Cookie" => cookie
+      # })
       # so that auth_bypass_id is discoverable by AssetManagerStorage
       new_image_data.images << image
       new_image_data.save!
