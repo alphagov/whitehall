31fca2ac42a0fec33021cd08aa24425ace35cdf2
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-09-08 10:57:27 +0100

    Add 'Processing' and dynamic swap-out of image upon load
    This behaviour already exists on the cropping screen and on the
    specialised images area (e.g. Header logo), but general embeddable
    images still got stuck displaying "Processing" and required users
    to refresh the page.

--- test/components/admin/edition_images/image_card_component_test.rb
+++ test/components/admin/edition_images/image_card_component_test.rb
@@ -58,6 +58,15 @@ class Admin::EditionImages::ImageCardComponentTest < ViewComponent::TestCase
     assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Caption\")) .govuk-summary-list__value", text: "Not set"
   end
 
+  test "renders 'Not set' when image is missing" do
+    ConfigurableDocumentType.setup_test_types(build_configurable_document_type("test_type"))
+    edition = build_stubbed(:standard_edition, images: [])
+
+    render_inline(Admin::EditionImages::ImageCardComponent.new(edition:, image: nil, image_usage: ImageUsage.new(key: "test_usage", label: "Test usage")))
+
+    assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Image\")) .govuk-summary-list__value", text: "Not set"
+  end
+
   test "renders the 'PROCESSING' tag when image has missing assets" do
     image_data = build_stubbed(:image_data_with_no_assets, image_kind: "default")
     image = build_stubbed(:image, image_data:)
@@ -66,7 +75,7 @@ class Admin::EditionImages::ImageCardComponentTest < ViewComponent::TestCase
 
     render_inline(Admin::EditionImages::ImageCardComponent.new(edition:, image:, image_usage: ImageUsage.new(key: "test_usage", label: "Test usage")))
 
-    assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Image\")) .govuk-summary-list__value", text: "Processing"
+    assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Image\")) .govuk-summary-list__value .js-image-processing-status .govuk-tag--green", text: "Processing"
   end
 
   test "renders the 'Requires crop' tag when the image needs cropping" do
@@ -77,7 +86,7 @@ class Admin::EditionImages::ImageCardComponentTest < ViewComponent::TestCase
 
     render_inline(Admin::EditionImages::ImageCardComponent.new(edition:, image:, image_usage: ImageUsage.new(key: "test_usage", label: "Test usage")))
 
-    assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Image\")) .govuk-summary-list__value", text: "Requires crop"
+    assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Image\")) .govuk-summary-list__value .govuk-tag--red", text: "Requires cropping"
   end
 
   test "does not render caption row when caption_enabled is false" do
@@ -98,7 +107,7 @@ class Admin::EditionImages::ImageCardComponentTest < ViewComponent::TestCase
 
     render_inline(Admin::EditionImages::ImageCardComponent.new(edition:, image:, image_usage: ImageUsage.new(key: "test_usage", label: "Test usage")))
 
-    assert_selector "img[src='#{image.thumbnail}']"
+    assert_selector ".govuk-summary-list__row:has(.govuk-summary-list__key:contains(\"Image\")) .govuk-summary-list__value img.app-view-edition-resource__preview[src='#{image.thumbnail}'][alt='']"
   end
 
   test "renders the markdown code for embeddable usages" do
