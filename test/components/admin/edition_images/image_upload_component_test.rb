0242c836e12a7d69e7b54f017fff8346cdc3dc3c
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-09-08 09:55:42 +0100

    Remove 'What kind of image is this?' radio buttons
    This was only used on the PfC pages, which have now been removed.
    It used to be that there was a shared image uploader for every kind
    of image, and the user had to specify what kind of image they were
    uploading in order to then get the correct validation etc on that
    image.
    
    Now, we have a dedicated image uploader for every image kind, so
    this branching logic is redundant.

--- test/components/admin/edition_images/image_upload_component_test.rb
+++ test/components/admin/edition_images/image_upload_component_test.rb
@@ -31,23 +31,12 @@ class Admin::EditionImages::ImageUploadComponentTest < ViewComponent::TestCase
     assert_selector "input[name=\"usage\"][value=\"test_usage\"]", visible: false
   end
 
-  test "renders hidden image data kind input for single usage" do
+  test "renders hidden image data kind input" do
     edition = build_stubbed(:draft_publication)
     usage = ImageUsage.new(key: "test_usage", kinds: [Whitehall.image_kinds.fetch("default")], multiple: false, label: "test")
     render_inline(Admin::EditionImages::ImageUploadComponent.new(edition:, image_usage: usage))
 
     assert_selector "input[name=\"image_kind\"][value=\"default\"]", visible: false
-  end
-
-  test "renders radio button inputs for multiple usage" do
-    edition = build_stubbed(:draft_publication)
-    image_kinds = [Whitehall.image_kinds.fetch("default"), Whitehall.image_kinds.fetch("topical_event_header")]
-    usage = ImageUsage.new(key: "test_usage", kinds: image_kinds, multiple: true, label: "test")
-    render_inline(Admin::EditionImages::ImageUploadComponent.new(edition:, image_usage: usage))
-
-    image_kinds.each do |kind|
-      assert_selector "input[name=\"image_kind\"][value=\"#{kind.name}\"]"
-    end
   end
 
   test "renders inline errors for failed images" do
