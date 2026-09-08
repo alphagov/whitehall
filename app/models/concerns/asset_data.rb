d1ad79686830df4eb885d3341c7b44938f26d478
Patrick Cartlidge <patrick.cartlidge@digital.cabinet-office.gov.uk>
2026-09-08 11:21:12 +0100

    Move `all_asset_variants_uploaded?` to `AssetData`

--- app/models/concerns/asset_data.rb
+++ app/models/concerns/asset_data.rb
@@ -17,6 +17,8 @@ module AssetData
 
   delegate :auth_bypass_id, to: :attachable
 
+  delegate :url, :path, :content_type, to: :file, allow_nil: true
+
   def attachable
     Attachable::Null.new
   end
@@ -105,6 +107,21 @@ module AssetData
     elsif significant_attachable.is_a?(PolicyGroup)
       significant_attachable.public_url
     end
+  end
+
+  def filename
+    file&.file&.filename
+  end
+
+  def all_asset_variants_uploaded?
+    asset_variants = assets.map(&:variant).map(&:to_sym)
+    required_variants = file.active_version_names + [:original]
+
+    (required_variants - asset_variants).empty?
+  end
+
+  def assets_match_updated_image_filename
+    assets.all? { |asset| asset.filename.include?(filename) } if filename
   end
 
 private
