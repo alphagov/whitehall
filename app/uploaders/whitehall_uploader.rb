ebf7c2f13aff0863e7be5ead3fff28b83cd2f724
Patrick Cartlidge <patrick.cartlidge@digital.cabinet-office.gov.uk>
2026-09-08 13:49:27 +0100

    rubocopy

--- app/uploaders/whitehall_uploader.rb
+++ app/uploaders/whitehall_uploader.rb
@@ -30,5 +30,5 @@ class WhitehallUploader < CarrierWave::Uploader::Base
     # active_versions is protected, so it can only be called by subclasses
     # it returns an array of [key, value] pairs, and we want the keys
     active_versions.map(&:first)
-  end  
+  end
 end
