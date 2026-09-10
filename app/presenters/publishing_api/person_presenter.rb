468ed099366a66d0861f8b265b9ccb2ccacdf83f
Catalina <96050928+catalinailie@users.noreply.github.com>
2026-09-10 09:13:25 +0100

    Revert "Change rendering app for Person pages to frontend"

--- app/presenters/publishing_api/person_presenter.rb
+++ app/presenters/publishing_api/person_presenter.rb
@@ -21,7 +21,7 @@ module PublishingApi
         details:,
         document_type: "person",
         public_updated_at: item.updated_at,
-        rendering_app: Whitehall::RenderingApp::FRONTEND,
+        rendering_app: Whitehall::RenderingApp::COLLECTIONS_FRONTEND,
         schema_name: "person",
       )
       content.merge!(PayloadBuilder::PolymorphicPath.for(item))
