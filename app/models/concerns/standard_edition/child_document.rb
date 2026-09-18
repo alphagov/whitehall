1dacaa03369837e52cd9d31ab6364dc0ebbdf2d2
Chris Ashton <christopher.ashton@dsit.gov.uk>
2026-09-18 09:57:41 +0100

    Add 'parent edition must exist' logic

--- app/models/concerns/standard_edition/child_document.rb
+++ app/models/concerns/standard_edition/child_document.rb
@@ -7,9 +7,20 @@ module StandardEdition::ChildDocument
             foreign_key: :child_document_id,
             primary_key: :document_id
     has_one :parent_edition, through: :parent_relationship
+
+    validate :parent_edition_must_exist
   end
 
   def parent_edition_id
     parent_relationship&.parent_edition_id
+  end
+
+private
+
+  def parent_edition_must_exist
+    return if parent_edition_id.blank?
+    return if StandardEdition.exists?(parent_edition_id)
+
+    errors.add(:parent_edition_id, "must correspond to an existing StandardEdition")
   end
 end
--- test/unit/app/models/concerns/standard_edition/child_document_test.rb
+++ test/unit/app/models/concerns/standard_edition/child_document_test.rb
@@ -45,4 +45,15 @@ class StandardEdition::ChildDocumentTest < ActiveSupport::TestCase
 
     assert_equal child_edition.reload.parent_edition_id, parent_edition.id
   end
+
+  test "parent_edition_must_exist validation works correctly" do
+    test_type = build_configurable_document_type("test_type")
+    ConfigurableDocumentType.setup_test_types(test_type)
+
+    child_edition = build(:standard_edition, configurable_document_type: "test_type")
+    child_edition.build_parent_relationship(parent_edition_id: 9999) # Non-existent parent edition
+
+    assert_not child_edition.valid?
+    assert_includes child_edition.errors[:parent_edition_id], "must correspond to an existing StandardEdition"
+  end
 end
