ce3507c19be3d5a12051641686b57d161adc1bc0
eYinka <olayinka.oladele@dsit.gov.uk>
2026-09-22 10:00:20 +0100

    Validate a child document's type against its parent's allowed list When a document is created with a parent_edition_id, its own type must be in the parents document's configured `allowed_child_document_types` list, otherwise it is rejected with a validation error.

--- app/models/concerns/standard_edition/child_document.rb
+++ app/models/concerns/standard_edition/child_document.rb
@@ -9,6 +9,7 @@ module StandardEdition::ChildDocument
     has_one :parent_edition, through: :parent_relationship
 
     validate :parent_edition_must_exist
+    validate :child_document_type_must_be_allowed_by_parent
   end
 
   def parent_edition_id
@@ -31,5 +32,15 @@ private
     return if StandardEdition.exists?(parent_edition_id)
 
     errors.add(:parent_edition_id, "must correspond to an existing StandardEdition")
+  end
+
+  def child_document_type_must_be_allowed_by_parent
+    return if parent_edition_id.blank?
+
+    parent = StandardEdition.find_by(id: parent_edition_id)
+    return if parent.nil? # handled by parent_edition_must_exist
+    return if parent.type_instance.allows_child_document_type?(configurable_document_type)
+
+    errors.add(:parent_edition_id, "must be configured to allow this child document")
   end
 end
